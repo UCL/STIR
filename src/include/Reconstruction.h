d09d3e7eb5175dbddf4afcb29b0d49708f48af13
Darren Hague <Darren.Hague@stir>
1997-11-13 17:18:56 +0000

    Fixed header file stuff

--- src/include/Reconstruction.h
+++ src/include/Reconstruction.h
@@ -1,8 +1,11 @@
 #ifndef __RECONSTRUCTION_H__
 #define __RECONSTRUCTION_H__
 
-// $Id$ : $Date$
-
+//#include "PARAPET.h"
+#include <Filter.h>
+#include <String.h>
+#include <strstream.h>
+#include <PROMIS/glgtime.h>
 /*
    reconstruction methods are also defined as classes.
    use as follows:
@@ -13,14 +16,6 @@
 		
 	recon.reconstruct(s,v);
  */
-#include "imagedata.h"
-#include "sinodata.h"
-#include "filter.h"
-#include <strstream.h>
-
-//KT 11/11 glgtime not used here, and it shouldn't be anyway
-//#include "glgtime.h"
-
 
 extern PETSinogramOfVolume& CorrectForAttenuation(PETSinogramOfVolume s, PETSinogramOfVolume a);
 extern PETSegmentBySinogram& CorrectForAttenuation(PETSegmentBySinogram& s, PETSegmentBySinogram& a);
@@ -28,9 +23,9 @@ extern PETSegmentBySinogram& CorrectForAttenuation(PETSegmentBySinogram& s, PETS
 class PETReconstruction
 {
 public:
-  virtual string method_info()
+  virtual String method_info()
     { return ""; }
-  virtual string parameter_info()
+  virtual String parameter_info()
     { return ""; }
   virtual void reconstruct(PETSinogramOfVolume, PETImageOfVolume) 
     = 0;
@@ -45,7 +40,7 @@ public:
   int delta_min;
   int delta_max;
   Filter filter;
-  virtual string parameter_info();
+  virtual String parameter_info();
   PETAnalyticReconstruction(int min, int max, Filter f);
 };
 
@@ -54,7 +49,7 @@ inline PETAnalyticReconstruction::PETAnalyticReconstruction
      :  delta_min(min), delta_max(max), filter(f)
 {};
 
-inline string PETAnalyticReconstruction::parameter_info()
+inline String PETAnalyticReconstruction::parameter_info()
 { char str[100];
 
   ostrstream s(str, 100);
@@ -78,7 +73,7 @@ inline PETIterativeReconstruction::PETIterativeReconstruction
 class PETPROMISReconstruction : public PETAnalyticReconstruction
 {
 public:
-  string method()
+  String method()
     { return("PROMIS"); }
   PETPROMISReconstruction(int min, int max, Filter f);
   void reconstruct(PETSinogramOfVolume, PETImageOfVolume);
@@ -94,9 +89,9 @@ inline PETPROMISReconstruction::PETPROMISReconstruction(int min, int max, Filter
 class PETReconstruction2D
 {
 public:
-  virtual string method_info()
+  virtual String method_info()
     { return ""; }
-  virtual string parameter_info()
+  virtual String parameter_info()
     { return ""; }
   virtual void reconstruct(PETSegmentBySinogram, PETImageOfVolume) 
     = 0;
@@ -105,86 +100,20 @@ public:
     { reconstruct(CorrectForAttenuation(s, a), v); }
 };
 
-class PETAnalyticReconstruction: public PETReconstruction
-{
- public:
-  int delta_min;
-  int delta_max;
-  Filter filter;
-  virtual string parameter_info();
-  PETAnalyticReconstruction(int min, int max, Filter f);
-};
-
-inline PETAnalyticReconstruction::PETAnalyticReconstruction
-     (int min, int max, Filter f)
-       :  delta_min(min), delta_max(max), filter(f)
-{};
-
-inline string PETAnalyticReconstruction::parameter_info()
-{ char str[100];
-
-  ostrstream s(str, 100);
-
-  s << "delta_min " << delta_min << " delta_max" << delta_max;
-  return str;
-}
-
-class PETIterativeReconstruction: public PETReconstruction
-{
- public:
-  int max_iterations;
-  PETIterativeReconstruction(int max);
-};
-
-inline PETIterativeReconstruction::PETIterativeReconstruction
-     (int max)
-       :  max_iterations(max)
-{}
-
-class PETPROMISReconstruction : public PETAnalyticReconstruction
-{
- public:
-  string method()
-    { return("PROMIS"); }
-  PETPROMISReconstruction(int min, int max, Filter f);
-  void reconstruct(PETSinogramOfVolume, PETImageOfVolume);
-};
-
-inline PETPROMISReconstruction::PETPROMISReconstruction(int min, int max, Filter f)
-  : PETAnalyticReconstruction(min, max, f)
-{
-}
-
-
-/******************* 2D reconstructions ************/
-class PETReconstruction2D
-{
- public:
-  virtual string method_info()
-    { return ""; }
-  virtual string parameter_info()
-    { return ""; }
-  virtual void reconstruct(PETSegmentBySinogram, PETImageOfVolume) 
-    = 0;
-  virtual void reconstruct(PETSegmentBySinogram s, 
-			      PETSegmentBySinogram a, PETImageOfVolume v) 
-    { reconstruct(CorrectForAttenuation(s, a), v); }
-};
-
 class PETAnalyticReconstruction2D: public PETReconstruction2D
 {
   Filter1D<float> *filter;
- public:
-  virtual string parameter_info();
+public:
+  virtual String parameter_info();
   PETAnalyticReconstruction2D(Filter1D<float> *f);
 };
 
 inline PETAnalyticReconstruction2D::PETAnalyticReconstruction2D
      (Filter1D<float> *f)
-       :  filter(f)
+     :  filter(f)
 {};
 
-inline string PETAnalyticReconstruction2D::parameter_info()
+inline String PETAnalyticReconstruction2D::parameter_info()
 { char str[100];
 
   ostrstream s(str, 100);
