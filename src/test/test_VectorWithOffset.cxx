02fa2eb0e326f7e4c24b8feb87e3b325545b9ce2
Kris Thielemans <Kris.Thielemans@stir>
1997-11-27 11:56:34 +0000

    Initial revision

---
+++ src/test/test_VectorWithOffset.cxx
@@ -0,0 +1,42 @@
+// $Id$: $Date$
+// Test program for VectorWithOffset.h
+// Version 1.0 : KT
+
+#include "VectorWithOffset.h"
+#ifdef __MSL__
+#include <algorithm.h>
+#include <numeric.h>
+#else
+#include <algo.h>
+#endif
+
+#include <iostream.h>
+
+main()
+{
+  cerr << "Test program for VectorWithOffset.h.\n"
+       << "Everythings is fine if the program runs without any output." << endl;
+  
+  VectorWithOffset<int> v(-3, 40);
+
+  iota(v.begin(), v.end(), -3);
+  assert(v[4] == 4);
+
+  {
+    int *p=find(v.begin(), v.end(), 6);
+    assert(p - v.begin() == 9);
+    assert(v.get_index(p) == 6);
+  }
+  {
+    VectorWithOffset<int>::reverse_iterator pr = v.rbegin();
+    assert((*pr++) == 40);
+    assert((*pr++) == 39);
+  }
+
+#ifndef __GNUG__
+  // sadly doesn't work under g++ 2.7
+  sort(v.rbegin(), v.rend());
+  assert(v[-3] == 40);
+  assert(v[0] == 37);
+#endif
+}
