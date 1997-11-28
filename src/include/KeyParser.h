8a06952876217a4cf99288385f3e45a3eee783b3
Patrick Valente <Patrick.Valente@stir>
1997-11-28 18:54:05 +0000

    Initial revision

---
+++ src/include/KeyParser.h
@@ -0,0 +1,105 @@
+
+#ifndef __KEYPARSER_H__
+#define __KEYPARSER_H__
+
+#include <map.h>
+#include <String.h>
+
+#include <vector.h>
+#include <fstream.h>
+
+#pragma warning(disable: 4786)
+
+#define		STATUS_END_PARSING	0
+#define		STATUS_PARSING		1
+
+#define		BYSINO				0
+#define		BYVIEW				1
+
+typedef enum {NONE,ASCII,ASCIIlist,LNUMBER,NUMBER,NUMBERlist,listASCIIlist} argtype;
+typedef vector<int> IntVect;
+
+
+class KeyParser;
+
+class map_element
+{
+public :
+	argtype type;
+	void (KeyParser::*p_object_member)();	// pointer to a member function
+	void* p_object_variable;				// pointer to a variable 
+
+	map_element();
+	~map_element();
+	
+	map_element& operator()(argtype t, void (KeyParser::*pom)(),void* pov= NULL);
+	map_element& operator=(const map_element& me);
+};
+
+
+	
+typedef map<String,map_element> Keymap;
+
+class KeyParser
+{
+private :					// varaibles
+	Keymap kmap;
+	fstream* input;
+	int	status;				// parsing 0,end_parsing 1
+	map_element* current;
+	int current_index;
+	String keyword;
+
+	vector<String>  par_asciilist;
+	IntVect			par_numberlist;
+	String			par_ascii;
+	int				par_number;	
+	long			par_lnumber;
+
+private :
+
+	int ParseLine();
+	int MapKeyword(String keyword);
+	void ChangeStatus(int value);
+	int ProcessKey();
+
+public :
+
+	// variables here
+	fstream*			in_stream;
+	int					max_r_index;
+	int					storage_order;
+	String				image_data_byte_order;
+	String				number_format;
+	int					bytes_per_pixel;
+	int					number_of_dimensions;
+	int					number_of_time_frames;
+	vector<String>		matrix_labels;
+	vector<IntVect>		matrix_size;
+	IntVect				sqc;
+	IntVect				image_scaling_factor;
+	vector<long int>	data_offset;
+	vector<String>		pettype;
+	vector<String>		index_nesting_level;
+public : 
+
+	// members
+
+	KeyParser();
+	KeyParser(fstream* f);
+	~KeyParser();
+
+	void Init();
+	int StartParsing();
+	
+private :
+
+
+	void ReadMatrixInfo();
+	void ReadFramesInfo();
+	void OpenFileStream();
+	void SetEndStatus();
+	void SetVariable();
+};
+
+#endif
---
+++ src/include/line.h
@@ -0,0 +1,29 @@
+
+#ifndef __LINE_H__
+#define __LINE_H__
+
+#include <pet_string.h>
+#include <vector.h>
+#define LINE_ERROR -1
+#define LINE_OK		0
+#define MAX_LINE_LENGTH 256
+
+class Line : private String
+{
+
+public :
+	Line() : String()
+	{}
+
+	Line& operator=(const char* ch);
+
+	String get_keyword();
+	int get_index();
+	int get_param(vector<int>& v);
+	int get_param(vector<String>& v);
+	int get_param(String& s);
+	int get_param(int& i);
+};
+
+#endif
+
