354c98fa4bf0335fc094589b2c7fba88654c8f35
Kris Thielemans <Kris.Thielemans@stir>
1998-05-27 17:18:09 +0000

    changed CPUTimer according to different systems

--- src/include/Timer.h
+++ src/include/Timer.h
@@ -2,7 +2,7 @@
 
 /*
   This file defines two classes: Timer and a derived class CPUTimer.
-  Version 1.0 by Kris Thielemans
+  Version 1.0 and 1.1 by Kris Thielemans
 
 
   Timer is a general base class for timers. Interface is like a stop watch:
@@ -20,24 +20,44 @@
   Derived classes simply have to implement the virtual function 
   double get_current_value()
 
+  CPUTimer is derived from Timer, and hence has the same interface. It
+  returns the amount of CPU time used by the current process and its 
+  children. If too long CPU times are measured, there will occur a
+  wrap-around. See below for details.
 
-  CPUTimer  is derived from Timer, and hence has the same interface.
-  CPUTimer::value() returns elapsed CPU time using the ANSI clock()
-  function.  From the man page: "The reported time is the sum
+  For most systems, CPUTimer::value() returns elapsed CPU time using the 
+  ANSI clock() function.  From the man page: "The reported time is the sum
   of the CPU time of the calling process and its terminated child
   processes for which it has executed wait, system, or pclose
   subroutines."
+  clock() has a possible problem of wrap-around. For many Unix systems this
+  occurs very soon. For a SUN, the man page states this is every 2147 secs 
+  of CPU time (36 minutes !). AIX should be similar, as both have 
+  CLOCKS_PER_SEC = 10e6, and clock_t == int == 32 bit.
 
-  Possible problem: 
-  When the internal counter used by clock() wraps around, there is no way
-  of detecting how many wraps occured, so I don't attempt to handle wraps 
-  at all.
-  For a SUN, the man page states this is every 2147 secs of CPU time (36 
-  minutes !). AIX should be similar, as both have CLOCKS_PER_SEC = 10e6,
-  and clock_t == int == 32 bit.
-  Visual C++ is more sensible and has  CLOCKS_PER_SEC = 1000, so wrap around
-  should occur only every 36000 minutes.
- 
+  So, since version 1.1 the times() function (with the rms_utime field) 
+  is used on AIX, SUN, OSF. The  man page states something very similar 
+  to clock():
+  "tms_utime       The CPU time used for executing instructions in the user
+   space of the calling process.
+
+   This information is read from the calling process as well as from
+   each completed child process for which the calling process executed
+   a wait subroutine."
+  As times() returns results measured in clock interrupt clicks (on AIX
+  100 per second), wrap around occurs much later.
+
+
+  Finally, VC++ 5.0 seems to have a bug in clock() that makes its results
+  dependent on the load of the system (i.e. it is more like 'wall clock 
+  time').
+  We now uses GetProcessTimes() (with the lpUserTime variable) instead. 
+  The documentation states:
+  "... the amount of time that the process has executed in user mode.
+  The time that each of the threads of the process has executed in user 
+  mode is determined, and then all of those times are summed together to 
+  obtain this value. 
+  Warning: the VC++ doc says that this only works for NT.
 */
 
 
@@ -96,9 +116,52 @@ public:
     }
 };
 
+#if defined(_AIX) || defined (sun) || defined(__osf__)
+// KT 27/05/98 use times() instead of clock() for AIX, sun, OSF.
+// This probably works on other Unixs as well, but I couldn't check that.
+
+#include <sys/times.h>
+#include <unistd.h>
+
+class CPUTimer : public Timer
+{
+private:
+  virtual double get_current_value() const
+    {  
+      struct tms t;
+      times(&t);
+      return double( t.tms_utime ) / sysconf(_SC_CLK_TCK);
+    }
+};
+
+#elif defined(_MSC_VER) && defined(_WIN32)
+// KT 27/05/98 VC++ 5.0 needs GetProcessTimes(), due to a bug in clock()
+// This possible breaks on Win95, but should work on WinNT
+#include <windows.h>
+
+class CPUTimer : public Timer
+{
+private:
+  virtual double get_current_value() const
+    {  
+      FILETIME CreationTime;  // when the process was created 
+      FILETIME ExitTime;  // when the process exited 
+      FILETIME KernelTime;  // time the process has spent in kernel mode 
+      FILETIME UserTime;  // time the process has spent in user mode 
+      LARGE_INTEGER ll;
+
+      GetProcessTimes(GetCurrentProcess(), 
+		      &CreationTime, &ExitTime, &KernelTime, &UserTime);
+
+      ll.LowPart = UserTime.dwLowDateTime;
+      ll.HighPart =  UserTime.dwHighDateTime; 
+
+      return double(ll.QuadPart ) / 1E7;
+    }
+};
+#else // all other systems
+
 #include <time.h>
-
-
 
 class CPUTimer : public Timer
 {
@@ -109,3 +172,4 @@ private:
     }
 };
 
+#endif
