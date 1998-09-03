b000b04228842429f6a82f7254143b23717a2973
Kris Thielemans <Kris.Thielemans@stir>
1998-09-03 12:39:50 +0000

    revamped

--- src/buildblock/KeyParser.cxx
+++ src/buildblock/KeyParser.cxx
@@ -1,39 +1,55 @@
+//
+// $Id$ :$Date$
+//
 
-#include <fstream.h>
+// KT 01/08/98 removed ! from keywords
+#include <iostream>
+#include <fstream>
 // KT 14/12 changed <> to "" in next 3 includes
 #include "KeyParser.h"
 #include "line.h"
-#include "pet_string.h"
+// KT 20/06/98 needed for PETerror
+#include "pet_common.h"
 
+// KT 20/06/98 do this pragma only for VC++
+#ifdef _MSC_VER
 #pragma warning(disable: 4786)
+#endif
 
 // map_element implementation;
 
 map_element::map_element()
 {
-	type=NONE;
-	p_object_member=NULL;
-	p_object_variable=NULL;
+  type=NONE;
+  p_object_member=NULL;
+  p_object_variable=NULL;
+  p_object_list_of_values=NULL;
 }
 
 map_element::~map_element()
 {
 }
 
-map_element& map_element::operator()(argtype t, void (KeyParser::*pom)(),void* pov)
+map_element& map_element::operator()(argtype t, 
+				     void (KeyParser::*pom)(),
+				     void* pov,
+				     const ASCIIlist_type *list_of_values)
 {
-	type=t;
-	p_object_member=pom;
-	p_object_variable=pov;
-	return *this;
+  type=t;
+  p_object_member=pom;
+  p_object_variable=pov;
+  p_object_list_of_values=list_of_values;
+  return *this;
 }
 
 map_element& map_element::operator=(const map_element& me)
 {
-	type=me.type;
-	p_object_member=me.p_object_member;
-	p_object_variable=me.p_object_variable;
-	return *this;
+  type=me.type;
+  p_object_member=me.p_object_member;
+  p_object_variable=me.p_object_variable;
+  // KT 28/07/98 new
+  p_object_list_of_values=me.p_object_list_of_values;
+  return *this;
 }
 
 
@@ -41,283 +57,455 @@ map_element& map_element::operator=(const map_element& me)
 
 KeyParser::KeyParser()
 {
-	status=STATUS_PARSING;
-	current_index=-1;
-	in_stream=NULL;
-	current=new map_element();
+  status=STATUS_PARSING;
+  current_index=-1;
+  in_stream=NULL;
+  current=new map_element();
 }
 
 KeyParser::KeyParser(fstream* f)
 {
-	input=f;
-	in_stream=NULL;
-	current_index=-1;
-	status=STATUS_PARSING;
-	current=new map_element();
+  input=f;
+  in_stream=NULL;
+  current_index=-1;
+  status=STATUS_PARSING;
+  current=new map_element();
 }
 
 KeyParser::~KeyParser()
 {
-
-	if(in_stream!=NULL)
-	{
-		in_stream->close();
-		delete in_stream;
-	}
-	//	delete current;   
+  // KT 01/08/98 leave this for calling program
+  
+#if 0
+  if(in_stream!=NULL)
+  {
+    in_stream->close();
+    delete in_stream;
+  }
+#endif
+  // Note: 'current' is not allocated with 'new', so does not need a 'delete'
 }
 
 void KeyParser::Init()
 {
-	map_element m;
-
-	kmap["!INTERFILE"]=					m(NONE,			NULL);
-	kmap["!name of data file"]=			m(ASCII,		&KeyParser::OpenFileStream);
-	kmap["image data byte order"]=		m(ASCII,		&KeyParser::SetVariable,&image_data_byte_order);
-	kmap["!PET STUDY (General)"]=		m(NONE,			NULL);
-// KT 14/122 changed next 2 types from ASCIIlist to ASCII
-	kmap["PET data type"]=				m(ASCII,	&KeyParser::SetVariable,&pettype);
-	kmap["data format"]=				m(ASCII,	NULL);
-	kmap["!number format"]=				m(ASCII,		&KeyParser::SetVariable,&number_format);
-	kmap["!number of bytes per pixel"]=	m(NUMBER,		&KeyParser::SetVariable,&bytes_per_pixel);
-	kmap["Number of dimensions"]=		m(NUMBER,		&KeyParser::ReadMatrixInfo,&number_of_dimensions);
-	kmap["!matrix size"]=				m(NUMBERlist,	&KeyParser::SetVariable,&matrix_size);
-	kmap["matrix axis label"]=			m(ASCII,		&KeyParser::SetVariable,&matrix_labels);
-	kmap["number of time frames"]=		m(NUMBER,		&KeyParser::ReadFramesInfo,&number_of_time_frames);
-	kmap["!PET STUDY (Emission data)"]=	m(NONE,			NULL);
-	kmap["segment storage order"]=		m(NUMBERlist,	&KeyParser::SetVariable,&sqc);
-	kmap["!IMAGE DATA DESCRIPTION"]=	m(NONE,			NULL);
-	kmap["!index nesting level"]=		m(ASCIIlist,	&KeyParser::SetVariable,&index_nesting_level);
-	kmap["!image scaling factor"]=		m(NUMBER,		&KeyParser::SetVariable,&image_scaling_factor);
-	kmap["data offset in bytes"]=		m(LNUMBER,		&KeyParser::SetVariable,&data_offset);
-	kmap["!END OF INTERFILE"]=			m(NONE,			&KeyParser::SetEndStatus);
-
-
+  map_element m;
+  
+  // KT 20/06/98 new, unfortunate syntax...
+  number_format_values.push_back("bit");
+  number_format_values.push_back("ascii");
+  number_format_values.push_back("signed integer");
+  number_format_values.push_back("unsigned integer");
+  number_format_values.push_back("float");
+  
+  // KT 01/08/98 new
+  byte_order_values.push_back("LITTLEENDIAN");
+  byte_order_values.push_back("BIGENDIAN");
+  
+  PET_data_type_values.push_back("Emission");
+  PET_data_type_values.push_back("Transmission");
+  PET_data_type_values.push_back("Blank");
+  PET_data_type_values.push_back("AttenuationCorrection");
+  PET_data_type_values.push_back("Normalisation");
+  PET_data_type_values.push_back("Image");
+  
+  type_of_data_values.push_back("Static");
+  type_of_data_values.push_back("Dynamic");
+  type_of_data_values.push_back("Tomographic");
+  type_of_data_values.push_back("Curve");
+  type_of_data_values.push_back("ROI");
+  type_of_data_values.push_back("PET");
+  type_of_data_values.push_back("Other");
+  
+  // default values
+  file_byte_order = ByteOrder::big_endian;
+  type_of_data_index = 6; // PET
+  
+  kmap["INTERFILE"]=			m(NONE,		NULL);
+  // KT 01/08/98 just set data_file_name variable now
+  kmap["name of data file"]=		m(ASCII,	&KeyParser::SetVariable, &data_file_name);
+  kmap["GENERAL DATA"]=			m(NONE,		NULL);
+  kmap["GENERAL IMAGE DATA"]=		m(NONE,		NULL);
+  kmap["type of data"]=			m(ASCIIlist,	&KeyParser::SetVariable,
+    &type_of_data_index, 
+    &type_of_data_values);
+  
+  kmap["image data byte order"]=	m(ASCIIlist,	&KeyParser::SetVariable,
+    &byte_order_index, 
+    &byte_order_values);
+  kmap["PET STUDY (General)"]=		m(NONE,		NULL);
+  kmap["PET data type"]=		m(ASCIIlist,	&KeyParser::SetVariable,
+    &PET_data_type_index, 
+    &PET_data_type_values);
+  
+  kmap["data format"]=			m(ASCII,	NULL);
+  // KT 20/06/98 use ASCIIlist
+  kmap["number format"]=		m(ASCIIlist,	&KeyParser::SetVariable,
+    &number_format_index,
+    &number_format_values);
+  kmap["number of bytes per pixel"]=	m(INT,		&KeyParser::SetVariable,&bytes_per_pixel);
+  kmap["number of dimensions"]=		m(INT,	&KeyParser::ReadMatrixInfo,&num_dimensions);
+  kmap["matrix size"]=			m(LIST_OF_INTS,	&KeyParser::SetVariable,&matrix_size);
+  kmap["matrix axis label"]=		m(ASCII,	&KeyParser::SetVariable,&matrix_labels);
+  kmap["scaling factor (mm/pixel)"]=	m(DOUBLE,	&KeyParser::SetVariable,&pixel_sizes);
+  kmap["number of time frames"]=	m(INT,	&KeyParser::ReadFramesInfo,&num_time_frames);
+  kmap["PET STUDY (Emission data)"]=	m(NONE,		NULL);
+  kmap["PET STUDY (Image data)"]=	m(NONE,		NULL);
+  kmap["IMAGE DATA DESCRIPTION"]=	m(NONE,		NULL);
+  kmap["image scaling factor"]=		m(DOUBLE,	&KeyParser::SetVariable,&image_scaling_factor);
+  kmap["data offset in bytes"]=		m(ULONG,	&KeyParser::SetVariable,&data_offset);
+  kmap["END OF INTERFILE"]=		m(NONE,		&KeyParser::SetEndStatus);
+  
+  
+  
 }
+
+
 
 int KeyParser::StartParsing()
 {
-
-	char buf[256];
-	int i=0;
-
-
-	Line line;
-	
-	// checks if INTERFILE HEADER :
-
-	input->getline(buf,256,'\n');
-	line=buf;
-	keyword=line.get_keyword();	
-
-	if(keyword!="!INTERFILE")
+  
+  char buf[256];
+  int i=0;
+  
+  
+  Line line;
+  
+  // checks if INTERFILE HEADER :
+  
+  // TODO replace with input >> line;
+  input->getline(buf,256,'\n');
+  line=buf;
+  keyword=line.get_keyword();	
+  
+  if(keyword!="INTERFILE")
 		{
-			printf("This is not an Interfile");
-			return 1;
+    printf("This is not an Interfile");
+    return 1;
 		}
-	else 
-		printf("Ok :interfile! \n");
-	
-	// initializes keyword map
-	Init();
-	
-	while(status)
-	{
-		if(ParseLine())	
-			ProcessKey();
-		
-	}
-
-	// prepares data for PETSinogramOfVolume Constructor
-
-	while(matrix_labels[i]!="segment")
-		i++;
+  else 
+    printf("Ok :interfile! \n");
+  
+  // initializes keyword map
+  Init();
+  
+  while(status)
+  {
+    if(ParseLine())	
+      ProcessKey();
+    // KT 01/08/98 added warning message
+    else
+      cerr << "Interfile Warning: unrecognized keyword: " << keyword << endl;
+    
+  }
+  
+  // KT 20/06/98 new
+  type_of_numbers = NumericType(number_format_values[number_format_index], bytes_per_pixel);
+  
+  // KT 01/08/98 new
+  file_byte_order = byte_order_index==0 ? 
+    ByteOrder::little_endian : ByteOrder::big_endian;
+  
+  // This has to be here to be able to differentiate between
+  // binary open or text open (for ASCII type).
+  // TODO we don't support ASCII yet.
+  in_stream = new fstream;
+  open_read_binary(*in_stream, data_file_name.c_str());
+  if(type_of_data_values[type_of_data_index] != "PET")
+    cerr << "Interfile Warning: only 'type of data := PET' supported." << endl;
+  
+  if (PET_data_type_values[PET_data_type_index] != "Image")
+  { cerr << "Interfile error: handling images only now." << endl;  return 1; }
+  
+  if (num_dimensions != 3)
+  { cerr << "Interfile error: expecting 3D image " << endl; return 1; }
+  
+  if (matrix_labels[0]!="z" || matrix_labels[1]!="y" ||
+    matrix_labels[2]!="x")
+  { cerr << "Interfile: only supporting z,y,x order of coordinates now."
+  << endl; return 1; }
+  
+  /*
+  // prepares data for PETSinogramOfVolume Constructor
+  
+    
+      // KT 28/07/98 stop if not there
+      while(i<matrix_labels.size() && matrix_labels[i]!="segment")
+      i++;
+      if (i==matrix_labels.size())
+      {
+      cerr << "Matrix labels not in expected format.\n";
+      return 0;
+      }
+      
 	max_r_index=i;
 	i=0;
 	
-	while(matrix_labels[i]!="z" && matrix_labels[i]!="angle")
-		i++;
-	if(matrix_labels[i]=="z")
-		storage_order=BYSINO;
-	else
-		storage_order=BYVIEW;
-	
-	return 0;
-	
+	  while(matrix_labels[i]!="z" && matrix_labels[i]!="angle")
+	  i++;
+	  if(matrix_labels[i]=="z")
+	  storage_order=BYSINO;
+	  else
+	  storage_order=BYVIEW;
+  */
+  
+  return 0;
+  
 }	
 void KeyParser::ChangeStatus(int value)
 {
-	status=value;
+  status=value;
 }
 
 int KeyParser::ParseLine()
 {
-	char buf[MAX_LINE_LENGTH];
-	Line line;
-	int c=0;
-	
-	input->getline(buf,256,'\n');
-	line=buf;
+  char buf[MAX_LINE_LENGTH];
+  Line line;
+  int c=0;
+  
+  input->getline(buf,256,'\n');
+  line=buf;
 		// gets keyword
-	keyword=line.get_keyword();
-	current_index=line.get_index();
+  keyword=line.get_keyword();
+  current_index=line.get_index();
 		// maps keyword to appropriate map_element (sets current)
-	if(MapKeyword(keyword))	
-	{
-		switch(current->type)							// depending on the par_type, gets the correct value from the line
-		{											// and sets the right temporary variable
-		case NONE :
-			break;
-		case ASCII :
-			line.get_param(par_ascii);
-			break;
-		case NUMBER :
-			line.get_param(par_number);
-			break;
-		case NUMBERlist :
-			par_numberlist.clear();
-			line.get_param(par_numberlist);
-			break;
-		case ASCIIlist :
-			par_asciilist.clear();
-			line.get_param(par_asciilist);
-			break;
-		default :
-			break;
-		}
-		return 1;
-	}
-	return 0;
+  if(MapKeyword(keyword))	
+  {
+    switch(current->type)	// depending on the par_type, gets the correct value from the line
+    {				// and sets the right temporary variable
+    case NONE :
+      break;
+    case ASCII :
+      // KT 28/07/98 new
+    case ASCIIlist :
+      line.get_param(par_ascii);
+      break;
+    case INT :
+      line.get_param(par_int);
+      break;
+    case ULONG :
+      line.get_param(par_ulong);
+      break;
+    case DOUBLE :
+      line.get_param(par_double);
+      break;
+    case LIST_OF_INTS :
+      par_intlist.clear();
+      line.get_param(par_intlist);
+      break;
+    case LIST_OF_ASCII :
+      par_asciilist.clear();
+      line.get_param(par_asciilist);
+      break;
+    default :
+      break;
+    }
+    return 1;
+  }
+  return 0;
 }
 
 void KeyParser::SetEndStatus()
 {
-	if(status==STATUS_PARSING)
-		status=STATUS_END_PARSING;
+  if(status==STATUS_PARSING)
+    status=STATUS_END_PARSING;
 }
-
+#if 0
+// KT 01/08/98 just set data_file_name variable now
 void KeyParser::OpenFileStream()
 {
-	in_stream=new fstream(par_ascii.c_str(),ios::in);
-	if (in_stream->fail() || in_stream->bad())
-		printf("Error opening file\n"); 
-	else
-	{
-		current->p_object_variable=in_stream;
-	}
-	
-	printf("parameter : %s\n", par_ascii.c_str());
+  // TODO open as binary, except when type of data is ASCII...
+  in_stream=new fstream(par_ascii.c_str(),ios::in);
+  if (in_stream->fail() || in_stream->bad())
+    printf("Error opening file\n"); 
+  else
+  {
+    current->p_object_variable=in_stream;
+  }
+  
+  printf("parameter : %s\n", par_ascii.c_str());
 }
+#endif
 
 void KeyParser::ReadMatrixInfo()
 {
-	SetVariable();
-	matrix_labels.resize(number_of_dimensions);
-	matrix_size.resize(number_of_dimensions);
-	
+  SetVariable();
+  matrix_labels.resize(num_dimensions);
+  matrix_size.resize(num_dimensions);
+  pixel_sizes.resize(num_dimensions);
+  
 }
 
 void KeyParser::ReadFramesInfo()
 {
-	SetVariable();
-	image_scaling_factor.resize(number_of_time_frames);
-	data_offset.resize(number_of_time_frames);
-	
+  SetVariable();
+  image_scaling_factor.resize(num_time_frames);
+  data_offset.resize(num_time_frames);
+  
 }
 
 void KeyParser::SetVariable()
 {
-	if(!current_index)
-	{
-		switch(current->type)
-		{
-		case NUMBER :
-			{
-				int* p_number=(int*)current->p_object_variable;	// performs the required casting
-				*p_number=par_number;
-				break;
-			}
-		case ASCII :
-			{
-				String* p_string=(String*)current->p_object_variable;	// performs the required casting
-				*p_string=par_ascii;
-				break;
-			}
-		case NUMBERlist :
-			{
-				IntVect* p_vectint=(IntVect*)current->p_object_variable;
-				*p_vectint=par_numberlist;
-				break;
-			}
-		case ASCIIlist :
-			{
-				vector<String>* p_vectstring=(vector<String>*)current->p_object_variable;
-				*p_vectstring=par_asciilist;
-				break;
-			}
-		default :
-			break;
-		}
-	}
-	else														// Sets vector elements using current_index
-	{
-		switch(current->type)
-		{
-		case NUMBER :
-			{
-				IntVect* p_vnumber=(IntVect*)current->p_object_variable;	// performs the required casting
-				p_vnumber->at(current_index-1)=par_number;
-				break;
-			}
-		case ASCII :
-			{
-				vector<String>* p_vstring=(vector<String>*)current->p_object_variable;	// performs the required casting
-				p_vstring->at(current_index-1)=par_ascii;
-				break;
-			}
-		case NUMBERlist :
-			{
-				vector<IntVect>* p_matrixint=(vector<IntVect>*)current->p_object_variable;
-				p_matrixint->at(current_index-1)=par_numberlist;
-				break;
-			}
-	/*	case ASCIIlist :
-			{
-				vector<String>* p_vectstring=(vector<String>*)current->p_object_variable;
-				*p_vectstring=par_asciilist;
-				break;
-			}*/
-		default :
-			break;
-		}
-	}
+  // TODO this does not handle the vectored key convention
+  
+  // current_index is set to 0 when there was no index
+  if(!current_index)
+  {
+    switch(current->type)
+    {
+      // KT 01/08/98 NUMBER->INT
+    case INT :
+      {
+	int* p_int=(int*)current->p_object_variable;	// performs the required casting
+	*p_int=par_int;
+	break;
+      }
+      // KT 01/08/98 new
+    case ULONG :
+      {
+	unsigned long* p_ulong=(unsigned long*)current->p_object_variable;	// performs the required casting
+	*p_ulong=par_ulong;
+	break;
+      }
+      // KT 01/08/98 new
+    case DOUBLE :
+      {
+	double* p_double=(double*)current->p_object_variable;	// performs the required casting
+	*p_double=par_double;
+	break;
+      }
+    case ASCII :
+      {
+	String* p_string=(String*)current->p_object_variable;	// performs the required casting
+	*p_string=par_ascii;
+	break;
+      }
+      // KT 20/06/98 new
+    case ASCIIlist :
+      {
+	*((int *)current->p_object_variable) =
+	  find_in_ASCIIlist(par_ascii, *(current->p_object_list_of_values));
+	break;
+      }
+    case LIST_OF_INTS :
+      {
+	IntVect* p_vectint=(IntVect*)current->p_object_variable;
+	*p_vectint=par_intlist;
+	break;
+      }
+    case LIST_OF_ASCII :
+      {
+	vector<String>* p_vectstring=(vector<String>*)current->p_object_variable;
+	*p_vectstring=par_asciilist;
+	break;
+      }
+    default :
+      break;
+    }
+  }
+  else														// Sets vector elements using current_index
+  {
+    switch(current->type)
+    {
+      // KT 01/08/98 NUMBER->INT
+    case INT :
+      {
+	IntVect* p_vnumber=(IntVect*)current->p_object_variable;	// performs the required casting
+	p_vnumber->at(current_index-1)=par_int;
+	break;
+      }
+      // KT 01/08/98 new
+    case ULONG :
+      {
+	UlongVect* p_vnumber=(UlongVect*)current->p_object_variable;	// performs the required casting
+	p_vnumber->at(current_index-1)=par_ulong;
+	break;
+      }
+      // KT 01/08/98 new
+    case DOUBLE :
+      {
+	DoubleVect* p_vnumber=(DoubleVect*)current->p_object_variable;	// performs the required casting
+	p_vnumber->at(current_index-1)=par_double;
+	break;
+      }
+    case ASCII :
+      {
+	vector<String>* p_vstring=(vector<String>*)current->p_object_variable;	// performs the required casting
+	p_vstring->at(current_index-1)=par_ascii;
+	break;
+      }
+      // KT 20/06/98 new
+    case ASCIIlist :
+      {
+	IntVect* p_vnumber=(IntVect*)current->p_object_variable;	// performs the required casting
+	p_vnumber->at(current_index-1) =
+	  find_in_ASCIIlist(par_ascii, *(current->p_object_list_of_values));
+	break;
+      }
+    case LIST_OF_INTS :
+      {
+	vector<IntVect>* p_matrixint=(vector<IntVect>*)current->p_object_variable;
+	p_matrixint->at(current_index-1)=par_intlist;
+	break;
+      }
+      /*	case LIST_OF_ASCII :
+      {
+      vector<String>* p_vectstring=(vector<String>*)current->p_object_variable;
+      *p_vectstring=par_asciilist;
+      break;
+    }*/
+    default :
+      break;
+    }
+  }
 }
+
+// KT 20/06/98 new
+int KeyParser::find_in_ASCIIlist(const String& par_ascii, const ASCIIlist_type& list_of_values)
+{
+  {
+    // TODO, once we know for sure type type of ASCIIlist_type, we could use STL find()
+    for (int i=0; i<list_of_values.size(); i++)
+      // TODO standardise to lowercase etc
+      if (par_ascii == list_of_values[i])
+	return i;
+  }
+  // it was not in the list
+  cerr << "Interfile warning : value of keyword \""
+    << keyword << "\" is \""
+    << par_ascii
+    << "\"\n\tshould have been one of:";
+  {
+    for (int i=0; i<list_of_values.size(); i++)
+      cerr << "\n\t" << list_of_values[i];
+  }
+  cerr << endl << endl;
+  return -1;
+}  	
 
 int KeyParser::MapKeyword(String keyword)
 {
-	Keymap::iterator it;
-
-	it=kmap.find(keyword);
-	
-	if(it!=kmap.end())
-	{
-		current=&(*it).second;
-		return 1;
-	}
-	
-	return 0;
-
-	//if found
+  Keymap::iterator it;
+  
+  it=kmap.find(keyword);
+  
+  if(it!=kmap.end())
+  {
+    current=&(*it).second;
+    return 1;
+  }
+  
+  return 0;
+  
 }
 
 
 
 int KeyParser::ProcessKey()
 {
-	if(current->p_object_member!=NULL)
-	{
-		(this->*(current->p_object_member))();		//calls appropriate member function
-		return 0;
-	}
-	return 1;										// no server function specified
+  if(current->p_object_member!=NULL)
+  {
+    (this->*(current->p_object_member))();		//calls appropriate member function
+    return 0;
+  }
+  return 1;						// no server function specified
 }
--- src/buildblock/line.cxx
+++ src/buildblock/line.cxx
@@ -1,10 +1,20 @@
+//
+// $Id$ :$Date$
+//
+
+// KT 01/08/98 removed ! from keywords
 
 
-// line.cxx
 // KT 14/12 changed <> to "" in next include
 #include "line.h"
-#include <stdio.h>
-#include <vector.h>
+#include <vector>
+// KT 09/08/98 added for using cerr for warnings
+#include <iostream>
+
+#if defined(_MSC_VER)
+// disable warning about truncation of very long type names for debugger
+#pragma warning(once: 4786)
+#endif
 
 String Line::get_keyword()
 {
@@ -12,15 +22,24 @@ String Line::get_keyword()
 	int sok,eok;		//start & end pos. of keyword
 	int cp =0;		//current index
 	
-	cp=find_first_not_of(' ',0);
+	// skip white space
+	// KT 01/08/98 added \t for skippings tabs
+	cp=find_first_not_of(" \t",0);
 
 
 	if(cp!=String::npos)
 	{
-		sok=find_first_not_of(' ',cp);
+		// skip first !, as this is not strictly part of the keyword
+		// KT 01/08/98 skip first ! now instead of space
+		sok=find_first_not_of('!',cp);
+		// keyword stops at either := or an index []
 		cp=find_first_of(":[",sok);
-		eok=find_last_not_of(' ',--cp);
-		kw=Mid(sok,eok-sok+1);
+		// remove trailing white spaces
+		// KT 01/08/98 added \t for skippings tabs
+		eok=find_last_not_of(" \t",--cp);
+		// KT 01/08/98 replaced Mid by substr
+		kw=substr(sok,eok-sok+1);
+		//TODO standardise, e.g. make all the same case
 	}
 
 	return kw;
@@ -28,21 +47,30 @@ String Line::get_keyword()
 int Line::get_index()
 {
 	int cp,sok,eok;
+	// we take 0 as a default value for the index
 	int in=0;
 	String sin;
-	cp=find_first_of('[',0);
-	if(cp!=String::npos)
+	// KT 20/06/98 make sure that the index is part of the key (i.e. before :=)
+	cp=find_first_of(":[",0);
+	if(cp!=String::npos && operator[](cp) == '[')
 	{
 		sok=cp+1;
 		eok=find_first_of(']',cp);
-		sin=Mid(sok,eok-sok);
+		// KT 09/08/98 check if closing bracket really there
+		if (eok == string::npos)
+		{
+		  // TODO do something more graceful
+		  cerr << "Interfile warning: invalid vectored key in line \n'"
+		       << *this << "'.\n" 
+		       << "Assuming this is not a vectored key." << endl;
+		  return 0;
+		}
+		// KT 09/08/98 replaced Mid by substr
+		sin=substr(sok,eok-sok);
 		in=atoi(sin.c_str());
 	}
 	return in;
 }
-
-//int Line::get_param(vector<String>& v)
-//{}
 
 int Line::get_param(String& s)
 {
@@ -58,13 +86,17 @@ int Line::get_param(String& s)
 		if(sok!=String::npos)
 		{
 			cp=length();
-			eok=find_last_not_of(' ',cp);
-			s=Mid(sok,eok-sok+1);
+			// KT 09/08/98 added tab to list
+			// strip trailing white space
+			eok=find_last_not_of(" \t",cp);
+			// KT 09/08/98 replace Mid by substr
+			s=substr(sok,eok-sok+1);
 			return LINE_OK;
 		}
 	}
 	return LINE_ERROR;
 }
+
 int Line::get_param(int& i)
 {
 	String s;
@@ -77,6 +109,35 @@ int Line::get_param(int& i)
 	return r;
 }
 
+// KT 01/08/98 new
+int Line::get_param(unsigned long& i)
+{
+	String s;
+	int r;
+
+	r=get_param(s);
+	if(r==LINE_OK)
+		// TODO not unsigned now
+		i=atol(s.c_str());
+	
+	return r;
+}
+
+// KT 01/08/98 new
+int Line::get_param(double& i)
+{
+	String s;
+	int r;
+
+	r=get_param(s);
+	if(r==LINE_OK)
+		i=atof(s.c_str());
+	
+	return r;
+}
+
+
+
 int Line::get_param(vector<int>& v)
 {
 	String s;
@@ -86,9 +147,14 @@ int Line::get_param(vector<int>& v)
 	bool end=false;
 
 	cp=find_first_of('=',0)+1;
+	// KT 20/06/98 has to be here now as I removed space from the list later on
+	// skip white space
+	cp=find_first_not_of(" \t",cp);
+	// TODO? this does not check if brackets are balanced
 	while (!end)
 	{
-		cp=find_first_not_of("{}, ",cp);
+		// KT 20/06/98 removed space from the list, as this allowed numbers separated by spaces
+		cp=find_first_not_of("{},",cp);
 
 		if(cp==String::npos)
 		{
@@ -96,16 +162,18 @@ int Line::get_param(vector<int>& v)
 		}
 		else
 		{
-			eop=find_first_of(",} ",cp);
+			// KT 20/06/98 removed space from the list, as this allowed numbers separated by spaces
+		        eop=find_first_of(",}",cp);
 			if(eop==String::npos)
 			{
 				end=true;
 				eop=length();
 			}
-			s=Mid(cp,eop-cp);
+			// KT 09/08/98 replaced Mid by substr
+			s=substr(cp,eop-cp);
+			// TODO use strstream, would allow templates
 			r=atoi(s.c_str());
 			v.push_back(r);
-			printf("%d\n",r);
 			cp=eop+1;
 		}
 	}
@@ -119,12 +187,18 @@ int Line::get_param(vector<String>& v)
 	int cp;
 	int eop;
 	bool end=false;
-	char temp;
 
 	cp=find_first_of('=',0)+1;
+	// KT 20/06/98 has to be here now as I removed space from the list later on
+	// skip white space
+	cp=find_first_not_of(" \t",cp);
+	// TODO? this does not check if brackets are balanced
 	while (!end)
 	{
-		cp=find_first_not_of("{}, ",cp);
+		// KT 20/06/98 removed space from the list
+		cp=find_first_not_of("{},",cp);
+		// KT 09/08/98 skip white space now
+		cp=find_first_not_of(" \t",cp);
 
 		if(cp==String::npos)
 		{
@@ -132,26 +206,35 @@ int Line::get_param(vector<String>& v)
 		}
 		else
 		{
-			eop=find_first_of(",} ",cp);
+			// KT 20/06/98 removed space from the list
+			eop=find_first_of(",}",cp);
 			if(eop==String::npos)
 			{
 				end=true;
 				eop=length();
 			}
+			// KT 20/06/98 this if() is never true anymore, as eop points to one of ",}"
+			// I think it didn't handle multiple words between commas anyway
+			/*
 			if(operator[](eop)==' ')
 			{
-				temp=find_first_not_of(' ',eop+1);
+				char temp=find_first_not_of(' ',eop+1);
 				if(operator[](temp)!=',' && operator[](temp)!='}')
 					eop=find_first_of(",} ",temp);
 			}
-			s=Mid(cp,eop-cp);
+			*/
+			// KT 09/08/98 trim ending white space
+			size_type eop2 = find_last_not_of(" \t",eop);
+			// KT 09/08/98 changed Mid to substr
+			s=substr(cp,eop2-cp);
+
 			v.push_back(s);
-			printf("%s\n",s.c_str());
 			cp=eop+1;
 		}
 	}
 	return r;
 }
+
 Line& Line::operator=(const char* ch)
 {
 	String::operator=(ch);
