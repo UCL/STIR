d41658e4560774cbdfd705143ebf834da4082dbf
Kris Thielemans <Kris.Thielemans@stir>
1998-10-08 10:50:07 +0000

    added cylindroid case and cleaned up a bit

--- src/recon_test/fwdtest.cxx
+++ src/recon_test/fwdtest.cxx
@@ -6,13 +6,110 @@
 
 #include "pet_common.h" 
 
-// KT 09/06/98 don't use glgtime.h anymore
-//#include "PROMIS/glgtime.h"
 #include "recon_buildblock/fwdproj.h"
 #include "display.h"
-#include "Timer.h"
+// KT 06/10/98 use forward projection timer
+#include "recon_buildblock/timers.h"
 
-// KT 09/06/98 don't use glgtime.h anymore, so declarations of Vor_proj etc removed
+
+// KT 07/10/98 new, isolated from main()
+void fill_cuboid(PETImageOfVolume& image, const PETScannerInfo& scanner)
+{
+  int max_bin = (-scanner.num_bins/2) + scanner.num_bins-1;
+  if (scanner.num_bins % 2 == 0)
+    max_bin++;
+  
+  const int nbins = scanner.num_bins;
+
+  int xs = ask_num("Start X coordinate", -nbins/2, max_bin, 0);
+  int ys = ask_num("Start Y coordinate", -nbins/2, max_bin, 0);
+  int zs = ask_num("Start Z coordinate", 0, 2*scanner.num_rings-2, 0);
+  
+  int xe = ask_num("End X coordinate", -nbins/2, max_bin, xs);
+  int ye = ask_num("End Y coordinate", -nbins/2, max_bin, ys);
+  int ze = ask_num("End Z coordinate", 0, 2*scanner.num_rings-2, zs);
+  
+  
+  cerr << "Start coordinate: (x,y,z) = (" 
+       << xs << ", " << ys << ", " << zs  
+       << ")" << endl;
+  cerr << "End   coordinate: (x,y,z) = (" 
+       << xe << ", " << ye << ", " << ze  
+       << ")" << endl;	
+  
+  image.fill(0);
+  for (int z=zs; z<=ze; z++)
+    for (int y=ys; y <= ye; y++)
+      for (int x=xs; x<= xe; x++)
+	image[z][y][x] = 1; 
+}
+  
+// KT 07/10/98 new, isolated from main()
+void fill_cylindroid(PETImageOfVolume& image, const PETScannerInfo& scanner)
+{
+  int max_bin = (-scanner.num_bins/2) + scanner.num_bins-1;
+  if (scanner.num_bins % 2 == 0)
+    max_bin++;
+  
+  const int nbins = scanner.num_bins;
+
+  const int xc = 
+    ask_num("Centre X coordinate", -nbins/2, max_bin, 0);
+  const int yc = 
+    ask_num("Centre Y coordinate", -nbins/2, max_bin, 0);
+  const int zc = 
+    ask_num("Centre Z coordinate", 0, 2*scanner.num_rings-2, scanner.num_rings-1);
+  
+  const float Rcyl = 
+    ask_num("Radius", .5F, nbins/2.F, nbins/4.F);
+  const float Lcyl = 
+    ask_num("Length", 2.F, 2*scanner.num_rings-2.F, 2*scanner.num_rings-2.F);
+  
+  
+  cerr << "Centre coordinate: (x,y,z) = (" 
+       << xc << ", " << yc << ", " << zc  
+       << ")" << endl;
+  cerr << "Radius = " << Rcyl << ", Length = " << Lcyl << endl; 
+  
+  const int num_samples = 
+    ask_num("With how many points do I sample each unit distance ?",1,100,5);
+  
+  Tensor2D<float> plane = image[0];
+
+  for (int y=image.get_min_y(); y<=image.get_max_y(); y++)
+    for (int x=image.get_min_x(); x<=image.get_max_x(); x++)
+    {
+      double value = 0;
+
+      for (double ysmall=-(num_samples-1.)/num_samples/2.; 
+      ysmall < 0.5; 
+      ysmall+= 1./num_samples)
+      {
+	const double ytry = y-ysmall-yc;
+	
+        for (double xsmall=-(num_samples-1.)/num_samples/2.; 
+	xsmall < 0.5; 
+	xsmall+= 1./num_samples)
+	{
+	  const double xtry = x-xsmall-xc;
+	  
+	  if (xtry*xtry + ytry*ytry <= Rcyl*Rcyl)
+	    value++;
+	}
+      }
+      // update plane with normalised value (independent of num_samples)
+      plane[y][x] = value/(num_samples*num_samples); 
+    }
+
+    for (int z=image.get_min_z(); z<=image.get_max_z(); z++)
+    {
+      float zfactor = (min(z+.5, zc+Lcyl/2) - max(z-.5, zc-Lcyl/2));
+      if (zfactor<0) zfactor = 0;
+      image[z] = plane;
+      image[z] *= zfactor;
+    }
+  
+}
 
 int
 main()
@@ -48,9 +145,12 @@ main()
     // scanner.ring_spacing = scanner.FOV_axial / scanner.num_rings;
   }    
 
-  int disp = ask_num("Display images ? (0: No, 1: Yes)", 0, 1, 1);
+  // KT 06/10/98 more options
+  int disp = 
+    ask_num("Display images ? no (0), end result only (1), start image as well (2)", 0,2,1);
 
-  int save = ask_num("Save  images (0: No, 1: Yes) ? ", 0,1,0);
+  // KT 06/10/98 use ask
+  bool save = ask("Save  images ? ", false);
 
  
   Point3D origin(0,0,0);
@@ -67,32 +167,15 @@ main()
 					  -(scanner.num_bins/2), max_bin),
 			 origin, voxel_size);
 
-  // KT 02/07/98 moved image initialisation out of the loop
-  const int nbins = scanner.num_bins;
+  // KT 06/10/98 allow 2 types of images
+  if (ask_num("Start image is cuboid (1) or cylindroid (2)",1,2,1)==1)
+    fill_cuboid(image, scanner);
+  else
+    fill_cylindroid(image, scanner);
 
-  int xs = ask_num("Start X coordinate", -nbins/2, max_bin, 0);
-  int ys = ask_num("Start Y coordinate", -nbins/2, max_bin, 0);
-  int zs = ask_num("Start Z coordinate", 0, 2*scanner.num_rings-2, 0);
-  
-  int xe = ask_num("End X coordinate", -nbins/2, max_bin, xs);
-  int ye = ask_num("End Y coordinate", -nbins/2, max_bin, ys);
-  int ze = ask_num("End Z coordinate", 0, 2*scanner.num_rings-2, zs);
-  
-  
-  cerr << "Start coordinate: (x,y,z) = (" 
-       << xs << ", " << ys << ", " << zs  
-       << ")" << endl;
-  cerr << "End   coordinate: (x,y,z) = (" 
-       << xe << ", " << ye << ", " << ze  
-       << ")" << endl;	
-  
-  image.fill(0);
-  for (int z=zs; z<=ze; z++)
-    for (int y=ys; y <= ye; y++)
-      for (int x=xs; x<= xe; x++)
-	image[z][y][x] = 1; 
-  
-  if (disp)
+
+  // KT 06/10/98 only when selected by user
+  if (disp==2)
     {
       cerr << "Displaying start image";
       display(Tensor3D<float>(image));
@@ -102,22 +185,21 @@ main()
 
   do
     {
-      int max_ring_diff = scanner.num_rings-1; // DB another 'Advanced' bug fixed
-      if (scanner_num == 3) // GE Advance
+      int max_ring_diff = scanner.num_rings-1; 
+      // KT 06/10/98 use name
+      if (scanner.type == PETScannerInfo::Advance)
 	max_ring_diff = 11;
 
-      int abs_ave_ring_diff = ask_num("Ring difference to forward project (0 to end)",
+      int abs_ave_ring_diff = ask_num("Ring difference to forward project",
 				      0, max_ring_diff, 0);
-      // KT 02/07/98 allow segment 0
-      //if (abs_ave_ring_diff == 0)
-      // break;
 
       //      int abs_segment_num = segment_num >= 0 ? segment_num : -segment_num;
       int abs_segment_num = abs_ave_ring_diff;
 
       //DB GEAdvance has different numbers: segment number and average_ring diff
 
-      if (scanner_num == 3) // GE Advance
+      // KT 06/10/98 use name
+      if (scanner.type == PETScannerInfo::Advance)
 	if (abs_ave_ring_diff == 1)
 	  {
 	    PETerror("ring difference 1 not allowed for GE Advance\n");
@@ -146,14 +228,11 @@ main()
 				    0, scanner.num_rings-1 - abs_ave_ring_diff, // DB see above
 				    -scanner.num_bins/2, max_bin),
 		    &scanner,
-		    -abs_segment_num, // DB ABS!!!!!!!!!!!!!!!
+		    -abs_segment_num, // DB ABS
 		    -abs_ave_ring_diff, // min ring diff
 		    -abs_ave_ring_diff); // max ring diff
 
-      { // forward
-	segment_pos.fill(0);
-	segment_neg.fill(0);
-	  
+      {   
 
 	const int nviews = scanner.num_views;
 	cerr << "Special views are at 0, "
@@ -163,6 +242,8 @@ main()
 	int end_view = ask_num("End   view", 0, nviews-1, start_view);
         CPUTimer timer;
 	timer.restart();
+	// KT 06/10/98 added forward projection timer
+	timer_fp.restart();
 
 	
 	Tensor1D<int> processed_views(0, nviews/4);
@@ -176,8 +257,7 @@ main()
 	      continue;
 	    processed_views[view_to_process] = 1;
 
-	    cerr << endl
-		 << "Symmetry related view which will be passed to forward_project :"
+	    cerr << "Symmetry related view which will be passed to forward_project :"
 		 << view_to_process << endl;
 
 	    if (segment_pos.get_segment_num() == 0 && use_2D_forwardprojection)
@@ -193,16 +273,22 @@ main()
 
 	timer.stop();
 	cerr << timer.value() << " s CPU time"<<endl;
+	// KT 08/10/98 added print out of backprojection timer
+	cerr << timer_fp.value() << " s CPU time forward projection"<<endl;
+
 
 	cerr << "min and max in segment_pos " << segment_pos.find_min() 
 	     << " " << segment_pos.find_max() << endl;
 
-	for (int view=0; view< segment_pos.get_num_views(); view++)
+	// KT 06/10/98 added {} for VC++
+	{
+	  for (int view=0; view< segment_pos.get_num_views(); view++)
 	  {
 	    cerr << "min and max for view " << view 
 		 << ": " << segment_pos[view].find_min() 
 		 << ", " << segment_pos[view].find_max() << endl;
 	  }
+	}
 
 	if (disp)
 	  {
@@ -234,7 +320,8 @@ main()
       }
 
     }
-    while (ask_num("One more (0: No, 1: Yes) ? ", 0,1,1));
+    // KT 06/10/98 use ask()
+    while (ask("One more ? ", true));
 
   return 0;
 
