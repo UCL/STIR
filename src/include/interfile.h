4e605a1fd10f4249a9db5950883fdb22ea5e39f4
Kris Thielemans <Kris.Thielemans@stir>
1998-10-09 15:56:10 +0000

    updated description

--- src/include/interfile.h
+++ src/include/interfile.h
@@ -10,12 +10,18 @@
 #include "imagedata.h"
 
 // This reads the first image from an Interfile header
+// If there is trouble interpreting the header, 
+// ask_image_details() is called instead
 PETImageOfVolume read_interfile_image(fstream& input);
 
 // This outputs an Interfile header and data for a Tensor3D object.
 // Extensions .hv and .v will be added to the parameter 'filename' 
 // Return is 'true' when succesful, 'false' otherwise.
-
+// 
+// In fact, at the moment 2 headers are output:
+// .hv is 'new-style' (suitable for Mediman for instance), 
+// .ahv is 'old-style' (suitable for Analyze for instance)
+// They both point to the same file with binary data.
 
 template <class NUMBER>
 bool write_basic_interfile(const char * const filename, Tensor3D<NUMBER>& image);
