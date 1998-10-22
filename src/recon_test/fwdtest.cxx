22f9ff3443469dc72254b6225753c4adc5a3e207
Kris Thielemans <Kris.Thielemans@stir>
1998-10-22 11:35:03 +0000

    don't use scanner dimensions anymore for filling in the start image allow reading in of start image corrected dimension problems in questions for cylinder change cylindroid->cylinder

--- src/recon_test/fwdtest.cxx
+++ src/recon_test/fwdtest.cxx
@@ -2,8 +2,6 @@
 // $Id$: $Date$
 //
 
-#include <fstream.h>
-
 #include "pet_common.h" 
 
 #include "recon_buildblock/fwdproj.h"
@@ -12,24 +10,26 @@
 #include "recon_buildblock/timers.h"
 // KT 09/10/98 use interfile output
 #include "interfile.h"
-// KT 21/10/98 include for allowing input from file
+// KT 21/10/98 include for ask_filename...
 #include "utilities.h"
 
 
 // KT 14/10/98 completely restructed to allow full forward projection
 
 /******************* Declarations local functions *******************/
-// KT 14/10/98 new, isolated from main()
-void 
+// KT 21/10/98 made all of these static
+static void 
 do_segments(const PETImageOfVolume& image, const PETScannerInfo& scanner,
 	    const int abs_segment_num, 
 	    const int start_view, const int end_view,
 	    ostream * out,
 	    const int disp, const int save);
-// KT 07/10/98 new, isolated from main()
-void fill_cuboid(PETImageOfVolume& image, const PETScannerInfo& scanner);
-// KT 07/10/98 new, isolated from main()
-void fill_cylindroid(PETImageOfVolume& image, const PETScannerInfo& scanner);
+// KT 21/10/98 without scanner argument now
+static void 
+fill_cuboid(PETImageOfVolume& image);
+// KT 21/10/98 without scanner argument now
+static void 
+fill_cylinder(PETImageOfVolume& image);
 
 
 /*************************** main *************** *******************/
@@ -94,13 +94,13 @@ main()
 			 origin, voxel_size);
 
   // KT 21/10/98 allow 2 types of images and input from file
-  switch (ask_num("Start image is cuboid (1) or cylindroid (2) or on file (3)",1,3,2))
+  switch (ask_num("Start image is cuboid (1) or cylinder (2) or on file (3)",1,3,2))
   {
   case 1:
-    fill_cuboid(image, scanner);
+    fill_cuboid(image);
     break;
   case 2:
-    fill_cylindroid(image, scanner);
+    fill_cylinder(image);
     break;
   case 3:
     ifstream input;
@@ -224,119 +224,119 @@ do_segments(const PETImageOfVolume& image, const PETScannerInfo& scanner,
   // GEAdvance has different segment number etc
   // KT 06/10/98 use name
   if (scanner.type == PETScannerInfo::Advance)
-  {
-    if (abs_segment_num == 0)
     {
-      pos_max_ring_diff = 1;
-      pos_min_ring_diff = -1;
-      num_rings_this_segment = 2*scanner.num_rings-1;
-    }
-    else
-    { 
-      pos_max_ring_diff++;
-      pos_min_ring_diff++;
-      num_rings_this_segment--;
-    }
+      if (abs_segment_num == 0)
+	{
+	  pos_max_ring_diff = 1;
+	  pos_min_ring_diff = -1;
+	  num_rings_this_segment = 2*scanner.num_rings-1;
+	}
+      else
+	{ 
+	  pos_max_ring_diff++;
+	  pos_min_ring_diff++;
+	  num_rings_this_segment--;
+	}
     
-  }
+    }
   
   
   PETSegmentByView 
     segment_pos(
-    Tensor3D<float>(0, scanner.num_views-1, 
-    0, num_rings_this_segment - 1,
-    -scanner.num_bins/2, max_bin),
-    &scanner,
-    abs_segment_num,
-    pos_min_ring_diff,
-    pos_max_ring_diff);
+		Tensor3D<float>(0, scanner.num_views-1, 
+				0, num_rings_this_segment - 1,
+				-scanner.num_bins/2, max_bin),
+		&scanner,
+		abs_segment_num,
+		pos_min_ring_diff,
+		pos_max_ring_diff);
   
   PETSegmentByView 
     segment_neg(
-    Tensor3D<float>(0, scanner.num_views-1, 
-    0, num_rings_this_segment - 1,
-    -scanner.num_bins/2, max_bin),
-    &scanner,
-    -abs_segment_num, 
-    -pos_max_ring_diff, 
-    -pos_min_ring_diff); 
+		Tensor3D<float>(0, scanner.num_views-1, 
+				0, num_rings_this_segment - 1,
+				-scanner.num_bins/2, max_bin),
+		&scanner,
+		-abs_segment_num, 
+		-pos_max_ring_diff, 
+		-pos_min_ring_diff); 
   
   {       
     
     Tensor1D<int> processed_views(0, nviews/4);
     
     for (int view= start_view; view<=end_view; view++)
-    { 
-      int view_to_process = view % (nviews/2);
-      if (view_to_process > nviews/4)
-	view_to_process = nviews/2 - view_to_process;
-      if (processed_views[view_to_process])
-	continue;
-      processed_views[view_to_process] = 1;
+      { 
+	int view_to_process = view % (nviews/2);
+	if (view_to_process > nviews/4)
+	  view_to_process = nviews/2 - view_to_process;
+	if (processed_views[view_to_process])
+	  continue;
+	processed_views[view_to_process] = 1;
       
-      // KT 03/09/98 add output of segment number
-      cerr << "Processing view " << view_to_process 
-	   << " of segment-pair " <<segment_pos.get_segment_num()
-	   << endl;
+	// KT 03/09/98 add output of segment number
+	cerr << "Processing view " << view_to_process 
+	     << " of segment-pair " <<segment_pos.get_segment_num()
+	     << endl;
       
-      if (segment_pos.get_segment_num() == 0)
-      {
-	forward_project_2D(image, segment_pos, view_to_process);
+	if (segment_pos.get_segment_num() == 0)
+	  {
+	    forward_project_2D(image, segment_pos, view_to_process);
+	  }
+	else
+	  {
+	    forward_project(image, segment_pos, segment_neg, view_to_process);
+	  }
+      
       }
-      else
-      {
-	forward_project(image, segment_pos, segment_neg, view_to_process);
-      }
-      
-    }
     
     // KT 14/10/98 added number
     cerr << "min and max in segment " << abs_segment_num 
-	     << ": " << segment_pos.find_min() 
-	     << " " << segment_pos.find_max() << endl;
+	 << ": " << segment_pos.find_min() 
+	 << " " << segment_pos.find_max() << endl;
     // KT 14/10/98 output segment_neg as well
     if (segment_pos.get_segment_num() != 0)
       cerr << "min and max in segment " << -abs_segment_num 
-      << ": " << segment_neg.find_min() 
-      << " " << segment_neg.find_max() << endl;
+	   << ": " << segment_neg.find_min() 
+	   << " " << segment_neg.find_max() << endl;
     
     if (disp)
-    {
-      // KT 14/10/98 added number
-      cerr << "Displaying segment " << abs_segment_num << endl;
-      display(segment_pos, segment_pos.find_max());
-      // KT 14/10/98 only when non-zero segment
-      if (segment_pos.get_segment_num() != 0)
       {
-	cerr << "Displaying segment " << -abs_segment_num<< endl;
-	display(segment_neg, segment_neg.find_max());
+	// KT 14/10/98 added number
+	cerr << "Displaying segment " << abs_segment_num << endl;
+	display(segment_pos, segment_pos.find_max());
+	// KT 14/10/98 only when non-zero segment
+	if (segment_pos.get_segment_num() != 0)
+	  {
+	    cerr << "Displaying segment " << -abs_segment_num<< endl;
+	    display(segment_neg, segment_neg.find_max());
+	  }
       }
-    }
     if (save && out==0)
-    {
       {
-	char* file = "fwdtestpos";
-	cerr <<"  - Saving " << file << endl;
-	// KT 09/10/98 use interfile output
-	write_basic_interfile(file, segment_pos);
+	{
+	  char* file = "fwdtestpos";
+	  cerr <<"  - Saving " << file << endl;
+	  // KT 09/10/98 use interfile output
+	  write_basic_interfile(file, segment_pos);
+	}
+	// KT 14/10/98 only when non-zero segment
+	if (segment_pos.get_segment_num() != 0)	    
+	  {
+	    char* file = "fwdtestneg";
+	    cerr <<"  - Saving " << file << endl;
+	    // KT 09/10/98 use interfile output
+	    write_basic_interfile(file, segment_neg);
+	  }
       }
-      // KT 14/10/98 only when non-zero segment
-      if (segment_pos.get_segment_num() != 0)	    
-      {
-	char* file = "fwdtestneg";
-	cerr <<"  - Saving " << file << endl;
-	// KT 09/10/98 use interfile output
-	write_basic_interfile(file, segment_neg);
-      }
-    }
     if (out != 0)
-    {
-      segment_pos.write_data(*out);
-      if (segment_pos.get_segment_num() != 0)
       {
-	segment_neg.write_data(*out);
+	segment_pos.write_data(*out);
+	if (segment_pos.get_segment_num() != 0)
+	  {
+	    segment_neg.write_data(*out);
+	  }
       }
-    }
     
     
     
@@ -346,30 +346,31 @@ do_segments(const PETImageOfVolume& image, const PETScannerInfo& scanner,
 
 
 
-// KT 07/10/98 new, isolated from main()
-void fill_cuboid(PETImageOfVolume& image, const PETScannerInfo& scanner)
+// KT 21/10/98 use dimensions from the image itself
+void fill_cuboid(PETImageOfVolume& image)
 {
-  int max_bin = (-scanner.num_bins/2) + scanner.num_bins-1;
-  if (scanner.num_bins % 2 == 0)
-    max_bin++;
   
-  const int nbins = scanner.num_bins;
+  const int xs = ask_num("Start X coordinate", 
+			 image.get_min_x(), image.get_max_x(), 
+			 (image.get_min_x()+ image.get_max_x())/2);
+  const int ys = ask_num("Start Y coordinate", 
+			 image.get_min_y(), image.get_max_y(), 
+			 (image.get_min_y()+ image.get_max_y())/2);
+  const int zs = ask_num("Start Z coordinate", 
+			 image.get_min_z(), image.get_max_z(), 
+			 (image.get_min_z()+ image.get_max_z())/2);
   
-  int xs = ask_num("Start X coordinate", -nbins/2, max_bin, 0);
-  int ys = ask_num("Start Y coordinate", -nbins/2, max_bin, 0);
-  int zs = ask_num("Start Z coordinate", 0, 2*scanner.num_rings-2, 0);
-  
-  int xe = ask_num("End X coordinate", -nbins/2, max_bin, xs);
-  int ye = ask_num("End Y coordinate", -nbins/2, max_bin, ys);
-  int ze = ask_num("End Z coordinate", 0, 2*scanner.num_rings-2, zs);
+  const int xe = ask_num("End X coordinate", xs, image.get_max_x(), xs);
+  const int ye = ask_num("End Y coordinate", ys, image.get_max_y(), ys);
+  const int ze = ask_num("End Z coordinate", zs, image.get_max_z(), zs);
   
   
   cerr << "Start coordinate: (x,y,z) = (" 
-    << xs << ", " << ys << ", " << zs  
-    << ")" << endl;
+       << xs << ", " << ys << ", " << zs  
+       << ")" << endl;
   cerr << "End   coordinate: (x,y,z) = (" 
-    << xe << ", " << ye << ", " << ze  
-    << ")" << endl;	
+       << xe << ", " << ye << ", " << ze  
+       << ")" << endl;	
   
   image.fill(0);
   for (int z=zs; z<=ze; z++)
@@ -378,64 +379,75 @@ void fill_cuboid(PETImageOfVolume& image, const PETScannerInfo& scanner)
 	image[z][y][x] = 1; 
 }
 
-// KT 07/10/98 new, isolated from main()
-void fill_cylindroid(PETImageOfVolume& image, const PETScannerInfo& scanner)
+// KT 21/10/98 use dimensions from the image itself
+void fill_cylinder(PETImageOfVolume& image)
 {
-  int max_bin = (-scanner.num_bins/2) + scanner.num_bins-1;
-  if (scanner.num_bins % 2 == 0)
-    max_bin++;
+ 
+  // KT 21/10/98 made double
+  const double xc = 
+    ask_num("Centre X coordinate", 
+	    (double)image.get_min_x(), (double)image.get_max_x(), 
+	    (image.get_min_x()+ image.get_max_x())/2.);
   
-  const int nbins = scanner.num_bins;
+  const double yc = 
+    ask_num("Centre Y coordinate", 
+	    (double)image.get_min_y(), (double)image.get_max_y(), 
+	    (image.get_min_y()+ image.get_max_y())/2.);
   
-  const int xc = 
-    ask_num("Centre X coordinate", -nbins/2, max_bin, 0);
-  const int yc = 
-    ask_num("Centre Y coordinate", -nbins/2, max_bin, 0);
-  const int zc = 
-    ask_num("Centre Z coordinate", 0, 2*scanner.num_rings-2, scanner.num_rings-1);
+  const double zc = 
+    ask_num("Centre Z coordinate",     
+	    (double)image.get_min_z(), (double)image.get_max_z(), 
+	    (image.get_min_z()+ image.get_max_z())/2.);
+
   
-  const float Rcyl = 
-    ask_num("Radius", .5F, nbins/2.F, nbins/4.F);
-  const float Lcyl = 
-    ask_num("Length", 2.F, 2*scanner.num_rings-2.F, 2*scanner.num_rings-2.F);
+  const double Rcyl = 
+    ask_num("Radius", 
+	    .5, (image.get_max_x()- image.get_min_x())/2.,
+	    (image.get_max_x()- image.get_min_x())/4.);
+  
+  // Max length is num_planes+1 because of edges of voxels
+  const double Lcyl = 
+    ask_num("Length", 2., (image.get_max_z()- image.get_min_z())+1.,
+	    (image.get_max_z()- image.get_min_z())+1.);
   
   
   cerr << "Centre coordinate: (x,y,z) = (" 
-    << xc << ", " << yc << ", " << zc  
-    << ")" << endl;
+       << xc << ", " << yc << ", " << zc  
+       << ")" << endl;
   cerr << "Radius = " << Rcyl << ", Length = " << Lcyl << endl; 
   
   const int num_samples = 
-    ask_num("With how many points (in x,y direction) do I sample each voxel ?",1,100,5);
+    ask_num("With how many points (in x,y direction) do I sample each voxel ?",
+	    1,100,5);
   
   Tensor2D<float> plane = image[0];
   
   for (int y=image.get_min_y(); y<=image.get_max_y(); y++)
     for (int x=image.get_min_x(); x<=image.get_max_x(); x++)
-    {
-      double value = 0;
-      
-      for (double ysmall=-(num_samples-1.)/num_samples/2.; 
-      ysmall < 0.5; 
-      ysmall+= 1./num_samples)
       {
-	const double ytry = y-ysmall-yc;
+	double value = 0;
+      
+	for (double ysmall=-(num_samples-1.)/num_samples/2.; 
+	     ysmall < 0.5; 
+	     ysmall+= 1./num_samples)
+	  {
+	    const double ytry = y-ysmall-yc;
 	
-        for (double xsmall=-(num_samples-1.)/num_samples/2.; 
-	xsmall < 0.5; 
-	xsmall+= 1./num_samples)
-	{
-	  const double xtry = x-xsmall-xc;
+	    for (double xsmall=-(num_samples-1.)/num_samples/2.; 
+		 xsmall < 0.5; 
+		 xsmall+= 1./num_samples)
+	      {
+		const double xtry = x-xsmall-xc;
 	  
-	  if (xtry*xtry + ytry*ytry <= Rcyl*Rcyl)
-	    value++;
-	}
+		if (xtry*xtry + ytry*ytry <= Rcyl*Rcyl)
+		  value++;
+	      }
+	  }
+	// update plane with normalised value (independent of num_samples)
+	plane[y][x] = value/(num_samples*num_samples); 
       }
-      // update plane with normalised value (independent of num_samples)
-      plane[y][x] = value/(num_samples*num_samples); 
-    }
     
-    for (int z=image.get_min_z(); z<=image.get_max_z(); z++)
+  for (int z=image.get_min_z(); z<=image.get_max_z(); z++)
     {
       // KT 09/10/98 changed 2 -> 2. to make both args of min() and max() double
       float zfactor = (min(z+.5, zc+Lcyl/2.) - max(z-.5, zc-Lcyl/2.));
