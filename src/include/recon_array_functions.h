34e7f3023a0518ca87d0252e8a178dc14e9e18ec
Matthew Jacobson <Matthew.Jacobson@stir>
1998-11-19 09:06:29 +0000

    Initial revision

---
+++ src/include/recon_array_functions.h
@@ -0,0 +1,33 @@
+//
+// $Id$: $Date$
+//
+
+#ifndef __recon_array_functions_h_
+#define __recon_array_functions_h_
+
+
+#include "sinodata.h"
+//#include "PETScannerInfo.h"
+#include "imagedata.h"
+
+// KT 09/11/98 new
+void truncate_rim(PETSegmentByView& seg, const int rim_truncation_sino);
+void truncate_rim(PETSegmentBySinogram& seg, const int rim_truncation_sino);
+void truncate_rim(PETImageOfVolume& image_input, 
+		  const int rim_truncation_image);
+
+void multiply_and_add(PETImageOfVolume &image_res, const PETImageOfVolume &image_scaled, float scalar);
+
+float neg_trunc(float x);
+
+void divide_and_truncate(PETImageOfVolume& numerator, 
+			 const PETImageOfVolume& denominator,
+			 const int rim_truncation,
+			 int & count);
+
+
+
+
+
+#endif // __recon_array_functions_h_
+
