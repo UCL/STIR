b44e577a2bf72c4acfd3a8ae4a5f832880f585cc
Kris Thielemans <Kris.Thielemans@stir>
1999-12-10 13:01:30 +0000

    Initial revision

---
+++ src/test/test_linear_regression.cxx
@@ -0,0 +1,145 @@
+
+//
+// $Id$: $Date$
+//
+
+/*
+  A simple programme to test the linear_regression function.
+  It relies on the existence of a file "test_linear_regression.in"
+  This should contain a number of test cases for the fit.
+  File contents should as follows:
+    1 line with some general text
+    1 line with some text for the next test case
+    list_of_numbers
+    1 line with some text for the next test case
+    list_of_numbers
+    ...
+
+    
+    where list_of_numbers is the following list of numbers 
+    (white space is ignored)
+    number_of_points
+    coordinates
+    data	 
+    weights
+    expected_constant  expected_scale  
+    expected_chi_square  
+    expected_variance_of_constant  expected_variance_of_scale  
+    expected_covariance_of_constant_with_scale
+
+  Kris Thielemans, 08/12/1999
+*/
+
+#include "pet_common.h"
+#include "linear_regression.h"
+#include <fstream>
+#include <iostream>
+
+const double tolerance = 1E-4F;
+
+bool check_if_equal(double a, double b, char *str = "Should be equal")
+{
+  if ((fabs(b)>tolerance && fabs(a/b-1) > tolerance)
+      || (fabs(b)<=tolerance && fabs(a-b) > tolerance))
+  {
+    cerr << "Error : values are " << a << " and " << b 
+         << " (" << str<< ")"  << endl;
+    return false;
+  }
+  else
+    return true;
+}
+
+#include <string>
+
+int main(int argc, char **argv)
+{
+  ifstream in("test_linear_regression.in");
+
+  if (!in)
+  {
+    cerr << argv[0] 
+         << ": Error opening input file test_linear_regression.in\nExiting."
+	 << endl;
+    return 1;
+  }
+  
+  char text[200];
+  in.get(text,200);
+  cerr << text << endl;
+  int test_case = 0;
+  
+  while (in)
+  {
+    // first get rid of EOL
+    in.getline(text,200);
+    // now get real text
+    in.get(text,200);
+    test_case ++;
+    
+    int size = 0;
+    in >> size;
+    if (size<=0)
+      break;
+
+    cerr << text << endl;
+    VectorWithOffset<float> coordinates(size);
+    VectorWithOffset<float> measured_data(size);
+    VectorWithOffset<float> weights(size);
+    for (int i=0; i<size; i++)
+      in >> coordinates[i];
+    for (int i=0; i<size; i++)
+      in >> measured_data[i];
+    for (int i=0; i<size; i++)
+      in >> weights[i];
+    
+    double expected_scale;
+    double expected_constant;
+    double expected_variance_of_scale;
+    double expected_variance_of_constant;
+    double expected_covariance_of_constant_with_scale;
+    double expected_chi_square;
+    
+    in >> expected_constant >> expected_scale 
+      >> expected_chi_square 
+      >> expected_variance_of_constant >> expected_variance_of_scale 
+      >> expected_covariance_of_constant_with_scale;
+    
+    
+    double scale=0;
+    double constant=0;
+    double variance_of_scale=0;
+    double variance_of_constant=0;
+    double covariance_of_constant_with_scale=0;
+    double chi_square = 0;
+    
+    linear_regression(
+      constant, scale,
+      chi_square,
+      variance_of_constant,
+      variance_of_scale,
+      covariance_of_constant_with_scale,
+      measured_data,
+      coordinates,
+      weights);
+    
+    if (
+      !check_if_equal(expected_constant, constant, 
+      "for parameter constant, should be equal") || 
+      !check_if_equal(expected_scale, scale, 
+      "for parameter scale, should be equal") ||
+      !check_if_equal(expected_chi_square, chi_square, 
+      "for parameter chi_square, should be equal") ||
+      !check_if_equal(expected_variance_of_constant, variance_of_constant, 
+      "for parameter variance_of_constant, should be equal") ||
+      !check_if_equal(expected_variance_of_scale, variance_of_scale, 
+      "for parameter variance_of_scale, should be equal") ||
+      !check_if_equal(expected_covariance_of_constant_with_scale, 
+      covariance_of_constant_with_scale, 
+      "for parameter covariance_of_constant_with_scale should be equal")
+      )
+      exit(1);
+  }
+  return 0;
+}
+
