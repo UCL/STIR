426a4b25d0cc4d1ba1d770e322be28e7ef351ee1
Kris Thielemans <Kris.Thielemans@stir>
2000-03-07 15:54:37 +0000

    changed handling of defaults depending on compiler added title, removed num_images removed my_display and display2D added doxygen header

--- src/include/display.h
+++ src/include/display.h
@@ -3,129 +3,161 @@
 #ifndef __DISPLAY_H__
 #define __DISPLAY_H__
 
-/* Functions to display Tensor3D and Tensor2D objects
-   Version 1.0: KT
+/*! 
+  \file
+  
+  \brief   Functions to display 2D and 3D Tensor objects
 
-   The display functions of Tensor3D objects take parameters as follows:
-   - plane_stack, or plane : 
+  \author Kris Thielemans
+  \author PARAPET project
+
+  \date    $Date$
+
+  \version $Revision$
+*/
+
+
+#include "VectorWithOffset.h"
+#include "Tensor3D.h"
+
+START_NAMESPACE_TOMO
+
+# if defined(__GNUC__) && (__GNUC__ == 2 && __GNUC_MINOR__ >= 95)
+// gcc 2.95.2 is the only compiler we've used that handles the defaults properly
+
+/*! \brief The main function to display Tensor3D<elemT> objects.
+
+   This function is templated for generality.
+   elemT is the type of elements in the Tensor3D<elemT>
+   scaleT is the type of the scale factors
+   CHARP is the type of the text strings (could be char * or variations with const)
+
+   \param plane_stack
         the Tensor object
-   - scale_factors : 
+   \param scale_factors 
         a VectorWithOffset of factors which are multiplied with the numbers
 	in the Tensor object to give the "real" values
-   - text :
+   \param text
         a VectorWithOffset of strings that are displayed below the images
-   - maxi :
+   \param maxi
         a double which gives the ("real") value that will correspond to the
 	maximum of the color scale. All bigger values are displayed with the
 	same color. 
 	If maxi is 0, all planes are scaled independently.
-   - zoom :
+   \param title
+        text that will be used as a title for the display. title==0 means no text.
+   \param zoom 
         an int giving the number of times the image should be enlarged.
 	Enlargement currently is with linear interpolation, giving
 	reasonably smooth images (although one could want to see the 
 	'pixels', but I didn't implement that yet).
 	If zoom = 0, maximum enlargement is used.
-   - num_in_window :
-        an int giving the desired maximum number of images displayed in one 
-	window (there can be less images if they don't fit).
-	If num_in_window = 0, the maximum number of images will be displayed.
 
-   Note that the scale_factors and text arrays are supposed to have the
-   same range is the outer dimension of the Tensor3D object.
-
-   For Tensor2D objects, the parameters are similar, but scale_factors and
-   text are not vectors anymore.
+   Note that the scale_factors and text Tensors are required to have the
+   same range is the outer dimension of the Tensor3D<elemT> object.
 
    Note that there is an effective threshold at 0 currently (i.e. negative
    numbers are cut out)..
-*/
 
-/* Below is the main function to display Tensor3D objects.
+   \warning Due to the choice of defaults, if \c maxi is not given, the images will
+   be scaled independently.
 
-   This function is templated for generality.
-   NUMBER is the type of elements in the Tensor3D
-   SCALE is the type of the scale factors
-   CHARP is the type of the text strings (could be String as well)
-
-   Templates instantiated in display.cxx:
-   NUMBER : short, SCALE : short, CHARP : char *
-   NUMBER : short, SCALE : float, CHARP : char *
-   NUMBER : float, SCALE : float, CHARP : char *
-   If you need other types, you'll have to recompile display.cxx
-   */
-template <class NUMBER, class SCALE, class CHARP>
-#ifndef __MSL__
-// KT 17/02/98 for some reason CodeWarrior Pro protests to the extern
-extern 
-#endif
-void display(const Tensor3D<NUMBER>& plane_stack,
-             const VectorWithOffset<SCALE>& scale_factors,
+   \warning due to compiler problems, it might be that the prototypes actually
+   declared are different from what you see in the auto-generated doc. 
+   Check display.h
+  */
+// TODO, make version with CHARP ='string' but requires function overloading
+template <class elemT, class scaleT, class CHARP>
+void display(const Tensor3D<elemT>& plane_stack,
+             const VectorWithOffset<scaleT>& scale_factors,
              const VectorWithOffset<CHARP>& text,
              double maxi = 0,
-             int zoom = 0,
-             int num_in_window = 0);
+	     const char * const title = 0,
+             int zoom = 0);
+
+//! \brief display for Tensor3D<elemT> without scale factors and text.
+template <class elemT>
+inline void display(const Tensor3D<elemT>& plane_stack,
+	     double maxi = 0,
+	     const char * const title = 0, int zoom = 0);
 
 
-
-/* A version without scale factors and text */
-template <class NUMBER>
-inline void display(const Tensor3D<NUMBER>& plane_stack,
-	     double maxi = 0, int zoom = 0,
-	     int num_in_window = 0)
-{
-  VectorWithOffset<Real> scale_factors(plane_stack.get_min_index3(), 
-				plane_stack.get_max_index3());
-  scale_factors.fill(1.);
-  VectorWithOffset<char *> text(plane_stack.get_min_index3(), 
-				plane_stack.get_max_index3());
-  text.fill("");
-
-  display(plane_stack, scale_factors, text,
-	  maxi, zoom, num_in_window);
-}
+//! \brief display for Tensor2D<elemT> objects, text==0 means no text.  
+template <class elemT>
+inline void display(const Tensor2D<elemT>& plane,
+		    const char * const text = 0,
+		    double maxi = 0, int zoom = 0);
 
 
-/* The function for Tensor2D objects */
-template <class NUMBER, class SCALE, class CHARP>
-inline void display(const Tensor2D<NUMBER>& plane,
-		    const SCALE scale_factor,
-		    const CHARP& text,
-		    double maxi = 0, int zoom = 0,
-		    int num_in_window = 0)
-{ 
-  // KT 06/02/98 added more size parameters
-  Tensor3D<NUMBER> stack(0,0,
-			 plane.get_min_index2(),plane.get_max_index2(),
-			 plane.get_min_index1(),plane.get_max_index1());
-  stack[0] = plane;
-  VectorWithOffset<SCALE> scale_factors(1);
-  scale_factors[0] = scale_factor;
-  VectorWithOffset<CHARP> texts(1);
-  texts[0] = text;
-  
-  display(stack, scale_factors, texts, maxi, zoom, num_in_window);
-}
+#else // !gcc 2.95
 
 
+// VC and gcc 2.8.1 have problems with the defaults in the above declarations.
+// So, we have to do them by hand...
 
-/* A version without scale factors and text */
-template <class NUMBER>
-inline void display(const Tensor2D<NUMBER>& plane,
-	     double maxi = 0, int zoom = 0,
-	     int num_in_window = 0)
-{
-  // KT 06/02/98 added Real conversion to 0, to avoid calling a new 
-  // template with SCALE=int
-  display(plane, Real(0), "", maxi, zoom, num_in_window);
-}
+template <class elemT, class scaleT, class CHARP>
+void display(const Tensor3D<elemT>& plane_stack,
+             const VectorWithOffset<scaleT>& scale_factors,
+             const VectorWithOffset<CHARP>& text,
+             double maxi,
+	     const char * const title ,
+             int zoom);
 
-/* CL 061098 Move two function from main_promis, promis_span... to here */
-#if 1
-template <class NUMBER, class CHARP>
-void my_display(const Tensor3D<NUMBER> &image, CHARP text); 
+template <class elemT, class scaleT, class CHARP>
+inline void display(const Tensor3D<elemT>& plane_stack,
+             const VectorWithOffset<scaleT>& scale_factors,
+             const VectorWithOffset<CHARP>& text,
+             double maxi,
+	     const char * const title);
 
-template <class NUMBER>
-void display2D(const Tensor2D<NUMBER> &plane);
+template <class elemT, class scaleT, class CHARP>
+inline void display(const Tensor3D<elemT>& plane_stack,
+             const VectorWithOffset<scaleT>& scale_factors,
+             const VectorWithOffset<CHARP>& text,
+             double maxi);
+
+
+template <class elemT, class scaleT, class CHARP>
+inline void display(const Tensor3D<elemT>& plane_stack,
+             const VectorWithOffset<scaleT>& scale_factors,
+             const VectorWithOffset<CHARP>& text);
+
+template <class elemT>
+inline void display(const Tensor3D<elemT>& plane_stack,
+             double maxi,
+	     const char * const title,
+	     int zoom);
+
+template <class elemT>
+inline void display(const Tensor3D<elemT>& plane_stack,
+             double maxi,
+	     const char * const title);
+
+template <class elemT>
+inline void display(const Tensor3D<elemT>& plane_stack,
+             double maxi);
+
+template <class elemT>
+inline void display(const Tensor3D<elemT>& plane_stack);
+
+template <class elemT>
+inline void display(const Tensor2D<elemT>& plane,
+		    const char * const text,
+		    double maxi, int zoom);
+
+template <class elemT>
+inline void display(const Tensor2D<elemT>& plane,
+		    const char * const text,
+		    double maxi);
+
+template <class elemT>
+inline void display(const Tensor2D<elemT>& plane,
+		    const char * const text);
+
+template <class elemT>
+inline void display(const Tensor2D<elemT>& plane);
+
+#endif
 
 
 #include "sinodata.h"
@@ -133,5 +165,9 @@ void display_8_views(const PETViewgram& v1, const PETViewgram& v2,
                      const PETViewgram& v3, const PETViewgram& v4,
                      const PETViewgram& v5, const PETViewgram& v6, 
                      const PETViewgram& v7, const PETViewgram& v8);
-#endif 
+
+#include "display.inl"
+
+END_NAMESPACE_TOMO
+
 #endif 
