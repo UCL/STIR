aa74cb9ebcf98050e6e38ff046a2bc30ae6657d5
Kris Thielemans <Kris.Thielemans@stir>
2000-03-08 15:49:20 +0000

    added reset doxygen

--- src/include/Timer.h
+++ src/include/Timer.h
@@ -1,10 +1,34 @@
 // 
 // $Id$: $Date$
 //
+/*!
 
-/*
-  Timer is a general base class for timers. Interface is like a stop watch:
+  \file
 
+  \brief This declares the Timer class.
+
+  \author Kris Thielemans
+  \author PARAPET project
+
+  \date $Date$
+
+  \version $Revision$
+*/
+
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#include "Tomography_common.h"
+
+START_NAMESPACE_TOMO
+
+/*!
+  \brief
+  a general base class for timers. Interface is like a stop watch.
+
+  Example of usage:
+
+  \code
   DerivedFromTimer t;
   t.start();
   // do things
@@ -12,37 +36,27 @@
   // do things
   t.stop();
   // do things
-  t.restart()
+  t.reset()
   // etc
+  \endcode
 
   Derived classes simply have to implement the virtual function 
-  double get_current_value()
-
-  History:
-   1.0 by Kris Thielemans
-   1.1 by Kris Thielemans
-     CPUTimer uses times() and GetProcessTimes()
-   1.2 by Kris Thielemans
-     moved inlines to separate file
-     made Timer::~Timer virtual
-
+  \c double get_current_value()
 */
 
 
-#ifndef __TIMER_H__
-#define __TIMER_H__
-
-#include "pet_common.h"
 
 class Timer
 {  
 public:
   inline Timer();
-  // KT 12/01/2000 made virtual
   inline virtual ~Timer();  
   inline void start();
   inline void stop();
+  inline void reset();
+  // TODO remove
   inline void restart() ;
+  //! return value is undefined when start() is not called first.
   inline double value() const;
 
 protected:
@@ -58,6 +72,6 @@ protected:
 // TODO remove
 #include "CPUTimer.h"
 
+END_NAMESPACE_TOMO
+
 #endif // __TIMER_H__
-
-
