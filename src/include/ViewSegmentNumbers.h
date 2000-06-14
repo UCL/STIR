7798e0f2b944b2f817923a28c61e967cf88ee5ba
Kris Thielemans <Kris.Thielemans@stir>
2000-06-14 20:15:54 +0000

    made members private

--- src/include/ViewSegmentNumbers.h
+++ src/include/ViewSegmentNumbers.h
@@ -1,32 +1,67 @@
 //
 // $Id$: $Date$
 //
+/*!
+  \file
+  \ingroup buildblock
 
-// First version : Kris Thielemans 03/99
+  \brief Definition of class ViewSegmentNumbers
+
+  \author Kris Thielemans
+  \author Sanida Mustafovic
+  \author PARAPET project
+  
+  \date $Date$
+
+  \version $Revision$
+*/
+
 
 
 #ifndef __ViewSegmentNumbers_h__
 #define __ViewSegmentNumbers_h__
 
+START_NAMESPACE_TOMO
+
+/*!
+  \brief A very simple class to store view and segment numbers 
+  \ingroup buildblock 
+*/
 class ViewSegmentNumbers
 {
 public:
-  int segment_num;
-  int view_num;
 
-  ViewSegmentNumbers( const int view_num,const int segment_num)
-    : segment_num(segment_num),
-      view_num(view_num)
-  {}
+  //! an empty constructor (sets everything to 0)
+  inline  ViewSegmentNumbers();
+  //! constructor taking view and segment number as arguments
+  inline ViewSegmentNumbers( const int view_num,const int segment_num);
 
-  // comparison operator, only useful for sorting
-  // order : (0,1) < (0,-1) < (1,1) ...
-  // KT 15/03/99 new
-  bool operator<(const ViewSegmentNumbers& other) const
-  {
-    return (view_num < other.view_num) ||
-      ((view_num == other.view_num) && (segment_num > other.segment_num));
-  }
+  //! get segment number for const objects
+  inline int segment_num() const;
+  //! get view number for const objects
+  inline int view_num() const;
+
+  //! get reference to segment number
+  inline int&  segment_num();
+  //! get reference to view number
+  inline int&  view_num();
+
+ 
+  //! comparison operator, only useful for sorting
+  /*! order : (0,1) < (0,-1) < (1,1) ...*/
+  inline bool operator<(const ViewSegmentNumbers& other) const;
+
+  //! test for equality
+  inline bool operator==(const ViewSegmentNumbers& other) const;
+
+private:
+  int segment;
+  int view;
+
 };
+
+END_NAMESPACE_TOMO
+
+#include "ViewSegmentNumbers.inl"
 
 #endif
