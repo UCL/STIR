b21e9a950e5d36929339ada2736b47e104a77c77
Kris Thielemans <Kris.Thielemans@stir>
2000-06-15 04:32:00 +0000

    new design

--- src/recon_test/fwdtest.cxx
+++ src/recon_test/fwdtest.cxx
@@ -31,58 +31,47 @@
   If no proj_data_file is given, some questions are asked to use 'standard'
   characteristics.        
 */
-
-#include "Tomography_common.h"
-
-#include "recon_buildblock/fwdproj.h"
+#include "recon_buildblock/ForwardProjectorByBinUsingRayTracing.h"
 #include "display.h"
-// for forward projection timer
-//#include "recon_buildblock/timers.h"
 #include "interfile.h"
+#include "ProjDataFromStream.h"
+#include "ProjDataInfo.h"
 // for ask_filename...
 #include "utilities.h"
+#include "IndexRange3D.h"
+#include "RelatedViewgrams.h"
+#include <fstream>
+
 
 // for auto_ptr
 #include <memory>
-#include <iostream>
-#include <fstream>
 
-//#ifndef TOMO_NO_NAMESPACES
-//using std::fstream;
-//using std::endl;
-//#endif
-
+USING_NAMESPACE_TOMO
 USING_NAMESPACE_STD
-//USING_NAMESPACE_TOMO
 
-
-START_NAMESPACE_TOMO
-
-// KT 06/04/2000 use auto_ptr, renamed s3d to proj_data
 
 /******************* Declarations local functions *******************/
-auto_ptr<PETSinogramOfVolume> ask_parameters();
+auto_ptr<ProjDataFromStream> ask_parameters();
 
 static void 
-do_segments(const PETImageOfVolume& image, const PETSinogramOfVolume& s3d,
+do_segments(const VoxelsOnCartesianGrid<float>& image, ProjDataFromStream& s3d,
 	    const int abs_segment_num, 
 	    const int start_view, const int end_view,
-	    ostream * out,
-	    const int fwdproj_method,
+	    ForwardProjectorByBin*,
 	    const int disp, const int save);
 static void 
-fill_cuboid(PETImageOfVolume& image);
+fill_cuboid(VoxelsOnCartesianGrid<float>& image);
 static void 
-fill_cylinder(PETImageOfVolume& image);
+fill_cylinder(VoxelsOnCartesianGrid<float>& image);
 
 
-/*************************** main *************** *******************/
+
+/*************************** main ***********************************/
 int 
 main(int argc, char *argv[])
 {
-  auto_ptr<PETSinogramOfVolume> proj_data_ptr;
 
-  if(argc!=2) 
+/*  if(argc!=2) 
   {
     cerr<<"Usage: " << argv[0] << " [PSOV-file]\n"
         <<"The PSOV-file will be used to get the scanner, mashing etc. details" 
@@ -90,25 +79,52 @@ main(int argc, char *argv[])
   }
   if (argc>2)
     exit(EXIT_FAILURE);
+*/
+
+ // if(argc==2)
+ // {
+   shared_ptr<ProjData> proj_data_ptr = 
+     ProjData::read_from_file(argv[1]);
+  //}
+ // else
+// {
+ //   proj_data_ptr = ask_parameters();
+  //}
+
+ // auto_ptr<ProjDataFromStream> proj_data_ptr(ProjDataFromStream::ask_parameters());
+ // }
+
+#if 1
+  const ProjDataInfo* data_info = proj_data_ptr->get_proj_data_info_ptr();
+  string header_name = argv[1];
+  header_name += "_copy.hs"; 
+  string raw_data = argv[1];
+   raw_data+=" _copy.prj";
+  ProjDataInfo* new_data_info= data_info->clone();
+ 
   
-  if(argc==2)
+  sino_stream = new fstream (raw_data.c_str(), ios::out|ios::binary);
+  if (!sino_stream->good())
   {
-    proj_data_ptr = 
-      auto_ptr<PETSinogramOfVolume>(new PETSinogramOfVolume(read_interfile_PSOV(argv[1])));
+    error("fwdtest: error opening file %s\n",raw_data);
   }
-  else
-  {
-    proj_data_ptr = ask_parameters();
-  }
+
+  
+   ProjDataFromStream new_data(new_data_info,sino_stream);
+ 
+#endif
+
+#if 1
 
   // make num_bins odd (TODO remove)
   {
-    int num_bins = proj_data_ptr->scan_info.get_num_bins();
+    int num_bins = new_data.get_proj_data_info_ptr()->get_num_tangential_poss();
     if (num_bins%2 == 0)
       num_bins++;
-    proj_data_ptr->scan_info.set_num_bins(num_bins);
+    new_data_info->set_num_tangential_poss(num_bins);
   }
- 
+#endif
+
   const int fwdproj_method = ask_num("Which method (0: Approximate, 1: Accurate)",0,1,0);
 
   int disp = 
@@ -120,103 +136,73 @@ main(int argc, char *argv[])
     0,2,1);
 
    
-  PETImageOfVolume image(proj_data_ptr->scan_info);
+
+  
+  shared_ptr<DiscretisedDensity<3,float> > image_sptr; 
+  
+  VoxelsOnCartesianGrid<float> * vox_image_ptr;
+     
 
   switch (ask_num("Start image is cuboid (1) or cylinder (2) or on file (3)",1,3,2))
   {
   case 1:
-    fill_cuboid(image);
+    image_sptr = vox_image_ptr =
+     new VoxelsOnCartesianGrid<float>(*(proj_data_ptr->get_proj_data_info_ptr()));
+
+    fill_cuboid(*vox_image_ptr);
     break;
   case 2:
-    fill_cylinder(image);
+    image_sptr = vox_image_ptr =
+     new VoxelsOnCartesianGrid<float>(*(proj_data_ptr->get_proj_data_info_ptr()));
+    fill_cylinder(*vox_image_ptr);
     break;
   case 3:
-    ifstream input;
-    ask_filename_and_open(
-      input,
-      "Name of image file to forward project", ".hv", 
-      ios::in);  
+  
+   char filename[max_filename_length];
+   
+    ask_filename_with_extension(filename, "Input file name ?", ".hv");
 
-    image = read_interfile_image(input);
+    image_sptr =
+      DiscretisedDensity<3,float>::read_from_file(filename);
+    vox_image_ptr = dynamic_cast<VoxelsOnCartesianGrid<float> *> (image_sptr.get());
+
     // TODO remove whenever we don't need odd sizes anymore
-    if (image.get_x_size() %2 == 0)
+    if (vox_image_ptr->get_x_size() %2 == 0)
     {
-      image.grow(image.get_min_z(),image.get_max_z(),
-	         image.get_min_y(), -image.get_min_y(),
-		 image.get_min_x(), -image.get_min_x());
+      vox_image_ptr->grow(IndexRange3D(vox_image_ptr->get_min_z(),vox_image_ptr->get_max_z(),
+	         vox_image_ptr->get_min_y(), -vox_image_ptr->get_min_y(),
+		 vox_image_ptr->get_min_x(), -vox_image_ptr->get_min_x()));
     }
     break;
   }
 
+  ForwardProjectorByBinUsingRayTracing * forw_projector_ptr = 
+    new ForwardProjectorByBinUsingRayTracing(proj_data_ptr->get_proj_data_info_ptr()->clone(), 
+        image_sptr);
+    
+  
   if (disp==2)
     {
       cerr << "Displaying start image";
-      display(image);
+      display(*vox_image_ptr);
     }
-
-  fstream *out = 0;
+  
 
   if (save==2)
   {
     cerr << "Saving start image to 'test_image'" << endl;
-    write_basic_interfile("test_image", image);
+    write_basic_interfile("test_image", *vox_image_ptr);
   }
     
   if (ask("Do full forward projection ?", true))
   {
     const int max_segment_num_to_process = 
-      ask_num("max_segment_num_to_process",0,  proj_data_ptr->get_max_segment(),  proj_data_ptr->get_max_segment());
-    if (save)
-    {
-      vector<int> segment_sequence(2*max_segment_num_to_process+1);
-      vector<int> min_ring_diff(2*max_segment_num_to_process+1);
-      vector<int> max_ring_diff(2*max_segment_num_to_process+1);
-      vector<int> min_r(2*max_segment_num_to_process+1);
-      vector<int> max_r(2*max_segment_num_to_process+1);
-      segment_sequence[0] = 0;
-      for (int s=1; s<= max_segment_num_to_process; s++)
-      {
-	segment_sequence[2*s-1] = s;
-	segment_sequence[2*s] = -s;
-      }
-      { 
-	// VC 5.0 bug: can't define 'int index' in the for loop
-	int index = 0;
-	for (int * iter = segment_sequence.begin();
- 	     iter != segment_sequence.end(); 
-	     iter++, index++)
-	{
-	  min_ring_diff[index] = proj_data_ptr->get_min_ring_difference(*iter);
-	  max_ring_diff[index] = proj_data_ptr->get_max_ring_difference(*iter);
-	  min_r[index] = 0;
-	  max_r[index] = proj_data_ptr->get_num_rings(*iter)-1;
-	}
-      }
-      
-      cerr << "Saving in 'fwd_image.hs'" << endl;
-      out = new (fstream);
-      open_write_binary(*out, "fwd_image.s");
-      PETSinogramOfVolume news3d(proj_data_ptr->scan_info, 
-	segment_sequence,
-	min_ring_diff, 	max_ring_diff,
-	min_r, 	max_r,
-	0, proj_data_ptr->get_num_views()-1,
-	-(proj_data_ptr->get_num_bins()/2), -(proj_data_ptr->get_num_bins()/2)+proj_data_ptr->get_num_bins(),
-		      *out, 0L, 
-		      PETSinogramOfVolume::SegmentViewRingBin,
-		      NumericType::FLOAT,
-		      ByteOrder::native);
+      ask_num("max_segment_num_to_process",0,  proj_data_ptr->get_max_segment_num(),  proj_data_ptr->get_max_segment_num());
+    
 
-      write_basic_interfile_PSOV_header("fwd_image.hs",
-				       "fwd_image.s",
-				       news3d);
-    }
-
-   // CPUTimer timer;
-   // timer.reset();
-   // timer_fp.reset();
-   // timer.start();
-   // timer_fp.start();
+    CPUTimer timer;
+    timer.reset();
+    timer.start();
 
     for (int abs_segment_num=0; 
          abs_segment_num<= max_segment_num_to_process; 
@@ -224,20 +210,15 @@ main(int argc, char *argv[])
     {
       cerr << "  - Processing segment num " << abs_segment_num << endl;
           
-      // CL&KT 14/02/2000 added fwdproj_method
-      do_segments(image, *proj_data_ptr,
-                  abs_segment_num, 0, proj_data_ptr->scan_info.get_num_views()-1,
-                  out,  fwdproj_method, disp, save);
+      do_segments(*vox_image_ptr, new_data/*,proj_data_ptr*/,
+                  abs_segment_num, 0, proj_data_ptr->get_proj_data_info_ptr()->get_num_views()-1,
+                  forw_projector_ptr, disp, save);
+			   
+
+
     }
-    //timer.stop();
-   // cerr << timer.value() << " s CPU time"<<endl;
-   // cerr << timer_fp.value() << " s CPU time forward projection"<<endl;
-    
-    if (save)
-    {
-      out->close();
-      delete out;
-    }
+    timer.stop();
+    cerr << timer.value() << " s CPU time"<<endl;
 
 
   }
@@ -245,31 +226,28 @@ main(int argc, char *argv[])
   {   
     do
     {
-     // CPUTimer timer;
-     // timer.reset();
-     // timer_fp.reset();
-     // timer.start();
-      //timer_fp.start();
+      CPUTimer timer;
+      timer.reset();
+      timer.start();
       
-      int abs_segment_num = ask_num("Segment number to forward project",
-				    0, proj_data_ptr->get_max_segment(), 0);
+      int abs_segment_num = ask_num("Segment<float> number to forward project",
+				    0, proj_data_ptr->get_max_segment_num(), 0);
       
-      const int nviews = proj_data_ptr->scan_info.get_num_views();
+      const int nviews = proj_data_ptr->get_proj_data_info_ptr()->get_num_views();
       cerr << "Special views are at 0, "
 	<< nviews/4 <<", " << nviews/2 <<", " << nviews/4*3 << endl;
       int start_view = ask_num("Start view", 0, nviews-1, 0);
       int end_view = ask_num("End   view", 0, nviews-1, start_view);
       
-      do_segments(image, *proj_data_ptr, 
+      do_segments(*vox_image_ptr,new_data,/*proj_data_ptr*/ 
 	          abs_segment_num, 
 	          start_view, end_view,
-	          0, 
-		  fwdproj_method,
+	          forw_projector_ptr,
 	          disp, save);
+      
 
-     // timer.stop();
-     // cerr << timer.value() << " s CPU time"<<endl;
-    //  cerr << timer_fp.value() << " s CPU time forward projection"<<endl;   
+      timer.stop();
+      cerr << timer.value() << " s CPU time"<<endl;
 
     }
     while (ask("One more ? ", true));
@@ -280,26 +258,21 @@ main(int argc, char *argv[])
 }
 
 /******************* Implementation local functions *******************/
-// CL&KT 14/02/2000 added fwdproj_method
 void
-do_segments(const PETImageOfVolume& image, 
-            const PETSinogramOfVolume& proj_data,
+do_segments(const VoxelsOnCartesianGrid<float>& image, 
+            ProjDataFromStream& proj_data,
 	    const int abs_segment_num, 
 	    const int start_view, const int end_view,
-	    ostream * out,
-	    const int fwdproj_method,
+	    ForwardProjectorByBin* forw_projector_ptr,
 	    const int disp, const int save)
 {
   const int nviews = proj_data.get_num_views();
+  shared_ptr<DataSymmetriesForViewSegmentNumbers> symmetries_sptr =
+    forw_projector_ptr->get_symmetries_used()->clone();
 
-  PETSegmentByView 
-    segment_pos = proj_data.get_empty_segment_view_copy(abs_segment_num);
-  PETSegmentByView 
-    segment_neg = proj_data.get_empty_segment_view_copy(-abs_segment_num);
-  
   {       
     
-    Tensor1D<int> processed_views(0, nviews/4);
+    Array<1,int> processed_views(0, nviews/4);
     
     for (int view= start_view; view<=end_view; view++)
       { 
@@ -311,62 +284,19 @@ do_segments(const PETImageOfVolume& image,
 	processed_views[view_to_process] = 1;
       
 	cerr << "Processing view " << view_to_process 
-	     << " of segment-pair " <<segment_pos.get_segment_num()
+	     << " of segment-pair " <<abs_segment_num
 	     << endl;
-      
-	if (segment_pos.get_segment_num() == 0)
-	  {
-	    forward_project_2D(image, segment_pos, view_to_process);
-	  }
-	else
-	  {
-	    // CL&KT 14/02/2000 added fwdproj_method
-	    forward_project(fwdproj_method, image, segment_pos, segment_neg, view_to_process);
-	  }
-      
-      }
-    
-    cerr << "min and max in segment " << abs_segment_num 
-	 << ": " << segment_pos.find_min() 
-	 << " " << segment_pos.find_max() << endl;
-    if (segment_pos.get_segment_num() != 0)
-      cerr << "min and max in segment " << -abs_segment_num 
-	   << ": " << segment_neg.find_min() 
-	   << " " << segment_neg.find_max() << endl;
-    
-    if (disp)
-      {
-	cerr << "Displaying segment " << abs_segment_num << endl;
-	display(segment_pos, segment_pos.find_max());
-	if (segment_pos.get_segment_num() != 0)
-	  {
-	    cerr << "Displaying segment " << -abs_segment_num<< endl;
-	    display(segment_neg, segment_neg.find_max());
-	  }
-      }
-    if (save && out==0)
-      {
-	{
-	  char* file = "fwdtestpos";
-	  cerr <<"  - Saving " << file << endl;
-	  write_basic_interfile(file, segment_pos);
-	}
-	if (segment_pos.get_segment_num() != 0)	    
-	  {
-	    char* file = "fwdtestneg";
-	    cerr <<"  - Saving " << file << endl;
-	    write_basic_interfile(file, segment_neg);
-	  }
-      }
 
-    // TODO replace by set_segment
-    if (out != 0)
-      {
-	segment_pos.write_data(*out);
-	if (segment_pos.get_segment_num() != 0)
-	  {
-	    segment_neg.write_data(*out);
-	  }
+	const ViewSegmentNumbers view_segmnet_num(view_to_process,abs_segment_num);
+	RelatedViewgrams<float> viewgrams = 
+	  proj_data.get_proj_data_info_ptr()->
+	  get_empty_related_viewgrams(view_segmnet_num,
+	    symmetries_sptr);
+         forw_projector_ptr->forward_project(viewgrams, image);	  
+	if (!(proj_data.set_related_viewgrams(viewgrams) == Succeeded::yes))
+	  error("Error set_related_viewgrams\n");
+
+	
       }
     
     
@@ -377,7 +307,7 @@ do_segments(const PETImageOfVolume& image,
 
 
 
-void fill_cuboid(PETImageOfVolume& image)
+void fill_cuboid(VoxelsOnCartesianGrid<float>& image)
 {
   const float voxel_value = ask_num("Voxel value",-10E10F, 10E10F,1.F);
   const int xs = ask_num("Start X coordinate", 
@@ -409,7 +339,7 @@ void fill_cuboid(PETImageOfVolume& image)
 	image[z][y][x] = voxel_value; 
 }
 
-void fill_cylinder(PETImageOfVolume& image)
+void fill_cylinder(VoxelsOnCartesianGrid<float>& image)
 {
   const float voxel_value = ask_num("Voxel value",-10E10F, 10E10F,1.F);
  
@@ -449,7 +379,7 @@ void fill_cylinder(PETImageOfVolume& image)
     ask_num("With how many points (in x,y direction) do I sample each voxel ?",
 	    1,100,5);
   
-  Tensor2D<float> plane = image[0];
+  Array<2,float> plane = image[0];
   
   for (int y=image.get_min_y(); y<=image.get_max_y(); y++)
     for (int x=image.get_min_x(); x<=image.get_max_x(); x++)
@@ -479,7 +409,7 @@ void fill_cylinder(PETImageOfVolume& image)
   for (int z=image.get_min_z(); z<=image.get_max_z(); z++)
     {
       // use 2. to make both args of min() and max() double
-      float zfactor = (min(z+.5, zc+Lcyl/2.) - max(z-.5, zc-Lcyl/2.));
+    float zfactor = (std::min(z+.5, zc+Lcyl/2.) - std::max(z-.5, zc-Lcyl/2.));
       if (zfactor<0) zfactor = 0;
       image[z] = plane;
       image[z] *= zfactor;
@@ -487,11 +417,12 @@ void fill_cylinder(PETImageOfVolume& image)
     
 }
 
-auto_ptr<PETSinogramOfVolume>
+/*
+auto_ptr<ProjDataFromStream>
 ask_parameters()
 {
     
-  PETScanInfo scan_info;
+  ProjDataInfo scan_info;
  
   int scanner_num = 
     ask_num("Enter scanner number (0: RPT, 1:1: 953, 2: 966, 3: GE, 4: ART) ? ", 
@@ -499,35 +430,35 @@ ask_parameters()
   switch( scanner_num )
     {
     case 0:
-      scan_info = (PETScannerInfo::RPT);
+      scan_info = (Scanner::RPT);
       break;
     case 1:
-      scan_info = (PETScannerInfo::E953);
+      scan_info = (Scanner::E953);
       break;
     case 2:
-      scan_info = (PETScannerInfo::E966);
+      scan_info = (Scanner::E966);
       break;
     case 3:
-      scan_info = (PETScannerInfo::Advance);
+      scan_info = (Scanner::Advance);
       break;
     case 4:
-      scan_info = (PETScannerInfo::ART);
+      scan_info = (Scanner::ART);
       break;
     }
 
   {
     const int new_num_bins = 
-      scan_info.get_num_bins() / ask_num("Reduce num_bins by factor", 1,16,1);
+      proj_data_info_ptr->get_num_tangential_poss() / ask_num("Reduce num_bins by factor", 1,16,1);
 
     // keep same radius of FOV
-    scan_info.set_bin_size(
-      (scan_info.get_bin_size()*scan_info.get_num_bins()) / new_num_bins
+    proj_data_info_ptr->set_bin_size(
+      (proj_data_info_ptr->get_bin_size()*proj_data_info_ptr->get_num_tangential_poss()) / new_num_bins
       );
 
-    scan_info.set_num_bins(new_num_bins); 
+    proj_data_info_ptr->set_num_bins(new_num_bins); 
 
-    scan_info.set_num_views(
-      scan_info.get_num_views()/ ask_num("Reduce num_views by factor", 1,16,1)
+    proj_data_info_ptr->set_num_views(
+      proj_data_info_ptr->get_num_views()/ ask_num("Reduce num_views by factor", 1,16,1)
       );  
 
   }    
@@ -535,11 +466,11 @@ ask_parameters()
   
   int span = 1;
   {
-    if ( scan_info.get_scanner().type != PETScannerInfo::Advance )
+    if ( proj_data_info_ptr->get_scanner().type != Scanner::Advance )
       {
 	do 
 	  {
-	    span = ask_num("Span ", 1, scan_info.get_num_rings()-1, 1);
+	    span = ask_num("Span ", 1, proj_data_info_ptr->get_num_axial_poss()-1, 1);
 	  }
 	while (span%2==0);
       }
@@ -549,14 +480,13 @@ ask_parameters()
 
   fstream *out = 0;
   
-  return new PETSinogramOfVolume(
+  return new ProjDataFromStream(
     scan_info, 
     span, 
-    scan_info.get_scanner().type != PETScannerInfo::Advance ?
-    scan_info.get_num_rings()-1 : 11,
+    proj_data_info_ptr->get_scanner().type != Scanner::Advance ?
+    proj_data_info_ptr->get_num_axial_poss()-1 : 11,
     *out, 0UL,
-    PETSinogramOfVolume::SegmentViewRingBin,
+    ProjDataFromStream::SegmentViewRingBin,
     NumericType::FLOAT);
 }
-
-END_NAMESPACE_TOMO
+*/
