dfe79702b1c29a51a9a5cc83320e490b072265e2
Kris Thielemans <Kris.Thielemans@stir>
2000-06-15 01:35:03 +0000

    new design

--- src/include/recon_array_functions.h
+++ src/include/recon_array_functions.h
@@ -12,6 +12,7 @@
   \brief a variety of useful functions
 
   \author Matthew Jacobson
+  \author Kris Thielemans
   \author PARAPET project
 
   \date    $Date$
@@ -19,25 +20,23 @@
 */
 
 
-#include "sinodata.h"
-#include "imagedata.h"
+#include "Tomography_common.h"
 
 START_NAMESPACE_TOMO
 
 
-// KT 09/11/98 new
+// TODO template this all in data type of Viewgram et al
 
-//! sets the first and last rim_truncation_sino bins at the 'edges' to zero
-void truncate_rim(PETSegmentByView& seg, const int rim_truncation_sino);
-//! sets the first and last rim_truncation_sino bins at the 'edges' to zero
-void truncate_rim(PETSegmentBySinogram& seg, const int rim_truncation_sino);
 
-//! sets to zero voxels within rim_truncation_image of the FOV rim
-void truncate_rim(PETImageOfVolume& image_input, 
-		  const int rim_truncation_image);
+template <typename elemT> class SegmentByView;
+template <typename elemT> class SegmentBySinogram;
+template <typename elemT> class Viewgram;
+template <typename elemT> class RelatedViewgrams;
+template <int num_dimensions, typename elemT> class DiscretisedDensity;
+
 
 //! scales an image and adds it to another
-void multiply_and_add(PETImageOfVolume &image_res, const PETImageOfVolume &image_scaled, float scalar);
+void multiply_and_add(DiscretisedDensity<3,float> &image_res, const DiscretisedDensity<3,float> &image_scaled, float scalar);
 
 //! truncates negative values to zero
 float neg_trunc(float x);
@@ -47,55 +46,72 @@ float neg_trunc(float x);
 //float neg_indicate(float x){return (x<=0.0)?1.0:0.0;}
 
 //! sets non-positive voxel values to small positive ones
-void set_negatives_small(PETImageOfVolume& input_image, const int rim_truncation_image=0);
+void truncate_min_to_small_positive_value(DiscretisedDensity<3,float>& input_image, const int rim_truncation_image=0);
 
 //! divide sinograms and set 'edge bins' to zero, put answer in numerator
-void divide_and_truncate(PETImageOfVolume& numerator, 
-			 const PETImageOfVolume& denominator,
+void divide_and_truncate(DiscretisedDensity<3,float>& numerator, 
+			 const DiscretisedDensity<3,float>& denominator,
 			 const int rim_truncation,
 			 int & count);
 
+#if 0
 // AZ 04/10/99: added
 //! divide sinograms and set 'edge bins' to zero, put answer in numerator
 void divide_and_truncate(const int view, // const int view45,
-			 PETSegmentBySinogram& numerator, const PETSegmentBySinogram& denominator,
+			 SegmentBySinogram<float>& numerator, const SegmentBySinogram<float>& denominator,
 			 const int rim_truncation_sino,
 			 int& count, int& count2, float* f = NULL);
 
-// AZ 04/10/99: added
+#endif
 
-//! divide sinograms and set 'edge bins' to zero, put answer in numerator
-void divide_and_truncate(PETViewgram& numerator, const PETViewgram& denominator,
+//! divide viewgrams and set 'edge bins' to zero, put answer in numerator
+void divide_and_truncate(Viewgram<float>& numerator, const Viewgram<float>& denominator,
 			 const int rim_truncation_sino,
 			 int& count, int& count2, float* f = NULL);
 
-// AZ 07/10/99: added
-//! sets the first and last rim_truncation_sino bins at the 'edges' to zero
-void truncate_rim(PETSegmentBySinogram& seg, const int rim_truncation_sino, const int view);
+//! divide related viewgrams and set 'edge bins' to zero, put answer in numerator
+void divide_and_truncate(RelatedViewgrams<float>& numerator, const RelatedViewgrams<float>& denominator,
+			 const int rim_truncation_sino,
+			 int& count, int& count2, float* f = NULL);
 
-// AZ 07/10/99: added
+
+//! sets to zero voxels within rim_truncation_image of the FOV rim
+void truncate_rim(DiscretisedDensity<3,float>& image_input, 
+		  const int rim_truncation_image);
+
+
+
 //! sets the first and last rim_truncation_sino bins at the 'edges' to zero
-void truncate_rim(PETViewgram& viewgram, const int rim_truncation_sino);
+void truncate_rim(SegmentByView<float>& seg, const int rim_truncation_sino);
+//! sets the first and last rim_truncation_sino bins at the 'edges' to zero
+void truncate_rim(SegmentBySinogram<float>& seg, const int rim_truncation_sino);
+#if 0
+//! sets the first and last rim_truncation_sino bins at the 'edges' to zero
+void truncate_rim(SegmentBySinogram<float>& seg, const int rim_truncation_sino, const int view);
+#endif
+
+//! sets the first and last rim_truncation_sino bins at the 'edges' to zero
+void truncate_rim(Viewgram<float>& viewgram, const int rim_truncation_sino);
 
 //! sets the end planes of the image to zero
-void truncate_end_planes(PETImageOfVolume &input_image, int plane_truncation=1);
+void truncate_end_planes(DiscretisedDensity<3,float> &input_image, int plane_truncation=1);
 
 //! simple division of two sinograms, x/0 = 0
-void divide_array(PETSegmentByView& numerator,PETSegmentByView& denominator);
+void divide_array(SegmentByView<float>& numerator, const SegmentByView<float>& denominator);
 
 //! simple division of two images, x/0 = 0
-void divide_array(PETImageOfVolume& numerator,PETImageOfVolume& denominator);
+void divide_array(DiscretisedDensity<3,float>& numerator, const DiscretisedDensity<3,float>& denominator);
 
 //MJ 03/01/2000  Trying to adhoc parallelize a loglikelihood computation
 
 //! compute the log term of the loglikelihood function for given part of the projection space
 void accumulate_loglikelihood(const int view,
-			 PETSegmentBySinogram& projection_data, 
-			 const PETSegmentBySinogram& estimated_projections,
+			 SegmentBySinogram<float>& projection_data, 
+			 const SegmentBySinogram<float>& estimated_projections,
 			 const int rim_truncation_sino, float *accum);
 //! compute the log term of the loglikelihood function for given part of the projection space
-void accumulate_loglikelihood(PETViewgram& projection_data, 
-			 const PETViewgram& estimated_projections,
+void accumulate_loglikelihood(Viewgram<float>& projection_data, 
+			 const Viewgram<float>& estimated_projections,
 			 const int rim_truncation_sino,
 			 float* accum);
 
