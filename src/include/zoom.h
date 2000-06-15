7ef3cb28b5401327dc0c987ca529bb27c7bafee9
Kris Thielemans <Kris.Thielemans@stir>
2000-06-15 06:37:14 +0000

    new design

--- src/include/zoom.h
+++ src/include/zoom.h
@@ -1,8 +1,8 @@
 //
 // $Id$: $Date$
 //
-#ifndef __zoom_H__
-#define  __zoom_H__
+#ifndef __ZOOM_H__
+#define  __ZOOM_H__
 
 /*!
   \file 
@@ -27,8 +27,7 @@
   of zoomed images with the same parameters. These functions have the following
   parameters:
   
-  \param  zoom  scales the projection bins (zoom larger than 1 means more detail, 
-  so smaller pixels)
+  \param  zoom  scales the projection bins (zoom larger than 1 means more detail, so smaller pixels)
   \param  x_offset_in_mm  x-coordinate of new origin (in mm)
   \param  y_offset_in_mm  y-coordinate of new origin (in mm)
   \param  size  the number of 'bins' in the new projection line (or equivalent for images)
@@ -36,9 +35,11 @@
          \c view_num to angles (in radians)
 		 
   In the case that the offsets are 0, the following holds:
-  - If \c size is equal to \c zoom*old_size, the same amount of data is represented.
-  - If it is less, data are truncated.
-  - If it is larger, the outer ends are filled with 0.
+  <UL>
+  <LI> If \c size is equal to \c zoom*old_size, the same amount of data is represented.
+  <LI> If it is less, data are truncated.
+  <LI> If it is larger, the outer ends are filled with 0.
+  </UL>
   
   The second class of functions is for images only. It allows three-dimensional
   zooming and translation. Parameters are derived either from
@@ -48,67 +49,71 @@
   \warning Because overlap_interpolate is used, the zooming is 'count-preserving',
   i.e. when the output range is large enough, the in.sum() == out.sum().
 
-  \warning Origins are taken relative to the centre of the coordinate range:
-    x_in_mm = (x_index - x_ctr_index) * voxel_size.x() + origin.x()
+  \warning Origins are taken relative to the centre of the coordinate range:<BR>
+    x_in_mm = (x_index - x_ctr_index) * voxel_size.x() + origin.x() <BR>
     where x_ctr_index = (x_max_index + x_min_index)/2
 */
 
+#ifdef SINO
 #include "sinodata.h"
-#include "imagedata.h" 
-#include "CartesianCoordinate3D.h"
+#endif
+#include "VoxelsOnCartesianGrid.h" 
+#include "PixelsOnCartesianGrid.h" 
 
 START_NAMESPACE_TOMO
+#ifdef SINO
+/*!
+  \brief zoom \c segment, replacing the first argument with the new data
 
-/*! 
- \brief zoom \c segment, replacing the first argument with the new data.
-
-\see zoom.h for parameter info
+ \see zoom.h for parameter info
 */
 void 
-zoom_segment (PETSegmentByView& segment, 
+zoom_segment (SegmentByView& segment, 
               const float zoom, const float x_offset_in_mm, const float y_offset_in_mm, 
               const int size, const float azimuthal_angle_sampling);
 
-/*! 
- \brief zoom \c segment, replacing the first argument with the new data. 
- 
+/*!
+  \brief zoom \c segment, replacing the first argument with the new data
+
  \see zoom.h for parameter info
- */
+*/
 void 
-zoom_segment (PETSegmentBySinogram& segment, 
+zoom_segment (SegmentBySinogram& segment, 
               const float zoom, const float x_offset_in_mm, const float y_offset_in_mm, 
               const int size, const float azimuthal_angle_sampling);
 
-/*! 
- \brief zoom \c viewgram, replacing the first argument with the new data. 
- 
+/*!
+  \brief zoom \c viewgram, replacing the first argument with the new data
+
  \see zoom.h for parameter info
- */
+*/
 void
-zoom_viewgram (PETViewgram& viewgram, 
+zoom_viewgram (Viewgram& viewgram, 
                const float zoom, const float x_offset_in_mm, const float y_offset_in_mm, 
                const int size, const float azimuthal_angle_sampling);
 
-/*! 
- \brief zoom \c image, replacing the first argument with the new data. 
- 
+/*!
+  \brief zoom \c image, replacing the first argument with the new data
+
  \see zoom.h for parameter info
- */
+*/
+#endif
+
 void
-zoom_image(PETImageOfVolume &image,
-                       const float zoom,
-                       const float x_offset_in_mm, const float y_offset_in_mm, 
-                       const int new_size);
+zoom_image(VoxelsOnCartesianGrid<float> &image,
+	   const float zoom,
+	   const float x_offset_in_mm, const float y_offset_in_mm, 
+	   const int new_size);
 
 /*! 
-\brief 
+  \brief 
   zoom \c image, replacing the first argument with the new data. 
   Full 3D shifts and zooms. 
-  
+
   \see zoom.h for parameter info.
-  */
+*/
 void
-zoom_image(PETImageOfVolume &image,
+zoom_image(VoxelsOnCartesianGrid<float> &image,
 	   const CartesianCoordinate3D<float>& zooms,
 	   const CartesianCoordinate3D<float>& offsets_in_mm,
 	   const CartesianCoordinate3D<int>& new_sizes);
@@ -119,19 +124,21 @@ zoom_image(PETImageOfVolume &image,
   \see zoom.h for parameter info
 */
 void 
-zoom_image(PETImageOfVolume &image_out, const PETImageOfVolume &image_in);
+zoom_image(VoxelsOnCartesianGrid<float> &image_out, 
+	   const VoxelsOnCartesianGrid<float> &image_in);
 
 /*! 
 \brief 
-zoom \c image2D_in according to dimensions, origin and voxel_size of 
+zoom \c image2D_in according to dimensions, origin and pixel_size of 
 \c image2D_out.
 
-Currently, information in the \a z components of origin and voxel_size is ignored.
-\see zoom.h for parameter info
+  \see zoom.h for parameter info
 */
 void
-zoom_image(PETPlane &image2D_out, const PETPlane &image2D_in);
+zoom_image(PixelsOnCartesianGrid<float> &image2D_out, 
+           const PixelsOnCartesianGrid<float> &image2D_in);
 
 END_NAMESPACE_TOMO
 
 #endif
+
