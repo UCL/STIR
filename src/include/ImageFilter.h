33e9973c15a457cd20cf9b3bade8402d783c87d1
Kris Thielemans <Kris.Thielemans@stir>
2000-06-16 18:47:50 +0000

    use VectorWithOffset instead of float

--- src/include/ImageFilter.h
+++ src/include/ImageFilter.h
@@ -1,5 +1,5 @@
 //
-// $Id$
+// $Id$: $Date$
 //
 
 #ifndef __ImageFilter_h_
@@ -11,19 +11,17 @@
  
   \brief declares the ImageFilter class
 
-  \author Matthew Jacobson
-  \author Kris Thielemans
+  \author Matthew Jacobson (some help by Kris Thielemans)
+  \author based on C-code by Roni Lefkowitz
   \author PARAPET project
 
   \date    $Date$
   \version $Revision$
 */
 
-#include "Tomography_common.h"
+#include "VectorWithOffset.h"
 
 
-
-// MJ 02/03/2000 ftype ---> kernels_built
 
 START_NAMESPACE_TOMO
 
@@ -38,28 +36,12 @@ template <int num_dimensions, typename elemT> class DiscretisedDensity;
 class ImageFilter
 {
 
-  //! kernel lengths
-  long kerlx,kerlz;
-
-  //! point to arrays of kernel data and to convolution input/output
-  float *onerow,*outrow,*kernelX,*kernelZ;
-
-  //! full width at half maximum parameters
-  double fwhmx,fwhmz;
-
-  //! Metz powers
-  float Nx,Nz;  //MJ 17/12/98 added Metz parameters
-
  public:
-
- //MJ 09/02/99 Added Metz filter parameters as function arguments
-
- //MJ 05/03/2000 got rid of scanner dependence, changed defaults
 
   //! generates the filter kernels
   void build(const DiscretisedDensity<3,float>& representative_image,double fwhmx_dir=0,double fwhmz_dir=0,float Nx_dir=0.0,float Nz_dir=0.0);
 
-  //! does 3D convolution with the 
+  //! does 3D convolution with the filter
   void apply(DiscretisedDensity<3,float>& input_image);
 
   //! constructor
@@ -70,6 +52,20 @@ class ImageFilter
 
   //! used to see if kernels have been generated
   bool kernels_built;
+
+ private:
+   
+  //! kernel lengths
+  int kerlx,kerlz;
+
+  //! arrays of kernel data and to convolution input/output
+  VectorWithOffset<float> onerow,outrow,kernelX,kernelZ;
+
+  //! full width at half maximum parameters
+  double fwhmx,fwhmz;
+
+  //! Metz powers
+  float Nx,Nz;  //MJ 17/12/98 added Metz parameters
 
 };
 
