3259eb568fa2fd7f06e1f3098e668f84f863fc9e
Kris Thielemans <Kris.Thielemans@stir>
2000-11-09 18:06:26 +0000

    - removed proj_data_info arg from constructor, and the private member - removed SymmetryOperation stuff

--- src/include/DataSymmetriesForViewSegmentNumbers.h
+++ src/include/DataSymmetriesForViewSegmentNumbers.h
@@ -18,11 +18,9 @@
 #ifndef __DataSymmetriesForViewSegmentNumbers_H__
 #define __DataSymmetriesForViewSegmentNumbers_H__
 
-#include "SymmetryOperation.h"
 #include "ViewSegmentNumbers.h"
 #include <vector>
 #include <memory>
-#include "shared_ptr.h"
 #include "ProjDataInfo.h"
 
 #ifndef TOMO_NO_NAMESPACES
@@ -40,17 +38,17 @@ class ViewSegmentIndexRange;
 
 
 /*!
-  \brief A class for encoding/find symmetries. Works only on
+  \brief A class for encoding/finding symmetries. Works only on
   ViewSegmentNumbers (instead of Bin).
 
-  TODOdoc
+  This class (mainly used in RelatedViewgrams and the projectors)
+  is useful to store and use all information on symmetries
+  common between the image representation and the projection data.
 
 */
 class DataSymmetriesForViewSegmentNumbers
 {
 public:
-  inline DataSymmetriesForViewSegmentNumbers(const shared_ptr<ProjDataInfo>& proj_data_info_ptr/*,
-                        const shared_ptr<DiscretisedDensity>& image_info*/);
 
   virtual ~DataSymmetriesForViewSegmentNumbers() {};
 
@@ -71,22 +69,22 @@ public:
 
   //! fills in a vector with all the view/segments that are related to 'v_s' (including itself)
   virtual void
-    get_related_view_segment_numbers(vector<ViewSegmentNumbers>&, const ViewSegmentNumbers&) const = 0;
+    get_related_view_segment_numbers(vector<ViewSegmentNumbers>&, const ViewSegmentNumbers& v_s) const = 0;
 
-  //! returns the number of bins related to 'b'
+  //! returns the number of view_segment_numbers related to 'v_s'
   virtual inline int
-    num_related_view_segment_numbers(const ViewSegmentNumbers&) const;
-
+    num_related_view_segment_numbers(const ViewSegmentNumbers& v_s) const;
+#if 0
   /*! \brief given an arbitrary view/segment, find the basic view/segment
   
   sets 'v_s' to the corresponding 'basic' view/segment and returns the symmetry 
   transformation from 'basic' to 'v_s'.
 
   Note that the symmetry operation is not completely defined by giving only view/segment.
-  The symmetry operation returned is the one where 'ring' and 'bin' are not changed.
   */
   virtual auto_ptr<SymmetryOperation>
     find_symmetry_operation_to_basic_view_segment_numbers(ViewSegmentNumbers&) const = 0;
+#endif
 
   /*! \brief given an arbitrary view/segment, find the basic view/segment
   
@@ -94,11 +92,8 @@ public:
   'v_s' is changed (i.e. it was NOT a basic view/segment).
   */  
   virtual inline bool
-    find_basic_view_segment_numbers(ViewSegmentNumbers& v_s) const;
+    find_basic_view_segment_numbers(ViewSegmentNumbers& v_s) const = 0;
 
-protected:
-  const shared_ptr<ProjDataInfo> proj_data_info_ptr;
-  //shared_ptr<DiscretisedDensity> image_info;
 };
 
 END_NAMESPACE_TOMO
