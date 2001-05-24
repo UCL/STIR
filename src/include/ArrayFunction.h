6d8dfd19cce1204e103e6efabf7c931f136abba8
Kris Thielemans <Kris.Thielemans@stir>
2001-05-24 16:34:55 +0000

    added  in_place_apply_array_functions_on_each_index and  in_place_apply_array_function_on_1st_index
    added some documentation

--- src/include/ArrayFunction.h
+++ src/include/ArrayFunction.h
@@ -1,4 +1,4 @@
-// $Id$: $Date$
+// $Id$
 /*!
 
   \file
@@ -16,24 +16,36 @@
   <ul>
   <li>
    functions which work on all Array objects, and which change every element of the
-   tensor: 
-   in_place_log, in_place_exp (these work only well when elements are float or double)
-   in_place_abs
-   in_place_apply_function<br>
+   array: 
+   <ul>
+   <li> in_place_log, in_place_exp (these work only well when elements are float or double)
+   <li>in_place_abs
+   <li>in_place_apply_function
+   <li>in_place_apply_array_function_on_1st_index
+   <li>in_place_apply_array_functions_on_each_index
+   </ul>
    All these functions return a reference to the (modified) array
    <li>
    functions specific to Array<1,elemT>:
-   inner_product, norm, angle
+   <ul>
+   <li>inner_product, norm, angle
+   </ul>
    <li>
    functions specific to Array<2,elemT>:
-   matrix_transpose, matrix_multiply
+   <ul>
+   <li>matrix_transpose, matrix_multiply
    </ul>
- 
+   </ul>
+
+   \warning Compilers without partial specialisation of templates are
+   catered for by explcit instantiations. If you need it for any other
+   types, you'd have to add them by hand.
  */
 #ifndef __ArrayFunction_H__
 #define __ArrayFunction_H__
   
 #include "Array.h"
+#include "shared_ptr.h"
 
 #include <cmath>
 
@@ -64,6 +76,7 @@ in_place_log(Array<1,float>& v)
 #endif
 
 
+//! apply log to each element of the multi-dimensional array
 template <int num_dimensions, class elemT>
 inline Array<num_dimensions, elemT>& 
 in_place_log(Array<num_dimensions, elemT>& v)  
@@ -92,6 +105,7 @@ in_place_exp(Array<1,float>& v)
 }
 #endif
 
+//! apply exp to each element of the multi-dimensional array
 template <int num_dimensions, class elemT>
 inline Array<num_dimensions, elemT>& 
 in_place_exp(Array<num_dimensions, elemT>& v)  
@@ -122,6 +136,7 @@ in_place_abs(Array<1,float>& v)
 }
 #endif
 
+//! store absolute value of each element of the multi-dimensional array
 template <int num_dimensions, class elemT>
 inline Array<num_dimensions, elemT>& 
 in_place_abs(Array<num_dimensions, elemT>& v)  
@@ -152,7 +167,13 @@ in_place_apply_function(Array<1,float>& v, float (*f)(float))
 }
 #endif
 
-// KT 28/10/98 corrected F->f and recursion
+
+//! apply any function(object) to each element of the multi-dimensional array
+/*! each element will be replaced by 
+    \code
+    elem = f(elem);
+    \endcode
+*/
 template <int num_dimensions, class elemT, class FUNCTION>
 inline Array<num_dimensions, elemT>& 
 in_place_apply_function(Array<num_dimensions, elemT>& v, FUNCTION f)  
@@ -161,6 +182,119 @@ in_place_apply_function(Array<num_dimensions, elemT>& v, FUNCTION f)
     in_place_apply_function(v[i], f); 
   return v; 
 }
+
+//! Apply a function object on all possible 1d arrays extracted by keeping all indices fixed, except the first one
+/*!
+  For the 2d case, this amounts to applying a function on all columns 
+  of the matrix.
+
+  For a 3d case, the following pseudo-code illustrates what happens.
+  \code
+  for all i2,i3
+  {
+    for all i
+    {
+      a[i] = array[i][i2][i3];
+    }
+    f(a);
+  }
+  \endcode
+*/
+template <int num_dim, typename elemT, typename FunctionObject> 
+void
+in_place_apply_array_function_on_1st_index(Array<num_dim, elemT>& array, FunctionObject f)
+{
+  assert(array.is_regular());
+  const int outer_min_index = array.get_min_index();
+  const int outer_max_index = array.get_max_index();
+
+  // construct a vector with a full_iterator for every array[i]
+  VectorWithOffset< Array<num_dim-1, elemT>::full_iterator > 
+    full_iterators (outer_min_index, outer_max_index);  
+  for (int i=outer_min_index; i<=outer_max_index; ++i)
+    full_iterators[i] = array[i].begin_all();
+  
+  // allocate 1d array
+  Array<1, elemT> array1d (outer_min_index, outer_max_index);
+
+  while (full_iterators[outer_min_index] != array[outer_min_index].end_all())
+  {
+    // copy elements into 1d array
+    for (int i=outer_min_index; i<=outer_max_index; ++i)    
+      array1d[i] = *full_iterators[i];
+    
+    // apply function
+    (*f)(array1d);
+    
+    // put results back
+    for (int i=outer_min_index; i<=outer_max_index; ++i)
+      *full_iterators[i] = array1d[i];
+    
+    // now increment full_iterators to do next index
+    for (int i=outer_min_index; i<=outer_max_index; ++i)
+      ++full_iterators[i];
+  }
+    
+}
+
+//! Apply a sequence of 1d array-function objects on every dimension of the input array
+/*!
+  The sequence of function object pointers is specified by iterators. There must be
+  num_dim function objects in the sequence, i.e. stop-start==num_dim.
+
+  The n-th function object (*(start+n)) is applied on the n-th index of the
+  array. So, (*start) is applied using
+  in_place_apply_array_function_on_1st_index(array, *start).
+  Similarly, (*(start+1) is applied using
+  in_place_apply_array_function_on_1st_index(array[i], *(start+1))
+  for every i. And so on.
+*/
+// TODO add specialisation that uses ArrayFunctionObject::is_trivial
+template <int num_dim, typename elemT, typename FunctionObjectIter> 
+void 
+in_place_apply_array_functions_on_each_index(Array<num_dim, elemT>& array, 
+                                             FunctionObjectIter start, 
+                                             FunctionObjectIter stop)
+{
+  assert(start+num_dim == stop);
+  assert(num_dim > 1);
+  in_place_apply_array_function_on_1st_index(array, *start);
+
+  ++start;
+  for (Array<num_dim, elemT>::iterator restiter = array.begin(); restiter != array.end(); ++restiter)
+    in_place_apply_array_functions_on_each_index(*restiter, start, stop);
+}
+
+
+
+typedef void (*floatarrayfunctionptr)(Array<1,float>&);
+
+//! 1d specialisation. 
+/*!
+  \warning 
+  Currently uses (**start)(array), while preferably it would use (*start)(array). However, this doesn't compile when the function objects are part of a 
+ std::vector< shared_ptr<ArrayFunctionObject<1,elemT> > >.
+ (TODO)
+*/
+#ifdef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+// silly business for deficient compilers (including VC 6.0)
+#define elemT float
+#define FunctionObjectIter std::vector< shared_ptr<ArrayFunctionObject<1,elemT> > >::const_iterator 
+#else
+template <typename elemT, typename FunctionObjectIter> 
+#endif
+void in_place_apply_array_functions_on_each_index(Array<1, elemT>& array, FunctionObjectIter start, FunctionObjectIter stop)
+{
+  assert(start != stop);
+  (**start)(array);
+  assert (++start == stop);
+}
+
+#ifdef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+#undef elemT
+#undef FunctionObjectIter
+#endif
+
 
 //----------------------------------------------------------------------
 // some functions specific for 1D Arrays
