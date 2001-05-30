a5f85902e8cfcb81633b4a8efdb42f7dbd3830fc
Kris Thielemans <Kris.Thielemans@stir>
2001-05-30 15:38:14 +0000

    Put files appropriate for PARAPET release 0.92 into CVS control

---
+++ recon_test_pack/run_tests.sh
@@ -0,0 +1,71 @@
+#! /bin/sh
+# Shell script for automatic running of the tests
+# see README.txt
+# Author: Kris Thielemans
+
+echo This script should work with PARAPET software 0.91. If you have
+echo a later version, you might have to update your test pack.
+echo Please check the web site.
+
+INSTALL_DIR=$1
+
+ThereWereErrors=0
+echo ------------- Converting ECAT6 file to Interfile ------------- 
+${INSTALL_DIR}convecat6_if my_Utahscat600k_ca_seg4 Utahscat600k_ca.scn 1> convecat6_if.log 2> convecat6_if_stderr.log <  convecat6_if.inp
+echo '---- Comparing output of convecat6 (error should be 0)'
+if ${INSTALL_DIR}compare_projdata my_Utahscat600k_ca_seg4.hs Utahscat600k_ca_seg4.hs 2>compare_projdata_stderr.log;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+echo
+echo ------------- Running sensitivity ------------- 
+${INSTALL_DIR}sensitivity Utahscat600k_ca_seg4.hs 1> sensitivity.log 2> sensitivity_stderr.log < sensitivity.inp
+
+echo '---- Comparing output of sensitivity (should be identical up to tolerance)'
+if ${INSTALL_DIR}compare_image RPTsens_seg4.hv my_RPTsens_seg4.hv;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+echo
+echo ------------- Running OSEMMain ------------- 
+
+${INSTALL_DIR}OSEMMain OSEMMain_test.par 1> OSEMMain.log 2> OSEMMain_stderr.log
+
+echo '---- Comparing output of OSEMMain subiter 3 (should be identical up to tolerance)'
+if ${INSTALL_DIR}compare_image test_image_3.hv my_test_image_3.hv;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+echo '---- Comparing output of OSEMMain subiter 3 (should be identical up to tolerance)'
+if ${INSTALL_DIR}compare_image test_image_5.hv my_test_image_5.hv;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+
+echo
+echo '--------------- End of tests -------------'
+echo
+if test ${ThereWereErrors} = 1  ; 
+then
+echo "Check what went wrong. The *.log files might help you."
+else
+echo "Everything seems to be fine !"
+fi
+
+
+
