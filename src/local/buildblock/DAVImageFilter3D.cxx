83cf7c57747b9089cd48f45b0ac9811169459113
Sanida Mustafovic <Sanida.Mustafovic@stir>
2001-05-31 16:42:15 +0000

    first versions

---
+++ src/local/buildblock/DAVImageFilter3D.cxx
@@ -0,0 +1,127 @@
+//
+// $Id$
+//
+/*!
+
+  \file
+
+  \brief 
+
+  \author Sanida Mustafovic
+  \author Kris Thielemans
+  
+  \date $Date$
+  \version $Revision$
+*/
+
+#include "tomo/local/DAVImageFilter3D.h"
+#include "CartesianCoordinate3D.h"
+#include "DiscretisedDensity.h"
+
+// TODO REMOVE
+#include <iostream>
+#include <fstream>
+
+#ifndef TOMO_NO_NAMESPACES
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+using std::cerr;
+using std::endl;
+#endif
+
+
+START_NAMESPACE_TOMO
+
+template <typename elemT>
+DAVImageFilter3D<elemT>:: DAVImageFilter3D(const CartesianCoordinate3D<int>& mask_radius)
+{
+  mask_radius_x = mask_radius.x();
+  mask_radius_y = mask_radius.y();
+  mask_radius_z = mask_radius.z();
+}
+
+template <typename elemT>
+DAVImageFilter3D<elemT>:: DAVImageFilter3D()
+{
+  set_defaults();
+}
+
+template <typename elemT>
+Succeeded
+DAVImageFilter3D<elemT>::virtual_build_filter (const DiscretisedDensity<3,elemT>& density)
+{
+
+   if (consistency_check(density) == Succeeded::no)
+      return Succeeded::no;
+   dav_filter = 
+     DAVArrayFilter3D<elemT>(Coordinate3D<int>
+     (mask_radius_z, mask_radius_y, mask_radius_x));
+
+   return Succeeded::yes;
+}
+
+template <typename elemT>
+void
+DAVImageFilter3D<elemT>::filter_it(DiscretisedDensity<3, elemT>& density) const
+{
+  assert(consistency_check(density) == Succeeded::yes);
+  dav_filter(density);
+   
+}
+
+template <typename elemT>
+void
+DAVImageFilter3D<elemT>::filter_it(DiscretisedDensity<3, elemT>& out_density, const DiscretisedDensity<3, elemT>& in_density) const
+{
+  assert(consistency_check(in_density) == Succeeded::yes);
+ // cerr << mask_radius_x << "   "  << mask_radius_y << endl;
+  dav_filter(out_density,in_density);
+   
+}
+
+template <typename elemT>
+void
+DAVImageFilter3D<elemT>::set_defaults()
+{
+  mask_radius_x = 0;
+  mask_radius_y = 0;
+  mask_radius_z = 0;
+}
+
+template <typename elemT>
+void 
+DAVImageFilter3D<elemT>::initialise_keymap()
+{
+  parser.add_start_key("DAV Filter Parameters");
+  parser.add_key("mask radius x", &mask_radius_x);
+  parser.add_key("mask radius y", &mask_radius_y);
+  parser.add_key("mask radius z", &mask_radius_z);
+  parser.add_stop_key("END DAV Filter Parameters");
+}
+
+
+const char * const 
+DAVImageFilter3D<float>::registered_name =
+  "DAV";
+
+#  ifdef _MSC_VER
+// prevent warning message on reinstantiation, 
+// note that we get a linking error if we don't have the explicit instantiation below
+#  pragma warning(disable:4660)
+#  endif
+
+#ifndef TOMO_STATIC
+// Register this class in the ImageProcessor registry
+// At the same time, the compiler will instantiate DAVImageFilter3D<float>
+static DAVImageFilter3D<float>::RegisterIt dummy;
+
+template DAVImageFilter3D<float>;
+#else
+
+
+template DAVImageFilter3D<float>;
+#endif
+
+END_NAMESPACE_TOMO
+
---
+++ src/local/buildblock/SVLCImageFilter.cxx
@@ -0,0 +1,128 @@
+#if 0
+//
+// %W%: %E%
+//
+/*!
+
+  \file
+
+  \brief 
+
+  \author Sanida Mustafovic
+  \author Kris Thielemans
+
+  \date %E%
+  \version %I%
+*/
+
+#include "tomo/SVLCImageFilter.h"
+#include "CartesianCoordinate3D.h"
+#include "DiscretisedDensity.h"
+
+// TODO REMOVE
+#include <iostream>
+#include <fstream>
+
+#ifndef TOMO_NO_NAMESPACES
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+using std::cerr;
+using std::endl;
+#endif
+
+
+START_NAMESPACE_TOMO
+
+template <typename elemT>
+SVLCImageFilter<elemT>:: SVLCImageFilter(const CartesianCoordinate3D<int>& mask_radius)
+{
+  mask_radius_x = mask_radius.x();
+  mask_radius_y = mask_radius.y();
+  mask_radius_z = mask_radius.z();
+}
+
+template <typename elemT>
+SVLCImageFilter<elemT>:: SVLCImageFilter()
+{
+  set_defaults();
+}
+
+template <typename elemT>
+Succeeded
+SVLCImageFilter<elemT>::virtual_build_filter (const DiscretisedDensity<3,elemT>& density)
+{
+
+   if (consistency_check(density) == Succeeded::no)
+      return Succeeded::no;
+   svlc_filter = 
+     SVLCArrayFilter<elemT>(Coordinate3D<int>
+     (mask_radius_z, mask_radius_y, mask_radius_x));
+
+   return Succeeded::yes;
+}
+
+template <typename elemT>
+void
+SVLCImageFilter<elemT>::filter_it(DiscretisedDensity<3, elemT>& density) const
+{
+  assert(consistency_check(density) == Succeeded::yes);
+  svlc_filter(density);
+   
+}
+
+template <typename elemT>
+void
+SVLCImageFilter<elemT>::filter_it(DiscretisedDensity<3, elemT>& out_density, const DiscretisedDensity<3, elemT>& in_density) const
+{
+  assert(consistency_check(in_density) == Succeeded::yes);
+  svlc_filter(out_density,in_density);
+   
+}
+
+template <typename elemT>
+void
+SVLCImageFilter<elemT>::set_defaults()
+{
+  mask_radius_x = 0;
+  mask_radius_y = 0;
+  mask_radius_z = 0;
+}
+
+template <typename elemT>
+void 
+SVLCImageFilter<elemT>::initialise_keymap()
+{
+  parser.add_start_key("SVLC Filter Parameters");
+  parser.add_key("mask radius x", &mask_radius_x);
+  parser.add_key("mask radius y", &mask_radius_y);
+  parser.add_key("mask radius z", &mask_radius_z);
+  parser.add_stop_key("END SVLC Filter Parameters");
+}
+
+
+const char * const 
+SVLCImageFilter<float>::registered_name =
+  "DAV";
+
+#  ifdef _MSC_VER
+// prevent warning message on reinstantiation, 
+// note that we get a linking error if we don't have the explicit instantiation below
+#  pragma warning(disable:4660)
+#  endif
+
+#ifndef TOMO_STATIC
+// Register this class in the ImageFilter registry
+// At the same time, the compiler will instantiate SVLCImageFilter<float>
+static SVLCImageFilter<float>::RegisterIt dummy;
+
+template SVLCImageFilter<float>;
+#else
+
+
+template SVLCImageFilter<float>;
+#endif
+
+END_NAMESPACE_TOMO
+
+#endif
