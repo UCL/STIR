2a01fe0f87d0027efeb530337f5e86ae6b2e5ed7
Kris Thielemans <Kris.Thielemans@stir>
2001-06-07 16:30:31 +0000

    added ChainedImageProcessor.cxx added TruncateMinToSmallPositiveValueImageProcessor.cxx

--- src/buildblock/buildblock_registries.cxx
+++ src/buildblock/buildblock_registries.cxx
@@ -1,8 +1,12 @@
 #include "tomo/SeparableCartesianMetzImageFilter.h"
 #include "tomo/MedianImageFilter3D.h"
+#include "tomo/ChainedImageProcessor.h"
+#include "tomo/TruncateMinToSmallPositiveValueImageProcessor.h"
 
 START_NAMESPACE_TOMO
 
 static MedianImageFilter3D<float>::RegisterIt dummy;
 static SeparableCartesianMetzImageFilter<float>::RegisterIt dummy2;
+static ChainedImageProcessor<3,float>::RegisterIt dummy3;
+static TruncateMinToSmallPositiveValueImageProcessor<float>::RegisterIt dummy4;
 END_NAMESPACE_TOMO
