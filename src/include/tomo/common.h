b5854ee88ca8014137e0e0ee4f552bad65fd0310
Kris Thielemans <Kris.Thielemans@stir>
2001-06-15 15:49:46 +0000

    moved doxygen groups to separate file instead of common.h such that adding to them will not case recompilation

--- src/include/tomo/common.h
+++ src/include/tomo/common.h
@@ -90,44 +90,6 @@
  </UL>
 */
 
-
-// define some doxygen groups
-/*!
-\defgroup buildblock Basic building blocks
-Library with things are not not specific to reconstructions
-*/
-/*!
-\defgroup recon_buildblock Reconstruction building blocks
-Library with 'general' reconstruction building blocks
-*/
-/*!
-\defgroup LogLikBased_buildblock Reconstruction building blocks for
-loglikelihood based algorithms
-Library with additional building blocks used for algorithms which
-are similar to EM.
-*/
-/*!
-\defgroup display Display functions
-Library for displaying of images
-*/
-/*!
-\defgroup para Parallel library 
-*/
-/*!
-\defgroup test Tests of the basic building blocks
-*/
-/*!
-\defgroup recontest Tests of reconstruction building blocks
-*/
-/*!
-\defgroup utilities Utility programmes
-*/
-/*!
-\defgroup reconstructors Reconstruction classes
-*/
-/*!
-\defgroup OSMAPOSL Implementation of the OSMAP One-Step-Late reconstruction algorithm
-*/
 #ifdef _MSC_VER
 // disable warnings on very long identifiers for debugging information
 #pragma warning(disable: 4786)
---
+++ src/include/tomo/doxygengroups.h
@@ -0,0 +1,44 @@
+//
+// $Id$
+//
+
+// define some doxygen groups
+// This file does not contain any code
+
+/*!
+\defgroup buildblock Basic building blocks
+Library with things that are not not specific to reconstructions.
+This includes multi-dimensional arrays, images, image processors, 
+projection data,...
+*/
+/*!
+\defgroup recon_buildblock Reconstruction building blocks
+Library with 'general' reconstruction building blocks
+*/
+/*!
+\defgroup LogLikBased_buildblock Reconstruction building blocks for loglikelihood based algorithms
+Library with additional building blocks used for algorithms which
+are similar to EM.
+*/
+/*!
+\defgroup display Display functions
+Library for displaying of images
+*/
+/*!
+\defgroup para Parallel library 
+*/
+/*!
+\defgroup test Tests of the basic building blocks
+*/
+/*!
+\defgroup recontest Tests of reconstruction building blocks
+*/
+/*!
+\defgroup utilities Utility programmes
+*/
+/*!
+\defgroup reconstructors Reconstruction classes
+*/
+/*!
+\defgroup OSMAPOSL Implementation of the OSMAP One-Step-Late reconstruction algorithm
+*/
