4f1cef7633165ccfd1c613190edd196ad8d1c7bf
Kris Thielemans <Kris.Thielemans@stir>
2001-06-15 11:39:00 +0000

    updated documentation

--- src/include/tomo/recon_buildblock/FilterRootPrior.h
+++ src/include/tomo/recon_buildblock/FilterRootPrior.h
@@ -30,20 +30,24 @@ template <int num_dimensions, typename elemT> class DiscretisedDensity;
 /*!
   \ingroup recon_buildblock
   \brief
-  A class for generalised priors a la Median Root Prior (invented by
+  A class in the GeneralisedPrior hierarchy. This implements 'generalised' 
+  priors a la the Median Root Prior (which was invented by
   Sakari Alenius).
 
   This class takes an ImageProcessor object (i.e. a filter), and computes 
   the prior 'gradient' as
-  \f[ G_v = \beta ( {\lambda_v \ over F_v} - 1) \f]
+  \f[ G_v = \beta ( {\lambda_v \over F_v} - 1) \f]
   where \f$ \lambda\f$ is the image where to compute the gradient, and
   \f$F\f$ is the image obtained by filtering \f$\lambda\f$.
 
   Note that for nearly all filters, this is not a real prior, as this
   'gradient' is \e not the gradient of a function. This can be checked
-  by computing the 'Hessian' and seeing if it is symmetric.
+  by computing the 'Hessian' (i.e. the partial derivatives of the components
+  of the gradient). For most (interesting) filters, the Hessian will no be
+  symmetric.
 
-  The Median Root Prior is obtained by using a median as ImageProcessor.
+  The Median Root Prior is obtained by using a MedianImageFilter3D as 
+  ImageProcessor.
 */
 template <typename elemT>
 class FilterRootPrior:  public 
@@ -53,14 +57,17 @@ class FilterRootPrior:  public
 	       >
 {
 public:
-
+  //! Name which will be used when parsing a GeneralisedPrior object
   static const char * const registered_name; 
 
+  //! Default constructor (no filter)
   FilterRootPrior();
 
+  //! Constructs it explicitly
+  /*! \todo first argument should be shared_ptr */
   FilterRootPrior(ImageProcessor<3,elemT>* filter, float penalization_factor);
   
-  // compute gradient by applying the filter
+  //! compute gradient by applying the filter
   void compute_gradient(DiscretisedDensity<3,elemT>& prior_gradient, 
 			const DiscretisedDensity<3,elemT> &current_image_estimate);
 
