6a8ed00c5617c623b0fd26f6a0665d4784c50f0e
Kris Thielemans <Kris.Thielemans@stir>
2001-06-19 17:01:13 +0000

    added

---
+++ src/include/local/fft.h
@@ -0,0 +1,43 @@
+//
+// $Id$
+//
+//
+/*!
+
+  \file
+
+  \brief Declaration of FFT routines
+
+  This are C++-ified version of Numerical Recipes routines. You need to have
+  the book before you can use this!
+
+  \warning This will be removed.
+
+  \author Claire Labbe
+  \author Kris Thielemans
+  \author PARAPET project
+
+  $Date :$
+
+  $Revision$
+*/
+#ifndef __FFT_H__
+#define __FFT_H__
+
+#include "Tomography_common.h"
+
+START_NAMESPACE_TOMO
+
+template <int num_dimensions, typename elemT> class Array;
+
+void four1(Array<1,float> &data, int nn, int isign);
+void fourn (Array<1,float> &data, Array<1,int> &nn, int ndim, int isign);
+//void convlv (Array<1,float> &data, const Array<1,float> &filter, int n);
+void convlvC (Array<1,float> &data, const Array<1,float> &filter, int n);
+//void rlft3(Array<3,float> &data, Array<2,float> &speq, unsigned long nn1, unsigned long nn2, unsigned long nn3, int isign);
+void realft ( Array<1,float> &data, int n, int isign);
+
+END_NAMESPACE_TOMO
+
+#endif
+
