d5944c75892d9b6b9b62fe547a45917b07acf88c
Kris Thielemans <Kris.Thielemans@stir>
2001-09-17 17:41:10 +0000

    - base on RegisteredParsingObject and add necessary members for parsing - added virtual set_up - changed 'int s' parameter of proj_Siddon to float s_in_mm

--- src/include/recon_buildblock/ForwardProjectorByBinUsingRayTracing.h
+++ src/include/recon_buildblock/ForwardProjectorByBinUsingRayTracing.h
@@ -1,5 +1,5 @@
 //
-// $Id$: $Date$
+// $Id$
 //
 /*!
 
@@ -11,45 +11,79 @@
   \author Kris Thielemans
   \author PARAPET project
       
-   \date $Date$
-   \version $Revision$
+   $Date$
+   $Revision$
 */
 #include "recon_buildblock/ForwardProjectorByBin.h"
+#include "tomo/RegisteredParsingObject.h"
+#include "shared_ptr.h"
 
 START_NAMESPACE_TOMO
 
-template <typename T> class shared_ptr;
+//template <typename T> class shared_ptr;
 template <typename elemT> class Viewgram;
 template <typename elemT> class RelatedViewgrams;
 template <typename elemT> class VoxelsOnCartesianGrid;
 template <int num_dimensions, typename elemT> class Array;
 class ProjDataInfo;
-class ProjDataInfoCylindricalArcCorr;
+class ProjDataInfoCylindrical;
 class DataSymmetriesForViewSegmentNumbers;
+class DataSymmetriesForBins_PET_CartesianGrid;
 
 /*!
   \ingroup recon_buildblock
   \brief This class implements forward projection using Siddon's algorithm for
   ray tracing. That is, it computes length of intersection with the voxels.
-*/
-  /*TODOdoc
 
+  Currently, the LOIs are divided by voxel_size.x(), unless NEWSCALE is
+  #defined during compilation time of ForwardProjectorByBinUsingRayTracing_Siddon.cxx. 
+
+  If the z voxel size is exactly twice the sampling in axial direction,
+  multiple LORs are used, to avoid missing voxels. (TODOdoc describe how).
+
+  Currently, a FOV is used which is circular, and is slightly 'inside' the 
+  image (i.e. the radius is about 1 voxel smaller than the maximum possible).
+
+  \warning Current implementation assumes that x,y voxel sizes are at least as 
+  large as the sampling in tangential direction, and that z voxel size is either
+  equal to or exactly twice the sampling in axial direction of the segments.
+
+  \warning For each bin, maximum 3 LORs are 'traced'
+  \warning The image forward projected HAS to be of type VoxelsOnCartesianGrid.
+  \warning The projection data info HAS to be of type ProjDataInfoCylindrical
+  \warning The implementation assumes that the \c s -coordinate is antisymmetric
+  in terms of the tangential_pos_num, i.e.
+  \code
+  proj_data_info_ptr->get_s(Bin(...,tang_pos_num)) == 
+  - proj_data_info_ptr->get_s(Bin(...,-tang_pos_num))
+  \endcode
 */
 
-class ForwardProjectorByBinUsingRayTracing : public ForwardProjectorByBin
+class ForwardProjectorByBinUsingRayTracing : 
+  public RegisteredParsingObject<ForwardProjectorByBinUsingRayTracing,
+                                 ForwardProjectorByBin>
 {
 public:
+    //! Name which will be used when parsing a ForwardProjectorByBin object
+  static const char * const registered_name; 
 
 
+  ForwardProjectorByBinUsingRayTracing();
+
+  //! Constructor
+  /*! \warning Obsolete */
   ForwardProjectorByBinUsingRayTracing(
                        const shared_ptr<ProjDataInfo>&,
                        const shared_ptr<DiscretisedDensity<3,float> >&);
+  //! Stores all necessary geometric info
+  /*! Note that the density_info_ptr is not stored in this object. It's only used to get some info on sizes etc.
+  */
+  virtual void set_up(		 
+    const shared_ptr<ProjDataInfo>& proj_data_info_ptr,
+    const shared_ptr<DiscretisedDensity<3,float> >& density_info_ptr // TODO should be Info only
+    );
 
-  // Informs on which symmetries the projector handles
-  // It should get data related by at least those symmetries.
-  // Otherwise, a run-time error will occur (unless the derived
-  // class has other behaviour).
-  const DataSymmetriesForViewSegmentNumbers * get_symmetries_used() const;
+  virtual const DataSymmetriesForViewSegmentNumbers * get_symmetries_used() const;
 
 
 private:
@@ -59,7 +93,8 @@ private:
 		  const int min_tangential_pos_num, const int max_tangential_pos_num);
 
 
-  const DataSymmetriesForViewSegmentNumbers * symmetries_ptr;
+  // KT 20/06/2001 changed type from 'const DataSymmetriesForViewSegmentNumbers *'
+  shared_ptr<DataSymmetriesForBins_PET_CartesianGrid> symmetries_ptr;
   /*
     The version which uses all possible symmetries.
     Here 0<=view < num_views/4 (= 45 degrees)
@@ -87,8 +122,6 @@ private:
     the derived class)
     Here 0<=view < num_views/2 (= 90 degrees)
     */
-
-public:
   virtual void 
   forward_project_view_plus_90_and_delta(
 					 Viewgram<float> & pos_view, 
@@ -113,15 +146,20 @@ forward_project_view_plus_90_and_delta_2D(Viewgram<float> & pos_view,
  				          const int min_axial_pos_num, const int max_axial_pos_num,
 				          const int min_tangential_pos_num, const int max_tangential_pos_num);
 
-//! The actual implementation of Siddon's algorithm 
+
+  // KT 20/06/2001 changed 'int s' parameter to 'float s_in_mm'
+  //! The actual implementation of Siddon's algorithm 
   static void proj_Siddon(Array<4,float> &Projptr, const VoxelsOnCartesianGrid<float> &, 
-                 const ProjDataInfoCylindricalArcCorr* proj_data_info_ptr, 
-                 const float cphi, const float sphi, const float delta, const int s, 
-                 const float R, const int rmin, const int rmax, const float offset, 
-                 const int Siddon,
-                 const int num_planes_per_virtual_ring,
-		 const float virtual_ring_offset);
+			  const ProjDataInfoCylindrical* proj_data_info_ptr, 
+			  const float cphi, const float sphi, const float delta, 
+			  const float s_in_mm, 
+			  const float R, const int rmin, const int rmax, const float offset, 
+			  const int Siddon,
+			  const float num_planes_per_virtual_ring,
+			  const float virtual_ring_offset);
 
 
+   virtual void set_defaults();
+   virtual void initialise_keymap();
 };
 END_NAMESPACE_TOMO
