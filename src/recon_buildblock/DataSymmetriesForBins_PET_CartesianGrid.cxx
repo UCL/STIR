df86a47a042c4034ec124fca4f88eecc26b36989
Kris Thielemans <Kris.Thielemans@stir>
2001-09-19 10:54:03 +0000

    - take origin.z() into account - added check that num_views is a multiple of 4 - added check that negative segments have - theta of the positive segment - added check on s-symmetry

--- src/recon_buildblock/DataSymmetriesForBins_PET_CartesianGrid.cxx
+++ src/recon_buildblock/DataSymmetriesForBins_PET_CartesianGrid.cxx
@@ -1,8 +1,7 @@
 //
-// $Id$: $Date$
+// $Id$
 //
 /*!
-
   \file
   \ingroup buildblock
   \brief non-inline implementations for class 
@@ -11,14 +10,14 @@
   \author Kris Thielemans
   \author PARAPET project
 
-  \date $Date$
-
-  \version $Revision$
+  $Date$
+  $Revision$
 */
 #include "recon_buildblock/DataSymmetriesForBins_PET_CartesianGrid.h"
 #include "ProjDataInfoCylindrical.h"
 #include "DiscretisedDensityOnCartesianGrid.h"
 #include "shared_ptr.h"
+#include "tomo/round.h"
 #include <typeinfo>
 
 START_NAMESPACE_TOMO
@@ -50,13 +49,20 @@ find_relation_between_coordinate_systems(int& num_planes_per_scanner_ring,
     const float num_planes_per_scanner_ring_float = 
       proj_data_info_cyl_ptr->get_ring_spacing() / image_plane_spacing;
         
-    num_planes_per_scanner_ring = static_cast<int>(num_planes_per_scanner_ring_float + 0.5);
+    num_planes_per_scanner_ring = round(num_planes_per_scanner_ring_float);
     
     if (fabs(num_planes_per_scanner_ring_float - num_planes_per_scanner_ring) > 1.E-5)
-    error("DataSymmetriesForBins_PET_CartesianGrid can currently only support z-grid spacing\
-    equal to the ring spacing of the scanner divided by an integer. Sorry\n");
+      error("DataSymmetriesForBins_PET_CartesianGrid can currently only support z-grid spacing "
+	    "equal to the ring spacing of the scanner divided by an integer. Sorry\n");
   }
   
+  if (fabs( cartesian_grid_info_ptr->get_origin().x()) > 1.E-5)
+    error("DataSymmetriesForBins_PET_CartesianGrid can currently only support x-origin = 0 "
+	  "Sorry\n");
+  if (fabs( cartesian_grid_info_ptr->get_origin().y()) > 1.E-5)
+    error("DataSymmetriesForBins_PET_CartesianGrid can currently only support y-origin = 0 "
+	  "Sorry\n");
+      
   
   for (int segment_num=min_segment_num; segment_num<=max_segment_num; ++segment_num)  
   {
@@ -65,18 +71,20 @@ find_relation_between_coordinate_systems(int& num_planes_per_scanner_ring,
         num_planes_per_axial_pos_float = 
         proj_data_info_cyl_ptr->get_axial_sampling(segment_num)/image_plane_spacing;
       
-      num_planes_per_axial_pos[segment_num] = static_cast<int>(num_planes_per_axial_pos_float + 0.5);
+      num_planes_per_axial_pos[segment_num] = round(num_planes_per_axial_pos_float);
       
       if (fabs(num_planes_per_axial_pos_float - num_planes_per_axial_pos[segment_num]) > 1.E-5)
-        error("DataSymmetriesForBins_PET_CartesianGrid can currently only support z-grid spacing\
-        equal to the axial sampling in the projection data divided by an integer. Sorry\n");
+        error("DataSymmetriesForBins_PET_CartesianGrid can currently only support z-grid spacing "
+	      "equal to the axial sampling in the projection data divided by an integer. Sorry\n");
       
     }  
     
     const float delta = proj_data_info_cyl_ptr->get_average_ring_difference(segment_num);
     
+    // KT 20/06/2001 take origin.z() into account
     axial_pos_to_z_offset[segment_num] = 
       (cartesian_grid_info_ptr->get_max_z() + cartesian_grid_info_ptr->get_min_z())/2.F
+      - cartesian_grid_info_ptr->get_origin().z()/image_plane_spacing
       -
       (num_planes_per_axial_pos[segment_num]
        *(proj_data_info_cyl_ptr->get_max_axial_pos_num(segment_num)  
@@ -100,7 +108,8 @@ DataSymmetriesForBins_PET_CartesianGrid
   : DataSymmetriesForBins(proj_data_info_ptr)
 {
   if(dynamic_cast<ProjDataInfoCylindrical *>(proj_data_info_ptr.get()) == NULL)
-    error("DataSymmetriesForBins_PET_CartesianGrid constructed with wrong type of ProjDataInfo: %s\n",
+    error("DataSymmetriesForBins_PET_CartesianGrid constructed with wrong type of ProjDataInfo: %s\n"
+          "(can only handle projection data corresponding to a cylinder)\n",
       typeid(*proj_data_info_ptr).name());
 
   const DiscretisedDensityOnCartesianGrid<3,float> *
@@ -112,8 +121,46 @@ DataSymmetriesForBins_PET_CartesianGrid
     error("DataSymmetriesForBins_PET_CartesianGrid constructed with wrong type of image info: %s\n",
       typeid(*image_info_ptr).name());
 
+  // WARNING get_grid_spacing()[1] == z
+  const float z_origin_in_planes =
+    image_info_ptr->get_origin().z()/cartesian_grid_info_ptr->get_grid_spacing()[1];
+  // z_origin_in_planes should be an integer
+  if (fabs(round(z_origin_in_planes) - z_origin_in_planes) > 1.E-4)
+    error("DataSymmetriesForBins_PET_CartesianGrid: the shift in the "
+          "z-direction of the origin (which is %g) should be a multiple of the plane "
+          "separation (%g)\n",
+          image_info_ptr->get_origin().z(), cartesian_grid_info_ptr->get_grid_spacing()[1]);
+
+
 
   num_views= proj_data_info_ptr->get_num_views();
+
+  if (num_views%4!=0)
+    error("DataSymmetriesForBins_PET_CartesianGrid can only handle projection data "
+	  "with the number of views a multiple of 4, while it is %d\n",
+	  num_views);
+
+  // check on segment symmetry
+  if (proj_data_info_ptr->get_tantheta(Bin(0,0,0,0)) != 0)
+    error("DataSymmetriesForBins_PET_CartesianGrid can only handle projection data "
+	  "with segment 0 corresponding to direct planes (i.e. theta==0)\n");
+
+  for (int segment_num=1; 
+       segment_num<= min(proj_data_info_ptr->get_max_segment_num(),
+			 -proj_data_info_ptr->get_min_segment_num());
+       ++segment_num)
+    if (fabs(proj_data_info_ptr->get_tantheta(Bin(segment_num,0,0,0)) +
+	     proj_data_info_ptr->get_tantheta(Bin(-segment_num,0,0,0))) > 1.E-4F)
+    error("DataSymmetriesForBins_PET_CartesianGrid can only handle projection data "
+	  "with negative segment numbers corresponding to -theta of the positive segments. "
+	  "This is not true for segment pair %d.\n", 
+	  segment_num);
+
+  //feable check on s-symmetry
+  if (fabs(proj_data_info_ptr->get_s(Bin(0,0,0,1)) + 
+           proj_data_info_ptr->get_s(Bin(0,0,0,-1))) > 1.E-4F)
+    error("DataSymmetriesForBins_PET_CartesianGrid can only handle projection data "
+	  "with tangential_pos_num s.t. get_s(...,tang_pos_num)==-get_s(...,-tang_pos_num)\n");
   
   find_relation_between_coordinate_systems(num_planes_per_scanner_ring,
                                          num_planes_per_axial_pos,
