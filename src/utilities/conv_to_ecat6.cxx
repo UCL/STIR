0fdc9aaa57591616b5ca0652617162c832d2d68f
Kris Thielemans <Kris.Thielemans@stir>
2001-09-19 11:45:17 +0000

    - can now put more than one image/projdata into a single ECAT6 file

--- src/utilities/conv_to_ecat6.cxx
+++ src/utilities/conv_to_ecat6.cxx
@@ -1,5 +1,5 @@
 //
-// $Id$: $Date$
+// $Id$
 //
 
 /*! 
@@ -8,8 +8,8 @@
 \brief Conversion from interfile to ECAT 6 cti (image and sinogram data)
 \author Kris Thielemans
 \author PARAPET project
-\version $Date$
-\date  $Revision$
+$Date$
+$Revision$
 
 */
 
@@ -19,11 +19,16 @@
 #include "shared_ptr.h"
 #include "utilities.h"
 #include "CTI/Tomo_cti.h"
-
+#include "CTI/cti_utils.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 #ifndef TOMO_NO_NAMESPACES
 using std::cerr;
+using std::endl;
+using std::vector;
+using std::string;
 #endif
 
 USING_NAMESPACE_TOMO
@@ -33,23 +38,32 @@ USING_NAMESPACE_TOMO
 
 int main(int argc, char *argv[])
 {
-  char header_name[1000], cti_name[1000], scanner_name[1000] = "";
+  char cti_name[1000], scanner_name[1000] = "";
+  vector<string> filenames;
   bool its_an_image = true;
   
-  if(argc==4)
+  if(argc>=4)
   {
     if (strcmp(argv[1],"-s")==0)
       {
 	its_an_image = false;
-	strcpy(header_name,argv[3]);
-	strcpy(cti_name,argv[2]);
+        strcpy(cti_name,argv[2]);
+        int num_files = argc-3;
+        argv+=3;
+        filenames.reserve(num_files);
+        for (; num_files>0; --num_files, ++argv)
+          filenames.push_back(*argv);	
       }
     else 
       {
-	its_an_image = true;	
-	strcpy(header_name,argv[2]);
-	strcpy(cti_name,argv[1]);
-	strcpy(scanner_name,argv[3]);
+	its_an_image = true;
+	strcpy(cti_name,argv[1]);	
+        int num_files = argc-3;
+        argv+=2;
+        filenames.reserve(num_files);
+        for (; num_files>0; --num_files, ++argv)
+          filenames.push_back(*argv);	
+	strcpy(scanner_name,*argv);
       }
   }  
   else 
@@ -57,16 +71,24 @@ int main(int argc, char *argv[])
     cerr<<"\nConversion from data to ECAT6 CTI.\n"
         <<"Usage: 2 possible forms depending on data type\n"
 	<< "For sinogram data:\n"
-	<< "\tconv_ecat6 -s ECAT6_name orig_name\n"
+	<< "\tconv_ecat6 -s ECAT6_name1 [ECAT6_name2 ...] orig_filename\n"
 	<< "For image data:\n"
-	<< "\tconv_ecat6 ECAT6_name orig_name scanner_name\n"
+	<< "\tconv_ecat6 ECAT6_name1  [ECAT6_name2 ...] orig_name scanner_name\n"
 	<< "scanner_name has to be recognised by the Scanner class\n"
 	<< "Examples are : \"ECAT 953\", \"RPT\" etc.\n"
 	<< "(the quotes are required when used as a command line argument)\n\n"
 	<< "I will now ask you the same info interactively...\n\n";
     
-    ask_filename_with_extension(header_name,"Name of the input file? ",".hs");    
-    its_an_image = ask("Is this an image",true);
+    its_an_image = ask("Converting images?",true);
+    int num_files = ask_num("Number of files",1,10000,1);
+    filenames.reserve(num_files);
+    char cur_name[max_filename_length];
+    for (; num_files>0; --num_files)
+    {
+      ask_filename_with_extension(cur_name,"Name of the input file? ",its_an_image?".hv":".hs");
+      filenames.push_back(cur_name);
+    }
+    
     ask_filename_with_extension(cti_name,"Name of the ECAT6 file? ",
       its_an_image ? ".img" : ".scn");
   }
@@ -79,25 +101,86 @@ int main(int argc, char *argv[])
       Scanner::ask_parameters() :
       Scanner::get_scanner_from_name(scanner_name);
 
+    // read first image
+    cerr << "Reading " << filenames[0] << endl;
     shared_ptr<DiscretisedDensity<3,float> > density_ptr =
-      DiscretisedDensity<3,float>::read_from_file(header_name);
+      DiscretisedDensity<3,float>::read_from_file(filenames[0]);
+  
+    Main_header mhead;
+    make_ECAT6_main_header(mhead, *scanner_ptr, filenames[0], *density_ptr);
+    mhead.num_frames = filenames.size();
 
-    if (DiscretisedDensity_to_ECAT6(*density_ptr, cti_name, header_name, 
-				    *scanner_ptr) == Succeeded::yes)
-      return EXIT_SUCCESS;    
-    else
+    FILE *fptr= cti_create (cti_name, &mhead);
+    if (fptr == NULL)
+    {
+      warning("conv_to_ecat6: error opening output file %s\n", cti_name);
       return EXIT_FAILURE;
+    }
+    unsigned int frame_num = 1;
+
+    while (1)
+    {
+      if (DiscretisedDensity_to_ECAT6(fptr,
+                                      *density_ptr, 
+                                      mhead,
+                                      frame_num)
+                                      == Succeeded::no)
+      {
+        fclose(fptr);
+        return EXIT_FAILURE;
+      }
+      if (++frame_num > filenames.size())
+      {
+        fclose(fptr);
+        return EXIT_SUCCESS;
+      }
+      cerr << "Reading " << filenames[frame_num-1] << endl;
+      density_ptr =
+        DiscretisedDensity<3,float>::read_from_file(filenames[frame_num-1]);
+    }
   }
   else 
   {
-    shared_ptr<ProjData> proj_data_ptr=
-       ProjData::read_from_file(header_name);
+ 
+    // read first data set
+    cerr << "Reading " << filenames[0] << endl;
+    shared_ptr<ProjData > proj_data_ptr =
+      ProjData::read_from_file(filenames[0]);
+  
+    Main_header mhead;
+    make_ECAT6_main_header(mhead, filenames[0], *proj_data_ptr->get_proj_data_info_ptr());
+    mhead.num_frames = filenames.size();
 
-    if (ProjData_to_ECAT6(*proj_data_ptr, cti_name, header_name) == Succeeded::yes)
-      return EXIT_SUCCESS;
-    else
+    FILE *fptr= cti_create (cti_name, &mhead);
+    if (fptr == NULL)
+    {
+      warning("conv_to_ecat6: error opening output file %s\n", cti_name);
       return EXIT_FAILURE;
-  }
+    }
+
+    unsigned int frame_num = 1;
+
+    while (1)
+    {
+      if (ProjData_to_ECAT6(fptr,
+                            *proj_data_ptr, 
+                            mhead,
+                            frame_num)
+                            == Succeeded::no)
+      {
+        fclose(fptr);
+        return EXIT_FAILURE;
+      }
+      if (++frame_num > filenames.size())
+      {
+        fclose(fptr);
+        return EXIT_SUCCESS;
+      }
+      cerr << "Reading " << filenames[frame_num-1] << endl;
+      proj_data_ptr =
+        ProjData::read_from_file(filenames[frame_num-1]);
+    }
+  }  
 }
 
 
