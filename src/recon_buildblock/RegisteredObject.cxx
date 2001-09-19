dd7f7c93e9883437e777ce9e485519c066cdd963
Kris Thielemans <Kris.Thielemans@stir>
2001-09-19 10:58:56 +0000

    - added projmatrix, projector and normalisation entries

--- src/recon_buildblock/RegisteredObject.cxx
+++ src/recon_buildblock/RegisteredObject.cxx
@@ -17,6 +17,22 @@
 #ifdef _MSC_VER
 #pragma message("instantiating RegisteredObject<GeneralisedPrior<float> >")
 #include "tomo/recon_buildblock/GeneralisedPrior.h"
+
+#pragma message("instantiating RegisteredObject<ProjMatrixByBin>")
+#include "recon_buildblock/ProjMatrixByBin.h"
+
+#pragma message("instantiating RegisteredObject<ProjectorByBinPair>")
+#include "recon_buildblock/ProjectorByBinPair.h"
+
+#pragma message("instantiating RegisteredObject<ForwardProjectorByBin>")
+#include "recon_buildblock/ForwardProjectorByBin.h"
+
+#pragma message("instantiating RegisteredObject<BackProjectorByBin>")
+#include "recon_buildblock/BackProjectorByBin.h"
+
+#pragma message("instantiating RegisteredObject<BinNormalisation>")
+#include "tomo/recon_buildblock/BinNormalisation.h"
+
 // and others
 START_NAMESPACE_TOMO
 
@@ -28,9 +44,24 @@ RegisteredObject<Root>::registry ()
   return the_registry;
 }
 
+#  ifdef _MSC_VER
+// prevent warning message on reinstantiation, 
+// note that we get a linking error if we don't have the explicit instantiation below
+#  pragma warning(disable:4660)
+#  endif
+
+
 template RegisteredObject<GeneralisedPrior<float> >;
 
+template RegisteredObject<ProjMatrixByBin>; 
 
+template RegisteredObject<ProjectorByBinPair>;
+
+template RegisteredObject<ForwardProjectorByBin>;
+
+template RegisteredObject<BackProjectorByBin>;
+
+template RegisteredObject<BinNormalisation>;
 END_NAMESPACE_TOMO
 
 #endif
