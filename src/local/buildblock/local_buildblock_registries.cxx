b8534747c64e3f0acc45ac4ac0510e8a7a8f5eac
Sanida Mustafovic <Sanida.Mustafovic@stir>
2001-10-03 09:47:43 +0000

    added SeparableLowPassFilter

--- src/local/buildblock/local_buildblock_registries.cxx
+++ src/local/buildblock/local_buildblock_registries.cxx
@@ -1,7 +1,14 @@
 #include "local/tomo/DAVImageFilter3D.h"
+#include "local/tomo/ModifiedInverseAverigingImageFilter.h"
+#include "local/tomo/InverseSeparableCartesianMetzImageFilter.h"
+#include "local/tomo/SeparableLowPassImageFilter.h"
 
 
 START_NAMESPACE_TOMO
 
-static DAVImageFilter3D<float>::RegisterIt dummy3;
+static DAVImageFilter3D<float>::RegisterIt dummy1;
+static ModifiedInverseAverigingImageFilter<float>::RegisterIt dummy2;
+static InverseSeparableCartesianMetzImageFilter <float>::RegisterIt dummy3;
+static SeparableLowPassImageFilter <float>::RegisterIt dummy4;
+
 END_NAMESPACE_TOMO
