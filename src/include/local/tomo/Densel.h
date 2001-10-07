2d7a715ae582ac19287c61dec870d821cfab45cb
Kris Thielemans <Kris.Thielemans@stir>
2001-10-07 01:07:53 +0000

    - first version. Contrary to Bin , doesn't have data value (yet)

---
+++ src/include/local/tomo/Densel.h
@@ -0,0 +1,15 @@
+//
+// $Id$
+//
+#ifndef __tomo_Densel__H_
+#define __tomo_Densel__H_
+//TODO
+#include "Coordinate3D.h"
+
+START_NAMESPACE_TOMO
+
+typedef Coordinate3D<int> Densel;
+
+END_NAMESPACE_TOMO
+
+#endif
