3b19483805fdd40f1ae08704e691a6b162c887d4
Kris Thielemans <Kris.Thielemans@stir>
2001-10-08 15:34:07 +0000

    - added QuadraticPrior

--- src/local/recon_buildblock/local_recon_buildblock_registries.cxx
+++ src/local/recon_buildblock/local_recon_buildblock_registries.cxx
@@ -1,5 +1,6 @@
 
 #include "local/tomo/recon_buildblock/ProjMatrixByBinUsingSolidAngle.h"
+#include "local/tomo/recon_buildblock/QuadraticPrior.h"
 #include "local/recon_buildblock/oldForwardProjectorByBinUsingRayTracing.h"
 #include "local/recon_buildblock/oldBackProjectorByBinUsingInterpolation.h"
 #include "local/recon_buildblock/PostsmoothingForwardProjectorByBin.h"
@@ -12,4 +13,5 @@ static oldForwardProjectorByBinUsingRayTracing::RegisterIt dummy1;
 static PostsmoothingForwardProjectorByBin::RegisterIt dummy2;
 static oldBackProjectorByBinUsingInterpolation::RegisterIt dummy3;
 
+static QuadraticPrior<float>::RegisterIt dummy21;
 END_NAMESPACE_TOMO
