77f75ce64a397e8ab1600943788ea9e0c00a9ba5
Kris Thielemans <Kris.Thielemans@stir>
2001-11-29 19:51:04 +0000

    - made parseable - made sure it can normalise arbitrary sizes of tang_pos_num. However, it still   supposes that the 'bin-size' in the profile and the data are the same.   Also, the size of the profile is still hard-wired.

--- src/local/recon_buildblock/BinNormalisationUsingProfile.cxx
+++ src/local/recon_buildblock/BinNormalisationUsingProfile.cxx
@@ -23,10 +23,31 @@ const char * const
   registered_name = "Using Profile"; 
 
 void BinNormalisationUsingProfile::set_defaults()
-{}//TODO
+{
+  profile_filename = "";
+}
 
 void BinNormalisationUsingProfile::initialise_keymap()
-{}
+{
+  parser.add_start_key("Bin Normalisation Using Profile");
+  parser.add_key("normalisation_profile_filename", &profile_filename);
+  parser.add_stop_key("End Bin Normalisation Using Profile");
+}
+
+bool 
+BinNormalisationUsingProfile::
+post_processing()
+{
+  profile = Array<1,float>(-114,113);
+  ifstream profile_data(profile_filename.c_str());
+  for (int i=profile.get_min_index(); i<=profile.get_max_index(); ++i)
+    profile_data >> profile[i];
+  if (!profile_data)
+    error("Error reading profile %s\n", profile_filename.c_str());
+
+  return false;
+}
+
 
 BinNormalisationUsingProfile::
 BinNormalisationUsingProfile()
@@ -36,7 +57,7 @@ BinNormalisationUsingProfile()
 
 BinNormalisationUsingProfile::
 BinNormalisationUsingProfile(const string& filename)
-    : profile(  Array<1,float>(-40,39) )
+    : profile(  Array<1,float>(-114,113) )
 {
   ifstream profile_data(filename.c_str());
   for (int i=profile.get_min_index(); i<=profile.get_max_index(); ++i)
@@ -59,7 +80,12 @@ apply(RelatedViewgrams<float>& viewgrams) const
 	   ax_pos_num<=viewgrams_iter->get_max_index();
 	   ++ax_pos_num)
 	{
-	  (*viewgrams_iter)[ax_pos_num] *= profile;
+	  for (int i=std::max(profile.get_min_index(),viewgrams.get_min_tangential_pos_num());
+	       i <= std::min(profile.get_max_index(),viewgrams.get_max_tangential_pos_num());
+	       ++i)
+	  (*viewgrams_iter)[ax_pos_num][i] *= profile[i];
+
+	  // (*viewgrams_iter)[ax_pos_num] *= profile;
 	}
     }
 }
@@ -68,6 +94,20 @@ void
 BinNormalisationUsingProfile::
 undo(RelatedViewgrams<float>& viewgrams) const 
 {
+  /*
+  const int old_min = profile.get_min_index();
+  const int old_max = profile.get_max_index();
+  profile.grow(viewgrams.get_min_tangential_pos_num(),viewgrams.get_max_tangential_pos_num());
+  if (profile.get_min_index() != old_min ||
+      profile.get_max_index() != old_max)
+    warning("BinNormalisationUsingProfile: growing profile to (%d,%d)\n",
+      profile.get_min_index(), profile.get_max_index());
+  for (int i=profile.get_min_index(); i<old_min; ++i)
+    profile[i] = 1.F;
+  for (int i=profile.get_max_index(); i>old_max; --i)
+    profile[i] = 1.F;
+  */
+
   RelatedViewgrams<float>::iterator viewgrams_iter = 
     viewgrams.begin();
   for (; 
@@ -78,7 +118,10 @@ undo(RelatedViewgrams<float>& viewgrams) const
 	   ax_pos_num<=viewgrams_iter->get_max_index();
 	   ++ax_pos_num)
 	{
-	  (*viewgrams_iter)[ax_pos_num] /= profile;
+	  for (int i=std::max(profile.get_min_index(),viewgrams.get_min_tangential_pos_num());
+	       i <= std::min(profile.get_max_index(),viewgrams.get_max_tangential_pos_num());
+	       ++i)
+	  (*viewgrams_iter)[ax_pos_num][i] /= profile[i];
 	}
     }
 }
