d81b9568491185df087d9f369e53595fb29bb509
Kris Thielemans <Kris.Thielemans@stir>
2001-11-29 19:36:53 +0000

    - added doc about interleaving - added get_view_tangential_pos_num_for_det_num_pair         get_det_num_pair_for_view_tangential_pos_num         get_bin_for_det_pair         get_det_pair_for_bin

--- src/include/ProjDataInfoCylindricalNoArcCorr.h
+++ src/include/ProjDataInfoCylindricalNoArcCorr.h
@@ -21,18 +21,49 @@
 
 START_NAMESPACE_TOMO
 
+class Succeeded;
+
 /*!
   \ingroup buildblock 
-  \brief Projection data info for data which are not arc-corrected
+  \brief Projection data info for data which are not arc-corrected.
 
-  This means that 'tangential_pos_num' actually indexes an angular coordinate
+  For this class, 'tangential_pos_num' actually indexes an angular coordinate
   with a particular angular sampling (usually given by half the angle between
   detectors). That is
   \code
     get_s(Bin(..., tang_pos_num)) == ring_radius * sin(tang_pos_num*angular_increment)
   \endcode
-  This class does \c not take the 'interleaving' into account which is 
-  customarily applied to raw PET data.
+
+  This class also contains some functions specific for (static) full-ring PET 
+  scanners. In this case, it is assumed that for 'raw' data (i.e. no mashing)
+  sinogram space is 'interleaved': 2 adjacent LOR_angles are
+  merged to 1 'view', while the corresponding bins are
+  interleaved:
+
+  \verbatim
+     before interleaving               after interleaving
+     a00     a01     a02 ...      view 0: a00 a10 a01 a11  ...
+         a10     a11     ...
+     a20     a21     a22 ...      view 1: a20 a30 a21 a31 ...
+         a30     a31     ...
+	 \endverbatim
+  This (standard) interleaving is done because for 'odd' LOR_angles there
+  is no LOR which goes through the origin.
+
+
+  Interchanging the 2 detectors:
+  ------------------------------
+  When the ring difference = 0 (i.e. a 2D - or direct - sinogram),
+  interchanging the 2 detectors does not change the LOR. This is why
+  (in 2D) one gets away with a full sinogram size of
+  num_views * 2 * num_views, where the size of 'detector-space' is
+  twice as large.
+  However, in 3D, interchanging the detectors, also interchanges the
+  rings, and we have a totally different LOR. One has 2 options:
+  - have 1 sinogram with twice as many views, together with the rings
+    as 'unordered pair' (i.e. ring_difference is always >0)
+  - have 2 sinograms of the same size as in 2D, together with the rings
+    as 'ordered pair' (i.e. ring_difference can be positive and negative).
   */
 class ProjDataInfoCylindricalNoArcCorr : public ProjDataInfoCylindrical
 {
@@ -58,10 +89,86 @@ public:
     const int num_views,const int num_tangential_poss);
 
   ProjDataInfo* clone() const;
-  
+
+  //! Gets s coordinate in mm
+  /*! \warning   
+    This does \c not take the 'interleaving' into account which is 
+    customarily applied to raw PET data.
+  */
   inline virtual float get_s(const Bin&) const;
 
   virtual string parameter_info() const;
+
+  //! This gets view_num and tang_pos_num for a particular detector pair
+  /*! This function makes only sense if the scanner is a full-ring scanner 
+      with discrete detectors and there is no rotation or wobble.
+
+      \arg view runs currently from 0 to num_views-1
+
+      \arg tang_pos_num is centred around 0, where 0 corresponds
+      to opposing detectors. The maximum range of tangential positions for any
+      scanner is (-(num_detectors)/2,-(num_detectors)/2+num_detectors) , but 
+      this range is never used (as the 'last' tang_pos_num would be a LOR 
+      between two adjacent detectors).
+
+      \arg det_num1, \a det_num2 run from 0 to num_detectors-1
+
+      \return \c true if the detector pair is stored in a positive segment,
+              \c false otherwise.
+
+      \warning Current implementation only checked for Siemens/CTI scanners.
+      It assumes interleaved data.
+
+      \see get_det_num_pair_for_view_tangential_pos_num()
+  */			
+  inline bool 
+     get_view_tangential_pos_num_for_det_num_pair(int& view_num,
+						 int& tang_pos_num,
+						 const int det_num1,
+						 const int det_num2) const;
+  //! This routine gets \a det_num1 and \a det_num2
+  /*! 
+      It sets the detectors in a particular order (i.e. it fixes the 
+      orientation of the LOR) corresponding to the detector pair belonging
+      to a positive segment.
+
+      \warning This currently only works when no mashing of views is used,
+      as otherwise there would be multiple detector pairs corresponding 
+      to a single bin.
+      
+      \see get_view_tangential_pos_num_for_det_num_pair() for info and 
+      restrictions.
+   */
+  inline void
+    get_det_num_pair_for_view_tangential_pos_num(
+						 int& det_num1,
+						 int& det_num2,
+						 const int view_num,
+						 const int tang_pos_num) const;
+
+
+  //! This gets view_num and tang_pos_num for a particular detector pair
+  /*! 
+    \return Succeeded::yes when a corresponding segment is found
+    \see get_view_tangential_pos_num_for_det_num_pair() for restrictions
+  */		       
+  inline Succeeded 
+    get_bin_for_det_pair(Bin&,
+			 const int det_num1, const int ring_num1,
+			 const int det_num2, const int ring_num2) const;
+
+
+  //! This routine gets the detector pair corresponding to a bin.
+  /*! 
+    \see get_det_pair_for_view_tangential_pos_num() for
+    restrictions. In addition, this routine only works for span=1 data,
+    i.e. no axial compression.
+   */
+  inline void
+    get_det_pair_for_bin(
+			 int& det_num1, int& ring_num1,
+			 int& det_num2, int& ring_num2,
+			 const Bin&) const;
 
 private:
   
--- src/include/ProjDataInfoCylindricalNoArcCorr.inl
+++ src/include/ProjDataInfoCylindricalNoArcCorr.inl
@@ -16,6 +16,7 @@
 */
 
 #include "Bin.h"
+#include "tomo/Succeeded.h"
 #include <math.h>
 
 START_NAMESPACE_TOMO
@@ -26,6 +27,150 @@ ProjDataInfoCylindricalNoArcCorr::get_s(const Bin& bin) const
   return ring_radius * sin(bin.tangential_pos_num()*angular_increment);
 }
 
+/*
+   TODO make compile time assert
+
+   Warning:
+   this code makes use of an implementation dependent feature:
+   bit shifting negative ints to the right.
+    -1 >> 1 should be -1
+    -2 >> 1 should be -1
+   This is ok on SUNs (gcc, but probably SUNs cc as well), Parsytec (gcc),
+   Pentium (gcc, VC++) and probably every other system which uses
+   the 2-complement convention.
+*/
+
+/*
+  Go from sinograms to detectors.
+  Because sinograms are not arc-corrected, tang_pos_num corresponds
+  to an angle as well. Before interleaving we have that
+  \verbatim
+  det_angle_1 = LOR_angle + bin_angle
+  det_angle_2 = LOR_angle + (Pi - bin_angle)
+  \endverbatim
+  (Hint: understand this first at LOR_angle=0, then realise that
+  other LOR_angles follow just by rotation)
+
+  Code gets slightly intricate because:
+  - angles have to be defined modulo 2 Pi (so num_detectors)
+  - interleaving
+*/
+void
+ProjDataInfoCylindricalNoArcCorr::
+get_det_num_pair_for_view_tangential_pos_num(
+					     int& det_num1,
+					     int& det_num2,
+					     const int view_num,
+					     const int tang_pos_num) const
+{
+  assert(get_view_mashing_factor() == 1);
+  // TODO check on 180 degrees for views
+  const int num_detectors =
+    get_scanner_ptr()->get_num_detectors_per_ring();
+
+  assert(num_detectors%2 == 0);
+  /*
+     adapted from CTI code
+     Note for implementation: avoid using % with negative numbers
+     so add num_detectors before doing modulo num_detectors)
+  */
+  det_num1 = (view_num + (tang_pos_num >> 1) + num_detectors) % num_detectors;
+  det_num2 = (view_num - ( (tang_pos_num + 1) >> 1 ) + num_detectors/2) % num_detectors;
+}
+
+
+bool 
+ProjDataInfoCylindricalNoArcCorr::
+get_view_tangential_pos_num_for_det_num_pair(int& view_num,
+					     int& tang_pos_num,
+					     const int det_num1,
+					     const int det_num2) const
+{
+  /*
+     Note for implementation: avoid using % with negative numbers
+     so add 2*nv before doing modulo 2*nv
+
+     This somewhat obscure formula was obtained by inverting the code above
+     TODO This can be simplified (especially all the branching later on).
+ */
+  // TODO check on 180 degrees for views
+  const int num_detectors =
+    get_scanner_ptr()->get_num_detectors_per_ring();
+
+  assert(num_detectors%2 == 0);
+  const int max_num_views = num_detectors/2;
+
+  int swap_detectors;
+  tang_pos_num = (det_num1 - det_num2 +  3*num_detectors/2) % num_detectors;
+  int view = (det_num1 - (tang_pos_num >> 1) +  num_detectors) % num_detectors;
+
+  /* Now adjust ranges for view, tang_pos_num.
+     The next lines go only wrong in the singular (and irrelevant) case
+     det_num1 == det_num2 (when tang_pos_num == num_detectors - tang_pos_num)
+
+     We use the combinations of the following 'symmetries' of
+     (tang_pos_num, view) == (tang_pos_num+2*num_views, view + num_views)
+                 == (-tang_pos_num, view + num_views)
+     Using the latter interchanges det_num1 and det_num2, and this leaves
+     the LOR the same the 2D case. However, in 3D this interchanges the rings
+     as well. So, we keep track of this in swap_detectors, and return its final
+     value.
+     */
+  if (view <  max_num_views)
+    {
+      if (tang_pos_num >=  max_num_views)
+      {
+	tang_pos_num = num_detectors - tang_pos_num;
+	swap_detectors = 1;
+      }
+      else
+      {
+        swap_detectors = 0;
+      }
+    }
+  else
+    {
+      view -= max_num_views;
+      if (tang_pos_num >=  max_num_views)
+      {
+	tang_pos_num -= num_detectors;
+        swap_detectors = 0;
+      }
+      else
+      {
+	tang_pos_num *= -1;
+	swap_detectors = 1;
+      }
+    }
+
+  view_num = view/get_view_mashing_factor();
+  return swap_detectors==0;
+}
+
+
+Succeeded 
+ProjDataInfoCylindricalNoArcCorr::
+get_bin_for_det_pair(Bin& bin,
+		     const int det_num1, const int ring_num1,
+		     const int det_num2, const int ring_num2) const
+{  
+  if (get_view_tangential_pos_num_for_det_num_pair(bin.view_num(), bin.tangential_pos_num(), det_num1, det_num2))
+    return get_segment_axial_pos_num_for_ring_pair(bin.segment_num(), bin.axial_pos_num(), ring_num1, ring_num2);
+  else
+    return get_segment_axial_pos_num_for_ring_pair(bin.segment_num(), bin.axial_pos_num(), ring_num2, ring_num1);
+}
+
+void
+ProjDataInfoCylindricalNoArcCorr::
+get_det_pair_for_bin(
+		     int& det_num1, int& ring_num1,
+		     int& det_num2, int& ring_num2,
+		     const Bin& bin) const
+{
+  get_det_num_pair_for_view_tangential_pos_num(det_num1, det_num2, bin.view_num(), bin.tangential_pos_num());
+  get_ring_pair_for_segment_axial_pos_num( ring_num1, ring_num2, bin.segment_num(), bin.axial_pos_num());
+
+}
 
 
 END_NAMESPACE_TOMO
