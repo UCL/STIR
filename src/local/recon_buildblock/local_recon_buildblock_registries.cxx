18f113b4f1b9642260a641220c953835d4ee3763
Kris Thielemans <Kris.Thielemans@stir>
2001-12-19 18:36:06 +0000

    - removed old projector from the normal build

--- src/local/recon_buildblock/local_recon_buildblock_registries.cxx
+++ src/local/recon_buildblock/local_recon_buildblock_registries.cxx
@@ -1,8 +1,8 @@
 
 #include "local/tomo/recon_buildblock/ProjMatrixByBinUsingSolidAngle.h"
 #include "local/tomo/recon_buildblock/QuadraticPrior.h"
-#include "local/recon_buildblock/oldForwardProjectorByBinUsingRayTracing.h"
-#include "local/recon_buildblock/oldBackProjectorByBinUsingInterpolation.h"
+//#include "local/recon_buildblock/oldForwardProjectorByBinUsingRayTracing.h"
+//#include "local/recon_buildblock/oldBackProjectorByBinUsingInterpolation.h"
 #include "local/recon_buildblock/PostsmoothingForwardProjectorByBin.h"
 #include "local/tomo/recon_buildblock/BinNormalisationUsingProfile.h"
 
@@ -10,9 +10,9 @@ START_NAMESPACE_TOMO
 
 static ProjMatrixByBinUsingSolidAngle::RegisterIt dummy11;
 
-static oldForwardProjectorByBinUsingRayTracing::RegisterIt dummy1;
+//static oldForwardProjectorByBinUsingRayTracing::RegisterIt dummy1;
 static PostsmoothingForwardProjectorByBin::RegisterIt dummy2;
-static oldBackProjectorByBinUsingInterpolation::RegisterIt dummy3;
+//static oldBackProjectorByBinUsingInterpolation::RegisterIt dummy3;
 
 static QuadraticPrior<float>::RegisterIt dummy21;
 
