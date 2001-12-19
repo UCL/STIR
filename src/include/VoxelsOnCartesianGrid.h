6aec4a407646b92e7fa23690fd7a0692c2b25821
Kris Thielemans <Kris.Thielemans@stir>
2001-12-19 17:48:22 +0000

    - made constructors and set_voxel non-inline - removed constructor with ProjDataInfo and make_xy_size_odd - changed  constructor with ProjDataInfo with xy_size to a more general one allowing changes in z_size as well

--- src/include/VoxelsOnCartesianGrid.h
+++ src/include/VoxelsOnCartesianGrid.h
@@ -18,11 +18,13 @@
   $Revision$
 
 */
-#include "PixelsOnCartesianGrid.h"
+#include "DiscretisedDensityOnCartesianGrid.h"
+#include "CartesianCoordinate3D.h"
 
 START_NAMESPACE_TOMO
 
 class ProjDataInfo;
+template <typename elemT> class PixelsOnCartesianGrid;
 
 /*!
   \ingroup buildblock
@@ -41,44 +43,52 @@ public:
 static VoxelsOnCartesianGrid ask_parameters();
 
 //! Construct an empty VoxelsOnCartesianGrid (empty range, 0 origin, 0 grid_spacing)
-inline VoxelsOnCartesianGrid();
+VoxelsOnCartesianGrid();
 
 //! Construct a VoxelsOnCartesianGrid, initialising data from the Array<3,elemT> object.
-inline VoxelsOnCartesianGrid(const Array<3,elemT>& v,
+/*! All elements are set 0. */
+VoxelsOnCartesianGrid(const Array<3,elemT>& v,
 		      const CartesianCoordinate3D<float>& origin,
 		      const BasicCoordinate<3,float>& grid_spacing);
 
-//! Construct a VoxelsOnCartesianGrid, setting elements to 0
-inline VoxelsOnCartesianGrid(const IndexRange<3>& range,
+//! Construct a VoxelsOnCartesianGrid from an index range
+/*! All elements are set 0. */
+VoxelsOnCartesianGrid(const IndexRange<3>& range,
 		      const CartesianCoordinate3D<float>& origin, 
 		      const BasicCoordinate<3,float>& grid_spacing);
 
+  // KT 10/12/2001 replace 2 constructors with the more general one below
 //! use ProjDataInfo to obtain the size information
 /*!
-   A default xy_size is found by taking the diameter 
-   of the FOV spanned by the projection data 
-*/
-VoxelsOnCartesianGrid(const ProjDataInfo& proj_data_info_ptr,
-		      const float zoom = 1.F, 
-		      const CartesianCoordinate3D<float>& origin = CartesianCoordinate3D<float>(0.F,0.F,0.F) ,
-		      const bool make_xy_size_odd = true);
+   When sizes.x() is -1, a default size in x is found by taking the diameter 
+   of the FOV spanned by the projection data. Similar for sizes.y().
 
-//! use ProjDataInfo to obtain the size information, but use non-default size for x,y (unless it's -1)
-/*!
-   When xy_size is -1, a default size is found by taking the diameter 
-   of the FOV spanned by the projection data 
+   When sizes.z() is -1, a default size in z is found by taking the number of planes as
+   <ul>
+   </li> $N_0$ when segment 0 is axially compressed,
+   </li> $2N_0-1$ when segment 0 is not axially compressed,
+   </ul>
+   where $N_0$ is the number of sinograms in segment 0.
+
+   Actual index ranges start from 0 for z, but from -(x_size_used/2) for x (and similar for y).
+
+   x,y grid spacing are set to the sampling distance in s (for bin 0).
+   z grid spacing is set to half the scanner ring distance.
+
+   All elements are set 0.
+
 */
 VoxelsOnCartesianGrid(const ProjDataInfo& proj_data_info_ptr,
-		      const float zoom,
-		      const CartesianCoordinate3D<float>& origin, 
-		      const int xy_size);
+		      const float zoom = 1.F,
+		      const CartesianCoordinate3D<float>& origin = CartesianCoordinate3D<float>(0.F,0.F,0.F), 
+		      const CartesianCoordinate3D<int>& sizes = CartesianCoordinate3D<int>(-1,-1,-1));
 
 
 //! Definition of the pure virtual defined in DiscretisedDensity
 #ifdef TOMO_NO_COVARIANT_RETURN_TYPES
-inline DiscretisedDensity<3,elemT>*
+DiscretisedDensity<3,elemT>*
 #else
-inline VoxelsOnCartesianGrid<elemT>*
+VoxelsOnCartesianGrid<elemT>*
 #endif
  get_empty_discretised_density() const;
 
@@ -98,7 +108,7 @@ void set_plane(const PixelsOnCartesianGrid<elemT>& plane, const int z);
 inline CartesianCoordinate3D<float> get_voxel_size() const;
 
 //! is the same as set_grid_spacing()
-inline void set_voxel_size(const BasicCoordinate<3,float>&);
+void set_voxel_size(const BasicCoordinate<3,float>&);
 
 //! Growing of outer dimension only
 void grow_z_range(const int min_z, const int max_z);
