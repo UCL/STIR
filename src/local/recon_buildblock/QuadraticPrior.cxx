6a09baa25d6479d4461679544fd59ed43a01cae6
Sanida Mustafovic <Sanida.Mustafovic@stir>
2002-01-21 13:30:11 +0000

    implemented parabolic_surrogate_curvature

--- src/local/recon_buildblock/QuadraticPrior.cxx
+++ src/local/recon_buildblock/QuadraticPrior.cxx
@@ -28,8 +28,8 @@ template <typename elemT>
 void 
 QuadraticPrior<elemT>::initialise_keymap()
 {
+  GeneralisedPrior<elemT>::initialise_keymap();
   parser.add_start_key("Quadratic Prior Parameters");
-  parser.add_key("penalisation_factor", &penalisation_factor);
   parser.add_key("only 2D", &only_2D); 
   parser.add_key("kappa filename", &kappa_filename);
   parser.add_stop_key("END Quadratic Prior Parameters");
@@ -40,6 +40,8 @@ template <typename elemT>
 bool 
 QuadraticPrior<elemT>::post_processing()
 {
+  if (GeneralisedPrior<elemT>::post_processing()==true)
+    return true;
   if (kappa_filename.size() != 0)
     kappa_ptr = DiscretisedDensity<3,elemT>::read_from_file(kappa_filename);
   return false;
@@ -49,8 +51,8 @@ template <typename elemT>
 void
 QuadraticPrior<elemT>::set_defaults()
 {
+  GeneralisedPrior<elemT>::set_defaults();
   only_2D = false;
-  penalisation_factor = 0;
   kappa_ptr = 0;  
 }
 
@@ -117,6 +119,7 @@ compute_gradient(DiscretisedDensity<3,elemT>& prior_gradient,
     dynamic_cast<DiscretisedDensityOnCartesianGrid<3,float> &>(prior_gradient);
 
   compute_weights(weights, current_image_cast.get_grid_spacing());
+ 
   
   const bool do_kappa = kappa_ptr.use_count() != 0;
   
@@ -174,15 +177,101 @@ compute_gradient(DiscretisedDensity<3,elemT>& prior_gradient,
 
   std::cerr << "Prior gradient max " << prior_gradient.find_max()
     << ", min " << prior_gradient.find_min() << std::endl;
-  {
+ /* {
     static int count = 0;
     ++count;
     char filename[20];
     sprintf(filename, "gradient%d.v",count);
     write_basic_interfile(filename, prior_gradient);
-  }
+  }*/
 }
 
+template <typename elemT>
+void 
+QuadraticPrior<elemT>::parabolic_surrogate_curvature(DiscretisedDensity<3,elemT>& parabolic_surrogate_curvature, 
+			const DiscretisedDensity<3,elemT> &current_image_estimate)
+{
+
+  assert( parabolic_surrogate_curvature.get_index_range() == current_image_estimate.get_index_range());  
+  if (penalisation_factor==0)
+  {
+    parabolic_surrogate_curvature.fill(0);
+    return;
+  }
+  
+  
+  const DiscretisedDensityOnCartesianGrid<3,float>& current_image_cast =
+    dynamic_cast< const DiscretisedDensityOnCartesianGrid<3,float> &>(current_image_estimate);
+  
+  DiscretisedDensityOnCartesianGrid<3,float>& parabolic_surrogate_curvature_cast =
+    dynamic_cast<DiscretisedDensityOnCartesianGrid<3,float> &>(parabolic_surrogate_curvature);
+
+  compute_weights(weights, current_image_cast.get_grid_spacing());
+  
+  const bool do_kappa = kappa_ptr.use_count() != 0;
+  
+  if (do_kappa && kappa_ptr->get_index_range() != current_image_estimate.get_index_range())
+    error("QuadraticPrior: kappa image has not the same index range as the reconstructed image\n");
+
+  for (int z=parabolic_surrogate_curvature_cast.get_min_z();z<= parabolic_surrogate_curvature_cast.get_max_z();z++)
+    {
+	int min_dz, max_dz;
+	if (only_2D)
+	  {
+	    min_dz = max_dz = 0;
+	  }
+	else
+	  {
+	    min_dz = max(-1, parabolic_surrogate_curvature_cast.get_min_z()-z);
+	    max_dz = min(1, parabolic_surrogate_curvature_cast.get_max_z()-z);
+	  }
+	
+	for (int y=parabolic_surrogate_curvature_cast.get_min_y();y<= parabolic_surrogate_curvature_cast.get_max_y();y++)
+	  {
+	    int min_dy, max_dy;
+	    {
+	      min_dy = max(-1, parabolic_surrogate_curvature_cast.get_min_y()-y);
+	      max_dy = min(1, parabolic_surrogate_curvature_cast.get_max_y()-y);
+	    }
+
+	    for (int x=parabolic_surrogate_curvature_cast.get_min_x();x<= parabolic_surrogate_curvature_cast.get_max_x();x++)       
+	      {
+		int min_dx, max_dx;
+		{
+		  min_dx = max(-1, parabolic_surrogate_curvature_cast.get_min_x()-x);
+		  max_dx = min(1, parabolic_surrogate_curvature_cast.get_max_x()-x);
+		}
+		elemT gradient = 0;
+		for (int dz=min_dz;dz<=max_dz;++dz)
+		  for (int dy=min_dy;dy<=max_dy;++dy)
+		    for (int dx=min_dx;dx<=max_dx;++dx)
+		      {
+			// 1 comes form omega = psi'(t)/t = 2*t/2t =1		       
+			elemT current =
+			  weights[dz][dy][dx] *1;
+
+			 if (do_kappa)
+			  current *= 
+			    (*kappa_ptr)[z][y][x] * (*kappa_ptr)[z+dz][y+dy][x+dx];
+
+			gradient += current;
+		      }
+		
+		parabolic_surrogate_curvature[z][y][x]= gradient * penalisation_factor;
+	      }              
+	  }
+    }
+
+  std::cerr << " parabolic_surrogate_curvature max " << parabolic_surrogate_curvature.find_max()
+    << ", min " << parabolic_surrogate_curvature.find_min() << std::endl;
+  /*{
+    static int count = 0;
+    ++count;
+    char filename[20];
+    sprintf(filename, "normalised_gradient%d.v",count);
+    write_basic_interfile(filename, parabolic_surrogate_curvature);
+  }*/
+}
 
 #  ifdef _MSC_VER
 // prevent warning message on reinstantiation, 
@@ -190,13 +279,6 @@ compute_gradient(DiscretisedDensity<3,elemT>& prior_gradient,
 #  pragma warning(disable:4660)
 #  endif
 
-
-#if 0
-// registration stuff moved to recon_buildblock_registries.cxx
-
-// Register this class in the ImageProcessor registry
-
-#endif
 
 template QuadraticPrior<float>;
 
