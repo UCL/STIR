d446b145b7b6056e13e4804ce04ec294002ae302
Sanida Mustafovic <Sanida.Mustafovic@stir>
2002-01-21 13:22:15 +0000

    first version - penalisation factor now parrsed from here rather that in each derived class

---
+++ src/recon_buildblock/GeneralisedPrior.cxx
@@ -0,0 +1,47 @@
+//
+// $Id$
+//
+/*!
+  \file
+  \ingroup recon_buildblock
+  \brief  implementation of the GeneralisedPrior
+    
+  \author Kris Thielemans
+  \author Sanida Mustafovic      
+  $Date$  
+  $Revision$
+*/
+/*
+    Copyright (C) 2002- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
+#include "stir/recon_buildblock/GeneralisedPrior.h"
+
+START_NAMESPACE_STIR
+
+
+template <typename elemT>
+void 
+GeneralisedPrior<elemT>::initialise_keymap()
+{
+  parser.add_key("penalisation factor", &penalisation_factor); 
+}
+
+
+template <typename elemT>
+void
+GeneralisedPrior<elemT>::set_defaults()
+{
+  penalisation_factor = 0;  
+}
+
+#  ifdef _MSC_VER
+// prevent warning message on instantiation of abstract class 
+#  pragma warning(disable:4661)
+#  endif
+
+template GeneralisedPrior<float>;
+
+END_NAMESPACE_STIR
+
