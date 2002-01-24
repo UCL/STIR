b30611f4e9cc87c1f94aa0cff0e4c7e87d3cfa31
Kris Thielemans <Kris.Thielemans@stir>
2002-01-24 15:55:19 +0000

    - take into account new location of executables (in VC subdir)

--- src/test/run_tests.bat
+++ src/test/run_tests.bat
@@ -1,5 +1,5 @@
-@rem $Id$: $Date$
-@rem Batch programme to run PARAPET tests
+@rem $Id$
+@rem Batch programme to run STIR tests
 @rem Kris Thielemans
 
 @if #%1==# goto usage
@@ -8,25 +8,25 @@
 @echo All tests should end with 'All tests ok !'
 @echo There we go...
 @pause
-test_VectorWithOffset\%1\test_VectorWithOffset
+VC\test_VectorWithOffset\%1\test_VectorWithOffset
 @pause Press any key to continue to next test programme
-test_Array\%1\test_Array
+VC\test_Array\%1\test_Array
 @pause 
-test_convert_array\%1\test_convert_array
+VC\test_convert_array\%1\test_convert_array
 @pause 
-test_IndexRange\%1\test_IndexRange
+VC\test_IndexRange\%1\test_IndexRange
 @pause 
-test_filename_functions\%1\test_filename_functions
+VC\test_filename_functions\%1\test_filename_functions
 @pause 
-test_linear_regression\%1\test_linear_regression  input\test_linear_regression.in
+VC\test_linear_regression\%1\test_linear_regression  input\test_linear_regression.in
 @pause 
 test_coordinates\%1\test_coordinates
 @pause 
 test_filename_functions\%1\test_filename_functions 
 @pause 
-test_VoxelsOnCartesianGrid\%1\test_VoxelsOnCartesianGrid
+VC\test_VoxelsOnCartesianGrid\%1\test_VoxelsOnCartesianGrid
 @pause 
-test_proj_data_info\%1\test_proj_data_info
+VC\test_proj_data_info\%1\test_proj_data_info
 @echo.
 @echo End of tests !
 @echo.
