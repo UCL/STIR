f58ba55a78763ce3cb29b5f6bf177cf5c08094df
Sanida Mustafovic <Sanida.Mustafovic@stir>
2002-03-07 10:06:55 +0000

    added SeparableGaussianImageFilter

--- src/local/buildblock/local_buildblock_registries.cxx
+++ src/local/buildblock/local_buildblock_registries.cxx
@@ -1,7 +1,13 @@
+/*
+    Copyright (C) 2000- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
 #include "local/stir/DAVImageFilter3D.h"
 #include "local/stir/ModifiedInverseAverigingImageFilter.h"
 //#include "local/stir/InverseSeparableCartesianMetzImageFilter.h"
 #include "local/stir/SeparableLowPassImageFilter.h"
+#include "local/stir/SeparableGaussianImageFilter.h"
 
 
 START_NAMESPACE_STIR
@@ -10,9 +16,7 @@ static DAVImageFilter3D<float>::RegisterIt dummy1;
 
 //static InverseSeparableCartesianMetzImageFilter <float>::RegisterIt dummy3;
 static SeparableLowPassImageFilter<float>::RegisterIt dummy4;
+static SeparableGaussianImageFilter<float>::RegisterIt dummy5;
 
 END_NAMESPACE_STIR
-/*
-    Copyright (C) 2000- $Date$, IRSL
-    See STIR/LICENSE.txt for details
-*/
+
