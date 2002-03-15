70e8173ba0222209697cf4b3e11a02885ae07849
Kris Thielemans <Kris.Thielemans@stir>
2002-03-15 11:38:54 +0000

    - added attenuation - more things on command line - fewer questions

--- src/local/utilities/precompute_denominator_SPS.cxx
+++ src/local/utilities/precompute_denominator_SPS.cxx
@@ -1,4 +1,32 @@
 
+/*
+  \file
+  \ingroup utilities 
+  \brief precomputes denominator for SPS
+
+  \author Sanida Mustafovic
+
+  $Date$ 
+  $Revision$ 
+  
+    This program precomputes denominator for the SPS 
+    (separable paraboloidal surrogates)  in ET.
+    the denominator is given by :  
+    dj = sum Aij gamma h''(yi) where
+    h(l) = yi log (l) - l; h''(yi) = -1/yi; 
+    gamma = sum Aik;
+    
+      =>  dj = sum Aij(-i/yi) sum Aik
+      
+    if there is penalty added to it there is another term in the 
+    denominator that includes penalty term
+*/
+
+/*
+    Copyright (C) 2002- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
 #include "stir/recon_buildblock/BackProjectorByBinUsingProjMatrixByBin.h"
 #include "stir/recon_buildblock/ProjMatrixByBin.h"
 #include "stir/interfile.h"
@@ -31,34 +59,6 @@ using std::endl;
 
 
 
-/*
-  \file
-  \ingroup local\utilities 
-  \brief precomputes denominator for SPS
-
-  \author Sanida Mustafovic
-
-  $Date: 
-  $Revision: 
-  
-    This program precomputes denominator for the SPS 
-    (separable paraboloidal surrogates)  in ET.
-    the denominator is given by :  
-    dj = sum Aij gamma h''(yi) where
-    h(l) = yi log (l) - l; h''(yi) = -1/yi; 
-    gamma = sum Aik;
-    
-      =>  dj = sum Aij(-i/yi) sum Aik
-      
-    if there is penalty added to it there is another term in the 
-    denominator that includes penalty term
-*/
-
-/*
-    Copyright (C) 2002- $Date$, IRSL
-    See STIR/LICENSE.txt for details
-*/
-
 START_NAMESPACE_STIR
 
 void
@@ -69,87 +69,58 @@ do_segments(DiscretisedDensity<3,float>& image,
 	    BackProjectorByBin& back_projector,
 	    bool fill_with_1);
 
-void 
-find_inverse( ProjData* proj_data_ptr_out, const ProjData* proj_data_ptr_in);
 
 
 void 
-find_inverse( ProjData* proj_data_ptr_out, const ProjData* proj_data_ptr_in)
+find_inverse( ProjData& proj_data_out, const ProjData& proj_data_in,
+	      const int min_segment_num, const int max_segment_num)
 {  
-  const ProjDataInfo * proj_data_info_ptr =
-    proj_data_ptr_in->get_proj_data_info_ptr();
-  
-  const ProjDataInfo * proj_data_info_ptr_out =
-    proj_data_ptr_out->get_proj_data_info_ptr();
-  
-  const ProjDataFromStream*  projdatafromstream_in = 
-    dynamic_cast< const ProjDataFromStream*>(proj_data_ptr_in);
-  
-  ProjDataFromStream*  projdatafromstream_out = 
-    dynamic_cast<ProjDataFromStream*>(proj_data_ptr_out);
-  float inv;
-  float bin;
-
-   
-  float max_in_viewgram =0.F;
-
-  int min_segment_num = ask_num("Minimum segment number to invert",
-      proj_data_info_ptr->get_min_segment_num(), proj_data_info_ptr->get_max_segment_num(), 0);
-  int max_segment_num = ask_num("Maximum segment number to invert",
-      min_segment_num,proj_data_info_ptr->get_max_segment_num(), 
-      min_segment_num);
+     
+  float max_in_projdata =0.F;
   
  
   for (int segment_num = min_segment_num; segment_num<= max_segment_num;
-  segment_num++) 
+       segment_num++) 
   {
     SegmentByView<float> segment_by_view = 
-      projdatafromstream_in->get_segment_by_view(segment_num);
-    const float current_max_in_viewgram = segment_by_view.find_max();
-    if ( current_max_in_viewgram >= max_in_viewgram)
-     max_in_viewgram = current_max_in_viewgram ;
-      else
+      proj_data_in.get_segment_by_view(segment_num);
+    const float current_max_in_projdata = segment_by_view.find_max();
+    if ( current_max_in_projdata >= max_in_projdata)
+      max_in_projdata = current_max_in_projdata ;
+    else
       continue;
   }
   
   for (int segment_num = min_segment_num; segment_num<= max_segment_num;
-  segment_num++) 
-    for ( int view_num = proj_data_info_ptr->get_min_view_num();
-    view_num<=proj_data_info_ptr->get_max_view_num(); view_num++)
+       segment_num++) 
+    for ( int view_num = proj_data_in.get_min_view_num();
+	  view_num<=proj_data_in.get_max_view_num(); view_num++)
     {
-      Viewgram<float> viewgram_in = projdatafromstream_in->get_viewgram(view_num,segment_num);
-      Viewgram<float> viewgram_out = proj_data_info_ptr_out->get_empty_viewgram(view_num,segment_num);
+      Viewgram<float> viewgram_in = proj_data_in.get_viewgram(view_num,segment_num);
+      Viewgram<float> viewgram_out = proj_data_in.get_empty_viewgram(view_num,segment_num);
 
       // the following const was found in the ls_cyl.hs and the same value will
       // be used for thresholding both kappa_0 and kappa_1.
-      // threshold  = 10^-4/max_in_sinogram
-      // TODO - find out batter way of finding the threshold  
-  //    const float max_in_viewgram = 54.0F;
-
-	//segment_by_view.find_max();
    
-     const float threshold = 0.000001F*max_in_viewgram;    
-     cerr << threshold << endl;
+     const float threshold = 0.000001F*max_in_projdata;    
+     //cerr << threshold << endl;
       
       for (int i= viewgram_in.get_min_axial_pos_num(); i<=viewgram_in.get_max_axial_pos_num();i++)
 	for (int j= viewgram_in.get_min_tangential_pos_num(); j<=viewgram_in.get_max_tangential_pos_num();j++)
 	{
-	  bin= viewgram_in[i][j];
+	  const float bin= viewgram_in[i][j];
 	 	 	 
 	  if (bin >= threshold)
 	  {
-	    inv = 1.F/bin;
-	    viewgram_out[i][j] = inv;
-	    viewgram_out[i][j] *= 1;
+	    viewgram_out[i][j] = 1/bin;
 	  }
 	 else
 	  {	  
-	   //inv =1/threshold;
-	   viewgram_out[i][j] = 0;
+	    viewgram_out[i][j] = 1/threshold; 
 	  }
 
     	}
-	projdatafromstream_out->set_viewgram(viewgram_out);
+	proj_data_out.set_viewgram(viewgram_out);
     }
     
 }
@@ -217,106 +188,108 @@ int
 main(int argc, char *argv[])
 {
 
-  if(argc!=3)
+  if(argc<4 || argc>6)
   {
-    cerr<< "Usage: " << argv[0] << " input sinogram_1  sinogram_2 \n";
+    cerr<< "Usage: " << argv[0] << "output_image_filename input_proj_data  fwd_allones_projdata [normalisation_projdata [template_image]]\n";
     exit(EXIT_FAILURE);
   }
 
-  shared_ptr<ProjData> proj_data_ptr;
-  proj_data_ptr = ProjData::read_from_file(argv[1]);
+  const string output_filename = argv[1];
+  shared_ptr<ProjData> proj_data_ptr = ProjData::read_from_file(argv[2]);
+  shared_ptr<ProjData> fwd_ones = ProjData::read_from_file(argv[3]); 
+  shared_ptr<ProjData> norm_proj_data_ptr = 
+    argc>=5 ? ProjData::read_from_file(argv[4]) : 0; 
 
-  shared_ptr<ProjData> fwd_ones;
-  fwd_ones = ProjData::read_from_file(argv[2]); 
-
-  ProjDataFromStream * fwd_ones_cast = 
-    dynamic_cast< ProjDataFromStream* >(fwd_ones.get());
-  
   const ProjDataInfo * proj_data_info_ptr = 
     proj_data_ptr->get_proj_data_info_ptr();
-
-  shared_ptr< ProjDataInfo >  data_info = proj_data_info_ptr->clone();
-
-  VoxelsOnCartesianGrid<float> * vox_image_ptr =
-    new VoxelsOnCartesianGrid<float>(*proj_data_info_ptr); 
-
-  // find the inverse
- 
- const string output_file_name = "inverse.s";
- shared_ptr<iostream> sino_stream = new fstream (output_file_name.c_str(), ios::trunc|ios::out|ios::in|ios::binary);
- 
-  shared_ptr<ProjDataFromStream >  proj_data_inv_ptr =
-   new  ProjDataFromStream (data_info,sino_stream);
-
-  find_inverse(proj_data_inv_ptr.get(),proj_data_ptr.get());
-
-  //write_basic_interfile_PDFS_header(output_file_name, *proj_data_inv_ptr);
-  int min_segmnet_num = 0;
-  int max_segmnet_num = 0;
-  for (int segment_num = min_segmnet_num; segment_num<= max_segmnet_num;segment_num++) 
+  shared_ptr<DiscretisedDensity<3,float> > image_sptr;
+  if (argc >=6)
     {
-     SegmentByView<float> segmnet_0 = proj_data_inv_ptr->get_segment_by_view(segment_num);
-     SegmentByView<float> segmnet_1 = fwd_ones_cast->get_segment_by_view(segment_num);
-
-     segmnet_0 *=segmnet_1;
-     if (!(proj_data_inv_ptr->set_segment(segmnet_0) == Succeeded::yes))
-	warning("Error set_segment %d\n", segment_num);   
+      image_sptr = DiscretisedDensity<3,float>::read_from_file(argv[5]);
+      image_sptr->fill(0);
     }
+  else
+    image_sptr =
+      new VoxelsOnCartesianGrid<float>(*proj_data_info_ptr); 
 
-  shared_ptr<DiscretisedDensity<3,float> > image_sptr = vox_image_ptr;
-
+  
+  const int max_segment_num = 
+    ask_num("Maximum absolute segment number to backproject",
+	    0,proj_data_ptr->get_max_segment_num(), 
+	    proj_data_ptr->get_max_segment_num());
   bool projector_type = 
-    ask ( " Which projector do you want to use Ray Tracing (Y) or Solid Angle (N) ", true) ;
+    ask ( " Which back projector do you want to use Ray Tracing (Y) or Solid Angle (N) ", true) ;
   string name ;
   if ( projector_type )
    name = "Ray Tracing";
   else
     name = "Solid Angle";  
- 
  shared_ptr<ProjMatrixByBin> PM = 
     ProjMatrixByBin :: read_registered_object(0, name);
+
+  
+  // find the inverse
+  shared_ptr<ProjDataFromStream >  proj_data_inv_ptr;
+  {
+    shared_ptr< ProjDataInfo >  data_info = proj_data_info_ptr->clone();
+    data_info->reduce_segment_range(-max_segment_num, max_segment_num);
+    
+    const string output_file_name = "inverse.s";
+    shared_ptr<iostream> sino_stream = new fstream (output_file_name.c_str(), ios::trunc|ios::out|ios::in|ios::binary);
+    
+    proj_data_inv_ptr =
+      new  ProjDataFromStream (data_info,sino_stream);
+    
+    find_inverse(*proj_data_inv_ptr,*proj_data_ptr,
+		 -max_segment_num, max_segment_num);
+    
+    write_basic_interfile_PDFS_header(output_file_name, *proj_data_inv_ptr);
+    for (int segment_num = -max_segment_num; segment_num<= max_segment_num;segment_num++) 
+      {
+	SegmentByView<float> segmnet_0 = proj_data_inv_ptr->get_segment_by_view(segment_num);
+	SegmentByView<float> segmnet_1 = fwd_ones->get_segment_by_view(segment_num);
+	
+	segmnet_0 *=segmnet_1;
+	
+	if (norm_proj_data_ptr.use_count()>0)
+	  {
+	    segmnet_1 = norm_proj_data_ptr->get_segment_by_view(segment_num);
+	    segmnet_0 /=segmnet_1;
+	    segmnet_0 /=segmnet_1;
+	  }
+	if (!(proj_data_inv_ptr->set_segment(segmnet_0) == Succeeded::yes))
+	  warning("Error set_segment %d\n", segment_num);   
+      }
+
+  } // end of  inverse
+
+ 
  
   PM->set_up(proj_data_ptr->get_proj_data_info_ptr()->clone(),image_sptr);
   shared_ptr<BackProjectorByBin>  bck_projector_ptr  =
     new BackProjectorByBinUsingProjMatrixByBin(PM);   
      
-    const int max_segment_num = 
-      ask_num("Maximum absolute segment number to backproject",
-        0,proj_data_info_ptr->get_max_segment_num(), 
-        proj_data_info_ptr->get_max_segment_num());
-        
-    image_sptr->fill(0);
-    
-    CPUTimer timer;
-    timer.reset();
-    timer.start();
- 
-    do_segments(*image_sptr, 
+            
+  CPUTimer timer;
+  timer.reset();
+  timer.start();
+  
+  do_segments(*image_sptr, 
       *proj_data_inv_ptr, 
       -max_segment_num, max_segment_num,
       proj_data_info_ptr->get_min_view_num(), proj_data_info_ptr->get_max_view_num(),
       *bck_projector_ptr,
       false);  
   
-   {
-    DiscretisedDensity<3,float>::full_iterator image_iter = image_sptr->begin_all();   
-     
-    //for (;
-       //image_iter != image_sptr->end_all();++image_iter)
-     // if (*image_iter > 500)
-       // *image_iter = 500;
-    }
-    timer.stop();
-    cerr << timer.value() << " s CPU time"<<endl;
-    cerr << "min and max in image " << image_sptr->find_min()
-      << ", " << image_sptr->find_max() << endl;
-    
-      string file= "precomputed_denominator_wod";
+  timer.stop();
+  cerr << timer.value() << " s CPU time"<<endl;
+  cerr << "min and max in image " << image_sptr->find_min()
+       << ", " << image_sptr->find_max() << endl;
+  
 
-      cerr <<"  - Saving " << file << endl;
-      write_basic_interfile(file, *image_sptr);
-
-
-    return EXIT_SUCCESS;
+  cerr <<"  - Saving " << output_filename << endl;
+  write_basic_interfile(output_filename, *image_sptr);
+  
+  return EXIT_SUCCESS;
 }
 
