fb6072632c369a3c54794de37dfa6d57c8cbba36
Kris Thielemans <Kris.Thielemans@stir>
2002-04-22 13:31:18 +0000

    - use new DetPairData class for restricted fan

--- src/local/buildblock/ML_norm.cxx
+++ src/local/buildblock/ML_norm.cxx
@@ -19,9 +19,112 @@
 */
 
 #include "local/stir/ML_norm.h"
+#include "stir/display.h"
 
 START_NAMESPACE_STIR
 
+DetPairData::DetPairData()
+{}
+
+DetPairData::DetPairData(const IndexRange<2>& range)
+:base_type(range), num_detectors(range.get_length())
+{
+}
+
+DetPairData& 
+DetPairData::operator=(const DetPairData& other)
+{
+  base_type::operator=(other);
+  num_detectors = other.num_detectors;
+  return *this;
+}
+
+float & DetPairData::operator()(const int a, const int b)
+{
+  return (*this)[a][b<get_min_index(a) ? b+num_detectors: b];
+}
+
+float DetPairData::operator()(const int a, const int b) const
+{
+  return (*this)[a][b<get_min_index(a) ? b+num_detectors: b];
+}
+
+bool 
+DetPairData::
+is_in_data(const int a, const int b) const
+{
+  if (b>=get_min_index(a))
+    return b<=get_max_index(a);
+  else
+    return b+num_detectors<=get_max_index(a);
+}
+
+void DetPairData::fill(const float d)
+{
+  base_type::fill(d);
+}
+
+void DetPairData::grow(const IndexRange<2>& range)
+{
+  base_type::grow(range);
+  num_detectors=range.get_length();
+}
+
+int DetPairData::get_min_index() const
+{
+  return base_type::get_min_index();
+}
+
+int DetPairData::get_max_index() const
+{
+  return base_type::get_max_index();
+}
+
+int DetPairData::get_min_index(const int a) const
+{
+  return (*this)[a].get_min_index();
+}
+
+int DetPairData::get_max_index(const int a) const
+{
+  return (*this)[a].get_max_index();
+}
+
+float DetPairData::sum() const
+{
+  return base_type::sum();
+}
+
+float DetPairData::sum(const int a) const
+{
+  return (*this)[a].sum();
+}
+
+float DetPairData::find_max() const
+{
+  return base_type::find_max();
+}
+
+float DetPairData::find_min() const
+{
+  return base_type::find_min();
+}
+
+int DetPairData::get_num_detectors() const
+{
+  return num_detectors;
+}
+
+void display(const DetPairData& det_pair_data, const char * const title)
+{
+  const int num_detectors = det_pair_data.get_num_detectors();
+  Array<2,float> full_data(IndexRange2D(num_detectors,num_detectors));
+  for (int a = det_pair_data.get_min_index(); a <= det_pair_data.get_max_index(); ++a)
+    for (int b = det_pair_data.get_min_index(a); b <= det_pair_data.get_max_index(a); ++b)      
+       full_data[a%num_detectors][b%num_detectors] =
+         det_pair_data(a,b);
+  display(full_data,title);
+}
 
 void make_det_pair_data(DetPairData& det_pair_data,
 			const ProjData& proj_data,
@@ -35,7 +138,21 @@ void make_det_pair_data(DetPairData& det_pair_data,
 
   const int num_detectors = 
     proj_data_info.get_scanner_ptr()->get_num_detectors_per_ring();
-  det_pair_data.grow(IndexRange2D(0,num_detectors-1, 0,num_detectors-1));
+  const int fan_size = 
+    2*max(proj_data_info.get_max_tangential_pos_num(),
+          -proj_data_info.get_min_tangential_pos_num()) + 1;
+  // fan will range from -half_fan_size to +half_fan_size (i.e. an odd number of elements)
+  const int half_fan_size = fan_size/2;
+
+  IndexRange<2> fan_indices;
+  fan_indices.grow(0,num_detectors-1);
+  for (int a = 0; a < num_detectors; ++a)
+  {
+    fan_indices[a] = 
+      IndexRange<1>(a+num_detectors/2-half_fan_size,
+                    a+num_detectors/2+half_fan_size);
+  }
+  det_pair_data.grow(fan_indices);
   det_pair_data.fill(0);
 
   shared_ptr<Sinogram<float> > pos_sino_ptr =
@@ -58,9 +175,9 @@ void make_det_pair_data(DetPairData& det_pair_data,
 
 	proj_data_info.get_det_num_pair_for_view_tangential_pos_num(det_num_a, det_num_b, view_num, tang_pos_num);
 
-	det_pair_data[det_num_a][det_num_b] =
+	det_pair_data(det_num_a,det_num_b) =
 	  (*pos_sino_ptr)[view_num][tang_pos_num];
-	det_pair_data[det_num_b][det_num_a] =
+	det_pair_data(det_num_b,det_num_a) =
 	  (*neg_sino_ptr)[view_num][tang_pos_num];
       }
 }
@@ -75,9 +192,8 @@ void set_det_pair_data(ProjData& proj_data,
   const ProjDataInfoCylindricalNoArcCorr& proj_data_info =
     dynamic_cast<const ProjDataInfoCylindricalNoArcCorr&>(*proj_data_info_ptr);
 
-  const int num_detectors = 
-    proj_data_info.get_scanner_ptr()->get_num_detectors_per_ring();
-  assert(det_pair_data.get_length() == num_detectors);
+  const int num_detectors = det_pair_data.get_num_detectors();
+  assert(proj_data_info.get_scanner_ptr()->get_num_detectors_per_ring() == num_detectors);
 
   shared_ptr<Sinogram<float> > pos_sino_ptr =
     new Sinogram<float>(proj_data.get_empty_sinogram(ax_pos_num,segment_num));
@@ -98,10 +214,10 @@ void set_det_pair_data(ProjData& proj_data,
 	proj_data_info.get_det_num_pair_for_view_tangential_pos_num(det_num_a, det_num_b, view_num, tang_pos_num);
 
 	(*pos_sino_ptr)[view_num][tang_pos_num] =
-          det_pair_data[det_num_a][det_num_b];
+          det_pair_data(det_num_a,det_num_b);
         if (segment_num!=0)
 	  (*neg_sino_ptr)[view_num][tang_pos_num] =
-            det_pair_data[det_num_b][det_num_a];
+            det_pair_data(det_num_b,det_num_a);
       }
   proj_data.set_sinogram(*pos_sino_ptr);
   if (segment_num != 0)
@@ -111,35 +227,35 @@ void set_det_pair_data(ProjData& proj_data,
 
 void apply_block_norm(DetPairData& det_pair_data, const BlockData& block_data, const bool apply)
 {
-  const int num_detectors = det_pair_data.get_length();
+  const int num_detectors = det_pair_data.get_num_detectors();
   const int num_blocks = block_data.get_length();
   const int num_crystals_per_block = num_detectors/num_blocks;
   assert(num_blocks * num_crystals_per_block == num_detectors);
   
-  for (int a = 0; a < num_detectors; ++a)
-    for (int b = 0; b < num_detectors; ++b)      
+  for (int a = det_pair_data.get_min_index(); a <= det_pair_data.get_max_index(); ++a)
+    for (int b = det_pair_data.get_min_index(a); b <= det_pair_data.get_max_index(a); ++b)      
       {
 	// note: add 2*num_detectors to newb to avoid using mod with negative numbers
-	if (det_pair_data[a][b] == 0)
+	if (det_pair_data(a,b) == 0)
 	  continue;
         if (apply)
-          det_pair_data[a][b] *=
-	    block_data[a/num_crystals_per_block][b/num_crystals_per_block];
+          det_pair_data(a,b) *=
+	    block_data[a/num_crystals_per_block][(b/num_crystals_per_block)%num_blocks];
         else
-          det_pair_data[a][b] /=
-	    block_data[a/num_crystals_per_block][b/num_crystals_per_block];
+          det_pair_data(a,b) /=
+	    block_data[a/num_crystals_per_block][(b/num_crystals_per_block)%num_blocks];
       }
 }
 
 void apply_geo_norm(DetPairData& det_pair_data, const GeoData& geo_data, const bool apply)
 {
-  const int num_detectors = det_pair_data.get_length();
+  const int num_detectors = det_pair_data.get_num_detectors();
   const int num_crystals_per_block = geo_data.get_length()*2;
 
-  for (int a = 0; a < num_detectors; ++a)
-    for (int b = 0; b < num_detectors; ++b)      
+  for (int a = det_pair_data.get_min_index(); a <= det_pair_data.get_max_index(); ++a)
+    for (int b = det_pair_data.get_min_index(a); b <= det_pair_data.get_max_index(a); ++b)      
       {
-	if (det_pair_data[a][b] == 0)
+	if (det_pair_data(a,b) == 0)
 	  continue;
         int newa = a % num_crystals_per_block;
 	int newb = b - (a - newa); 
@@ -150,28 +266,28 @@ void apply_geo_norm(DetPairData& det_pair_data, const GeoData& geo_data, const b
 	  }
 	// note: add 2*num_detectors to newb to avoid using mod with negative numbers
         if (apply)
-          det_pair_data[a][b] *=
+          det_pair_data(a,b) *=
 	    geo_data[newa][(2*num_detectors + newb)%num_detectors];
         else
-          det_pair_data[a][b] /=
+          det_pair_data(a,b) /=
 	    geo_data[newa][(2*num_detectors + newb)%num_detectors];
       }
 }
 
 void apply_efficiencies(DetPairData& det_pair_data, const Array<1,float>& efficiencies, const bool apply)
 {
-  const int num_detectors = det_pair_data.get_length();
-  for (int det_num_a = 0; det_num_a < num_detectors; ++det_num_a)
-    for (int det_num_b = 0; det_num_b < num_detectors; ++det_num_b)      
+  const int num_detectors = det_pair_data.get_num_detectors();
+  for (int a = det_pair_data.get_min_index(); a <= det_pair_data.get_max_index(); ++a)
+    for (int b = det_pair_data.get_min_index(a); b <= det_pair_data.get_max_index(a); ++b)      
       {
-	if (det_pair_data[det_num_a][det_num_b] == 0)
+	if (det_pair_data(a,b) == 0)
 	  continue;
         if (apply)
-	  det_pair_data[det_num_a][det_num_b] *=
-	    efficiencies[det_num_a]*efficiencies[det_num_b];
+	  det_pair_data(a,b) *=
+	    efficiencies[a]*efficiencies[b%num_detectors];
         else
-          det_pair_data[det_num_a][det_num_b] /=
-	    efficiencies[det_num_a]*efficiencies[det_num_b];
+          det_pair_data(a,b) /=
+	    efficiencies[a]*efficiencies[b%num_detectors];
       }
 }
 
