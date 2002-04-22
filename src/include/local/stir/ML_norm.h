e02ef3f576d7b2fa13e3b95543900dacad2adef1
Kris Thielemans <Kris.Thielemans@stir>
2002-04-22 11:47:11 +0000

    - new class DetPairData to store only actual fan used

--- src/include/local/stir/ML_norm.h
+++ src/include/local/stir/ML_norm.h
@@ -26,12 +26,48 @@
 #include "stir/ProjDataInfoCylindricalNoArcCorr.h"
 #include "stir/IndexRange2D.h"
 #include "stir/Sinogram.h"
+#include <string>
+#ifndef STIR_NO_NAMESPACES
+using std::string;
+#endif
 
 START_NAMESPACE_STIR
 
-typedef Array<2,float> DetPairData;
 typedef Array<2,float> GeoData;
 typedef Array<2,float> BlockData;
+
+class DetPairData : private Array<2,float>
+{
+public:
+
+  DetPairData();
+  DetPairData(const IndexRange<2>& range);
+  DetPairData& operator=(const DetPairData&);
+
+  float& operator()(const int a, const int b);
+  
+  float operator()(const int a, const int b) const;
+
+  void fill(const float d);
+
+  int get_min_index() const;
+  int get_max_index() const;
+  int get_min_index(const int a) const;
+  int get_max_index(const int a) const;
+  bool is_in_data(const int a, const int b) const;
+  float sum() const;
+  float sum(const int a) const;
+  float find_max() const;
+  float find_min() const;
+  int get_num_detectors() const;
+  void grow(const IndexRange<2>&);
+
+private:
+  typedef Array<2,float> base_type;
+  int num_detectors;
+};
+
+void display(const DetPairData&,const char * const);
 
 void make_det_pair_data(DetPairData& det_pair_data,
 			const ProjData& proj_data,
