678414039fad713b264dd1565adb014304bd8487
Kris Thielemans <Kris.Thielemans@stir>
2002-04-22 11:39:46 +0000

    - moved functions to ML_norm.cxx - added block stuff - different thresholds to get KL to work - some clean-up

--- src/local/utilities/find_ML_normfactors.cxx
+++ src/local/utilities/find_ML_normfactors.cxx
@@ -14,21 +14,17 @@
   $Revision$
 */
 /*
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2001- $Date$, IRSL
     See STIR/LICENSE.txt for details
 */
-#define ARRAY_CONST_IT  
 
-#include "stir/ProjDataInfoCylindricalNoArcCorr.h"
-#include "stir/ProjData.h"
+#include "local/stir/ML_norm.h"
+
 #include "stir/Scanner.h"
-#include "stir/Bin.h"
 #include "stir/stream.h"
-#include "stir/Array.h"
-#include "stir/Sinogram.h"
-#include "stir/IndexRange2D.h"
 #include "stir/display.h"
 #include "stir/CPUTimer.h"
+#include "stir/utilities.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -42,49 +38,12 @@ using std::string;
 
 START_NAMESPACE_STIR
 
-typedef Array<2,float> DetPairData;
-typedef Array<2,float> GeoData;
 
-void make_det_pair_data(DetPairData& det_pair_data,
-			const ProjData& proj_data,
-			const int segment_num,
-			const int ax_pos_num)
+void make_fan_sum_data(Array<1,float>& data_fan_sums, const DetPairData& det_pair_data)
 {
-  const ProjDataInfo* proj_data_info_ptr =
-    proj_data.get_proj_data_info_ptr();
-  const ProjDataInfoCylindricalNoArcCorr& proj_data_info =
-    dynamic_cast<const ProjDataInfoCylindricalNoArcCorr&>(*proj_data_info_ptr);
-
-  const int num_detectors = 
-    proj_data_info.get_scanner_ptr()->get_num_detectors_per_ring();
-  det_pair_data.grow(IndexRange2D(0,num_detectors-1, 0,num_detectors-1));
-  det_pair_data.fill(0);
-
-  shared_ptr<Sinogram<float> > pos_sino_ptr =
-    new Sinogram<float>(proj_data.get_sinogram(ax_pos_num,segment_num));
-  shared_ptr<Sinogram<float> > neg_sino_ptr;
-  if (segment_num == 0)
-    neg_sino_ptr = pos_sino_ptr;
-  else
-    neg_sino_ptr =
-      new Sinogram<float>(proj_data.get_sinogram(ax_pos_num,-segment_num));
-  
-    
-  for (int view_num = 0; view_num < num_detectors/2; view_num++)
-    for (int tang_pos_num = proj_data.get_min_tangential_pos_num();
-	 tang_pos_num <= proj_data.get_max_tangential_pos_num();
-	 ++tang_pos_num)
-      {
-	int det_num_a = 0;
-	int det_num_b = 0;
-
-	proj_data_info.get_det_num_pair_for_view_tangential_pos_num(det_num_a, det_num_b, view_num, tang_pos_num);
-
-	det_pair_data[det_num_a][det_num_b] =
-	  (*pos_sino_ptr)[view_num][tang_pos_num];
-	det_pair_data[det_num_b][det_num_a] =
-	  (*neg_sino_ptr)[view_num][tang_pos_num];
-      }
+  const int num_detectors = det_pair_data.get_length();
+  for (int det_num_a = 0; det_num_a < num_detectors; ++det_num_a)
+    data_fan_sums[det_num_a] = det_pair_data[det_num_a].sum();
 }
 
 void make_geo_data(GeoData& geo_data, const DetPairData& det_pair_data)
@@ -120,47 +79,28 @@ void make_geo_data(GeoData& geo_data, const DetPairData& det_pair_data)
 	  }
       }
   geo_data /= 2*num_blocks;
+  // TODO why normalisation?
 }
-
-void apply_geo_norm(DetPairData& det_pair_data, const GeoData& geo_data)
+ 
+void make_block_data(BlockData& block_data, const DetPairData& det_pair_data)
 {
   const int num_detectors = det_pair_data.get_length();
-  const int num_crystals_per_block = geo_data.get_length()*2;
+  const int num_blocks = block_data.get_length();
+  const int num_crystals_per_block = num_detectors/num_blocks;
+  assert(num_blocks * num_crystals_per_block == num_detectors);
 
-  for (int a = 0; a < num_detectors; ++a)
-    for (int b = 0; b < num_detectors; ++b)      
-      {
-        int newa = a % num_crystals_per_block;
-	int newb = b - (a - newa); 
-	if (newa > num_crystals_per_block - 1 - newa)
-	  { 
-	    newa = num_crystals_per_block - 1 - newa; 
-	    newb = - newb + num_crystals_per_block - 1;
-	  }
-	// note: add 2*num_detectors to newb to avoid using mod with negative numbers
-	det_pair_data[a][b] *=
-	  geo_data[newa][(2*num_detectors + newb)%num_detectors];
-      }
-}
-
-void make_fan_sum_data(Array<1,float>& data_fan_sums, const DetPairData& det_pair_data)
-{
-  const int num_detectors = det_pair_data.get_length();
-  for (int det_num_a = 0; det_num_a < num_detectors; ++det_num_a)
-    data_fan_sums[det_num_a] = det_pair_data[det_num_a].sum();
-}
-
-void apply_efficiencies(DetPairData& det_pair_data, const Array<1,float>& efficiencies)
-{
-  const int num_detectors = det_pair_data.get_length();
+  block_data.fill(0);
   for (int det_num_a = 0; det_num_a < num_detectors; ++det_num_a)
     for (int det_num_b = 0; det_num_b < num_detectors; ++det_num_b)      
       {
-	det_pair_data[det_num_a][det_num_b] *=
-	  efficiencies[det_num_a]*efficiencies[det_num_b];
+	block_data[det_num_a/num_crystals_per_block][det_num_b/num_crystals_per_block] += 
+	  det_pair_data[det_num_a][det_num_b];
       }
-}
   
+  block_data /= square(num_crystals_per_block);
+  // TODO why normalisation?
+}
+ 
 void iterate_efficiencies(Array<1,float>& efficiencies,
 			  const Array<1,float>& data_fan_sums,
 			  const DetPairData& model)
@@ -173,6 +113,7 @@ void iterate_efficiencies(Array<1,float>& efficiencies,
 	efficiencies[det_num_a] = 0;
       else
 	{
+          //const float denominator = inner_product(efficiencies,model[det_num_a]);
 	  float denominator = 0;
 	  for (int det_num_b = 0; det_num_b < num_detectors; ++det_num_b)      
 	    denominator += efficiencies[det_num_b]*model[det_num_a][det_num_b];
@@ -189,60 +130,120 @@ void iterate_geo_norm(GeoData& norm_geo_data,
   //norm_geo_data = measured_geo_data / norm_geo_data;
   const int num_detectors = model.get_length();
   const int num_crystals_per_block = measured_geo_data.get_length()*2;
-  const float threshold = measured_geo_data.find_max()/100000.F;
+  const float threshold = measured_geo_data.find_max()/10000.F;
   for (int a = 0; a < num_crystals_per_block/2; ++a)
     for (int b = 0; b < num_detectors; ++b)      
       {
 	norm_geo_data[a][b] =
-	  (measured_geo_data[a][b]>threshold)
+	  (measured_geo_data[a][b]>=threshold ||
+	   measured_geo_data[a][b] < 10000*norm_geo_data[a][b])
 	  ? measured_geo_data[a][b] / norm_geo_data[a][b]
+	  : 0;
+      }
+}
+  
+void iterate_block_norm(BlockData& norm_block_data,
+		      const BlockData& measured_block_data,
+		      const DetPairData& model)
+{
+  make_block_data(norm_block_data, model);
+  //norm_block_data = measured_block_data / norm_block_data;
+  const int num_blocks = norm_block_data.get_length();
+  const float threshold = measured_block_data.find_max()/10000.F;
+  for (int a = 0; a < num_blocks; ++a)
+    for (int b = 0; b < num_blocks; ++b)      
+      {
+	norm_block_data[a][b] =
+	  (measured_block_data[a][b]>=threshold ||
+	   measured_block_data[a][b] < 10000*norm_block_data[a][b])
+	  ? measured_block_data[a][b] / norm_block_data[a][b]
 	  : 0;
       }
 }
   
 #if 0
 void check_geo_data()
-  {
-    GeoData measured_geo_data(IndexRange2D(num_crystals_per_block/2, num_detectors));
-    GeoData norm_geo_data(IndexRange2D(num_crystals_per_block/2, num_detectors));
-
-    DetPairData det_pair_data(IndexRange2D(num_detectors, num_detectors));
-    det_pair_data.fill(1);
-    for (int a = 0; a < num_crystals_per_block/2; ++a)
-      for (int b = 0; b < num_detectors; ++b)      
+{
+  const int num_detectors = 392;
+  const int num_crystals_per_block = 8;
+  GeoData measured_geo_data(IndexRange2D(num_crystals_per_block/2, num_detectors));
+  GeoData norm_geo_data(IndexRange2D(num_crystals_per_block/2, num_detectors));
+  
+  DetPairData det_pair_data(IndexRange2D(num_detectors, num_detectors));
+  det_pair_data.fill(1);
+  for (int a = 0; a < num_crystals_per_block/2; ++a)
+    for (int b = 0; b < num_detectors; ++b)      
       {
 	norm_geo_data[a][b] =(a+1)*cos((b-num_detectors/2)*_PI/num_detectors);
       }
-    apply_geo_norm(det_pair_data, norm_geo_data);
-    //display(det_pair_data,  "1*geo");
-    make_geo_data(measured_geo_data, det_pair_data);
-    {
-      GeoData diff = measured_geo_data-norm_geo_data;
-      cerr << "(org geo) min max: " << norm_geo_data.find_min() << ',' << norm_geo_data.find_max() << endl;
-      cerr << "(-org geo + make geo) min max: " << diff.find_min() << ',' << diff.find_max() << endl;
-    }
+  apply_geo_norm(det_pair_data, norm_geo_data);
+  //display(det_pair_data,  "1*geo");
+  make_geo_data(measured_geo_data, det_pair_data);
+  {
+    GeoData diff = measured_geo_data-norm_geo_data;
+    cerr << "(org geo) min max: " << norm_geo_data.find_min() << ',' << norm_geo_data.find_max() << endl;
+    cerr << "(-org geo + make geo) min max: " << diff.find_min() << ',' << diff.find_max() << endl;
   }
+
+  {
+    DetPairData model_det_pair_data = det_pair_data;
+    for (int a = 0; a < num_detectors; ++a)
+      for (int b = 0; b < num_detectors; ++b)      
+	model_det_pair_data[a][b]=square((a+b-2*num_detectors)/2.F);
+    {
+      det_pair_data = model_det_pair_data;
+      apply_geo_norm(det_pair_data, norm_geo_data);
+      apply_geo_norm(det_pair_data, norm_geo_data,false);
+      DetPairData diff = det_pair_data - model_det_pair_data;
+      cerr << "(org model) min max: " << model_det_pair_data.find_min() << ',' << model_det_pair_data.find_max() << endl;
+      cerr << "(-org model + apply/undo model) min max: " << diff.find_min() << ',' << diff.find_max() << endl;
+      //display(diff,  "model- apply/undo model");
+      //display(det_pair_data,  "apply/undo model");
+      //display(model_det_pair_data,  " model");
+
+    }   
+    det_pair_data = model_det_pair_data;   
+    apply_geo_norm(det_pair_data, norm_geo_data);
+    //display(det_pair_data,  "model*geo");
+    make_geo_data(measured_geo_data, det_pair_data);
+    GeoData new_norm_geo_data = norm_geo_data;
+    new_norm_geo_data.fill(0);
+    iterate_geo_norm(new_norm_geo_data,
+		     measured_geo_data,
+		     model_det_pair_data);
+    GeoData diff = new_norm_geo_data-norm_geo_data;
+    cerr << "(org geo) min max: " << norm_geo_data.find_min() << ',' << norm_geo_data.find_max() << endl;
+    cerr << "(-org geo + iterate geo) min max: " << diff.find_min() << ',' << diff.find_max() << endl;
+  }
+
+}
 
 #endif
   
-inline float KL(const float a, const float b)
+inline float KL(const float a, const float b, const float threshold_a = 0)
 {
   assert(a>=0);
   assert(b>=0);
-  float res = a==0 ? b : (a*(log(a)-log(b)) + b - a);
-  assert(res>=0);
+  float res = a<=threshold_a ? b : (a*(log(a)-log(b)) + b - a);
+#ifndef NDEBUG
+  if (res != res)
+    warning("KL nan at a=%g b=%g, threshold %g\n",a,b,threshold_a);
+  if (res > 1.E20)
+    warning("KL large at a=%g b=%g, threshold %g\n",a,b,threshold_a);
+#endif
+  assert(res>=-1.e-4);
   return res;
 }
 
 template <int num_dimensions, typename elemT>
-float KL(const Array<num_dimensions, elemT>& a, const Array<num_dimensions, elemT>& b)
+float KL(const Array<num_dimensions, elemT>& a, const Array<num_dimensions, elemT>& b, const float threshold_a = 0)
 {
   float sum = 0;
   Array<num_dimensions, elemT>::const_full_iterator iter_a = a.begin_all();
   Array<num_dimensions, elemT>::const_full_iterator iter_b = b.begin_all();
   while (iter_a != a.end_all())
     {
-      sum += KL(*iter_a++, *iter_b++);
+      sum += KL(*iter_a++, *iter_b++, threshold_a);
     }
   return sum;
 }
@@ -252,14 +253,17 @@ END_NAMESPACE_STIR
 USING_NAMESPACE_STIR
 
 int main(int argc, char **argv)
-{
+{  
+  //check_geo_data();
   if (argc!=6)
     {
       cerr << "Usage: " << argv[0] 
-	   << " efficiency_out_filename_prefix measured_data model num_iterations num_eff_iterations\n";
+	   << " out_filename_prefix measured_data model num_iterations num_eff_iterations\n"
+	   << " set num_iterations to 0 to do only efficiencies\n";
       return EXIT_FAILURE;
     }
-
+  const bool do_display = ask("Display",false);
+  const bool do_KL = ask("Compute KL distances?",false);
   const int num_eff_iterations = atoi(argv[5]);
   const int num_iterations = atoi(argv[4]);
   shared_ptr<ProjData> model_data = ProjData::read_from_file(argv[3]);
@@ -268,6 +272,7 @@ int main(int argc, char **argv)
   const int num_detectors = 
     measured_data->get_proj_data_info_ptr()->get_scanner_ptr()->get_num_detectors_per_ring();
   const int num_crystals_per_block = 8;
+  const int num_blocks = num_detectors/num_crystals_per_block;
 
   CPUTimer timer;
   timer.start();
@@ -280,32 +285,28 @@ int main(int argc, char **argv)
   assert(num_crystals_per_block%2 == 0);
   GeoData measured_geo_data(IndexRange2D(num_crystals_per_block/2, num_detectors));
   GeoData norm_geo_data(IndexRange2D(num_crystals_per_block/2, num_detectors));
-
+  BlockData measured_block_data(IndexRange2D(num_blocks, num_blocks));
+  BlockData norm_block_data(IndexRange2D(num_blocks, num_blocks));
 
   for (int ax_pos_num = measured_data->get_min_axial_pos_num(segment_num);
        ax_pos_num <= measured_data->get_max_axial_pos_num(segment_num);
        ++ax_pos_num)
     {
-      // next could be local of KL is not computed below
+      // next could be local if KL is not computed below
       DetPairData measured_det_pair_data;
+      float threshold_for_KL;
       // compute factors dependent on the data
       {
 	make_det_pair_data(measured_det_pair_data, *measured_data, segment_num, ax_pos_num);
-#if 1
-	// insert known geo factors
-	for (int a = 0; a < num_crystals_per_block/2; ++a)
-	  for (int b = 0; b < num_detectors; ++b)      
-	    {
-	      norm_geo_data[a][b] =(a+1)*cos((b-num_detectors/2)*_PI/num_detectors)+.1;
-	    }
-	apply_geo_norm(measured_det_pair_data, norm_geo_data);
-#endif
+	threshold_for_KL = measured_det_pair_data.find_max()/100000.F;
 	cerr << "ax_pos " << ax_pos_num << endl;
 	//display(measured_det_pair_data, "measured data");
 	
 	make_fan_sum_data(data_fan_sums, measured_det_pair_data);
 	make_geo_data(measured_geo_data, measured_det_pair_data);
-	
+	make_block_data(measured_block_data, measured_det_pair_data);
+	if (do_display)
+	  display(measured_block_data, "raw block data from measurements");	
 	/*{
 	  char *out_filename = new char[20];
 	  sprintf(out_filename, "%s_%d.out", 
@@ -320,19 +321,22 @@ int main(int argc, char **argv)
       make_det_pair_data(model_det_pair_data, *model_data, segment_num, ax_pos_num);
       //display(model_det_pair_data, "model");
 
-      for (int iter_num = 0; iter_num<num_iterations; ++iter_num)
+      for (int iter_num = 1; iter_num<=max(num_iterations, 1); ++iter_num)
 	{
-	  if (iter_num== 0)
+	  if (iter_num== 1)
 	    {
-	      efficiencies.fill(data_fan_sums.sum()/model_det_pair_data.sum());
+	      efficiencies.fill(sqrt(data_fan_sums.sum()/model_det_pair_data.sum()));
 	      norm_geo_data.fill(1);
+	      norm_block_data.fill(1);
 	    }
 	  // efficiencies
 	  {
 	    det_pair_data = model_det_pair_data;
 	    apply_geo_norm(det_pair_data, norm_geo_data);
-	    //display(det_pair_data,  "model*geo");
-	    for (int eff_iter_num = 0; eff_iter_num<num_eff_iterations; ++eff_iter_num)
+            apply_block_norm(det_pair_data, norm_block_data);
+	    if (do_display)
+	      display(det_pair_data,  "model*geo*block");
+	    for (int eff_iter_num = 1; eff_iter_num<=num_eff_iterations; ++eff_iter_num)
 	      {
 		iterate_efficiencies(efficiencies, data_fan_sums, det_pair_data);
 		{
@@ -343,22 +347,41 @@ int main(int argc, char **argv)
 		  out << efficiencies;
 		  delete out_filename;
 		}
-		{
-		  DetPairData model_times_norm = det_pair_data;
-		  apply_efficiencies(model_times_norm, efficiencies);
-		  //display( model_times_norm, "model_times_norm");
-		  //cerr << "model_times_norm min max: " << model_times_norm.find_min() << ',' << model_times_norm.find_max() << endl;
+		if (do_KL)
+		  {
+		    DetPairData model_times_norm = det_pair_data;
+		    apply_efficiencies(model_times_norm, efficiencies);
+		    if (do_display)
+		      display( model_times_norm, "model_times_norm");
+		    //cerr << "model_times_norm min max: " << model_times_norm.find_min() << ',' << model_times_norm.find_max() << endl;
 
-		  cerr << "KL " << KL(measured_det_pair_data, model_times_norm) << endl;
-		}
+		    cerr << "KL " << KL(measured_det_pair_data, model_times_norm, threshold_for_KL) << endl;		  
+		  }
+		if (do_display)		 
+		  {
+		    DetPairData norm = det_pair_data;
+		    norm.fill(1);
+		    apply_efficiencies(norm, efficiencies);
+		    display(norm, "eff norm");
+		  }
 		  
 	    }
 	  }
+	  if (num_iterations==0)
+	    break;
 	  // geo norm
 	  {
 	    det_pair_data = model_det_pair_data;
 	    apply_efficiencies(det_pair_data, efficiencies);
-	    iterate_geo_norm(norm_geo_data, measured_geo_data, det_pair_data);
+	    apply_block_norm(det_pair_data, norm_block_data);
+            iterate_geo_norm(norm_geo_data, measured_geo_data, det_pair_data);
+	    { // check 
+	      for (int a=0; a<measured_geo_data.get_length(); ++a)
+		  for (int b=0; b<num_detectors; ++b)
+		    if (norm_geo_data[a][b]==0 && measured_geo_data[a][b]!=0)
+		      warning("norm geo 0 at a=%d b=%d measured value=%g\n",
+			      a,b,measured_geo_data[a][b]);
+	    }
 	    {
 	      char *out_filename = new char[out_filename_prefix.size() + 30];
 	      sprintf(out_filename, "%s_%s_%d_%d.out", 
@@ -367,11 +390,58 @@ int main(int argc, char **argv)
 	      out << norm_geo_data;
 	      delete out_filename;
 	    }
-	    {
-	      apply_geo_norm(det_pair_data, norm_geo_data);
-	      cerr << "KL " << KL(measured_det_pair_data, det_pair_data) << endl;
+	    if (do_KL)
+	      {
+		apply_geo_norm(det_pair_data, norm_geo_data);
+		for (int a=0; a<num_detectors; ++a)
+		  for (int b=0; b<num_detectors; ++b)
+		    if (det_pair_data[a][b]==0 && measured_det_pair_data[a][b]!=0)
+		      warning("geo 0 at a=%d b=%d measured value=%g\n",
+			      a,b,measured_det_pair_data[a][b]);
+		cerr << "KL " << KL(measured_det_pair_data, det_pair_data, threshold_for_KL) << endl;
+	      }
+	    if (do_display)		 
+	      {
+		DetPairData norm = det_pair_data;
+		norm.fill(1);
+		apply_geo_norm(norm, norm_geo_data);
+		display(norm, "geo norm");
+	      }
+	  }
+          // block norm
+	  {
+	    det_pair_data = model_det_pair_data;
+	    apply_efficiencies(det_pair_data, efficiencies);
+	    apply_geo_norm(det_pair_data, norm_geo_data);
+            iterate_block_norm(norm_block_data, measured_block_data, det_pair_data);
+	    { // check 
+	      for (int a=0; a<measured_block_data.get_length(); ++a)
+		  for (int b=0; b<measured_block_data[0].get_length(); ++b)
+		    if (norm_block_data[a][b]==0 && measured_block_data[a][b]!=0)
+		      warning("block norm 0 at a=%d b=%d measured value=%g\n",
+			      a,b,measured_block_data[a][b]);
 	    }
-
+	    {
+	      char *out_filename = new char[out_filename_prefix.size() + 30];
+	      sprintf(out_filename, "%s_%s_%d_%d.out", 
+		      out_filename_prefix.c_str(), "block", ax_pos_num, iter_num);
+	      ofstream out(out_filename);
+	      out << norm_block_data;
+	      delete out_filename;
+	    }
+	    if (do_KL)
+	      {
+		apply_block_norm(det_pair_data, norm_block_data);
+		cerr << "KL " << KL(measured_det_pair_data, det_pair_data, threshold_for_KL) << endl;
+	      }
+	    if (do_display)		 
+	      {
+		DetPairData norm = det_pair_data;
+		norm.fill(1);
+		apply_block_norm(norm, norm_block_data);
+		display(norm_block_data, "raw block norm");
+		display(norm, "block norm");
+	      }
 	  }
 	}
     }
