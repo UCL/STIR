7455a9af8acc74a45a96ba1c81d51facea7d55f8
Kris Thielemans <Kris.Thielemans@stir>
2002-05-22 13:12:50 +0000

    - moved everything from .inl here - start and stop timers

--- src/recon_buildblock/BackProjectorByBin.cxx
+++ src/recon_buildblock/BackProjectorByBin.cxx
@@ -11,7 +11,6 @@
   \author PARAPET project
   
   $Date$
-
   $Revision$
 */
 /*
@@ -20,59 +19,59 @@
     See STIR/LICENSE.txt for details
 */
 
+
 #include "stir/recon_buildblock/BackProjectorByBin.h"
 #include "stir/RelatedViewgrams.h"
-#include "stir/DiscretisedDensity.h"
-//#include "stir/Viewgram.h"
 
 START_NAMESPACE_STIR
-
-#if 0
 
 BackProjectorByBin::BackProjectorByBin()
 {
 }
 
+BackProjectorByBin::~BackProjectorByBin()
+{
+}
 
 void 
-BackProjectorByBin::back_project( RelatedViewgrams<float>& viewgrams, 
-		     const DiscretisedDensity<3,float>& image)
+BackProjectorByBin::back_project( DiscretisedDensity<3,float>& image,
+				  const RelatedViewgrams<float>& viewgrams)
 {
-  back_project(viewgrams,
-              image,
-              viewgrams.get_min_axial_pos_num(),
-	      viewgrams.get_max_axial_pos_num(),
-	      viewgrams.get_min_tangential_pos_num(),
-	      viewgrams.get_max_tangential_pos_num());
+  back_project(image,viewgrams,
+                  viewgrams.get_min_axial_pos_num(),
+		  viewgrams.get_max_axial_pos_num(),
+		  viewgrams.get_min_tangential_pos_num(),
+		  viewgrams.get_max_tangential_pos_num());
 }
 
 void BackProjectorByBin::back_project
- ( RelatedViewgrams<float>& viewgrams, 
-   const DiscretisedDensity<3,float>& image,
+  (DiscretisedDensity<3,float>& image,
+   const RelatedViewgrams<float>& viewgrams,    
    const int min_axial_pos_num, 
    const int max_axial_pos_num)
 {
-  back_project(viewgrams, image,
+  back_project(image,viewgrams,
              min_axial_pos_num,
 	     max_axial_pos_num,
 	     viewgrams.get_min_tangential_pos_num(),
 	     viewgrams.get_max_tangential_pos_num());
 }
 
-
-void BackProjectorByBin::back_project
-  (RelatedViewgrams<float>& viewgrams, 
-		     const DiscretisedDensity<3,float>& density,
-		     const int min_axial_pos_num, const int max_axial_pos_num,
-		     const int min_tangential_pos_num, const int max_tangential_pos_num)
+void 
+BackProjectorByBin::
+back_project(DiscretisedDensity<3,float>& density,
+	     const RelatedViewgrams<float>& viewgrams, 		    
+	     const int min_axial_pos_num, const int max_axial_pos_num,
+	     const int min_tangential_pos_num, const int max_tangential_pos_num)
 {
-  actual_back_project(viewgrams, density,
+  start_timers();
+  actual_back_project(density,viewgrams,
              min_axial_pos_num,
 	     max_axial_pos_num,
 	     min_tangential_pos_num,
 	     max_tangential_pos_num);
+  stop_timers();
 }
 
-#endif
 
 END_NAMESPACE_STIR
