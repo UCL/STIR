3917ab32352552efe08248de74f717bb7984f23f
Kris Thielemans <Kris.Thielemans@stir>
2002-05-22 09:43:16 +0000

    - added  store_only_basic_bins_in_cache facility - derive from TimedObject (but doesn't start timers yet) - derive from ParsingObject and add keywords for cache configuration. The fact it is   derived from ParsingObject now means that all derived classes need to change though.

--- src/include/stir/recon_buildblock/ProjMatrixByBin.h
+++ src/include/stir/recon_buildblock/ProjMatrixByBin.h
@@ -24,12 +24,12 @@
     See STIR/LICENSE.txt for details
 */
 
-
-
 #include "stir/RegisteredObject.h"
+#include "stir/ParsingObject.h"
 #include "stir/recon_buildblock/ProjMatrixElemsForOneBin.h"
 #include "stir/recon_buildblock/DataSymmetriesForBins.h"
 #include "stir/shared_ptr.h"
+#include "stir/TimedObject.h"
 
 #include <map>
 
@@ -51,7 +51,6 @@ class ProjMatrixElemsForOneViewgram;
 class SubsetInfo;
 */
 	    
-//template <typename elemT> class RelatedViewgrams;	    
 class Bin;	    
 	    
 /*!
@@ -66,8 +65,22 @@ class Bin;
   Currently, the class provides for some (basic) caching.
   This functionality will probably be moved to a new class 
   ProjMatrixByBinWithCache. (TODO)
+
+  \par Parsing parameters
+
+  The following parameters can be set (default values are indicated):
+  \verbatim
+  disable caching := false
+  store only basic bins in cache := true
+  \endverbatim
+  The 2nd option allows to cache the whole matrix. This results in the fastest
+  behaviour IF your system does not start swapping. The default choice caches 
+  only the 'basic' bins, and computes symmetry related bins from the 'basic' ones.
 */
-class ProjMatrixByBin :  public RegisteredObject<ProjMatrixByBin>
+class ProjMatrixByBin :  
+  public RegisteredObject<ProjMatrixByBin>,  
+  public ParsingObject,
+  public TimedObject
 {
 public:
   
@@ -92,7 +105,7 @@ public:
   (i.e. data is NOT appended).
   
   The implementation is inline as it just gets it in
-  terms of the cached__proj_matrix_elems_for_one_bin or 
+  terms of the cached_proj_matrix_elems_for_one_bin or 
   calculate_proj_matrix_elems_for_one_bin.*/
   inline void 
     get_proj_matrix_elems_for_one_bin(
@@ -116,18 +129,24 @@ public:
   */
   
   //void set_maximum_cache_size(const unsigned long size){;}        
-  void enable_cache(bool v){cache_disabled = v;}
   /* TODO
   void set_subset_usage(const SubsetInfo&, const int num_access_times);
   */
-  
-  
+  void enable_cache(const bool v = true);
+  void store_only_basic_bins_in_cache(const bool v = true) ;
+
+  bool is_cache_enabled() const;
+  bool does_cache_store_only_basic_bins() const;
+
   
 protected:
   shared_ptr<DataSymmetriesForBins> symmetries_ptr;
   
-
-  //! default ctor (enables caching)
+  //! default ctor (calls set_defaults())
+  /*! Note that due to the C++ definition (and some good reasons), 
+      ProjMatrixByBin::set_defaults() is called,
+      even though this is a virtual function.
+   */
   ProjMatrixByBin();  
   
   /*! \brief This method needs to be implemented in the derived class.
@@ -140,10 +159,23 @@ protected:
     calculate_proj_matrix_elems_for_one_bin(
     ProjMatrixElemsForOneBin& 
     ) const = 0;
+
+  /////////////////////////////// parsing stuff //////////////////////
+  
+  //! sets value for caching configuration (enables caching, but for 'basic' bins only)
+  /*! Has to be called by set_defaults in the leaf-class */
+  virtual void set_defaults();
+  //! sets keys for caching configuration
+  /*! Has to be called by initialise_keymap in the leaf-class */
+  virtual void initialise_keymap();
+  //! Checks if parameters have sensible values
+  /*! Has to be called by post_processing in the leaf-class */
+  virtual bool post_processing();
   
   /////////////////////////////// caching stuff //////////////////////
 
   bool cache_disabled;  
+  bool cache_stores_only_basic_bins;
 
   /*! \brief The method that tries to get data from the cache.
   
@@ -178,7 +210,8 @@ private:
     MapProjMatrixElemsForOneBin cache_collection;
          
   //! create the key for caching
-  inline static CacheKey cache_key(const Bin& bin);
+  // KT 15/05/2002 not static anymore as it uses cache_stores_only_basic_bins
+  inline CacheKey cache_key(const Bin& bin) const;
 
    
 };
