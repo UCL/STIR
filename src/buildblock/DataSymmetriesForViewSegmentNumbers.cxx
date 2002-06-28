9e463aac40af8b48c16c13cccf9f0463dc792c85
Kris Thielemans <Kris.Thielemans@stir>
2002-06-28 15:00:03 +0000

    - moved here from .inl - added is_basic

---
+++ src/buildblock/DataSymmetriesForViewSegmentNumbers.cxx
@@ -0,0 +1,46 @@
+//
+// $Id$
+//
+/*!
+  \file
+  \ingroup buildblock 
+  \brief Implementations for class DataSymmetriesForViewSegmentNumbers
+
+  \author Kris Thielemans
+  \author PARAPET project
+
+  $Date$
+  $Revision$
+*/
+/*
+    Copyright (C) 2000 PARAPET partners
+    Copyright (C) 2000- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
+#include "stir/DataSymmetriesForViewSegmentNumbers.h"
+#include "stir/ViewSegmentNumbers.h"
+
+START_NAMESPACE_STIR
+
+DataSymmetriesForViewSegmentNumbers::
+~DataSymmetriesForViewSegmentNumbers()
+{}
+
+int
+DataSymmetriesForViewSegmentNumbers::num_related_view_segment_numbers(const ViewSegmentNumbers& vs) const
+{
+  vector<ViewSegmentNumbers> rel_vs;
+  get_related_view_segment_numbers(rel_vs, vs);
+  return rel_vs.size();
+}
+
+bool
+DataSymmetriesForViewSegmentNumbers::
+is_basic(const ViewSegmentNumbers& v_s) const
+{
+  ViewSegmentNumbers copy = v_s;
+  return !find_basic_view_segment_numbers(copy);
+}
+
+END_NAMESPACE_STIR
