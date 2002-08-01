099405dd67cd77fe1b1501a2797477635796558d
Kris Thielemans <Kris.Thielemans@stir>
2002-08-01 14:44:56 +0000

    - first version in archive

---
+++ src/test/test_stir_math.cxx
@@ -0,0 +1,304 @@
+//
+// $Id$
+//
+/*!
+
+  \file
+  \ingroup test
+  
+  \brief Test program for the stir_math  utility
+    
+  Takes as single command line argument the filename of the stir_math executable 
+  (with a relative or absolute path). If no argument is given, the executable im
+  the path is used.
+  \warning will overwite (and then delete) files called STIR$$*.
+  \author Kris Thielemans
+      
+  $Date$        
+  $Revision$
+*/
+/*
+    Copyright (C) 2002- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
+#include "stir/VoxelsOnCartesianGrid.h"
+#include "stir/ProjDataInfo.h"
+#include "stir/Scanner.h"
+#include "stir/IndexRange.h"
+#include "stir/CartesianCoordinate3D.h"
+
+#include "stir/RunTests.h"
+#include "stir/interfile.h"
+#include "stir/ProjDataInterfile.h"
+#include "stir/SegmentByView.h"
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef STIR_NO_NAMESPACE
+using std::generate;
+#endif
+
+START_NAMESPACE_STIR
+
+/*!
+  \ingroup test
+  \brief Test class for the stir_math utility
+
+  \warning will overwite (and then delete) files called STIR$$*.
+*/
+class stir_mathTests : public RunTests
+{
+public:
+  stir_mathTests(const string& stir_math_executable)
+    : stir_math_executable(stir_math_executable)
+  {}
+  
+  void run_tests();
+private:
+  string stir_math_executable;
+  //! returns true if it ran it successfully
+  bool run_stir_math(const char * const arguments);
+
+};
+
+
+bool
+stir_mathTests::
+run_stir_math(const char * const arguments)
+{
+  static string cmdline;
+  static string error_string;
+  
+  cmdline = stir_math_executable;
+  cmdline += ' ' ;
+  cmdline += arguments;
+  error_string = "error executing command '";
+  error_string += cmdline;
+  error_string += "'";
+  cerr << "\nExecuting '" << cmdline << "'\n";
+  return check(system(cmdline.c_str())==EXIT_SUCCESS,
+	       "executing previous command returned non-zero status\n.");
+}
+
+void
+stir_mathTests::run_tests()
+
+{ 
+  cerr << "Tests for the stir_math utility\n";
+  cerr << "WARNING: will overwite (and then delete) files called STIRtmp*\n";
+  
+  // images
+  {
+    CartesianCoordinate3D<float> origin (0,0,0);  
+    CartesianCoordinate3D<float> grid_spacing (3,4,5); 
+    IndexRange<3> 
+      range(CartesianCoordinate3D<int>(0,-2,-3),
+            CartesianCoordinate3D<int>(1,2,3));
+    VoxelsOnCartesianGrid<float>  data1(range,origin, grid_spacing);
+    VoxelsOnCartesianGrid<float>  data2(range,origin, grid_spacing);
+    VoxelsOnCartesianGrid<float>  data3(range,origin, grid_spacing);
+    VoxelsOnCartesianGrid<float> calc_data;
+    shared_ptr<DiscretisedDensity<3,float> > out_data_ptr;
+    
+    // fill with some random numbers
+    srand(1);
+    generate(data1.begin_all(), data1.end_all(), rand);
+    generate(data2.begin_all(), data2.end_all(), rand);
+    generate(data3.begin_all(), data3.end_all(), rand);
+    
+    write_basic_interfile("STIRtmp1.v", data1);
+    write_basic_interfile("STIRtmp2.v", data2);
+    write_basic_interfile("STIRtmp3.v", data3);
+    
+    set_tolerance(data1.find_max()/10000);
+    
+    // add
+    if (run_stir_math("STIRtmpout.v STIRtmp1.hv STIRtmp2.hv STIRtmp3.hv"))
+    {
+      out_data_ptr = DiscretisedDensity<3,float>::read_from_file("STIRtmpout.hv");
+      calc_data = data1;
+      calc_data += data2 + data3;
+      
+      check_if_equal( calc_data, *out_data_ptr,"test on adding");    
+    }
+    // mult
+    if (run_stir_math("--mult STIRtmpout.v STIRtmp1.hv STIRtmp2.hv STIRtmp3.hv"))
+    {
+      out_data_ptr = DiscretisedDensity<3,float>::read_from_file("STIRtmpout.hv");
+      calc_data = data1;
+      calc_data *= data2 * data3;
+      
+      check_if_equal( calc_data, *out_data_ptr,"test on multiplying");    
+    }
+    // add with power etc
+    if (run_stir_math("--power 2 --times-scalar 3.2 STIRtmpout.v STIRtmp1.hv STIRtmp2.hv STIRtmp3.hv"))
+    {
+      out_data_ptr = DiscretisedDensity<3,float>::read_from_file("STIRtmpout.hv");
+      calc_data.fill(0);
+      calc_data += data1 + (data2*data2 + data3*data3)*3.2F;
+      
+      check_if_equal( calc_data, *out_data_ptr,"test with power and scalar multiplication");    
+    }
+    // add with power etc and including-first
+    if (run_stir_math("--power 2 --times-scalar 3.2 --including-first STIRtmpout.v STIRtmp1.hv STIRtmp2.hv STIRtmp3.hv"))
+    {
+      out_data_ptr = DiscretisedDensity<3,float>::read_from_file("STIRtmpout.hv");
+      calc_data.fill(0);
+      calc_data += (data1*data1 + data2*data2 + data3*data3)*3.2F;      
+      check_if_equal( calc_data, *out_data_ptr,"test with power and scalar multiplication with --including-first");    
+    }
+    // add with power etc and accumulate 
+    if (run_stir_math("--accumulate --power 2 --times-scalar 3.2  --mult STIRtmp1.hv STIRtmp3.hv"))
+    {
+      calc_data.fill(0);
+      calc_data += data1;
+      calc_data += (data3*data3)*3.2F;      
+      out_data_ptr = DiscretisedDensity<3,float>::read_from_file("STIRtmp1.hv");
+      check_if_equal( calc_data, *out_data_ptr,"test with power and scalar multiplication with --accumulate");    
+    }
+    // add with power etc and accumulate and including-first
+    if (run_stir_math("--accumulate --power 2 --times-scalar 3.2 --including-first STIRtmp2.hv STIRtmp3.hv"))
+    {
+      calc_data.fill(0);
+      calc_data += data2;
+      calc_data *= calc_data * 3.2F;
+      calc_data += (data3*data3)*3.2F;      
+      out_data_ptr = DiscretisedDensity<3,float>::read_from_file("STIRtmp2.hv");
+      check_if_equal( calc_data, *out_data_ptr,"test with power and scalar multiplication with --including-first and --accumulate");    
+    }  
+
+    remove("STIRtmp1.ahv");
+    remove("STIRtmp1.hv");
+    remove("STIRtmp1.v");
+    remove("STIRtmp2.ahv");
+    remove("STIRtmp2.hv");
+    remove("STIRtmp2.v");
+    remove("STIRtmp3.ahv");
+    remove("STIRtmp3.hv");
+    remove("STIRtmp3.v");
+    remove("STIRtmpout.ahv");
+    remove("STIRtmpout.hv");
+    remove("STIRtmpout.v");
+  }
+
+  // projdata
+  {
+    // to  keep testing code below as close as possible to the image case, we'll just 
+    // take a single segment in the data.
+
+    shared_ptr<Scanner> scanner_ptr = new Scanner(Scanner::E953);
+    shared_ptr<ProjDataInfo> proj_data_info_ptr = 
+      ProjDataInfo::ProjDataInfoCTI(scanner_ptr, 
+			            /*span=*/1, 
+                                    /*max_delta=*/0,
+                                    /*num_views=*/8,
+                                    /*num_tang_poss=*/16);
+    SegmentByView<float> data1 = proj_data_info_ptr->get_empty_segment_by_view(0);
+    SegmentByView<float> data2 = proj_data_info_ptr->get_empty_segment_by_view(0);
+    SegmentByView<float> data3 = proj_data_info_ptr->get_empty_segment_by_view(0);
+    SegmentByView<float> calc_data = proj_data_info_ptr->get_empty_segment_by_view(0);
+    shared_ptr<ProjData> out_data_ptr;
+
+    // fill with some random numbers
+    srand(1);
+    generate(data1.begin_all(), data1.end_all(), rand);
+    generate(data2.begin_all(), data2.end_all(), rand);
+    generate(data3.begin_all(), data3.end_all(), rand);
+    
+    // first create files and close them afterwards
+    // This is necessary because system() requires all streams to be flushed.
+    {
+                                      
+      ProjDataInterfile proj_data1(proj_data_info_ptr, "STIRtmp1");
+      ProjDataInterfile proj_data2(proj_data_info_ptr, "STIRtmp2");
+      ProjDataInterfile proj_data3(proj_data_info_ptr, "STIRtmp3");
+      
+      proj_data1.set_segment(data1);
+      proj_data2.set_segment(data2);
+      proj_data3.set_segment(data3);
+    
+      set_tolerance(data1.find_max()/10000);
+    }
+    
+    // add
+    if (run_stir_math("-s STIRtmpout.hs STIRtmp1.hs STIRtmp2.hs STIRtmp3.hs"))
+    {
+      out_data_ptr = ProjData::read_from_file("STIRtmpout.hs");
+      calc_data = data1;
+      calc_data += data2 + data3;
+      
+      check_if_equal( calc_data, out_data_ptr->get_segment_by_view(0),"test on adding");    
+    }
+    // mult
+    if (run_stir_math("-s --mult STIRtmpout.hs STIRtmp1.hs STIRtmp2.hs STIRtmp3.hs"))
+    {
+      out_data_ptr = ProjData::read_from_file("STIRtmpout.hs");
+      calc_data = data1;
+      calc_data *= data2 * data3;
+      
+      check_if_equal( calc_data, out_data_ptr->get_segment_by_view(0),"test on multiplying");    
+    }
+    // add with power etc
+    if (run_stir_math("-s --power 2 --times-scalar 3.2 STIRtmpout.hs STIRtmp1.hs STIRtmp2.hs STIRtmp3.hs"))
+    {
+      out_data_ptr = ProjData::read_from_file("STIRtmpout.hs");
+      calc_data.fill(0);
+      calc_data += data1 + (data2*data2 + data3*data3)*3.2F;
+      
+      check_if_equal( calc_data, out_data_ptr->get_segment_by_view(0),"test with power and scalar multiplication");    
+    }
+    // add with power etc and including-first
+    if (run_stir_math("-s --power 2 --times-scalar 3.2 --including-first STIRtmpout.hs STIRtmp1.hs STIRtmp2.hs STIRtmp3.hs"))
+    {
+      out_data_ptr = ProjData::read_from_file("STIRtmpout.hs");
+      calc_data.fill(0);
+      calc_data += (data1*data1 + data2*data2 + data3*data3)*3.2F;      
+      check_if_equal( calc_data, out_data_ptr->get_segment_by_view(0),"test with power and scalar multiplication with --including-first");    
+    }
+    // add with power etc and accumulate 
+    if (run_stir_math("-s --accumulate --power 2 --times-scalar 3.2  --mult STIRtmp1.hs STIRtmp3.hs"))
+    {
+      calc_data.fill(0);
+      calc_data += data1;
+      calc_data += (data3*data3)*3.2F;      
+      out_data_ptr = ProjData::read_from_file("STIRtmp1.hs");
+      check_if_equal( calc_data, out_data_ptr->get_segment_by_view(0),"test with power and scalar multiplication with --accumulate");    
+    }
+    // add with power etc and accumulate and including-first
+    if (run_stir_math("-s --accumulate --power 2 --times-scalar 3.2 --including-first STIRtmp2.hs STIRtmp3.hs"))
+    {
+      calc_data.fill(0);
+      calc_data += data2;
+      calc_data *= calc_data * 3.2F;
+      calc_data += (data3*data3)*3.2F;      
+      out_data_ptr = ProjData::read_from_file("STIRtmp2.hs");
+      check_if_equal( calc_data, out_data_ptr->get_segment_by_view(0),"test with power and scalar multiplication with --including-first and --accumulate");    
+    }
+
+    remove("STIRtmp1.hs");
+    remove("STIRtmp1.s");
+    remove("STIRtmp2.hs");
+    remove("STIRtmp2.s");
+    remove("STIRtmp3.hs");
+    remove("STIRtmp3.s");
+    remove("STIRtmpout.hs");
+    remove("STIRtmpout.s");
+  }
+
+}
+
+END_NAMESPACE_STIR
+
+
+USING_NAMESPACE_STIR
+
+
+int main(int argc, char** argv)
+{
+  stir_mathTests tests(argc==2?argv[1] : "stir_math");
+  tests.run_tests();
+  return tests.main_return_value();
+}
---
+++ src/utilities/stir_math.cxx
@@ -0,0 +1,331 @@
+//
+// $Id$
+//
+
+/*!
+\file
+\ingroup utilities
+\brief add or multiply data, with some other basic math manipulations 
+
+This is a command line utility for adding or multiplying data, with a
+somewhat awkward syntax. 
+The command line arguments are as follows (but everything has to fit on 1 line):
+\code
+  [-s] 
+  [--add | --mult] 
+  [--times-scalar mult_scalar_float] 
+  [--power power_float] 
+  [--including-first] 
+  [--verbose]
+  output_filename_with_extension in_data1 [in_data2 [in_data3...]]
+\endcode
+or
+\code
+  --accumulate
+  [-s] 
+  [--add | --mult] 
+  [--times-scalar mult_scalar_float] 
+  [--power power_float] 
+  [--including-first] 
+  [--verbose]
+  out_and_input_filename in_data2 [in_data3 [in_data4...]]
+\endcode
+
+'--add' is default, and outputs the sum of the result of processed data.
+'--mult' outputs the multiplication of the result of processed data.<br>
+The '--include-first' option can be used such that power and scalar 
+multiplication are done on the first input argument as well. 
+Otherwise these manipulations are done only on the 2nd, 3rd,.. argument.<br>
+The '--accumulate' option can be used to say that the first filename given will be
+used for input AND output. Note that when using this option together with 
+'--including-first', the data in the
+first filename will first be manipulated according to '--power' and '--times-scalar'.<br>
+The '-s' option is necessary if the arguments are projection data.
+Otherwise, it is assumed the data are images.<br>
+Power is taken before multiplication with the scalar.
+
+\par Examples
+<ul>
+<li> subtracting 2 files<br>
+\code stir_math --times-scalar -1 output in1 in2 \endcode
+<li> sum the square of each file<br>
+\code stir_math --power 2 --including-first output in1 in2 \endcode
+<li> Dividing 2 files<br>
+\code stir_math --mult --power -1 output in1 in2 \endcode
+<li> Dividing 2 files, with first file set to the quotient<br>
+\code stir_math --accumulate --mult --power -1 in1 in2 \endcode
+
+</ul>
+\warning There is no check that the data sizes and other info are compatible 
+	and the output will have the largest data size in the input, 
+	and the charachteristics (like voxel-size or so) are taken from the first input data. 
+	Hence, lots of funny effects can happen if data are not compatible.
+
+\warning When '--accumulate' is not used, the output file HAS to be different from all
+          the input files.
+\warning The result of using non-integral powers on  negative numbers is probably 
+         system-dependent.
+
+\author Kris Thielemans 
+
+$Date$
+$Revision$ 
+*/
+/*
+    Copyright (C) 2001- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
+#include "stir/ProjDataFromStream.h"
+#include "stir/DiscretisedDensity.h"
+#include "stir/SegmentByView.h"
+#include "stir/interfile.h"
+#include "stir/ProjDataInterfile.h"
+#include "stir/utilities.h"
+#include "stir/ArrayFunction.h"
+#include "stir/Succeeded.h"
+
+#include <fstream> 
+#include <iostream> 
+#include <functional>
+#include <algorithm>
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::unary_function;
+using std::transform;
+#endif
+
+#ifdef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+#define	in_place_apply_function(A, B) \
+        transform((A).begin_all(), (A).end_all(), \
+                  (A).begin_all(), pow_times_object)
+#endif
+
+
+USING_NAMESPACE_STIR
+
+// a function object that takes a power of a float, and then multiplies with a float
+class pow_times: public unary_function<float,float>
+{
+public:
+  pow_times(const float mult_scalar, const float power)
+    : mult(mult_scalar), power(power)
+  {}
+
+  float operator()(float const arg)
+  {
+    return mult*(power==1?arg : pow(arg,power));
+  }
+private:
+  float mult;
+  float power;
+};
+
+int 
+main(int argc, char **argv)
+{
+  if(argc<4)
+  {
+    cerr<< "Usage: " << argv[0] << " [-s] [--accumulate] [--add | --mult] [--times-scalar mult_scalar_float] [--power power_float] [--including-first] [--verbose] output_filename_with_extension in_data1 [in_data2 [in_data3...]]\n"
+	<< "'--add' is default, and outputs the sum of the result of processed data.\n"
+	<< "'--mult' outputs the multiplication of the result of processed data.\n"
+	<< "The '--include-first' option can be used such that power and scalar "
+	<< "multiplication are done on the first input argument as well. "
+	<< "Otherwise these manipulations are done only on the 2nd, 3rd,.. argument.\n"
+        << "The '--accumulate' option can be used to say that the first filename given will be "
+           "used for input AND output. Note that when using this option together with "
+           "'--including-first', the data in the first filename will first be manipulated "
+           "according to '--power' and '--times-scalar'.\n"
+        << "Power is taken before multiplication with the scalar.\n"
+	<< "The '-s' option is necessary if the arguments are projection data."
+	<< " Otherwise, it is assumed the data are images.\n\n"
+	<< "WARNING: there is no check that the data sizes and other info are compatible "
+	<< "and the output will have the largest data size in the input, "
+	<< "and the charachteristics (like voxel-size or so) are taken from the first input data. "
+	<< "Hence, lots of funny effects can happen if data are not compatible.\n";
+    exit(EXIT_FAILURE);
+  }
+  const char * const program_name = argv[0];
+  // skip program name
+  --argc;
+  ++argv;
+
+  bool do_add = true;
+  bool accumulate=false;
+  float mult_scalar = 1.F;
+  float power = 1;
+  bool except_first = true;
+  bool verbose = false;
+  bool do_projdata = false;
+
+
+  // first process command line options
+
+  while (argc>0 && argv[0][0]=='-')
+  {
+    if (strcmp(argv[0], "--times-scalar")==0)
+    {
+      if (argc<2)
+      { cerr << "Option '--times-scalar' expects a (float) argument\n"; exit(EXIT_FAILURE); }
+      mult_scalar = atof(argv[1]);
+      argc-=2; argv+=2;
+    } 
+    else if (strcmp(argv[0], "--power")==0)
+    {
+      if (argc<2)
+      { cerr << "Option '--power' expects an argument\n"; exit(EXIT_FAILURE); }
+      power = atof(argv[1]);
+      argc-=2; argv+=2;
+    } 
+    else  if (strcmp(argv[0], "--including-first")==0)
+    {
+      except_first = false;
+      argc-=1; argv+=1;
+    }
+    else  if (strcmp(argv[0], "--verbose")==0)
+    {
+      verbose = true;
+      argc-=1; argv+=1;
+    }
+    else  if (strcmp(argv[0], "-s")==0)
+    {
+      do_projdata = true;
+      argc-=1; argv+=1;
+    }
+    else  if (strcmp(argv[0], "--add")==0)
+    {
+      do_add = true;
+      argc-=1; argv+=1;
+    }
+    else  if (strcmp(argv[0], "--mult")==0)
+    {
+      do_add = false;
+      argc-=1; argv+=1;
+    }
+    else  if (strcmp(argv[0], "--accumulate")==0)
+    {
+      accumulate = true;
+      argc-=1; argv+=1;
+    }
+    else
+    { cerr << "Unknown option '" << argv[0] <<"'\n"; exit(EXIT_FAILURE); }
+  }
+
+  if (argc==0)
+  { cerr << "No output file (nor input files) on command line\n"; exit(EXIT_FAILURE); }
+
+  // find output filename
+  const string output_file_name = *argv;
+  if (!accumulate)
+  {
+    --argc; ++argv;
+  }
+
+  // some basic error checking and output
+  const int num_files = argc;
+
+  if (num_files==0)
+  { cerr << "No input files on command line\n"; exit(EXIT_FAILURE); }
+
+  const bool no_math_on_data = power==1 && mult_scalar==1;
+
+  if (verbose)
+    {
+      cout << program_name << ": "
+	   << (do_add ? "adding " : "multiplying ")
+	   << num_files;
+      if (!no_math_on_data)
+	cout <<" files after taking a power of "
+	     << power << " and then multiplying with "
+	     << mult_scalar
+	     << (except_first?" except for" : " including")
+	     <<" the first file";
+      cout << endl;
+    }
+
+  // construct function object that does the manipulations on each data
+  pow_times pow_times_object(mult_scalar, power);
+
+  // start the main processing
+  if (!do_projdata)
+    {
+ 
+      shared_ptr< DiscretisedDensity<3,float> >  image_ptr = 
+	DiscretisedDensity<3,float>::read_from_file(*argv);
+      if (!no_math_on_data && !except_first )
+	in_place_apply_function(*image_ptr, pow_times_object);
+
+      shared_ptr< DiscretisedDensity<3,float> >  current_image_ptr;
+
+      for (int i=1; i<num_files; ++i)
+	{
+	  if (verbose)
+	    cout << "Reading image " << argv[i] << endl;
+	  current_image_ptr = 
+	    DiscretisedDensity<3,float>::read_from_file(argv[i]);
+	  if (!no_math_on_data)
+	    in_place_apply_function(*current_image_ptr, pow_times_object);
+	  if (do_add)
+	    *image_ptr += *current_image_ptr;
+	  else
+	    *image_ptr *= *current_image_ptr;
+	}
+
+      if (verbose)
+	cout << "Writing output image " << output_file_name << endl;
+      write_basic_interfile(output_file_name, *image_ptr);
+    }
+  else // do_projdata
+    {
+      vector< shared_ptr<ProjData> > all_proj_data(num_files);
+      shared_ptr<ProjData> out_proj_data_ptr;
+      if (accumulate)
+      {
+        all_proj_data[0] = ProjData::read_from_file(argv[0], ios::in | ios::out);
+        out_proj_data_ptr = all_proj_data[0];
+      }
+      else
+      {
+        all_proj_data[0] = ProjData::read_from_file(argv[0]);
+
+        out_proj_data_ptr =
+          new ProjDataInterfile((*all_proj_data[0]).get_proj_data_info_ptr()->clone(), 
+                                output_file_name);        
+      }
+
+      // read rest of projection data headers
+      for (int i=1; i<num_files; ++i)
+	all_proj_data[i] =  ProjData::read_from_file(argv[i]); 
+
+      // do reading/writing in a loop over segments
+      for (int segment_num = (*all_proj_data[0]).get_min_segment_num();
+	   segment_num <= (*all_proj_data[0]).get_max_segment_num();
+	   ++segment_num)
+	{   
+	  if (verbose)
+	    cout << "Processing segment num " << segment_num << "for all files" << endl;
+	  SegmentByView<float> segment_by_view = 
+	    (*all_proj_data[0]).get_segment_by_view(segment_num);
+	  if (!no_math_on_data && !except_first )
+	    in_place_apply_function(segment_by_view, pow_times_object);
+	  for (int i=1; i<num_files; ++i)
+	    {
+	      SegmentByView<float> current_segment_by_view = 
+		(*all_proj_data[i]).get_segment_by_view(segment_num);
+	      if (!no_math_on_data)
+		in_place_apply_function(current_segment_by_view, pow_times_object);
+	      if(do_add)
+		segment_by_view += current_segment_by_view;
+	      else
+		segment_by_view *= current_segment_by_view;
+	    }
+    
+	  if (!(out_proj_data_ptr->set_segment(segment_by_view) == Succeeded::yes))
+	    warning("Error set_segment %d\n", segment_num);   
+	}
+    } 
+  return EXIT_SUCCESS;
+}
