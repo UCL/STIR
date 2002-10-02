34101aca6fdf2b7980bbf6fd316ce46ae59fb496
Sanida Mustafovic <Sanida.Mustafovic@stir>
2002-10-02 17:32:42 +0000

    - added ModifiedInverseAveragingImageFilterAll

--- src/local/buildblock/local_buildblock_registries.cxx
+++ src/local/buildblock/local_buildblock_registries.cxx
@@ -5,7 +5,7 @@
 
 #include "local/stir/DAVImageFilter3D.h"
 #include "local/stir/ModifiedInverseAverigingImageFilter.h"
-//#include "local/stir/InverseSeparableCartesianMetzImageFilter.h"
+#include "local/stir/ModifiedInverseAveragingImageFilterAll.h"
 #include "local/stir/SeparableLowPassImageFilter.h"
 #include "local/stir/SeparableGaussianImageFilter.h"
 //#include "local/stir/SeparableGaussianImageFilterWithSquareCoefficients.h"
@@ -18,7 +18,8 @@ static DAVImageFilter3D<float>::RegisterIt dummy1;
 //static InverseSeparableCartesianMetzImageFilter <float>::RegisterIt dummy3;
 static SeparableLowPassImageFilter<float>::RegisterIt dummy4;
 static SeparableGaussianImageFilter<float>::RegisterIt dummy5;
-//static SeparableGaussianImageFilterWithSquareCoefficients<float>::RegisterIt dummy6;
+
+static ModifiedInverseAveragingImageFilterAll<float>::RegisterIt dummy6;
 
 END_NAMESPACE_STIR
 
