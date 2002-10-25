9322ba2ea39f85a3986eb9859c2768936d60586a
Kris Thielemans <Kris.Thielemans@stir>
2002-10-25 10:20:52 +0000

    - added ECAT7 - put ECAT6 in its own namespace

--- src/buildblock/DiscretisedDensity.cxx
+++ src/buildblock/DiscretisedDensity.cxx
@@ -22,7 +22,11 @@
 */
 #include "stir/DiscretisedDensity.h"
 #include "stir/interfile.h"
+#include "stir/IO/ecat6_utils.h"
+#include "stir/IO/stir_ecat6.h"
+#include "stir/IO/stir_ecat7.h"
 #include "stir/VoxelsOnCartesianGrid.h"
+#include "stir/is_null_ptr.h"
 #include <typeinfo>
 
 
@@ -52,7 +56,61 @@ DiscretisedDensity<num_dimensions,elemT>::
   if (num_dimensions != 3 || typeid(elemT) != typeid(float))
     error("DiscretisedDensity::read_from_file currently only supports 3d float images\n");
 
-  return read_interfile_image(filename);
+#ifndef NDEBUG
+  warning("DiscretisedDensity::read_from_file trying to read %s as Interfile\n", filename.c_str());
+#endif
+  DiscretisedDensity<num_dimensions,elemT> * density_ptr =
+    read_interfile_image(filename);
+  if (!is_null_ptr(density_ptr))
+    return density_ptr;
+
+
+  {
+#ifndef NDEBUG
+    warning("DiscretisedDensity::read_from_file trying to read %s as ECAT6\n", filename.c_str());
+#endif
+    USING_NAMESPACE_ECAT6;
+      if (is_ecat6_image_file(filename))
+      {
+        ECAT6_Main_header mhead;
+        FILE * cti_fptr=fopen(filename.c_str(), "rb"); 
+        if(cti_read_ECAT6_Main_header(cti_fptr, &mhead)!=EXIT_SUCCESS) 
+          error ("error reading main header in ECAT 6 file %s\n", filename.c_str());
+        
+        return
+          ECAT6_to_VoxelsOnCartesianGrid(/*frame_num, gate_num, data_num, bed_num*/1,1,0,0,
+          cti_fptr, mhead);
+      }
+  }
+
+#ifdef HAVE_LLN_MATRIX
+  {
+#ifndef NDEBUG
+    warning("DiscretisedDensity::read_from_file trying to read %s as ECAT7\n", filename.c_str());
+#endif
+    USING_NAMESPACE_ECAT7;
+
+    if (is_ecat7_image_file(filename))
+    {
+      string interfile_header_name;
+      if (write_basic_interfile_header_for_ecat7(interfile_header_name, filename, 1,1,0,0) ==
+        Succeeded::no)
+        return 0;
+#ifndef NDEBUG
+      warning("DiscretisedDensity::read_from_file wrote interfile header %s\nNow reading as interfile", 
+        interfile_header_name.c_str());
+#endif
+      return 
+        read_interfile_image(interfile_header_name);
+    }
+  }
+#endif // HAVE_LLN_MATRIX
+
+
+  error("DiscretisedDensity::read_from_file: %s seems to be in an unsupported file format\n",
+	filename.c_str());
+  return 0;
+
 }
 
 
