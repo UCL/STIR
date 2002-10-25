466f4817ba44a35bffd7f786dc5b4c020ecac0a3
Kris Thielemans <Kris.Thielemans@stir>
2002-10-25 10:17:08 +0000

    - added ECAT7

--- src/IO/IO_registries.cxx
+++ src/IO/IO_registries.cxx
@@ -20,9 +20,15 @@
 
 #include "stir/IO/InterfileOutputFileFormat.h"
 #include "stir/IO/ECAT6OutputFileFormat.h"
+#ifdef HAVE_LLN_MATRIX
+#include "stir/IO/ECAT7OutputFileFormat.h"
+#endif
 
 START_NAMESPACE_STIR
 
 static InterfileOutputFileFormat::RegisterIt dummy1;
 static ECAT6OutputFileFormat::RegisterIt dummy2;
+#ifdef HAVE_LLN_MATRIX
+static ECAT7OutputFileFormat::RegisterIt dummy3;
+#endif
 END_NAMESPACE_STIR
