dd5cf07b06a177419025f764791de4083b1741e4
Kris Thielemans <Kris.Thielemans@stir>
2002-12-12 19:01:31 +0000

    - added threshold_min_to_small_positive_value

--- src/include/stir/thresholding.h
+++ src/include/stir/thresholding.h
@@ -19,7 +19,12 @@
     Copyright (C) 2000- $Date$, IRSL
     See STIR/LICENSE.txt for details
 */
-#include "stir/common.h"
+#include "stir/min_positive_element.h"
+#include <algorithm>
+
+#ifndef STIR_NO_NAMESPACES
+using std::fill;
+#endif
 
 START_NAMESPACE_STIR
 
@@ -73,6 +78,36 @@ threshold_lower(forw_iterT begin, forw_iterT end,
       if (new_min > *iter)
 	*iter = new_min;
     }
+}
+
+//! sets non-positive values in the sequence to small positive ones
+/*!
+  Thresholds the sequence from below to  
+  \code *min_positive_element()*small_number
+  \end_code
+  However, if all values are less than or equal to 0, they are 
+  set to \a small_number.
+
+   \param start start of the sequence. Usually object.begin().
+   \param end end of the sequence in iterator sense (so actually one beyond
+     the last element). Usually object.end().
+   \param small_number see above
+
+   The iterator type has to satisfy the requirements of a forward iterator,
+   and its value_type has to be comparable using < and <=.
+*/ 
+template <typename ForwardIter_t, typename elemT>
+void
+threshold_min_to_small_positive_value(ForwardIter_t begin, ForwardIter_t end, 
+				      const elemT& small_number)
+{
+  const ForwardIter_t smallest_positive_element_iter =
+    min_positive_element(begin, end);
+
+  if (smallest_positive_element_iter!= end)
+    threshold_lower(begin, end,  (*smallest_positive_element_iter)*small_number);
+  else
+    fill(begin, end, small_number);
 }
 
 END_NAMESPACE_STIR
