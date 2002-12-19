07c1a0a7e66ce92177f8ccb707626040e5472cea
Kris Thielemans <Kris.Thielemans@stir>
2002-12-19 12:08:24 +0000

    - inserted checks on odd max_segment_num_to_process

--- src/local/utilities/SSRB.cxx
+++ src/local/utilities/SSRB.cxx
@@ -34,6 +34,8 @@ int main(int argc, char **argv)
     {
       cerr << "Usage:\n"
 	   << argv[0] << " output_filename input_projdata_name extra_span [ extra_mash [do_norm [max_in_segment_num_to_process ]]]\n"
+	   << "extra_span has to be odd. It is used as the number of segments\n"
+	   << "  in the original data to combine.\n"
 	   << "do_norm has to be 1 (normalise the result) or 0\n"
 	   << "max_in_segment_num_to_process defaults to all segments\n";
       exit(EXIT_FAILURE);
@@ -44,7 +46,12 @@ int main(int argc, char **argv)
   const int num_views_to_combine = argc<=4 ? 1 : atoi(argv[4]);
   const bool do_norm = argc<=5 ? true : atoi(argv[5]) != 0;
   const int max_segment_num_to_process = argc <=6 ? -1 : atoi(argv[5]);
-
+  if (max_segment_num_to_process>0 && max_segment_num_to_process%2==0)
+    {
+      warning("SSRB: the 'extra_span' argument has to be odd, but it is %d. Aborting\n", 
+	      max_segment_num_to_process);
+      return EXIT_FAILURE;
+    }
   SSRB(output_filename,
        *in_projdata_ptr,
        span,
