8727e435e2cab0da9fa6db7af2875d2e48099948
Kris Thielemans <Kris.Thielemans@stir>
2003-02-07 11:07:48 +0000

    first version in archive

---
+++ src/include/local/stir/multiply_plane_scale_factorsImageProcessor.h
@@ -0,0 +1,72 @@
+//
+// $Id$
+//
+/*!
+
+  \file
+  \ingroup buildblock  
+  \brief Declaration of class multiply_plane_scale_factorsImageProcessor
+    
+  \author Kris Thielemans
+      
+  $Date$
+  $Revision$
+*/
+/*
+    Copyright (C) 2003- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
+#ifndef __stir_multiply_plane_scale_factorsImageProcessor_H__
+#define __stir_multiply_plane_scale_factorsImageProcessor_H__
+
+
+#include "stir/RegisteredParsingObject.h"
+#include "stir/ImageProcessor.h"
+#include <vector>
+
+#ifndef STIR_NO_NAMESPACES
+using std::vector;
+#endif
+
+START_NAMESPACE_STIR
+
+template <typename elemT> class VectorWithOffset;
+
+/*!
+  \brief Simply multiplies each plane in an image with a scale factor.
+ */
+
+template <typename elemT>
+class multiply_plane_scale_factorsImageProcessor : 
+  public 
+    RegisteredParsingObject<
+        multiply_plane_scale_factorsImageProcessor<elemT>,
+        ImageProcessor<3,elemT>
+    >
+{
+public:
+  static const char * const registered_name;   
+  multiply_plane_scale_factorsImageProcessor();
+  multiply_plane_scale_factorsImageProcessor(const VectorWithOffset<double>&  plane_scale_factors);
+  multiply_plane_scale_factorsImageProcessor(const vector<double>&  plane_scale_factors);
+    
+  
+private:
+  vector<double>  plane_scale_factors;
+
+  virtual void set_defaults();
+  virtual void initialise_keymap();
+  
+  Succeeded virtual_set_up(const DiscretisedDensity<3,elemT>& image);
+
+  void  virtual_apply(DiscretisedDensity<3,elemT>& out_density, const DiscretisedDensity<3,elemT>& in_density) const;
+  void  virtual_apply(DiscretisedDensity<3,elemT>& density) const ;
+  
+};
+
+END_NAMESPACE_STIR
+
+#endif
+
+
