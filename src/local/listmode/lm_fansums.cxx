3a19cfc11c73135486d88a82655cf3c8879a07a4
Kris Thielemans <Kris.Thielemans@stir>
2003-03-11 18:52:11 +0000

    - moved class TimeFrameDefinitions to its own files

--- src/local/listmode/lm_fansums.cxx
+++ src/local/listmode/lm_fansums.cxx
@@ -41,6 +41,7 @@ INCLUDE_NORMALISATION_FACTORS:
 
 #endif
 #include "local/stir/listmode/CListModeData.h"
+#include "local/stir/listmode/TimeFrameDefinitions.h"
 #include "stir/Scanner.h"
 #include "stir/Array.h"
 #include "stir/IndexRange2D.h"
@@ -51,7 +52,6 @@ INCLUDE_NORMALISATION_FACTORS:
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <utility>
 
 #ifndef STIR_NO_NAMESPACES
 using std::fstream;
@@ -63,113 +63,11 @@ using std::flush;
 using std::endl;
 using std::min;
 using std::max;
-using std::pair;
 using std::vector;
-using std::make_pair;
 #endif
 
 START_NAMESPACE_STIR
 
-class TimeFrameDefinitions
-{
-public:
-  TimeFrameDefinitions();
-  explicit TimeFrameDefinitions(const string& fdef_filename);
-  
-  //! 1 based
-  double get_start_time(unsigned int frame_num) const;
-  double get_end_time(unsigned int frame_num) const;
-  
-  double get_start_time() const;
-  double get_end_time() const;
-  
-  unsigned int get_num_frames() const;
-  
-private:
-  vector<pair<double, double> > frame_times;
-};
-
-double
-TimeFrameDefinitions::
-get_start_time(unsigned int frame_num) const
-{
-  assert(frame_num>=1);
-  assert(frame_num<=get_num_frames());
-  return frame_times[frame_num-1].first;
-}
-
-double
-TimeFrameDefinitions::
-get_end_time(unsigned int frame_num) const
-{
-  assert(frame_num>=1);
-  assert(frame_num<=get_num_frames());
-  return frame_times[frame_num-1].second;
-}
-
-double
-TimeFrameDefinitions::
-get_start_time() const
-{
-  return get_start_time(1);
-}
-
-double
-TimeFrameDefinitions::
-get_end_time() const
-{
-  return get_end_time(get_num_frames());
-}
-
-unsigned int
-TimeFrameDefinitions::
-get_num_frames() const
-{
-  return frame_times.size();
-}
-
-TimeFrameDefinitions::
-TimeFrameDefinitions()
-{}
-
-TimeFrameDefinitions::
-TimeFrameDefinitions(const string& fdef_filename)
-{
-  ifstream in(fdef_filename.c_str());
-  if (!in)
-    error("Error reading %s\n", fdef_filename.c_str());
-
-  
-  double previous_end_time = 0;
-  while (true)
-  {
-    int num;
-    double duration;
-    in >> num >> duration;
-    if (!in || in.eof())
-      break;
-    if (num<=0 || duration<=0)
-        error("Reading frame_def file %s: encountered negative numbers (%d, %g)\n",
-	      fdef_filename.c_str(), num, duration);
-
-    while (num--)
-    {
-      frame_times.push_back(make_pair(previous_end_time, previous_end_time+duration));
-      previous_end_time+=duration;
-    }
-  }
-
-  cerr << "Frame definitions:\n{";
-  for (unsigned frame_num=1; frame_num<=get_num_frames(); ++frame_num)
-  {
-    cerr << '{' << get_start_time(frame_num) 
-         << ',' << get_end_time(frame_num) 
-         << '}';
-    if (frame_num<get_num_frames())
-      cerr << ',';
-  }
-  cerr << '}' << endl;
-}
 
 class LmFansums : public ParsingObject
 {
