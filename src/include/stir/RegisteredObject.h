882f0e8d38ab135b80ce9db6243a54485c0c8943
Kris Thielemans <Kris.Thielemans@stir>
2003-05-23 17:47:18 +0000

    - more documentation

--- src/include/stir/RegisteredObject.h
+++ src/include/stir/RegisteredObject.h
@@ -2,7 +2,6 @@
 // $Id$
 //
 /*!
-
   \file
   \ingroup buildblock
   \brief Declaration of class RegisteredObject
@@ -37,8 +36,33 @@ START_NAMESPACE_STIR
 
 /*!
   \brief Helper class to provide registry mechanisms to a Base class
+  \ingroup buildblock
 
-  Hierarchy looks normally as follows:
+  Suppose you have a hierarchy of classes with (nearly) all public
+  functionality provided by virtual functions of the Base class.
+  The aim is then to be able to select <i>at run-time</i> which 
+  of the nodes will be used. 
+
+  To do this, one needs to enter all
+  node classes in a registry. This registry contains a key and a
+  &quot;Base factory&quot; for every node-class. The factory for
+  the node-class returns (a pointer to) a new node-class object, 
+  which of course is also a Base object.
+
+  In STIR, FactoryRegistry provides the type for the registry.
+
+  In many cases, the factory constructs the new object from a stream. 
+  The current class provides the basic mechanisms for this, i.e.
+  a registry, and a function that looks up the relevant factory
+  in the registry and uses it to construct the object from a stream.
+  In addition, there is an interactive function for asking the type
+  and its parameters. This makes only sense if the object construction
+  can be interactive as well (see ask_type_and_parameters()).
+
+  In case the construction of the object is done by using ParsingObject,
+  nearly all of the necessary functionality can be provided to the
+  hierarchy by using RegisteredParsingObject in the hierarchy. In such 
+  a case, the hierarchy looks normally as follows:
   \code
   RegisteredObject<Base>
   Base
@@ -48,12 +72,36 @@ START_NAMESPACE_STIR
   Derived
   \endcode
 
+  A <strong>recommended</strong> variation on this is the following:
+  \code
+  RegisteredObject<Base>
+  Base <- ParsingObject
+  ...
+  RegisteredParsingObject<Derived,Base,Base>
+  Derived
+  \endcode
+  Aside from the fact that this is simpler, it also is more future proof.
+  Suppose that at some point you want to add parsing keys that are common to 
+  the whole hierarchy. The best/only place to do this is to add them to 
+  the \a Base class. But this can only be done if \a Base is derived from 
+  ParsingObject, and hence there is can be no other ParsingObject in the 
+  hierarchy. So, the recommended variation will need no change at all, while
+  the general version would need change to all nodes.
+
+  As a final note, RegisteredObject could also be used for hierarchies that
+  do not use ParsingObject.
+  
+  \see RegisteredParsingObject
+
+  \todo Currently there is a hard-wired value of &quot;None&quot;
+  for the default key (with a 0 factory). This is inappropriate 
+  in some cases.
 
   \warning Visual C++ cannot inline the registry() function. As a result,
   all possible instantiations of the RegisteredObject template have to be
-  defined in RegisteredObject.cxx file(s). 
+  defined in RegisteredObject.cxx file(s). You will have link errors if
+  you forgot to do this.
 */
-//TODOdoc more
 template <typename Root>
 class RegisteredObject : public Object
 {
@@ -61,13 +109,14 @@ public:
   inline RegisteredObject();
 
   /*!
-    \brief Construct a new object (of Root, but actual type determined by the  registered_name parameter) by parsing the istream
+    \brief Construct a new object (of a type derived from Root, its actual type determined by the  registered_name parameter) by parsing the istream
   
-    This works by finding a 'root factory' object in a registry, and calling the factory on this istream*.
+    This works by finding the 'root factory' object in a registry that corresponds to
+    \a registered_name, and calling the factory on this istream*.
   */
   inline static Root* read_registered_object(istream* in, const string& registered_name);
 
-  //! \brief ask the user for which type, and then calls read_registered_object(0, type)
+  //! \brief ask the user for the type, and then calls read_registered_object(0, type)
   /*! 
     \warning Relies on read_registered_object to be interactive when its first argument is 0.
 
