ccfbdc7638af63d970f1f4e37810c3e508a04b46
Sanida Mustafovic <Sanida.Mustafovic@stir>
2003-08-14 17:15:01 +0000

    - added loop over several ddetectors - make sure that swapped detectors are recognised without error

--- src/local/test/test_LmToProjdataWithMC.cxx
+++ src/local/test/test_LmToProjdataWithMC.cxx
@@ -7,12 +7,13 @@
 
   \brief Test program for LmToProjdataWithMC functions
   \author Sanida Mustafovic
+  \author Kris Thielemans
 
   $Date$
   $Revision$
 */
 /*
-    Copyright (C) 2003- $Date$ , Hammersmith Imanet Ltd
+    Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
 #include "stir/RunTests.h"
@@ -43,47 +44,44 @@ LmToProjDataWithMCTests::run_tests()
   const char * const par_filename = "test.par";
   LmToProjDataWithMC LmToProjDataWithMCObject(par_filename);
   shared_ptr<Scanner> scanner = new Scanner(Scanner::E966);
-
-  const int Ring_A = 5;
-  const int Ring_B = 6;
-  const int det1 = 144;
-  const int det2 = 60;
-
-  CartesianCoordinate3D<float> coord_1;
-  CartesianCoordinate3D<float> coord_2;
-  
-  // normally one cannot access private members of in the class but I have made it public while testing
-  LmToProjDataWithMCObject.find_cartesian_coordinates_given_scanner_coordinates (coord_1,coord_2,
-							Ring_A,Ring_B, 
-							det1,det2, 
-							*scanner);
-
-  CartesianCoordinate3D<float> tmp = (coord_2-coord_1);
-  tmp.x() = 5*tmp.x();
-  tmp.y() = 5*tmp.y();
-  tmp.z() = 5*tmp.z();
-
- CartesianCoordinate3D<float> ttt = (coord_2-coord_1);
-  ttt.x() = 2*ttt.x();
-  ttt.y() = 2*ttt.y();
-  ttt.z() = 2*ttt.z();
-
-  CartesianCoordinate3D<float> coord_1_new = coord_1 + tmp;
-  CartesianCoordinate3D<float> coord_2_new = coord_1 + ttt;
-
-
-  int det1_f, det2_f,ring1_f, ring2_f;
-
-  LmToProjDataWithMCObject.find_scanner_coordinates_given_cartesian_coordinates(det1_f, det2_f, ring1_f, ring2_f,
-							coord_1_new, coord_2_new, 
-							*scanner);
-  cerr<< " WARNING for this check values 1 and 2 can be swapped" <<endl;
-  check_if_equal( det1_f, det1, "test on det1");
-  check_if_equal( det2_f, det2, "test on det2");
-  check_if_equal( Ring_A, ring1_f, "test on ring1");
-  check_if_equal( Ring_B, ring2_f, "test on ring1");
-
-
+  for ( int Ring_A = 1; Ring_A < 10; Ring_A++)
+    for ( int Ring_B = 1; Ring_B < 10; Ring_B++)
+      for ( int det1 =1; det1 <=10; det1 ++)
+	for ( int det2 =100; det2 <=110; det2 ++)
+	  
+	{
+	  CartesianCoordinate3D<float> coord_1;
+	  CartesianCoordinate3D<float> coord_2;
+	  
+	  // normally one cannot access private members of in the class but I have made it public while testing
+	  LmToProjDataWithMCObject.find_cartesian_coordinates_given_scanner_coordinates (coord_1,coord_2,
+	    Ring_A,Ring_B, 
+	    det1,det2, 
+	    *scanner);
+	  
+	  const CartesianCoordinate3D<float> coord_1_new = coord_1 + (coord_2-coord_1)*5;
+	  const CartesianCoordinate3D<float> coord_2_new = coord_1 + (coord_2-coord_1)*2;
+	  
+	  int det1_f, det2_f,ring1_f, ring2_f;
+	  
+	  LmToProjDataWithMCObject.find_scanner_coordinates_given_cartesian_coordinates(det1_f, det2_f, ring1_f, ring2_f,
+	    coord_1_new, coord_2_new, 
+	    *scanner);
+	  if (det1_f == det1 && Ring_A == ring1_f)
+	  { 
+	    check_if_equal( det1_f, det1, "test on det1");
+	    check_if_equal( Ring_A, ring1_f, "test on ring1");
+	    check_if_equal( det2_f, det2, "test on det2");
+	    check_if_equal( Ring_B, ring2_f, "test on ring1");
+	  }
+	  else
+	  {
+	    check_if_equal( det2_f, det1, "test on det1");
+	    check_if_equal( Ring_B, ring1_f, "test on ring1");
+	    check_if_equal( det1_f, det2, "test on det2");
+	    check_if_equal( Ring_A, ring2_f, "test on ring1");
+	  }
+	}
 }
 END_NAMESPACE_STIR
 
