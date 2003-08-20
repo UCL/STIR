7d6e4a384e0ac4b72639b441e9e6e7cbffd35e19
Sanida Mustafovic <Sanida.Mustafovic@stir>
2003-08-20 15:59:25 +0000

    included tests on find scanner given cartesian coorinates and vice versa

--- src/test/test_proj_data_info.cxx
+++ src/test/test_proj_data_info.cxx
@@ -572,6 +572,48 @@ test_proj_data_info(ProjDataInfoCylindricalNoArcCorr& proj_data_info)
 	
 	  } // end of get_det_pair_for_bin and back code
   }
+   cerr << endl;
+   cerr << "Testing find scanner coordinates given cartesian and vice versa" << endl; 
+   cerr << "No output means everything is fine." << endl;
+   {
+     
+  for ( int Ring_A = 1; Ring_A < 10; Ring_A++)
+    for ( int Ring_B = 1; Ring_B < 10; Ring_B++)
+      for ( int det1 =1; det1 <=10; det1 ++)
+	for ( int det2 =100; det2 <=110; det2 ++)
+	  
+	{
+	  CartesianCoordinate3D<float> coord_1;
+	  CartesianCoordinate3D<float> coord_2;
+	  
+	  // normally one cannot access private members of in the class but I have made it public while testing
+	  proj_data_info.find_cartesian_coordinates_given_scanner_coordinates (coord_1,coord_2,
+	    Ring_A,Ring_B, 
+	    det1,det2);
+	  
+	  const CartesianCoordinate3D<float> coord_1_new = coord_1 + (coord_2-coord_1)*5;
+	  const CartesianCoordinate3D<float> coord_2_new = coord_1 + (coord_2-coord_1)*2;
+	  
+	  int det1_f, det2_f,ring1_f, ring2_f;
+	  
+	  proj_data_info.find_scanner_coordinates_given_cartesian_coordinates(det1_f, det2_f, ring1_f, ring2_f,
+	    coord_1_new, coord_2_new);
+	  if (det1_f == det1 && Ring_A == ring1_f)
+	  { 
+	    check_if_equal( det1_f, det1, "test on det1");
+	    check_if_equal( Ring_A, ring1_f, "test on ring1");
+	    check_if_equal( det2_f, det2, "test on det2");
+	    check_if_equal( Ring_B, ring2_f, "test on ring1");
+	  }
+	  else
+	  {
+	    check_if_equal( det2_f, det1, "test on det1");
+	    check_if_equal( Ring_B, ring1_f, "test on ring1");
+	    check_if_equal( det1_f, det2, "test on det2");
+	    check_if_equal( Ring_A, ring2_f, "test on ring1");
+	  }
+	}
+   }
 }
 
 END_NAMESPACE_STIR
