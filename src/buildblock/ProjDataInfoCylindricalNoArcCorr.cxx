a495250c9b6413695f8de562d986dce9a96a7110
Sanida Mustafovic <Sanida.Mustafovic@stir>
2003-08-20 15:50:08 +0000

    included find scanner coordiantes and vice versa

--- src/buildblock/ProjDataInfoCylindricalNoArcCorr.cxx
+++ src/buildblock/ProjDataInfoCylindricalNoArcCorr.cxx
@@ -21,6 +21,9 @@
 
 #include "stir/ProjDataInfoCylindricalNoArcCorr.h"
 #include "stir/Bin.h"
+#include "stir/CartesianCoordinate3D.h"
+#include "stir/round.h"
+
 #ifdef BOOST_NO_STRINGSTREAM
 #include <strstream.h>
 #else
@@ -249,5 +252,158 @@ initialise_det1det2_to_view_tangpos() const
   }
   det1det2_to_view_tangpos_initialised = true;
 }
+
+Succeeded
+ProjDataInfoCylindricalNoArcCorr::
+find_scanner_coordinates_given_cartesian_coordinates(int& det1, int& det2, int& ring1, int& ring2,
+					             const CartesianCoordinate3D<float>& c1,
+						     const CartesianCoordinate3D<float>& c2) const
+{
+  const int num_detectors=get_scanner_ptr()->get_num_detectors_per_ring();
+  const float ring_spacing=get_scanner_ptr()->get_ring_spacing();
+  const float ring_radius=get_scanner_ptr()->get_ring_radius();
+
+  const CartesianCoordinate3D<float> d = c2 - c1;
+  /* parametrisation of LOR is 
+     c = l*d+c1
+     l has to be such that c.x^2 + c.y^2 = R^2
+     i.e.
+     (l*d.x+c1.x)^2+(l*d.y+c1.y)^2==R^2
+     l^2*(d.x^2+d.y^2) + 2*l*(d.x*c1.x + d.y*c1.y) + c1.x^2+c2.y^2-R^2==0
+     write as a*l^2+2*b*l+e==0
+     l = (-b +- sqrt(b^2-a*e))/a
+     argument of sqrt simplifies to
+     R^2*(d.x^2+d.y^2)-(d.x*c1.y-d.y*c1.x)^2
+  */
+  const float dxy2 = (square(d.x())+square(d.y()));
+  const float argsqrt=
+    (square(ring_radius)*dxy2-square(d.x()*c1.y()-d.y()*c1.x()));
+  if (argsqrt<=0)
+    return Succeeded::no; // LOR is outside detector radius
+  const float root = sqrt(argsqrt);
+
+  const float l1 = (- (d.x()*c1.x() + d.y()*c1.y())+root)/dxy2;
+  const float l2 = (- (d.x()*c1.x() + d.y()*c1.y())-root)/dxy2;
+  const CartesianCoordinate3D<float> coord_det1 = d*l1 + c1;
+  const CartesianCoordinate3D<float> coord_det2 = d*l2 + c1;
+  assert(fabs(square(coord_det1.x())+square(coord_det1.y())-square(ring_radius))<square(ring_radius)*10.E-5);
+  assert(fabs(square(coord_det2.x())+square(coord_det2.y())-square(ring_radius))<square(ring_radius)*10.E-5);
+
+  det1 = stir::round(((2.*_PI)+atan2(-coord_det1.x(),coord_det1.y()))/(2.*_PI/num_detectors))% num_detectors;
+  det2 = stir::round(((2.*_PI)+atan2(-coord_det2.x(),coord_det2.y()))/(2.*_PI/num_detectors))% num_detectors;
+  ring1 = round(coord_det1.z()/ring_spacing);
+  ring2 = round(coord_det2.z()/ring_spacing);
+
+
+#ifndef NDEBUG
+  {
+
+    CartesianCoordinate3D<float> check1, check2;
+    find_cartesian_coordinates_given_scanner_coordinates (check1, check2,
+							  ring1,ring2, 
+							  det1, det2);
+    assert(norm(coord_det1-check1)<ring_spacing);
+    assert(norm(coord_det2-check2)<ring_spacing);
+  }
+#endif
+  assert(det1 >=0 && det1<get_scanner_ptr()->get_num_detectors_per_ring());
+  assert(det2 >=0 && det2<get_scanner_ptr()->get_num_detectors_per_ring());
+
+  return 
+    (ring1 >=0 && ring1<get_scanner_ptr()->get_num_rings() &&
+     ring2 >=0 && ring2<get_scanner_ptr()->get_num_rings()) 
+     ? Succeeded::yes : Succeeded::no;
+}
+
+
+void 
+ProjDataInfoCylindricalNoArcCorr::
+find_cartesian_coordinates_of_detection(
+					CartesianCoordinate3D<float>& coord_1,
+					CartesianCoordinate3D<float>& coord_2,
+					const Bin& bin) const
+{
+ // find detectors
+  int det_num_a;
+  int det_num_b;
+  int ring_a;
+  int ring_b;
+  get_det_pair_for_bin(det_num_a, ring_a,
+                       det_num_b, ring_b, bin);
+  
+  // find corresponding cartesian coordinates
+  find_cartesian_coordinates_given_scanner_coordinates(coord_1,coord_2,
+    ring_a,ring_b,det_num_a,det_num_b);
+}
+
+
+void
+ProjDataInfoCylindricalNoArcCorr::
+find_cartesian_coordinates_given_scanner_coordinates (CartesianCoordinate3D<float>& coord_1,
+				 CartesianCoordinate3D<float>& coord_2,
+				 const int Ring_A,const int Ring_B, 
+				 const int det1, const int det2) const
+{
+//  assert(Ring_A >=0 && Ring_A<get_scanner_ptr()->get_num_rings());
+  //assert(Ring_B >=0 && Ring_B<get_scanner_ptr()->get_num_rings());
+  //assert(det1 >=0 && det1<get_scanner_ptr()->get_num_detectors_per_ring());
+  //assert(det2 >=0 && det2<get_scanner_ptr()->get_num_detectors_per_ring());
+
+  int num_detectors = get_scanner_ptr()->get_num_detectors_per_ring();
+
+  float df1 = (2.*_PI/num_detectors)*(det1);
+  float df2 = (2.*_PI/num_detectors)*(det2);
+  float x1 = get_scanner_ptr()->get_ring_radius()*cos(df1);
+  float y1 = get_scanner_ptr()->get_ring_radius()*sin(df1);
+  float x2 = get_scanner_ptr()->get_ring_radius()*cos(df2);
+  float y2 = get_scanner_ptr()->get_ring_radius()*sin(df2);
+  float z1 = Ring_A*get_scanner_ptr()->get_ring_spacing();
+  float z2 = Ring_B*get_scanner_ptr()->get_ring_spacing();
+  // make sure the return values are in STIR coordinates
+  coord_1.z() = z1;
+  coord_1.y() = x1;
+  coord_1.x() = -y1;
+
+  coord_2.z() = z2;
+  coord_2.y() = x2;
+  coord_2.x() = -y2; 
+}
+
+
+void 
+ProjDataInfoCylindricalNoArcCorr::
+find_bin_given_cartesian_coordinates_of_detection(Bin& bin,
+						  const CartesianCoordinate3D<float>& coord_1,
+						  const CartesianCoordinate3D<float>& coord_2) const
+{
+  int det_num_a_trans;
+  int det_num_b_trans;
+  int ring_a_trans;
+  int ring_b_trans;
+  
+  // given two CartesianCoordinates find the intersection     
+  if (find_scanner_coordinates_given_cartesian_coordinates(det_num_a_trans,det_num_b_trans,
+							   ring_a_trans, ring_b_trans,
+							   coord_1,
+							   coord_2) ==
+      Succeeded::no)
+  {
+    bin.set_bin_value(-1);
+    return;
+  }
+
+  if (ring_a_trans<0 ||
+      ring_a_trans>=get_scanner_ptr()->get_num_rings() ||
+      ring_b_trans<0 ||
+      ring_b_trans>=get_scanner_ptr()->get_num_rings() ||
+      get_bin_for_det_pair(bin,
+			   det_num_a_trans, ring_a_trans,
+			   det_num_b_trans, ring_b_trans) ==
+      Succeeded::no ||
+      bin.tangential_pos_num() < get_min_tangential_pos_num() ||
+      bin.tangential_pos_num() > get_max_tangential_pos_num())
+    bin.set_bin_value(-1);
+}
+
 END_NAMESPACE_STIR
 
