4ff6d64f73a800165a025a98ab604cc3fae1dbaa
Kris Thielemans <Kris.Thielemans@stir>
2003-08-29 23:57:35 +0000

    - made all time variables double - moved RigidObject3DMotionFromPolaris::Polaris_time_offset to here (as time_offset) - added virtual destructor - RigidObject3DMotion is now derived from ParsingObject, added all parsing functions - moved most things related to the transformation_to_reference here (from LmToProjDataWithMC)

--- src/include/local/stir/motion/RigidObject3DMotion.h
+++ src/include/local/stir/motion/RigidObject3DMotion.h
@@ -1,19 +1,19 @@
 //
-// $Id: 
+// $Id$
 //
 /*!
   \file
-  \ingroup local_buildblock
+  \ingroup motion
 
   \brief Declaration of class RigidObject3DMotion
 
   \author  Sanida Mustafovic and Kris Thielemans
-  $Date: 
-  $Revision: 
+  $Date$
+  $Revision$
 */
 
 /*
-    Copyright (C) 2000- $Date: , IRSL
+    Copyright (C) 2003- $Date$ , Hammersmith Imanet
     See STIR/LICENSE.txt for details
 */
 
@@ -21,22 +21,58 @@
 #include "local/stir/motion/RigidObject3DTransformation.h"
 #include "local/stir/listmode/CListModeData.h"
 #include "stir/RegisteredObject.h"
+#include "stir/ParsingObject.h"
 
 
 START_NAMESPACE_STIR
 
 
-class RigidObject3DMotion: public RegisteredObject<RigidObject3DMotion>
+class RigidObject3DMotion: public RegisteredObject<RigidObject3DMotion>,
+                           public ParsingObject
 {
 
 public:
-  virtual RigidObject3DTransformation compute_average_motion(const float start_time, const float end_time)const=0;
+  virtual ~RigidObject3DMotion() {}
 
-  virtual void get_motion(RigidObject3DTransformation& ro3dtrans, const float time) const =0;
+  virtual RigidObject3DTransformation 
+    compute_average_motion(const double start_time, const double end_time)const=0;
+
+  virtual void get_motion(RigidObject3DTransformation& ro3dtrans, const double time) const =0;
 
   virtual Succeeded synchronise(CListModeData&) =0;
-  
+  //!  Option to set  time offset manually in case synchronisation cannot be performed
+  void
+    set_time_offset(const double time_offset);
+  double 
+    get_time_offset() const;
+  //! Temporary function to allow others to see if they should call synchronise or not
+  bool is_time_offset_set() const;
+
+  const RigidObject3DTransformation& 
+    get_transformation_to_reference_position() const;
+
 protected:
+  virtual void set_defaults();
+  virtual void initialise_keymap();
+  virtual bool post_processing();
+
+  double time_offset;
+
+private:
+
+  static void 
+    find_ref_start_end_from_att_file (double& att_start_time, double& att_end_time, 
+				      const double transmission_duration, 
+				      const string& attnuation_filename);
+ 
+  
+  string attenuation_filename; 
+  double transmission_duration;
+  double reference_start_time;
+  double reference_end_time;
+  vector<double> reference_translation;
+  vector<double> reference_quaternion;
+  RigidObject3DTransformation transformation_to_reference_position;
 
 };
 
