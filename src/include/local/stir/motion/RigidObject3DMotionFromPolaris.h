6430da35d3fc0950126aa286310f7fff1398d335
Kris Thielemans <Kris.Thielemans@stir>
2003-08-29 23:54:37 +0000

    - made all time variables double - move Polaris_time_offset to RigidObject3DMotion::set_time_offset etc - removed destructor (RigidObject3DMotion has now a virtual destructor) - slightly changed base class as RigidObject3DMotion is now derived from ParsingObject  CVS: ----------------------------------------------------------------------

--- src/include/local/stir/motion/RigidObject3DMotionFromPolaris.h
+++ src/include/local/stir/motion/RigidObject3DMotionFromPolaris.h
@@ -1,22 +1,21 @@
 //
-// $Id: 
+// $Id$
 //
 /*!
   \file
-  \ingroup local_buildblock
+  \ingroup motion
 
   \brief Declaration of class RigidObject3DMotionFromPolaris
 
   \author  Sanida Mustafovic and Kris Thielemans
-  $Date: 
-  $Revision: 
+  $Date$
+  $Revision$ 
 */
 
 /*
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2003- $Date$, Hammersmith Imanet
     See STIR/LICENSE.txt for details
 */
-
 
 
 #include "local/stir/motion/RigidObject3DMotion.h"
@@ -25,25 +24,28 @@
 
 START_NAMESPACE_STIR
 
-class RigidObject3DMotionFromPolaris: public RegisteredParsingObject<RigidObject3DMotionFromPolaris,RigidObject3DMotion> 
+class RigidObject3DMotionFromPolaris: 
+  public 
+  RegisteredParsingObject< RigidObject3DMotionFromPolaris,
+			   RigidObject3DMotion,
+			   RigidObject3DMotion>
+                                                             
 {
 public:
   //! Name which will be used when parsing a MotionTracking object 
   static const char * const registered_name; 
-  // only neeed this to enable LmToProjDataWithMC(const char * const par_filename) function
 
+  // only need this to enable LmToProjDataWithMC(const char * const par_filename) function
   RigidObject3DMotionFromPolaris();
 
   RigidObject3DMotionFromPolaris(const string mt_filename,shared_ptr<Polaris_MT_File> mt_file_ptr);
 
-     // TODO ???
-  ~RigidObject3DMotionFromPolaris() {};
-
-  //! Set the reference position 
-  virtual RigidObject3DTransformation compute_average_motion(const float start_time, const float end_time) const;
+  //! Find average motion from the Polaris file 
+  virtual RigidObject3DTransformation 
+    compute_average_motion(const double start_time, const double end_time) const;
 
   //! Given the time obtain motion info, i.e. RigidObject3DTransformation
-  virtual void get_motion(RigidObject3DTransformation& ro3dtrans, const float time) const;
+  virtual void get_motion(RigidObject3DTransformation& ro3dtrans, const double time) const;
 
   //! Synchronise motion tracking file and listmode file
   virtual Succeeded synchronise(CListModeData& listmode_data);
@@ -59,15 +61,11 @@ public:
 
   void find_offset(CListModeData& listmode_data);
 
-  //! The option to set polaris time manualy in case syncronisation cannot be performed
-  Succeeded set_polaris_time_offset(float time_offset);
-
 
   shared_ptr<Polaris_MT_File> mt_file_ptr;
-  float Polaris_time_offset;
   string mt_filename;  
   //string lm_filename;
-
+ 
 #if 1
   virtual void set_defaults();
   virtual void initialise_keymap();
