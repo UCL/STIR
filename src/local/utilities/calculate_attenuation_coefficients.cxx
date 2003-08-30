01631c722be5ca6e5cd013371347ac987d89673b
Kris Thielemans <Kris.Thielemans@stir>
2003-08-30 21:29:10 +0000

    - some code clean-up - removed truncate_rim (it was doing nothing anyway) - make sure there is no crash when the command line parameters are wrong

--- src/local/utilities/calculate_attenuation_coefficients.cxx
+++ src/local/utilities/calculate_attenuation_coefficients.cxx
@@ -1,14 +1,23 @@
 //
-// $Id:
+// $Id$
 //
 
 /*!
   \file
-  \ingroup 
+  \ingroup utilities
 
-  \brief Calucaltes attenuation coefficients :
-    Attenuation_image_ptr has to contain an estimate of the mu-map for the image. It will used
+  \brief Calculates attenuation coefficients 
+
+    Attenuation_image has to contain an estimate of the mu-map for the image. It will used
     to estimate attenuation factors as exp(-forw_proj(*attenuation_image_ptr)).
+
+  \par Usage: 
+  \code 
+  calculate_attenuation_coefficients  <output filename > <input header file name> <template_proj_data>
+  \endcode
+
+  \warning attenuation image data are supposed to be in units cm^-1.
+  Reference: water has mu .096 cm^-1.
 
   \author Sanida Mustafovic
   \author Kris Thielemans
@@ -17,23 +26,19 @@
   $Revision$
 */
 /*
-    Copyright (C) 2001- $Year:$, IRSL
+    Copyright (C) 2001- $Date$, IRSL
     See STIR/LICENSE.txt for details
 */
 
 
-#include "stir/utilities.h"
 #include "stir/ProjDataInterfile.h"
 #include "stir/RelatedViewgrams.h"
 #include "stir/VoxelsOnCartesianGrid.h"
 #include "stir/Viewgram.h"
 #include "stir/ArrayFunction.h"
-#include "stir/recon_array_functions.h"
 #include "stir/recon_buildblock/ForwardProjectorByBin.h"
 #include "stir/recon_buildblock/ForwardProjectorByBinUsingProjMatrixByBin.h"
 #include "stir/recon_buildblock/ProjMatrixByBinUsingRayTracing.h"
-
-#include "stir/SegmentByView.h"
 
 #include <iostream>
 #include <list>
@@ -50,15 +55,6 @@ using std::endl;
 
 START_NAMESPACE_STIR
 
-void
-do_segments(const VoxelsOnCartesianGrid<float>& image, 
-	    ProjData& proj_data,
-	    const int start_segment_num, const int end_segment_num,
-	    const int start_view, const int end_view,
-	    const int start_tangential_pos_num, const int end_tangential_pos_num,
-	    ForwardProjectorByBin& forw_projector,
-	    const bool disp);
-
 
 void
 do_segments(const VoxelsOnCartesianGrid<float>& image, 
@@ -66,8 +62,7 @@ do_segments(const VoxelsOnCartesianGrid<float>& image,
 	    const int start_segment_num, const int end_segment_num,
 	    const int start_view, const int end_view,
 	    const int start_tangential_pos_num, const int end_tangential_pos_num,
-	    ForwardProjectorByBin& forw_projector,
-	    const bool disp)
+	    ForwardProjectorByBin& forw_projector)
 {
   shared_ptr<DataSymmetriesForViewSegmentNumbers> symmetries_sptr =
     forw_projector.get_symmetries_used()->clone();  
@@ -77,7 +72,7 @@ do_segments(const VoxelsOnCartesianGrid<float>& image,
   for (int segment_num = start_segment_num; segment_num <= end_segment_num; ++segment_num)
     for (int view= start_view; view<=end_view; view++)      
     {       
-      ViewSegmentNumbers vs(view, segment_num);
+      const ViewSegmentNumbers vs(view, segment_num);
       symmetries_sptr->find_basic_view_segment_numbers(vs);
       if (find(already_processed.begin(), already_processed.end(), vs)
 	!= already_processed.end())
@@ -87,35 +82,22 @@ do_segments(const VoxelsOnCartesianGrid<float>& image,
       
       
       RelatedViewgrams<float> viewgrams = 
-	proj_data.get_empty_related_viewgrams(vs, symmetries_sptr,false);
+	proj_data.get_empty_related_viewgrams(vs, symmetries_sptr);
 
       forw_projector.forward_project(viewgrams, image,
-	viewgrams.get_min_axial_pos_num(),
-	viewgrams.get_max_axial_pos_num(),
-	start_tangential_pos_num, end_tangential_pos_num);
+				     viewgrams.get_min_axial_pos_num(),
+				     viewgrams.get_max_axial_pos_num(),
+				     start_tangential_pos_num, end_tangential_pos_num);
       
       // do the exp 
-      RelatedViewgrams<float>::iterator viewgrams_iter= 
-	viewgrams.begin();
-      for (; 
-      viewgrams_iter != viewgrams.end();
-      ++viewgrams_iter)
+      for (RelatedViewgrams<float>::iterator viewgrams_iter= viewgrams.begin();
+	   viewgrams_iter != viewgrams.end();
+	   ++viewgrams_iter)
       {
 	Viewgram<float>& viewgram = *viewgrams_iter;
 	viewgram *= -1;
 	in_place_exp(viewgram);
-	// set rim_truncation_sino to 0 as there is no scatter and 
-	// presumably there would not be any influance whatsoever
-	int rim_truncation_sino = 0;
-	truncate_rim(viewgram, rim_truncation_sino);
-      }
-      //if (multiplicative_binwise_correction_ptr != NULL)
-      //{
-      // KTXXX temporary (?) fix to divide instead of multiply
-      //*viewgrams_ptr /= *multiplicative_binwise_correction_ptr;
-      
-      //}
-      
+      }      
       
       if (!(proj_data.set_related_viewgrams(viewgrams) == Succeeded::yes))
 	error("Error set_related_viewgrams\n");            
@@ -133,7 +115,8 @@ main (int argc, char * argv[])
   
   if (argc!=3)
   {
-    cerr<<"\nUsage: calculate attenuation coefficients : <output filename > <input header file name> <proj_data> \n"<<endl;
+    cerr<<"\nUsage: calculate_attenuation_coefficients : <output filename > <input header file name> <template_proj_data> \n"<<endl;
+    return EXIT_FAILURE;
   }
   
   shared_ptr <DiscretisedDensity<3,float> > attenuation_density_ptr =
@@ -141,19 +124,29 @@ main (int argc, char * argv[])
   VoxelsOnCartesianGrid<float> *  attenuation_image_ptr = 
     dynamic_cast<VoxelsOnCartesianGrid<float> *> (attenuation_density_ptr.get());
 
-  // projectors work in pixel units, so convert attenuation data 
-  // from cm^-1 to pixel_units^-1 
-  const float rescale = attenuation_image_ptr->get_voxel_size().x()/10;
-  *attenuation_image_ptr *= rescale;      
+    cerr << "WARNING: attenuation image data are supposed to be in units cm^-1\n"
+      "Reference: water has mu .096 cm^-1" << endl;
+    cerr<< "Max in attenuation image:" 
+      << attenuation_image_ptr->find_max() << endl;
+#ifndef NORESCALE
+    /*
+      cerr << "WARNING: multiplying attenuation image by x-voxel size "
+      << " to correct for scale factor in forward projectors...\n";
+    */
+    // projectors work in pixel units, so convert attenuation data 
+    // from cm^-1 to pixel_units^-1
+    const float rescale = attenuation_image_ptr->get_voxel_size().x()/10;
+#else
+    const float rescale = 
+      10.F;
+#endif
+    *attenuation_image_ptr *= rescale;
 
   shared_ptr<ProjData> proj_data_ptr = 
     ProjData::read_from_file(argv[3]);
 
-   ProjDataInfo* new_data_info_ptr;
-   new_data_info_ptr= proj_data_ptr->get_proj_data_info_ptr()->clone();
-
   shared_ptr<ProjMatrixByBin> PM = 
-    new  ProjMatrixByBinUsingRayTracing(); //attenuation_image_ptr , proj_data_ptr->get_proj_data_info_ptr()->clone()); 	
+    new  ProjMatrixByBinUsingRayTracing();
   ForwardProjectorByBin* forw_projector_ptr =
     new ForwardProjectorByBinUsingProjMatrixByBin(PM); 
   
@@ -167,13 +160,12 @@ main (int argc, char * argv[])
 			  output_file_name);
   
   do_segments(*attenuation_image_ptr,*out_proj_data_ptr,
-      proj_data_ptr->get_min_segment_num(), proj_data_ptr->get_max_segment_num(), 
-      proj_data_ptr->get_min_view_num(), 
-      proj_data_ptr->get_max_view_num(),
-      proj_data_ptr->get_min_tangential_pos_num(), 
-      proj_data_ptr->get_max_tangential_pos_num(),
-      *forw_projector_ptr,
-	      false);  
+	      proj_data_ptr->get_min_segment_num(), proj_data_ptr->get_max_segment_num(), 
+	      proj_data_ptr->get_min_view_num(), 
+	      proj_data_ptr->get_max_view_num(),
+	      proj_data_ptr->get_min_tangential_pos_num(), 
+	      proj_data_ptr->get_max_tangential_pos_num(),
+	      *forw_projector_ptr);  
   
   return EXIT_SUCCESS;
 }
