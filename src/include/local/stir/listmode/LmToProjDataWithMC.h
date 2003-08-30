6fd4c2b04de203872d7c1c44b45609c1aa7b3b29
Kris Thielemans <Kris.Thielemans@stir>
2003-08-30 00:01:25 +0000

    - moved Polaris_time_offset to RigidObject3DMotion - moved most things related to the transformation_to_reference to RigidObject3DMotion - overload process_new_time_event()

--- src/include/local/stir/listmode/LmToProjDataWithMC.h
+++ src/include/local/stir/listmode/LmToProjDataWithMC.h
@@ -14,7 +14,7 @@
   $Revision$
 */
 /*
-    Copyright (C) 2003- $Date$, IRSL
+    Copyright (C) 2003- $Date$, Hammersmith Imanet
     See STIR/LICENSE.txt for details
 */
 
@@ -25,7 +25,6 @@
 #include "local/stir/listmode/LmToProjData.h"
 #include "stir/CartesianCoordinate3D.h"
 
-#include "local/stir/motion/Polaris_MT_File.h"
 #include "local/stir/motion/RigidObject3DMotionFromPolaris.h"
 
 START_NAMESPACE_STIR
@@ -37,41 +36,19 @@ public:
   LmToProjDataWithMC(const char * const par_filename);
 
   virtual void get_bin_from_event(Bin& bin, const CListEvent&) const;
+  virtual void process_new_time_event(const CListTime& time_event);
 
-
-//private:
-
-  void find_ref_pos_from_att_file (float& att_start_time, float& att_end_time, 
-	const float transmission_duration, const string attnuation_filename);
-  /*
-  void find_cartesian_coordinates_given_scanner_coordinates (CartesianCoordinate3D<float>& coord_1,
-				 CartesianCoordinate3D<float>& coord_2,
-				 const int Ring_A,const int Ring_B, 
-				 const int det1, const int det2, 
-				 const Scanner& scanner) const;
-
-
-  Succeeded find_scanner_coordinates_given_cartesian_coordinates(int& det1, int& det2, int& ring1, int& ring2,
-							  const CartesianCoordinate3D<float>& coord_1_in,
-							  const CartesianCoordinate3D<float>& coord_2_in, 
-							  const Scanner& scanner) const;*/
-
-  void transform_detector_pair_into_view_bin (int& view,int& bin, 
-					    const int det1,const int det2, 
-					    const Scanner& scanner) const;
+private:
 
   shared_ptr<RigidObject3DMotion> ro3d_ptr;
-  string attenuation_filename; 
-  float transmission_duration;
-  float polaris_time_offset;
    
   virtual void set_defaults();
   virtual void initialise_keymap();
   virtual bool post_processing();
 
-  mutable RigidObject3DTransformation ro3d_move_to_reference_position;
   RigidObject3DTransformation move_to_scanner;
   RigidObject3DTransformation move_from_scanner;
+  RigidObject3DTransformation ro3dtrans; // actual Polaris  motion for current_time
 
   
 };
