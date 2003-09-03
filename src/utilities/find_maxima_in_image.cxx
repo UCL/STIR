69fb4c744d804c94e1009f0bed6b4335ab9da61f
Kris Thielemans <Kris.Thielemans@stir>
2003-09-03 08:36:07 +0000

    - can now do arbitrary number of maxima. Works by masking previously found   maxima with 0. To do this, the command line takes extra parameters. - also write location in mm (if possible)

--- src/utilities/find_maxima_in_image.cxx
+++ src/utilities/find_maxima_in_image.cxx
@@ -1,98 +1,148 @@
+//
+// $Id$
+//
+/*!
+  \file
+  \ingroup utilities
+  \brief List coordinates of maxima in the image
+
+  \author Sanida Mustafovic
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+
+  \par Usage:
+   \code
+   find_maximum_in_image filename [num_maxima [ mask_size_xy [mask_size z]] ]
+   \endcode
+   \param num_maxima defaults to 1
+   \param mask_size_xy defaults to 1
+    \param mask_size_z defaults to \a mask_size_xy
+*/
+/*
+    Copyright (C) 2003- $Date$, Hammersmith Imanet
+    See STIR/LICENSE.txt for details
+*/
 #include "stir/shared_ptr.h"
 #include "stir/DiscretisedDensity.h"
-#include "stir/VoxelsOnCartesianGrid.h"
+#include "stir/DiscretisedDensityOnCartesianGrid.h"
 
 #include <iostream>
-#include <fstream>
+#include <iomanip>
+#include <algorithm>
 
 
 #ifndef STIR_NO_NAMESPACES
-using std::iostream;
-using std::ifstream;
 using std::endl;
-using std::find;
+using std::cout;
 using std::cerr;
+using std::min;
+using std::max;
+using std::setw;
 #endif
 
 USING_NAMESPACE_STIR
-
-bool is_inside_range (int lower, int upper, int num)
-{
-  if ( num >= lower && num <= upper)
-    return true;
-  else
-    return false;
-}
-
-
-
  
 int main(int argc, char *argv[])
 { 
   
-  if (argc!= 2)
+  if (argc< 2 || argc>5)
   {
-    cerr << "Usage:" << argv[0] << " input image"<<endl<<endl;    
+    cerr << "Usage:" << argv[0] << " input_image [num_maxima [ mask_size_xy [mask_size z]] ]\n"
+	 << "\tnum_maxima defaults to 1\n"
+	 << "\tmask_size_xy defaults to 1\n"
+	 << "\tmask_size_z defaults to mask_size_xy" <<endl;    
     return (EXIT_FAILURE);
   }
 
-  ifstream in (argv[1]);  
+  const unsigned num_maxima = argc>=3 ? atoi(argv[2]) : 1;
+  const int mask_size_xy = argc>=4 ? atoi(argv[3]) : 1;
+  const int mask_size_z = argc>=5 ? atoi(argv[4]) : mask_size_xy;
 
- shared_ptr< DiscretisedDensity<3,float> >  input_image = 
-   DiscretisedDensity<3,float>::read_from_file(argv[1]);
+  cerr << "Finding " << num_maxima << " maxima, each at least \n\t"
+       << mask_size_xy << " pixels from each other in x,y direction, and\n\t"
+       << mask_size_z << " pixels from each other in z direction.\n";
 
- VoxelsOnCartesianGrid <float>*  input_image_vox = 0;
-   input_image_vox = 
-     dynamic_cast< VoxelsOnCartesianGrid<float>*  > (input_image.get());
+  shared_ptr< DiscretisedDensity<3,float> >  input_image_sptr = 
+    DiscretisedDensity<3,float>::read_from_file(argv[1]);
+  DiscretisedDensity<3,float>& input_image = *input_image_sptr;
 
-  float first_maximum = input_image_vox->find_max();
-  int max_k, max_j,max_i;
-  max_k = max_j = max_i =0;
-  for ( int k = input_image_vox->get_min_index(); k<= input_image_vox->get_max_index();k++)
-    for ( int j = (*input_image_vox)[k].get_min_index(); j<= (*input_image_vox)[k].get_max_index();j++)
-      for ( int i = (*input_image_vox)[k][j].get_min_index(); i<= (*input_image_vox)[k][j].get_max_index();i++)
+  const DiscretisedDensityOnCartesianGrid <3,float>*  input_image_cartesian_ptr = 
+    dynamic_cast< DiscretisedDensityOnCartesianGrid<3,float>*  > (input_image_sptr.get());
+
+  const bool is_cartesian_grid = 
+    input_image_cartesian_ptr!=0;
+
+  CartesianCoordinate3D<float> grid_spacing;
+  if (is_cartesian_grid)
+    grid_spacing = input_image_cartesian_ptr->get_grid_spacing();
+
+  for (unsigned int maximum_num=0; maximum_num!=num_maxima; ++ maximum_num)
+    {
+      const float current_maximum = input_image.find_max();
+      int max_k=0, max_j=0,max_i=0; // initialise to avoid compiler warnings
+      bool found=false;
       {
-	if ( (*input_image_vox)[k][j][i] == first_maximum)
-	{
-	  max_k = k;
-	  max_j = j;
-	  max_i = i;
-	}
-	else
-	  continue;
+	const int min_k_index = input_image.get_min_index(); 
+	const int max_k_index = input_image.get_max_index();
+	for ( int k = min_k_index; k<= max_k_index && !found; ++k)
+	  {
+	    const int min_j_index = input_image[k].get_min_index(); 
+	    const int max_j_index = input_image[k].get_max_index();
+	    for ( int j = min_j_index; j<= max_j_index && !found; ++j)
+	      {
+		const int min_i_index = input_image[k][j].get_min_index(); 
+		const int max_i_index = input_image[k][j].get_max_index();
+		for ( int i = min_i_index; i<= max_i_index && !found; ++i)
+		  {
+		    if ( input_image[k][j][i] == current_maximum)
+		      {
+			max_k = k;
+			max_j = j;
+			max_i = i;
+			cout << "max: " << setw(6) << current_maximum 
+			     << " at: " 
+			     << setw(3) << max_k 
+			     << ',' << setw(3) << max_j 
+			     << ',' << setw(3)  << max_i;
+			if (is_cartesian_grid)
+			  {
+			    cout << " which is "
+			     << setw(6) << max_k*grid_spacing[1] 
+			     << ',' << setw(6) << max_j*grid_spacing[2] 
+			     << ',' << setw(6)  << max_i*grid_spacing[3]
+				 << in mm relative to origin";
+			  }
+			cout << '\n';
+			found = true;		  
+		      }
+		  }
+	      }
+	  }
+	if (!found)
+	  {
+	    warning("Something strange going on: can't find maximum %g\n", current_maximum);
+	    return EXIT_FAILURE;
+	  }
+	if (maximum_num+1!=num_maxima)
+	  {
+	    // now mask it out for next run
+	    for ( int k = max(max_k-mask_size_z,min_k_index); k<= min(max_k+mask_size_z,max_k_index); ++k)
+	    {
+	      const int min_j_index = input_image[k].get_min_index(); 
+	      const int max_j_index = input_image[k].get_max_index();
+	      for ( int j = max(max_j-mask_size_xy,min_j_index); j<= min(max_j+mask_size_xy,max_j_index); ++j)
+	      {
+		const int min_i_index = input_image[k][j].get_min_index(); 
+		const int max_i_index = input_image[k][j].get_max_index();
+		for ( int i = max(max_i-mask_size_xy,min_i_index); i<= min(max_i+mask_size_xy,max_i_index); ++i)
+		  input_image[k][j][i] = 0;
+	      }
+	    }
+	  }
       }
-#if 1
-      float second_maximum =-1.0;
-      int max_k_s;
-      int max_j_s;
-      int max_i_s;
-
-    // now find the other maximum 
-  for ( int k = input_image_vox->get_min_index(); k<= input_image_vox->get_max_index();k++)
-    for ( int j = (*input_image_vox)[k].get_min_index(); j<= (*input_image_vox)[k].get_max_index();j++)
-      for ( int i = (*input_image_vox)[k][j].get_min_index(); i<= (*input_image_vox)[k][j].get_max_index();i++)
-         {
-         if (is_inside_range (max_k-7, max_k+7, k) && is_inside_range (max_j-7, max_j+7, j)
-	   && is_inside_range (max_i-7, max_i+7, i))
-	   continue;
-	 else
-	   if ((*input_image_vox)[k][j][i] > second_maximum)
-	   {
-	   second_maximum = (*input_image_vox)[k][j][i];
-	   max_k_s=k;
-	   max_j_s=j;
-	   max_i_s=i;
-	   }
-	   else
-	     continue;
-      }
-#endif
-      cerr << "The  first maximum is:   " <<  first_maximum << "    " << " at position:  " << max_k <<"   " << max_j <<"   " << max_i << "   ";
-      cerr << "The  second maximum is:   " <<  second_maximum << "    " << " at position:  " << max_k_s <<"   " << max_j_s <<"   " << max_i_s << "   ";
-
-
-
-
-    return EXIT_SUCCESS;
+    }
+  return EXIT_SUCCESS;
 }
   
