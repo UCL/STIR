2cabcbc2d11a8bb115d25ae137176bdb521b5a78
Sanida Mustafovic <Sanida.Mustafovic@stir>
2003-09-12 09:55:49 +0000

    added SinglesRateFromECAT7

--- src/local/buildblock/local_buildblock_registries.cxx
+++ src/local/buildblock/local_buildblock_registries.cxx
@@ -13,18 +13,30 @@
 #include "local/stir/cleanup966ImageProcessor.h"
 #include "local/stir/multiply_plane_scale_factorsImageProcessor.h"
 
+#ifdef HAVE_LLN_MATRIX
+#include "local/stir/SinglesRateFromECAT7.h"
+#endif
+
 START_NAMESPACE_STIR
 static ModifiedInverseAverigingImageFilter<float>::RegisterIt dummy2;
 static DAVImageFilter3D<float>::RegisterIt dummy1;
 
 //static InverseSeparableCartesianMetzImageFilter <float>::RegisterIt dummy3;
 static SeparableLowPassImageFilter<float>::RegisterIt dummy4;
-static ModifiedInverseAveragingImageFilterAll<float>::RegisterIt dummy6;
+//static ModifiedInverseAveragingImageFilterAll<float>::RegisterIt dummy6;
 static NonseparableSpatiallyVaryingFilters<float>:: RegisterIt dummy7;
 static NonseparableSpatiallyVaryingFilters3D<float>::RegisterIt dummy8;
 
 static cleanup966ImageProcessor<float>::RegisterIt dummy100;
 static multiply_plane_scale_factorsImageProcessor<float>::RegisterIt dummy101;
+
+#ifdef HAVE_LLN_MATRIX
+START_NAMESPACE_ECAT
+START_NAMESPACE_ECAT7
+static SinglesRatesFromECAT7::RegisterIt dummy100;
+END_NAMESPACE_ECAT7
+END_NAMESPACE_ECAT
+#endif
 
 END_NAMESPACE_STIR
 
