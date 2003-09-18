273b7663158ab6cebb4686476353c9bea16a0db1
Sanida Mustafovic <Sanida.Mustafovic@stir>
2003-09-18 10:12:41 +0000

    new class for singles from sgl file

--- src/include/local/stir/SinglesRatesFromSglFile.h
+++ src/include/local/stir/SinglesRatesFromSglFile.h
@@ -27,7 +27,7 @@
 #include "stir/Array.h"
 #include "stir/RegisteredParsingObject.h"
 #include "stir/IO/stir_ecat7.h"
-#include "local/stir/listmode/TimeFrameDefinitions.h"
+
 
 START_NAMESPACE_STIR
 START_NAMESPACE_ECAT
@@ -53,25 +53,32 @@ public:
   SinglesRatesFromSglFile ();
 
   //! The function that reads singles from *.sgl file 
-  Array<3,long int> read_singles_from_sgl_file (const string& sgl_filename);
+  Array<3,float> read_singles_from_sgl_file (const string& sgl_filename);
   
   //! Given the detection position get the singles rate   
   virtual float get_singles_rate (const DetectionPosition<>& det_pos, 
-				  const float start_time,
-				  const float end_time) const;
-    
+				  const double start_time,
+				  const double end_time) const;
+  
+  vector<double> get_times() const;
+
 private:
-  Array<3,long int> singles;
-  //Array<1,long int> times;
+  Array<3,float> singles;
+  //Array<1,float> times;
+  vector<double> times;
   string sgl_filename;
   virtual void set_defaults();
   virtual void initialise_keymap();
   virtual bool post_processing();
+
+//#ifdef HAVE_LLN_MATRIX
+  Main_header singles_main_header;
+//#endif
   //TODO 
-  int transBlocksPerBucket;
-  int angularCrystalsPerBlock;
-  int axialCrystalsPerBlock;
-  
+  int trans_blocks_per_bucket;
+  int angular_crystals_per_block;
+  int axial_crystals_per_block;
+
 };
 
 END_NAMESPACE_ECAT7
