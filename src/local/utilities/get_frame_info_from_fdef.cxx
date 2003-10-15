5c2e0cf029f941f04267ccdf169d88c7426dece9
Sanida Mustafovic <Sanida.Mustafovic@stir>
2003-10-15 10:39:00 +0000

    get frame duration and the frame start time given fdef and frame number

---
+++ src/local/utilities/get_frame_info_from_fdef.cxx
@@ -0,0 +1,33 @@
+#include "local/stir/listmode/TimeFrameDefinitions.h"
+#include <iostream>
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+using std::endl;
+using std::cout;
+#endif
+
+USING_NAMESPACE_STIR
+
+int
+main(int argc, char* argv[])
+{
+  if(argc !=3)
+  {
+    cerr << "Usage:" << argv[0] << " Frame def filename, Frame number " << endl;
+    return EXIT_FAILURE;
+  }
+
+
+  TimeFrameDefinitions time_def(argv[1]);
+  const int frame_num = atoi(argv[2]);
+  const double start_frame = time_def.get_start_time(frame_num);
+  const double end_frame = time_def.get_end_time(frame_num);
+  double frame_duration = end_frame-start_frame;
+
+  cout << "Start frame " << start_frame << endl;
+  cout << "Frame duration " << frame_duration << endl;
+
+
+
+  return EXIT_SUCCESS;
+}
