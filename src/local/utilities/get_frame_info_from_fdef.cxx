e611f7a6799d3f6dd370dc8f2564118c7ce7deeb
Kris Thielemans <Kris.Thielemans@stir>
2003-10-17 23:35:28 +0000

    - write output in millisecs - removed some obsolete lines

--- src/local/utilities/get_frame_info_from_fdef.cxx
+++ src/local/utilities/get_frame_info_from_fdef.cxx
@@ -1,5 +1,6 @@
 #include "local/stir/listmode/TimeFrameDefinitions.h"
 #include <iostream>
+
 #ifndef STIR_NO_NAMESPACES
 using std::cerr;
 using std::endl;
@@ -16,18 +17,18 @@ main(int argc, char* argv[])
     cerr << "Usage:" << argv[0] << " Frame def filename, Frame number " << endl;
     return EXIT_FAILURE;
   }
-
-
+  
   TimeFrameDefinitions time_def(argv[1]);
   const int frame_num = atoi(argv[2]);
   const double start_frame = time_def.get_start_time(frame_num);
   const double end_frame = time_def.get_end_time(frame_num);
   double frame_duration = end_frame-start_frame;
 
-  cout << "Start frame " << start_frame << endl;
-  cout << "Frame duration " << frame_duration << endl;
+  cout << "Start frame :" << start_frame*1000 << endl;
+  cout << "Frame duration :" << frame_duration*1000 << endl;
 
 
 
   return EXIT_SUCCESS;
 }
+
