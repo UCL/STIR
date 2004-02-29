e794e2ba9fd94ae1f6df3243e7257f8158e56318
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-02-29 14:52:06 +0000

    The option for the NEMA Standard NU-2001 added

--- src/local/utilities/find_fwhm_in_image.cxx
+++ src/local/utilities/find_fwhm_in_image.cxx
@@ -4,26 +4,38 @@
 /*!
   \file
   \ingroup utilities
-  \brief List coordinates of maxima in the image
+  \brief List of FWHM and its location in the image
+  
 
-  \author Sanida Mustafovic
-  \author Kris Thielemans
   \author Charalampos Tsoumpas
+  \author Kris Thielemans
+  \author Sanida Mustafovic
 
   $Date$
   $Revision$
 
   \par Usage:
    \code
-   find_fwhm_in_image filename [num_maxima] [level] [dimension]
+   find_fwhm_in_image filename [num_maxima][level][dimension][nema]
    \endcode
    \param num_maxima defaults to 1
-   \if num_maxima more than 10, the [num_maxima] slices are printed into a file
-   \param level defaults to 2   
-   \param dimension can take the values of the axis at which the line source is along to.
-   \along z axis : 1 (default)
-   \along y axis : 2
-   \along x axis : 3          
+   \param level defaults to 2 (half maximum)  
+   \param dimension:
+   \for point sources (default) set to 0
+   \for line source along z, y, x -axis,
+   \set to: 1, 2, 3 respectively
+   \param NEMA defaults to 1 
+   
+  
+  If you have point sources, it prints the value of the [num_maxima] maximum point source with its location 
+  and the resolution at the three dimensions, sorting from the maximum to the minimum. If you have a line 
+  source, a text file is returned that it contains the maximum value at its one slice, which is sorted from
+  the mimimum to maximum slice index, at the wanted direction, with its location and the resolution at the 
+  three dimensions. The resolution at the axis of the line is set to be 0. If given as [num_maxima] less 
+  than the total slices it returns the results for some slices by sampling with the same step the total 
+  slices of the wanted dimension. The [nema] parameter enables the oportunity of using the NEMA Standards 
+  Publication NU 2-2001. If it is set to 0 the function estimates the FWHM using 3D interpolation, for 
+  closer approximation. 
 */
 /*
     Copyright (C) 2004- $Date$, Hammersmith Imanet
@@ -42,10 +54,7 @@
 #ifndef STIR_NO_NAMESPACES
 using std::endl;
 using std::cout;
-using std::cin;
 using std::cerr;
-using std::min;
-using std::max;
 using std::setw;
 #endif
 
@@ -54,104 +63,83 @@ int main(int argc, char *argv[])
 {         
   USING_NAMESPACE_STIR
   using namespace std;                                                                
-  if (argc< 2 || argc>5)
+  if (argc< 2 || argc>6)
   {
-    cerr << "Usage:" << argv[0] << " input_image [num_maxima] [level] [dimension]\n"
+    cerr << "Usage:" << argv[0] << " input_image [num_maxima][level][dimension][nema]\n"
 	       << "\tnum_maxima defaults to 1\n" 
          << "\tlevel of maximum defaults at half maximum: 2\n"  
-         << "\tfor point sources print less than 11 num_maxima\n"
-         << "\tfor line sources print more than 11 num_maxima\n"
-         << "\tline source along z-axis dimension=1 (default)\n"  
-         << "\tline source along y-axis dimension=2 \n"
-         << "\tline source along x-axis dimension=3 \n" ;         
+         << "\tfor point sources dimension set to 0 (default)\n"
+         << "\tfor line source along z, y, x -axis dimension set to 1, 2, 3 respectively:\n"
+         << "\tnema defaults to 1, NEMA Standards NU 2-2001 is enabled\n"
+         << "returns a file containing the resolutions\n\n";
+
     return EXIT_FAILURE;            
   } 
   const unsigned short num_maxima = argc>=3 ? atoi(argv[2]) : 1 ;
   const float level = argc>=4 ? atoi(argv[3]) : 2 ;  
-  const int dimension = argc>=5 ? atoi(argv[4]) : 1 ;  
+  const int dimension = argc>=5 ? atoi(argv[4]) : 0 ; 
+  const bool nema = argc>=6 ? atoi(argv[5]) : 1 ; 
   cerr << "Finding " << num_maxima << " maxima\n" ;    
-  shared_ptr< DiscretisedDensity<3,float> >  input_image_sptr = 
+  shared_ptr< DiscretisedDensity<3,float> >  input_image_sptr= 
   DiscretisedDensity<3,float>::read_from_file(argv[1]);
-  DiscretisedDensity<3,float>& input_image = *input_image_sptr;      
-  const DiscretisedDensityOnCartesianGrid <3,float>*  input_image_cartesian_ptr =
-  dynamic_cast< DiscretisedDensityOnCartesianGrid<3,float>*> (input_image_sptr.get());  
-  const bool is_cartesian_grid = input_image_cartesian_ptr!=0; 
-  CartesianCoordinate3D<float> grid_spacing;     
-  if (is_cartesian_grid)   																					
+  DiscretisedDensity<3,float>& input_image = *input_image_sptr;  
+  list<ResolutionIndex<3,float> > list_res_index = 
+  find_fwhm_in_image(input_image,num_maxima,level,dimension,nema);    
+  list<ResolutionIndex<3,float> >:: iterator current_iter=list_res_index.begin();    
+  if (dimension!=0)    
   {
-    grid_spacing = input_image_cartesian_ptr->get_grid_spacing(); 
-    list<ResolutionIndex<3,float> > list_res_index = 
-    find_fwhm_in_image(input_image,num_maxima,level,dimension);    
-    list<ResolutionIndex<3,float> >:: iterator current_iter=list_res_index.begin();
-  
-    if (num_maxima>10)    
+    string output_string;
+    string input_string(argv[1]);
+    string slices_string(argv[2]);             
+    string:: iterator string_iter;
+    for(string_iter=input_string.begin(); 
+        string_iter!=input_string.end() && *string_iter!='.' ;
+        ++string_iter)  
+    output_string.push_back(*string_iter);     
+    if (argc>=4)
     {
-      string output_string;
-      string input_string(argv[1]);
-      string slices_string(argv[2]);             
-      string:: iterator string_iter;
-      for(string_iter=input_string.begin(); 
-          string_iter!=input_string.end() && *string_iter!='.' ;
-          ++string_iter)  
-      output_string.push_back(*string_iter);     
-
-      if (argc>=4)
-      {
-         string level_string(argv[3]);
-         output_string +=  '_' + slices_string + "_slices_FW" + level_string + 'M' ;
-      }
-      else
-      output_string +=  '_' + slices_string + "_slices_FWHM" ;  
+       string level_string(argv[3]);
+       output_string +=  '_' + slices_string + "_slices_FW" + level_string + 'M' ;
+    }
+    else
+    output_string +=  '_' + slices_string + "_slices_FWHM" ;  
       
-      ofstream out(output_string.c_str()); //output file //
-      if(!out)
-      {
-        cout << "Cannot open text file.\n" ;
-        return EXIT_FAILURE;
-      }       
-      out << "Slice\t Z\tY\t X\tResZ(mm) ResY(mm) ResX(mm) Value\n"; //t Fitted Maximum Values \n";
-      for (short counter=0 ; counter!=num_maxima ; ++counter)
-      { 
-        out << setw(3) << counter+1 << "\t" 
-            << setw(3) << current_iter->voxel_location[1] << "\t"
-		        << setw(3) << current_iter->voxel_location[2] << "\t"
-            << setw(3) << current_iter->voxel_location[3] << "\t" 
-  	        << setw(6) << current_iter->resolution[1]*grid_spacing[1]<< "\t"
-        	  << setw(6) << current_iter->resolution[2]*grid_spacing[2]<< "\t"
-       	    << setw(6) << current_iter->resolution[3]*grid_spacing[3]<< "\t"
-            << setw(9) << current_iter->voxel_value << "\n" ;
-        //   << setw(9) << current_iter->real_maximum_value[2] << "\t"
-        //   << setw(9) << current_iter->real_maximum_value[3] << "\n";      
-        ++current_iter;         
-      }
-      out.close();  
-      return EXIT_SUCCESS;
-    } 
-    else 
+    ofstream out(output_string.c_str()); //output file //
+    if(!out)
+    {
+      cout << "Cannot open text file.\n" ;
+      return EXIT_FAILURE;
+    }       
+    out << "Slice\t Z\tY\t X\tResZ(mm) ResY(mm) ResX(mm) Value\n"; 
+    for (short counter=0 ; counter!=num_maxima ; ++counter)
+    { 
+      out << setw(3) << counter+1 << "\t" 
+          << setw(3) << current_iter->voxel_location[1] << "\t"
+          << setw(3) << current_iter->voxel_location[2] << "\t"
+          << setw(3) << current_iter->voxel_location[3] << "\t" 
+ 	        << setw(6) << current_iter->resolution[1]<< "\t"
+       	  << setw(6) << current_iter->resolution[2]<< "\t"
+     	    << setw(6) << current_iter->resolution[3]<< "\t"
+          << setw(9) << current_iter->voxel_value << "\n" ;
+      ++current_iter;         
+    }
+    out.close();       
+  } 
+  else 
     for (short counter=0 ; counter!=num_maxima ; ++counter)
     {
       cout << counter+1 << ". max: " << setw(6)	<<  current_iter->voxel_value	  
 	         << " at: "  << setw(3) << current_iter->voxel_location[1]
 		       << " (Z) ," << setw(3) << current_iter->voxel_location[2]
            << " (Y) ," << setw(3) << current_iter->voxel_location[3] << " (X) \n" ;
-		  cout << "which is "
-		       << ':' << setw(6) << (current_iter->voxel_location[1])*grid_spacing[1]
-			     << ',' << setw(6) << (current_iter->voxel_location[2])*grid_spacing[2]
-				   << ',' << setw(6) << (current_iter->voxel_location[3])*grid_spacing[3]
-					 << "  in mm relative to origin";
   	  cout << "  \n The resolution in z axis is "
-           << setw(6) << (current_iter->resolution[1])*grid_spacing[1]
+           << setw(6) << (current_iter->resolution[1])
        	   << ", \n The resolution in y axis is "
-     	     << setw(6) << (current_iter->resolution[2])*grid_spacing[2]
+     	     << setw(6) << (current_iter->resolution[2])
            << ", \n The resolution in x axis is "
-     	     << setw(6) << (current_iter->resolution[3])*grid_spacing[3]
+     	     << setw(6) << (current_iter->resolution[3])
            << ", in mm relative to origin. \n \n";  
       ++current_iter;         
-    }        
-  }      
-  else
-  { 
-    cout << "THERE IS NOT A CARTESIAN GRID\n";
-    return EXIT_FAILURE;  
-  }
+    }                
+  return EXIT_SUCCESS;
 }                 
