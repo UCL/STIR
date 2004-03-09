b19e7bdb3039baff05f6d125c8bbc7c27f0e3884
Kris Thielemans <Kris.Thielemans@stir>
2004-03-09 12:33:24 +0000

    - renamed radius_a/b/c to radius_x/y/z - added post_processing()

--- src/local/Shape_buildblock/Ellipsoid.cxx
+++ src/local/Shape_buildblock/Ellipsoid.cxx
@@ -12,29 +12,81 @@
   $Revision$
 */
 /*
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet
     See STIR/LICENSE.txt for details
 */
 #include "local/stir/Shape/Ellipsoid.h"
 
 START_NAMESPACE_STIR
 
+const char * const 
+Ellipsoid::registered_name = "Ellipsoid";
+
+
+void 
+Ellipsoid::initialise_keymap()
+{
+  parser.add_start_key("Ellipsoid Parameters");
+  parser.add_key("radius-x (in mm)", &radius_x);
+  parser.add_key("radius-y (in mm)", &radius_y);
+  parser.add_key("radius-z (in mm)", &radius_z);
+  parser.add_stop_key("END");
+  Shape3DWithOrientation::initialise_keymap();
+}
+
+
+
+void
+Ellipsoid::set_defaults()
+{  
+  Shape3DWithOrientation::set_defaults();
+  radius_x=0;
+  radius_y=0;
+  radius_z=0;
+}
+
+
+bool
+Ellipsoid::
+post_processing()
+{
+  if (Shape3DWithOrientation::post_processing()==true)
+    return true;
+
+  if (radius_x <= 0)
+    {
+      warning("radius_x should be positive, but is %g\n", radius_x);
+      return true;
+    }
+  if (radius_y <= 0)
+    {
+      warning("radius_y should be positive, but is %g\n", radius_y);
+      return true;
+    }
+  if (radius_z <= 0)
+    {
+      warning("radius_z should be positive, but is %g\n", radius_z);
+      return true;
+    }
+  return false;
+}
+
 Ellipsoid::Ellipsoid()
 {
   set_defaults();
 }
 
-Ellipsoid::Ellipsoid(const float radius_av, 
-                      const float radius_bv,
-	              const float radius_cv,
+Ellipsoid::Ellipsoid(const float radius_xv, 
+                      const float radius_yv,
+	              const float radius_zv,
 	              const CartesianCoordinate3D<float>& centre_v,
 		      const float alpha_v,
 		      const float beta_v,                     
 		      const float gamma_v) 
                      ://Shape3DWithOrientation(centre_v,alpha_v,beta_v,gamma_v),
-                     radius_a(radius_av),
-		     radius_b(radius_bv),
-		     radius_c(radius_cv)		    
+                     radius_x(radius_xv),
+		     radius_y(radius_yv),
+		     radius_z(radius_zv)		    
                           
 {
   origin = centre_v;
@@ -43,17 +95,17 @@ Ellipsoid::Ellipsoid(const float radius_av,
 
  // dir_a, dir_b, dir_c must be orthogonal to each other
 
-Ellipsoid::Ellipsoid(const float radius_av, 
-                     const float radius_bv,
-	             const float radius_cv,
+Ellipsoid::Ellipsoid(const float radius_xv, 
+                     const float radius_yv,
+	             const float radius_zv,
 	             const CartesianCoordinate3D<float>& centre_v,
 	             const CartesianCoordinate3D<float>& dir_av,
 		     const CartesianCoordinate3D<float>& dir_bv,
 		     const CartesianCoordinate3D<float>& dir_cv) 
                     : //Shape3DWithOrientation(centre_v,dir_av,dir_bv,dir_cv),
-		     radius_a(radius_av),
-		     radius_b(radius_bv),
-		     radius_c(radius_cv)
+		     radius_x(radius_xv),
+		     radius_y(radius_yv),
+		     radius_z(radius_zv)
 {
   origin = centre_v;
   dir_x = dir_av;
@@ -64,22 +116,22 @@ Ellipsoid::Ellipsoid(const float radius_av,
 
 float Ellipsoid::get_geometric_volume() const
  {
-   return ((4*radius_a*radius_b*radius_c*_PI)/3);
+   return ((4*radius_x*radius_y*radius_z*_PI)/3);
  }
 
 
 bool Ellipsoid::is_inside_shape(const CartesianCoordinate3D<float>& index) const
 
 {
-  assert(radius_a > 0);
-  assert(radius_b > 0);
-  assert(radius_c > 0);
+  assert(radius_x > 0);
+  assert(radius_y > 0);
+  assert(radius_z > 0);
 
   CartesianCoordinate3D<float> r = index - origin;
   
-   if ((square(inner_product(r,dir_x))/square(radius_a)+ 
-        square(inner_product(r,dir_y))/square(radius_b)+
-        square(inner_product(r,dir_z))/square(radius_c))<=1)
+   if ((square(inner_product(r,dir_x))/square(radius_x)+ 
+        square(inner_product(r,dir_y))/square(radius_y)+
+        square(inner_product(r,dir_z))/square(radius_z))<=1)
       return true;    
    else 
       return false;
@@ -92,30 +144,5 @@ Shape3D* Ellipsoid:: clone() const
 }
 
 
-void 
-Ellipsoid::initialise_keymap()
-{
-  parser.add_start_key("Ellipsoid Parameters");
-  parser.add_key("radius-x (in mm)", &radius_a);
-  parser.add_key("radius-y (in mm)", &radius_b);
-  parser.add_key("radius-z (in mm)", &radius_c);
-  parser.add_stop_key("END");
-  Shape3DWithOrientation::initialise_keymap();
-}
-
-
-
-void
-Ellipsoid::set_defaults()
-{  
-  Shape3DWithOrientation::set_defaults();
-  radius_a=0;
-  radius_b=0;
-  radius_c=0;
-}
-
-
-const char * const 
-Ellipsoid::registered_name = "Ellipsoid";
 
 END_NAMESPACE_STIR
