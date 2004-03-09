3f0fc4950995fe93605311996a6608800fa5e430
Kris Thielemans <Kris.Thielemans@stir>
2004-03-09 12:29:57 +0000

    - renamed radius_a/b/c to radius_x/y/z - added post_processing() - more doxygen

--- src/include/local/stir/Shape/Ellipsoid.h
+++ src/include/local/stir/Shape/Ellipsoid.h
@@ -7,12 +7,30 @@
 
   \brief Declaration of class Ellipsoid
 
+  \par Description
+  A point with coordinates \a coord is inside the shape if for
+  \f$r = coord - origin\f$:
+  \f[
+  {(r.dir_x)^2 \over R_y^2} + {(r.dir_y)^2 \over R_y^2}+ + {(r.dir_z)^2 \over R_z^2} <= 1
+  \f]
+  where \f$dir_x, dir_y, dir_z\f$ are described in the documentation for class
+  Shape3DWithOrientation.
+
+  \par Parameters
+  \verbatim
+      Ellipsoidal Cylinder Parameters:=
+     radius-x (in mm):= <float>
+     radius-y (in mm):= <float>
+     radius-z (in mm):= <float>
+     ; any parameters of Shape3DWithOrientation
+     End:=
+  \endverbatim
   \author Sanida Mustafovic
   $Date$
   $Revision$
 */
 /*
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
 #ifndef __stir_Shape_Ellipsoid_h__
@@ -22,9 +40,6 @@
 #include "stir/RegisteredParsingObject.h"
 #include "local/stir/Shape/Shape3DWithOrientation.h"
 
-
-
-
 START_NAMESPACE_STIR
 
 /*!
@@ -32,8 +47,8 @@ START_NAMESPACE_STIR
   \brief Three-dimensional ellipsoid
   
    Ellipsoid with the dimensions 
-  (radius_a,radius_b,radius_c), where radius_a assumed to be
-   in x direction, radius_b in y direction,radius_c in z-direction,
+  (radius_x,radius_y,radius_z), where radius_x assumed to be
+   in x direction, radius_y in y direction,radius_z in z-direction,
    before any rotation with Euler angles.
 */
 class Ellipsoid: 
@@ -44,16 +59,16 @@ public:
   static const char * const registered_name; 
 
  Ellipsoid();
- Ellipsoid( const float radius_a,
-	    const float radius_b,
-	    const float radius_c,
+ Ellipsoid( const float radius_x,
+	    const float radius_y,
+	    const float radius_z,
 	    const CartesianCoordinate3D<float>& centre,
 	    const float alpha,
 	    const float beta,
 	    const float gamma); 
- Ellipsoid(  const float radius_a, 
-             const float radius_b,
-	     const float radius_c,
+ Ellipsoid(  const float radius_x, 
+             const float radius_y,
+	     const float radius_z,
 	     const CartesianCoordinate3D<float>& centre,
              const CartesianCoordinate3D<float>& dir_x,
              const CartesianCoordinate3D<float>& dir_y,
@@ -63,13 +78,17 @@ public:
 
   Shape3D* clone() const;
 
+protected:
+  //! Radius in x-direction if the shape is not rotated
+  float radius_x;
+  //! Radius in y-direction if the shape is not rotated
+  float radius_y;
+  //! Radius in z-direction if the shape is not rotated
+  float radius_z;
 private:
-  float radius_a;
-  float radius_b;
-  float radius_c;
-
   virtual void set_defaults();  
   virtual void initialise_keymap();
+  virtual bool post_processing();
   
 };
 
