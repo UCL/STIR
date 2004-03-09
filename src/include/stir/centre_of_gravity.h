a7447bd68b033807fc94114a17b8d9acf512977c
Kris Thielemans <Kris.Thielemans@stir>
2004-03-09 12:21:41 +0000

    - first version in archive

---
+++ src/include/stir/centre_of_gravity.h
@@ -0,0 +1,72 @@
+//
+// $Id$
+//
+/*!
+  \file 
+  \ingroup Array
+ 
+  \brief This file contains functions to compute the centre of gravity of
+  arrays and images.
+  \author Kris Thielemans
+  $Date$
+  $Revision$
+*/
+/*
+    Copyright (C) 2003- $Date$, Hammersmith Imanet
+    See STIR/LICENSE.txt for details
+*/
+
+
+#include "stir/common.h"
+
+START_NAMESPACE_STIR
+// predeclerations to avoid having to include the files and create unnecessary
+// dependencies
+template <int num_dimensions, class T> class BasicCoordinate;
+template <int num_dimensions, class T> class Array;
+template <class elemT> class VectorWithOffset;
+template <class coordT> class CartesianCoordinate3D;
+template <class elemT> class VoxelsOnCartesianGrid;
+
+
+//! Compute centre of gravity of an Array but without dividing by its sum
+/*! Each coordinate of the unweighted centre of gravity is computed as follows:
+   \f[
+      C_k = sum_{i_1...i_n} i_k A_{i1...i_n}
+   \f]
+*/
+template <int num_dimensions, class T>
+BasicCoordinate<num_dimensions,T> 
+find_unweighted_centre_of_gravity(const Array<num_dimensions,T>& );
+
+//! Compute centre of gravity of an Array
+/*! Calls find_unweighted_centre_of_gravity and divides the result with the
+    sum of all the elements in the array.
+
+    \warning When the sum is 0, error() is called.
+    \todo better error handling
+*/
+template <int num_dimensions, class T>
+BasicCoordinate<num_dimensions,T> 
+find_centre_of_gravity(const Array<num_dimensions,T>& );
+
+//! Computes centre of gravity for each plane
+/*!
+  The result is in mm, taking the origin (and min/max indices) into account.
+  Results are in standard STIR coordinate system.
+
+  The result can be used to find the central line of a (uniform) object, for
+  instance a cylinder. The output of this program can by used by
+  do_linear_regression.
+
+  The weight is currently simply the sum of the voxel values in that plane,
+  thresholded to be at least 0. If the weight is 0, the x,y coordinates are
+  simply set to 0.
+ */
+template <class T>
+void
+find_centre_of_gravity_in_mm_per_plane(  VectorWithOffset< CartesianCoordinate3D<float> >& allCoG,
+					 VectorWithOffset<T>& weights,
+					 const VoxelsOnCartesianGrid<T>& image);
+
+END_NAMESPACE_STIR
