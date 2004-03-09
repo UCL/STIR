4c3f67b28ee6ccc00793711827d50959854ed1a5
Kris Thielemans <Kris.Thielemans@stir>
2004-03-09 12:29:32 +0000

    - renamed radius_a/b to radius_x,y - added post_processing() - more doxygen

--- src/include/local/stir/Shape/EllipsoidalCylinder.h
+++ src/include/local/stir/Shape/EllipsoidalCylinder.h
@@ -7,12 +7,35 @@
 
   \brief Declaration of class EllipsoidalCylinder
 
+  \par Description
+  A point with coordinates \a coord is inside the shape if for
+  \f$r = coord - origin\f$:
+  \f[
+  |r.dir_z|<L/2
+  \f]
+  and
+  \f[
+  {(r.dir_x)^2 \over R_y^2} + {(r.dir_y)^2 \over R_y^2} <= 1
+  \f]
+  where \f$dir_x, dir_y, dir_z\f$ are described in the documentation for class
+  Shape3DWithOrientation.
+
+  \par Parameters
+  \verbatim
+      Ellipsoidal Cylinder Parameters:=
+     radius-x (in mm):= <float>
+     radius-y (in mm):= <float>
+     length-z (in mm):= <float>
+     ; any parameters of Shape3DWithOrientation
+     End:=
+  \endverbatim
   \author Sanida Mustafovic
+  \author Kris Thielemans
   $Date$
   $Revision$
 */
 /*
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
 
@@ -35,8 +58,8 @@ public:
   EllipsoidalCylinder();
 
   EllipsoidalCylinder(const float length, 
-                      const float radius_a,
-                      const float radius_b,
+                      const float radius_x,
+                      const float radius_y,
                       const CartesianCoordinate3D<float>& centre,
                       const CartesianCoordinate3D<float>& dir_x,
                       const CartesianCoordinate3D<float>& dir_y,
@@ -44,31 +67,36 @@ public:
   
   
   EllipsoidalCylinder(const float length, 
-                      const float radius_a,
-                      const float radius_b,
+                      const float radius_x,
+                      const float radius_y,
                       const CartesianCoordinate3D<float>& centre,
                       const float alpha,
                       const float beta,
                       const float gamma); 
+  Shape3D* clone() const; 
   
-  inline void scale(const CartesianCoordinate3D<float>& scale3D);
-  inline float get_geometric_volume() const;
-  bool is_inside_shape(const CartesianCoordinate3D<float>& index) const;
-  
-  inline Shape3D* clone() const; 
-  
-private:
 
+  //! Scale the cylinder
+  /*! \todo This cannot handle a rotated cylinder yet. Instead, it will call error(). */
+  void scale(const CartesianCoordinate3D<float>& scale3D);
+  float get_geometric_volume() const;
+  bool is_inside_shape(const CartesianCoordinate3D<float>&) const;
+  
+protected:
+
+  //! Length of the cylinder
   float length;
-  float radius_a;
-  float radius_b;
+  //! Radius in x-direction if the shape is not rotated
+  float radius_x;
+  //! Radius in y-direction if the shape is not rotated
+  float radius_y;
 
+private:
   virtual void set_defaults();  
   virtual void initialise_keymap();    
+  virtual bool post_processing();
 };
 
 END_NAMESPACE_STIR
-
-#include "local/stir/Shape/EllipsoidalCylinder.inl"
 
 #endif
