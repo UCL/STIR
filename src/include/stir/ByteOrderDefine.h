90cbb7aa094b0e09f4fbc427cf2aefc36d2dd308
Kris Thielemans <Kris.Thielemans@stir>
2004-03-19 15:57:23 +0000

    - WARNING: use new convention for preprocessor symbols. Check for   #if STIRIsNativeByteOrder... instead of some #ifdef. The old   convention was too dangerous (any misspelling would result in   incorrect code)

--- src/include/stir/ByteOrderDefine.h
+++ src/include/stir/ByteOrderDefine.h
@@ -21,27 +21,31 @@
 
 START_NAMESPACE_STIR
 
-#ifndef DOXYGEN_SKIP
-// define both if them when doxygen is run
-// otherwise doxygen will only generate the doc for the one that is actually defined
-
-#define STIRNativeByteOrderIsBigEndian
-#define STIRNativeByteOrderIsLittleEndian
-#endif
 
 /*!
-  \def  STIRNativeByteOrderIsBigEndian
+  \def  STIRIsNativeByteOrderIsBigEndian
   \ingroup buildblock  
-  \brief A macro that is defined \e only when the compilation is on
-  a big endian machine.
+  \brief A macro that is defined to 1 when the compilation is on
+  a big endian machine, otherwise it is set to 0. 
+
+  \par Usage
+  \code
+  #if STIRIsNativeByteOrderIsBigEndian
+  // code specific for big endian machines
+  #else
+  // code for little endian machines
+  #endif
+
+  \par Relation to the ByteOrder class.
 
   This should be used only in the case that you really need to know this
   at compilation time. Try to use class ByteOrder instead.
 
   The definition of class ByteOrder is independent of this macro.
 
-  \warning It is recommended to check these settings by using an assert on 
-  ByteOrder.
+  \warning It is recommended to check these settings by comparing with class 
+  ByteOrder. This is done by the ByteOrderTests class.
+
   \warning This macro depends on preprocessor defines set by your compiler.
   The list of defines is bound to be incomplete though. The current list sets
   LittleEndian for alpha, x86 or PowerPC processors or Windows using some
@@ -49,22 +53,27 @@ START_NAMESPACE_STIR
   with CodeWarrior). Otherwise, the
   architecture is assumed to be big endian. ByteOrder does not rely on 
   this type of conditionals.
+  \warning   The value of the macro in the doxygen documentation depends on 
+  what type of machine doxygen was run.
+
 */
 
 /*!
-  \def  STIRNativeByteOrderIsLittleEndian
+  \def  STIRIsNativeByteOrderLittleEndian
   \ingroup buildblock  
-  \brief A macro that is defined \e only when the compilation is on
-  a little endian machine.
+  \brief A macro that is defined to 1 when the compilation is on
+  a little endian machine, and to 0 otherwise.
 
-  \see STIRNativeByteOrderIsBigEndian for details.
+  \see STIRIsNativeByteOrderBigEndian for details.
 */
 
 // currently checked by asserts()
 #if !defined(__alpha) && (!defined(_WIN32) || defined(_M_PPC) || defined(_M_MPPC)) && !defined(__i386__) && !defined(__i486__) && !defined(__i586__) && !defined(__i686__) && !defined(__i786__)&& !defined(__i886__) && !defined(__k6__) && !defined(__athlon__) || (defined(__MSL__) && !defined(__LITTLE_ENDIAN))
-#define STIRNativeByteOrderIsBigEndian 1
+#define STIRIsNativeByteOrderBigEndian 1
+#define STIRIsNativeByteOrderLittleEndian 0
 #else
-#define STIRNativeByteOrderIsLittleEndian 1
+#define STIRIsNativeByteOrderBigEndian 0
+#define STIRIsNativeByteOrderLittleEndian 1
 #endif
 END_NAMESPACE_STIR
 
