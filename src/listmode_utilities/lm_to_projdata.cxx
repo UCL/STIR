acfc21a200d3ec4e71b3154e9d46e83556691ca8
Kris Thielemans <Kris.Thielemans@stir>
2004-03-19 14:56:02 +0000

    - i snow in global distro

---
+++ src/listmode_utilities/lm_to_projdata.cxx
@@ -0,0 +1,43 @@
+//
+// $Id$
+//
+/*!
+  \file 
+  \ingroup utilities
+
+  \brief Program to bin listmode data to 3d sinograms
+ 
+  \author Kris Thielemans
+  \author Sanida Mustafovic
+  
+  $Date$
+  $Revision $
+*/
+/*
+    Copyright (C) 2000- $Date$, IRSL
+    See STIR/LICENSE.txt for details
+*/
+
+#include "local/stir/listmode/LmToProjData.h"
+
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+#endif
+
+USING_NAMESPACE_STIR
+
+
+
+int main(int argc, char * argv[])
+{
+  
+  if (argc!=1 && argc!=2) {
+    cerr << "Usage: " << argv[0] << " [par_file]\n";
+    exit(EXIT_FAILURE);
+  }
+  LmToProjData application(argc==2 ? argv[1] : 0);
+  application.process_data();
+
+  return EXIT_SUCCESS;
+}
+
