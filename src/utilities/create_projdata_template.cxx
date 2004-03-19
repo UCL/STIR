7b5a8d9c8428d9fb7fa3abf1a60b43cd1b841859
Kris Thielemans <Kris.Thielemans@stir>
2004-03-19 09:45:04 +0000

    - first version in archve

---
+++ src/utilities/create_projdata_template.cxx
@@ -0,0 +1,58 @@
+//
+// $Id$
+//
+/*!
+  \file
+  \ingroup utilities
+
+  \brief A utility that creates a template projection data.
+  \par Usage
+  \verbatim
+  create_projdata_template output_filename
+  \endverbatim
+  This will ask questions to the user about the scanner, the data size,
+  etc. It will then output new projection data (in Interfile format).
+  However, the binary file will not contain any data.
+
+  This utility is mainly useful to create a template that can then
+  be used for other STIR utilities (such as fwdtest, lm_to_projdata etc.).
+
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+/*
+    Copyright (C) 2004- $Date$, Hammersmith Imanet Ltd
+    See STIR/LICENSE.txt for details
+*/
+
+#include "stir/ProjDataInterfile.h"
+#include "stir/ProjDataInfo.h"
+
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+#endif
+
+
+USING_NAMESPACE_STIR
+
+int main(int argc, char *argv[])
+{ 
+  
+  if(argc!=2) 
+  {
+    cerr<<"Usage: " << argv[0] << " output_filename\n";
+    return EXIT_FAILURE;
+  }
+
+
+  shared_ptr<ProjDataInfo> proj_data_info_ptr = 
+    ProjDataInfo::ask_parameters();
+  
+  const string output_file_name = argv[1];
+  shared_ptr<ProjData> proj_data_ptr =
+    new ProjDataInterfile(proj_data_info_ptr, output_file_name);
+
+  return EXIT_SUCCESS;
+}
