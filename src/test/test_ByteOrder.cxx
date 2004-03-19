6891f84bbb04acc35e2221a75155f0321833ba07
Kris Thielemans <Kris.Thielemans@stir>
2004-03-19 16:06:48 +0000

    - first version in archive

---
+++ src/test/test_ByteOrder.cxx
@@ -0,0 +1,81 @@
+// $Id$
+/*!
+
+  \file
+  \ingroup test
+
+  \brief Test program for ByteOrder and ByteOrderDefine.h
+
+  \author Kris Thielemans
+  \author PARAPET project
+
+  $Date$
+
+  $Revision$
+*/
+/*
+    Copyright (C) 2004- $Date$, Hammersmith Imanet Ltd
+    See STIR/LICENSE.txt for details
+*/
+
+#include "stir/ByteOrder.h"
+#include "stir/ByteOrderDefine.h"
+#include "stir/RunTests.h"
+
+#include <iostream>
+
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+using std::endl;
+#endif
+
+START_NAMESPACE_STIR
+
+
+/*!
+  \brief Test class for ByteOrder and the preprocessor defines from
+  ByteOrderDefine.h
+  \ingroup test
+*/
+class ByteOrderTests : public RunTests
+{
+public:
+  void run_tests();
+};
+
+void
+ByteOrderTests::run_tests()
+{
+  cerr << "Tests for ByteOrder\n"
+       << "Everythings is fine if the program runs without any output." << endl;
+  // if any of these is wrong, check ByteOrderDefine.h
+
+#if STIRIsNativeByteOrderBigEndian
+  check(ByteOrder::get_native_order() == ByteOrder::big_endian,
+	"STIRIsNativeByteOrderBigEndian preprocessor define is determined incorrectly.");
+#else
+  check(ByteOrder::get_native_order() == ByteOrder::little_endian,
+	"STIRIsNativeByteOrderBigEndian preprocessor define is determined incorrectly.");
+#endif
+
+#if STIRIsNativeByteOrderLittleEndian
+  check(ByteOrder::get_native_order() == ByteOrder::little_endian,
+	"STIRIsNativeByteOrderBigEndian preprocessor define is determined incorrectly.");
+#else
+  check(ByteOrder::get_native_order() == ByteOrder::big_endian,
+	"STIRIsNativeByteOrderBigEndian preprocessor define is determined incorrectly.");
+#endif
+
+}
+
+
+END_NAMESPACE_STIR
+
+USING_NAMESPACE_STIR
+
+int main()
+{
+  ByteOrderTests tests;
+  tests.run_tests();
+  return tests.main_return_value();
+}
