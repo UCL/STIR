664146bb57dfd80645b0d75082bdcc9b98854a31
Kris Thielemans <Kris.Thielemans@stir>
2004-03-19 16:09:27 +0000

    - added get_bin(LOR&)

--- src/buildblock/ProjDataInfoCylindricalArcCorr.cxx
+++ src/buildblock/ProjDataInfoCylindricalArcCorr.cxx
@@ -19,11 +19,14 @@
 */
 /*
     Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
 
 #include "stir/ProjDataInfoCylindricalArcCorr.h"
+#include "stir/Bin.h"
+#include "stir/round.h"
+#include "stir/LORCoordinates.h"
 #ifdef BOOST_NO_STRINGSTREAM
 #include <strstream.h>
 #else
@@ -52,7 +55,6 @@ ProjDataInfoCylindricalArcCorr:: ProjDataInfoCylindricalArcCorr(const shared_ptr
 								bin_size(bin_size_v)								
 								
 {}
-
 
 
 void
@@ -85,5 +87,64 @@ ProjDataInfoCylindricalArcCorr::parameter_info()  const
   return s.str();
 }
 
+
+Bin
+ProjDataInfoCylindricalArcCorr::
+get_bin(const LOR<float>& lor) const
+
+{
+  Bin bin;
+  LORInAxialAndSinogramCoordinates<float> lor_coords;
+  if (lor.change_representation(lor_coords, get_ring_radius()) == Succeeded::no)
+    {
+      bin.set_bin_value(-1);
+      return bin;
+    }
+
+  // first find view 
+  // unfortunately, phi ranges from [0,Pi[, but the rounding can
+  // map this to a view which corresponds to Pi anyway.
+  bin.view_num() = round(lor_coords.phi() / get_azimuthal_angle_sampling());
+  assert(bin.view_num()>=0);
+  assert(bin.view_num()<=get_num_views());
+  const bool swap_direction =
+    bin.view_num() > get_max_view_num();
+
+  int ring1, ring2;
+  if (!swap_direction)
+    {
+      ring1 = round(lor_coords.z1()/get_ring_spacing());
+      ring2 = round(lor_coords.z2()/get_ring_spacing());
+    }
+  else
+    {
+      ring2 = round(lor_coords.z1()/get_ring_spacing());
+      ring1 = round(lor_coords.z2()/get_ring_spacing());
+    }
+
+  if (!(ring1 >=0 && ring1<get_scanner_ptr()->get_num_rings() &&
+	ring2 >=0 && ring2<get_scanner_ptr()->get_num_rings() &&
+	get_segment_axial_pos_num_for_ring_pair(bin.segment_num(),
+						bin.axial_pos_num(),
+						ring1,
+						ring2) == Succeeded::yes)
+      )
+    {
+      bin.set_bin_value(-1);
+      return bin;
+    }
+
+  bin.tangential_pos_num() = round(lor_coords.s() / get_tangential_sampling());
+  if (swap_direction)
+    bin.tangential_pos_num() *= -1;
+
+  if (bin.tangential_pos_num() < get_min_tangential_pos_num() ||
+      bin.tangential_pos_num() > get_max_tangential_pos_num())
+    {
+      bin.set_bin_value(-1);
+    }
+
+  return bin;
+}
 END_NAMESPACE_STIR
 
