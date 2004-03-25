fa74bc4dcb35b1ce5810a95b4643fba55fc248fd
Kris Thielemans <Kris.Thielemans@stir>
2004-03-25 11:41:17 +0000

    - first version in archive

---
+++ src/iterative/LogLik_buildblock/lib.mk
@@ -0,0 +1,19 @@
+#
+# $Id$
+#
+
+dir := iterative/LogLik_buildblock
+
+
+$(dir)_LIB_SOURCES= \
+LogLikelihoodBasedReconstruction.cxx \
+common.cxx 
+
+include $(WORKSPACE)/lib.mk
+
+
+
+
+
+
+
---
+++ src/iterative/OSMAPOSL/lib.mk
@@ -0,0 +1,14 @@
+#
+# $Id$
+#
+dir := iterative/OSMAPOSL
+
+$(dir)_LIB_SOURCES =  \
+         OSMAPOSLReconstruction.cxx 
+
+
+include $(WORKSPACE)/lib.mk
+
+
+
+
