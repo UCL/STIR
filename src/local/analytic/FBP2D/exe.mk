028dcf767637c075980f639d74fc20df604bdb87
Kris Thielemans <Kris.Thielemans@stir>
2004-03-30 09:38:37 +0000

    - first version in archive

---
+++ src/local/analytic/FBP2D/exe.mk
@@ -0,0 +1,10 @@
+#
+# $Id$
+#
+
+dir:=local/analytic/FBP2D
+
+$(dir)_SOURCES = \
+	FBP2DMain.cxx
+
+include $(WORKSPACE)/exe.mk
---
+++ src/local/analytic/FBP3DRP/exe.mk
@@ -0,0 +1,10 @@
+#
+# $Id$
+#
+
+dir:=local/analytic/FBP3DRP
+
+$(dir)_SOURCES = \
+	FBP3DRPMain.cxx
+
+include $(WORKSPACE)/exe.mk
