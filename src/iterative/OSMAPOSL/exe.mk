42314bfb0294504640d637bfd0141875a9b4f23b
Kris Thielemans <Kris.Thielemans@stir>
2004-03-30 15:43:30 +0000

    - remvoed warning about conflicts as no longer relevant

--- src/iterative/OSMAPOSL/exe.mk
+++ src/iterative/OSMAPOSL/exe.mk
@@ -6,7 +6,6 @@ dir := iterative/OSMAPOSL
 $(dir)_SOURCES :=  \
          OSMAPOSL.cxx 
 
-$(warning Please ignore next warnings on overriding clean_iterative/OSMAPOSL target)
 include $(WORKSPACE)/exe.mk
 
 
