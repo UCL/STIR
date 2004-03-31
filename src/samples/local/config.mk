2ecdab9f87eee04f306274ca530a7b40d86092cf
Kris Thielemans <Kris.Thielemans@stir>
2004-03-31 10:45:19 +0000

    - updated comments, added INSTALL_PREFIX

--- src/samples/local/config.mk
+++ src/samples/local/config.mk
@@ -1,11 +1,13 @@
-# Sample file (by Kris Thielemans)
-# Use this file to modify defaults for some variables in Makefile_common
+# $Id$
+# Sample file for local/config.mk (by Kris Thielemans)
+# Use this file to modify defaults for some variables in the 'global' config.mk
 # Modify according to your own needs
 
 # set non-default location of Louvain la Neuve files 
-# This will (only) be used in utilities/Makefile to compile and link
-# ifheaders_for_ecat7
 LLN_INCLUDE_DIR=$(HOME)/lln/ecat
+
+# set non-default location where files will be installed
+INSTALL_PREFIX=/usr/local
 
 # This file only sets EXTRA_OPT
 # Changes w.r.t. defaults:
@@ -15,12 +17,12 @@ LLN_INCLUDE_DIR=$(HOME)/lln/ecat
 # for some systems, do more...
 ifeq ($(SYSTEM),LINUX)
 # our Linux system has Pentium III processors
-EXTRA_OPT=-g -Wall -march=i686
+EXTRA_OPT=-g -Wall -march=pentium3
 else
 ifeq ($(SYSTEM),CYGWIN)
 # our NT system has Pentium II processors
-# cygwin can't find X include files by default
-EXTRA_OPT=-g -Wall -march=i686 -I /usr/X11R6/include
+# maybe cygwin can't find X include files by default
+EXTRA_OPT=-g -Wall -march=pentium2 -I /usr/X11R6/include
 else
 ifeq ($(SYSTEM),CC)
 # our Parsytec CC system has gcc 2.8.1, so we don't use -Wall 
