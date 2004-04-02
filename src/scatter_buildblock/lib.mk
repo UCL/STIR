c951e816fed0d5aec0209dd3fb1cae052eb4733c
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-04-02 11:21:04 +0000

    cross_section.cxx is added

--- src/scatter_buildblock/lib.mk
+++ src/scatter_buildblock/lib.mk
@@ -5,7 +5,8 @@
 dir := local/scatter_buildblock
 $(dir)_LIB_SOURCES:= \
 	sample_scatter_points.cxx \
-	integral_scattpoint_det.cxx \
+	integral_scattpoint_det.cxx\ 
+	cross_section.cxx\
 	scatter_estimate_for_one_scatter_point.cxx\
 	scatter_estimate_for_all_scatter_points.cxx\
         scatter_viewgram.cxx
