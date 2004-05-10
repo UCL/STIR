d7049ba374e5046d851ead0357ea7395774a2c67
Kris Thielemans <Kris.Thielemans@stir>
2004-05-10 22:45:40 +0000

    - beginnings of allowing more planes, segments etc.  (currently only working when TOSCREEN is defined)

--- src/local/utilities/writePbv.cxx
+++ src/local/utilities/writePbv.cxx
@@ -7,6 +7,10 @@
   \ingroup utilities
 
   \brief File that writes a ProjMatrixByBin in a sparse ASCII format
+  (transposed) or to screen
+
+  \todo very preliminary. Lots of useful things could be asked instead of
+  hard-wired.
 
   \author Kris Thielemans
   
@@ -41,7 +45,14 @@ USING_NAMESPACE_STIR
 
 int main(int argc, char **argv)
 {
+  // TODO parse these?
+  // set when you want to see only coords in plane 0
 
+  bool only_plane_zero = false;
+  int start_segment_num=0;
+  int end_segment_num=0;
+  int start_axial_pos_num=0;
+  int end_axial_pos_num=45;
   if (argc!=3)
   {
     cerr << "Usage : " << argv[0] << "out_filename sample_proj_data_filename\n";
@@ -66,11 +77,20 @@ int main(int argc, char **argv)
   int xy_size = static_cast<int>(proj_data_info_ptr->get_num_tangential_poss()*zoom);
   xy_size = ask_num("Number of x,y pixels",3,xy_size*2,xy_size);
       
+  int z_size = 2*proj_data_info_ptr->get_scanner_ptr()->get_num_rings()-1;
+  z_size = ask_num("Number of z pixels",1,1000,z_size);
   const shared_ptr<DiscretisedDensity<3,float> > density_info_ptr =
-     new VoxelsOnCartesianGrid<float>(*proj_data_info_ptr,
-                                         zoom,
-                                         CartesianCoordinate3D<float>(0,0,0),
-                                         CartesianCoordinate3D<int>(-1,xy_size,xy_size));
+    new VoxelsOnCartesianGrid<float>(*proj_data_info_ptr,
+				     zoom,
+				     Coordinate3D<float>(0,0,0),
+				     Coordinate3D<int>(z_size,xy_size,xy_size));
+  const float z_origin = 
+    ask_num("Shift z-origin (in pixels)", 
+	    -density_info_ptr->get_length()/2,
+	    density_info_ptr->get_length()/2,
+             0)*
+    dynamic_cast<const VoxelsOnCartesianGrid<float>&>(*density_info_ptr).get_voxel_size().z();
+  density_info_ptr->set_origin(Coordinate3D<float>(z_origin,0,0));
 
   shared_ptr<ProjMatrixByBin> projmatrix_sptr;
   do 
@@ -89,12 +109,22 @@ int main(int argc, char **argv)
   const int xmin = min_range.x();
 
 #ifndef TOSCREEN
+  if (start_segment_num!=end_segment_num ||
+      start_axial_pos_num != end_axial_pos_num)
+    warning("Matrix Size will be wrong because more than 1 segment/axial_pos\n");
+  if (!only_first_plane)
+    warning("Column sizes will be wrong because more than 1 plane\n");
+
   output << proj_data_info_ptr->get_num_views()*proj_data_info_ptr->get_num_tangential_poss()
          << " "
-         << xsize*ysize
+         << xsize*ysize*(only_first_plane?1:density_info_ptr->get_length())
          << endl;
 #endif
 
+  for (int segment_num = start_segment_num; segment_num <= end_segment_num; ++segment_num)
+    for (int axial_pos_num = std::max(start_axial_pos_num,proj_data_info_ptr->get_min_axial_pos_num(segment_num));
+         axial_pos_num <= std::min(end_axial_pos_num,proj_data_info_ptr->get_max_axial_pos_num(segment_num));
+         ++axial_pos_num)
   for (int view_num = proj_data_info_ptr->get_min_view_num();
        view_num <= proj_data_info_ptr->get_max_view_num();
        ++view_num)
@@ -103,12 +133,16 @@ int main(int argc, char **argv)
          ++tang_pos_num)
     {
 #ifdef TOSCREEN
-      cout << "-------------- view " << view_num << ", tang_pos " <<tang_pos_num<<"----------------\n";
+      cout << "-------------- segment " << segment_num 
+	   << ", view " << view_num 
+	   << ", tang_pos " <<tang_pos_num
+	   << ", axial_pos " <<axial_pos_num
+	   <<"----------------\n";
 #endif
 
       ProjMatrixElemsForOneBin lor;
 
-      projmatrix_sptr->get_proj_matrix_elems_for_one_bin(lor,Bin(0,view_num, 0, tang_pos_num));
+      projmatrix_sptr->get_proj_matrix_elems_for_one_bin(lor,Bin(segment_num,view_num, axial_pos_num, tang_pos_num));
       // cout << lor.get_number_of_elements() << endl;
 
 #ifdef PBV3D
@@ -124,13 +158,14 @@ int main(int argc, char **argv)
            lor_elem_iter != lor.end();
            ++lor_elem_iter)
       {
-         if (lor_elem_iter->coord1()!=0)
+         if (only_plane_zero && lor_elem_iter->coord1()!=0)
            continue;
           ++elem_count;
 #ifdef TOSCREEN
          cout << lor_elem_iter->coord3() << ", " << lor_elem_iter->coord2() << ", " << lor_elem_iter->coord1()
             << " : " << lor_elem_iter->get_value() << endl;
 #else
+	 // TODO plane offset
          const int column_num = (lor_elem_iter->coord3() - xmin)*xsize+(lor_elem_iter->coord2() - ymin);
          assert(column_num>=0);
          assert(column_num<xsize*ysize);
