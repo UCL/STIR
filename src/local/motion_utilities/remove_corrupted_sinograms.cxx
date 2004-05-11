6fd93480c857d5ec8b623b4277aa01bd4363c4b9
Kris Thielemans <Kris.Thielemans@stir>
2004-05-11 13:54:21 +0000

    - corrected BUG: we have to make sure we cut an even number of sinograms.   Otherwise ProjDataInfoCylindrical currently has problems with get_m()   (and other functions?). - added check that the number of sinograms we cut in each segment   amount to a global shift in m.

--- src/local/motion_utilities/remove_corrupted_sinograms.cxx
+++ src/local/motion_utilities/remove_corrupted_sinograms.cxx
@@ -10,8 +10,18 @@
 
   This is a program that takes corrupted sinograms
   (normally after motion correction is applied) and cuts away planes that 
-  have too much missing data. The amount that is cut is determined by user 
-  in percentage).
+  have too much missing data. The amount that is cut is determined by the user 
+  in percentage of a total amount of bins in each efficiency sinogram.
+
+  Because of restrictions in ProjDataInfoCylindrical, we have to cut the same
+  number of sinograms in each segment. This is because get_m() et al rely
+  on the centre of the scanner to correspond to the middle sinogram. This 
+  results in potentially cutting valid data in the oblique segments..
+  
+  It would not be too difficult to change ProjDataInfoCylindrical to allow
+  different offsets, but then we need to write those into the Interfile 
+  headers etc.
+
 
   \author Sanida Mustafovic
   \author Kris Thielemans
@@ -31,6 +41,7 @@
 #include "stir/Sinogram.h"
 #include "stir/SegmentBySinogram.h"
 #include "stir/VectorWithOffset.h"
+#include "stir/Bin.h"
 
 #include <vector>
 #include <algorithm>
@@ -102,6 +113,28 @@ void check_for_corrupted_sinograms(int& axial_pos_to_remove_min,
       else
 	break;
     }	  
+
+  // check if any sinograms left
+  if (axial_pos_to_remove_max+axial_pos_to_remove_min >=
+      proj_data.get_num_axial_poss(segment_num))
+    return;
+
+  /* Now make sure that we cut an even number of sinograms. Otherwise current 
+     ProjDataInfoCylindrical has problems figuring out the offsets for get_m().
+  */
+  if ((axial_pos_to_remove_max+axial_pos_to_remove_min)%2 !=0)
+    {
+      const float percentage_corrupted_min =
+	get_percentage_corrupted(proj_data.get_sinogram(proj_data.get_min_axial_pos_num(segment_num)+axial_pos_to_remove_min, 0));
+      const float percentage_corrupted_max =
+	get_percentage_corrupted(proj_data.get_sinogram(proj_data.get_max_axial_pos_num(segment_num)-axial_pos_to_remove_min, 0));
+      if (percentage_corrupted_max>=percentage_corrupted_min)
+	++axial_pos_to_remove_max;
+      else
+	++axial_pos_to_remove_min;
+    }
+
+
 }
 
 int
@@ -150,6 +183,7 @@ main(int argc, char* argv[])
 				  tolerance_of_corruption);
 	   
    int number_of_axial_pos_to_remove = axial_pos_to_remove_min+axial_pos_to_remove_max;
+       
    record_of_number_of_planes_to_remove_min.push_back(axial_pos_to_remove_min);
    record_of_number_of_planes_to_remove_max.push_back(axial_pos_to_remove_max);
 
@@ -160,8 +194,10 @@ main(int argc, char* argv[])
      proj_data_info_ptr->set_num_axial_poss_per_segment(new_num_axial_poss_per_segment);
    else
      {
-       error("%s: there are not enough axial positions even in segment 0\n",
+       warning("%s: there are not enough axial positions even in segment 0\n"
+	       "Too much motion for this procedure to work.\n",
 	     argv[0]);
+       exit(EXIT_FAILURE);
      }
    
    for (int segment_num=proj_data_info_ptr->get_min_segment_num();
@@ -191,7 +227,40 @@ main(int argc, char* argv[])
 	     2*proj_data_info_ptr->get_max_segment_num()+1,
 	     2*max_segment_num_to_process+1);
   }
- 
+
+  // check if everything was done consistently
+  {
+    /* We do this by checking if all m-coordinates are shifted with the 
+       same amount.
+    */
+    const ProjDataInfo& in_proj_data_info = 
+      *(in_projdata_ptr->get_proj_data_info_ptr());
+
+    const float m_difference_segment_0 = 
+      in_proj_data_info.get_m(Bin(0,0,0,0)) -
+      proj_data_info_ptr->get_m(Bin(0,0,0,0));
+    // variable used to scale differences in floating point comparison
+    const float reference_sampling_in_m =
+      in_proj_data_info.get_sampling_in_m(Bin(0,0,0,0));
+
+    for (int segment_num=proj_data_info_ptr->get_min_segment_num();
+	 segment_num<=proj_data_info_ptr->get_max_segment_num();
+	 ++segment_num)
+      {
+	const float m_difference = 
+	  in_proj_data_info.get_m(Bin(segment_num,0,0,0)) -
+	  proj_data_info_ptr->get_m(Bin(segment_num,0,0,0)) -
+	  m_difference_segment_0;
+	if (fabs(m_difference) > .001*reference_sampling_in_m)
+	  {
+	    error("remove_corrupted_sinograms: inconsistent shift in axial direction.\n"
+		  "At segment %d, shift w.r.t segment 0 of %g mm.\n"
+		  "Check code!\n",
+		  segment_num, 
+		  m_difference );
+	  }
+      }
+  }
   ProjDataInterfile out_projdata(proj_data_info_ptr, output_filename); 
 
   Succeeded succes = Succeeded::yes;
@@ -212,6 +281,9 @@ main(int argc, char* argv[])
       int num_axial_poss_to_remove_at_min_side = *iter_record_of_number_of_planes_to_remove_min;
       int num_axial_poss_to_remove_at_max_side = *iter_record_of_number_of_planes_to_remove_max;
 
+      // warning: current reconstruction script relies on the following messages
+      // appearing in the log file!
+
       cerr << " Number of planes to remove on the min side is " << num_axial_poss_to_remove_at_min_side << endl;
       cerr << " Number of planes to remove on the max side is " << num_axial_poss_to_remove_at_max_side << endl;
 
@@ -219,15 +291,13 @@ main(int argc, char* argv[])
       int ax_pos_num_out = out_segment.get_min_axial_pos_num();
       for (int ax_pos_num=(in_segment.get_min_axial_pos_num()+num_axial_poss_to_remove_at_min_side);
            ax_pos_num<=in_segment.get_max_axial_pos_num()-num_axial_poss_to_remove_at_max_side;
-       ++ax_pos_num)
+	   ++ax_pos_num, ++ax_pos_num_out)
        {
 	 // cerr << ax_pos_num << "  ";
 	 // note: the next line is ok even with different proj_data_info's
 	 // for each segment. The reason is that Array::operator[] 
 	 // and assignment is used, which ignores proj_data_info
 	 out_segment[ax_pos_num_out] = in_segment[ax_pos_num];
-	  ax_pos_num_out++;
-	
        }
        if (out_projdata.set_segment(out_segment) == Succeeded::no)
              succes = Succeeded::no;
