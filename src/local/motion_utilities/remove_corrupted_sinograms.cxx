f6e7f5e19d09b6758111fb01956da39dc5aa96ec
Kris Thielemans <Kris.Thielemans@stir>
2004-05-11 12:58:29 +0000

    - moved code that checks for corruption to a function - marginal reorganisation

--- src/local/motion_utilities/remove_corrupted_sinograms.cxx
+++ src/local/motion_utilities/remove_corrupted_sinograms.cxx
@@ -14,7 +14,7 @@
   in percentage).
 
   \author Sanida Mustafovic
-
+  \author Kris Thielemans
   $Date$
   $Revision$
 */
@@ -26,10 +26,10 @@
 
 #include "stir/ProjData.h"
 #include "stir/ProjDataInfo.h"
-#include "stir/SegmentBySinogram.h"
 #include "stir/ProjDataInterfile.h"
 #include "stir/Succeeded.h"
 #include "stir/Sinogram.h"
+#include "stir/SegmentBySinogram.h"
 #include "stir/VectorWithOffset.h"
 
 #include <vector>
@@ -49,75 +49,61 @@ USING_NAMESPACE_STIR
 
 
 
+// note: we ignore outer bins as they tend not to contribute to the image anyway
+static float
+get_percentage_corrupted(const Sinogram<float>& sinogram)
+{
+  int counter=0;
+  const int total_num_bins_sino = sinogram.get_num_views()*(sinogram.get_num_tangential_poss()-40);
+  for ( int view_num = sinogram.get_min_view_num();
+	view_num <= sinogram.get_max_view_num(); view_num++)
+    for ( int tang_pos = sinogram.get_min_tangential_pos_num()+20;
+	  tang_pos <= sinogram.get_max_tangential_pos_num()-20;
+	  tang_pos++)
+      {
+	const float bin = sinogram[view_num][tang_pos];
+	if (fabs(bin/0.1) < 3)
+	  counter ++;
+      }
+  return static_cast<float>(counter)/static_cast<float> (total_num_bins_sino)*100;
+}
 
 void check_for_corrupted_sinograms(int& axial_pos_to_remove_min,
 				   int& axial_pos_to_remove_max,
-				   const SegmentBySinogram<float>& in_eff_segment,
+				   const ProjData& proj_data,
+				   const int segment_num,
 				   const float tolerance_of_corruption)
 
 {
   axial_pos_to_remove_min =0;
   axial_pos_to_remove_max =0;
-  //int number_of_corrupted_bins=0;
-  int counter;
-  for (int axial_pos_num =in_eff_segment.get_min_axial_pos_num();
-       axial_pos_num <=in_eff_segment.get_max_axial_pos_num();
-       axial_pos_num++)
+  for (int axial_pos_num =proj_data.get_min_axial_pos_num(segment_num);
+       axial_pos_num <=proj_data.get_max_axial_pos_num(segment_num);
+       ++axial_pos_num)
     {	
-      counter =0;
-      Sinogram<float> sinogram = in_eff_segment.get_sinogram(axial_pos_num);
-      {
-	int total_num_bins_sino = sinogram.get_num_views()*(sinogram.get_num_tangential_poss()-40);
-	for ( int view_num = sinogram.get_min_view_num();
-	      view_num <= sinogram.get_max_view_num(); view_num++)
-	  for ( int tang_pos = sinogram.get_min_tangential_pos_num()+20;
-		tang_pos <= sinogram.get_max_tangential_pos_num()-20;
-		tang_pos++)
-	    {
-	      float bin = sinogram[view_num][tang_pos];
-	      if (fabs(bin/0.1) < 3)
-		counter ++;
-	    }
-	float percetage_corrupted = static_cast<float>(counter)/static_cast<float> (total_num_bins_sino)*100;
-	cerr << "Percentage corrupted at plane " << axial_pos_num << " is " <<  percetage_corrupted << "\n";
-	if ( percetage_corrupted >tolerance_of_corruption)
-	  axial_pos_to_remove_min++;
-	else
-	  break;
-	      
-      }
+      const float percentage_corrupted=
+	get_percentage_corrupted(proj_data.get_sinogram(axial_pos_num, segment_num));
+      cerr << "Percentage corrupted at plane " << axial_pos_num << " is " <<  percentage_corrupted << "\n";
+      if ( percentage_corrupted >tolerance_of_corruption)
+	axial_pos_to_remove_min++;
+      else
+	break;
     }
   // now do the max side
-  for (int axial_pos_num =in_eff_segment.get_max_axial_pos_num();
-	 axial_pos_num >in_eff_segment.get_min_axial_pos_num()+axial_pos_to_remove_min; 
+  for (int axial_pos_num =proj_data.get_max_axial_pos_num(segment_num);
+	 axial_pos_num >proj_data.get_min_axial_pos_num(segment_num)+axial_pos_to_remove_min; 
        axial_pos_num--)
     {	
-      counter =0;
-      Sinogram<float> sinogram = in_eff_segment.get_sinogram(axial_pos_num);
-      {
-	int total_num_bins_sino = sinogram.get_num_views()*(sinogram.get_num_tangential_poss()-40);
-	for ( int view_num = sinogram.get_min_view_num();
-	      view_num <= sinogram.get_max_view_num(); view_num++)
-	  for ( int tang_pos = sinogram.get_min_tangential_pos_num()+20;
-		tang_pos <= sinogram.get_max_tangential_pos_num()-20;
-		tang_pos++)
-	    {
-	      float bin = sinogram[view_num][tang_pos];
-	      if ( fabs(bin/0.1) < 3)
-		counter ++;
-	    }
-	float percetage_corrupted = static_cast<float>(counter)/static_cast<float> (total_num_bins_sino)*100;
-	cerr << "Percentage corrupted at plane " << axial_pos_num << " is " <<  percetage_corrupted << "\n";
-	if ( percetage_corrupted >tolerance_of_corruption)
-	  axial_pos_to_remove_max++;
-	else
-	  break;
-      }
-    }
-	
-	  
-	  
+      const float percentage_corrupted=
+	get_percentage_corrupted(proj_data.get_sinogram(axial_pos_num, segment_num));
+      cerr << "Percentage corrupted at plane " << axial_pos_num << " is " <<  percentage_corrupted << "\n";
+      if ( percentage_corrupted >tolerance_of_corruption)
+	axial_pos_to_remove_max++;
+      else
+	break;
+    }	  
 }
+
 int
 main(int argc, char* argv[])
 {
@@ -144,7 +130,7 @@ main(int argc, char* argv[])
   vector<int> record_of_number_of_planes_to_remove_max;
     
   // construct new proj_data_info_ptr for output data
-  ProjDataInfo * proj_data_info_ptr =
+  shared_ptr<ProjDataInfo> proj_data_info_ptr =
     in_projdata_ptr->get_proj_data_info_ptr()->clone();
   {
     
@@ -158,12 +144,10 @@ main(int argc, char* argv[])
     int axial_pos_to_remove_min;
     int axial_pos_to_remove_max;
     // only do segment zero and use that for all other segments
-    const SegmentBySinogram<float> in_eff_segment = 
-	     eff_projdata_ptr->get_segment_by_sinogram(0);
-    
     check_for_corrupted_sinograms(axial_pos_to_remove_min,
 				  axial_pos_to_remove_max,
-				  in_eff_segment,tolerance_of_corruption);
+				  *eff_projdata_ptr, 0,
+				  tolerance_of_corruption);
 	   
    int number_of_axial_pos_to_remove = axial_pos_to_remove_min+axial_pos_to_remove_max;
    record_of_number_of_planes_to_remove_min.push_back(axial_pos_to_remove_min);
@@ -208,7 +192,7 @@ main(int argc, char* argv[])
 	     2*max_segment_num_to_process+1);
   }
  
-  ProjDataInterfile out_projdata(proj_data_info_ptr, output_filename, ios::out); 
+  ProjDataInterfile out_projdata(proj_data_info_ptr, output_filename); 
 
   Succeeded succes = Succeeded::yes;
   std::vector<int>::iterator iter_record_of_number_of_planes_to_remove_min =
