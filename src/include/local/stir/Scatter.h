7c31f600322b3b1855d372fc04f1938c06503747
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-05-12 19:33:44 +0000

    New functions are: writing_time(), writing_log(), Also added the: enum image_type

--- src/include/local/stir/Scatter.h
+++ src/include/local/stir/Scatter.h
@@ -26,39 +26,44 @@
 #include "stir/ProjData.h"
 #include <vector>
 #include <cmath>
-
+//#define point_sample_step 1
 
 START_NAMESPACE_STIR
 
-const double Qe=1.602E-19;   
-const double Me=9.109E-31;
-const double C=2.997E8;
-const double Re=2.818E-13;
-
+const double Qe = 1.602E-19;   
+const double Me = 9.109E-31;
+const double C = 2.997E8;
+const double Re = 2.818E-13;
+enum image_type{act_image_type, att_image_type};
 
 template <class coordT> class CartesianCoordinate3D;
-
+class ProjDataInfoCylindricalNoArcCorr;
 /*!
    \ingroup scatter
    \brief samples the scatters points randomly in the attenuation_map
    is used to sample points of the attenuation_map which is the transmission image.
    The sampling is uniformly randomly with a threshold that is defined by att_threshold,
    that only above of this the points are sampled. It returns a vector containing the 
-   scatter points location in voxel units.
-   max_scatt_points are the wanted sample scatter points and is used as a reference, so 
-   that when the sample scatter points are less than max_scatt_points, the sample scatter 
-   points are assigned to the max_scatt_points, giving a warning.
+   scatter points location in mm units.
+   scatt_points are the wanted sample scatter points and is used as a reference, so 
+   that when the sample scatter points are less than scatt_points, the sample scatter 
+   points are assigned to the scatt_points, giving a warning.
 */  
 
 std::vector<CartesianCoordinate3D<float> > 
 sample_scatter_points(
 		const DiscretisedDensityOnCartesianGrid<3,float>& attenuation_map,
-		int & max_scatt_points,
-    const float att_threshold);
+		int & scatt_points,
+        const float att_threshold, 
+		const bool random);
+
+inline 
+float calc_efficiency( float low, float high, float energy, float resolution );
+
 
 /*!						  
   \ingroup scatter
-  \brief Implementations of functions defined in scatter.h
+  \brief Implementations of functions defined in Scatter.h
      Function calculates the integral along LOR in a image (attenuation or 
 	 emission). From scatter point to detector coordinate.
 	 For the start voxel, the intersection length of the LOR with the whole 
@@ -66,14 +71,17 @@ sample_scatter_points(
 	 The same is true for the end voxel.
 */
 float integral_scattpoint_det (
-	  const DiscretisedDensityOnCartesianGrid<3,float>& image_as_density,
+	  const DiscretisedDensityOnCartesianGrid<3,float>& discretised_image,
 	  const CartesianCoordinate3D<float>& scatter_point, 
 	  const CartesianCoordinate3D<float>& detector_coord);  
 
+float  cached_factors(const DiscretisedDensityOnCartesianGrid<3,float>& discretised_image,
+	  				   const CartesianCoordinate3D<float>& scatter_point, 
+					   const CartesianCoordinate3D<float>& detector_coord,
+					   const image_type input_image_type);
 /*!
   \ingroup scatter
-  \brief 
-
+ \brief 
 */
 
 float scatter_estimate_for_one_scatter_point(
@@ -82,26 +90,31 @@ float scatter_estimate_for_one_scatter_point(
 	  const CartesianCoordinate3D<float>& scatter_point, 
 	  const CartesianCoordinate3D<float>& detector_coord_A, 
 	  const CartesianCoordinate3D<float>& detector_coord_B);
-/*!						
+/*!	\name Klein-Nishina functions					
   \ingroup scatter
   \brief computes the differential cross section
-   TODO Better inline?????
-
-  This function computes the differential cross section
+ 
+  These functions computes the differential cross section
 	for Compton scatter, based on the Klein-Nishina-Formula
 	(cf. http://www.physik.uni-giessen.de/lehre/fpra/compton/ )
 
 	theta:	azimuthal angle between incident and scattered photon
 	energy:	energy of incident photon ( in keV )
-*/  	
+*/ 
+//@{
+inline
+float dif_cross_section_511keV(const float cos_theta);
+ 	
+inline
 float dif_cross_section(const CartesianCoordinate3D<float>& scatter_point,
-	                      const CartesianCoordinate3D<float>& detector_coordA,
-				          		  const CartesianCoordinate3D<float>& detector_coordB,
-	                      const float energy);	
-
+	                    const CartesianCoordinate3D<float>& detector_coordA,
+				        const CartesianCoordinate3D<float>& detector_coordB,
+	                    const float energy);	
+inline
 float dif_cross_section_511keV(const CartesianCoordinate3D<float>& scatter_point,
-	                      const CartesianCoordinate3D<float>& detector_coordA,
-				          		  const CartesianCoordinate3D<float>& detector_coordB);
+	                           const CartesianCoordinate3D<float>& detector_coordA,
+				          	   const CartesianCoordinate3D<float>& detector_coordB);
+
 /*!						
   \ingroup scatter
   \brief computes the total cross section
@@ -113,8 +126,14 @@ float dif_cross_section_511keV(const CartesianCoordinate3D<float>& scatter_point
 
 	energy:	energy of incident photon ( in keV )
 
-*/
+*/ 
+inline
 float total_cross_section(float energy);
+//@}
+
+inline
+float energy_after_scatter_511keV(const float cos_theta);
+
 
 /*!
   \ingroup scatter
@@ -136,22 +155,35 @@ void scatter_viewgram(
 	ProjData& proj_data,
 	const DiscretisedDensityOnCartesianGrid<3,float>& image_as_activity,
 	const DiscretisedDensityOnCartesianGrid<3,float>& image_as_density,
-    int max_scatt_points, const float att_threshold);
+    int& scatt_points, const float att_threshold, const bool random);
 
 
 // Functions that could be in the BasicCoordinate Class
 template<int num_dimensions>
 inline 
-BasicCoordinate<num_dimensions,float> convert_int_to_float(const BasicCoordinate<num_dimensions,int>& cint)
-	{	  
-	  BasicCoordinate<num_dimensions,float> cfloat;
+BasicCoordinate<num_dimensions,float> convert_int_to_float(const BasicCoordinate<num_dimensions,int>& cint);
 
-	  for(int i=1;i<=num_dimensions;++i)
-		  cfloat[i]=(float)cint[i];
-	  return cfloat;
-	}	
+/* !\ingroup scatter
+     \brief Temporary implementation of the error function for the Visual C++
+*/
+#ifdef _MSC_VER
+extern "C"  double erf(const double x);
+#endif
+
+void writing_log(const DiscretisedDensityOnCartesianGrid<3,float>& activity_image,
+				 const DiscretisedDensityOnCartesianGrid<3,float>& density_image,
+				 const ProjDataInfoCylindricalNoArcCorr * proj_data_info_ptr,
+				 const float given_attenuation_threshold,
+				 const int total_scatt_points,
+				 const bool random, 
+				 const char *argv[]);
+
+void writing_time(const int simulation_time, const int scatt_points_vector_size);
+
+
+
 
 
 END_NAMESPACE_STIR
 
-
+#include "local/stir/Scatter.inl"
