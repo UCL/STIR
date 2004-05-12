ce20eb9f01154d56cf7513b59263ce0e432c3dba
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-05-12 19:12:53 +0000

    The enum image_type has been added to choose between attenuation and emmision.

--- src/local/scatter_buildblock/cached_factors.cxx
+++ src/local/scatter_buildblock/cached_factors.cxx
@@ -60,14 +60,14 @@ CartesianCoordinate3D<float> > key_type;
 float cached_factors(const DiscretisedDensityOnCartesianGrid<3,float>& discretised_image,
 	  			     const CartesianCoordinate3D<float>& scatter_point, 
 				     const CartesianCoordinate3D<float>& detector_coord,
-					 const int input_image_type)
+					 const image_type input_image_type)
 {				
 
 	typedef std::map<key_type,float> map_type;
 		const VoxelsOnCartesianGrid<float>& image =
 			dynamic_cast<const VoxelsOnCartesianGrid<float>& >(discretised_image);
 	const CartesianCoordinate3D<float> voxel_size = image.get_voxel_size();
-	if(input_image_type==1)
+	if(input_image_type==act_image_type)
 	{
 	    static map_type cached_integral_act_scattpoint_det;		
 		const key_type key= std::make_pair(scatter_point, detector_coord);
@@ -85,7 +85,7 @@ float cached_factors(const DiscretisedDensityOnCartesianGrid<3,float>& discretis
 		return cached_integral_act_scattpoint_det[key];
 	}	
 
-	if(input_image_type==0)
+	if(input_image_type==att_image_type)
 	{	
 		static map_type cached_integral_att_scattpoint_det;	
 		const key_type key= std::make_pair(scatter_point, detector_coord);
@@ -96,10 +96,10 @@ float cached_factors(const DiscretisedDensityOnCartesianGrid<3,float>& discretis
 			if (iter != cached_integral_att_scattpoint_det.end())
 				return iter->second;
 		}						
-		cached_integral_att_scattpoint_det[key] =integral_scattpoint_det(
+		cached_integral_att_scattpoint_det[key] = exp(-integral_scattpoint_det(
 			discretised_image,	  	
 			scatter_point, 
-			detector_coord);			
+			detector_coord));			
 		return cached_integral_att_scattpoint_det[key];
 	}		
 	else 
