0ad756619617d236a700537d232742467836653f
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-05-12 19:06:02 +0000

    write_statistics.cxx is added

--- src/scatter_utilities/exe.mk
+++ src/scatter_utilities/exe.mk
@@ -3,6 +3,8 @@
 #
 dir := local/scatter
 
-$(dir)_SOURCES :=  estimate_single_scatter.cxx
+$(dir)_SOURCES :=  estimate_single_scatter.cxx/
+                   write_statistics
+
 
 include $(WORKSPACE)/exe.mk
