435387be96f8a1873ae7e9b0b4bfc831aa760f4b
Kris Thielemans <Kris.Thielemans@stir>
2004-05-16 20:55:07 +0000

    - use total_detectors global variable to know the size for the cache - compare values in cache as <0 instead of ==-1, as floating point comparisons   with equality are dangerous. - cache and nocache now give again the same results

--- src/scatter_buildblock/cached_single_scatter_integrals.cxx
+++ src/scatter_buildblock/cached_single_scatter_integrals.cxx
@@ -24,6 +24,8 @@ image (attenuation or emission).
 
 #include "local/stir/Scatter.h"
 #include "stir/IndexRange.h" 
+#include "stir/Array.h"
+#include "stir/Coordinate3D.h"
 #include "stir/CartesianCoordinate2D.h" 
 
 START_NAMESPACE_STIR
@@ -33,43 +35,48 @@ float cached_factors(const DiscretisedDensityOnCartesianGrid<3,float>& discretis
 					 const unsigned det_num,
 					 const image_type input_image_type)
 {		 
-	IndexRange<3> range(CartesianCoordinate3D<int> (0,0,0), CartesianCoordinate3D<int> (1,2224,71));
-	static Array<3,float> cached_integral_scattpoint_det(range);
-	static bool to_initialize_array = true;
+  static IndexRange<3> range (Coordinate3D<int> (0,0,0), 
+			      Coordinate3D<int> (1,
+						 static_cast<int>(scatt_points_vector.size()-1),
+						 total_detectors-1));
+  static Array<3,float> 
+    cached_integral_scattpoint_det(range);
+  static bool to_initialize_array = true;
 	if(to_initialize_array)		
 	{
 		cached_integral_scattpoint_det.fill(-1.F);
 		to_initialize_array = false;
-	}
-
+	}	
 	if(input_image_type==act_image_type)
 	{	
-		if (cached_integral_scattpoint_det[0][scatter_point_num][det_num]!=-1.F)
-			return cached_integral_scattpoint_det[0][scatter_point_num][det_num];
-		else 
-			cached_integral_scattpoint_det[0][scatter_point_num][det_num]=
-			integral_scattpoint_det(
-			discretised_image,	  	
-			scatt_points_vector[scatter_point_num].coord,
-			detection_points_vector[det_num]);			
+		if (cached_integral_scattpoint_det[0][scatter_point_num][det_num]<0)
+		  {
+		    cached_integral_scattpoint_det[0][scatter_point_num][det_num]=
+		      integral_scattpoint_det(
+					      discretised_image,	  	
+					      scatt_points_vector[scatter_point_num].coord,
+					      detection_points_vector[det_num]);
+		  }
 		return 
-			cached_integral_scattpoint_det[0][scatter_point_num][det_num];
+		  cached_integral_scattpoint_det[0][scatter_point_num][det_num];
 	}
-	
-	if(input_image_type==att_image_type)
+	else if(input_image_type==att_image_type)
 	{
-		if (cached_integral_scattpoint_det[1][scatter_point_num][det_num]!=-1.F)
-			return cached_integral_scattpoint_det[1][scatter_point_num][det_num];
-		else 
-			cached_integral_scattpoint_det[1][scatter_point_num][det_num]=
-			exp(-integral_scattpoint_det(
-			discretised_image,	  	
-			scatt_points_vector[scatter_point_num].coord,
-			detection_points_vector[det_num]));			
+		if (cached_integral_scattpoint_det[1][scatter_point_num][det_num]<0)
+		  {
+		    cached_integral_scattpoint_det[1][scatter_point_num][det_num]=
+		      exp(-integral_scattpoint_det(
+						   discretised_image,	  	
+						   scatt_points_vector[scatter_point_num].coord,
+						   detection_points_vector[det_num]));
+		  }
 		return 
-			cached_integral_scattpoint_det[1][scatter_point_num][det_num];
+		  cached_integral_scattpoint_det[1][scatter_point_num][det_num];
 	}		
 	else 
-		return EXIT_FAILURE;
+	  {
+	    error("cached_factors: wrong type of image\n");
+	    return 0;// to avoid compiler warnings, but will never get here
+	  }
 }	
 END_NAMESPACE_STIR
