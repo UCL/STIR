596bf0c1cae794e37edbbd12225ea51f4d9ed154
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-06-04 12:59:27 +0000

    - added DSS

--- src/scatter_buildblock/single_scatter_estimate.cxx
+++ src/scatter_buildblock/single_scatter_estimate.cxx
@@ -33,31 +33,47 @@ float scatter_estimate_for_all_scatter_points(
 	  const float lower_energy_threshold, 
 	  const float upper_energy_threshold,		
 	  const bool use_cosphi,
-	  const bool use_cache)	
+	  const bool use_cache,
+	  const bool find_DS)	
 {	
-	float scatter_ratio = 0; 
+	float scatter_ratio=0, scatter_ratio_2 = 0; 
 		
 	const VoxelsOnCartesianGrid<float>& image =
 		dynamic_cast<const VoxelsOnCartesianGrid<float>&>(image_as_density);
 	const CartesianCoordinate3D<float> voxel_size = image.get_voxel_size();
+	const float scatter_volume = voxel_size[1]*voxel_size[2]*voxel_size[3];
 	
 	for(std::size_t scatter_point_num =0;
 		scatter_point_num < scatt_points_vector.size();
 	    ++scatter_point_num)
-	  {
-		  scatter_ratio +=
-			  scatter_estimate_for_one_scatter_point(
-			  image_as_activity, image_as_density, 
-			  scatter_point_num,
-			  det_num_A, det_num_B,
-			  lower_energy_threshold, 
-			  upper_energy_threshold,		
-			  use_cosphi, use_cache);
-	  }
-    
-	return voxel_size[1]*voxel_size[2]*voxel_size[3]*
-						scatter_ratio/total_cross_section_511keV;
+	{
+		if(!find_DS)
+			scatter_ratio +=
+			scatter_estimate_for_one_scatter_point(
+			image_as_activity, image_as_density, 
+			scatter_point_num,
+			det_num_A, det_num_B,
+			lower_energy_threshold, 
+			upper_energy_threshold,		
+			use_cosphi, use_cache);	
+		else
+			for(std::size_t scatter_point_2_num =0;
+			scatter_point_2_num < scatt_points_vector.size() ;
+			++scatter_point_2_num)			
+				if(scatter_point_2_num!=scatter_point_num)
+					scatter_ratio_2 +=
+					scatter_estimate_for_two_scatter_points(
+					image_as_activity, image_as_density, 
+					scatter_point_num,
+					scatter_point_2_num,
+					det_num_A, det_num_B,
+					lower_energy_threshold, 
+					upper_energy_threshold,		
+					use_cosphi, use_cache);
+	}	
+	return scatter_volume/total_cross_section_511keV*
+		(scatter_ratio+
+		scatter_volume/total_cross_section_511keV*scatter_ratio_2);
 }
-
 
 END_NAMESPACE_STIR
