9fbafced490173654680e364844ce771dc72f87e
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2004-06-06 18:24:14 +0000

    cache_factors_2.cxx added, to cache the two scatter points integrals

--- src/scatter_buildblock/lib.mk
+++ src/scatter_buildblock/lib.mk
@@ -7,6 +7,7 @@ $(dir)_LIB_SOURCES:= \
 	sample_scatter_points.cxx \
 	integral_scattpoint_det.cxx\
         cached_factors.cxx\
+	cached_factors_2.cxx\
 	scatter_estimate_for_one_scatter_point.cxx\
 	scatter_estimate_for_two_scatter_points.cxx\
 	scatter_estimate_for_all_scatter_points.cxx\
