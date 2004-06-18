38246c0bd155314d451dd6dd125f7d802ee5299f
Kris Thielemans <Kris.Thielemans@stir>
2004-06-18 18:08:47 +0000

    - added rules that do not link with all registries to save time during linking

--- src/recon_test/test.mk
+++ src/recon_test/test.mk
@@ -12,6 +12,16 @@ run_tests_$(dir): all_test_exes
     
 run_interactive_tests_$(dir): 
 
+# rules that do not link with all registries to save time during linking
+# Beware: the pattern below is dangerous as it relies on a naming style for the registries.
+# Note: have to be before the include below as that resets $(dir)
+
+${DEST}$(dir)/test_DataSymmetriesForBins_PET_CartesianGrid: ${DEST}$(dir)/test_DataSymmetriesForBins_PET_CartesianGrid.o \
+   $(STIR_LIB) $(filter %recon_buildblock_registries.o,$(STIR_REGISTRIES))
+	$(CXX) $(CFLAGS)  -o $@ $< \
+		$(filter %recon_buildblock_registries.o,$(STIR_REGISTRIES)) \
+		 $(STIR_LIB)  $(LINK_OPT) $(SYS_LIBS)
+
 include $(WORKSPACE)/test.mk
 
 
