c3720f82459e8e0ae0db37f1369a45105c9e5b73
Kris Thielemans <Kris.Thielemans@stir>
2004-06-21 16:52:56 +0000

    - added instantiations for signed char (i.e. 1 byte)

--- src/buildblock/Array.cxx
+++ src/buildblock/Array.cxx
@@ -1166,15 +1166,18 @@ void Array<1,elemT>::read_data(istream& s, NumericType type, float& scale,
 
 // add any other types you need
 #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+template class Array<1,signed char>;
 template class Array<1,short>;
 template class Array<1,unsigned short>;
 template class Array<1,float>;
 #endif
 
+template class Array<2,signed char>;
 template class Array<2,short>;
 template class Array<2,unsigned short>;
 template class Array<2,float>;
 
+template class Array<3, signed char>;
 template class Array<3, short>;
 template class Array<3,unsigned short>;
 template class Array<3,float>;
