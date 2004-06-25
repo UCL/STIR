7e2d3c067c3b5b6ce030d66cb28ed215e4fdf528
Kris Thielemans <Kris.Thielemans@stir>
2004-06-25 09:06:22 +0000

    - use transform_3d_object - update doxygen

--- src/local/motion_utilities/rigid_object_transform_projdata.cxx
+++ src/local/motion_utilities/rigid_object_transform_projdata.cxx
@@ -2,13 +2,9 @@
 // $Id$
 //
 /*!
-
   \file
   \ingroup utilities
-  \brief A utility to rorate projection data along the axial direction
-
-  This can be used as a crude way for motion correction, when the motion is only in 
-  z-direction.
+  \brief A utility to perform a rigid object transformation on projection data.
 
   \author Kris Thielemans
 
@@ -16,79 +12,32 @@
   $Revision$
 */
 /*
-    Copyright (C) 2003- $Date$, IRSL
+    Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
-#include "stir/ProjData.h"
-#include "stir/ProjDataInfoCylindricalNoArcCorr.h"
-#include "stir/SegmentByView.h"
 #include "stir/ProjDataInterfile.h"
-#include "stir/Bin.h"
 #include "stir/Succeeded.h"
 #include "stir/CartesianCoordinate3D.h"
-#include "stir/round.h"
 #include "local/stir/motion/RigidObject3DTransformation.h"
+#include "local/stir/motion/transform_3d_object.h"
 #include "local/stir/Quaternion.h"
 #include "stir/CPUTimer.h"
 #include <string>
-#include <algorithm>
-
-#ifndef STIR_NO_NAMESPACES
-using std::string;
-using std::cerr;
-using std::endl;
-using std::min;
-using std::max;
-#endif
 
 USING_NAMESPACE_STIR
 
-class TF
-{
-public:
-  TF(const shared_ptr<ProjDataInfo>& out_proj_data_info_ptr,
-     const shared_ptr<ProjDataInfo>& in_proj_data_info_ptr,
-     const RigidObject3DTransformation& transformation)
-    : out_proj_data_info_ptr(out_proj_data_info_ptr),
-      in_proj_data_info_ptr(in_proj_data_info_ptr),
-      transformation(transformation)
-  {
-     out_proj_data_info_noarccor_ptr = 
-       dynamic_cast<ProjDataInfoCylindricalNoArcCorr*>(out_proj_data_info_ptr.get());
-     in_proj_data_info_noarccor_ptr = 
-       dynamic_cast<ProjDataInfoCylindricalNoArcCorr*>(in_proj_data_info_ptr.get());
-     if (out_proj_data_info_noarccor_ptr == 0 ||
-	 in_proj_data_info_noarccor_ptr == 0)
-       error("Wrong type of proj_data_info\n");
-
-
-  }
-
-  void transform_bin(Bin& bin) const
-  {
-     transformation.transform_bin(bin, 
-                                  *out_proj_data_info_noarccor_ptr,
-		                  *in_proj_data_info_noarccor_ptr);
-  }
-	
-private:
-  shared_ptr<ProjDataInfo> out_proj_data_info_ptr;
-  shared_ptr<ProjDataInfo> in_proj_data_info_ptr;
-  ProjDataInfoCylindricalNoArcCorr *out_proj_data_info_noarccor_ptr;
-  ProjDataInfoCylindricalNoArcCorr *in_proj_data_info_noarccor_ptr;
-  RigidObject3DTransformation transformation;
-};
 
 int main(int argc, char **argv)
 {
   if (argc < 10 || argc > 12)
     {
-      cerr << "Usage:\n"
+      std::cerr << "Usage:\n"
 	   << argv[0] << " output_filename input_projdata_name q0 qx qy qz tx ty tz [max_in_segment_num_to_process [max_out_segment_num_to_process ]]\n"
-	   << "max_in_segment_num_to_process defaults to all segments\n";
+	   << "max_in_segment_num_to_process defaults to all segments\n"
+	   << "max_out_segment_num_to_process defaults to max_in_segment_num_to_process\n";
       exit(EXIT_FAILURE);
     }
-  const string  output_filename = argv[1];
+  const std::string  output_filename = argv[1];
   shared_ptr<ProjData> in_projdata_ptr = ProjData::read_from_file(argv[2]);  
   //const float angle_around_x =  atof(argv[3]) *_PI/180;
   const Quaternion<float> quat(atof(argv[3]),atof(argv[4]),atof(argv[5]),atof(argv[6]));
@@ -97,67 +46,20 @@ int main(int argc, char **argv)
   const int max_out_segment_num_to_process = argc <=11 ? max_in_segment_num_to_process : atoi(argv[11]);
 
 
-  ProjDataInfo * proj_data_info_ptr =
+  shared_ptr<ProjDataInfo> proj_data_info_ptr =
     in_projdata_ptr->get_proj_data_info_ptr()->clone();
   proj_data_info_ptr->reduce_segment_range(-max_out_segment_num_to_process,max_out_segment_num_to_process);
 
   ProjDataInterfile out_projdata(proj_data_info_ptr, output_filename, ios::out); 
 
-  TF move_lor(out_projdata.get_proj_data_info_ptr()->clone(),
-	      in_projdata_ptr->get_proj_data_info_ptr()->clone(),
-	      RigidObject3DTransformation(quat, translation));
-  const int out_min_segment_num = out_projdata.get_min_segment_num();
-  const int out_max_segment_num = out_projdata.get_max_segment_num();
-  VectorWithOffset<shared_ptr<SegmentByView<float> > > out_seg_ptr(out_min_segment_num, out_max_segment_num);
-  for (int segment_num = out_projdata.get_min_segment_num();
-       segment_num <= out_projdata.get_max_segment_num();
-       ++segment_num)    
-    out_seg_ptr[segment_num] = 
-      new SegmentByView<float>(out_projdata.get_empty_segment_by_view(segment_num));
-
   CPUTimer timer;
   timer.start();
-  for (int segment_num = -max_in_segment_num_to_process;
-       segment_num <= max_in_segment_num_to_process;
-       ++segment_num)    
-    {       
-      const SegmentByView<float> in_segment = 
-        in_projdata_ptr->get_segment_by_view( segment_num);
-      cerr << "segment_num "<< segment_num << endl;
-      const int in_max_ax_pos_num = in_segment.get_max_axial_pos_num();
-      const int in_min_ax_pos_num = in_segment.get_min_axial_pos_num();
-      const int in_max_view_num = in_segment.get_max_view_num();
-      const int in_min_view_num = in_segment.get_min_view_num();
-      const int in_max_tang_pos_num = in_segment.get_max_tangential_pos_num();
-      const int in_min_tang_pos_num = in_segment.get_min_tangential_pos_num();
-      for (int view_num=in_min_view_num; view_num<=in_max_view_num; ++view_num)
-	for (int ax_pos_num=in_min_ax_pos_num; ax_pos_num<=in_max_ax_pos_num; ++ax_pos_num)
-	  for (int tang_pos_num=in_min_tang_pos_num; tang_pos_num<=in_max_tang_pos_num; ++tang_pos_num)
-	    {
-	      Bin bin(segment_num, view_num, ax_pos_num, tang_pos_num,
-		      in_segment[view_num][ax_pos_num][tang_pos_num]);
-	      if (bin.get_bin_value()<=0)
-		continue;
-	      move_lor.transform_bin(bin);
-	      if (bin.get_bin_value()>0)
-		(*out_seg_ptr[bin.segment_num()])[bin.view_num()]
-						 [bin.axial_pos_num()]
-						 [bin.tangential_pos_num()] +=
-		  bin.get_bin_value();
-	    }
-    }
-
+  Succeeded succes =
+    transform_3d_object(out_projdata, *in_projdata_ptr,
+			RigidObject3DTransformation(quat, translation),
+			-max_in_segment_num_to_process,
+			max_in_segment_num_to_process);
   timer.stop();
-  cerr << "CPU time " << timer.value() << endl;
-
-  Succeeded succes = Succeeded::yes;
-  for (int segment_num = out_projdata.get_min_segment_num();
-       segment_num <= out_projdata.get_max_segment_num();
-       ++segment_num)    
-    {       
-      if (out_projdata.set_segment(*out_seg_ptr[segment_num]) == Succeeded::no)
-             succes = Succeeded::no;
-    }
-
-    return succes == Succeeded::yes ? EXIT_SUCCESS : EXIT_FAILURE;
+  std::cerr << "CPU time " << timer.value() << '\n';
+  return succes == Succeeded::yes ? EXIT_SUCCESS : EXIT_FAILURE;
 }
