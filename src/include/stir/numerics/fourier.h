93d342df41d0b0b72c692e1a76dd98faa7013170
Kris Thielemans <Kris.Thielemans@stir>
2004-07-21 10:54:51 +0000

    - added a lot more doxygen

--- src/include/stir/numerics/fourier.h
+++ src/include/stir/numerics/fourier.h
@@ -31,9 +31,9 @@ START_NAMESPACE_STIR
   The function will then compute the \a n- dimensional fourier transform of the data,
   and store the result in \a c.
 
-  \param[in] sign This can be used to implement a different convention for the DFT
+  \param[in] sign This can be used to implement a different convention for the DFT.
 
-  \see fourier_1d for conventions
+  \see fourier_1d for conventions and restrictions
 
   \warning Currently, the array has to have \c get_min_index()==0 at each dimension.
 */
@@ -43,6 +43,12 @@ fourier(T& c, const int sign = 1);
 //fourier(VectorWithOffset<elemT>& c, const int sign = 1);
 
 
+/*! \ingroup DFT
+  \brief Compute the inverse of the multi-dimensional discrete fourier transform.
+
+  The scale factor is such that <tt>inverse(fourier(c,sign),sign)==c</tt>, aside
+  from numerical error of course.
+*/
 template <typename T>
 inline void inverse_fourier(T& c, const int sign=1)
 {
@@ -54,19 +60,21 @@ inline void inverse_fourier(T& c, const int sign=1)
 
   \brief Compute one-dimensional discrete fourier transform of an array.
 
-  \param[in,out] c The type of \a c should normally be \verbatim Array&lt;n,std::complex<T> > \endverbatim (but see below).
+  \param[in,out] c The type of \a c should normally be \verbatim Array<n,std::complex<T> > \endverbatim (but see below).
   The function will then compute the one-dimensional fourier transform (i.e. on the
   'outer' index) of the data, and store the result in \a c.
 
   \param sign This can be used to implement a different convention for the DFT
 
   \warning Currently, the array has to be indexed from 0.
+  \warning Currently, the length of the array has to be a power of 2.
    
   The convention used is as follows.
   For a vector of length \a n, the result is
   \f[
     r_s = \sum_{s=0}^{n-1} c_r e^{\mathrm{sign} 2\pi i r s/n}
   \f]
+  This means that the zero-frequency will be returned in <tt>c[0]</tt>
    
   This function can be used with more general type of \a c (if instantiated in fourier.cxx).
   The type \a T has to be such that \a T::value_type, \a T::reference and 
@@ -77,19 +85,62 @@ inline void inverse_fourier(T& c, const int sign=1)
   <tt>operator*=(T::reference, int)</tt>,
    have to be defined as well.
 */
-
 template <typename T>
 void fourier_1d(T& c, const int sign);
 
+/*! \ingroup DFT
+
+  \brief Compute one-dimensional discrete fourier transform of a real array (of even size).
+
+  \param[in] c The type of \a c should normally be \verbatim Array<1,float > \endverbatim 
+          (or \c double).
+  \param[in] sign  This can be used to implement a different convention for the DFT.
+  \return The positive frequencies of the DFT as an array of complex numbers. That is,
+  for <tt>c.size()==2*n</tt>, the returned array will have indices from <tt>0</tt> 
+  to <tt>n</tt>, with the 0 frequency at 0.
+
+  For a real array, the DFT is such that the values at negative frequencies are the
+  complex conjugate of the value at the corresponding positive frequency.
+  In addition, if the length of the real array is even, the DFT can be computed
+  efficiently by creating a complex array of half the length (by putting the
+  even-numbered elements as the real part, and the odd-numbered as the imaginary part).
+
+  This function implements the above and is hence (probably) a faster way to compute
+  DFTs of real arrays. Note however, that in contrast to the Numerical Recipes routines,
+  the result is not stored in the same memory location as the input. For a recent
+  compiler implement the Named-Return-Value-Optimisation, this should not be a problem
+  in most cases.
+
+  Result is such that  
+  <tt>pos_frequencies_to_all(fourier_1d_for_real_data(v,sign))==fourier(v,sign)</tt> 
+  (this is meant symbolically. In code you'd have to make \c v into an array of complex
+  numbers before passing to \c fourier).
+    
+  \see pos_frequencies_to_all()
+  \see fourier_1d() for conventions and restrictions
+
+  \warning Currently, the array has to have \c get_min_index()==0.
+*/
 template <typename T>
 Array<1,std::complex<typename T::value_type> >
 fourier_1d_for_real_data(const T& v, const int sign = 1);
 
-/*! \warning destroys values in (and resizes) first argument \a c */
+/*! \ingroup DFT
+
+  \brief Compute the inverse of the one-dimensional discrete fourier transform of a real array (of even size).
+
+  \warning destroys values in (and resizes) first argument \a c
+  \see fourier_1d_for_real_data()
+*/
 template <typename T>
 Array<1,T>
   inverse_fourier_1d_for_real_data(Array<1,std::complex<T> >& c, const int sign = 1);
 
+/*! \ingroup DFT
+  \brief Adds negative frequencies to a complex array of positive frequences by complex conjugation.
+
+  \see fourier_1d_for_real_data()
+*/
 template <typename T>
 Array<1,std::complex<T> > 
 pos_frequencies_to_all(const VectorWithOffset<std::complex<T> >& c)
