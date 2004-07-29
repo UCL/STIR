b08c7ef9db634bfdf9e9223dced54afe24546598
Kris Thielemans <Kris.Thielemans@stir>
2004-07-29 16:22:21 +0000

    - moved here from read_data, and put in namespace detail

---
+++ src/include/stir/read_data_1d.h
@@ -0,0 +1,54 @@
+// $Id$
+#ifndef __stir_read_data_1d_H__
+#define __stir_read_data_1d_H__
+/*!
+  \file 
+  \ingroup Array 
+  \brief Declaration of read_data_1d() functions for reading Arrays to file
+
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+/*
+    Copyright (C) 2004- $Date$, Hammersmith Imanet Ltd
+    See STIR/LICENSE.txt for details
+*/
+#include "stir/common.h"
+#include <stdio.h>
+#include <iostream>
+
+START_NAMESPACE_STIR
+class Succeeded;
+class ByteOrder;
+template <int num_dimensions, class elemT> class Array;
+
+namespace detail {
+/*! \ingroup Array
+  \brief This is an internal function called by \c read_data(). It does the actual reading
+   to \c std::istream.
+
+  This function might propagate any exceptions by std::istream::read.
+ */
+template <class elemT>
+inline Succeeded
+read_data_1d(std::istream& s, Array<1, elemT>& data,
+	     const ByteOrder byte_order);
+
+
+/* \ingroup Array
+  \brief  This is the (internal) function that does the actual reading from a FILE*.
+  \internal
+ */
+template <class elemT>
+inline Succeeded
+read_data_1d(FILE*& , Array<1, elemT>& data,
+	     const ByteOrder byte_order);
+
+} // end namespace detail
+END_NAMESPACE_STIR
+
+#include "stir/read_data_1d.inl"
+
+#endif
