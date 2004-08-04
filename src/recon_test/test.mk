02c9a42e2860fdb38f4fa001ce7284afc0347f8f
Kris Thielemans <Kris.Thielemans@stir>
2004-08-04 16:26:25 +0000

    - simplify because of changes in the root test.mk

--- src/recon_test/test.mk
+++ src/recon_test/test.mk
@@ -6,21 +6,15 @@ dir := recon_test
 $(dir)_TEST_SOURCES := test_DataSymmetriesForBins_PET_CartesianGrid.cxx
 
 
-run_tests_$(dir): all_test_exes 
-	$(DEST)recon_test/test_DataSymmetriesForBins_PET_CartesianGrid
-
-    
-run_interactive_tests_$(dir): 
-
 # rules that do not link with all registries to save time during linking
 # Beware: the pattern below is dangerous as it relies on a naming style for the registries.
 # Note: have to be before the include below as that resets $(dir)
 
-${DEST}$(dir)/test_DataSymmetriesForBins_PET_CartesianGrid: ${DEST}$(dir)/test_DataSymmetriesForBins_PET_CartesianGrid.o \
-   $(STIR_LIB) $(filter %recon_buildblock_registries.o,$(STIR_REGISTRIES))
-	$(CXX) $(CFLAGS)  -o $@ $< \
-		$(filter %recon_buildblock_registries.o,$(STIR_REGISTRIES)) \
-		 $(STIR_LIB)  $(LINK_OPT) $(SYS_LIBS)
+${DEST}$(dir)/test_DataSymmetriesForBins_PET_CartesianGrid: ${DEST}$(dir)/test_DataSymmetriesForBins_PET_CartesianGrid${O_SUFFIX} \
+   $(STIR_LIB) $(filter %recon_buildblock_registries${O_SUFFIX},$(STIR_REGISTRIES))
+	$(LINK) $(EXE_OUTFLAG)$@ $< \
+		$(filter %recon_buildblock_registries${O_SUFFIX},$(STIR_REGISTRIES)) \
+		 $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
 
 include $(WORKSPACE)/test.mk
 
