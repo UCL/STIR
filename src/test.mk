24311fe9fc823a4c96f3fdbebb490a2415f7ded3
Kris Thielemans <Kris.Thielemans@stir>
2004-08-04 16:25:13 +0000

    - use $(O_SUFFIX) etc, see log for change in Makefile - major change in providing extra macros etc for the 'client' test.mk. In particular, the run_tests_$(dir) target is now automatically generated from $($dir)_TEST_SOURCES) (and similar for the interactive case). When the test program does not need any command line parameters, the relevant rules are automatically generated. When you do need it, defined a target run_$(dir)/some_test_file with the relevant parameters. See test/test.mk for an example.

--- src/test.mk
+++ src/test.mk
@@ -10,26 +10,65 @@
 # the skeleton should set the following variables
 #    dir must be set to the subdirectory name (relative to the location of Makefile)
 #    $(dir)_TEST_SOURCES must be set to a list of .cxx and/or .c files (without path)
-#    it must have a target run_tests_$(dir) (that depends on all_test_exes)
-#    it must have a target run_interactive_tests_$(dir) (that depends on all_test_exes)
+#         that will be used for non-interactive tests
+#    $(dir)_TEST_INTERACTIVE_SOURCES is similar for interactive tests
+#    it must have a target run_$(dir)/some_test_exe 
+#          for any 'some_test_exe' that needs command line arguments 
+#	   or other non-standard command line
+#          Ideally, all your run_$(dir)/some_test_exe targets depend on PHONY_TARGET (see below)
+#    similarly, it must have a target run_interactive_$(dir)/some_test_exe
 # Result:
 # targets   build_tests_$(dir), clean_tests_$(dir)
-# variables $(dir)_TEST_SOURCES (with $dir)/ prepended)
-#           $(dir)_TEST_EXES, $(dir)_TEST_EXE_FILENAMES
+#           run_tests_$(dir), run_interactive_tests_$(dir)
+#	    run_$(dir)/test_exe for any file in $dir)_TEST_SOURCES
+# some variables (but you shouldn't count on those remaining the same)
 # 
 #
 # Example for dir=SUBDIR
 #
 # make build_tests_SUBDIR will compile and link all executables
 # make clean_tests_SUBDIR will remove all generated files
-# make run_tests_SUBDIR will run tests for all executables
+# make run_tests_SUBDIR will run tests for all (non-interactive) executables
 
 #$(warning including test.mk from $(dir))
 
-$(dir)_TEST_SOURCES:= $(addprefix $(dir)/, $($(dir)_TEST_SOURCES))
+
+$(dir)_RUN_TEST_TARGETS:= \
+	$(patsubst %.cxx, run_$(dir)/%, $(filter %.cxx, $($(dir)_TEST_SOURCES))) \
+	$(patsubst %.c, run_$(dir)/%, $(filter %.c, $($(dir)_TEST_SOURCES)))
+$(dir)_RUN_INTERACTIVE_TEST_TARGETS:= \
+	$(patsubst %.cxx, run_$(dir)/%, $(filter %.cxx, $($(dir)_INTERACTIVE_TEST_SOURCES))) \
+	$(patsubst %.c, run_$(dir)/%, $(filter %.c, $($(dir)_INTERACTIVE_TEST_SOURCES)))
+
+
+run_tests_$(dir):  $($(dir)_RUN_TEST_TARGETS)
+
+run_interactive_tests_$(dir):  $($(dir)_RUN_INTERACTIVE_TEST_TARGETS)
+
+# default rule for run_$(dir)/some_test_exe
+# This rule just runs the corresponding executable
+run_$(dir)/% : $(DEST)$(dir)/% PHONY_TARGET
+	$<
+# Notes on above rule:
+# Ideally, we would make all targets in $($(dir)_RUN_TEST_TARGETS) phony.
+# However, make does not look for built-in rules for phony targets.
+# So, instead, we let them depend on a phony target. Sorry.
+#
+# For some reason, I need $(dir) in there. The following does not work
+# run_% : $(DEST)% PHONY_TARGET
+# (probably because of the /)
+
+.PHONY: PHONY_TARGET
+
+# variables for the build* and clean* targets
+$(dir)_ALL_TEST_SOURCES:= $($(dir)_TEST_SOURCES)  $($(dir)_TEST_INTERACTIVE_SOURCES)
+$(dir)_ALL_TEST_SOURCES:= $(addprefix $(dir)/, $($(dir)_ALL_TEST_SOURCES))
+
+$(dir)_TEST_OBJS:= \
+	$(patsubst %.cxx, $(DEST)%$(O_SUFFIX), $(filter %.cxx, $($(dir)_ALL_TEST_SOURCES))) \
+	$(patsubst %.c, $(DEST)%$(O_SUFFIX), $(filter %.c, $($(dir)_ALL_TEST_SOURCES)))
 $(dir)_TEST_EXES:= \
-	$(patsubst %.cxx, $(DEST)%, $(filter %.cxx, $($(dir)_TEST_SOURCES))) \
-	$(patsubst %.c, $(DEST)%, $(filter %.c, $($(dir)_TEST_SOURCES)))
+	$(patsubst %$(O_SUFFIX), %,  $($(dir)_TEST_OBJS))
 
 
 $(dir)_TEST_EXE_FILENAMES := $(addsuffix $(EXE_SUFFIX), $($(dir)_TEST_EXES))
@@ -37,21 +76,22 @@ $(dir)_TEST_EXE_FILENAMES := $(addsuffix $(EXE_SUFFIX), $($(dir)_TEST_EXES))
 .PHONY: build_tests_$(dir) clean_tests_$(dir)  run_tests_$(dir) run_interactive_tests_$(dir)
 
 
-# make sure make keeps the .o files
-# otherwise it will be deleted
-.PRECIOUS: $(patsubst %.cxx, $(DEST)%.o, $(filter %.cxx, $($(dir)_TEST_SOURCES))) 
+# make sure 'make' keeps the .o files
+# otherwise they will be deleted
+.PRECIOUS: $($(dir)_ALL_TEST_OBJS)) 
 
 build_tests_$(dir):  $($(dir)_TEST_EXES)
 
 clean_tests_$(dir):
 	rm -f $($(@:clean_tests_%=%)_TEST_EXE_FILENAMES)
-	rm -f $(DEST)$(@:clean_tests_%=%)/*.[oP]
+	rm -f $($(@:clean_tests_%=%)_TEST_OBJS)
+	rm -f $(DEST)$(@:clean_tests_%=%)/*.P 
 
 
 ifneq ($(MAKECMDGOALS:clean%=clean),clean)
   -include \
-	$(patsubst %.cxx, $(DEST)%.P, $(filter %.cxx, $($(dir)_TEST_SOURCES))) \
-	$(patsubst %.c, $(DEST)%.P, $(filter %.c, $($(dir)_TEST_SOURCES)))
+	$(patsubst %.cxx, $(DEST)%.P, $(filter %.cxx, $($(dir)_ALL_TEST_SOURCES))) \
+	$(patsubst %.c, $(DEST)%.P, $(filter %.c, $($(dir)_ALL_TEST_SOURCES)))
 endif
 
 # set to some garbage such that we get an error when  the next skeleton forgets to set dir
