a7e43e069c19d8569ad65f6991a7f7ac950226c3
Kris Thielemans <Kris.Thielemans@stir>
2004-08-04 16:18:41 +0000

    - replace EXTRA_OPT with EXTRA_CFLAGS, see log for change in Makefile

--- src/samples/local/config.mk
+++ src/samples/local/config.mk
@@ -17,20 +17,20 @@ INSTALL_PREFIX=/usr/local
 # for some systems, do more...
 ifeq ($(SYSTEM),LINUX)
 # our Linux system has Pentium III processors
-EXTRA_OPT=-g -Wall -march=pentium3
+EXTRA_CFLAGS=-g -Wall -march=pentium3
 else
 ifeq ($(SYSTEM),CYGWIN)
 # our NT system has Pentium II processors
 # maybe cygwin can't find X include files by default
-EXTRA_OPT=-g -Wall -march=pentium2 -I /usr/X11R6/include
+EXTRA_CFLAGS=-g -Wall -march=pentium2 -I /usr/X11R6/include
 else
 ifeq ($(SYSTEM),CC)
 # our Parsytec CC system has gcc 2.8.1, so we don't use -Wall 
 # and have to set the template depth
-EXTRA_OPT= -ftemplate-depth-25
+EXTRA_CFLAGS= -ftemplate-depth-25
 else
 # default
-EXTRA_OPT=-g -Wall
+EXTRA_CFLAGS=-g -Wall
 endif
 endif
 endif
