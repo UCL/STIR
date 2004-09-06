8f4eef369c298014e2dc7ebe7ecd7e263b78aa53
Kris Thielemans <Kris.Thielemans@stir>
2004-09-06 12:48:25 +0000

    - removed all .cxx files as now in global distro

--- src/local/analytic/FBP2D/lib.mk
+++ src/local/analytic/FBP2D/lib.mk
@@ -3,8 +3,8 @@
 #
 
 dir := local/analytic/FBP2D
-$(dir)_LIB_SOURCES:= \
-	RampFilter.cxx FBP2DReconstruction.cxx
+#$(dir)_LIB_SOURCES:= \
+#	RampFilter.cxx FBP2DReconstruction.cxx
 
 #$(dir)_REGISTRY_SOURCES:= local_motion_registries.cxx
 
