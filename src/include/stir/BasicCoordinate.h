0d080883a482869265ff1c68d968f55ad9990079
Kris Thielemans <Kris.Thielemans@stir>
2004-09-06 13:13:28 +0000

    - made coords member private - added constructor taking BasicCoordinate with other coordT - added operator< (and all other comparisons via boost/operators) - added join(coordT&, BasicCoordinate&) - added cut_last_dimension(BasicCoordinate&) - removed #ifdef on gcc 2.8.1. The library wouldn't compile anyway

--- src/include/stir/BasicCoordinate.h
+++ src/include/stir/BasicCoordinate.h
@@ -30,6 +30,7 @@
 
 
 #include "stir/common.h"
+#include <boost/operators.hpp>
 #include <iterator>
 
 #ifndef STIR_NO_NAMESPACES
@@ -41,10 +42,10 @@ using std::random_access_iterator_tag;
 START_NAMESPACE_STIR
 /*!
   \ingroup Coordinate
-  \brief class BasicCoordinate<\c int \c num_dimensions, \c typename \c coordT> defines \c num_dimensions -dimensional coordinates.
+  \brief class BasicCoordinate&lt;\c int \c num_dimensions, \c typename \c coordT&gt; defines \c num_dimensions -dimensional coordinates.
 
 
-   A BasicCoordinate<\c num_dimensions, \c coordT> is essentially a vector of size \c num_dimensions, but 
+   A BasicCoordinate&lt;\c num_dimensions, \c coordT&gt; is essentially a vector of size \c num_dimensions, but 
    as the dimension is templated, it has better performance.
 
    Access to the individual coordinates is through operator[].
@@ -53,7 +54,10 @@ START_NAMESPACE_STIR
 
 */
 template <int num_dimensions, typename coordT>
-class BasicCoordinate
+  class BasicCoordinate :
+  boost::partially_ordered<BasicCoordinate<num_dimensions, coordT>,  // have operator>, <= etc for free
+  boost::equality_comparable<BasicCoordinate<num_dimensions, coordT> // have operator!= for free
+  > >
 { 
 
 public:
@@ -71,19 +75,29 @@ public:
   inline BasicCoordinate();
   //! copy constructor
   inline BasicCoordinate(const BasicCoordinate& c);
-
+  //! constructor from different type of \c coordT
+  /*! Uses assignment after <pre>static_cast<coordT></pre> */
+  // Note implementation here for VC 6.0
+  template <typename coordT2>
+  explicit inline 
+    BasicCoordinate(const BasicCoordinate<num_dimensions, coordT2>& c)
+    {
+      for (int i=1; i<=num_dimensions;++ i)
+	coords[i] = static_cast<coordT>(c[i]);
+    }
   // virtual destructor, not implemented at the moment 
   // It's only needed whenever there is a chance that an object will be deleted
   // through a pointer to its base class and derived classes have extra
   // data members
   // virtual ~BasicCoordinate() {}
 
-  // assignment
+  //! assignment
   inline BasicCoordinate & operator=(const BasicCoordinate& c);
 
-  // comparison
+  //! comparison
   inline bool operator==(const BasicCoordinate& c) const;
-  inline bool operator!=(const BasicCoordinate& c) const;
+  //! less-than (using lexical ordering)
+  inline bool operator<(const BasicCoordinate& c) const;
 
   // access to elements
   inline coordT& operator[](const int d);
@@ -118,7 +132,7 @@ public:
   inline iterator end();
   inline const_iterator end() const;
 
-protected:
+ private:
   // allocate 1 too many to leave space for coords[0] (which is never used)
   coordT coords[num_dimensions+1];
 
@@ -169,11 +183,8 @@ inline double
 cos_angle (const BasicCoordinate<num_dimensions, coordT>& p1, 
           const BasicCoordinate<num_dimensions, coordT>& p2);
 
-#if !defined( __GNUC__) || !(__GNUC__ == 2 && __GNUC_MINOR__ < 9)
-  // gcc 2.8.1 bug:
-  // It cannot call 'join' (it generates a bad mangled name for the function)
-  // so we only define it when it's not gcc 2.8.1
-
+// Note: gcc 2.8.1 bug:
+// It cannot call 'join' (it generates a bad mangled name for the function)
 //! make a longer BasicCoordinate, by prepending \c  c with the single \c coordT
 /*! \ingroup Coordinate */
 template <int num_dimensions, typename coordT>
@@ -181,7 +192,18 @@ inline BasicCoordinate<num_dimensions+1, coordT>
 join(const coordT& a, 
      const BasicCoordinate<num_dimensions, coordT>& c);    
 
-#endif // gcc 2.8.1
+//! make a longer BasicCoordinate, by appending the \c coordT to  \c c 
+/*! \ingroup Coordinate */
+template <int num_dimensions, class coordT>
+inline BasicCoordinate<num_dimensions+1, coordT> 
+  join(const BasicCoordinate<num_dimensions, coordT>& c, const coordT& a);
+
+//! make a shorter BasicCoordinate, by cutting the last element from \c c  
+/*! \ingroup Coordinate */
+template <int num_dimensions, class coordT>
+inline BasicCoordinate<num_dimensions-1, coordT> 
+  cut_last_dimension(const BasicCoordinate<num_dimensions, coordT>& c);
+
 
 //! make a shorter BasicCoordinate, by cutting the first element from \c c  
 /*! \ingroup Coordinate */
