42e9a652b4eff2a9c414db1cf3437bbaf2b7db42
Kris Thielemans <Kris.Thielemans@stir>
2004-09-08 09:56:16 +0000

    - include test_Fourier.P as it wasn't before (Because test_Fourier is not added to TEST_SOURCES at present)

--- src/local/test/test.mk
+++ src/local/test/test.mk
@@ -5,12 +5,16 @@ dir := local/test
 
 $(dir)_TEST_SOURCES :=  \
 test_RigidObject3DTransformation.cxx \
-	test_Quaternion.cxx
+	test_Quaternion.cxx 	
 
 # rule to ignore registries
 # note: has to be before include statement as that changes value of $(dir)
 ${DEST}$(dir)/test_Fourier: ${DEST}$(dir)/test_Fourier$(O_SUFFIX) $(STIR_LIB) 
 	$(CXX) $(CFLAGS)  $(EXE_OUTFLAG)$@ $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
+# need to add this as it's not in the TEST_SOURCES macro
+ifneq ($(MAKECMDGOALS:clean%=clean),clean)
+ -include ${DEST}$(dir)/test_Fourier.P
+endif
 
 ${DEST}$(dir)/test_RigidObject3DTransformation: ${DEST}$(dir)/test_RigidObject3DTransformation$(O_SUFFIX) $(STIR_LIB) 
 	$(CXX) $(CFLAGS)  $(EXE_OUTFLAG)$@ $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
