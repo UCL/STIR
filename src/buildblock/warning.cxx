6530d7f481e40f4c3652330af3c925e0414157f0
Kris Thielemans <Kris.Thielemans@stir>
2004-09-09 14:54:20 +0000

    - output to cerr not stderr to avoid desynchronisation issues with the rest of the library - prefix warning with WARNING - add some error checking that the warning message got formatted

--- src/buildblock/warning.cxx
+++ src/buildblock/warning.cxx
@@ -16,21 +16,32 @@
 */
 /*
     Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
 #include "stir/common.h"
 
 #include <cstdarg>
 #include <stdlib.h>
-
+#include <iostream>
 START_NAMESPACE_STIR
 
 void warning(const char *const s, ...)
 {
   va_list ap;
   va_start(ap, s);
-  vfprintf(stderr, s, ap);
+  const unsigned size=10000;
+  char tmp[size];
+  const int returned_size= vsnprintf(tmp,size, s, ap);
+  if (returned_size<0)
+    std::cerr << "\nWARNING: error formatting warning message" << std::endl;
+  else
+    {
+      std::cerr << "\nWARNING: " << tmp <<std::endl;
+      if (static_cast<unsigned>(returned_size)>=size)
+	std::cerr << "\nWARNING: previous warning message truncated as it exceeds "
+		  << size << "bytes" << std::endl;
+    }
 }
 
 END_NAMESPACE_STIR
