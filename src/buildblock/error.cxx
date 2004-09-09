cf63cfca8d5c51aa24ea6134251e269918206a33
Kris Thielemans <Kris.Thielemans@stir>
2004-09-09 14:55:10 +0000

    - output to cerr not stderr to avoid desynchronisation issues with the rest of the library - prefix error message with ERROR - add some error checking that the error message got formatted - exit using exit(EXIT_FAILURE), not via abort()

--- src/buildblock/error.cxx
+++ src/buildblock/error.cxx
@@ -15,21 +15,33 @@
 */
 /*
     Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
     See STIR/LICENSE.txt for details
 */
 #include "stir/common.h"
 
 #include <cstdarg>
 #include <stdlib.h>
+#include <iostream>
 
 START_NAMESPACE_STIR
 
 void error(const char *const s, ...)
-{
+{  
   va_list ap;
   va_start(ap, s);
-  vfprintf(stderr, s, ap);
-  abort();
+  const unsigned size=10000;
+  char tmp[size];
+  const int returned_size= vsnprintf(tmp,size, s, ap);
+  if (returned_size<0)
+    std::cerr << "\nERROR: but error formatting error message" << std::endl;
+  else
+    {
+      std::cerr << "\nERROR: " << tmp <<std::endl;
+      if (static_cast<unsigned>(returned_size)>=size)
+	std::cerr << "\nWARNING: previous error message truncated as it exceeds "
+		  << size << "bytes" << std::endl;
+    }
+  exit(EXIT_FAILURE);
 }
 END_NAMESPACE_STIR
