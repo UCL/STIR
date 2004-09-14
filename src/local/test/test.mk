f4f8f59ca663ae0010c16880948cd68d10e0edb9
Kris Thielemans <Kris.Thielemans@stir>
2004-09-14 21:05:21 +0000

    - replaced $(CXX) with $(LINK) in explicit linking rules

--- src/local/test/test.mk
+++ src/local/test/test.mk
@@ -10,13 +10,14 @@ test_RigidObject3DTransformation.cxx \
 # rule to ignore registries
 # note: has to be before include statement as that changes value of $(dir)
 ${DEST}$(dir)/test_Fourier: ${DEST}$(dir)/test_Fourier$(O_SUFFIX) $(STIR_LIB) 
-	$(CXX) $(CFLAGS)  $(EXE_OUTFLAG)$@ $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
+	$(LINK) $(EXE_OUTFLAG)$(@)$(EXE_SUFFIX) $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
+
 # need to add this as it's not in the TEST_SOURCES macro
 ifneq ($(MAKECMDGOALS:clean%=clean),clean)
  -include ${DEST}$(dir)/test_Fourier.P
 endif
 
 ${DEST}$(dir)/test_RigidObject3DTransformation: ${DEST}$(dir)/test_RigidObject3DTransformation$(O_SUFFIX) $(STIR_LIB) 
-	$(CXX) $(CFLAGS)  $(EXE_OUTFLAG)$@ $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
+	$(LINK) $(EXE_OUTFLAG)$(@)$(EXE_SUFFIX) $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
 
 include $(WORKSPACE)/test.mk
