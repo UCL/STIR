7502a187d6909d523cbaa4feff746de96d521572
Kris Thielemans <Kris.Thielemans@stir>
2004-09-16 14:06:21 +0000

    - improved rsync lines

--- src/local/scripts/distrib.sh
+++ src/local/scripts/distrib.sh
@@ -1,13 +1,24 @@
+#! /bin/sh
+set -e
+# rsync of website note: stalls on gluon,wren,hurricane, but works fine from shark
+
 VERSION=1.3
 destination=krthie@shell.sf.net:stir/htdocs/
+RSYNC_OPTS=--progress
+#destination=web@wren:htdocs/STIR/
+#RSYNC_OPTS=z  --progress --rsync-path=/home/kris/bin/rsync 
 mkdir -p ~/distrib
 cd ~/distrib
-# somehow make release_${VERSION}.htm
-#cvs -d parapet:/usr/local/cvsroot checkout parapet
+echo somehow make release_${VERSION}.htm
+
+# ecat.tar.gz
+#tar --exclude VC --exclude CVS -czf ecat.tar.gz ecat
+if [ -r parapet ]; then
+  cvs -d parapet:/usr/local/cvsroot checkout parapet
+fi
 cd parapet
 cvs up -dP
 cd PPhead
-rm -rf local include/local
 
 # update VERSION.txt
 echo $VERSION > VERSION.txt
@@ -16,9 +27,10 @@ cvs commit -m "- updated for release of version $VERSION" VERSION.txt
 #check directories etc
 #first remove list of files at the end
 #then
-find . -name "*[xhlc]" -print | xargs grep -l PARAPET >>LICENSE.txt 
-cvs commit  -m "- updated for release of version $VERSION" LICENSE.txt
+#find . -name "*[xhlc]" -print | xargs grep -l PARAPET >>LICENSE.txt 
+#cvs commit  -m "- updated for release of version $VERSION" LICENSE.txt
 
+rm -rf local include/local
 cvs2cl.pl -F trunk
 cp ChangeLog ~/distrib
 mv  ChangeLog ~/parapet/documentation/distrib
@@ -53,19 +65,25 @@ zip -r -l STIR_${VERSION}.zip STIR
 # recon_test_pack
  cd parapet/
 rm -rf recon_test_pack/CVS
-zip -r ../recon_test_pack_${VERSION}.zip recon_test_pack
+zip -ur ../recon_test_pack_${VERSION}.zip recon_test_pack
 tar zcvf ../recon_test_pack_${VERSION}.tar.gz recon_test_pack
+cd ..
+
+chmod go+r *${VERSION}* ChangeLog
+chmod go-wx *${VERSION}* ChangeLog
 
 # put it all there
-rsync  ~/lln/ecat/VC/ecat.dsp ~/lln/ecat.tar.gz \
+rsync --progress -uavz ${RSYNC_OPTS}  ~/lln/ecat/VC/ecat.dsp ~/lln/ecat.tar.gz \
     STIR_${VERSION}.zip VCprojects_${VERSION}.zip \
     recon_test_pack_${VERSION}.tar.gz recon_test_pack_${VERSION}.zip \
     ${destination}registered
-rsync ChangeLog release_${VERSION}.htm STIR_doc_${VERSION}.zip  ${destination}documentation
+rsync --progress -uavz ${RSYNC_OPTS} ChangeLog release_${VERSION}.htm STIR_doc_${VERSION}.zip  \
+    ${destination}documentation
 
 exit 
 # remote
 VERSION=1.3
+echo EDIT documentation/history.htm
 rm  recon_test_pack.tar.gz STIR.zip VCprojects.zip recon_test_pack.zip 
 ln -s STIR_${VERSION}.zip STIR.zip 
 ln -s VCprojects_${VERSION}.zip  VCprojects.zip
@@ -78,3 +96,4 @@ rm -fr doxy
 
 unzip STIR_doc
  chmod go+r STIR_doc_${VERSION}.zip
+R_doc.zip 
