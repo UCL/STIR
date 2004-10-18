6f6306fc940de862e04e10333108065ddd211948
Kris Thielemans <Kris.Thielemans@stir>
2004-10-18 16:06:57 +0000

    - added rule to speed up linking by avoid other registries

--- src/local/analytic/FBP3DRP/exe.mk
+++ src/local/analytic/FBP3DRP/exe.mk
@@ -8,3 +8,10 @@ $(dir)_SOURCES = \
 	FBP3DRPMain.cxx
 
 include $(WORKSPACE)/exe.mk
+
+${DEST}$(dir)/FBP3DRPMain: ${DEST}$(dir)/FBP3DRPMain$(O_SUFFIX) \
+   $(STIR_LIB) \
+   $(filter %recon_buildblock_registries$(O_SUFFIX) %IO_registries$(O_SUFFIX),$(STIR_REGISTRIES))
+	$(LINK) $(EXE_OUTFLAG)$@ $< \
+                 $(filter %recon_buildblock_registries $(O_SUFFIX) %IO_registries$(O_SUFFIX),$(STIR_REGISTRIES)) \
+		 $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
