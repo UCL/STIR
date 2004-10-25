1c158016bfa03a1dac04c5b4fc61e39eab72458a
Kris Thielemans <Kris.Thielemans@stir>
2004-10-25 00:05:09 +0000

    - moved FBP3DRP to global distro

--- src/analytic/FBP3DRP/FBP3DRP.cxx
+++ src/analytic/FBP3DRP/FBP3DRP.cxx
@@ -12,11 +12,24 @@
 */
 /*
     Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, IRSL
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
-#include "local/stir/FBP3DRP/FBP3DRPReconstruction.h"
+#include "stir/analytic/FBP3DRP/FBP3DRPReconstruction.h"
 #ifndef PARALLEL
 
 #define Main  main
