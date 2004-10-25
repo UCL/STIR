e72db7b219d8f58778bf81e6b97a388acc853375
Kris Thielemans <Kris.Thielemans@stir>
2004-10-25 00:05:12 +0000

    - moved FBP3DRP to global distro

--- src/analytic/FBP3DRP/lib.mk
+++ src/analytic/FBP3DRP/lib.mk
@@ -2,11 +2,11 @@
 # $Id$
 #
 
-dir := local/analytic/FBP3DRP
+dir := analytic/FBP3DRP
 $(dir)_LIB_SOURCES:= \
 	ColsherFilter.cxx  FBP3DRPReconstruction.cxx
 
-#$(dir)_REGISTRY_SOURCES:= local_motion_registries.cxx
+#$(dir)_REGISTRY_SOURCES:= registries.cxx
 
 include $(WORKSPACE)/lib.mk
 
