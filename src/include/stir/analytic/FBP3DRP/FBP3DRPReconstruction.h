e86f9621129f3cae5c993a4211e1c340615ea8e1
Kris Thielemans <Kris.Thielemans@stir>
2004-10-25 00:05:14 +0000

    - moved FBP3DRP to global distro

--- src/include/stir/analytic/FBP3DRP/FBP3DRPReconstruction.h
+++ src/include/stir/analytic/FBP3DRP/FBP3DRPReconstruction.h
@@ -15,6 +15,19 @@
 /*
     Copyright (C) 2000 PARAPET partners
     Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
@@ -27,7 +40,7 @@
 #include "stir/ProjDataInfoCylindricalArcCorr.h"
 #include "stir/recon_buildblock/ForwardProjectorByBin.h"
 #include "stir/recon_buildblock/BackProjectorByBin.h"
-#include "local/stir/FBP3DRP/ColsherFilter.h"
+#include "stir/analytic/FBP3DRP/ColsherFilter.h"
 #include "stir/shared_ptr.h"
 
 START_NAMESPACE_STIR
@@ -184,7 +197,7 @@ public:
 protected:
 
   //! Switch to display intermediate images, 0,1,2
-  int disp;
+  int display_level;
 
   //! Switch to save files after each segment, 0 or 1
   int save_intermediate_files;
