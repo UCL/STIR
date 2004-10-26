8acbb3c48a49a828951fdf1dad8cbd5145ca3483
Kris Thielemans <Kris.Thielemans@stir>
2004-10-26 16:55:06 +0000

    - automated the LICENSE.txt processing - added switches to do only part of the processing (currently by editing this file though) - find LLN files in root directory of parapet

--- src/local/scripts/distrib.sh
+++ src/local/scripts/distrib.sh
@@ -1,9 +1,19 @@
 #! /bin/sh
+do_update=1
+do_license=1
+do_ChangeLog=1
+do_doc=1
+do_zip_source=1
+do_recon_test_pack=1
+do_transfer=1
+
 set -e
 # rsync of website note: stalls on gluon,wren,hurricane, but works fine from shark
 VERSION=1.3
 cd $WORKSPACE/../..
 DISTRIB=`pwd`/distrib
+LLN=${DISTRIB}/../lln
+WORKSPACE=${DISTRIB}/parapet/PPhead 
 
 destination=krthie@shell.sf.net:stir/htdocs/
 RSYNC_OPTS=--progress
@@ -11,17 +21,24 @@ RSYNC_OPTS=--progress
 #RSYNC_OPTS=  --progress --rsync-path=/home/kris/bin/rsync 
 mkdir -p ${DISTRIB}
 cd ${DISTRIB}
-echo somehow make release_${VERSION}.htm
 
-echo LLN stuff
-# ecat.tar.gz
+echo "Did you  make ${DISTRIB}/release_${VERSION}.htm ?"
+
+echo LLN stuff (still by hand)
+if [ ! -r ${LLN}/ecat.tar.gz ]; then
+  echo Need LLN in ${LLN}
+  exit 1
+fi
 #tar --exclude VC --exclude CVS -czf ecat.tar.gz ecat
+
 if [ ! -r parapet ]; then
   cvs -d parapet:/usr/local/cvsroot checkout parapet
   cd parapet
 else
   cd parapet
-  cvs up -dP
+  if [ $do_update = 1 ]; then
+    cvs up -dP
+  fi
 fi
 rm -f STIR
 ln -s PPhead STIR
@@ -30,52 +47,78 @@ cd PPhead
 # update VERSION.txt
 echo $VERSION > VERSION.txt
 cvs commit -m "- updated for release of version $VERSION" VERSION.txt
+
 # update LICENSE.txt
-#check directories etc
-#first remove list of files at the end
-#then
-# find . -path ./local -prune -path ./include/local -prune -name "*[xhlc]" -o -print|grep -v CVS | grep -v .dsp| xargs grep -l PARAPET >>LICENSE.txt 
-#cvs commit  -m "- updated for release of version $VERSION" LICENSE.txt
+if [ $do_license = 1 ]; then
+  cd $WORKSPACE
+  # put version in there
+  cat LICENSE.txt | \
+  sed "s/Licensing information for STIR .*/Licensing information for STIR $VERSION/" \
+  > tmp_LICENSE.txt
+  # remove list of files at the end (dangerous: relies on the text in the file)
+  END_STRING="----------------------------------------------------"
+  AWK_PROG="{ if( \$1 ~ \"$END_STRING\") {
+                 exit 0;
+            } else {
+              print \$0
+            }
+          }"
+  awk "$AWK_PROG" tmp_LICENSE.txt > LICENSE.txt
+  echo $END_STRING >> LICENSE.txt
+  #then add new list on again
+  find . -path ./local -prune -path ./include/local -prune \
+     -o -name "*[xhlkc]"  -print|grep -v CVS | xargs grep -l PARAPET >>LICENSE.txt 
+  cvs commit  -m "- updated for release of version $VERSION" LICENSE.txt
+fi
 
 # make ChangeLog file
-# maybe use --accum
-mv local xxlocal
-cvs2cl.pl -I 'xxlocal/' -I 'include/local'  --no-indent -F trunk
-mv xxlocal local
-cp ChangeLog ${DISTRIB}
+if [ $do_ChangeLog = 1 ]; then
+  cd $WORKSPACE
+  # maybe use --accum
+  mv local xxlocal
+  cvs2cl.pl -I 'xxlocal/' -I 'include/local'  --no-indent -F trunk
+  mv xxlocal local
+  cp ChangeLog ${DISTRIB}
+fi
 
-# make doxygen
-doxygen
-# make documentation PDFs BY HAND
-cd ../documentation
-make
-zip -ur ${DISTRIB}/STIR_doc_${VERSION}.zip *.pdf  doxy
+if [ $do_doc = 1 ]; then
+  cd $WORKSPACE
+  # make doxygen
+  doxygen
+  # make documentation PDFs BY HAND
+  cd ../documentation
+  make
+  zip -ur ${DISTRIB}/STIR_doc_${VERSION}.zip *.pdf  doxy
+fi
 
-cd ${DISTRIB}
-rm -f parapet/all.zip parapet/VCprojects.zip
-WORKSPACE=${DISTRIB}/parapet/PPhead zipit --distrib
-WORKSPACE=${DISTRIB}/parapet/PPhead zipproj --distrib
-mv parapet/VCprojects.zip VCprojects_${VERSION}.zip 
-mv parapet/all.zip STIR_${VERSION}.zip 
+if [ $do_zip_source ]; then
+  cd ${DISTRIB}
+  rm -f parapet/all.zip parapet/VCprojects.zip
+  zipit --distrib
+  zipproj --distrib
+  mv parapet/VCprojects.zip VCprojects_${VERSION}.zip 
+  mv parapet/all.zip STIR_${VERSION}.zip 
+fi
 
+if [ $do_recon_test_pack = 1 ]; then
+  cd ${DISTRIB}/parapet/
+  #rm -rf recon_test_pack/CVS
+  zip -ulr ../recon_test_pack_${VERSION}.zip recon_test_pack -i CVS/ CVS/*
+  #tar zcvf ../recon_test_pack_${VERSION}.tar.gz recon_test_pack
+fi
 
-# recon_test_pack
- cd parapet/
-#rm -rf recon_test_pack/CVS
-zip -ulr ../recon_test_pack_${VERSION}.zip recon_test_pack -i CVS/ CVS/*
-#tar zcvf ../recon_test_pack_${VERSION}.tar.gz recon_test_pack
-cd ..
+if [ $do_transfer = 1 ]; then
+  chmod go+r *${VERSION}* ChangeLog
+  chmod go-wx *${VERSION}* ChangeLog
 
-chmod go+r *${VERSION}* ChangeLog
-chmod go-wx *${VERSION}* ChangeLog
-
-# put it all there
-rsync --progress -uavz ${RSYNC_OPTS}  ~/lln/ecat/VC/ecat.dsp ~/lln/ecat.tar.gz \
+  # put it all there
+  rsync --progress -uavz ${RSYNC_OPTS}  ${LLN}/ecat/VC/ecat.dsp ${LLN}/ecat.tar.gz \
     STIR_${VERSION}.zip VCprojects_${VERSION}.zip \
     recon_test_pack_${VERSION}.zip \
     ${destination}registered
-rsync --progress -uavz ${RSYNC_OPTS} ChangeLog release_${VERSION}.htm STIR_doc_${VERSION}.zip  \
+  rsync --progress -uavz ${RSYNC_OPTS} ChangeLog release_${VERSION}.htm STIR_doc_${VERSION}.zip  \
     ${destination}documentation
+fi
 
 exit 
 # remote
