a8276565106ee2fd2a2b97e6085a42e9ab877506
Kris Thielemans <Kris.Thielemans@stir>
2004-10-27 10:49:39 +0000

    - corrected sizes for the Advance [Roman Krais] - corrected sizes for the Discovery ST

--- src/buildblock/Scanner.cxx
+++ src/buildblock/Scanner.cxx
@@ -101,18 +101,20 @@ Scanner::Scanner(Type scanner_type)
   {
     // 283 bins (non-uniform sampling) 
     // 281 bins (uniform sampling)
-    set_params(Advance,string_list("GE Advance", "Advance"), 18, 283,281,2*336,469.5F, 8.5F, 1.96F, 0,0,0,0,0, 1);
+    /* crystal size 4x8x30*/
+    set_params(Advance,string_list("GE Advance", "Advance"), 18, 283,281,2*336,471.875F, 8.5F, 1.970177F, 0,0,0,6,6, 1);
   }
   else if (scanner_type == DiscoveryLS)
   {
     // identical to Advance
-    set_params(DiscoveryLS,string_list("GE Discovery LS", "Discovery LS"), 18, 283,281,2*336,469.5F, 8.5F, 1.96F, 0,0,0,0,0, 1);
+    set_params(DiscoveryLS,string_list("GE Discovery LS", "Discovery LS"), 18, 283,281,2*336,471.875F, 8.5F, 1.970177F, 0,0,0,6,6, 1);
   }
   else if (scanner_type == DiscoveryST)
   {
     // 249 bins (non-uniform sampling) 
-    // 221 bins (uniform sampling)
-    set_params(DiscoveryST,string_list("GE Discovery ST", "Discovery ST"), 24, 249,221,2*210,451.5F, 6.3F, 3.19F, 0,0,0,0,0, 1);// TODO check sizes
+    // 221 ? bins (uniform sampling)
+    /* crystal size: 6.3 x 6.3 x 30 mm*/
+    set_params(DiscoveryST,string_list("GE Discovery ST", "Discovery ST"), 24, 249,221,2*210,451.5F, 6.52916F, 3.1695F, 0,0,0,6,6, 1);// TODO check sizes
   }
   else if (scanner_type == HZLR)
     set_params(HZLR,string_list("Positron HZL/R"), 32, 256,2* 192, 780.0F, 5.1875F, 2.F, 0,0,0,0,0, 1);
