a971bc8c9fa8d7d5970a265dcb2500c4c9a5f22b
Kris Thielemans <Kris.Thielemans@stir>
2004-12-07 12:08:54 +0000

    - added round_to to be able to use different output types

--- src/include/stir/round.h
+++ src/include/stir/round.h
@@ -17,6 +17,8 @@
 */
 /*
     Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This software is distributed under the terms of the GNU Lesser General 
+    Public Licence (LGPL).
     See STIR/LICENSE.txt for details
 */
 #include "stir/BasicCoordinate.h"
@@ -57,6 +59,30 @@ inline int round(const double x);
 template <int num_dimensions, class elemT>
 inline BasicCoordinate<num_dimensions,int>
 round(const BasicCoordinate<num_dimensions,elemT>& x);
+
+
+//! Implements rounding of floating point numbers to other integer types
+/*!
+   This function is templated in the output type for convenience of implementation.
+
+   \warning if an unsigned type is used for \x integerT, the result for negative \a x
+   will be system dependent
+
+   \see round(const float)
+
+   \todo add code to check that \c integerT is really an integer type at compilation time
+ */
+template <typename integerT>
+inline void
+round_to(integerT& result, const float x);
+
+//! Implements rounding of a BasicCoordinate object to other integer types
+/*! \see round_to(integerT, float)
+ */
+template <int num_dimensions, class integerT, class elemT>
+inline void
+round_to(BasicCoordinate<num_dimensions,integerT>& result,
+	 const BasicCoordinate<num_dimensions,elemT>& x);
 
 //@}
 
