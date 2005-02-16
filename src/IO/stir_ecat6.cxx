21bc9dc0c802ad3784106b379065ef4b29ad4158
Kris Thielemans <Kris.Thielemans@stir>
2005-02-16 16:43:40 +0000

    - corrected BUG: changed delete of a char* to delete[] in ECAT6_to_VoxelsOnCartesianGrid. This was a memory allocation problem which in principle could cause problems (but didn't seem to). It was found by Frederic Schoenahl by running valgrind.

--- src/IO/stir_ecat6.cxx
+++ src/IO/stir_ecat6.cxx
@@ -17,6 +17,18 @@
 /*
     Copyright (C) 2000 PARAPET partners
     Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
@@ -291,7 +303,7 @@ ECAT6_to_VoxelsOnCartesianGrid(const int frame_num, const int gate_num, const in
 #endif
     } // end loop on planes
     
-  delete cti_data;
+  delete[] cti_data;
   return image_ptr;
 }
 
