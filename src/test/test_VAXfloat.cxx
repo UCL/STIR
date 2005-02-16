40af33d819d47792d00fca230f423684b6dff77a
Kris Thielemans <Kris.Thielemans@stir>
2005-02-16 17:07:36 +0000

    - fixed memory leak

--- src/test/test_VAXfloat.cxx
+++ src/test/test_VAXfloat.cxx
@@ -28,6 +28,18 @@
 */
 /*
     Copyright (C) 2004- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 #include "stir/ByteOrderDefine.h"
@@ -139,7 +151,8 @@ int main(int argc, char **argv)
     }
 
   fflush(stderr);
-  fclose (fptr);
+  fclose(fptr);
+  free(bufr);
   if (success)
     fprintf(stderr, "All tests ok\n");
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
