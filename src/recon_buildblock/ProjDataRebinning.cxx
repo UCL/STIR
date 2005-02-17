219538d5bb207a8d606136c9d8cd76d516e96319
Kris Thielemans <Kris.Thielemans@stir>
2005-02-17 20:38:32 +0000

    - added extra checks in set_up() - added set/get functions [Oliver Nix]

--- src/recon_buildblock/ProjDataRebinning.cxx
+++ src/recon_buildblock/ProjDataRebinning.cxx
@@ -11,13 +11,25 @@
   $Revision$
 */
 /*
-    Copyright (C) 2003- $Date$, Hammersmith Imanet
+    Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
 #include "stir/recon_buildblock/ProjDataRebinning.h"
 #include "stir/Succeeded.h"
-
+#include "stir/is_null_ptr.h"
 
 START_NAMESPACE_STIR
 
@@ -47,7 +59,7 @@ bool
 ProjDataRebinning::
 post_processing()
 {
-  if (output_filename_prefix.length() == 0)// KT 160899 changed name of variable
+  if (output_filename_prefix.length() == 0)
   { warning("You need to specify an output prefix\n"); return true; }
 
   if (input_filename.length() == 0)
@@ -59,9 +71,6 @@ post_processing()
 #endif
  
   proj_data_sptr= ProjData::read_from_file(input_filename);
-
-  if(max_segment_num_to_process > proj_data_sptr->get_max_segment_num()) 
-    { warning("Range error in number of segments\n"); return true;}
   
   return false;
 }
@@ -77,6 +86,7 @@ initialise(const string& parameter_filename)
 
     set_defaults();
     ask_parameters();
+
   }
 
 else
@@ -97,11 +107,64 @@ Succeeded
 ProjDataRebinning::
 set_up()
 {
+  if (is_null_ptr(proj_data_sptr))
+    {
+      warning("ProjDataRebinning: input projection data not set");
+      return Succeeded::no;
+    } 
+  if (max_segment_num_to_process == -1)
+    max_segment_num_to_process = proj_data_sptr->get_max_segment_num();
+  else  if(max_segment_num_to_process > proj_data_sptr->get_max_segment_num()) 
+    { 
+      warning("ProjDataRebinning: Range error in number of segments to process.\n"
+	      "Max segment number in data is %d while you asked for %d",
+	      proj_data_sptr->get_max_segment_num(),
+	      max_segment_num_to_process); 
+      return Succeeded::no;
+    }
   return Succeeded::yes;
 }
 
 ProjDataRebinning::
 ~ProjDataRebinning()
 {}
+
+void ProjDataRebinning::set_max_segment_num_to_process(int ns)
+{ max_segment_num_to_process = ns;
+}
+
+int ProjDataRebinning::get_max_segment_num_to_process() const
+{ return max_segment_num_to_process;
+}
+void 
+ProjDataRebinning::set_output_filename_prefix(const string& s)
+{
+  output_filename_prefix = s;
+}
+string 
+ProjDataRebinning::get_output_filename_prefix() const
+{
+  return output_filename_prefix;
+}
+
+
+shared_ptr<ProjData> 
+ProjDataRebinning::
+get_proj_data_sptr()
+{
+  /* KT: deleted warning messages about null pointers. 
+     The user should check this, and might not want the have the 
+     warnings written to stderr. */
+     return proj_data_sptr;
+ } 
+
+
+void 
+ProjDataRebinning::
+set_input_proj_data_sptr(const shared_ptr<ProjData>& new_proj_data_sptr)
+{
+  proj_data_sptr = new_proj_data_sptr;
+} 
+
 
 END_NAMESPACE_STIR
