3077b51e030b28477d6d3a6cbdf4d77962896aeb
Kris Thielemans <Kris.Thielemans@stir>
2005-02-17 20:32:01 +0000

    [ most changes by Oliver Nix, some minor editing by KT] - added more doxygen and some cleanup - added a set of get and set utility functions to access the rebinning parameters

--- src/include/stir/recon_buildblock/FourierRebinning.h
+++ src/include/stir/recon_buildblock/FourierRebinning.h
@@ -8,28 +8,41 @@
   \ingroup recon_buildblock
   \author Claire LABBE
   \author Kris Thielemans
+  \author Oliver Nix
   \author PARAPET project
   $Date$
   $Revision$
 */
 /*
     Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, Hammersmith Imanet
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    Copyright (C) 2004 - 2005 DKFZ Heidelberg, Germany KTTODO check text
+
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
-#ifndef __stir_FORE_FourierRebinning_H__
-#define __stir_FORE_FourierRebinning_H__
+#ifndef __stir_recon_buildblock_FourierRebinning_H__
+#define __stir_recon_buildblock_FourierRebinning_H__
 
-#include <complex.h>
 #include "stir/recon_buildblock/ProjDataRebinning.h"
 #include "stir/RegisteredParsingObject.h"
+#include <complex.h>
 
 
 START_NAMESPACE_STIR
-template <typename elemT> class SegmentByView;
 template <typename elemT> class SegmentBySinogram;
-//template <typename elemT> class Sinogram;
 template <int num_dimensions, typename elemT> class Array;
 class Succeeded;
 
@@ -87,21 +100,24 @@ class PETCount_rebinned
   d) Calculate the 2D inverse FFT of each Pr(w,k) to get the rebinned sinogram Pr(s,f);
 
   As FORE is based on a high frequency approximation, it is necessary to handle separately low and high frequencies. 
-  This is done by subdividing the (w,k) plane into three sub regions defining by two parameters 
+  This is done by subdividing the (w,k) plane into three sub regions defined by two parameters 
   in Fourier space, w (the continuous frequency corresponding to the radial coordinates s) and k 
   (the integer Fourier index corresponding to the azimuthal angle f), and by applying in each region 
   a different method to estimated the rebinned sinogram.
 
-  The rebinned data are represented by  in spatial space with |s|<=R, 0<=f<p, |z|<=L/2, 
+  KTTODO ^A in the next sentence?
+
+  The rebinned data are represented by  in spatial space with |s|\<=R, 0<=f\<p, |z|\<=L/2, 
   where L is the length of the axial FOV and R the ring radius.
   In the high frequencies (region 1), the rebinned data are estimated using Fourier rebinning.
   In the second high frequency region (region 2), the consistency condition is not satisfied 
-  and hence all the rebinned data are forced to 0: Pr(w,k, z) = 0, when |k/w|>=R; |w|>wlim or |k|>klim.
+  and hence all the rebinned data are forced to 0: Pr(w,k, z) = 0, when |k/w|\>=R; |w|\>wlim or |k|\>klim.
   Finally, in the low-frequency (region 3), Fourier rebinning is not applicable. 
   Therefore the rebinned data are estimated using only the oblique sinograms with 
   a small value of d : dlim. Owing to the small value of d, the axial shift can be 
   neglected as in the SSRB approximation.
 */
+
 class FourierRebinning : public   RegisteredParsingObject<
                                   FourierRebinning,
                                   ProjDataRebinning,
@@ -122,13 +138,11 @@ class FourierRebinning : public   RegisteredParsingObject<
     int deltamin;            
 //! kc index for consistency
     int kc;                  
-//CL10/03/00 Remove these parameters as not useful
-        //   int LUT;                 
-
-//    int store;               
-    
+//! fore_debug_level. Setting it to >0 will produce some debug information 
+    int fore_debug_level; 
+        
  public:
-    //! default constructor calls set_defaults();
+//! default constructor calls set_defaults();
     FourierRebinning();
 
 //! This method returns the type of the algorithm for the rebinning
@@ -136,10 +150,22 @@ class FourierRebinning : public   RegisteredParsingObject<
         { return("FORE"); }
 
 
-    //! This method creates a stack of 2D rebinned sinograms from the whole 3D data set (i.e. the ProjData data) and saves it.
+//! This method creates a stack of 2D rebinned sinograms from the whole 3D data set (i.e. the ProjData data) and saves it.
    Succeeded rebin();
 
-
+//! A set of get and set utility functions to access the rebinning parameters     
+  inline void set_kmin(int km){kmin = km;}
+  inline void set_wmin(int wm){wmin = wm;}
+  inline void set_deltamin(int dm){deltamin = dm;}
+  inline void set_kc(int kcc) {kc = kcc;}
+  inline void set_fore_debug_level(int fdebug){fore_debug_level = fdebug;}
+  
+  inline int get_kmin(){return kmin;}
+  inline int get_wmin(){return wmin;}
+  inline int get_deltamin(){return deltamin;}
+  inline int get_kc() {return kc;}
+  inline int get_fore_debug_level(){return fore_debug_level;}     
+ 
  private:
 
 /*! 
@@ -160,8 +186,7 @@ class FourierRebinning : public   RegisteredParsingObject<
 
 /*!
   \brief This method takes as input the real 3D data set
-  (in which the number of
-  views have been extended  to a number of power of 2)
+  (in which the number of views have been extended to a number of power of 2)
   and  returns the rebinned sinograms in Fourier space, their weighting factors
   as well as the counter rebinned elements
 
@@ -170,49 +195,35 @@ class FourierRebinning : public   RegisteredParsingObject<
   z - (tk/w) with t=((ring0 -ring1)*ring_spacing/(2*R) with R=ring_radius, 
   Pm(w,k) = Pm(w,k) + Pij(w,k) (i=ring0 and j=ring1), and m is the nearest integer to (i+j) -k(i-j)/(Rw)).
 */
-    void do_rebinning( SegmentBySinogram<float>&segment, int &num_tang_poss_pow2,
+    void do_rebinning( const SegmentBySinogram<float>&segment, int &num_tang_poss_pow2,
                        int &num_views_pow2, const int &num_planes, const float& average_ring_difference_in_segment,
                        const float &half_distance_between_rings,const float &sampling_distance_in_s,
                        const float &radial_sampling_freq_w, const float &R_field_of_view_mm,const float &ratio_ring_spacing_to_ring_radius,
                        PETCount_rebinned &num_rebinned_total, Array<3,std::complex<float> > &FTdata, Array<3,float> &weight);
 
-//! This method takes as input the segment or the 3D sinograms and returns the new segment on where views have been added by num_views_to_add. This method is general used fro direct planes.
-    //    void do_mashing(SegmentBySinogram<float> &direct_sinos);
-    
 /*!
   This method takes as input the information of the 3D data set (ProjData)
   as well as the reconstruction parameters and prints out the informations of reconstruction
   parameters implemented in FORE as well as the CPU and relative timing of differents process
   (i.e I/O, rebinning, backprojection, matrix handling).
+  fore_debug_level must be set to a integer value > 0
 */
     void do_log_file();
 
-
-//CL10/03/00 Remove these lines as not useful
-#if 0
-//  This is a function to save on fly the look-up-table
-void do_LUT(char lutfilename[80], Array<3,std::complex<float> > &weight);
-
-//  This is a function to store the look-up-table (weighting factors)
-void do_storeLUT(char lutfilename[80], Array<3,float> &weight);
-#endif
-
 //! This is a function to display the current counter of all rebinned elements 
-void do_display_count(PETCount_rebinned &num_rebinned_total);
+    void do_display_count(PETCount_rebinned &num_rebinned_total);
 
 
 //! This is a function to adjust the number of views of a segment to the next power of 2
-void do_adjust_nb_views_to_pow2(SegmentBySinogram<float> &segment) ;
+    void do_adjust_nb_views_to_pow2(SegmentBySinogram<float> &segment) ;
 
-//! This function checks if the steering and input paramters for FORE are inside the allowed range of parameters
-//void fore_check_parameters(int num_tang_poss_pow2, int num_views_pow2, int max_segment_num_to_process,
-//                           int deltamin, int kmin, int wmin, int kc);
- void fore_check_parameters(int num_tang_poss_pow2, int num_views_pow2, int max_segment_num_to_process);
+//! This function checks if the steering and input paramters for FORE are inside the possible range of parameters
+    void fore_check_parameters(int num_tang_poss_pow2, int num_views_pow2, int max_segment_num_to_process);
 
 
  protected:
   virtual bool post_processing();  
-  // TODO virtual void set_defaults();
+  virtual void set_defaults();
   virtual void initialise_keymap();
 
 };
