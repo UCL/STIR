ddd4a5c4e12b5b7a7917e61d17b2a8bcfa208292
Kris Thielemans <Kris.Thielemans@stir>
2005-02-17 20:03:09 +0000

    - changed type of stored objects and of arguments of constructor from ChainedBinNormalisation to BinNormalisation, as this obviously works with any BinNormalisation.

--- src/include/stir/recon_buildblock/ChainedBinNormalisation.h
+++ src/include/stir/recon_buildblock/ChainedBinNormalisation.h
@@ -13,6 +13,18 @@
 */
 /*
     Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
@@ -81,8 +93,8 @@ public:
   */
   ChainedBinNormalisation();
 
-ChainedBinNormalisation(shared_ptr<ChainedBinNormalisation> const& apply_first,
-		        shared_ptr<ChainedBinNormalisation> const& apply_second);
+ChainedBinNormalisation(shared_ptr<BinNormalisation> const& apply_first,
+		        shared_ptr<BinNormalisation> const& apply_second);
 
   //! Checks if we can handle certain projection data.
   /*! Calls set_up for the BinNormalisation members. */
@@ -104,8 +116,8 @@ ChainedBinNormalisation(shared_ptr<ChainedBinNormalisation> const& apply_first,
  
 
 private:
-  shared_ptr<ChainedBinNormalisation> apply_first;
-  shared_ptr<ChainedBinNormalisation> apply_second;
+  shared_ptr<BinNormalisation> apply_first;
+  shared_ptr<BinNormalisation> apply_second;
   // parsing stuff
   virtual void set_defaults();
   virtual void initialise_keymap();
