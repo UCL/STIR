9e18687100d4a87c131c07c8df8947cad29d660c
Kris Thielemans <Kris.Thielemans@stir>
2005-02-17 21:23:31 +0000

    - added LGPL statement

--- src/include/stir/data/SinglesRatesFromECAT7.h
+++ src/include/stir/data/SinglesRatesFromECAT7.h
@@ -1,20 +1,31 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup local_buildblock
 
   \brief Declaration of class SinglesRatesFromECAT7
-
+  \todo file-name is incorrect (misses an s)
   \author  Sanida Mustafovic and Kris Thielemans
   $Date$
   $Revision$ 
-*/
-
-/*
-    Copyright (C) 2000- $Date$, IRSL
-    See STIR/LICENSE.txt for details
 */
 
 #ifndef __stir_SinglesRatesFromECAT7_H__
