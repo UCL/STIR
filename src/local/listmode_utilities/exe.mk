ccdf9148092a36e458cc2bfed995057a8cf898fc
Kris Thielemans <Kris.Thielemans@stir>
2005-02-24 14:48:51 +0000

    - moved find_motion_corrected_norm_factors.cxx to motion_utilities

--- src/local/listmode_utilities/exe.mk
+++ src/local/listmode_utilities/exe.mk
@@ -8,7 +8,6 @@ $(dir)_SOURCES = \
 	lm_to_projdata_bootstrap.cxx \
 	lm_to_projdata_with_MC.cxx \
 	lm_fansums.cxx \
-	find_motion_corrected_norm_factors.cxx \
 	generate_headcurve.cxx \
 	list_time_events.cxx
 
