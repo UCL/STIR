7258f4b16687c7c66088bfc21e736761d4a9ed30
Kris Thielemans <Kris.Thielemans@stir>
2005-02-24 14:50:10 +0000

    - moved some programs here

---
+++ src/local/motion_utilities/exe.mk
@@ -0,0 +1,18 @@
+#
+# $Id$
+#
+
+dir:=local/motion_utilities
+
+$(dir)_SOURCES = \
+	rigid_object_transform_projdata.cxx \
+	rigid_object_transform_image.cxx \
+	move_image.cxx \
+	move_projdata.cxx \
+	remove_corrupted_sinograms.cxx \
+	fwd_image_and_fill_missing_data.cxx \
+	add_planes_to_image.cxx \
+	sync_polaris.cxx \
+	find_motion_corrected_norm_factors.cxx
+
+include $(WORKSPACE)/exe.mk
