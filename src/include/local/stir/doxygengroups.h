b0657b14af589af5b35c8da035925b3fb1afb932
Kris Thielemans <Kris.Thielemans@stir>
2005-02-24 15:08:14 +0000

    *** empty log message ***

---
+++ src/include/local/stir/doxygengroups.h
@@ -0,0 +1,19 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    For internal GE use only
+*/
+
+// define some doxygen groups and namespace
+// This file does not contain any code
+/*!
+\defgroup motion Items relating to motion and motion correction
+\ingroup STIR_library
+*/
+
+/*!
+\defgroup motion_utilities Utility programs for motion and motion correction
+\ingroup utilities
+*/
