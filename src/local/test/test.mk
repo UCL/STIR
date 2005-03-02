70fe32ea126824289c79f0e2d27d43b8a879a347
Kris Thielemans <Kris.Thielemans@stir>
2005-03-02 11:04:08 +0000

    - added test_proj_data_info_LOR

--- src/local/test/test.mk
+++ src/local/test/test.mk
@@ -5,7 +5,8 @@ dir := local/test
 
 $(dir)_TEST_SOURCES :=  \
 test_RigidObject3DTransformation.cxx \
-	test_Quaternion.cxx 	
+	test_Quaternion.cxx \
+	test_proj_data_info_LOR.cxx
 
 # rule to ignore registries
 # note: has to be before include statement as that changes value of $(dir)
@@ -16,6 +17,9 @@ ${DEST}$(dir)/test_Fourier: ${DEST}$(dir)/test_Fourier$(O_SUFFIX) $(STIR_LIB)
 ifneq ($(MAKECMDGOALS:clean%=clean),clean)
  -include ${DEST}$(dir)/test_Fourier.P
 endif
+
+run_$(dir)/test_proj_data_info_LOR: $(DEST)$(dir)/test_proj_data_info_LOR PHONY_TARGET
+	$<  4 5 60 7 8 40 10 45 12 5 3 60
 
 ${DEST}$(dir)/test_RigidObject3DTransformation: ${DEST}$(dir)/test_RigidObject3DTransformation$(O_SUFFIX) $(STIR_LIB) 
 	$(LINK) $(EXE_OUTFLAG)$(@)$(EXE_SUFFIX) $< $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
