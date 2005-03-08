abafd3aa7743645f6b97b330f807f20a1c949745
Kris Thielemans <Kris.Thielemans@stir>
2005-03-08 14:35:28 +0000

    - corrected doxygen

--- src/include/stir/cross_product.h
+++ src/include/stir/cross_product.h
@@ -3,6 +3,22 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file 
   \ingroup buildblock 
@@ -13,11 +29,6 @@
   $Revision$
 
 */
-/*
-    Copyright (C) 2003- $Date$, Hammersmith Imanet
-    See STIR/LICENSE.txt for details
-*/
-
 
 #include "stir/CartesianCoordinate3D.h"
 
@@ -26,10 +37,11 @@ START_NAMESPACE_STIR
 /*!
   \ingroup buildblock
   \brief the cross-product for 3-dimensional coordinates.
+
   \warning This implements <strong>minus</strong> the 'usual' definition 
   of the cross-product. This is done because STIR uses a left-handed 
   coordinate system. The definition of \a cross_product is such that 
-  \f$ {a, b, a\cross b}\f$ forms a <i>left</i>-handed
+  \f$ {a, b, a\times b}\f$ forms a <i>left</i>-handed
   coordinate system.
 */
 
