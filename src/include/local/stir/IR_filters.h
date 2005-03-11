311a6b631a0da53ac88f9b06783de2d4ba896fc3
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-03-11 17:42:28 +0000

    - removed Bspline stuff that was commented out

--- src/include/local/stir/IR_filters.h
+++ src/include/local/stir/IR_filters.h
@@ -1,23 +1,35 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
-  \file
-  \ingroup Filters
+  \file 
+  \ingroup numerics_buildblock
   \brief Implementation of the IIR and FIR filters
-  
+
   \author Charalampos Tsoumpas
   \author Kris Thielemans
-  
+
   $Date$
   $Revision$
 */
-/*
-    Copyright (C) 2004- $Date$, Hammersmith Imanet
-    See STIR/LICENSE.txt for details
-*/
-
 
 #include "stir/shared_ptr.h"
-
-//#include <iterator_traits.h>
 #include <vector>
 #include <iostream>
 #include <cmath>
@@ -52,54 +64,6 @@ FIR_filter(RandIter1 output_begin_iterator,
 						   const RandIter3 input_factor_begin_iterator,
 						   const RandIter3 input_factor_end_iterator,
 						   const bool if_initial_exists);
-
-template <class RandIter>
-float
-inline
-sequence_sum(RandIter input_iterator,
-	   unsigned int Nmax,
-	   float pole, // to be complex as well?
-	   double precision 
-	   );
-
-template <class RandIter1, 
-		  class RandIter2>
-void
-inline 
-BSplines_coef(RandIter1 c_begin_iterator, 
-			   RandIter1 c_end_iterator,
-			   RandIter2 input_signal_begin_iterator, 
-			   RandIter2 input_signal_end_iterator);
-
-template <class elemT>
-elemT 
-inline
-BSpline(const elemT relative_position) ;
-
-/*
-enum enum_spline_level 
-{
-	constant, linear, cubic, quadratic
-} spline_level;
-*/
-
-
-template <class elemT>
-inline
-elemT BSplines_weight(elemT relative_position);
-
-template <class RandIter1, 
-		  class RandIter2>
-void
-inline 
-BSplines1DRegular(RandIter1 c_begin_iterator, 
-				  RandIter1 c_end_iterator,
-				  RandIter2 input_signal_begin_iterator, 
-				  RandIter2 input_signal_end_iterator);
-
-
-
-//*/
 END_NAMESPACE_STIR
 
 #include "local/stir/IR_filters.inl"
