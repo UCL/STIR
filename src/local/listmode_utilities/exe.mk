636dd4dd1c2b234ce0b68ad7f2812deabee38537
Kris Thielemans <Kris.Thielemans@stir>
2005-03-11 13:43:58 +0000

    - added change_lm_time_tags.cxx

--- src/local/listmode_utilities/exe.mk
+++ src/local/listmode_utilities/exe.mk
@@ -9,6 +9,7 @@ $(dir)_SOURCES = \
 	lm_to_projdata_with_MC.cxx \
 	lm_fansums.cxx \
 	generate_headcurve.cxx \
-	list_time_events.cxx
+	list_time_events.cxx \
+	change_lm_time_tags.cxx
 
 include $(WORKSPACE)/exe.mk
