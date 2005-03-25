15d83bcc7a34cce2d042b3e546c806af8ebd7154
Kris Thielemans <Kris.Thielemans@stir>
2005-03-25 19:56:14 +0000

    - added reference_position_is_average_position_in_frame - a tiny bit of doxygen

--- src/include/local/stir/listmode/LmToProjDataWithMC.h
+++ src/include/local/stir/listmode/LmToProjDataWithMC.h
@@ -1,21 +1,21 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2003- $Date$, Hammersmith Imanet
+    For GE internal use only
+*/
 /*!
-
   \file
   \ingroup listmode
-  \brief Class for rebinning listmode files with motion correction
+  \brief Declaration of class stir::LmToProjDataWithMC
+
     
   \author Sanida Mustafovic
   \author Kris Thielemans
       
   $Date$
   $Revision$
-*/
-/*
-    Copyright (C) 2003- $Date$, Hammersmith Imanet
-    See STIR/LICENSE.txt for details
 */
 
 #ifndef __stir_listmode_LmToProjDataWithMC_H__
@@ -29,6 +29,12 @@
 
 START_NAMESPACE_STIR
 
+/*! \ingroup listmode
+  \brief Class for binning list mode files with motion correction
+
+  Implements LOR repositioning during bining of list mode data into sinograms.
+
+*/
 class LmToProjDataWithMC : public LmToProjData
 {
 public:
@@ -38,25 +44,27 @@ public:
   virtual void get_bin_from_event(Bin& bin, const CListEvent&) const;
   virtual void process_new_time_event(const CListTime& time_event);
 
-private:
-
+protected: 
+  //! motion information
   shared_ptr<RigidObject3DMotion> ro3d_ptr;
+  //! switch between constant reference position, or one for each frame
+  bool reference_position_is_average_position_in_frame;
+  //! constant reference position (if used)
+  shared_ptr<AbsTimeInterval> _reference_abs_time_sptr;  
+
+  virtual void start_new_time_frame(const unsigned int new_frame_num);
+
    
   virtual void set_defaults();
   virtual void initialise_keymap();
   virtual bool post_processing();
 
-  RigidObject3DTransformation move_to_scanner;
-  RigidObject3DTransformation move_from_scanner;
-  shared_ptr<AbsTimeInterval> _reference_abs_time_sptr;
-  
+private:
+
   RigidObject3DTransformation _transformation_to_reference_position;
-  RigidObject3DTransformation ro3dtrans; // actual Polaris  motion for current_time
-  
 
-
-
-  
+  RigidObject3DTransformation ro3dtrans; // actual  motion for current_time
+    
 };
 
 END_NAMESPACE_STIR
