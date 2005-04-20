31b7b7c3227164086010ee4ecfdce9d04e9dbc72
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-04-20 20:51:36 +0000

    - corrected oblique segments to take simset convention into account - added FOV_radius parameter to set bin_size correctly

--- src/SimSET/conv_SimSET_projdata_to_STIR.cxx
+++ src/SimSET/conv_SimSET_projdata_to_STIR.cxx
@@ -13,24 +13,23 @@
 //#include <util/nrutil.h>
 //#include <util/sequen.h>
 #include "stir/ProjDataInterfile.h"
-#include "stir/ProjDataInfo.h"
+#include "stir/ProjDataInfoCylindricalArcCorr.h"
 #include "stir/Sinogram.h"
 #include "stir/Scanner.h"
 #include "stir/shared_ptr.h"
 #include "stir/IO/read_data.h"
 #include "stir/Succeeded.h"
-#define NUMARG 9
+#define NUMARG 10
 
 
 int main(int argc,char **argv)
 {
   using namespace stir;
 
-int i,k,ncol,nfil,slices,segment,nitems,num_ima_extr,n,ii;
-int i_segment,diag,lim_down,lim_up,ima_serie,i_write,i_read,sino;
-//float *seq,*q,s,qq,stot;
+int ncol,nfil,slices,segment,nitems,n,ii;
+int i_segment,lim_down,lim_up;
 char fseq[180],fima[180],tipoin[3];
-FILE *file,*file2; /*files to read and to write*/
+FILE *file; /*files to read and to write*/
 
 static char *opcions[]={
     "argv[1]  SimSET file\n",
@@ -39,14 +38,15 @@ static char *opcions[]={
     "argv[4]  Bins in SimSET filele\n",
     "argv[5]  Axial slices in SimSET file\n",
     "argv[6]  Segment\n",
-    "argv[7]  index of 3d-sinogram in file (0-based)\n"
-    "argv[8]  STIR file name\n"
+    "argv[7]  FOV_radius in cm as given to simset binning module (max_td)\n",
+    "argv[8]  index of 3d-sinogram in file (0-based)\n"
+    "argv[9]  STIR file name\n"
     };
 if (argc!=NUMARG){
   printf("\n\nConvert SimSET to STIR\n\n");
   printf("Not enough arguments !!! ..\n");
-  for (i=1;i<NUMARG;i++) printf(opcions[i-1]);
-  exit(0);
+  for (int i=1;i<NUMARG;i++) printf(opcions[i-1]);
+  exit(EXIT_FAILURE);
   }
 
 strcpy(fseq,argv[1]);
@@ -55,22 +55,17 @@ nfil=atoi(argv[3]);
 ncol=atoi(argv[4]);
 slices=atoi(argv[5]);
 segment=atoi(argv[6]);
-const int dataset_num = atoi(argv[7]);
-strcpy(fima,argv[8]);
+ const float FOV_radius = atof(argv[7])*10; // times 10 for mm
+const int dataset_num = atoi(argv[8]);
+strcpy(fima,argv[9]);
 nitems=nfil*ncol;
 
 
 
 if( (file=fopen(fseq,"rb")) ==NULL){
-    printf("\n\nNO SE PUEDE ABRIR EL ARCHIVO DE LECTURA\n\n");
-    exit(0);
+    printf("\n\nCannot open the simset file\n\n");
+    exit(EXIT_FAILURE);
     }
-/*if( (file2=fopen(fima,"wb")) ==NULL){
-    printf("\n\nNO SE PUEDE ABRIR EL ARCHIVO DE ESCRITURA\n\n");
-    exit(0);
-    }
-    num_ima_extr=num_imagenes(slices,segment);
-*/
  shared_ptr<Scanner> scanner_sptr = new Scanner(Scanner::E962);
  const float scanner_length = 
    scanner_sptr->get_num_rings() * scanner_sptr->get_ring_spacing();
@@ -84,6 +79,9 @@ if( (file=fopen(fseq,"rb")) ==NULL){
 				  nfil,
 				  ncol,
 				  /*arc_corrected =*/ true);
+ dynamic_cast<ProjDataInfoCylindricalArcCorr&>(*proj_data_info_sptr).
+   set_tangential_sampling(2*FOV_radius/ncol);
+
  ProjDataInterfile proj_data(proj_data_info_sptr,
 			     fima, std::ios::out);
  
@@ -102,51 +100,44 @@ else printf("\nFORMATO DE FICHERO NO VALIDO\n\n");
   Array<1,float> seq(0,(slices*slices*nitems)-1);
    read_data(file, seq /*, byteorder */);
 
-i_segment=segment*(-1);
-k=0;
+i_segment=0;
 n=0;
 while(i_segment<=segment)
     {
-    if(i_segment<0)
-        {
-        lim_down=i_segment*slices*(-1);
-        lim_up=lim_down+((slices+i_segment-1)*(slices+1));
-        ima_serie=((lim_up-lim_down)/(slices+1))+1;
-        //printf("\n\n  %d  -  %d",lim_down,lim_up);
-        //printf("**   %d  imagenes  de esta serie**\n\n",ima_serie);
-        }
-    else if(i_segment>=0)
-        {
-        lim_down=i_segment;
+      {
+        lim_down=0;
         lim_up=lim_down+((slices-i_segment-1)*(slices+1));
-        ima_serie=((lim_up-lim_down)/(slices+1))+1;
-        //printf("\n\n  %d  -  %d",lim_down,lim_up);
-        //printf("**   %d  imagenes de esta serie **\n\n",ima_serie);
-        }
-    for(n=lim_down;n<=lim_up;) //Extraccion de los sinogramas de una serie !!!
-    {
-      // TODO potentially use -i_segment
+      }
+      for(n=lim_down;n<=lim_up;) //Extraccion de los sinogramas de una serie !!!
+	{
 
-      Sinogram<float> sino = proj_data.get_empty_sinogram((n-lim_down)/(slices+1), i_segment);
-      Sinogram<float>::full_iterator sino_iter = sino.begin_all();
+      Sinogram<float> pos_sino = proj_data.get_empty_sinogram((n-lim_down)/(slices+1), i_segment);
+      Sinogram<float> neg_sino = proj_data.get_empty_sinogram((n-lim_down)/(slices+1), -i_segment);
+      Sinogram<float>::full_iterator pos_sino_iter = pos_sino.begin_all();
+      Sinogram<float>::full_iterator neg_sino_iter = neg_sino.begin_all();
 
     ii=nitems-1;
+    const int i_r1r2=(n + i_segment)*nitems;
+    const int i_r2r1=(n + i_segment*slices)*nitems;
      
-    for(i=0;i<nitems;)   //Extraccion de un sinograma !!!
+    // get 2 sinograms from simset data
+    for(int i=0;i<nitems/2;++i)  
     {
-    i_read=n*nitems;
-    //i_write=k*nitems;
-    //*(q+i_write+i)=*(seq+i_read+ii);
-    *sino_iter = seq[i_read+ii];
-    ++sino_iter;
-    i=i+1;
-    ii=ii-1;  
+      *pos_sino_iter++ = seq[i_r1r2+ii];
+      *neg_sino_iter++ = seq[i_r2r1+ii];
+      ii=ii-1;  
     }
-    k=k+1;
+    for(int i=nitems/2;i<nitems;++i)   
+    {
+      *neg_sino_iter++ = seq[i_r1r2+ii];
+      *pos_sino_iter++ = seq[i_r2r1+ii];
+      ii=ii-1;  
+    }
     n=n+slices+1;
-    proj_data.set_sinogram(sino);
-    }
-    i_segment=i_segment+1;
+    proj_data.set_sinogram(pos_sino);
+    proj_data.set_sinogram(neg_sino);
+	}
+      i_segment=i_segment+1;
     }
 
 //fwrite(q,4,num_ima_extr*nitems,file2);
