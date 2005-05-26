deece68e72b155cf790b5c9f4d5ad715cdea5697
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-05-26 13:09:32 +0000

    Just including the copyrights etc. in the beginning.

--- src/include/local/stir/BSplines_dim.h
+++ src/include/local/stir/BSplines_dim.h
@@ -1,3 +1,33 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*!
+  \file 
+  \ingroup numerics_buildblock
+  \brief Implementation of the B-Splines Interpolation 
+
+  \author Kris Thielemans
+  \author Charalampos Tsoumpas
+
+  $Date$
+  $Revision$
+*/
 // 1
 template <typename T, int num_dimensions>
 inline 
