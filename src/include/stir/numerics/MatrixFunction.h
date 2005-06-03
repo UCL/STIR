76050df6954f18b3659d0a7e1289fb4e3b35ff8f
Kris Thielemans <Kris.Thielemans@stir>
2005-06-03 15:21:01 +0000

    - added diagonal_matrix

--- src/include/stir/numerics/MatrixFunction.h
+++ src/include/stir/numerics/MatrixFunction.h
@@ -32,6 +32,7 @@
   $Revision$
 */
 #include "stir/Array.h"
+#include "stir/BasicCoordinate.h"
 
 START_NAMESPACE_STIR
 
@@ -81,9 +82,53 @@ inline Array<2,elemT>
   matrix_multiply(const Array<2,elemT> &m1, const Array<2,elemT>& m2);
 
 //! matrix transposition
-template <class elemT>	
+template <class elemT>
 inline Array<2,elemT>
   matrix_transpose (const Array<2,elemT>& m);
+
+//! construct a diagonal matrix with all elements on the diagonal equal
+/*! \param[in] dimension specifies the size of the matrix
+   \param[in] value gives the value on the diagonal. Note that its 
+    type determines the type of the return value.
+
+    \par Example
+    \code
+    // a 3x3 identity matrix
+    Array<2,float> iden = diagonal_matrix(3, 1.F);
+    \endcode
+
+    Index-range of the matrix will be <code>0</code> till 
+    <code>dimensions-1</code>.
+*/
+template <class elemT>
+inline 
+Array<2,elemT>
+  diagonal_matrix(const unsigned dimension, const elemT value);
+
+//! construct a diagonal matrix 
+/*! 
+   \param[in] values gives the values on the diagonal. Note that its 
+    type determines the type of the return value.
+
+    \par Example
+    \code
+    // a 3x3 diagonal matrix with values 1,2,3 on the diagonal
+    Array<2,float> iden = diagonal_matrix(Coordinate3D<float>(1,2,3));
+    \endcode
+
+    Index-range of the matrix will be <code>0</code> till 
+    <code>dimensions-1</code>. (Note that this is different from
+    \a values).
+*/
+template <int dimension, class elemT>
+inline 
+Array<2,elemT>
+  diagonal_matrix(const BasicCoordinate<dimension,elemT>& values);
+
 //@}
 
 END_NAMESPACE_STIR
+
+#include "stir/numerics/MatrixFunction.inl"
+
+#endif
