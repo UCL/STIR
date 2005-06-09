631683570a15d6f49a5033c60491ef273a91763c
Kris Thielemans <Kris.Thielemans@stir>
2005-06-09 10:36:31 +0000

    - exit gracefully when data could not be read by ProjData::read_from_file

--- src/utilities/list_projdata_info.cxx
+++ src/utilities/list_projdata_info.cxx
@@ -1,3 +1,22 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2002- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup utilities
@@ -9,22 +28,11 @@
   $Date$
   $Revision$
 */
-/*
-    Copyright (C) 2002- $Date$, Hammersmith Imanet Ltd
-    See STIR/LICENSE.txt for details
-*/
 
 #include "stir/ProjData.h"
 #include "stir/ProjDataInfo.h"
-
+#include "stir/is_null_ptr.h"
 #include <iostream> 
-
-#ifndef STIR_NO_NAMESPACES
-using std::cerr;
-using std::endl;
-using std::cout;
-#endif
-
 
 USING_NAMESPACE_STIR
 
@@ -33,16 +41,21 @@ int main(int argc, char *argv[])
   
   if(argc!=2) 
   {
-    cerr<<"Usage: " << argv[0] << " projdata_file\n";
+    std::cerr<<"Usage: " << argv[0] << " projdata_file\n";
     return EXIT_FAILURE;
 
   }
 
   shared_ptr<ProjData> projdata_ptr = 
-  ProjData::read_from_file(argv[1], ios::in|ios::out);
+  ProjData::read_from_file(argv[1]);
 
-  cout << "Info for file " << argv[1] << '\n';
-  cout << projdata_ptr->get_proj_data_info_ptr()->parameter_info() << endl;
+  if (is_null_ptr(projdata_ptr))
+    {
+      warning("Could not read %s", argv[1]);
+      return EXIT_FAILURE;
+    }
+  std::cout << "Info for file " << argv[1] << '\n';
+  std::cout << projdata_ptr->get_proj_data_info_ptr()->parameter_info() << '\n';
 
   return EXIT_SUCCESS;
 }
