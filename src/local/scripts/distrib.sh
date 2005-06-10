090ba67445b6d5ab7749bf44133523581844797c
Kris Thielemans <Kris.Thielemans@stir>
2005-06-10 11:57:30 +0000

    - tar LLN stuff - include release notes in doc.zip - some more warnings

--- src/local/scripts/distrib.sh
+++ src/local/scripts/distrib.sh
@@ -1,44 +1,54 @@
 #! /bin/sh
 do_update=1
-do_license=1
+do_license=0
 do_ChangeLog=1
 do_doc=1
 do_zip_source=1
-do_recon_test_pack=1
+do_recon_test_pack=0
 do_transfer=0
 
 set -e
 # rsync of website note: stalls on gluon,wren,hurricane, but works fine from shark
-VERSION=1.3
-CHECKOUTOPTS="-r rel_1_30"
+VERSION=1.4alpha
+# TODO  problems with LICENSE.txt
+# need to get it without tag, and then update and then assign tag
+#CHECKOUTOPTS="-r rel_1_30"
 cd $WORKSPACE/../..
 DISTRIB=`pwd`/distrib
 LLN=${DISTRIB}/../lln
 WORKSPACE=${DISTRIB}/parapet/PPhead 
 
-destination=krthie@shell.sf.net:stir/htdocs/
-RSYNC_OPTS=
-#destination=web@wren:htdocs/STIR/
-#RSYNC_OPTS=--rsync-path=/home/kris/bin/rsync 
+#destination=krthie@shell.sf.net:stir/htdocs/
+#RSYNC_OPTS=
+destination=web@wren:htdocs/STIR/
+RSYNC_OPTS=--rsync-path=/home/kris/bin/rsync 
+
+
+if [ $do_doc = 1 -a -r ${DISTRIB}/STIR_doc_${VERSION}.zip ]; then
+  echo WARNING: updating existing zip file ${DISTRIB}/STIR_doc_${VERSION}.zip
+fi
+if [ $do_recon_test_pack = 1 -a -r ${DISTRIB}/recon_test_pack_${VERSION}.zip ]; then
+  echo "WARNING: updating existing zip file ${DISTRIB}/recon_test_pack_${VERSION}.zip"
+fi
+
+
 mkdir -p ${DISTRIB}
+
+echo "LLN stuff "
+cd ${LLN}
+  cvs up
+  tar -v --exclude VC --exclude CVS -czf ecat.tar.gz \
+    ecat/*[ch] ecat/Makefile.*   ecat/utils/*[ch] ecat/utils/Makefile.* 
+
 cd ${DISTRIB}
 
-echo "Did you  make ${DISTRIB}/release_${VERSION}.htm ?"
-
-echo LLN stuff (still by hand)
-if [ ! -r ${LLN}/ecat.tar.gz ]; then
-  echo Need LLN in ${LLN}
-  exit 1
-fi
-#tar --exclude VC --exclude CVS -czf ecat.tar.gz ecat
-
 if [ ! -r parapet ]; then
-  cvs -d parapet:/usr/local/cvsroot $CHECKOUTOPTS checkout parapet
+
   cd parapet
 else
   cd parapet
   if [ $do_update = 1 ]; then
-    cvs up -dP
+    cvs up -dP  $CHECKOUTOPTS
   fi
 fi
 rm -f STIR
@@ -74,6 +84,7 @@ fi
 
 # make ChangeLog file
 if [ $do_ChangeLog = 1 ]; then
+  echo Do ChangeLog
   cd $WORKSPACE
   # maybe use --accum
   mv local xxlocal
@@ -85,18 +96,19 @@ fi
 if [ $do_doc = 1 ]; then
   cd $WORKSPACE
   # make doxygen
-  doxygen
+  #doxygen
   # make documentation PDFs BY HAND
   cd ../documentation
   make
-  zip -ur ${DISTRIB}/STIR_doc_${VERSION}.zip *.pdf  doxy
+  zip -ur ${DISTRIB}/STIR_doc_${VERSION}.zip *.pdf *.htm  doxy
 fi
 
 if [ $do_zip_source ]; then
+  echo Do zip source
   cd ${DISTRIB}
   rm -f parapet/all.zip parapet/VCprojects.zip
-  zipit --distrib
-  zipproj --distrib
+  zipit --distrib > /dev/null
+  zipproj --distrib > /dev/null
   mv parapet/VCprojects.zip VCprojects_${VERSION}.zip 
   mv parapet/all.zip STIR_${VERSION}.zip 
 fi
@@ -109,6 +121,7 @@ if [ $do_recon_test_pack = 1 ]; then
 fi
 
 if [ $do_transfer = 1 ]; then
+  cd ${DISTRIB}
   chmod go+r *${VERSION}* ChangeLog
   chmod go-wx *${VERSION}* ChangeLog
 
@@ -117,13 +130,14 @@ if [ $do_transfer = 1 ]; then
     STIR_${VERSION}.zip VCprojects_${VERSION}.zip \
     recon_test_pack_${VERSION}.zip \
     ${destination}registered
-  rsync --progress -uavz ${RSYNC_OPTS} ChangeLog release_${VERSION}.htm STIR_doc_${VERSION}.zip  \
+  rsync --progress -uavz ${RSYNC_OPTS} ChangeLog STIR_doc_${VERSION}.zip  \
     ${destination}documentation
 fi
 
 exit 
 # remote
 VERSION=1.3
+cd registered
 echo EDIT documentation/history.htm
 rm  recon_test_pack.tar.gz STIR.zip VCprojects.zip recon_test_pack.zip 
 ln -s STIR_${VERSION}.zip STIR.zip 
