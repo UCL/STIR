6093120e5c0a4e5a60c323f171b18c4b084aa27c
Kris Thielemans <Kris.Thielemans@stir>
2005-06-21 10:19:20 +0000

    - moved functionality in match_tracker_and_scanner.cxx to a separate class

--- src/local/motion/lib.mk
+++ src/local/motion/lib.mk
@@ -9,6 +9,7 @@ $(dir)_LIB_SOURCES:= \
 	RigidObject3DMotion.cxx\
 	RigidObject3DMotionFromPolaris.cxx \
 	RigidObject3DTransformation.cxx \
+	MatchTrackerAndScanner.cxx \
 	transform_3d_object.cxx
 
 $(dir)_REGISTRY_SOURCES:= local_motion_registries.cxx
