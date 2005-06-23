f85aa2b7b6dc6771a267b5c9768d0040fb14688c
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-06-23 11:47:19 +0000

    It compiles now, but it does not give the correct result, yet.

--- src/local/buildblock/interpolate_projdata.cxx
+++ src/local/buildblock/interpolate_projdata.cxx
@@ -30,57 +30,118 @@ This file is part of STIR.
 */
 #include "stir/ProjData.h"
 #include "stir/ProjDataInfo.h"
-//#include "local/stir/BSplines.h"
+#include "stir/BasicCoordinate.h"
+#include "local/stir/BSplines.h"
 #include "local/stir/BSplinesRegularGrid.h"
 #include "local/stir/interpolate_projdata.h"
 #include "stir/Sinogram.h"
+#include "stir/SegmentBySinogram.h"
 #include "stir/Bin.h"
 #include "stir/Succeeded.h"
 
-using namespace BSpline;
+
 
 START_NAMESPACE_STIR
 
+using namespace BSpline;
+
 /*Succeeded 
 interpolate_projdata(ProjData& proj_data_out,
-					 const ProjData& proj_data_in, const BSplineType this_type)
+					 const ProjData& proj_data_in, const BSplineType these_types)
 {
-	BasicCoordinate<3, BSplineType> this_type_3; this_type_3[1]=this_type_3[2]=this_type_3[3]=this_type;
-	interpolate_projdata(proj_data_out,proj_data_in,this_type_3);
+	BasicCoordinate<3, BSplineType> these_types_3; these_types_3[1]=these_types_3[2]=these_types_3[3]=these_types;
+	interpolate_projdata(proj_data_out,proj_data_in,these_types_3);
 	return Succeeded::yes;
 }*/
 
 Succeeded 
 interpolate_projdata(ProjData& proj_data_out,
 					 const ProjData& proj_data_in,
-					 const BasicCoordinate<3, BSplineType> & this_type)
+					 const BasicCoordinate<3, BSplineType> & these_types)
 {
-	const ProjDataInfo * const proj_data_in_info_ptr =
+	/*const ProjDataInfo * const proj_data_in_info_ptr =
 		dynamic_cast<ProjDataInfo const * >
 		(proj_data_in.get_proj_data_info_ptr());
 	const ProjDataInfo * const proj_data_out_info_ptr =
 		dynamic_cast<ProjDataInfo const * >
-		(proj_data_out.get_proj_data_info_ptr());
+		(proj_data_out.get_proj_data_info_ptr());*/
 	
 	const SegmentBySinogram<float> sino_3D_in = proj_data_in.get_segment_by_sinogram(0) ;
+	SegmentBySinogram<float> sino_3D_out = proj_data_out.get_empty_segment_by_sinogram(0) ;
+	BSpline::BSplinesRegularGrid<3, float, float> BSplines_Projdata(sino_3D_in, these_types);
+	BasicCoordinate<3, double> relative_positions, min_in,  max_in, length_in, 
+		min_out, max_out, length_out, step  ;
+
+
+	int kmin=sino_3D_in.get_min_index(),
+	jmin=sino_3D_in[kmin].get_min_index(),
+	imin=sino_3D_in[kmin][jmin].get_min_index(),
+	kmax=sino_3D_in.get_max_index(),
+	jmax=sino_3D_in[kmax].get_max_index(),
+	imax=sino_3D_in[kmax][jmax].get_max_index();
+/*	for (int k=sino_3D_in.get_min_index(), kmin=k; k<=sino_3D_in.get_max_index(); ++k)
+		for (int j=sino_3D_in.get_min_index(), jmin=j; j<=sino_3D_in[k].get_max_index(); ++j)
+			for (int i=sino_3D_in.get_min_index(), imin=i; i<=sino_3D_in[k][j].get_max_index(); ++i)
+			{
+				kmin=min(k,kmin); jmin=min(j,jmin); imin=min(i,imin);				
+				kmax=max(k,kmax); jmax=max(j,jmax); imax=max(i,imax);				
+			}
+*/	min_in[1]=static_cast<double>(kmin);
+	min_in[2]=static_cast<double>(jmin);
+	min_in[3]=static_cast<double>(imin);
+	max_in[1]=static_cast<double>(kmax);
+	max_in[2]=static_cast<double>(jmax);
+	max_in[3]=static_cast<double>(imax);
+
+	kmin=sino_3D_out.get_min_index();
+	jmin=sino_3D_out[kmin].get_min_index();
+	imin=sino_3D_out[kmin][jmin].get_min_index();
+	kmax=sino_3D_out.get_max_index();
+	jmax=sino_3D_out[kmax].get_max_index();
+	imax=sino_3D_out[kmax][jmax].get_max_index();;
+/*	for (int k=sino_3D_out.get_min_index(), kmin=k; k<=sino_3D_out.get_max_index(); ++k)
+		for (int j=sino_3D_out.get_min_index(), jmin=j; j<=sino_3D_out[k].get_max_index(); ++j)
+			for (int i=sino_3D_out.get_min_index(), imin=i; i<=sino_3D_out[k][j].get_max_index(); ++i)
+			{
+				kmin=min(k,kmin); jmin=min(j,jmin); imin=min(i,imin);				
+				kmax=max(k,kmax); jmax=max(j,jmax); imax=max(i,imax);				
+			}
+*/  min_out[1]=static_cast<double>(kmin);
+	min_out[2]=static_cast<double>(jmin);
+	min_out[3]=static_cast<double>(imin);
+	max_out[1]=static_cast<double>(kmax);
+	max_out[2]=static_cast<double>(jmax);
+	max_out[3]=static_cast<double>(imax);
 	
-	BSplinesRegularGrid<3, float, float> BSplines_Projdata(sino_3D_in, this_type);
-	BasicCoordinate<3, BSplineType> & this_type ;
-	const SegmentBySinogram<float> sino_3D_out = proj_data_out.get_segment_by_sinogram(0) ;
-		
-	for (float k=sino_3D_out.get_min_index() ; k<=sino_3D_out.get_max_index() ; ++k)	
-		for (float j=sino_3D_out[k].get_min_index() ; j<=sino_3D_out[k].get_max_index() ; ++j)	
-			for (float i=sino_3D_out[k][j].get_min_index() ; i<=sino_3D_out[k][j].get_max_index() ; ++i)	
+	length_in = (max_in-min_in+1.) ;
+	length_out = (max_out-min_out+1.) ;
+	step = length_in/(length_out +1.);
+	step[2] -= .5/(length_out[2] +1.);
+
+	//zoom = length_out/length_in OR 1/step ???;
+	
+	int k_out=kmin, j_out=jmin, i_out=imin;
+			
+	for (double k_in=min_in[1]-0.5+step[1] ; 
+	         k_out<=max_out[1] && k_in<=max_in[1]+0.5-step[1] ; 
+		   ++k_out, k_in+=step[1])	
+		for (double j_in=min_in[2] ; 
+		         j_out<=max_out[2] && j_in<=max_in[2]+0.5-step[2] ; 
+		       ++j_out, j_in+=step[2])	
+			for (double i_in=min_in[3]-0.5+step[3] ; 
+			         i_out<=max_out[3] && i_in<=max_in[3]+0.5-step[3] ; 
+				   ++i_out, i_in+=step[3])	
 			{				
-				relative_positions[1]=k;
-				relative_positions[2]=j;
-				relative_positions[3]=i;
-				sino_3D_out[k][j][i]=BSplines_Projdata(relative_positions) ; 
+				relative_positions[1]=k_in;
+				relative_positions[2]=j_in;
+				relative_positions[3]=i_in;
+				sino_3D_out[k_out][j_out][i_out]=
+					BSplines_Projdata(relative_positions) ; 
 			}
 	proj_data_out.set_segment(sino_3D_out);
 	if (proj_data_out.set_segment(sino_3D_out) == Succeeded::no)
 		return Succeeded::no;
-	break;
+
 	return Succeeded::yes;
 }
 END_NAMESPACE_STIR
