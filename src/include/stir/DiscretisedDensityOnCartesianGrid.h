a4d532722d84056caf49c06d5a23481c0883475d
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-06-24 15:23:18 +0000

    Updated copyrights

--- src/include/stir/DiscretisedDensityOnCartesianGrid.h
+++ src/include/stir/DiscretisedDensityOnCartesianGrid.h
@@ -3,7 +3,23 @@
 //
 #ifndef __DiscretisedDensityOnCartesianGrid_H__
 #define __DiscretisedDensityOnCartesianGrid_H__
+/*
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    Copyright (C) 2000 PARAPET partners
 
+    This file is part of STIR.
+	This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+ */
 /*!
   \file 
   \ingroup densitydata 
@@ -19,11 +35,6 @@
 
   $Revision$
 
-*/
-/*
-    Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, IRSL
-    See STIR/LICENSE.txt for details
 */
 #include "stir/DiscretisedDensity.h"
 
