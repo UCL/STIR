3f3845cf26156b75edb19fcacc1cbf3e687e802d
Tim Borgeaud <Tim.Borgeaud@stir>
2005-07-12 09:39:37 +0000

    Corrected conv_to_ecat7.cxx and modified recon_test_pack/run_tests.sh to perform a test on conv_to_ecat7

--- recon_test_pack/run_tests.sh
+++ recon_test_pack/run_tests.sh
@@ -9,17 +9,63 @@ echo a later version, you might have to update your test pack.
 echo Please check the web site.
 echo
 
-if test tmp$1 = tmp--nointbp; then
-  NOINTBP=1
+#
+# Options
+#
+NOINTBP=0
+DO_ECAT7_TESTS=0
+
+
+#
+# Parse option arguments (--)
+# Note that the -- is required to suppress interpretation of $1 as options 
+# to expr
+#
+while test `expr -- "$1" : "--.*"` -gt 0
+do
+
+  if test "$1" = "--nointbp"
+  then
+    NOINTBP=1
+
+  elif test "$1" = "--ecat7"
+  then
+    DO_ECAT7_TESTS=1;
+
+  else
+    echo Warning: Unknown option "$1"
+    echo
+
+  fi
+
+  shift 1
+
+done 
+
+
+
+if test $NOINTBP -eq 1; then
   echo Not executing tests that use the interpolating backprojector
   echo
-  shift
 else
-  NOINTBP=0
   echo Executing tests that use the interpolating backprojector
   echo If this is not what you want, rerun this script with the option --nointbp
   echo
 fi
+
+
+if test $DO_ECAT7_TESTS -eq 1; then
+  echo Executing tests on ecat7 file format conversion
+  echo
+else
+  echo Not testing ecat7 file format conversion.
+  echo If this is not what you want, rerun this script with the option --ecat7
+  echo
+fi
+
+
+
+
 
 # first delete any files remaining from a previous run
 rm -f my_*v my_*s
@@ -32,13 +78,57 @@ echo Running ${INSTALL_DIR}convecat6_if
 ${INSTALL_DIR}convecat6_if my_Utahscat600k_ca_seg4 Utahscat600k_ca.scn 1> convecat6_if.log 2> convecat6_if_stderr.log <  convecat6_if.inp
 echo '---- Comparing output of convecat6 (error should be 0)'
 echo Running ${INSTALL_DIR}compare_projdata
-if ${INSTALL_DIR}compare_projdata my_Utahscat600k_ca_seg4.hs Utahscat600k_ca_seg4.hs 2>compare_projdata_stderr.log;
+if ${INSTALL_DIR}compare_projdata my_Utahscat600k_ca_seg4.hs Utahscat600k_ca_seg4.hs 2>compare_projdata_convecat6_if_stderr.log;
 then
 echo ---- This test seems to be ok !;
 else
 echo There were problems here!;
 ThereWereErrors=1;
 fi
+
+if test $DO_ECAT7_TESTS=1; then
+
+echo ------------- Converting Interfile to ECAT7 file ------------- 
+echo Running ${INSTALL_DIR}conv_to_ecat7
+${INSTALL_DIR}conv_to_ecat7 -s my_Utahscat600k_ca_seg4_ecat7.S Utahscat600k_ca_seg4.hs 1> conv_to_ecat7.log 2> conv_to_ecat7_stderr.log
+echo '---- Comparing output of conv_to_ecat7 (error should be 0)'
+echo Running ${INSTALL_DIR}compare_projdata
+if ${INSTALL_DIR}compare_projdata my_Utahscat600k_ca_seg4_ecat7.S Utahscat600k_ca_seg4.hs 2>compare_projdata_conv_to_ecat7_stderr.log;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+
+echo ------------- Making Interfile headers for ECAT7 ------------- 
+echo Running ${INSTALL_DIR}ifheaders_for_ecat7
+${INSTALL_DIR}ifheaders_for_ecat7  my_Utahscat600k_ca_seg4_ecat7.S < /dev/null 1> ifheaders_for_ecat7.log 2> ifheaders_for_ecat7_stderr.log
+echo '---- Comparing output of ifheaders_for_ecat7 (error should be 0)'
+echo Running ${INSTALL_DIR}compare_projdata
+if ${INSTALL_DIR}compare_projdata my_Utahscat600k_ca_seg4_ecat7_S_f1g1d0b0.hs Utahscat600k_ca_seg4.hs 2>compare_projdata_ifheaders_for_ecat7_stderr.log;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+
+echo ------------- Testing native reading of ECAT7 projdata ------------- 
+echo '---- Comparing compare_projdata directly on ECAT7 sinograms to test STIR IO (error should be 0)'
+echo Running ${INSTALL_DIR}compare_projdata
+if ${INSTALL_DIR}compare_projdata my_Utahscat600k_ca_seg4_ecat7.S Utahscat600k_ca_seg4.hs 2>compare_projdata__ecat7_stderr.log;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+fi # end of ECAT7 tests
+
 
 if test $NOINTBP = 0; then
 echo
