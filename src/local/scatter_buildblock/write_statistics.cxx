558d0057c1cdff8825810cbc6bc3b597b9c90e7e
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-08-02 17:10:04 +0000

    Energy Resolution has been introduced in order to implement different kind of detectors.

--- src/local/scatter_buildblock/write_statistics.cxx
+++ src/local/scatter_buildblock/write_statistics.cxx
@@ -49,60 +49,62 @@ void writing_log(const DiscretisedDensityOnCartesianGrid<3,float>& activity_imag
 				 const float given_attenuation_threshold,
 				 const int total_scatt_points, 
 				 const float lower_energy_threshold, 
-				 const float upper_energy_threshold,
+		 const float upper_energy_threshold,
+		 const float resolution,
 				 const bool use_cache,
 				 const bool random, const char *argv[])
 {	
-	Bin bin;
-	int axial_bins = 0 ;
-	for (bin.segment_num()=proj_data_info_ptr->get_min_segment_num();
-	bin.segment_num()<=proj_data_info_ptr->get_max_segment_num();
-	++bin.segment_num())	
-		axial_bins += proj_data_info_ptr->get_num_axial_poss(bin.segment_num());	
-    const int total_bins = proj_data_info_ptr->get_num_views() * axial_bins *
-		proj_data_info_ptr->get_num_tangential_poss()	;	
-	/*
-	cerr << "\n Total bins : " << total_bins << " = " 
-		 << proj_data_info_ptr->get_num_views() 
-		 << " view_bins * " 
-		 << axial_bins << " axial_bins * "
-		 << proj_data_info_ptr->get_num_tangential_poss() 
-		 << " tangential_bins\n"   ;		
-    */
-	fstream mystream("statistics.txt", ios::out | ios::app); //output file //
-	if(!mystream)    
-		warning("Cannot open text file.\n") ;	              
-	mystream << "Output_proj_data_filename is:" << argv[4]
-		<< "\nActivity image: " << argv[1]
-		<< "\nwith SIZE: " 
-		<< activity_image.get_z_size() << " * " 
-		<< activity_image.get_y_size() << " * " 
-		<< activity_image.get_x_size()
-		<< "\nAttenuation image: " << argv[2]
-		<< "\nwith SIZE: " 
-		<< density_image.get_z_size() << " * "
-		<< density_image.get_y_size() << " * " 
-		<< density_image.get_x_size()
-		<< "\n\nTemplate proj_data: " << argv[3]
-		<< "\nwith total bins : " << total_bins << " = " 
-		<< proj_data_info_ptr->get_num_views() 		 
-		<< " view_bins * " 
-		<< axial_bins << " axial_bins * "
-		<< proj_data_info_ptr->get_num_tangential_poss() 
-		<< " tangential_bins\n"  
-		<< "\n - The energy window was set to: [" 
-		<< lower_energy_threshold << "," << upper_energy_threshold 
-		<< "]\n - Threshold was set to: " << given_attenuation_threshold
-		<< "\n - Scatter Points are taken all above the threshold";		
-	if (random)
-		mystream << " and have picked randomly\n";
-	if (!random)
-		mystream << " and have picked in the center of the Voxel\n";
-	if(use_cache)
-		mystream << " - Use of caching\n";
-	if(!use_cache)
-		mystream << " - No use of caching for SS - Use of caching for the LoRs in DS\n";
-    mystream <<"\n\n\t ****************** END ****************\n\n\n\n\n\a";
+  Bin bin;
+  int axial_bins = 0 ;
+  for (bin.segment_num()=proj_data_info_ptr->get_min_segment_num();
+       bin.segment_num()<=proj_data_info_ptr->get_max_segment_num();
+       ++bin.segment_num())	
+    axial_bins += proj_data_info_ptr->get_num_axial_poss(bin.segment_num());	
+  const int total_bins = proj_data_info_ptr->get_num_views() * axial_bins *
+    proj_data_info_ptr->get_num_tangential_poss()	;	
+  /*
+    cerr << "\n Total bins : " << total_bins << " = " 
+    << proj_data_info_ptr->get_num_views() 
+    << " view_bins * " 
+    << axial_bins << " axial_bins * "
+    << proj_data_info_ptr->get_num_tangential_poss() 
+    << " tangential_bins\n"   ;		
+  */
+  fstream mystream("statistics.txt", ios::out | ios::app); //output file //
+  if(!mystream)    
+    warning("Cannot open text file.\n") ;	              
+  mystream << "Output_proj_data_filename is:" << argv[4]
+	   << "\nActivity image: " << argv[1]
+	   << "\nwith SIZE: " 
+	   << activity_image.get_z_size() << " * " 
+	   << activity_image.get_y_size() << " * " 
+	   << activity_image.get_x_size()
+	   << "\nAttenuation image: " << argv[2]
+	   << "\nwith SIZE: " 
+	   << density_image.get_z_size() << " * "
+	   << density_image.get_y_size() << " * " 
+	   << density_image.get_x_size()
+	   << "\n\nTemplate proj_data: " << argv[3]
+	   << "\nwith total bins : " << total_bins << " = " 
+	   << proj_data_info_ptr->get_num_views() 		 
+	   << " view_bins * " 
+	   << axial_bins << " axial_bins * "
+	   << proj_data_info_ptr->get_num_tangential_poss() 
+	   << " tangential_bins\n"  
+	   << "\n - The energy window was set to: [" 
+	   << lower_energy_threshold << "," << upper_energy_threshold 
+           << "\n and the energy resolution was set to:" << resolution
+	   << "]\n - Attenuation threshold was set to: " << given_attenuation_threshold
+	   << "\n - Scatter Points are taken all above the threshold";		
+  if (random)
+    mystream << " and have picked randomly\n";
+  if (!random)
+    mystream << " and have picked in the center of the Voxel\n";
+  if(use_cache)
+    mystream << " - Use of caching\n";
+  if(!use_cache)
+    mystream << " - No use of caching for SS - Use of caching for the LoRs in DS\n";
+  mystream <<"\n\n\t ****************** END ****************\n\n\n\n\n\a";
 }
 void writing_time(const double simulation_time, 
 				  const int scatt_points_vector_size, 
