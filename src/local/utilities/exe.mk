07f78bfc7dc5efb545157d1804c634760bb22a31
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-08-05 14:58:03 +0000

    Nothing special

--- src/local/utilities/exe.mk
+++ src/local/utilities/exe.mk
@@ -31,7 +31,7 @@ $(dir)_SOURCES = \
 	line_profiles_through_projdata.cxx \
 	inverse_SSRB.cxx \
 	interpolate_projdata.cxx \
-#	read_input_function.cxx \
+	read_input_function.cxx \
 #	read_dynamic_images.cxx \
 	calculate_attenuation_coefficients.cxx
 #	CoG.cxx \
