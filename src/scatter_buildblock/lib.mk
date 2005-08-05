cc83ffcfd80dd61b3098ebbe2bb0aaadbd9e863c
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-08-05 14:29:26 +0000

    Splitted cases are included.

--- src/scatter_buildblock/lib.mk
+++ src/scatter_buildblock/lib.mk
@@ -5,14 +5,17 @@
 dir := local/scatter_buildblock
 $(dir)_LIB_SOURCES:= \
 	sample_scatter_points.cxx \
-	integral_scattpoint_det.cxx\
-        cached_factors.cxx\
-	cached_factors_2.cxx\
-	scatter_estimate_for_one_scatter_point.cxx\
-	scatter_estimate_for_none_scatter_point.cxx\
-	scatter_estimate_for_two_scatter_points.cxx\
-	scatter_estimate_for_all_scatter_points.cxx\
+	integral_scattpoint_det.cxx \
+        cached_factors.cxx \
+	cached_factors_2.cxx \
+	scatter_estimate_for_one_scatter_point.cxx \
+	scatter_estimate_for_none_scatter_point.cxx \
+	scatter_estimate_for_two_scatter_points.cxx \
+	scatter_estimate_for_all_scatter_points.cxx \
 	scatter_viewgram.cxx \
+	scatter_estimate_for_all_scatter_points_splitted.cxx \
+	scatter_estimate_for_two_scatter_points_splitted.cxx \
+	scatter_viewgram_splitted.cxx \
 	write_statistics.cxx \
 	scale_factors_per_sinogram.cxx \
 	att_estimate_for_no_scatter.cxx \
