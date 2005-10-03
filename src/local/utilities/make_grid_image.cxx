3b119b83adf2d1e99263cee46a44378ac9cff1d7
Kris Thielemans <Kris.Thielemans@stir>
2005-10-03 18:50:36 +0000

    - added 2 extra cmdline parameters to allow 'gridding' in z as well

--- src/local/utilities/make_grid_image.cxx
+++ src/local/utilities/make_grid_image.cxx
@@ -1,21 +1,18 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file 
   \ingroup utilities
  
-  \brief This programme attempts to get rid of the central artefact
-  caused by normalisation+scatter problems on the 966. 
-
-  \see cleanup966ImageProcessor
+  \brief This program creates a simple grid image
   \author Kris Thielemans
   $Date$
   $Revision$
-*/
-/*
-    Copyright (C) 2003- $Date$, IRSL
-    See STIR/LICENSE.txt for details
 */
 #include "stir/DiscretisedDensity.h"
 #include "stir/IO/DefaultOutputFileFormat.h"
@@ -31,9 +28,10 @@ USING_NAMESPACE_STIR
 
 int main(int argc, char **argv)
 {
-  if(argc<3 || argc>5) {
-    cerr<<"Usage: " << argv[0] << " <output image filename> <input image filename> [spacing [size]]\n"
-	<< "spacing defaults to 2, size to 1";
+  if(argc<3 || argc>7) {
+    cerr<<"Usage: " << argv[0] << " <output image filename> <input image filename> [xy-spacing [xy-size][z-spacing [z-size] ] ] ]\n"
+	<< "xy-spacing defaults to 2, xy-size to 1\n"
+	<< "z-spacing defaults to 1, z-size to 1\n";
     exit(EXIT_FAILURE);
   }
   
@@ -41,8 +39,10 @@ int main(int argc, char **argv)
 
   char const * const output_filename = argv[1];
   char const * const input_filename = argv[2];
-  const int spacing = argc>3?atoi(argv[3]):2;
-  const int size = argc>4?atoi(argv[4]):1;
+  const int xy_spacing = argc>3?atoi(argv[3]):2;
+  const int xy_size = argc>4?atoi(argv[4]):1;
+  const int z_spacing = argc>5?atoi(argv[5]):1;
+  const int z_size = argc>6?atoi(argv[6]):1;
   // read image 
 
   shared_ptr<DiscretisedDensity<3,float> >  density_sptr = 
@@ -56,13 +56,16 @@ int main(int argc, char **argv)
       const int min2=(*density_sptr)[c[1]].get_min_index();
       const int max2=(*density_sptr)[c[1]].get_max_index();
 
+      if (modulo(c[1],z_spacing)>z_size)
+	continue; // just zeroes on this plane
+
       for (c[2]=min2; c[2]<=max2; ++c[2])
 	{
 	  const int min3=(*density_sptr)[c[1]][c[2]].get_min_index();
 	  const int max3=(*density_sptr)[c[1]][c[2]].get_max_index();
 	  for (c[3]=min3; c[3]<=max3; ++c[3])
 	    {
-	      if (modulo(c[2],spacing)<size && modulo(c[3],spacing)<size)
+	      if (modulo(c[2],xy_spacing)<xy_size && modulo(c[3],xy_spacing)<xy_size)
 		(*density_sptr)[c] = 1;
 	      else
 		(*density_sptr)[c] = 0;
