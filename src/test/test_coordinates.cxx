225bebce3eaadb832a823467265305b7e2dc6563
Kris Thielemans <Kris.Thielemans@stir>
2005-10-03 18:50:52 +0000

    - added tests for make_coordinate

--- src/test/test_coordinates.cxx
+++ src/test/test_coordinates.cxx
@@ -1,6 +1,23 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2000 PARAPET partners
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 
 /*!
   \file 
@@ -16,13 +33,10 @@
   $Revision$
 
 */
-/*
-    Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
-    See STIR/LICENSE.txt for details
-*/
 
+#include "stir/Coordinate2D.h"
 #include "stir/CartesianCoordinate3D.h"
+#include "stir/Coordinate4D.h"
 #include "stir/round.h"
 #include "stir/RunTests.h"
 #include <iostream>
@@ -325,6 +339,21 @@ coordinateTests::run_tests()
     check_if_equal(aint, Coordinate3D<int> (1,-1,3));
     const BasicCoordinate<3,float> af2(aint);
     check_if_equal(af2, Coordinate3D<float> (1.F,-1.F,3.F));
+  }
+
+  {
+    cerr << "Testing make_coordinate" << endl;
+    check_if_equal(make_coordinate(1.F)[1],1.F, "test make_coordinate with 1 arg");
+    check_if_equal(make_coordinate(1.F,3.4F),Coordinate2D<float>(1.F,3.4F), 
+		   "test make_coordinate with 2 args");
+    check_if_equal(make_coordinate(1.,3.4,-4.8),Coordinate3D<double>(1.,3.4,-4.8), 
+		   "test make_coordinate with 3 args");
+    check_if_equal(make_coordinate(1,2,3,4),Coordinate4D<int>(1,2,3,4), 
+		   "test make_coordinate with 4 args");
+    check_if_equal(make_coordinate(1,2,3,4,5),join(Coordinate4D<int>(1,2,3,4),5), 
+		   "test make_coordinate with 5 args");
+    check_if_equal(make_coordinate(1,2,3,4,5,6),join(join(Coordinate4D<int>(1,2,3,4),5),6), 
+		   "test make_coordinate with 6 args");
   }
 }
 
