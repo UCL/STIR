547815a00e691245eac088525c56f7e86698690b
Kris Thielemans <Kris.Thielemans@stir>
2005-10-04 00:51:51 +0000

    - commented out special rules for bcktest and fwdtest. They are intended to speed up the linking process, but are somewhat dangerous.

--- src/recon_test/exe.mk
+++ src/recon_test/exe.mk
@@ -9,17 +9,17 @@ $(dir)_SOURCES = bcktest.cxx fwdtest.cxx
 # Beware: the pattern below is dangerous as it relies on a naming style.
 # Note: have to be before the include below as that resets $(dir)
 
-${DEST}$(dir)/bcktest: ${DEST}$(dir)/bcktest$(O_SUFFIX) \
-   $(STIR_LIB) $(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES))
-	$(LINK) $(EXE_OUTFLAG)$@ $< \
-		$(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES)) \
-		 $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
+#${DEST}$(dir)/bcktest: ${DEST}$(dir)/bcktest$(O_SUFFIX) \
+#   $(STIR_LIB) $(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES))
+#	$(LINK) $(EXE_OUTFLAG)$@ $< \
+#		$(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES)) \
+#		 $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
 
 
-${DEST}$(dir)/fwdtest: ${DEST}$(dir)/fwdtest$(O_SUFFIX) \
-   $(STIR_LIB) $(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES))
-	$(LINK) $(EXE_OUTFLAG)$@ $< \
-		$(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES)) \
-		 $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
+#${DEST}$(dir)/fwdtest: ${DEST}$(dir)/fwdtest$(O_SUFFIX) \
+#   $(STIR_LIB) $(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES))
+#	$(LINK) $(EXE_OUTFLAG)$@ $< \
+#		$(filter %recon_buildblock_registries$(O_SUFFIX),$(STIR_REGISTRIES)) \
+#		 $(STIR_LIB)  $(LINKFLAGS) $(SYS_LIBS)
 
 include $(WORKSPACE)/exe.mk
