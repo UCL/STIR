cd5007326ea44f20c6c960d4911a97fbe7bb4ad6
Kris Thielemans <Kris.Thielemans@stir>
2005-10-11 17:18:25 +0000

    - removed *splitted versions - added estimate_scatter.cxx (which calls ScatterEstimationByBin)

--- src/scatter_utilities/exe.mk
+++ src/scatter_utilities/exe.mk
@@ -4,7 +4,9 @@
 dir := local/scatter
 
 $(dir)_SOURCES :=  estimate_single_scatter.cxx \
-	estimate_single_scatter_splitted.cxx \
+	estimate_scatter.cxx \
 	scale_single_scatter.cxx
+
+#	estimate_single_scatter_splitted.cxx \
 
 include $(WORKSPACE)/exe.mk
