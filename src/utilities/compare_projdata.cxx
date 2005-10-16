093b8ba2c5a90338f3d3a3406808389ef8331254
Kris Thielemans <Kris.Thielemans@stir>
2005-10-16 01:24:31 +0000

    - if max_segment_num parameter is specified, compare proj_data_info after reducing the segment range of both input data, such that we can compare data with different numbers of segments.

--- src/utilities/compare_projdata.cxx
+++ src/utilities/compare_projdata.cxx
@@ -1,7 +1,23 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2000 PARAPET partners
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
 
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
 \file
 \ingroup utilities
@@ -23,14 +39,6 @@ value (which is 1 on most systems) might also indicate an error reading
 the data.
 
 */
-/*
-    Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, Hammersmith Imanet
-    See STIR/LICENSE.txt for details
-*/
-
-
-
 
 #include "stir/ProjData.h"
 #include "stir/SegmentByView.h"
@@ -103,16 +111,27 @@ int main(int argc, char *argv[])
   shared_ptr<ProjData> first_operand=ProjData::read_from_file(argv[1]);
   shared_ptr<ProjData> second_operand=ProjData::read_from_file(argv[2]);
 
-  if (*first_operand->get_proj_data_info_ptr() !=
-      *second_operand->get_proj_data_info_ptr())
-    {
-      cout << "\nProjection data sizes or other data characteristics are not identical. \n"
-	   << "Use list_projdata_info to investigate.\n";
-      return EXIT_FAILURE;    }
   int max_segment=first_operand->get_max_segment_num();
   if(argc==4 && atoi(argv[3])>=0 && atoi(argv[3])<max_segment) 
     max_segment=atoi(argv[3]);
 
+  // compare proj_data_info
+  {
+    shared_ptr<ProjDataInfo> first_sptr =
+      first_operand->get_proj_data_info_ptr()->clone();
+    shared_ptr<ProjDataInfo> second_sptr =
+      second_operand->get_proj_data_info_ptr()->clone();
+    if (argc==4)
+      {
+	first_sptr->reduce_segment_range(-max_segment, max_segment);
+	second_sptr->reduce_segment_range(-max_segment, max_segment);
+      }
+    if (*first_sptr != *second_sptr)
+      {
+	cout << "\nProjection data sizes or other data characteristics are not identical. \n"
+	     << "Use list_projdata_info to investigate.\n";
+	return EXIT_FAILURE;    }
+  }
 
   const float tolerance=0.0001F;
 
