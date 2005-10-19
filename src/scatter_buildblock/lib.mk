4d8f1d72ded543c4d99ebacfe2cdce56663324a2
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-10-19 14:06:16 +0000

    - added code for scaling per viewgram as opposed to per sinogram

--- src/scatter_buildblock/lib.mk
+++ src/scatter_buildblock/lib.mk
@@ -15,9 +15,11 @@ $(dir)_LIB_SOURCES:= \
 	scatter_viewgram.cxx \
 	write_statistics.cxx \
 	scale_factors_per_sinogram.cxx \
+	scale_factors_per_viewgram.cxx \
 	att_estimate_for_no_scatter.cxx \
 	estimate_att_viewgram.cxx \
 	scale_scatter_per_sinogram.cxx \
+	scale_scatter_per_viewgram.cxx \
 	ScatterEstimationByBin.cxx
 
 #	scatter_estimate_for_all_scatter_points_splitted.cxx \
