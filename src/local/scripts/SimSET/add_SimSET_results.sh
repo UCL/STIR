69b44b56e948cd57545e56095cd24faab4bfc9b3
Kris Thielemans <Kris.Thielemans@stir>
2005-10-22 15:04:58 +0000

    - no longer process rec.weight here, but rely on convert_Simset_to_STIR_splitted.sh to be called first. - use mult_num_photons first

--- src/local/scripts/SimSET/add_SimSET_results.sh
+++ src/local/scripts/SimSET/add_SimSET_results.sh
@@ -1,66 +1,29 @@
 #! /bin/bash
 #
-
-set -e
-trap "echo ERROR in script " ERR
-echo 
-#echo dir $DIR
-#cd ~/sim_pet/data/${DIR}
-
-if [ ! -r rec.weight ]; then
-gunzip rec.weight.gz 
+if [ $# -lt 2 ]; then
+    echo "usage:"
+    echo "$0 out_dir input_dir1 input_dir2 ...."
+    exit 1
 fi
 
-for i in 
-results_dir=`~/simset/data/RESULTS$i
+out_dir=$1
+shift
+echo "will process $*"
+set -e
+trap "echo ERROR in script " ERR
 
-bin/printheader rec.weight |grep "Binning: min number of scatters" |awk '{ print $6 }'`
+mult_num_photons.sh $*
 
-num_scatter_bins=$(( $max_scatter_bin_num - $min_scatter_bin_num + 1 ))
+all=(doubles11  doubles20  multiples  singles  trues)
 
-all_scatter_bin_nums=`count $min_scatter_bin_num $max_scatter_bin_num`
-
-for i in $all_scatter_bin_nums ;
-do for j in $all_scatter_bin_nums ;
-do 
- scatter_bin_num=$(( ${num_scatter_bins} * $i + $j ))
- echo extracting scatter bin $scatter_bin_num
-
-~/parapet/PPhead/opt/local/SimSET/conv_SimSET_STIR \
-rec.weight fl \
-`~/simset/bin/printheader rec.weight |grep "Binning: number of AA bins" |awk '{ print $6 }'` \
-`~/simset/bin/printheader rec.weight |grep "Binning: number of TD bins" |awk '{ print $6 }'` \
-`~/simset/bin/printheader rec.weight |grep "Binning: number of Z bins" |awk '{ print $6 }'` $max_ring_difference \
-`~/simset/bin/printheader rec.weight |grep "Binning: max Transaxial Distance" |awk '{ print $5 }'` \
-${scatter_bin_num}  blue${i}_pink${j} ;
-done ; done 
-
-# get list of all contributing sinograms from blue/pink sinograms
-
-# first initialise lists to empty
-all_trues=
-all_singles=
-all_doubles=
-all_multiples=
-for i in $all_scatter_bin_nums;
-  do for j in $all_scatter_bin_nums;
-  do
-  current=blue${i}_pink${j}.hs
-  case $(( $i + $j )) in
-    0) all_trues="$all_trues $current";;
-    1) all_singles="$all_singles $current";;
-    2) all_doubles="$all_doubles $current";;
-    *) all_multiples="$all_multiples $current";;
-  esac
+for i in `count 0 $(( ${#all[*]} - 1))`; do
+  all_files=""
+  for dir in $*; do
+     all_files="${all_files} $dir/${all[i]}_norm.hs"
   done
-done
-
+  echo "Adding ${all_files}"  
 # TODO we currently pipe all output to /dev/null because there's a lot of stuff because blocksizes do not work
-stir_math -s --add trues $all_trues >& /dev/null
-stir_math -s --add singles $all_singles >& /dev/null
-stir_math -s --add doubles $all_doubles >& /dev/null
-stir_math -s --add multiples $all_multiples >& /dev/null
-
-rm -f blue*s
-
+trap "echo ERROR in executing stir_math  $out_dir/${all[i]}  ${all_files} " ERR
+  stir_math -s --add $out_dir/${all[i]}  ${all_files} >& /dev/null
+done
 
