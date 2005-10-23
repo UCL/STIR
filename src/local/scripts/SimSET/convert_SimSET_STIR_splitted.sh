bca90450973288e8cee9859351c5578f8926f788
Kris Thielemans <Kris.Thielemans@stir>
2005-10-23 16:55:23 +0000

    - now also support scatter_parameter==1

--- src/local/scripts/SimSET/convert_SimSET_STIR_splitted.sh
+++ src/local/scripts/SimSET/convert_SimSET_STIR_splitted.sh
@@ -5,8 +5,8 @@
 #  Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
 #      See STIR/LICENSE.txt for details
 #      
-# Author /Kris Thielemans
-# Author /Charalampos Tsoumpas 
+# Author Kris Thielemans
+# Author Charalampos Tsoumpas 
 # This script is used to convert SimSET output into STIR projdata format, splitting double scatter sinogram into different files.
 
 #! /bin/bash
@@ -21,6 +21,7 @@ max_ring_difference=$1
 
 set -e
 trap "echo ERROR in script " ERR
+PRINTHEADER=${SIMSET_DIR}/bin/printheader
 
 #echo dir $DIR
 #cd ~/sim_pet/data/${DIR}
@@ -29,28 +30,43 @@ if [ ! -r rec.weight ]; then
 gunzip rec.weight.gz 
 fi
 
-min_scatter_bin_num=`${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: min number of scatters" |awk '{ print $6 }'`
-max_scatter_bin_num=`${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: max number of scatters" |awk '{ print $6 }'`
+min_scatter_bin_num=`${PRINTHEADER} rec.weight |grep "Binning: min number of scatters" |awk '{ print $6 }'`
+max_scatter_bin_num=`${PRINTHEADER} rec.weight |grep "Binning: max number of scatters" |awk '{ print $6 }'`
+
+scatter_parameter=`${PRINTHEADER} rec.weight |grep "Binning: scatter parameter" |awk '{ print $4 }'`
 
 num_scatter_bins=$(( $max_scatter_bin_num - $min_scatter_bin_num + 1 ))
 
+conv_cmdline="conv_SimSET_STIR \
+        rec.weight fl \
+        `${PRINTHEADER} rec.weight |grep "Binning: number of AA bins" |awk '{ print $6 }'` \
+        `${PRINTHEADER} rec.weight |grep "Binning: number of TD bins" |awk '{ print $6 }'` \
+        `${PRINTHEADER} rec.weight |grep "Binning: number of Z bins" |awk '{ print $6 }'` \
+        $max_ring_difference \
+        `${PRINTHEADER} rec.weight |grep "Binning: max Transaxial Distance" |awk '{ print $5 }'` \
+        `${PRINTHEADER} rec.weight |grep "Binning: range on Z value" |awk '{ print $6 }'` "
+
 all_scatter_bin_nums=`count $min_scatter_bin_num $max_scatter_bin_num`
+
+case $scatter_parameter in 
+
+  1)
+  if [ $min_scatter_bin_num == 0 ]; then
+    ${conv_cmdline}  0 trues
+  fi
+  if [ $max_scatter_bin_num == 1 ]; then
+    ${conv_cmdline} 1 scatter
+  fi;;
+
+
+  3) # scatter_parameter
 
 for i in $all_scatter_bin_nums ;
 do for j in $all_scatter_bin_nums ;
   do 
      scatter_bin_num=$(( ${num_scatter_bins} * $i + $j ))
      echo extracting scatter bin $scatter_bin_num
-
-     conv_SimSET_STIR \
-        rec.weight fl \
-        `${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: number of AA bins" |awk '{ print $6 }'` \
-        `${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: number of TD bins" |awk '{ print $6 }'` \
-        `${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: number of Z bins" |awk '{ print $6 }'` \
-        $max_ring_difference \
-        `${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: max Transaxial Distance" |awk '{ print $5 }'` \
-        `${SIMSET_DIR}/bin/printheader rec.weight |grep "Binning: range on Z value" |awk '{ print $6 }'` \
-        ${scatter_bin_num} blue${i}_pink${j};
+     ${conv_cmdline} ${scatter_bin_num} blue${i}_pink${j};
   done 
 done 
 
@@ -85,5 +101,12 @@ stir_math -s --add doubles20 $all_doubles1 $all_doubles2 >& /dev/null
 stir_math -s --add multiples $all_multiples >& /dev/null
 
 rm -f blue*s
+  #end of  scatter_param == 3
+  ;;  
 
+  *)
+    echo "scatter_parameter $scatter_parameter currently unsupported"
+    exit 1
+    ;;
 
+esac
