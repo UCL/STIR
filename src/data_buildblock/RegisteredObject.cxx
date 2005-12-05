8926787f8085f0b89b1124a0b6d3e1f4198f0338
Kris Thielemans <Kris.Thielemans@stir>
2005-12-05 10:47:10 +0000

    - moved SinglesRates from local to global distro

---
+++ src/data_buildblock/RegisteredObject.cxx
@@ -0,0 +1,64 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*!
+
+  \file
+  \ingroup buildblock
+  \brief instantiations of RegisteredObject for classes in recon_buildblock
+  (only useful for Microsoft Visual Studio 6.0)
+
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+
+// note: include has to be before #ifdef as it's in this file that
+// __STIR_REGISTRY_NOT_INLINE is defined
+#include "stir/RegisteredObject.h"
+#ifdef __STIR_REGISTRY_NOT_INLINE
+
+#pragma message("instantiating RegisteredObject<SinglesRates >")
+#include "stir/data/SinglesRates.h"
+
+
+// and others
+START_NAMESPACE_STIR
+
+template <typename Root>
+RegisteredObject<Root>::RegistryType& 
+RegisteredObject<Root>::registry ()
+{
+  static RegistryType the_registry("None", 0);
+  return the_registry;
+}
+
+#  ifdef _MSC_VER
+// prevent warning message on reinstantiation, 
+// note that we get a linking error if we don't have the explicit instantiation below
+#  pragma warning(disable:4660)
+#  endif
+
+
+template RegisteredObject<SinglesRates >;
+
+END_NAMESPACE_STIR
+
+#endif
