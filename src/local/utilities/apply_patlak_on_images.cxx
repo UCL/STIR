ed8e47878d552fba5a949f69251305cd0db3ad00
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-12-07 22:57:54 +0000

    Apply simple decay correction method. Apply calibration factors for quantification. Introduce more input arguments. Output files are both parameters. Devide with the plasma_vector with the time duration, as CTI for frames.

--- src/local/utilities/apply_patlak_on_images.cxx
+++ src/local/utilities/apply_patlak_on_images.cxx
@@ -34,11 +34,9 @@
 #include "stir/linear_regression.h"
 #include "stir/shared_ptr.h"
 #include "stir/Array.h"
-#include "stir/VectorWithOffset.h"
-#include "stir/DiscretisedDensityOnCartesianGrid.h"
 #include "stir/Succeeded.h"
-#include "stir/IO/interfile.h"
-#include "stir/DiscretisedDensity.h"
+#include "stir/VectorWithOffset.h"
+#include "stir/IO/DefaultOutputFileFormat.h"
 #include <utility>
 #include <vector>
 #include <string>
@@ -57,42 +55,55 @@ int main(int argc, char *argv[])
 { 
   USING_NAMESPACE_STIR
   
-  if (argc<3 || argc>4)
+  if (argc<3 || argc>8)
     {
       std::cerr << "Usage:" << argv[0] << "\n"
 	   << "\t[dynamic_image_filename]\n"
-	   << "\t[plasma_data_filename]\n" ;
+	   << "\t[plasma_data_filename]\n" 
+	   << "\t[input function time_shift in sec]\n" 
+	   << "\t[blood volume (bv)]\n" 
+	   << "\t[Model starting time]\n" 
+	   << "\t[is_decay_corrected]\n"
+	   << "\ttime_shift: default to 13 sec\n"
+	   << "\tbv: default to 0.05\n" 
+	   << "\tstarting_time: default to 0 sec\n"
+	   << "\tis_decay_corrected: is set to false. Give 1 for true.\n";
       return EXIT_FAILURE;            
     }       
-
+  const float time_shift = argc>=4 ? atof(argv[3]) : 13.F ;
+  const float bv = argc>=5 ? atof(argv[4]) : 0.05F ; // Blood Volume, usually constant. ChT::ToDo:Maybe add to the PlasmaData class!!!
+  const float starting_time = argc>=6 ? atof(argv[5]) : 0.F ;
+  const bool is_decay_corrected = argc>=7 ? atoi(argv[6]) : false ;
   //Read Dynamic Sequence of ECAT7 Images, in respect to their center in x, y axes as origin
   const shared_ptr< DynamicDiscretisedDensity >  dyn_image_sptr= 
   DynamicDiscretisedDensity::read_from_file(argv[1]);
-  DynamicDiscretisedDensity& dyn_image = *dyn_image_sptr;// REFERENCE OK???
+  DynamicDiscretisedDensity dyn_image = *dyn_image_sptr;// NOT REFERENCE BECAUSE THEY ARE CORRECTED AFTERWARDS!!!
 
   const unsigned int num_frames=(dyn_image.get_time_frame_definitions()).get_num_frames();
   string input_string(argv[1]);
-  //  string input_string_cut(input_string.c_str());
-  string output_string;
-  output_string += "slope_"+ input_string  ;
- 
-  shared_ptr<DiscretisedDensity<3,float> > output_image_sptr = 
+  // Prepare slope_image
+  string slope_string;
+  slope_string += "slope_it_"+ input_string  ;
+  shared_ptr<DiscretisedDensity<3,float> > slope_image_sptr = 
     dyn_image_sptr->get_density(1).clone();
-  DiscretisedDensity<3,float>& output_image = *output_image_sptr;// REFERENCE OK???
+  DiscretisedDensity<3,float>& slope_image = *slope_image_sptr;
+  // Prepare constant_image
+  string constant_string;
+  constant_string += "constant_it_"+ input_string  ;
+  shared_ptr<DiscretisedDensity<3,float> > constant_image_sptr = 
+    dyn_image_sptr->get_density(1).clone();
+  DiscretisedDensity<3,float>& constant_image = *constant_image_sptr;
 
-
-  // Calibrate and Decay Correct Image Data
-  dyn_image.calibrate_frames();
-  //  dyn_image.decay_correct_frames();
-
+  // Apply calibration_factor and Decay Correct Frame Image Data
+   dyn_image.calibrate_frames();
+   if(!is_decay_corrected)
+     dyn_image.decay_correct_frames();
 
   // Read the Plasma Data from the given file.
   // Shift the Plasma Data as appropriately. 
-  // ChT::ToDo:In the future this value should be either given externally, or estimated somehow, in the fit process.  
   PlasmaData plasma_data;
   plasma_data.read_plasma_data(argv[2]);   // The implementation currently assumes three list file of blood. 
-  plasma_data.shift_time(13.F); // Shift of 13 seconds. ChT::ToDo:give automatically from argv[3].
-  const float bv = 0.05F ;// Blood Volume, usually constant. ChT::ToDo:Maybe add to the PlasmaData class!!!
+  plasma_data.shift_time(time_shift);
 
   // Initialise the vector where the plasma values for each frame to 0. 
   std::vector<float>  plasma_vector(num_frames,0) ; 
@@ -134,7 +145,7 @@ int main(int argc, char *argv[])
 	    }
 	}
 	    plasma_sum_vector[frame_num-1]+=plasma_vector[frame_num-1] ;
-	    // The reconstructed CTI images and the STIR (when using reconstruction script) are alwaysdevided by the time frame duration.
+	    // The reconstructed CTI images and the STIR (when using reconstruction script) are always devided by the time frame duration.
 	    plasma_vector[frame_num-1]/=(dyn_image.get_time_frame_definitions()).get_duration(frame_num);
     }
 
@@ -151,17 +162,42 @@ int main(int argc, char *argv[])
     VectorWithOffset<float> patlak_y(0,num_frames-1);
     VectorWithOffset<float> patlak_x(0,num_frames-1);
     VectorWithOffset<float> weights(0,num_frames-1);
-
-	const int min_k_index = output_image.get_min_index(); 
-	const int max_k_index = output_image.get_max_index();
+#if 0 // This is to simplify the method, but not working, yet.
+ 
+	 DiscretisedDensity<3,float>::full_iterator slope_iter = slope_image.begin_all();
+	 while( slope_iter != slope_image.end_all()) //ChT::ToDo:ParametricImage Class
+		{
+      		  const int counter=slope_iter-slope_image.begin_all();
+		  for (  frame_num = 1;  //ChT::TakeCare: Frames time is assumed not to overlap.
+			   frame_num<=num_frames ; ++frame_num )
+		    {
+		        DiscretisedDensity<3,float>::const_full_iterator frame_iter = dyn_image[frame_num].begin_all_const();
+       			patlak_y[frame_num-1]=*(counter+frame_iter)-bv*blood_vector[frame_num-1])/plasma_vector[frame_num-1];
+			patlak_x[frame_num-1]=plasma_sum_vector[frame_num-1]/plasma_vector[frame_num-1];
+			weights[frame_num-1]=1;
+			linear_regression(constant, scale,
+				       chi_square,
+				       variance_of_constant,
+				       variance_of_scale,
+				       covariance_of_constant_with_scale,
+				       patlak_y,
+				       patlak_x,		      
+				       weights);
+			*slope_iter=scale;
+		}
+	       ++slope_iter;
+       }
+#else
+	const int min_k_index = slope_image.get_min_index(); 
+	const int max_k_index = slope_image.get_max_index();
 	for ( int k = min_k_index; k<= max_k_index; ++k)
 	  {
-	    const int min_j_index = output_image[k].get_min_index(); 
-	    const int max_j_index = output_image[k].get_max_index();
+	    const int min_j_index = slope_image[k].get_min_index(); 
+	    const int max_j_index = slope_image[k].get_max_index();
 	    for ( int j = min_j_index; j<= max_j_index; ++j)
 	      {
-		const int min_i_index = output_image[k][j].get_min_index(); 
-		const int max_i_index = output_image[k][j].get_max_index();
+		const int min_i_index = slope_image[k][j].get_min_index(); 
+		const int max_i_index = slope_image[k][j].get_max_index();
 		for ( int i = min_i_index; i<= max_i_index; ++i)
 		   {
 		     for (  frame_num = 1;  //ChT::TakeCare: Frames time is assumed not to overlap.
@@ -180,14 +216,23 @@ int main(int argc, char *argv[])
 				       patlak_y,
 				       patlak_x,		      
 				       weights);			
-		     output_image[k][j][i]=static_cast<float> (scale);
+		     slope_image[k][j][i]=scale;
+		     constant_image[k][j][i]=constant;
 	      }
 	  }
 	  }
+#endif
   }
-		Succeeded success = 
-			write_basic_interfile(output_string, *output_image_sptr);
-		return success == Succeeded::yes ? EXIT_SUCCESS : EXIT_FAILURE;	
+
+  shared_ptr<OutputFileFormat> output_format_sptr =
+    new DefaultOutputFileFormat;
+  std::cerr << "Writing 'slope'-image: " << slope_string << endl;
+
+  Succeeded success =
+    output_format_sptr->write_to_file(slope_string, *slope_image_sptr);
+  std::cerr << "Writing 'constant'-image: " << constant_string << endl;
+  success = Succeeded::yes ? output_format_sptr->write_to_file(constant_string, *constant_image_sptr) : Succeeded::no ;
+  return success == Succeeded::yes ? EXIT_SUCCESS : EXIT_FAILURE;	
 }
 
 
