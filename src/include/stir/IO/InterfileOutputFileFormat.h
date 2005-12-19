4775af2dd40a522812392bcf0db0de0361a234d1
Kris Thielemans <Kris.Thielemans@stir>
2005-12-19 16:06:14 +0000

    - added set_defaults(), calling OutputFileFormat::set_defaults(). Fixes problem that scale factor was not reinitialised. - updated copyright message

--- src/include/stir/IO/InterfileOutputFileFormat.h
+++ src/include/stir/IO/InterfileOutputFileFormat.h
@@ -1,20 +1,31 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2003-$Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
 
   \file
   \ingroup InterfileIO
-  \brief Declaration of class InterfileOutputFileFormat
+  \brief Declaration of class stir::InterfileOutputFileFormat
 
   \author Kris Thielemans
 
   $Date$
   $Revision$
-*/
-/*
-    Copyright (C) 2003-$Date$, IRSL
-    See STIR/LICENSE.txt for details
 */
 
 #ifndef __stir_IO_InterfileOutputFileFormat_H__
@@ -52,6 +63,7 @@ public :
 		  const DiscretisedDensity<3,float>& density) const;
 
 
+  virtual void set_defaults();
   virtual void initialise_keymap();
   virtual bool post_processing();
 
