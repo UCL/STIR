bb85460acc81be897067f3185b5984639ce52f1b
Kris Thielemans <Kris.Thielemans@stir>
2005-12-19 16:00:06 +0000

    - call OutputFileFormat::set_defaults() in constructor. Fixes problem that scale factor was not initialised. - updated copyright message

--- src/IO/ECAT6OutputFileFormat.cxx
+++ src/IO/ECAT6OutputFileFormat.cxx
@@ -1,20 +1,31 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2002-$Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
 
   \file
   \ingroup ECAT
-  \brief Implementation of class ECAT6OutputFileFormat
+  \brief Implementation of class stir::ECAT6OutputFileFormat
 
   \author Kris Thielemans
 
   $Date$
   $Revision$
-*/
-/*
-    Copyright (C) 2002-2$Date$, IRSL
-    See STIR/LICENSE.txt for details
 */
 
 #include "stir/IO/ECAT6OutputFileFormat.h"
@@ -36,6 +47,7 @@ ECAT6OutputFileFormat::
 ECAT6OutputFileFormat(const NumericType& type, 
                    const ByteOrder& byte_order) 
 {
+  OutputFileFormat::set_defaults();
   set_type_of_numbers(type);
   set_byte_order(byte_order);
 }
