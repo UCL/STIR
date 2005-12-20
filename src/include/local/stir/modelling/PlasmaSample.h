7b451fa27c46083f662134edd7ebdb21370684be
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-12-20 12:10:50 +0000

    PlasmaSample class has been moved to different file. It is used for brain studies in Hammersmith Imanet Ltd. The procedure is for the 962 and 966 scanners.

---
+++ src/include/local/stir/modelling/PlasmaSample.h
@@ -0,0 +1,76 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2005 - $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*!
+  \file
+  \ingroup modelling
+
+  \brief Declaration of class stir::PlasmaData
+
+  \author Charalampos Tsoumpas
+ 
+  $Date$
+  $Revision$
+*/
+
+#ifndef __stir_modelling_PlasmaSample_H__
+#define __stir_modelling_PlasmaSample_H__
+
+#include "stir/common.h"
+#include <vector>
+#include <fstream>
+
+START_NAMESPACE_STIR
+
+class PlasmaSample
+{ 
+public:
+   //! default constructor
+  inline PlasmaSample();
+
+  //!  A constructor : constructs a PlasmaSample object  
+  inline PlasmaSample( const float sample_time, const float plasma_sample_counts, const float blood_sample_counts);
+
+  //! default destructor
+  inline ~PlasmaSample();
+   
+ //! set the time of the sample
+  inline void set_time_in_s( const float time );
+ //! get the time of the sample
+  inline float get_time_in_s() const; 
+ //! set the blood counts of the sample
+  inline void set_blood_counts_in_kBq( const float blood_counts );
+ //! get the blood counts of the sample
+  inline float get_blood_counts_in_kBq() const; 
+ //! set the plasma counts of the sample
+  inline void set_plasma_counts_in_kBq( const float plasma_counts );
+ //! get the plasma counts of the sample
+  inline float get_plasma_counts_in_kBq() const; 
+  
+private : 
+  float _blood_counts;
+  float _plasma_counts;
+  float _time;
+};
+
+END_NAMESPACE_STIR
+
+#include "local/stir/modelling/PlasmaSample.inl"
+
+#endif //__stir_modelling_PlasmaSample_H__
