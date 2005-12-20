d8dc8737ac039309d03189f64457e4bc9c44e895
Charalampos Tsoumpas <Charalampos.Tsoumpas@stir>
2005-12-20 12:35:35 +0000

    Lib for the modelling functions.

---
+++ src/local/modelling/lib.mk
@@ -0,0 +1,12 @@
+#
+# $Id$
+#
+
+dir := local/modelling
+$(dir)_LIB_SOURCES:= \
+	apply_patlak_to_images.cxx 
+
+#$(dir)_REGISTRY_SOURCES:= local_modelling_registries.cxx
+
+include $(WORKSPACE)/lib.mk
+
