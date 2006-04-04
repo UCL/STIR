ace34da01bad971cfcc998de59f27d9d535007c8
Nikos Dikaios <Nikos.Dikaios@stir>
2006-04-04 19:38:06 +0000

    - renamed cached_factors functions (and split them into 2, one for activity and one for attenuation). Also included the check for caching in there. This means that we can these functions now always, i.e. we don't have to check on caching anymore outside these functions. This enables removing some lines in the scatter_estimate functions.

--- src/scatter_buildblock/cached_single_scatter_integrals.cxx
+++ src/scatter_buildblock/cached_single_scatter_integrals.cxx
@@ -2,74 +2,97 @@
 // $Id$
 //
 /*!
-\file
-\ingroup scatter
-\brief Implementations of functions defined in Scatter.h
-Function calculates the integral along LOR in an image (attenuation or emission). 
-(From scatter point to detector coordinate)
+  \file
+  \ingroup scatter
+  \brief Implementations of functions defined in stir::ScatterEstimationByBin
+
+  Function calculates the integral along LOR in an image (attenuation or emission). 
+  (From scatter point to detector coordinate)
 
   \author Charalampos Tsoumpas
+  \author Nikolaos Dikaios
   \author Kris Thielemans
   	  
-		$Date$
-		$Revision$
+  $Date$
+  $Revision$
 		
-		  Copyright (C) 2004- $Date$, Hammersmith Imanet
-		  See STIR/LICENSE.txt for details
+  Copyright (C) 2004- $Date$, Hammersmith Imanet
+  See STIR/LICENSE.txt for details
 */
 #include "local/stir/ScatterEstimationByBin.h"
 #include "stir/IndexRange.h" 
-#include "stir/Array.h"
-#include "stir/Coordinate3D.h"
-#include "stir/CartesianCoordinate2D.h" 
+#include "stir/Coordinate2D.h"
 START_NAMESPACE_STIR
+
+const float cache_init_value = -1.234F; // an arbitrary value that should never occur
+
+void
+ScatterEstimationByBin::
+initialise_cache_for_scattpoint_det()
+{
+  IndexRange<2> range (Coordinate2D<int> (0,0), 
+		       Coordinate2D<int> (static_cast<int>(this->scatt_points_vector.size()-1),
+					  this->total_detectors-1));
+
+  this->cached_activity_integral_scattpoint_det.resize(range);
+  this->cached_attenuation_integral_scattpoint_det.resize(range);
+
+  this->cached_activity_integral_scattpoint_det.fill(cache_init_value);
+  this->cached_attenuation_integral_scattpoint_det.fill(cache_init_value);
+}
 
 float 
 ScatterEstimationByBin::
-cached_factors(const unsigned scatter_point_num, 
-	       const unsigned det_num,
-	       const image_type input_image_type)
+cached_integral_over_activity_image_between_scattpoint_det(const unsigned scatter_point_num, 
+							   const unsigned det_num)
 {		 
-  static IndexRange<3> range (Coordinate3D<int> (0,0,0), 
-			      Coordinate3D<int> (1,
-						 static_cast<int>(scatt_points_vector.size()-1),
-						 total_detectors-1));
-  static Array<3,float> 
-    cached_integral_scattpoint_det(range);
-  static bool to_initialize_array = true;
-	if(to_initialize_array)		
-	{
-		cached_integral_scattpoint_det.fill(-1.F);
-		to_initialize_array = false;
-	}	
-	if(input_image_type==act_image_type)
-	{	
-		if (cached_integral_scattpoint_det[0][scatter_point_num][det_num]<0)
-		  {
-		    cached_integral_scattpoint_det[0][scatter_point_num][det_num]=
-		      integral_over_activity_image_between_scattpoint_det(
-					      scatt_points_vector[scatter_point_num].coord,
-					      detection_points_vector[det_num]);
-		  }
-		return 
-		  cached_integral_scattpoint_det[0][scatter_point_num][det_num];
-	}
-	else if(input_image_type==att_image_type)
-	{
-		if (cached_integral_scattpoint_det[1][scatter_point_num][det_num]<0)
-		  {
-		    cached_integral_scattpoint_det[1][scatter_point_num][det_num]=
-		      exp_integral_over_attenuation_image_between_scattpoint_det(
-						   scatt_points_vector[scatter_point_num].coord,
-						   detection_points_vector[det_num]);
-		  }
-		return 
-		  cached_integral_scattpoint_det[1][scatter_point_num][det_num];
-	}		
-	else 
-	  {
-	    error("cached_factors: wrong type of image\n");
-	    return 0;// to avoid compiler warnings, but will never get here
-	  }
-}	
+  float * location_in_cache = 
+    this->use_cache
+    ? &cached_activity_integral_scattpoint_det[scatter_point_num][det_num]
+    : 0;
+
+  if (this->use_cache && *location_in_cache!=cache_init_value)
+    {
+      return *location_in_cache;
+    }
+  else
+    {
+      const float result =
+	integral_over_activity_image_between_scattpoint_det
+	(scatt_points_vector[scatter_point_num].coord,
+	 detection_points_vector[det_num]
+	 );
+      if (this->use_cache)
+	*location_in_cache=result;
+      return result;
+    }
+}
+
+float 
+ScatterEstimationByBin::
+cached_exp_integral_over_attenuation_image_between_scattpoint_det(const unsigned scatter_point_num, 
+								  const unsigned det_num)
+{
+  float * location_in_cache = 
+    this->use_cache
+    ? &cached_attenuation_integral_scattpoint_det[scatter_point_num][det_num]
+    : 0;
+
+  if (this->use_cache && *location_in_cache!=cache_init_value)
+    {
+      return *location_in_cache;
+    }
+  else
+    {
+      const float result =
+	exp_integral_over_attenuation_image_between_scattpoint_det
+	(scatt_points_vector[scatter_point_num].coord,
+	 detection_points_vector[det_num]
+	 );
+      if (this->use_cache)
+	*location_in_cache=result;
+      return result;
+    }
+}
+	
 END_NAMESPACE_STIR
