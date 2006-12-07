8ece05d0db1fa0641ca8409015f8cd534e17b5c8
Kris Thielemans <Kris.Thielemans@stir>
2006-12-07 18:30:29 +0000

    - put in --min-scale-factor option

--- src/local/scatter/scale_single_scatter.cxx
+++ src/local/scatter/scale_single_scatter.cxx
@@ -39,35 +39,62 @@ See STIR/LICENSE.txt for details
 #include "stir/IndexRange2D.h" 
 #include "stir/stream.h"
 #include "stir/Succeeded.h"
+#include "stir/thresholding.h"
 #include "stir/is_null_ptr.h"
 
 /***********************************************************/     
 
+static void
+print_usage_and_exit(const char * const prog_name)
+{
+  std::cerr << "\nUsage:\n" << prog_name << "\n"
+	    << "\t[--min-scale-factor number]\n"
+	    << "\tattenuation_correction_factors\n"
+	    << "\temission_projdata\n"
+	    << "\tscatter_projdata\n" 
+	    << "\toutput_filename\n"
+	    << "\t[attenuation_threshold \n"
+	    << "\t[correct_for_interleaving]\n"
+	    << "\t[estimate_scale_factor]\n"
+#ifdef SCFOLD
+	    << "\t[mask_radius_in_mm]]]\n"
+#else
+	    << "\t[back_off_size]]\n"
+#endif
+	    << "correct_for_interleaving default to 1\n"
+	    << "attenuation_threshold defaults to 1.01\n" 
+	    << "estimate_scale_factor defaults to 1 for scaling per sinogram\n"
+	    << "0 for  no scaling, 2 for by viewgram\n"
+	    << "If mask_radius_in_mm is not specified, will use all available data\n";
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, const char *argv[])                                  
 {         
-	USING_NAMESPACE_STIR
-	if (argc< 5 || argc>9)
+  USING_NAMESPACE_STIR;
+  const char * const prog_name = argv[0];
+
+  float min_scale_factor = 1.E-5F;
+  // option processing
+  while (argc>1 && argv[1][1] == '-')
+    {
+      if (strcmp(argv[1], "--min-scale-factor")==0)
 	{
-	   std::cerr << "Usage:\n" << argv[0] << "\n"
-		     << "\tattenuation_correction_factors\n"
-		     << "\temission_projdata\n"
-		     << "\tscatter_projdata\n" 
-		     << "\toutput_filename\n"
-		     << "\t[attenuation_threshold \n"
-		     << "\t[correct_for_interleaving]\n"
-		     << "\t[estimate_scale_factor]\n"
-#ifdef SCFOLD
-		     << "\t[mask_radius_in_mm]]]\n"
-#else
-		     << "\t[back_off_size]]\n"
-#endif
-		     << "correct_for_interleaving default to 1\n"
-		     << "attenuation_threshold defaults to 1.01\n" 
-		     << "estimate_scale_factor defaults to 1 for scaling per sinogram\n"
-		     << "0 for  no scaling, 2 for by viewgram\n"
-		     << "If mask_radius_in_mm is not specified, will use all available data\n";
-		return EXIT_FAILURE;            
-	}      
+	  min_scale_factor = atof(argv[2]);
+	  argc-=2; argv +=2;
+	}
+      else
+	{
+	  std::cerr << "\nUnknown option: " << argv[1];
+	  print_usage_and_exit(prog_name);
+	}
+    }
+    
+  if (argc< 5 || argc>9)
+    {
+      print_usage_and_exit(prog_name);
+    }      
+
 	const float attenuation_threshold = argc>=6 ? atof(argv[5]) : 1.01 ;
 	const bool remove_interleaving = argc>=7 ? atoi(argv[6]) : 1 ; 
 	const int est_scale_factor_per_sino = argc>=8 ? atoi(argv[7]) : 1 ; 
@@ -125,6 +152,11 @@ int main(int argc, const char *argv[])
 #endif
 				       );
 	    std::cout << scale_factors;
+	    threshold_lower(scale_factors.begin_all(), 
+			    scale_factors.end_all(),
+			    min_scale_factor);
+	    std::cout << "After thresholding:\n";
+	    std::cout << scale_factors;
 	    std::cout << "applying scale factors" << std::endl;
 	    scale_scatter_per_sinogram(scaled_scatter_proj_data, 
 				       interpolated_scatter,
@@ -148,6 +180,11 @@ int main(int argc, const char *argv[])
 				       -1.F // TODO back_off not implemented yet
 #endif
 );
+	    std::cout << scale_factors;
+	    threshold_lower(scale_factors.begin_all(), 
+			    scale_factors.end_all(),
+			    min_scale_factor);
+	    std::cout << "After thresholding:\n";
 	    std::cout << scale_factors;
 	    std::cout << "applying scale factors" << std::endl;
 	    scale_scatter_per_viewgram(scaled_scatter_proj_data, 
