2a1b10103a48a5e0c15c90ddded7ba7d66ffbc08
Kris Thielemans <Kris.Thielemans@stir>
2007-08-28 09:10:13 +0000

    *** empty log message ***

---
+++ src/examples/extra_dirs.mk
@@ -0,0 +1,11 @@
+#
+# $Id$
+#
+# sample file that includes the examples directory into the STIR build.
+# You should copy this file to STIR/local (unless you have one there already, in 
+# which case you need to edit that file instead of course).
+# See also examples/README.txt and the section on how to extend STIR with your own 
+# files in the STIR developer's Guide.
+
+
+EXEDIRS += examples
