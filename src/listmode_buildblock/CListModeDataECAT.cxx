c29d14e5b199edf9253c58d3484fd1eb7c2abdba
Kris Thielemans <Kris.Thielemans@stir>
2007-10-08 12:22:11 +0000

    - added copyright - prefix class name with stir:: in doxygen

--- src/listmode_buildblock/CListModeDataECAT.cxx
+++ src/listmode_buildblock/CListModeDataECAT.cxx
@@ -1,21 +1,31 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup listmode  
-  \brief Implementation of class CListModeDataECAT
+  \brief Implementation of class stir::CListModeDataECAT
     
   \author Kris Thielemans
       
   $Date$
   $Revision$
-*/
-/*
-    Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
-    This software is distributed under the terms 
-    of the GNU Lesser General  Public Licence (LGPL).
-    See STIR/LICENSE.txt for details
 */
 
 
