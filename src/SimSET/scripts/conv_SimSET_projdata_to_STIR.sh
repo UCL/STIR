fe3f3f2016e6d5bf7b40904cd9aa2e6ca13eb3c1
Kris Thielemans <Kris.Thielemans@stir>
2008-03-22 00:10:35 +0000

    - added copyright and a bit more documentation

--- src/SimSET/scripts/conv_SimSET_projdata_to_STIR.sh
+++ src/SimSET/scripts/conv_SimSET_projdata_to_STIR.sh
@@ -5,11 +5,28 @@
 #
 #
 #  Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
-#      See STIR/LICENSE.txt for details
+#  This file is part of STIR.
+#
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  See STIR/LICENSE.txt for details
 #      
 # Author Kris Thielemans
 # Author Charalampos Tsoumpas 
-# This script is used to convert SimSET output into STIR projdata format, splitting double scatter sinogram into different files.
+# This script is used to convert SimSET output into STIR projdata format.
+# Multiple files are written for the different scatter orders in the Simset file (if present).
+# Up to double scatter sinogram is currently in different files. Higher order scatter is
+# currently called "multiples" which is not a good name.
+
+# TODO: currently simset_filename and scanner_name are hard-wired in this script
 
 if [ $# -ne 1 ]; then
     echo "usage:"
