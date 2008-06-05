ac0ab3506683059d013563bac8a016fde44dd7bf
Kris Thielemans <Kris.Thielemans@stir>
2008-06-05 13:07:52 +0000

    corrected copyright (mentioned CTI stuff which was inappropriate)

--- src/recon_buildblock/recon_buildblock_registries.cxx
+++ src/recon_buildblock/recon_buildblock_registries.cxx
@@ -5,12 +5,7 @@
     Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
     This file is part of STIR.
 
-    Some parts of this file originate in CTI code, distributed as
-    part of the matrix library from Louvain-la-Neuve, and hence carries
-    its restrictive license. Affected parts are the dead-time correction
-    in get_deadtime_efficiency and geo_Z_corr related code.
-
-    Most of this file is free software; you can redistribute that part and/or modify
+    This file is free software; you can redistribute that part and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation; either version 2.1 of the License, or
     (at your option) any later version.
@@ -26,7 +21,7 @@
   \file
   \ingroup recon_buildblock
 
-  \brief File that registers all RegisterObject children in recon_buildblock
+  \brief File that registers all stir::RegisterObject children in recon_buildblock
 
   \author Kris Thielemans
   
