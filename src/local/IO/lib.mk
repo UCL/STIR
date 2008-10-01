f53b3b8023349d86cccd3ba18b03d7fa63fe92d8
Kris Thielemans <Kris.Thielemans@stir>
2008-10-01 14:13:40 +0000

    - added local_InputFileFormatRegistry.cxx and renamed OutputFileFormatRegistry.cxx

--- src/local/IO/lib.mk
+++ src/local/IO/lib.mk
@@ -7,7 +7,8 @@ dir := local/IO
 $(dir)_LIB_SOURCES:= \
 	InterfileDynamicDiscretisedDensityOutputFileFormat.cxx \
 	InterfileParametricDensityOutputFileFormat.cxx \
-	OutputFileFormat_default.cxx
+	local_OutputFileFormat_default.cxx \
+	local_InputFileFormatRegistry.cxx
 
 ifeq ($(HAVE_LLN_MATRIX),1)
   $(dir)_LIB_SOURCES += ECAT7ParametricDensityOutputFileFormat.cxx \
