790acf75822e42a3ef96b6f2e33f071316bbc6b2
Kris Thielemans <Kris.Thielemans@stir>
2009-04-30 23:43:19 +0000

    added new targets dir/filename such that you can make a single executable without have to specifying DEST

--- src/exe.mk
+++ src/exe.mk
@@ -13,19 +13,27 @@
 #    $(dir)_SOURCES must be set to a list of .cxx and/or .c files (without path)
 # Result:
 # targets $(dir) clean_exes_$(dir) install_exes_$(dir), uninstall_exes_$(dir)
+#    and for each file in $(dir)_SOURCES there will be a (phony) target
+#    $(dir)/file (without extension) set to depend on $(DEST)$(dir)/file
+#    (see below)
 # variables $(dir)_SOURCES (with $dir)/ prepended)
 #           $(dir)_EXES, $(dir)_EXE_FILENAMES
 #
 # Example for dir=SUBDIR
 #
-# make SUBDIR will compile and link all executables
-# make clean_exes_SUBDIR will remove all generated files
-# make install_SUBDIR will install all executables
+# make SUBDIR              -> will compile and link all executables
+# make clean_exes_SUBDIR   -> will remove all generated files
+# make install_exes_SUBDIR      -> will install all executables
+# make $(dir)/file         -> will compile and link single file
+#                             (there is no need to use the $(DEST) prefix)
 
 $(dir)_SOURCES:= $(addprefix $(dir)/, $($(dir)_SOURCES))
 $(dir)_EXES:= \
 	$(patsubst %.cxx, $(DEST)%, $(filter %.cxx, $($(dir)_SOURCES))) \
 	$(patsubst %.c, $(DEST)%, $(filter %.c, $($(dir)_SOURCES)))
+$(dir)_EXES_without_DEST:= \
+	$(patsubst %.cxx, %, $(filter %.cxx, $($(dir)_SOURCES))) \
+	$(patsubst %.c, %, $(filter %.c, $($(dir)_SOURCES)))
 
 .PHONY: $(dir) clean_exes_$(dir) install_exes_$(dir) uninstall_exes_$(dir)
 
@@ -35,6 +43,20 @@ $(dir)_EXES:= \
 
 $(dir):  $($(dir)_EXES)
 
+# trick (from the GNU make manual) to a target for every file which just
+# depends on $(DEST)/file. The advantage for the user is that she doesn't
+# have to type $(DEST) explictly anymore
+
+define PROGRAM_template
+$(1): $(DEST)$(1)
+
+.PHONY: $(1) 
+endef
+
+$(foreach prog,$($(dir)_EXES_without_DEST),$(eval $(call PROGRAM_template,$(prog))))
+
+
+# set up variable for clean_exes
 $(dir)_EXE_FILENAMES := $(addsuffix $(EXE_SUFFIX), $($(dir)_EXES))
 
 
