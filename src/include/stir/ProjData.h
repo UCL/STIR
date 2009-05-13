3d1c23f391705b5acfd966292fec7dfcd22b3f97
Kris Thielemans <Kris.Thielemans@stir>
2009-05-13 15:45:41 +0000

    updated doxygen header and added copyright

--- src/include/stir/ProjData.h
+++ src/include/stir/ProjData.h
@@ -1,10 +1,27 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2000 PARAPET partners
+    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup projdata
-  \brief Declaration of class ProjData
+  \brief Declaration of class stir::ProjData
 
   \author Sanida Mustafovic
   \author Kris Thielemans
@@ -12,11 +29,6 @@
 
   $Date$
   $Revision$
-*/
-/*
-    Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, IRSL
-    See STIR/LICENSE.txt for details
 */
 #ifndef __ProjData_H__
 #define __ProjData_H__
