396e6af1d91f255b2b8d447692d1c71d11bdb2b1
Kris Thielemans <Kris.Thielemans@stir>
2009-10-06 09:59:25 +0000

    moved to global distro

---
+++ src/test/modelling/test_ParametricDiscretisedDensity.cxx
@@ -0,0 +1,246 @@
+/*  $Id$
+    Copyright (C) 2006- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+  
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+    
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+  
+    See STIR/LICENSE.txt for details
+*/
+/*! 
+ 
+\file
+\ingroup test
+ 
+\brief testing stir::ParametricDiscretisedDensity class
+ 
+\author T. Borgeaud
+\author Charalampos Tsoumpas 
+ 
+$Date$
+ 
+$Revision$ 
+ 
+*/
+
+#include <iostream>
+#include "stir/RunTests.h"
+#include "stir/IndexRange2D.h"
+#include "stir/ProjDataInfoCylindricalNoArcCorr.h"
+#include "stir/stream.h"
+#include "stir/Succeeded.h"
+#include <fstream>
+#include "stir/VoxelsOnCartesianGrid.h"
+#include "stir/DiscretisedDensity.h"
+#include "stir/shared_ptr.h"
+#include "stir/TimeFrameDefinitions.h"
+#include "stir/Scanner.h"
+#include <utility>
+#include <vector>
+#include <string>
+
+#include <algorithm>
+#include <iomanip>
+//-----------
+#include "local/stir/modelling/ParametricDiscretisedDensity.h"
+#include "stir/is_null_ptr.h"
+#include "stir/IO/OutputFileFormat.h"
+
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+using std::ifstream;
+using std::istream;
+using std::setw;
+#endif
+
+START_NAMESPACE_STIR
+
+class ParametricDiscretisedDensityTests : public RunTests
+{
+public:
+  ParametricDiscretisedDensityTests() 
+  {}
+  void run_tests();
+  //private:
+};
+
+void ParametricDiscretisedDensityTests::run_tests()
+{
+  set_tolerance(0.000000000000001);
+
+  const unsigned num_params = ParametricVoxelsOnCartesianGrid::get_num_params();
+
+  //! Setup the scanner details first
+  const Scanner::Type test_scanner=Scanner::E966;
+  const shared_ptr<Scanner> scanner_sptr = new Scanner(test_scanner);
+  VectorWithOffset<int> num_axial_pos_per_segment;  num_axial_pos_per_segment.resize(0,0);  num_axial_pos_per_segment[0]=48;
+  VectorWithOffset<int> min_ring_diff;  min_ring_diff.resize(0,0);  min_ring_diff[0]=0;
+  VectorWithOffset<int> max_ring_diff; max_ring_diff.resize(0,0);  max_ring_diff[0]=0;
+  const int num_views=144; const int num_tangential_poss=144; 
+  ProjDataInfoCylindricalNoArcCorr proj_data_info(scanner_sptr,num_axial_pos_per_segment,min_ring_diff,max_ring_diff,num_views,num_tangential_poss);
+  //! Setup some of the image details
+  const CartesianCoordinate3D< float > origin (0.F,0.F,0.F);
+  const CartesianCoordinate3D<float> grid_spacing (1.F,1.F,1.F);
+  const float zoom=1.F;
+  {
+    cerr << "Testing ParametricDiscretisedDensity class for one voxel..." << endl;
+    const CartesianCoordinate3D<int> sizes (1,1,1);
+    
+    const shared_ptr<ParametricVoxelsOnCartesianGrid> parametric_image_sptr = new 
+      ParametricVoxelsOnCartesianGrid(
+				      ParametricVoxelsOnCartesianGridBaseType(proj_data_info,
+									      zoom,grid_spacing,sizes));
+    ParametricVoxelsOnCartesianGrid & parametric_image = *parametric_image_sptr;
+    parametric_image[0][0][0][1]=1.F;  parametric_image[0][0][0][2]=2.F;
+
+    check_if_equal(parametric_image[0][0][0][1],1.F,"check ParametricVoxelsOnCartesianGrid class implementation");
+    check_if_equal(parametric_image[0][0][0][2],2.F,"check ParametricVoxelsOnCartesianGrid class implementation");
+  }
+
+  {
+    //  Test of two frame images, read voxel  
+    cerr << "Writing ParametricDiscretisedDensity class for more voxels: 63x128x128..." << endl;
+
+    const CartesianCoordinate3D<int> sizes (63,128,128);
+  
+    //const shared_ptr<ParametricVoxelsOnCartesianGrid> parametric_image_sptr = 
+    // new ParametricVoxelsOnCartesianGrid(ParametricVoxelsOnCartesianGridBaseType(proj_data_info,zoom,grid_spacing,sizes));
+    //ParametricVoxelsOnCartesianGrid & parametric_image = *parametric_image_sptr;
+    ParametricVoxelsOnCartesianGrid parametric_image(ParametricVoxelsOnCartesianGridBaseType(proj_data_info,zoom,grid_spacing,sizes));
+    for(int k=0;k<63;++k)
+      for(int j=-64;j<63;++j)  
+	for(int i=-64;i<63;++i)
+	  for(unsigned int par_num=1; par_num<=num_params; ++par_num)
+	    parametric_image[k][j][i][par_num] = static_cast<float> (par_num*(i*1.F+j*5.F-k*10.F));
+
+    cerr << "- Checking the [] operator. " << endl;
+    for(int k=0;k<63;++k)
+      for(int j=-64;j<63;++j)  
+	for(int i=-64;i<63;++i)
+	    for(unsigned int par_num=1; par_num<=num_params; ++par_num)
+	      check_if_equal(parametric_image[k][j][i][par_num],static_cast<float> (par_num*(i*1.F+j*5.F-k*10.F)),
+			     "Please, check the [] operator implementation");   
+
+
+    cerr << "- Checking construct_single_density(param_num) implementation." << endl;
+    // TODO tests need to be rewritten to accomodate for other num_params!=2
+    if (num_params!=2)
+      {
+	warning("test_ParametricDiscretisedDensity test only tests first 2 maps");
+      }
+    ParametricVoxelsOnCartesianGrid::SingleDiscretisedDensityType single_density_1 = parametric_image.construct_single_density(1);
+    ParametricVoxelsOnCartesianGrid::SingleDiscretisedDensityType single_density_2 = parametric_image.construct_single_density(2);
+
+    for(int k=0;k<63;++k)
+      for(int j=-64;j<63;++j)  
+	for(int i=-64;i<63;++i)
+	  {
+	    check_if_equal(parametric_image[k][j][i][1],single_density_1[k][j][i],
+			   "Please, check construct_single_density(param_num) implementation");   
+	    check_if_equal(parametric_image[k][j][i][2],single_density_2[k][j][i],
+			   "Please, check construct_single_density(param_num) implementation");   
+	    for(unsigned int par_num=1; par_num<=num_params; ++par_num)
+	      check_if_equal(parametric_image[k][j][i][par_num],static_cast<float> (par_num*(i*1.F+j*5.F-k*10.F)),
+			     "Please, check the construct_single_density(param_num) implementation");   
+	  }
+
+    ParametricVoxelsOnCartesianGrid::SingleDiscretisedDensityType::full_iterator cur_iter_1 =
+      single_density_1.begin_all();
+    ParametricVoxelsOnCartesianGrid::SingleDiscretisedDensityType::full_iterator cur_iter_2 =
+      single_density_2.begin_all();
+    for (; cur_iter_1!=single_density_1.end_all() &&
+	   cur_iter_2!=single_density_2.end_all();	  
+	 ++cur_iter_1, ++cur_iter_2)			  
+      check_if_equal(*cur_iter_1*2.F, *cur_iter_2,
+		     "Please, check construct_single_density(param_num) implementation");   
+
+    std::fill(single_density_1.begin_all(), single_density_1.end_all(), 1.F);
+    std::fill(single_density_2.begin_all(), single_density_2.end_all(), 2.F);
+    VectorWithOffset<shared_ptr<ParametricVoxelsOnCartesianGrid::SingleDiscretisedDensityType> > v_test;
+    v_test.resize(1,2); v_test[1]=single_density_1.clone(); v_test[2]=single_density_2.clone();
+#if 0
+    // test disabled as function currently removed from class
+    cerr << "- Checking constructor from(VectorWithOffset single_densities) implementation." << endl;
+    {
+    ParametricVoxelsOnCartesianGrid test_create_par(v_test);
+
+    // test origin and index-range
+    check_if_equal(v_test[1]->get_origin(), test_create_par.get_origin(),
+		   "test if origin is equal after create_parametric_image");
+    check_if_equal(v_test[1]->get_index_range(), test_create_par.get_index_range(),
+		   "test if index_range is equal after create_parametric_image");
+    // test grid-spacing (only appropriate for VoxelsOnCartesianGrid)
+    check_if_equal(v_test[1]->get_grid_spacing(), test_create_par.get_grid_spacing(),
+		   "test if grid-spacing is equal after create_parametric_image");
+
+    // test values
+    { 
+      bool still_equal = true;
+      for(int k=0;still_equal && k<63;++k)
+	for(int j=-64;still_equal && j<63;++j)  
+	  for(int i=-64;still_equal && i<63;++i)
+	    for(unsigned int par_num=1;par_num<=num_params;++par_num)
+	    {
+	      still_equal =
+		check_if_equal(test_create_par[k][j][i][par_num],static_cast<float> (par_num),
+			     "Please, check create_parametric_image(VectorWithOffset single_denisties) implementation");   
+	      // next test doesn't have anything to do with test_create_par
+	      //check_if_equal(parametric_image[k][j][i][par_num],static_cast<double> (par_num*(i*1.F+j*5.F-k*10.F)),
+	      //	     "Please, check the create_parametric_image(VectorWithOffset single_densities)");   
+	    }
+    }
+    }
+#endif
+#if 0
+    // test disabled as function currently removed from class
+    {
+      cerr << "- Checking update_parametric_image(VectorWithOffset) implementation." << endl;
+      parametric_image.update_parametric_image(v_test);
+      for(int k=0;k<63;++k)
+	for(int j=-64;j<63;++j)  
+	  for(int i=-64;i<63;++i)
+	    for(unsigned int par_num=1;par_num<=num_params;++par_num)
+	      check_if_equal(parametric_image[k][j][i][par_num],static_cast<float> (par_num),
+			     "Please, check update_parametric_image(VectorWithOffset single_densities) implementation");   
+    }
+#endif
+    cerr << "- Checking update_parametric_image(single_density,param_num) implementation." << endl;
+    parametric_image.update_parametric_image(single_density_1,2);
+#if 1
+    parametric_image.update_parametric_image(single_density_2,1);
+
+    // Check if the result has been reversed.
+    {
+      bool still_equal = true;
+      for(int k=0;still_equal && k<63;++k)
+	for(int j=-64;still_equal && j<63;++j)  
+	  for(int i=-64;still_equal && i<63;++i)
+	    for(unsigned int par_num=1;still_equal && par_num<=num_params;++par_num)
+	      still_equal = check_if_equal(parametric_image[k][j][i][par_num],static_cast<float> (2+1-par_num),
+					  "Please, check update_parametric_image(VectorWithOffset) implementation");   
+    }
+#endif
+  }
+}
+
+END_NAMESPACE_STIR
+USING_NAMESPACE_STIR
+
+int main(int argc, char **argv)
+{
+  if (argc != 1)
+    {
+      cerr << "Usage : " << argv[0] << " \n";
+      return EXIT_FAILURE;
+    }
+  ParametricDiscretisedDensityTests tests;
+  tests.run_tests();
+  return tests.main_return_value();
+}
---
+++ src/test/test_DynamicDiscretisedDensity.cxx
@@ -0,0 +1,258 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*!
+  \file
+  \ingroup test
+  \brief tests the stir::DynamicDiscretisedDensity class
+
+  \author Charalampos Tsoumpas
+  \author Kris Thielemans
+  $Date$
+  $Revision$
+*/
+
+
+#include <iostream>
+
+#include "stir/RunTests.h"
+#include "stir/IndexRange2D.h"
+#include "stir/stream.h"
+#include "stir/Succeeded.h"
+#include <fstream>
+#include "local/stir/DynamicDiscretisedDensity.h"
+#include "stir/VoxelsOnCartesianGrid.h"
+#include "stir/DiscretisedDensity.h"
+#include "stir/shared_ptr.h"
+#include "stir/TimeFrameDefinitions.h"
+#include "stir/Scanner.h"
+#include <utility>
+#include <vector>
+#include <string>
+
+#include <algorithm>
+#include <iomanip>
+
+#ifndef STIR_NO_NAMESPACES
+using std::cerr;
+using std::ifstream;
+using std::istream;
+using std::setw;
+#endif
+
+START_NAMESPACE_STIR
+ 
+  class DynamicDiscretisedDensityTests : public RunTests
+  {
+  public:
+    DynamicDiscretisedDensityTests() 
+    {}
+    void run_tests();
+    //private:
+  };
+
+ void DynamicDiscretisedDensityTests::run_tests()
+{
+  {
+    // Simple Test of one voxel  
+  cerr << "Testing DynamicDiscretisedDensity class for one voxel..." << endl;
+
+  set_tolerance(0.000000000000001);
+  const CartesianCoordinate3D< float > origin (0.F,0.F,0.F);
+  BasicCoordinate<3, float > grid_spacing ;
+  grid_spacing[1] = 1.F;
+  grid_spacing[2] = 1.F;
+  grid_spacing[3] = 1.F;
+  BasicCoordinate<3,int> sizes ;
+  sizes[1]=1;
+  sizes[2]=1;
+  sizes[3]=1;
+  IndexRange<3> range(sizes);
+  
+  const shared_ptr<DiscretisedDensity<3,float>  > frame1_sptr = 
+    new VoxelsOnCartesianGrid<float> (range, origin,  grid_spacing) ;  
+  (*frame1_sptr)[0][0][0] = 1.F;
+
+  std::vector< std::pair< double, double > > time_frame_definitions_vector(1) ;
+  std::pair< double, double > time_frame_pair(1.,1.) ;
+  time_frame_definitions_vector[0]=time_frame_pair;
+  const TimeFrameDefinitions time_frame_definitions(time_frame_definitions_vector);
+  Scanner::Type test_scanner=Scanner::E966;
+  shared_ptr<Scanner> scanner_sptr = new Scanner(test_scanner);
+  DynamicDiscretisedDensity dynamic_image(time_frame_definitions,scanner_sptr); 
+  dynamic_image.set_density_sptr(frame1_sptr, 1);
+  check_if_equal(dynamic_image[1][0][0][0],1.F,"check DynamicDiscretisedDensity class implementation");
+  }
+  {
+  //  Test of two frame images, read voxel  
+  cerr << "Writing DynamicDiscretisedDensity class for two frames 63x128x128..." << endl;
+
+  set_tolerance(0.001);
+  const CartesianCoordinate3D< float > origin (0.F,0.F,0.F);
+  BasicCoordinate<3, float > grid_spacing ;
+  grid_spacing[1] = 2.425F;
+  grid_spacing[2] = 2.0594F;
+  grid_spacing[3] = 2.0594F;
+  BasicCoordinate<3,int> min_size, max_size;
+  min_size[1]=0;  min_size[2]=-64;  min_size[3]=-64;
+  max_size[1]=63;  max_size[2]=63;  max_size[3]=63;
+  
+  IndexRange<3> range(min_size,max_size);
+  const shared_ptr<DiscretisedDensity<3,float>  > frame1_2_sptr = 
+    new VoxelsOnCartesianGrid<float> (range, origin,  grid_spacing) ;  
+  const shared_ptr<DiscretisedDensity<3,float>  > frame2_2_sptr = 
+    new VoxelsOnCartesianGrid<float> (range, origin,  grid_spacing) ;  
+
+      for(int k=min_size[3];k<min_size[3];++k)
+	for(int j=max_size[2];j<max_size[2];++j)  
+	  for(int i=min_size[1];i<min_size[1];++i)
+	    {
+	      (*frame1_2_sptr)[k][j][i] = 1*(i+j*5.F-k*10.F) ;
+	      (*frame2_2_sptr)[k][j][i] = 2*(i+j*5.F-k*10.F) ;
+	    }
+
+  std::vector< std::pair< double, double > > time_frame_definitions_vector(2) ;
+  std::pair< double, double > first_time_frame_pair(1.,3.) ;
+  std::pair< double, double > second_time_frame_pair(3.,6.) ;
+
+  time_frame_definitions_vector[0]=first_time_frame_pair;
+  time_frame_definitions_vector[1]=second_time_frame_pair;
+
+  const TimeFrameDefinitions time_frame_definitions(time_frame_definitions_vector);
+  Scanner::Type test_scanner=Scanner::E966;
+  shared_ptr<Scanner> scanner_sptr = new Scanner(test_scanner);
+
+  DynamicDiscretisedDensity empty_dynamic_image(time_frame_definitions,scanner_sptr,frame1_2_sptr); 
+
+  DynamicDiscretisedDensity dynamic_image(time_frame_definitions,scanner_sptr); 
+  dynamic_image.set_density_sptr(frame1_2_sptr, 1);
+  dynamic_image.set_density_sptr(frame2_2_sptr, 2);
+
+  string string_test("STIRtmp_dyn2f.img");
+  string string_empty_test("STIRtmp_dyn2f_empty.img");
+
+#ifdef HAVE_LLN_MATRIX
+  check(dynamic_image.write_to_ecat7(string_test)==Succeeded::yes,"check DynamicDiscretisedDensity::write_to_ecat7 implementation");
+#endif
+  check_if_equal((empty_dynamic_image.get_density(1)).find_min(),0.F,"check DynamicDiscretisedDensity constructor implementation");  	     
+  check_if_equal((empty_dynamic_image.get_density(1)).find_max(),0.F,"check DynamicDiscretisedDensity constructor implementation");  	     
+  check_if_equal((empty_dynamic_image.get_density(2)).find_min(),0.F,"check DynamicDiscretisedDensity constructor implementation");  	     
+  check_if_equal((empty_dynamic_image.get_density(2)).find_max(),0.F,"check DynamicDiscretisedDensity constructor implementation");  	     
+}
+  {
+  //  Test of three frame images, read voxel  
+  cerr << "Testing DynamicDiscretisedDensity class for three frames..." << endl;
+
+  set_tolerance(0.001);
+  const CartesianCoordinate3D< float > origin (0.F,0.F,0.F);
+  BasicCoordinate<3, float > grid_spacing ;
+  grid_spacing[1] = 2.425F;
+  grid_spacing[2] = 2.0594F;
+  grid_spacing[3] = 2.0594F;
+  BasicCoordinate<3,int> min_size, max_size;
+  min_size[1]=0;  min_size[2]=-64;  min_size[3]=-64;
+  max_size[1]=63;  max_size[2]=63;  max_size[3]=63;  
+  IndexRange<3> range(min_size,max_size);
+  const shared_ptr<DiscretisedDensity<3,float>  > frame1_3_sptr = 
+    new VoxelsOnCartesianGrid<float> (range, origin,  grid_spacing) ;  
+  const shared_ptr<DiscretisedDensity<3,float>  > frame2_3_sptr = 
+    new VoxelsOnCartesianGrid<float> (range, origin,  grid_spacing) ;  
+  const shared_ptr<DiscretisedDensity<3,float>  > frame3_3_sptr = 
+    new VoxelsOnCartesianGrid<float> (range, origin,  grid_spacing) ;  
+
+      for(int k=min_size[3];k<min_size[3];++k)
+	for(int j=max_size[2];j<max_size[2];++j)  
+	  for(int i=min_size[1];i<min_size[1];++i)
+	    {
+	      (*frame1_3_sptr)[k][j][i] = 1*(i+j*5.F-k*10.F) ;
+	      (*frame2_3_sptr)[k][j][i] = 2*(i+j*5.F-k*10.F) ;
+	      (*frame3_3_sptr)[k][j][i] = 3*(i+j*5.F-k*10.F) ;
+	    }
+
+  std::vector< std::pair< double, double > > time_frame_definitions_vector(3) ;
+  std::pair< double, double > first_time_frame_pair(1.,3.) ;
+  std::pair< double, double > second_time_frame_pair(3.,6.) ;
+  std::pair< double, double > third_time_frame_pair(6.5,7.) ;
+
+  time_frame_definitions_vector[0]=first_time_frame_pair;
+  time_frame_definitions_vector[1]=second_time_frame_pair;
+  time_frame_definitions_vector[2]=third_time_frame_pair;
+
+  const TimeFrameDefinitions time_frame_definitions(time_frame_definitions_vector);
+  Scanner::Type test_scanner=Scanner::E966;
+  shared_ptr<Scanner> scanner_sptr = new Scanner(test_scanner);
+  DynamicDiscretisedDensity dynamic_image(time_frame_definitions,scanner_sptr); 
+  dynamic_image.set_density_sptr(frame1_3_sptr, 1);
+  dynamic_image.set_density_sptr(frame2_3_sptr, 2);
+  dynamic_image.set_density_sptr(frame3_3_sptr, 3);
+#ifndef HAVE_LLN_MATRIX
+  warning("write_to_ecat7 not tested as LLN library not present");
+#else
+  string string_test("STIRtmp_dyn3f.img");//TODO: Use the path info!!!
+  //  string string_test2("./local/samples/dyn_image_write_to_ecat7_test2.img");
+  //  dynamic_image.write_to_ecat7(string_test);
+  check(dynamic_image.write_to_ecat7(string_test)==Succeeded::yes,"check DynamicDiscretisedDensity::write_to_ecat7 implementation");
+  shared_ptr< DynamicDiscretisedDensity >  dyn_image_read_test_sptr =  
+    DynamicDiscretisedDensity::read_from_file(string_test);
+  const DynamicDiscretisedDensity & dyn_image_read_test = *dyn_image_read_test_sptr;
+  //  dyn_image_read_test.write_to_ecat7(string_test2);
+
+  for(int k=min_size[3];k<min_size[3];++k)
+    for(int j=max_size[2];j<max_size[2];++j)  
+      for(int i=min_size[1];i<min_size[1];++i)
+	{
+	  check_if_equal(dynamic_image[1][k][j][i],(*frame1_3_sptr)[k][j][i],"check DynamicDiscretisedDensity class implementation");  	     
+	  check_if_equal(dynamic_image[2][k][j][i],(*frame2_3_sptr)[k][j][i],"check DynamicDiscretisedDensity class implementation");
+	  check_if_equal(dynamic_image[3][k][j][i],(*frame3_3_sptr)[k][j][i],"check DynamicDiscretisedDensity class implementation");
+	  check_if_equal(dyn_image_read_test[1][k][j-64][i-64],(*frame1_3_sptr)[k][j][i],"check DynamicDiscretisedDensity::read_from_file implementation"); // The written image is read in respect to its center as origin!!!
+	  check_if_equal(dyn_image_read_test[2][k][j-64][i-64],(*frame2_3_sptr)[k][j][i],"check DynamicDiscretisedDensity::read_from_file implementation");
+	  check_if_equal(dyn_image_read_test[3][k][j-64][i-64],(*frame3_3_sptr)[k][j][i],"check DynamicDiscretisedDensity::read_from_file implementation");	
+	}
+    check_if_equal((dynamic_image.get_time_frame_definitions()).get_end_time(1),3.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dynamic_image.get_time_frame_definitions()).get_start_time(1),1.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dynamic_image.get_time_frame_definitions()).get_end_time(2),6.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dynamic_image.get_time_frame_definitions()).get_start_time(2),3.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dynamic_image.get_time_frame_definitions()).get_end_time(3),7.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dynamic_image.get_time_frame_definitions()).get_start_time(3),6.5,"check DynamicDiscretisedDensity class implementation");
+    /* To be tested when write_time_frame_definitions() will be implemented.
+    check_if_equal((dyn_image_read_test.get_time_frame_definitions()).get_end_time(1),3.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dyn_image_read_test.get_time_frame_definitions()).get_start_time(1),1.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dyn_image_read_test.get_time_frame_definitions()).get_end_time(2),6.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dyn_image_read_test.get_time_frame_definitions()).get_start_time(2),3.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dyn_image_read_test.get_time_frame_definitions()).get_end_time(3),7.,"check DynamicDiscretisedDensity class implementation");
+    check_if_equal((dyn_image_read_test.get_time_frame_definitions()).get_start_time(3),6.5,"check DynamicDiscretisedDensity class implementation");
+    */
+#endif
+   }  
+}
+
+END_NAMESPACE_STIR
+USING_NAMESPACE_STIR
+
+int main(int argc, char **argv)
+{
+  if (argc != 1)
+  {
+    cerr << "Usage : " << argv[0] << " \n";
+    return EXIT_FAILURE;
+  }
+  DynamicDiscretisedDensityTests tests;
+  tests.run_tests();
+  return tests.main_return_value();
+}
