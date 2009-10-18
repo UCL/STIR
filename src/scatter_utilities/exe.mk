2f5284f806758a9126c11a841fdd5f509e6bf3eb
Kris Thielemans <Kris.Thielemans@stir>
2009-10-18 10:30:41 +0000

    added create_tail_mask_from_ACFs

--- src/scatter_utilities/exe.mk
+++ src/scatter_utilities/exe.mk
@@ -5,7 +5,8 @@ dir := local/scatter
 
 $(dir)_SOURCES := \
 	estimate_scatter.cxx \
-	scale_single_scatter.cxx
+	scale_single_scatter.cxx \
+	create_tail_mask_from_ACFs.cxx
 
 #	estimate_single_scatter_splitted.cxx \
 
