1aa5d590275722c22be804196c8883d2d6b57608
Kris Thielemans <Kris.Thielemans@stir>
2009-10-21 19:00:23 +0000

    moved integral_over_activity_image_between_scattpoint_det into the same file is the other integration functions

--- src/scatter_buildblock/lib.mk
+++ src/scatter_buildblock/lib.mk
@@ -7,7 +7,6 @@ dir := local/scatter_buildblock
 $(dir)_LIB_SOURCES:= \
 	sample_scatter_points.cxx \
 	integral_scattpoint_det.cxx \
-	integral_over_activity_image_between_scattpoint_det.cxx \
 	integral_over_activity_image_between_scattpoints.cxx \
         cached_factors.cxx \
 	cached_factors_2.cxx \
