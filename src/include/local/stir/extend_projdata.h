c58f205f2acdebd7cfb2b6d3368d5eff0fc7821f
Kris Thielemans <Kris.Thielemans@stir>
2009-10-21 18:04:02 +0000

    - added "_in_views" to the function names - added checks on segment 0 - added doxygen - standardised code formatting

--- src/include/local/stir/extend_projdata.h
+++ src/include/local/stir/extend_projdata.h
@@ -19,7 +19,7 @@
 */
 /*
   \ingroup projdata
-  \file  
+  \file  Functions that extend a direct sinogram or segment in the view direction
   
   \author Charalampos Tsoumpas
   \author Kris Thielemans
@@ -32,19 +32,22 @@ START_NAMESPACE_STIR
 //@{
 /*!						  
 \ingroup projdata
-\brief Extension of projection data in view direction.
-	Functions that extend the given sinogram or segment in the direction of views taking 
-	periodicity into account, if exists. If the sinogram is not symmetric the values 
-	are extrapolated by nearest neighbour known values.
+\brief Extension of direct projection data in view direction.
+
+Functions that extend the given sinogram or segment in the view direction taking 
+periodicity into account, if exists. If the sinogram is not symmetric in
+tangential position, the values are extrapolated by nearest neighbour known values.
+
+This is probably only useful before calling interpolation routines, or for FORE.
 */
 inline
 Array<3,float>
-extend_segment(const SegmentBySinogram<float>& sino, 
-						const int min_view_extension, const int max_view_extension);
+extend_segment_in_views(const SegmentBySinogram<float>& sino, 
+			const int min_view_extension, const int max_view_extension);
 inline
 Array<2,float>
-extend_sinogram(const Sinogram<float>& sino,
-						const int min_view_extension, const int max_view_extension);
+extend_sinogram_in_views(const Sinogram<float>& sino,
+			 const int min_view_extension, const int max_view_extension);
 //@}
 
 END_NAMESPACE_STIR
