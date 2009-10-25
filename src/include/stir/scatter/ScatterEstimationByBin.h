04c38494c035f4635d8b5cc40ae2d4a097a4b992
Kris Thielemans <Kris.Thielemans@stir>
2009-10-25 10:25:42 +0000

    moved to global distro

---
+++ src/include/stir/scatter/ScatterEstimationByBin.h
@@ -0,0 +1,239 @@
+//
+// $Id$
+//
+#ifndef __stir_ScatterEstimationByBin_H__
+#define __stir_ScatterEstimationByBin_H__
+
+/*
+    Copyright (C) 2004 - $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*!
+  \file
+  \ingroup scatter
+  \brief Definition of class stir::ScatterEstimationByBin.
+  
+  \author Charalampos Tsoumpas
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+
+#include "stir/shared_ptr.h"
+#include "stir/DiscretisedDensity.h"
+#include "stir/DiscretisedDensityOnCartesianGrid.h"
+#include "stir/ProjData.h"
+#include "stir/ParsingObject.h"
+#include "stir/numerics/BSplines.h"
+#include <vector>
+#include <cmath>
+#include "stir/CartesianCoordinate3D.h"
+START_NAMESPACE_STIR
+
+class Succeeded;
+class ProjDataInfoCylindricalNoArcCorr;
+class ViewSegmentNumbers;
+
+/*!
+  \ingroup scatter
+ \brief Estimate of the scatter probability for a number of scatter points.
+*/
+class ScatterEstimationByBin : public ParsingObject
+{
+ public:
+ static void
+   upsample_and_fit_scatter_estimate(ProjData& scaled_scatter_proj_data,
+				     const  ProjData& emission_proj_data,
+				     const ProjData& scatter_proj_data,
+				     const ProjData& weights_proj_data,
+				     const float min_scale_factor,
+				     const float max_scale_factor,
+				     const unsigned half_filter_width,
+				     BSpline::BSplineType spline_type,
+				     const bool remove_interleaving = true);
+
+
+  //! Default constructor (calls set_defaults())
+  ScatterEstimationByBin();
+  Succeeded process_data();
+
+  // TODO write_log can't be const because parameter_info isn't const
+  void
+    write_log(const double simulation_time, 
+	      const float total_scatter);
+
+ protected:
+  void set_defaults();
+  void initialise_keymap();
+  bool post_processing();
+  
+  float attenuation_threshold;
+
+  bool random;
+  bool use_cache;
+  //! reference used when specifying the energy
+  float reference_energy; 
+  float energy_resolution;
+  float lower_energy_threshold;
+  float upper_energy_threshold;
+
+  std::string activity_image_filename;
+  std::string density_image_filename;
+  std::string density_image_for_scatter_points_filename;
+  std::string template_proj_data_filename;
+  std::string output_proj_data_filename;
+
+  shared_ptr<DiscretisedDensity<3,float> > density_image_for_scatter_points_sptr;
+  shared_ptr<DiscretisedDensity<3,float> > density_image_sptr;
+  shared_ptr<DiscretisedDensity<3,float> > activity_image_sptr;
+  shared_ptr<ProjData> output_proj_data_sptr; 
+
+  virtual 
+    void
+    find_detectors(unsigned& det_num_A, unsigned& det_num_B, const Bin& bin) const; 
+
+  unsigned 
+    find_in_detection_points_vector(const CartesianCoordinate3D<float>& coord) const;
+  // private:
+  const ProjDataInfoCylindricalNoArcCorr * proj_data_info_ptr;
+  CartesianCoordinate3D<float>  shift_detector_coordinates_to_origin;
+
+  /*************** functions that do the work **********/
+
+  enum image_type{act_image_type, att_image_type};
+  struct ScatterPoint
+  { 
+    CartesianCoordinate3D<float> coord;
+    float mu_value;
+  };
+  
+  std::vector< ScatterPoint> scatt_points_vector;
+  float scatter_volume;
+  // next needs to be mutable because find_in_detection_points_vector is const
+  mutable std::vector<CartesianCoordinate3D<float> > detection_points_vector;
+  int total_detectors;
+
+  //! fill in scatt_points_vector and scatter_volume       
+  void 
+    sample_scatter_points();
+	
+
+  inline float 
+    detection_efficiency(const float energy) const;
+
+  double
+    detection_efficiency_no_scatter(const unsigned det_num_A, 
+				    const unsigned det_num_B) const;
+
+  //! computes scatter for one viewgram
+  /*! \return total scatter estimated for this viewgram */
+  virtual double
+    process_data_for_view_segment_num(const ViewSegmentNumbers& vs_num);
+
+  static
+    float 
+    integral_between_2_points(const DiscretisedDensity<3,float>& density,
+			      const CartesianCoordinate3D<float>& point1, 
+			      const CartesianCoordinate3D<float>& point2);
+
+  float 
+    exp_integral_over_attenuation_image_between_scattpoint_det (const CartesianCoordinate3D<float>& scatter_point, 
+								const CartesianCoordinate3D<float>& detector_coord);
+	
+
+
+  float 
+    integral_over_activity_image_between_scattpoint_det (const CartesianCoordinate3D<float>& scatter_point,
+							 const CartesianCoordinate3D<float>& detector_coord);
+  
+ 
+    
+  float 
+    cached_integral_over_activity_image_between_scattpoint_det(const unsigned scatter_point_num, 
+							       const unsigned det_num);
+  
+  float 
+    cached_exp_integral_over_attenuation_image_between_scattpoint_det(const unsigned scatter_point_num, 
+								      const unsigned det_num);
+
+  
+	
+  float 
+    single_scatter_estimate_for_one_scatter_point(const std::size_t scatter_point_num,
+					   const unsigned det_num_A, 
+					   const unsigned det_num_B);
+
+
+  void
+    single_scatter_estimate(double& scatter_ratio_singles,
+			    const unsigned det_num_A, 
+			    const unsigned det_num_B);
+
+      
+  virtual double
+    scatter_estimate(const unsigned det_num_A, 
+		     const unsigned det_num_B);
+  
+
+      
+ public:
+  static
+    inline float 
+    dif_cross_section(const float cos_theta, float energy);
+	
+	
+  static
+    inline float 
+    total_cross_section(float energy);
+	
+  static
+    inline float
+    energy_after_scatter(const float cos_theta, const float energy);
+
+  static
+    inline float
+    energy_after_scatter_511keV(const float cos_theta);
+
+  static
+    inline float 
+    total_cross_section_relative_to_511keV(const float energy);
+	
+  static
+    inline float 
+    max_cos_angle(const float low, const float approx, const float resolution_at_511keV);
+
+  static
+    inline float 
+    energy_lower_limit(const float low, const float approx, const float resolution_at_511keV);
+ protected:
+	  
+  inline float  
+    compute_emis_to_det_points_solid_angle_factor(const CartesianCoordinate3D<float>& emis_point,
+						  const CartesianCoordinate3D<float>& detector_coord) ;
+
+ private:
+  Array<2,float> cached_activity_integral_scattpoint_det;
+  Array<2,float> cached_attenuation_integral_scattpoint_det;
+  void initialise_cache_for_scattpoint_det();
+
+};
+
+
+END_NAMESPACE_STIR
+
+#include "local/stir/ScatterEstimationByBin.inl"
+
+#endif
---
+++ src/include/stir/scatter/ScatterEstimationByBin.inl
@@ -0,0 +1,135 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2004 - $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*!
+  \file
+  \ingroup scatter
+  \brief Inline implementations of class stir::ScatterEstimationByBin.
+  
+  \author Nikolaos Dikaios
+  \author Charalampos Tsoumpas
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+START_NAMESPACE_STIR
+
+float
+ScatterEstimationByBin::
+compute_emis_to_det_points_solid_angle_factor(
+					      const CartesianCoordinate3D<float>& emis_point,
+					      const CartesianCoordinate3D<float>& detector_coord)
+{
+  
+  const CartesianCoordinate3D<float> dist_vector = emis_point - detector_coord ;
+ 
+
+  const float dist_emis_det_squared = norm_squared(dist_vector);
+
+  const float emis_det_solid_angle_factor = 1.F/ dist_emis_det_squared ;
+
+  return emis_det_solid_angle_factor ;
+}
+
+float
+ScatterEstimationByBin::
+dif_cross_section(const float cos_theta, float energy)
+{ 
+  const double Re = 2.818E-13;   // aktina peristrofis electroniou gia to atomo tou H
+  const float sin_theta_2= 1-cos_theta*cos_theta ;
+  const float P= 1/(1+(energy/511.0)*(1-cos_theta)); 
+  return( (Re*Re/2) * P * (1 - P * sin_theta_2 + P * P));
+}
+
+float
+ScatterEstimationByBin::
+energy_after_scatter(const float cos_theta, const float energy)
+{
+  return energy/(1+(energy/511.0)*(1-cos_theta));   // For an arbitrary energy 
+}
+
+float
+ScatterEstimationByBin::
+energy_after_scatter_511keV(const float cos_theta)
+{
+  return 511/(2-cos_theta); // for a given energy, energy := 511 keV
+}
+
+float
+ScatterEstimationByBin::
+total_cross_section(const float energy)
+{
+  const float a= energy/511.0;
+  const float l= log(1.0+2.0*a); 
+    const double sigma0= 6.65E-25;   // sigma0=8*pi*a*a/(3*m*m)
+    return( 0.75*sigma0  * ( (1.0+a)/(a*a)*( 2.0*(1.0+a)/(1.0+2.0*a)- l/a ) + l/(2.0*a) - (1.0+3.0*a)/(1.0+2.0*a)/(1.0+2.0*a) ) ); // Klein - Nishina formula = sigma / sigma0
+}
+
+
+float
+ScatterEstimationByBin::
+total_cross_section_relative_to_511keV(const float energy)
+{
+  const float a= energy/511.0;
+  static const float prefactor = 9/(-40 + 27*log(3.)); //Klein-Nishina formula for a=1 & devided with 0.75 == (40 - 27*log(3)) / 9
+
+  return //check this in Mathematica
+    prefactor*
+    (((-4 - a*(16 + a*(18 + 2*a)))/square(1 + 2*a) +       
+      ((2 + (2 - a)*a)*log(1 + 2*a))/a)/square(a)
+     );
+}
+
+float
+ScatterEstimationByBin::
+detection_efficiency(const float energy) const
+{
+  // factor 2.35482 is used to convert FWHM to sigma
+  const float sigma_times_sqrt2= 
+    sqrt(2.*energy*this->reference_energy)*this->energy_resolution/2.35482;  // 2.35482=2 * sqrt( 2 * ( log(2) )
+  
+  // sigma_times_sqrt2= sqrt(2) * sigma   // resolution proportional to FWHM 	
+  
+	const float efficiency =
+	  0.5*( erf((this->upper_energy_threshold-energy)/sigma_times_sqrt2) 
+		- erf((this->lower_energy_threshold-energy)/sigma_times_sqrt2 ));	
+                /* Maximum efficiency is 1.*/
+	return efficiency;
+}
+
+float
+ScatterEstimationByBin::
+max_cos_angle(const float low, const float approx, const float resolution_at_511keV)
+{
+	return
+	2. - (8176.*log(2.))/(square(approx*resolution_at_511keV)*(511. + (16.*low*log(2.))/square(approx*resolution_at_511keV) - 
+	sqrt(511.)*sqrt(511. + (32.*low*log(2.))/square(approx*resolution_at_511keV)))) ;
+}
+
+
+float
+ScatterEstimationByBin::
+energy_lower_limit(const float low, const float approx, const float resolution_at_511keV)
+{
+  return
+  low + (approx*resolution_at_511keV)*(approx*resolution_at_511keV)*(46.0761 - 2.03829*sqrt(22.1807*low/square(approx*resolution_at_511keV)+511.));
+}
+
+END_NAMESPACE_STIR
