620bfa035f33af95069c64b2db8a1e0aadade786
Kris Thielemans <Kris.Thielemans@stir>
2009-10-25 16:23:37 +0000

    moved to global distro

---
+++ src/include/stir/extend_projdata.h
@@ -0,0 +1,57 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*
+  \ingroup projdata
+  \file  Functions that extend a direct sinogram or segment in the view direction
+  
+  \author Charalampos Tsoumpas
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+START_NAMESPACE_STIR
+
+//@{
+/*!						  
+\ingroup projdata
+\brief Extension of direct projection data in view direction.
+
+Functions that extend the given sinogram or segment in the view direction taking 
+periodicity into account, if exists. If the sinogram is not symmetric in
+tangential position, the values are extrapolated by nearest neighbour known values.
+
+This is probably only useful before calling interpolation routines, or for FORE.
+*/
+inline
+Array<3,float>
+extend_segment_in_views(const SegmentBySinogram<float>& sino, 
+			const int min_view_extension, const int max_view_extension);
+inline
+Array<2,float>
+extend_sinogram_in_views(const Sinogram<float>& sino,
+			 const int min_view_extension, const int max_view_extension);
+//@}
+
+END_NAMESPACE_STIR
+
+#include "local/stir/extend_projdata.inl"
+
+
---
+++ src/include/stir/interpolate_projdata.h
@@ -0,0 +1,80 @@
+//
+// $Id$
+//
+/*
+    Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
+/*
+  \ingroup projdata
+  \file Declaration of stir::interpolate_projdata
+  
+  \author Charalampos Tsoumpas
+  \author Kris Thielemans
+
+  $Date$
+  $Revision$
+*/
+
+#include "stir/numerics/BSplines.h"
+
+START_NAMESPACE_STIR
+
+class ProjData;
+template <int num_dimensions, class T> class BasicCoordinate;
+template <class elemT> class Sinogram;
+template <class elemT> class SegmentBySinogram;
+
+
+//! \brief Perform B-Splines Interpolation
+/*! 
+  \ingroup projdata
+  \param[out] proj_data_out Its projection_data_info is used to 
+  determine output characteristics. Data will be 'put' in here using 
+  ProjData::set_sinogram().
+  \param[in] proj_data_in input data 
+  \param[in] spline_type determines which type of BSpline will be used
+  \param[in] remove_interleaving 
+  The STIR implementation of interpolating 3D (for the moment) projdata is a generalisation that applies 
+  B-Splines Interpolation to projdata supposing that every dimension is a regular grid. For instance, for 
+  a 3D dataset, interpolating can produce a new expanded 3D dataset based on the given information 
+  (proj_data_out). This mostly is useful in the scatter sinogram expansion.
+
+  See STIR documentation about B-Spline interpolation or scatter correction.     
+
+  \todo This currently only works for direct sinograms (i.e. segment 0).
+  \warning Because of the boundary conditions in the B-spline interpolation,
+  strange results can occur if the output sinogram has a larger range than 
+  the input sinogram.
+*/  
+//@{
+Succeeded 
+interpolate_projdata(ProjData& proj_data_out,
+		     const ProjData& proj_data_in, 
+		     const BSpline::BSplineType spline_type,
+		     const bool remove_interleaving = false,
+		     const bool use_view_offset = false);
+Succeeded 
+interpolate_projdata(ProjData& proj_data_out,
+		     const ProjData& proj_data_in,
+		     const BasicCoordinate<3, BSpline::BSplineType> & spline_type,
+		     const bool remove_interleaving = false,
+		     const bool use_view_offset = false);
+//@}
+
+END_NAMESPACE_STIR
+
+
+
