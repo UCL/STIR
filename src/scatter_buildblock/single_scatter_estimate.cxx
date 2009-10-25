0df9e9bbc56ea7eb9da2fbf9e728b5b6da5c5567
Kris Thielemans <Kris.Thielemans@stir>
2009-10-25 17:21:35 +0000

    moved scatter and related utilities to the global distro, general code clean-up and some extra documentation

--- src/scatter_buildblock/single_scatter_estimate.cxx
+++ src/scatter_buildblock/single_scatter_estimate.cxx
@@ -1,10 +1,26 @@
 //
 // $Id$
 //
+/*
+  Copyright (C) 2004- $Date$, Hammersmith Imanet
+  This file is part of STIR.
+
+  This file is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This file is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details. 
+
+  See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup scatter
-  \brief Implementations of functions defined in Scatter.h
+  \brief Implementations of stir::ScatterEstimationByBin::scatter_estimate and stir::ScatterEstimationByBin::single_scatter_estimate
 
   \author Charalampos Tsoumpas
   \author Pablo Aguiar
@@ -12,56 +28,16 @@
 
   $Date$
   $Revision$
-
-    Copyright (C) 2004- $Date$, Hammersmith Imanet
-    See STIR/LICENSE.txt for details
 */
-#include "local/stir/ScatterEstimationByBin.h"
+
+#include "stir/scatter/ScatterEstimationByBin.h"
 using namespace std;
 START_NAMESPACE_STIR
-static const float total_cross_section_511keV = 
+static const float total_Compton_cross_section_511keV = 
 ScatterEstimationByBin::
-  total_cross_section(511.F); 
+  total_Compton_cross_section(511.F); 
 
 
-double
-ScatterEstimationByBin::
-detection_efficiency_no_scatter(const unsigned det_num_A, 
-				const unsigned det_num_B) const
-{
-  // TODO: slightly dangerous to use a static here
-  // it would give wrong results when the energy_thresholds are changed...
-  static const float detector_efficiency_no_scatter =
-    detection_efficiency(511.F) > 0 
-    ? detection_efficiency(511.F)
-    : (std::cerr << "Zero detection efficiency for 511. Will normalise to 1\n", 1.F);
-
-  const CartesianCoordinate3D<float>& detector_coord_A =
-    detection_points_vector[det_num_A];
-  const CartesianCoordinate3D<float>& detector_coord_B =
-    detection_points_vector[det_num_B];
-  const CartesianCoordinate3D<float> 
-    detA_to_ring_center(0,-detector_coord_A[2],-detector_coord_A[3]);
-  const CartesianCoordinate3D<float> 
-    detB_to_ring_center(0,-detector_coord_B[2],-detector_coord_B[3]);
-  const float rAB_squared=norm_squared(detector_coord_A-detector_coord_B);
-  const float cos_incident_angle_A = 
-    cos_angle(detector_coord_B - detector_coord_A,
-	      detA_to_ring_center) ;
-  const float cos_incident_angle_B = 
-    cos_angle(detector_coord_A - detector_coord_B,
-	      detB_to_ring_center) ;
-
-  //0.75 is due to the volume of the pyramid approximation!
-  return
-    1./(  0.75/2./_PI *
-    rAB_squared
-    /detector_efficiency_no_scatter/
-    (cos_incident_angle_A*
-     cos_incident_angle_B));
-}
-
-// for compatiblity with scatter_viewgram.cxx 
 double
 ScatterEstimationByBin::
 scatter_estimate(const unsigned det_num_A, 
@@ -103,10 +79,10 @@ single_scatter_estimate(double& scatter_ratio_singles,
   // there is a scatter_volume factor for every scatter point, as the sum over scatter points
   // is an approximation for the integral over the scatter point.
 
-  // the factors total_cross_section_511keV should probably be moved to the scatter_computation code
+  // the factors total_Compton_cross_section_511keV should probably be moved to the scatter_computation code
   const double common_factor =
     1/detection_efficiency_no_scatter(det_num_A, det_num_B) *
-    scatter_volume/total_cross_section_511keV;
+    scatter_volume/total_Compton_cross_section_511keV;
 
   scatter_ratio_singles *= common_factor;
 }
--- src/scatter_buildblock/single_scatter_integrals.cxx
+++ src/scatter_buildblock/single_scatter_integrals.cxx
@@ -1,10 +1,27 @@
 //
 // $Id$
 //
+/*
+  Copyright (C) 2004- $Date$, Hammersmith Imanet Ltd
+  This file is part of STIR.
+
+  This file is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This file is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details. 
+  
+  See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup scatter
-  \brief Implementations of integrating function in stir::ScatterEstimationByBin
+  \brief Implementations of integrating functions in stir::ScatterEstimationByBin
+
   Functions calculates the integral along LOR in an image (attenuation or emission). 
   (from scatter point to detector coordinate)
   
@@ -14,11 +31,8 @@
   
   $Date$
   $Revision$
-  
-  Copyright (C) 2004- $Date$, Hammersmith Imanet Ltd
-  See STIR/LICENSE.txt for details
-*/
-#include "local/stir/ScatterEstimationByBin.h"
+  */
+#include "stir/scatter/ScatterEstimationByBin.h"
 #include "stir/VoxelsOnCartesianGrid.h"
 #include "stir/recon_buildblock/ProjMatrixElemsForOneBin.h"
 #include "stir/recon_buildblock/RayTraceVoxelsOnCartesianGrid.h"
@@ -27,108 +41,108 @@ START_NAMESPACE_STIR
 float 
 ScatterEstimationByBin::
 exp_integral_over_attenuation_image_between_scattpoint_det (const CartesianCoordinate3D<float>& scatter_point, 
-			 const CartesianCoordinate3D<float>& detector_coord)
-{	
-#ifndef NEWSCALE		
+                         const CartesianCoordinate3D<float>& detector_coord)
+{       
+#ifndef NEWSCALE                
   /* projectors work in pixel units, so convert attenuation data 
      from cm^-1 to pixel_units^-1 */
-  const float	rescale = 
+  const float   rescale = 
     dynamic_cast<const DiscretisedDensityOnCartesianGrid<3,float> &>(*density_image_sptr).
     get_grid_spacing()[3]/10;
 #else
-  const float	rescale = 
+  const float   rescale = 
     0.1F;
 #endif
 
   return
     exp(-rescale*
-	integral_between_2_points(*density_image_sptr,
-				  scatter_point,
-				  detector_coord)
-	);
+        integral_between_2_points(*density_image_sptr,
+                                  scatter_point,
+                                  detector_coord)
+        );
 }
 
 
 float
 ScatterEstimationByBin::
 integral_over_activity_image_between_scattpoint_det (const CartesianCoordinate3D<float>& scatter_point, 
-						     const CartesianCoordinate3D<float>& detector_coord)
+                                                     const CartesianCoordinate3D<float>& detector_coord)
 {
   {
-  const CartesianCoordinate3D<float> dist_vector = scatter_point - detector_coord ;
+    const CartesianCoordinate3D<float> dist_vector = scatter_point - detector_coord ;
 
-  const float dist_sp1_det_squared = norm_squared(dist_vector);
+    const float dist_sp1_det_squared = norm_squared(dist_vector);
 
-  const float solid_angle_factor = 
-    std::min(static_cast<float>(_PI/2), 1.F  / dist_sp1_det_squared) ;
+    const float solid_angle_factor = 
+      std::min(static_cast<float>(_PI/2), 1.F  / dist_sp1_det_squared) ;
  
-  return
-    solid_angle_factor *
-    integral_between_2_points(*activity_image_sptr,
-			      scatter_point,
-			      detector_coord);
+    return
+      solid_angle_factor *
+      integral_between_2_points(*activity_image_sptr,
+                                scatter_point,
+                                detector_coord);
   }
 }
 
 float 
 ScatterEstimationByBin::
 integral_between_2_points(const DiscretisedDensity<3,float>& density,
-			  const CartesianCoordinate3D<float>& scatter_point, 
-			  const CartesianCoordinate3D<float>& detector_coord)
-{	
+                          const CartesianCoordinate3D<float>& scatter_point, 
+                          const CartesianCoordinate3D<float>& detector_coord)
+{       
 
 
   const VoxelsOnCartesianGrid<float>& image =
     dynamic_cast<const VoxelsOnCartesianGrid<float>& >
     (density);
   
-	const CartesianCoordinate3D<float> voxel_size = image.get_grid_spacing();
-	
-	CartesianCoordinate3D<float>  origin = 
-		image.get_origin();
-	const float z_to_middle =
-		(image.get_max_index() + image.get_min_index())*voxel_size.z()/2.F;
-	origin.z() -= z_to_middle;
-	/* TODO replace with image.get_index_coordinates_for_physical_coordinates */
-		ProjMatrixElemsForOneBin lor;
-		RayTraceVoxelsOnCartesianGrid(lor, 
-			(scatter_point-origin)/voxel_size,  // should be in voxel units
-			(detector_coord-origin)/voxel_size, // should be in voxel units
-			voxel_size, //should be in mm
+  const CartesianCoordinate3D<float> voxel_size = image.get_grid_spacing();
+        
+  CartesianCoordinate3D<float>  origin = 
+    image.get_origin();
+  const float z_to_middle =
+    (image.get_max_index() + image.get_min_index())*voxel_size.z()/2.F;
+  origin.z() -= z_to_middle;
+  /* TODO replace with image.get_index_coordinates_for_physical_coordinates */
+  ProjMatrixElemsForOneBin lor;
+  RayTraceVoxelsOnCartesianGrid(lor, 
+                                (scatter_point-origin)/voxel_size,  // should be in voxel units
+                                (detector_coord-origin)/voxel_size, // should be in voxel units
+                                voxel_size, //should be in mm
 #ifdef NEWSCALE
-				  1.F // normalise to mm
+                                1.F // normalise to mm
 #else
-				  1/voxel_size.x() // normalise to some kind of 'pixel units'
+                                1/voxel_size.x() // normalise to some kind of 'pixel units'
 #endif
-        );
-		lor.sort();
-		float sum = 0;  // add up values along LOR
-		{     
-			ProjMatrixElemsForOneBin::iterator element_ptr =lor.begin() ;
-			bool we_have_been_within_the_image = false;
-			while (element_ptr != lor.end())
-			{
-				const BasicCoordinate<3,int> coords = element_ptr->get_coords();				
-				if (coords[1] >= image.get_min_index() && 
-					coords[1] <= image.get_max_index() &&
-					coords[2] >= image[coords[1]].get_min_index() && 
-					coords[2] <= image[coords[1]].get_max_index() &&
-					coords[3] >= image[coords[1]][coords[2]].get_min_index() && 
-					coords[3] <= image[coords[1]][coords[2]].get_max_index())
-				{
-					we_have_been_within_the_image = true;
-					sum += image[coords] * element_ptr->get_value();				
-				}
-				else if (we_have_been_within_the_image)
-				{
-					// we jump out of the loop as we are now at the other side of 
-					// the image
-				  //					break; 
-				}
-				++element_ptr;		
-			}	      
-		}  		
-		return sum;	
-}						   
+                                );
+  lor.sort();
+  float sum = 0;  // add up values along LOR
+  {     
+    ProjMatrixElemsForOneBin::iterator element_ptr =lor.begin() ;
+    bool we_have_been_within_the_image = false;
+    while (element_ptr != lor.end())
+      {
+        const BasicCoordinate<3,int> coords = element_ptr->get_coords();                                
+        if (coords[1] >= image.get_min_index() && 
+            coords[1] <= image.get_max_index() &&
+            coords[2] >= image[coords[1]].get_min_index() && 
+            coords[2] <= image[coords[1]].get_max_index() &&
+            coords[3] >= image[coords[1]][coords[2]].get_min_index() && 
+            coords[3] <= image[coords[1]][coords[2]].get_max_index())
+          {
+            we_have_been_within_the_image = true;
+            sum += image[coords] * element_ptr->get_value();                            
+          }
+        else if (we_have_been_within_the_image)
+          {
+            // we jump out of the loop as we are now at the other side of 
+            // the image
+            //                                  break; 
+          }
+        ++element_ptr;          
+      }       
+  }             
+  return sum;   
+}                                                  
 END_NAMESPACE_STIR
 
