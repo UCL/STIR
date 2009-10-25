9033d28672ee78b3b8ed9f0903a930086c6112ae
Kris Thielemans <Kris.Thielemans@stir>
2009-10-25 09:19:39 +0000

    corrected usage message, changed defaults and expanded doxygen

--- src/local/scatter/create_tail_mask_from_ACFs.cxx
+++ src/local/scatter/create_tail_mask_from_ACFs.cxx
@@ -2,15 +2,36 @@
 // $Id$
 //
 /*
-Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
-See STIR/LICENSE.txt for details
+  Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+  This file is part of STIR.
+
+  This file is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation; either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This file is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details. 
+
+  See STIR/LICENSE.txt for details
 */
 /*!
-\file
-\ingroup utilities
-\brief   
+  \file
+  \ingroup utilities
+  \ingroup scatter
+  \brief Compute a mask for the "tails" in the sinogram
 
-  \author Charalampos Tsoumpas
+  This computes the outer part of the projection data that 
+  does not receive any (unscattered) contributions from inside the body.
+  Its normal input is a projection data with attenuation-correction factors
+  (ACFs). We take a threshold as parameter (which should be a bit larger 
+  than 1). Along each line in the sinogram, we search from the edges 
+  until we find the first bin above the threshold. We then go back a
+  few bins (as set by \a safety_margin). All bins from the edge until
+  this point are included in the mask.  
+
   \author Kris Thielemans
   
   $Date$
@@ -18,11 +39,7 @@ See STIR/LICENSE.txt for details
 	
   \par Usage:
   see .cxx file  
-\code
-  \endcode
-  						
-  Output: Viewgram with name scaled_scatter_filename
-              
+  						              
   \param ACF_threshold defaults to 1.01 (should be larger than 1)	  
 */
 
@@ -40,31 +57,15 @@ See STIR/LICENSE.txt for details
 
 /***********************************************************/     
 
-using boost::lambda::_1;
-
 static void
 print_usage_and_exit(const char * const prog_name)
 {
   std::cerr << "\nUsage:\n" << prog_name << "\n"
-	    << "\t[--min-scale-factor number]\n"
-	    << "\t[--max-scale-factor number]\n"
-	    << "\tattenuation_correction_factors\n"
-	    << "\temission_projdata\n"
-	    << "\tscatter_projdata\n" 
-	    << "\toutput_filename\n"
-	    << "\t[ACF_threshold \n"
-	    << "\t[correct_for_interleaving]\n"
-	    << "\t[estimate_scale_factor]\n"
-#ifdef SCFOLD
-	    << "\t[mask_radius_in_mm]]]\n"
-#else
-	    << "\t[back_off_size]]\n"
-#endif
-	    << "correct_for_interleaving default to 1\n"
-	    << "ACF_threshold defaults to 1.01\n" 
-	    << "estimate_scale_factor defaults to 1 for scaling per sinogram\n"
-	    << "0 for  no scaling, 1 for by sinogram\n"
-	    << "If mask_radius_in_mm is not specified, will use all available data\n";
+	    << "\t--ACF-filename <filename>\n"
+	    << "\t--output-filename <filename>\n"
+	    << "\t[--ACF_threshold <float>]\n"
+	    << "\t[--safety-margin <integer>]\n"
+	    << "ACF_threshold defaults to 1.1, safety-margin to 4\n";
   exit(EXIT_FAILURE);
 }
 
@@ -74,8 +75,8 @@ int main(int argc, const char *argv[])
   const char * const prog_name = argv[0];
 
   // option processing
-  float ACF_threshold = 1.01;
-  int safety_margin=0;
+  float ACF_threshold = 1.1;
+  int safety_margin=4;
   std::string ACF_filename;
   std::string output_filename;
   while (argc>1 && argv[1][1] == '-')
@@ -111,6 +112,9 @@ int main(int argc, const char *argv[])
     {
       print_usage_and_exit(prog_name);
     }      
+  if (ACF_threshold<=1)
+    error("ACF-threshold should be larger than 1");
+
   std::cout << "\nACF_threshold: " << ACF_threshold << " safety-margin : " << safety_margin << std::endl;
 
 
@@ -160,6 +164,9 @@ int main(int argc, const char *argv[])
 		    mask_sinogram[bin.view_num()][bin.tangential_pos_num()]=0;
 #else
 	      const Array<1,float>& att_line=att_sinogram[bin.view_num()];
+
+              using boost::lambda::_1;
+
 	      // find left and right mask sizes
 	      // sorry: a load of ugly casting to make sure we allow all datatypes
 	      std::size_t mask_left_size =
@@ -169,7 +176,7 @@ int main(int argc, const char *argv[])
 						  (std::find_if(att_line.begin(), att_line.end(),
 								_1>=ACF_threshold) -
 						   att_line.begin()) -
-						  static_cast<int>(safety_margin))
+						  safety_margin)
 					 );
 	      std::size_t mask_right_size =
 		static_cast<std::size_t>(
@@ -178,9 +185,11 @@ int main(int argc, const char *argv[])
 						  (std::find_if(att_line.rbegin(), att_line.rend() - mask_left_size,
 								_1>=ACF_threshold) -
 						   att_line.rbegin()) -
-						  static_cast<int>(safety_margin))
+						  safety_margin)
 					 );
+#if 0
 	      std::cout << "mask sizes " << mask_left_size << ", " << mask_right_size << '\n';
+#endif
 	      std::fill(mask_sinogram[bin.view_num()].begin(),
 			mask_sinogram[bin.view_num()].begin() + mask_left_size,
 			1.F);
