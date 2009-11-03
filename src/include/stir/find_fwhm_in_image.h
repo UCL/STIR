8e57d7f9aec07a3924eaed71e4d10a208a878f4e
Kris Thielemans <Kris.Thielemans@stir>
2009-11-03 04:14:25 +0000

    cleaned doxygen, renamed flexible_mask to assign_to_subregion (and changed calling sequence)

--- src/include/stir/find_fwhm_in_image.h
+++ src/include/stir/find_fwhm_in_image.h
@@ -1,6 +1,22 @@
 //
 // $Id$
 //
+/*
+    Copyright (C) 2004- $Date$, Hammersmith Imanet
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+*/
 /*!
   \file
   \ingroup resolution
@@ -13,10 +29,6 @@
   $Revision$
 
  */
-/*
-    Copyright (C) 2004- $Date$, Hammersmith Imanet
-    See STIR/LICENSE.txt for details
-*/
 #include "stir/shared_ptr.h"
 #include "stir/DiscretisedDensity.h"
 #include "stir/DiscretisedDensityOnCartesianGrid.h"
@@ -40,9 +52,9 @@ float find_level_width(const RandomAccessIterType& begin_iterator,
                        const float level_height)  ;  
 /*!
    \ingroup resolution
-   \brief 
-   takes as input the Begin, the End and the Max_Element Iterators of a sequence 
-   of numbers (e.g. vector) and the level of the maximum you want to have (e.g. maximum/2 or maximum/10). 
+   \brief find width at a level
+   takes as input the Begin and the End Iterators of a sequence 
+   of numbers (e.g. vector) and the level you want to have (e.g. maximum/2 or maximum/10). 
    It gives as output resolution in pixel size.   
 */
 template <class RandomAccessIterType>
@@ -63,9 +75,8 @@ maximum_location_per_slice(const Array<num_dimensions,elemT>& input_array,
 /*!
    \ingroup resolution
    \brief
-  extracts a column from the given array, that includes the particular voxel and 
-  returns a column in Array<1,elemT> type, at the wanted dimension (z=1, y=2, x=3).
-  {Only 3D implementation) 
+  extracts a line from the given array (that includes the particular index)
+  in the direction of the specified dimension.
 */ 
 template <int num_dimensions, class elemT>
 Array<1,elemT>
@@ -81,8 +92,6 @@ extract_line(const Array<num_dimensions,elemT> &,
   It finds the real maximum location, using the 3 points parabolic fit. Then, linear interpolation is used  
   to find the whole line at the given dimension (z:1,y:2,x:3) taking into account the voxels that
   intersects the interpolated voxel which has in its center the point with the real_maximum_value.
-  The do_direction is used so that in the direction of the line source will not happen any interpolation
-  (Only 3D implementation).
 */ 
 template <int num_dimensions, class elemT>
 Array<1,elemT>
@@ -104,21 +113,28 @@ struct ResolutionIndex
 };       
 /*!
    \ingroup resolution
-   \brief Finds FWHM in pixel size units for a number of point sources or a line source   
+   \brief Finds FWHM, FWTM etc (in mm) for a number of point sources or a line source   
   
-   is the basic function that the utility is based on. It takes the 3D input_image, the number of maxima, 
-   the level of the maximum (2 as half maximum, 10 as tenth maximum etc.), the dimension used for line 
-   sources and a boolean nema which enables the calculation based on the NEMA Standards Publication 
-   NU 2-2001. The implementation calculates and returns in a list container the maximum_value and its 
-   location per slice for line sources, sorted by minimum to maximum slice of the using dimension, or per 
-   whole image for point sources, sorted by maximum to minimum value. It calculates, as well, the resolution 
-   at the same location in the size of units that is used (usually mm). If the dimension is set to 0 it finds 
-   the resolution of each one point sources, else it finds the algorithm for the line sources giving its 
-   direction (z=1, y=2, x=3), respectively. If given as [num_maxima] less than the total slices it returns 
-   the results for some slices by sampling with the same step the total slices of the wanted dimension. 
-   (Only 3D implementation). In case it is not needed to be followed to the NEMA standard, for better 
-   approximation could be achieved by setting the nema to 0. 
-   (In this case the interpolated_line function is used). 
+   \param[in] input_image
+   \param[in] num_maxima  the number of maxima to find (see below)
+   \param[in] level level at which to compute the width (2 for half maximum, 10 for tenth maximum etc.)
+   \param[in] dimension the dimension along which the line source is oriented, or 0 for point sources
+   \param[in] nema enables the calculation based on the NEMA Standards Publication 
+   NU 2-2001. 
+   \return a list containing the maximum_value, its location per slice and resolution. 
+   For line sources, sorted by minimum to maximum slice of the requested dimension, or for point sources, 
+   sorted by maximum to minimum value. 
+
+   For line sources, \a num_maxima slices are sampled (from first to last slice, with steps given by num_slices/(num_maxima+1)
+
+   For point sources, if \a num_maxima is larger than 1, after finding a maximum and the resolution, the data is masked out in
+   a neigbhourhood of half-size (resolution*2/level). This will only ork properly if the point sources are not too close to eachother
+   and have roughly the maximum.
+
+   The value of the maximum is computed using a parabolic fit through the 3 points around the maximum as specified
+   in NEMA 2001. 
+
+   If nema=false, the interpolated_line() function is used to find a line, otherwise we use  extract_line().
 */
 
 template <int num_dimensions, class elemT>           
@@ -128,50 +144,17 @@ find_fwhm_in_image(DiscretisedDensity<num_dimensions,elemT> & input_image,
                    const int dimension, const bool nema);
 /*!  
    \ingroup resolution
-   \brief                              
-   Masks the input_array
+   \brief assign a value to a sub-region of an array
 
-  it masks the input_array at the location of a point source setting all values to -1, with a mask size 
-  depending on the resolution of the previous point (in pixel units), at each of the directions. In the 
-  code there is a scale factor to change easily the size of the mask.
-  (mask_size(z,y,x)=int(6*resolution(z,y,x)/level)). 
+   sets all values for indices between \a mask_location - \a half_size and \a mask_location + \a half_size to \a value,
+   taking care of staing inside the index-range of the array.
 */
-template <int num_dimensions, class elemT>   
+template <class elemT>   
 void 
-flexible_mask(Array<num_dimensions,elemT>& input_array, 
-              const BasicCoordinate<num_dimensions,int>& max_location,
-              const BasicCoordinate<num_dimensions,elemT>& resolution,
-              const float level);
-/*!  
-   \ingroup resolution
-   \brief     
-   Calculates the maximum point (x0,y0) of a parabola that passes through 3 points.
-
-   As input it takes the Begin and End Iterators of a sequence of numbers (e.g. vector). 
-   The three points are the maximum (x2,y2) of this sequence and the two neihbour points 
-   (x1,y1) and (x3,y3).  
-
-  It returns the maximum point value y0. 
-*/           
-//@{
-
-/*!
-   \ingroup resolution
-   \brief     
-   It returns the maximum point value y0. 
-*/
-template <class RandomAccessIterType>
-   float parabolic_3points_fit(const RandomAccessIterType& begin_iter,
-   const RandomAccessIterType& end_iter);
-/*!
-   \ingroup resolution
-   \brief     
-   It returns the maximum point location x0 in (-1,1). 
-*/
-template <class RandomAccessIterType>
-float parabolic_3points_fit_x0(const RandomAccessIterType& begin_iter,
-							   const RandomAccessIterType& end_iter);
-//@}
+assign_to_subregion(Array<num_dimensions,elemT>& input_array, 
+                    const BasicCoordinate<num_dimensions,int>& mask_location,
+                    const BasicCoordinate<num_dimensions,int>& half_size,
+                    const elemT& value);
 
 
 END_NAMESPACE_STIR
