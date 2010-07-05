3ce0254d3c905d9b7c5054b73de8d63fea6283ca
Kris Thielemans <Kris.Thielemans@stir>
2010-07-05 09:13:38 +0000

    - in post_processing(), set max_ring_difference_num_to_process to num_rings-1 if it wasn't set by the parser - some reformatting

--- src/recon_buildblock/PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin.cxx
+++ src/recon_buildblock/PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin.cxx
@@ -109,10 +109,10 @@ Succeeded
 PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin<TargetT>::set_up(shared_ptr <TargetT > const& target_sptr) 
 { 
 #ifdef STIR_MPI
-	//broadcast objective_function (100=PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin)
-	distributed::send_int_value(100, -1);
+        //broadcast objective_function (100=PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin)
+        distributed::send_int_value(100, -1);
 #endif
-	
+        
  
   if (base_type::set_up(target_sptr) != Succeeded::yes) 
     return Succeeded::no; 
@@ -133,36 +133,39 @@ PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin<Tar
     return true; 
  
 #if 1
-   if (is_null_ptr(this->PM_sptr)) 
+  if (is_null_ptr(this->PM_sptr)) 
 
-  { warning("You need to specify a projection matrix"); return true; } 
+    { warning("You need to specify a projection matrix"); return true; } 
 
 #else
-   if(is_null_ptr(this->projector_pair_sptr->get_forward_projector_sptr()))
-   {
-     warning("No valid forward projector is defined"); return true;}
+  if(is_null_ptr(this->projector_pair_sptr->get_forward_projector_sptr()))
+    {
+      warning("No valid forward projector is defined"); return true;
+    }
 
-   if(is_null_ptr(this->projector_pair_sptr->get_back_projector_sptr()))
-   {
-     warning("No valid back projector is defined"); return true;}
+  if(is_null_ptr(this->projector_pair_sptr->get_back_projector_sptr()))
+    {
+      warning("No valid back projector is defined"); return true;
+    }
 #endif
-   shared_ptr<Scanner> scanner_sptr = new Scanner(*this->list_mode_data_sptr->get_scanner_ptr());
+  shared_ptr<Scanner> scanner_sptr = new Scanner(*this->list_mode_data_sptr->get_scanner_ptr());
 
-   if (this->max_ring_difference_num_to_process == -1)
-      {
+  if (this->max_ring_difference_num_to_process == -1)
+    {
+      this->max_ring_difference_num_to_process = 
         scanner_sptr->get_num_rings()-1;
-      }
+    }
 
      
-   if (this->additive_projection_data_filename != "0") 
-  { 
-    cerr << "\nReading additive projdata data " 
-	 << additive_projection_data_filename  
-	 << endl; 
-    shared_ptr <ProjData> temp_additive_proj_data_sptr =  
-      ProjData::read_from_file(this->additive_projection_data_filename); 
-    this->additive_proj_data_sptr = new ProjDataInMemory(* temp_additive_proj_data_sptr);
-  } 
+  if (this->additive_projection_data_filename != "0") 
+    { 
+      cerr << "\nReading additive projdata data " 
+           << additive_projection_data_filename  
+           << endl; 
+      shared_ptr <ProjData> temp_additive_proj_data_sptr =  
+        ProjData::read_from_file(this->additive_projection_data_filename); 
+      this->additive_proj_data_sptr = new ProjDataInMemory(* temp_additive_proj_data_sptr);
+    } 
   
 
   this->proj_data_info_cyl_uncompressed_ptr =
@@ -186,11 +189,11 @@ construct_target_ptr() const
 
  return 
       new VoxelsOnCartesianGrid<float> (*this->proj_data_info_cyl_uncompressed_ptr, 
-					static_cast<float>(this->zoom), 
-					CartesianCoordinate3D<float>(static_cast<float>(this->Zoffset), 
-								     static_cast<float>(this->Yoffset), 
-								     static_cast<float>(this->Xoffset)), 
-					CartesianCoordinate3D<int>(this->output_image_size_z, 
+                                        static_cast<float>(this->zoom), 
+                                        CartesianCoordinate3D<float>(static_cast<float>(this->Zoffset), 
+                                                                     static_cast<float>(this->Yoffset), 
+                                                                     static_cast<float>(this->Xoffset)), 
+                                        CartesianCoordinate3D<int>(this->output_image_size_z, 
                                                                    this->output_image_size_xy, 
                                                                    this->output_image_size_xy) 
                                        ); 
@@ -201,8 +204,8 @@ template <typename TargetT>
 void 
 PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin<TargetT>:: 
 compute_sub_gradient_without_penalty_plus_sensitivity(TargetT& gradient,  
-	    				 const TargetT &current_estimate,  
-					 const int subset_num) 
+                                         const TargetT &current_estimate,  
+                                         const int subset_num) 
 { 
 
   const double start_time = this->frame_defs.get_start_time(this->current_frame_num);
@@ -224,30 +227,30 @@ compute_sub_gradient_without_penalty_plus_sensitivity(TargetT& gradient,
       {
        const double new_time = record.time().get_time_in_secs();
        if ( new_time >= end_time)
-	   break; // get out of while loop
+           break; // get out of while loop
        current_time = new_time;
       }
     else if (record.is_event() && record.event().is_prompt()) 
       { 
-	Bin measured_bin; 
-	record.event().get_bin(measured_bin, *proj_data_info_cyl_uncompressed_ptr); 
-	if (measured_bin.get_bin_value() <= 0)
-	  continue;	 
-	this->PM_sptr->get_proj_matrix_elems_for_one_bin(proj_matrix_row, measured_bin); 
-	//in_the_range++;
-	Bin fwd_bin; 
-	proj_matrix_row.forward_project(fwd_bin,current_estimate); 
-	// additive sinogram 
-	if (!is_null_ptr(this->additive_proj_data_sptr))
-	  {
-	    float add_value = this->additive_proj_data_sptr->get_bin_value(measured_bin);
-	    float value= fwd_bin.get_bin_value()+add_value;	    
-	    fwd_bin.set_bin_value(value);
-       	  }
+        Bin measured_bin; 
+        record.event().get_bin(measured_bin, *proj_data_info_cyl_uncompressed_ptr); 
+        if (measured_bin.get_bin_value() <= 0)
+          continue;      
+        this->PM_sptr->get_proj_matrix_elems_for_one_bin(proj_matrix_row, measured_bin); 
+        //in_the_range++;
+        Bin fwd_bin; 
+        proj_matrix_row.forward_project(fwd_bin,current_estimate); 
+        // additive sinogram 
+        if (!is_null_ptr(this->additive_proj_data_sptr))
+          {
+            float add_value = this->additive_proj_data_sptr->get_bin_value(measured_bin);
+            float value= fwd_bin.get_bin_value()+add_value;         
+            fwd_bin.set_bin_value(value);
+          }
         float  measured_div_fwd = measured_bin.get_bin_value()/fwd_bin.get_bin_value();
-	measured_bin.set_bin_value(measured_div_fwd);
-	proj_matrix_row.back_project(gradient, measured_bin); 
-	 
+        measured_bin.set_bin_value(measured_div_fwd);
+        proj_matrix_row.back_project(gradient, measured_bin); 
+         
       } 
   }
   //  cerr << " The number_of_events " << count_of_events << "   ";
