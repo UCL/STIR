8748656f41c2306dfa6da87f16fd577d6112213c
Kris Thielemans <Kris.Thielemans@stir>
2011-01-12 18:16:28 +0000

    - added test cases with pointers etc - added test for convertability of a normal iterator to a const iterator

--- src/test/test_NestedIterator.cxx
+++ src/test/test_NestedIterator.cxx
@@ -34,7 +34,10 @@
 
 #include "stir/RunTests.h"
 #include "stir/NestedIterator.h"
+#include "stir/shared_ptr.h"
 #include <iostream>
+#include <list>
+#include <vector>
 
 START_NAMESPACE_STIR
 
@@ -65,11 +68,11 @@ NestedIteratorTests::run_tests()
       Array<2,float> test2(range);
 
       {
-	float value = 1.2F;
-	for (Array<2,float>::full_iterator iter = test2.begin_all();
-	     iter != test2.end_all(); 
-	     )
-	  *iter++ = value++;
+        float value = 1.2F;
+        for (Array<2,float>::full_iterator iter = test2.begin_all();
+             iter != test2.end_all(); 
+             )
+          *iter++ = value++;
       }
 
       check(test2.begin()->begin() == BeginEndFunction<Array<2,float>::iterator>().begin(test2.begin()), "begin");
@@ -80,12 +83,12 @@ NestedIteratorTests::run_tests()
       //fiter1=fiter;
       const FullIter fiter_end(test2.end(),test2.end());      
       for (Array<2,float>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  check(fiter!=fiter_end, "fiter");
-	  check(*fiter++ == *iter++,"fiter==");
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          check(fiter!=fiter_end, "fiter");
+          check(*fiter++ == *iter++,"fiter==");
+        }
       
       check(fiter==fiter_end,"fiter end");      
       //      check(test2.begin()->begin() == make_begin_function(test2.begin())(), "begin");
@@ -104,17 +107,17 @@ NestedIteratorTests::run_tests()
     {
       float value = 1.2F;
       for (Array<2,elemT>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  *iter++ = make_coordinate(value,value+.3F);
-	  ++value;
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          *iter++ = make_coordinate(value,value+.3F);
+          ++value;
+        }
     }
 
     check(test2.begin()->begin() == BeginEndFunction<Array<2,elemT>::iterator>().begin(test2.begin()), "begin");
     check((test2.begin()+1)->begin() == BeginEndFunction<Array<2,elemT>::iterator>().begin(test2.begin()+1), "begin");
-    typedef NestedIterator<Array<2,elemT>::iterator>	
+    typedef NestedIterator<Array<2,elemT>::iterator>    
       FullIter;
     FullIter fiter1;
     FullIter fiter(test2.begin(),test2.end());
@@ -122,12 +125,12 @@ NestedIteratorTests::run_tests()
     const FullIter fiter_end(test2.end(),test2.end());      
     {
       for (Array<2,elemT>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  check(fiter!=fiter_end, "fiter");
-	  check(*fiter++ == *iter++,"fiter==");
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          check(fiter!=fiter_end, "fiter");
+          check(*fiter++ == *iter++,"fiter==");
+        }
     }
     check(fiter==fiter_end,"fiter end");      
   }
@@ -141,12 +144,12 @@ NestedIteratorTests::run_tests()
     {
       float value = 1.2F;
       for (Array<2,elemT>::full_iterator iter = test.begin_all();
-	   iter != test.end_all(); 
-	   )
-	{
-	  *iter++ = make_coordinate(value,value+.3F);
-	  ++value;
-	}
+           iter != test.end_all(); 
+           )
+        {
+          *iter++ = make_coordinate(value,value+.3F);
+          ++value;
+        }
     }
 
     const Array<2,elemT> test2 = test;
@@ -159,7 +162,7 @@ NestedIteratorTests::run_tests()
 
     check(test2.begin()->begin() == constbeginendfunction_type().begin(test2.begin()), "begin");
     check((test2.begin()+1)->begin() == constbeginendfunction_type().begin(test2.begin()+1), "begin");
-    typedef NestedIterator<Array<2,elemT>::const_iterator, constbeginendfunction_type>	
+    typedef NestedIterator<Array<2,elemT>::const_iterator, constbeginendfunction_type>  
       FullIter;
     FullIter fiter1;
     FullIter fiter(test2.begin(),test2.end());
@@ -168,12 +171,12 @@ NestedIteratorTests::run_tests()
       
     {
       for (Array<2,elemT>::const_full_iterator iter = test2.begin_all_const();
-	   iter != test2.end_all_const(); 
-	   )
-	{
-	  check(fiter!=fiter_end, "fiter");
-	  check(*fiter++ == *iter++,"fiter==");
-	}
+           iter != test2.end_all_const(); 
+           )
+        {
+          check(fiter!=fiter_end, "fiter");
+          check(*fiter++ == *iter++,"fiter==");
+        }
     }
     check(fiter==fiter_end,"fiter end");      
   }
@@ -187,12 +190,12 @@ NestedIteratorTests::run_tests()
     {
       float value = 1.2F;
       for (Array<2,elemT>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  *iter++ = make_coordinate(value,value+.3F);
-	  ++value;
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          *iter++ = make_coordinate(value,value+.3F);
+          ++value;
+        }
     }
 
     check(test2.begin()->begin() == BeginEndFunction<Array<2,elemT>::iterator>().begin(test2.begin()), "begin");
@@ -208,14 +211,14 @@ NestedIteratorTests::run_tests()
       
     {
       for (Array<2,elemT>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  check(fiter!=fiter_end, "fiter");
-	  check_if_equal(*fiter++, *iter->begin(),"fiter== 0");
-	  check_if_equal(*fiter++,*(iter->begin()+1),"fiter== 1");
-	  ++iter;
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          check(fiter!=fiter_end, "fiter");
+          check_if_equal(*fiter++, *iter->begin(),"fiter== 0");
+          check_if_equal(*fiter++,*(iter->begin()+1),"fiter== 1");
+          ++iter;
+        }
     }
     check(fiter==fiter_end,"fiter end");      
   }
@@ -229,19 +232,20 @@ NestedIteratorTests::run_tests()
     {
       float value = 1.2F;
       for (Array<2,elemT>::full_iterator iter = test.begin_all();
-	   iter != test.end_all(); 
-	   )
-	{
-	  *iter++ = make_coordinate(value,value+.3F);
-	  ++value;
-	}
+           iter != test.end_all(); 
+           )
+        {
+          *iter++ = make_coordinate(value,value+.3F);
+          ++value;
+        }
+
     }
     const Array<2,elemT> test2 = test;
 
     check(test2.begin_all_const()->begin() == ConstBeginEndFunction<Array<2,elemT>::const_full_iterator>().begin(test2.begin_all_const()), "begin");
     typedef
       NestedIterator<Array<2,elemT>::const_full_iterator,
-      ConstBeginEndFunction<Array<2,elemT>::const_full_iterator> >
+                     ConstBeginEndFunction<Array<2,elemT>::const_full_iterator> >
       FullIter;
     FullIter fiter1;
     FullIter fiter(test2.begin_all_const(),test2.end_all_const());
@@ -250,14 +254,14 @@ NestedIteratorTests::run_tests()
       
     {
       for (Array<2,elemT>::const_full_iterator iter = test2.begin_all_const();
-	   iter != test2.end_all_const(); 
-	   )
-	{
-	  check(fiter!=fiter_end, "fiter");
-	  check_if_equal(*fiter++, *iter->begin(),"fiter== 0");
-	  check_if_equal(*fiter++,*(iter->begin()+1),"fiter== 1");
-	  ++iter;
-	}
+           iter != test2.end_all_const(); 
+           )
+        {
+          check(fiter!=fiter_end, "fiter");
+          check_if_equal(*fiter++, *iter->begin(),"fiter== 0");
+          check_if_equal(*fiter++,*(iter->begin()+1),"fiter== 1");
+          ++iter;
+        }
     }
     check(fiter==fiter_end,"fiter end");      
   }
@@ -271,19 +275,19 @@ NestedIteratorTests::run_tests()
     {
       float value = 1.2F;
       for (Array<2,elemT>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  *iter++ = make_coordinate(value,value+.3F);
-	  ++value;
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          *iter++ = make_coordinate(value,value+.3F);
+          ++value;
+        }
     }
 
     check(test2.begin()->begin() == BeginEndFunction<Array<2,elemT>::iterator>().begin(test2.begin()), "begin");
     check((test2.begin()+1)->begin() == BeginEndFunction<Array<2,elemT>::iterator>().begin(test2.begin()+1), "begin");
     typedef
       NestedIterator<Array<2,elemT>::full_iterator,
-      BeginEndFunction<Array<2,elemT>::full_iterator> >
+                     BeginEndFunction<Array<2,elemT>::full_iterator> >
       FullIter;
     FullIter fiter1;
     FullIter fiter(test2.begin_all(),test2.end_all());
@@ -292,17 +296,139 @@ NestedIteratorTests::run_tests()
       
     {
       for (Array<2,elemT>::full_iterator iter = test2.begin_all();
-	   iter != test2.end_all(); 
-	   )
-	{
-	  check(fiter!=fiter_end, "fiter");
-	  check_if_equal(*fiter++, *iter->begin(),"fiter== 0");
-	  check_if_equal(*fiter++,*(iter->begin()+1),"fiter== 1");
-	  ++iter;
-	}
+           iter != test2.end_all(); 
+           )
+        {
+          check(fiter!=fiter_end, "fiter");
+          check_if_equal(*fiter++, *iter->begin(),"fiter== 0");
+          check_if_equal(*fiter++,*(iter->begin()+1),"fiter== 1");
+          ++iter;
+        }
     }
     check(fiter==fiter_end,"fiter end");      
   }
+  std::cerr<< " NestedIterator vector<list>\n";
+  {
+    typedef std::list<int> C2;
+    typedef std::vector<C2> C1;
+    C1 c(2);
+    c[0].push_back(1); c[0].push_back(2);
+    c[1].push_back(3); c[1].push_back(4); c[1].push_back(5);
+    // normal iterator
+    {
+      typedef NestedIterator<C1::iterator> FullIter;
+      FullIter fiter(c.begin(),c.end());
+      const FullIter fiter_end(c.end(),c.end());
+      int count=1;
+      while (fiter != fiter_end)
+      { 
+        check_if_equal(*fiter++, count++, "nestiterator of vector<list>");
+      }
+      check_if_equal(count, 6, "nestiterator of vector<list>: num elements");
+    }
+    // const iterator
+    {
+      typedef NestedIterator<C1::const_iterator, ConstBeginEndFunction<C1::const_iterator> > FullIter;
+      FullIter fiter(c.begin(),c.end());
+      const FullIter fiter_end(c.end(),c.end());
+      int count=1;
+      while (fiter != fiter_end)
+      { 
+        check_if_equal(*fiter++, count++, "nestiterator of vector<list>");
+      }
+      check_if_equal(count, 6, "nestiterator of vector<list>: num elements");
+    }
+    // test conversion from full_iterator to const_full_iterator
+    {
+      typedef NestedIterator<C1::const_iterator, ConstBeginEndFunction<C1::const_iterator> > CFullIter;
+      typedef NestedIterator<C1::iterator> FullIter;
+      FullIter fiter(c.begin(),c.end());
+      CFullIter cfiter= fiter; // this should compile
+    }
+  }
+  std::cerr<< " NestedIterator vector<list *>\n";
+  {
+    typedef std::list<int> C2;
+    typedef std::vector<C2 *> C1;
+    C1 c(2);
+    c[0] = new C2;
+    c[1] = new C2;
+    c[0]->push_back(1); c[0]->push_back(2);
+    c[1]->push_back(3); c[1]->push_back(4); c[1]->push_back(5);
+    // normal iterator
+    {
+      typedef NestedIterator<C1::iterator, PtrBeginEndFunction<C1::iterator> > FullIter;
+      FullIter fiter(c.begin(),c.end());
+      const FullIter fiter_end(c.end(),c.end());
+      int count=1;
+      while (fiter != fiter_end)
+      { 
+        check_if_equal(*fiter++, count++, "nestiterator of vector< list *>");
+      }
+      check_if_equal(count, 6, "nestiterator of vector<list *>: num elements");
+    }
+    // const iterator
+    {
+      typedef NestedIterator<C1::const_iterator, ConstPtrBeginEndFunction<C1::const_iterator> > FullIter;
+      FullIter fiter(c.begin(),c.end());
+      const FullIter fiter_end(c.end(),c.end());
+      int count=1;
+      while (fiter != fiter_end)
+      { 
+        check_if_equal(*fiter++, count++, "nestiterator of vector< list *>");
+      }
+      check_if_equal(count, 6, "nestiterator of vector<list *>: num elements");
+    }
+    delete c[0];
+    delete c[1];
+  }
+
+  std::cerr<< " NestedIterator vector< shared_ptr<Array<2,...> >\n";
+  {
+    IndexRange<2> range1(make_coordinate(0,0),make_coordinate(1,2));
+    IndexRange<2> range2(make_coordinate(0,0),make_coordinate(1,3));
+    typedef int elemT;
+    typedef Array<2,int> C2;
+    typedef std::vector<shared_ptr<C2> > C1;
+    C1 c(2);
+    c[0] = new C2(range1);
+    c[1] = new C2(range2);
+    int count = 1;
+    for (C2::full_iterator fullarrayiter = c[0]->begin_all(); fullarrayiter != c[0]->end_all();
+         ++fullarrayiter, ++count)
+      *fullarrayiter = count;
+    for (C2::full_iterator fullarrayiter = c[1]->begin_all(); fullarrayiter != c[1]->end_all();
+         ++fullarrayiter, ++count)
+      *fullarrayiter = count;
+
+    // normal iterator
+    {
+      typedef NestedIterator<C1::iterator, PtrBeginEndAllFunction<C1::iterator> > FullIter;
+      FullIter fiter(c.begin(),c.end());
+      const FullIter fiter_end(c.end(),c.end());
+      count=1;
+      while (fiter != fiter_end)
+      { 
+        check_if_equal(*fiter++, count++, "nestiterator of vector<shared_ptr<Array<2,int>>>");
+      }
+      check_if_equal(count, static_cast<int>(c[0]->size_all() + c[1]->size_all() + 1), 
+        "nestiterator of vector<shared_ptr<Array<2,int>>>: num elements");
+    }
+    // const iterator
+    {
+      typedef NestedIterator<C1::const_iterator, ConstPtrBeginEndAllFunction<C1::const_iterator> > FullIter;
+      FullIter fiter(c.begin(),c.end());
+      const FullIter fiter_end(c.end(),c.end());
+      count=1;
+      while (fiter != fiter_end)
+      { 
+        check_if_equal(*fiter++, count++, "nestiterator of vector<shared_ptr<Array<2,int>>>");
+      }
+      check_if_equal(count, static_cast<int>(c[0]->size_all() + c[1]->size_all() + 1), 
+        "nestiterator of vector<shared_ptr<Array<2,int>>>: num elements");
+    }
+  }
+
 }
 
 END_NAMESPACE_STIR
