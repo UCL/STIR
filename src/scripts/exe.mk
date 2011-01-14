badcb7a5a55237e9c5e611c0bd50ff860ff9c2e3
Kris Thielemans <Kris.Thielemans@stir>
2011-01-14 16:36:34 +0000

    first check-in

---
+++ src/scripts/exe.mk
@@ -0,0 +1,9 @@
+#
+# $Id$
+#
+
+dir:=scripts
+
+$(dir)_SCRIPTS:=stir_subtract stir_divide count stir_print_voxel_sizes.sh
+
+include $(WORKSPACE)/exe.mk
---
+++ src/scripts/stir_print_voxel_sizes.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# A simple script to print voxel sizes of an image
+# Example:
+# print_voxel_sizes.sh img.hv
+# => will print voxel sizes (in mm) as 3 numbers separated by spaces, in order z y x
+#
+#  Copyright (C) 2011- $Date$, Hammersmith Imanet Ltd
+#  This file is part of STIR.
+#
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  See STIR/LICENSE.txt for details
+#      
+# Author Kris Thielemans
+# $Id$
+# 
+if [ $# -ne 1 ]; then
+  echo "Usage: `basename $0` filename"
+  echo "Prints voxel-sizes in order z y x"
+  exit 1
+fi
+
+filename=$1
+
+list_image_info "${filename}" |awk -F: '/[Vv]oxel-size/ {value=$2; gsub("[{},]"," ",value);print value}'
