1e1896200d51e578cb445e1cdea4507cd9ae4672
Kris Thielemans <Kris.Thielemans@stir>
2011-06-29 08:25:11 +0000

    moved LmToProjDataBootstrap et al to global distro

--- src/listmode_buildblock/LmToProjDataBootstrap.cxx
+++ src/listmode_buildblock/LmToProjDataBootstrap.cxx
@@ -5,7 +5,7 @@
 
   \file
   \ingroup listmode
-  \brief Class for rebinning listmode files with the bootstrap method
+  \brief Class stir::LmToProjDataBootstrap for rebinning listmode files with the bootstrap method
     
   \author Kris Thielemans
       
@@ -14,10 +14,22 @@
 */
 /*
     Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
     See STIR/LICENSE.txt for details
 */
 
-#include "local/stir/listmode/LmToProjDataBootstrap.h"
+#include "stir/listmode/LmToProjDataBootstrap.h"
 #include "stir/listmode/CListRecord.h"
 #include "stir/Succeeded.h"
 #include <iostream>
