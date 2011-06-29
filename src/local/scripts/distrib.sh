f203ecec8a50f1c6b2e9b4f74f586f55c9cf09ca
Kris Thielemans <Kris.Thielemans@stir>
2011-06-29 23:36:44 +0000

    change version to 2.1

--- src/local/scripts/distrib.sh
+++ src/local/scripts/distrib.sh
@@ -5,16 +5,16 @@ do_version=1
 do_license=1
 do_ChangeLog=1
 do_doc=1
-do_doxygen=1
+do_doxygen=0
 do_zip_source=1
-do_recon_test_pack=0
-do_transfer=0
+do_recon_test_pack=1
+do_transfer=1
 
-do_website_final_version=0
+do_website_final_version=1
 do_website_sync=0
 
 set -e
-VERSION=2.1beta
+VERSION=2.1
 
 # for cvs2cl.pl
 BRANCH=trunk
