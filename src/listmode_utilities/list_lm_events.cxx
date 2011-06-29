8c153287032670610b32136211ffcb8fee138cf7
Kris Thielemans <Kris.Thielemans@stir>
2011-06-29 09:03:03 +0000

    added options to list only certain types of events

--- src/listmode_utilities/list_lm_events.cxx
+++ src/listmode_utilities/list_lm_events.cxx
@@ -3,7 +3,20 @@
 //
 /*
     Copyright (C) 2003- $Date$, Hammersmith Imanet Ltd
-    For GE internal use only.
+    This file is part of STIR.
+
+    This file is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This file is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    See STIR/LICENSE.txt for details
+
 */
 /*!
   \file 
@@ -38,98 +51,66 @@ using std::vector;
 
 
 USING_NAMESPACE_STIR
-#if 0
-// TODO this is only good for the Polaris data
-void
-find_and_store_gate_tag_values_from_lm(vector<float>& lm_time, 
-				       vector<unsigned>& lm_gate, 
-				       CListModeData& listmode_data,
-				       const unsigned long max_num_events)
-{
-  
-  unsigned  LastChannelState=0;
-  unsigned  ChState;
-  int PulseWidth = 0 ;
-  double StartPulseTime=0;
-  unsigned long more_events = max_num_events;
-  
-  // reset listmode to the beginning 
-  listmode_data.reset();
-  
-  shared_ptr <CListRecord> record_sptr = listmode_data.get_empty_record_sptr();
-  CListRecord& record = *record_sptr;
-  while (max_num_events==0 || more_events!=0)
-  {
-
-    if (listmode_data.get_next_record(record) == Succeeded::no) 
-    {
-       break; //get out of while loop
-    }
-    if (record.is_time())
-    {
-      unsigned CurrentChannelState =  record.time().get_gating() ;
-      double CurrentTime = record.time().get_time_in_secs();
-      
-      if ( LastChannelState != CurrentChannelState && CurrentChannelState )
-      {
-	if ( PulseWidth > 5 ) //TODO get rid of number 5
-	{
-	  lm_gate.push_back(ChState);
-	  lm_time.push_back(StartPulseTime);
-	  --more_events;
-	}
-	LastChannelState = CurrentChannelState ;
-	PulseWidth = 0 ;
-      }
-      else if ( LastChannelState == CurrentChannelState && CurrentChannelState )
-      {
-	if ( !PulseWidth ) StartPulseTime = CurrentTime ;
-	ChState = LastChannelState ;
-	PulseWidth += 1 ;
-      }
-    }
-  }
-  // reset listmode to the beginning 
-  listmode_data.reset();
- 
-}
-
-#endif
 
 int main(int argc, char *argv[])
 {
+  const char * const program_name = argv[0];
+  // skip program name
+  --argc;
+  ++argv;
 
-  if (argc<2 || argc>3)
+  bool list_time=true;
+  bool list_coincidence=false;
+  bool list_gating=true;
+  bool list_unknown=false;
+  unsigned long num_events_to_list = 0;
+  while (argc>1 && argv[0][0]=='-')
     {
-      cerr << "Usage: " << argv[0] << "lm_filename_prefix [num_time_events_to_list]\n";
+      if (strcmp(argv[0], "--num-events-to-list")==0)
+	{
+	  num_events_to_list = atol(argv[1]);
+	} 
+      else if (strcmp(argv[0], "--time")==0)
+        {
+          list_time = atoi(argv[1])!=0;
+        }
+      else if (strcmp(argv[0], "--gating")==0)
+        {
+          list_gating = atoi(argv[1])!=0;
+        }
+      else if (strcmp(argv[0], "--coincidence")==0)
+        {
+          list_coincidence = atoi(argv[1])!=0;
+        }
+      else if (strcmp(argv[0], "--unknown")==0)
+        {
+          list_unknown = atoi(argv[1])!=0;
+        }
+      else
+        { 
+          cerr << "Unrecognised option\n";
+          return EXIT_FAILURE;
+        }
+      argc-=2; argv+=2;
+    }
+
+  if (argc!=1)
+    {
+      cerr << "Usage: " << program_name << "[options] lm_filename\n"
+           << "Options:\n"
+           << "--list-time 0|1 : list time events or not (default: 1)\n"
+           << "--list-gating 0|1 : list gating events or not (default: 1)\n"
+           << "--list-coincidence 0|1 : list coincidence events or not (default: 0)\n"
+           << "--list-unknown 0|1 : list if event of unknown type encountered or not (default: 0)\n"
+           << "--num-events-to-list <num> : limit number of events written to stdout\n";
       return EXIT_FAILURE;
     }
 
   shared_ptr<CListModeData> lm_data_ptr =
-    CListModeData::read_from_file(argv[1]);
-  const unsigned long num_events_to_list =
-    argc==3 ? atol(argv[2]) : 0;
+    CListModeData::read_from_file(argv[0]);  
 
   cout << "Scanner: " << lm_data_ptr->get_scanner_ptr()->get_name() << endl;
 
-#if 0
-  // only changes in gates (appropriate for Polaris)
-
-  vector<float> lm_time;
-  vector<unsigned> lm_gate;
-  find_and_store_gate_tag_values_from_lm(lm_time, 
-					 lm_gate, 
-				         *lm_data_ptr,
-					 num_events_to_list);
-  vector<float>::const_iterator lm_time_iter = lm_time.begin();
-  vector<unsigned>::const_iterator lm_gate_iter = lm_gate.begin();
-  for (;lm_time_iter != lm_time.end(); ++lm_time_iter, ++lm_gate_iter)
-    {
-      cerr << *lm_time_iter << '\t' << hex << *lm_gate_iter << '\n';
-    }
-  
-#else
-  // all of them
   unsigned long num_listed_events = 0;
   {      
     // loop over all events in the listmode file
@@ -139,6 +120,7 @@ int main(int argc, char *argv[])
     while (num_events_to_list==0 || num_events_to_list!=num_listed_events)
       {
         bool recognised = false;
+        bool listed = false;
 	if (lm_data_ptr->get_next_record(record) == Succeeded::no) 
 	  {
 	    // no more events in file for some reason
@@ -147,47 +129,60 @@ int main(int argc, char *argv[])
 	if (record.is_time())
 	  {
             recognised=true;
-	    cout << "Time " << record.time().get_time_in_millisecs();
-	    ++num_listed_events;	    
+            if (list_time)
+              {
+                cout << "Time " << record.time().get_time_in_millisecs();
+                listed = true; 
+              }
 	   } 
         {
           CListRecordWithGatingInput * record_ptr = dynamic_cast<CListRecordWithGatingInput *>(&record);
           if (record_ptr!=0 && record_ptr->is_gating_input())
             {
               recognised=true;
-              cout << "Gating " << hex << record_ptr->gating_input().get_gating();
+              if (list_gating)
+                {
+                  cout << "Gating " << hex << record_ptr->gating_input().get_gating();
+                  listed = true; 
+                }
             }
         }
         if (record.is_event())
         {
           recognised=true;
-          CListEventCylindricalScannerWithDiscreteDetectors * event_ptr = 
-            dynamic_cast<CListEventCylindricalScannerWithDiscreteDetectors *>(&record.event());
-          if (event_ptr!=0)
-            {
-              DetectionPositionPair<> det_pos;
-              event_ptr->get_detection_position(det_pos);
-              cout << "Coincidence " << (event_ptr->is_prompt() ? "p " : "d ")
-                   << "(c:" << det_pos.pos1().tangential_coord()
-                   << ",r:" << det_pos.pos1().axial_coord()
-                   << ",l:" << det_pos.pos1().radial_coord()
-                   << ")-"
+          if (list_coincidence)
+            {CListEventCylindricalScannerWithDiscreteDetectors * event_ptr = 
+                dynamic_cast<CListEventCylindricalScannerWithDiscreteDetectors *>(&record.event());
+              if (event_ptr!=0)
+                {
+                  DetectionPositionPair<> det_pos;
+                  event_ptr->get_detection_position(det_pos);
+                  cout << "Coincidence " << (event_ptr->is_prompt() ? "p " : "d ")
+                       << "(c:" << det_pos.pos1().tangential_coord()
+                       << ",r:" << det_pos.pos1().axial_coord()
+                       << ",l:" << det_pos.pos1().radial_coord()
+                       << ")-"
                    << "(c:" << det_pos.pos2().tangential_coord()
-                   << ",r:" << det_pos.pos2().axial_coord()
-                   << ",l:" << det_pos.pos2().radial_coord()
-                   << ")";
+                       << ",r:" << det_pos.pos2().axial_coord()
+                       << ",l:" << det_pos.pos2().radial_coord()
+                       << ")";
+                  listed = true; 
+                }
             }
         }
-        if (!recognised)
+        if (!recognised && list_unknown)
         {
-          cout << "Unknown";
+          cout << "Unknown type";
         }
-        cout << '\n';
+        if (listed)
+          {
+            ++num_listed_events;	    
+            cout << '\n';
+          }
       }
     cout << '\n';
 
   }
-#endif
 
   return EXIT_SUCCESS;
 }
