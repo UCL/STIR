367905a408d75bb5d9edd817a5e84b37881bd9e1
Kris Thielemans <Kris.Thielemans@stir>
2012-02-05 15:15:21 +0000

    added write_phg_image_info.c

--- src/SimSET/exe.mk
+++ src/SimSET/exe.mk
@@ -5,6 +5,7 @@ dir := SimSET
 
 $(dir)_SOURCES :=  \
 	conv_SimSET_projdata_to_STIR.cxx \
-	conv_to_SimSET_att_image.cxx
+	conv_to_SimSET_att_image.cxx \
+	write_phg_image_info.c
 
 include $(WORKSPACE)/exe.mk
