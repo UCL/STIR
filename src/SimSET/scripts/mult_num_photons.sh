091b85374c7c34836c0cc269c6d8be5777f2a808
Kris Thielemans <Kris.Thielemans@stir>
2012-02-06 18:02:43 +0000

    no longer hard-wire rec.weight filename

--- src/SimSET/scripts/mult_num_photons.sh
+++ src/SimSET/scripts/mult_num_photons.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 # $Id$
 
 # Script to normalise simset simulations
@@ -9,9 +9,25 @@
 # This script multiplies the results with the number of photons
 # such that they correspond to simulations of an experiment with
 # a given number of counts.
-
+#
 # This script relies on names of files fixed on other scripts, so beware.
 
+#  Copyright (C) 2005, Hammersmith Imanet Ltd
+#  Copyright (C) 2011-07-01 - $Date$, Kris Thielemans
+#  This file is part of STIR.
+#
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published by
+#  the Free Software Foundation; either version 2.1 of the License, or
+#  (at your option) any later version.
+
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  See STIR/LICENSE.txt for details
+#      
 # Authors:  Kris Thielemans
 
 
@@ -27,8 +43,9 @@ script_name="$0"
 trap "echo ERROR in script $script_name" ERR
 PRINTHEADER=${SIMSET_DIR}/bin/printheader
 
+weight_filename=`awk -F= '/weight_image_path/ '{print $2}' $1/bin.rec |tr -d \"`
+all=`SimSET_STIR_names.sh $1/${weight_filename}`
 while [ $# -ne 0 ]; do
-  all=`SimSET_STIR_names.sh $1/rec.weight`
   dir="$1/"
   num_photons=`grep num_to_simulate ${dir}phg.rec|awk '{print $4}'`
   for f in $all; do
