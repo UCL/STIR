a4d76039c33d8cfb42705f89b360dc9624fc0138
Kris Thielemans <Kris.Thielemans@stir>
2012-02-07 07:09:44 +0000

    support for many more values of scatter_parameter

--- src/SimSET/scripts/conv_SimSET_projdata_to_STIR.sh
+++ src/SimSET/scripts/conv_SimSET_projdata_to_STIR.sh
@@ -1,10 +1,25 @@
 #! /bin/bash
 #
+# This script is used to convert SimSET output (weight file) into STIR projdata format.
+# Multiple files are written for the different scatter orders in the Simset file (if present).
+# The names of these files are currently not very predictable. Use SimSET_STIR_names.sh to
+# find out what we used.
+#
+# Most settings of SimSET's scatter_parameter are supported but not all. If you need
+# others, you can modify this script and contribute it to the STIR list.
+#
+# If SimSET's scatter_parameter was set to 3, sinograms for 2nd order scatter are added 
+# together (doubles02 and doubles11). Even higher order scatter is summed into a file
+# currently called "multiples" which is not a good name.
+# However, since SimSET 2.9, you probably want to use scatter_parameter 4 or 5 instead.
+#
+# Names do not reflect if randoms are included or not. If you use scatter_parameter>=6,
+# the total randoms are stored in a separate file (called randoms) in most cases.
 #
 # $Id$
 #
-#
-#  Copyright (C) 2005- $Date$, Hammersmith Imanet Ltd
+#  Copyright (C) 2005 - 2008/03/22, Hammersmith Imanet Ltd
+#  Copyright (C) 2011-07-01 - $Date$, Kris Thielemans
 #  This file is part of STIR.
 #
 #  This file is free software; you can redistribute it and/or modify
@@ -21,22 +36,18 @@
 #      
 # Author Kris Thielemans
 # Author Charalampos Tsoumpas 
-# This script is used to convert SimSET output into STIR projdata format.
-# Multiple files are written for the different scatter orders in the Simset file (if present).
-# Up to double scatter sinogram is currently in different files. Higher order scatter is
-# currently called "multiples" which is not a good name.
 
-# TODO: currently simset_filename and scanner_name are hard-wired in this script
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 3 ]; then
     echo "usage:"
-    echo "$0 max_ring_difference"
+    echo "$0 simset-weight-file max_ring_difference stir-scanner-name"
+    echo "Surround the scanner name with quotes if it contains spaces"
     exit 1
 fi
 
-max_ring_difference=$1
-simset_file=rec.weight
-scanner_name="ECAT 962"
+simset_file=$1
+max_ring_difference=$2
+scanner_name=$3
 
 set -e
 script_name="$0"
@@ -52,13 +63,17 @@ if [ ! -r ${simset_file} ]; then
   fi
 fi
 
+# find min and max scatter bins in the SimSET file
 min_scatter_bin_num=`${PRINTHEADER} ${simset_file} |grep "Binning: min number of scatters" |awk '{ print $6 }'`
 max_scatter_bin_num=`${PRINTHEADER} ${simset_file} |grep "Binning: max number of scatters" |awk '{ print $6 }'`
 
-scatter_parameter=`${PRINTHEADER} ${simset_file} |grep "Binning: scatter parameter" |awk '{ print $4 }'`
+# find scatter_parameter (maybe called scatter_randoms_parameter in the future)
+scatter_parameter=`${PRINTHEADER} ${simset_file} |grep "Binning: scatter.*parameter" |awk '{ print $4 }'`
 
 num_scatter_bins=$(( $max_scatter_bin_num - $min_scatter_bin_num + 1 ))
 
+# construct main part of the command line for the conv_SimSET_projdata_to_STIR executable
+# by parsing the header of the SimSET file
 conv_cmdline="conv_SimSET_projdata_to_STIR \
         ${simset_file} fl \
         `${PRINTHEADER} ${simset_file} |grep "Binning: number of AA bins" |awk '{ print $6 }'` \
@@ -66,70 +81,101 @@ conv_cmdline="conv_SimSET_projdata_to_STIR \
         `${PRINTHEADER} ${simset_file} |grep "Binning: number of Z bins" |awk '{ print $6 }'` \
         `${PRINTHEADER} ${simset_file} |grep "Binning: max Transaxial Distance" |awk '{ print $5 }'` \
         `${PRINTHEADER} ${simset_file} |grep "Binning: range on Z value" |awk '{ print $6 }'` \
-        \"${scanner_name}\" \
+        '${scanner_name}' \
         $max_ring_difference "
 all_scatter_bin_nums=`count $min_scatter_bin_num $max_scatter_bin_num`
 
+echo Executing ${conv_cmdline}
+
 case $scatter_parameter in 
 
-  1)
-  if [ $min_scatter_bin_num == 0 ]; then
-    ${conv_cmdline}  0 trues
-  fi
-  if [ $max_scatter_bin_num == 1 ]; then
-    ${conv_cmdline} 1 scatter
-  fi;;
+  0)
+    eval ${conv_cmdline}  0 total
+    ;;
+
+  1|6)
+    if [ $min_scatter_bin_num == 0 ]; then
+      eval ${conv_cmdline}  0 noscatter
+      if [ $max_scatter_bin_num -ge 1 ]; then
+        eval ${conv_cmdline} 1 scatter
+        if [ $scatter_parameter == 6 ]; then
+	  eval ${conv_cmdline} 2 randoms
+        fi
+      else
+        if [ $scatter_parameter == 6 ]; then
+          eval ${conv_cmdline} 1 randoms
+        fi
+      fi
+    fi
+   ;;
 
 
-  3) # scatter_parameter
+  3|8) # scatter_parameter
 
-for i in $all_scatter_bin_nums ;
-do for j in $all_scatter_bin_nums ;
-  do 
-     scatter_bin_num=$(( ${num_scatter_bins} * $i + $j ))
-     echo extracting scatter bin $scatter_bin_num
-     echo ${conv_cmdline} ${scatter_bin_num} blue${i}_pink${j};
-     ${conv_cmdline} ${scatter_bin_num} blue${i}_pink${j};
-  done 
-done 
-
-# get list of all contributing sinograms from blue/pink sinograms
-
-# first initialise lists to empty
-all_trues=
-all_singles=
-all_singles12=
-all_doubles1=
-all_doubles2=
-all_multiples=
-for i in $all_scatter_bin_nums
-  do for j in $all_scatter_bin_nums
-    do     
-      k=$(( $i + $j )) 
-      current=blue${i}_pink${j}.hs
-      if [ $k -eq 0 ]; then all_trues="$all_trues $current"; fi
-      if [ $k -eq 1 ]; then all_singles="$all_singles $current"; fi
-      if [ $k -eq 2 ]; then  if [ $i -eq 1 ]; then all_singles12="$all_singles12 $current";fi; fi;
-      if [ $k -eq 2 ]; then  if [ $j -eq 2 ]; then all_doubles1="$all_doubles1 $current";fi; fi;    
-      if [ $k -eq 2 ]; then  if [ $i -eq 2 ]; then all_doubles2="$all_doubles2 $current";fi; fi;
-      if [ $k -gt 2 ]; then all_multiples="$all_multiples $current"; fi      
+    for i in $all_scatter_bin_nums ;
+    do for j in $all_scatter_bin_nums ;
+      do 
+         scatter_bin_num=$(( ${num_scatter_bins} * $i + $j ))
+         echo extracting scatter bin $scatter_bin_num
+         echo ${conv_cmdline} ${scatter_bin_num} blue${i}_pink${j};
+         eval ${conv_cmdline} ${scatter_bin_num} blue${i}_pink${j};
+      done 
+    done 
+    
+    # get list of all contributing sinograms from blue/pink sinograms
+    
+    # first initialise lists to empty
+    all_trues=
+    all_singles=
+    all_singles12=
+    all_doubles1=
+    all_doubles2=
+    all_multiples=
+    for i in $all_scatter_bin_nums
+      do for j in $all_scatter_bin_nums
+        do     
+          k=$(( $i + $j )) 
+          current=blue${i}_pink${j}.hs
+          if [ $k -eq 0 ]; then all_trues="$all_trues $current"; fi
+          if [ $k -eq 1 ]; then all_singles="$all_singles $current"; fi
+          if [ $k -eq 2 ]; then  if [ $i -eq 1 ]; then all_singles12="$all_singles12 $current";fi; fi;
+          if [ $k -eq 2 ]; then  if [ $j -eq 2 ]; then all_doubles1="$all_doubles1 $current";fi; fi;    
+          if [ $k -eq 2 ]; then  if [ $i -eq 2 ]; then all_doubles2="$all_doubles2 $current";fi; fi;
+          if [ $k -gt 2 ]; then all_multiples="$all_multiples $current"; fi      
+       done
     done
-done
 
-# TODO we currently pipe all output to /dev/null because there's a lot of stuff because blocksizes do not work
-stir_math -s --add trues $all_trues >& /dev/null
-stir_math -s --add singles $all_singles >& /dev/null
-if [ ! -z "$all_singles12" ]; then
-  stir_math -s --add doubles11 $all_singles12 >& /dev/null
-fi
-if [ ! -z "$all_doubles1" ]; then
-  stir_math -s --add doubles20 $all_doubles1 $all_doubles2 >& /dev/null
-fi
-stir_math -s --add multiples $all_multiples >& /dev/null
+    # TODO we currently pipe all output to /dev/null because there's a lot of stuff because blocksizes do not work
+    stir_math -s --add trues $all_trues >& /dev/null
+    stir_math -s --add singles $all_singles >& /dev/null
+    if [ ! -z "$all_singles12" ]; then
+      stir_math -s --add doubles11 $all_singles12 >& /dev/null
+    fi
+    if [ ! -z "$all_doubles1" ]; then
+      stir_math -s --add doubles20 $all_doubles1 $all_doubles2 >& /dev/null
+    fi
+    stir_math -s --add multiples $all_multiples >& /dev/null
+    
+    rm -f blue*s
 
-rm -f blue*s
-  #end of  scatter_param == 3
-  ;;  
+    if [ $scatter_parameter == 8 ]; then
+      echo "WARNING: Not storing randoms yet"
+    fi
+    #end of  scatter_param == 3
+    ;;  
+
+  4|5|9|10) # scatter_parameter
+
+    for i in $all_scatter_bin_nums ;
+    do 
+        echo extracting scatter bin $scatter_bin_num  
+        echo ${conv_cmdline} ${scatter_bin_num} scatter${i};
+        eval ${conv_cmdline} ${scatter_bin_num} scatter${i};
+    done
+    if [ $scatter_parameter == 10 -o $scatter_parameter == 9  ]; then
+        eval ${conv_cmdline} $((max_scatter_bin_num - min_scatter_bin_num + 1)) randoms
+    fi 
+    ;;
 
   *)
     echo "scatter_parameter $scatter_parameter currently unsupported"
