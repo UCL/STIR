9dd3a2b394ce684da3b32624e8e82747095fc60d
Kris Thielemans <Kris.Thielemans@stir>
2012-11-19 10:04:53 +0000

    - use "flat" iterator for Arrays

--- src/swig/stirextra.py
+++ src/swig/stirextra.py
@@ -7,19 +7,8 @@ def toNumpy(image):
     image.get_regular_range(minind, maxind);
     sizes=maxind-minind+1;
 
-    npimage=numpy.zeros( (sizes[1], sizes[2], sizes[3]), dtype=numpy.float32);
-    #print 'start'
-    ind=stir.Int3BasicCoordinate()
-    for i1 in range(sizes[1]):
-        ind[1]=i1+minind[1];
-        #print ind[1]
-        for i2 in range(sizes[2]):
-            ind[2]=i2+minind[2];
-            #print '...', ind[2]
-            for i3 in range(sizes[3]):
-                ind[3]=i3+minind[3];
-                npimage[i1,i2,i3] = image[ind];
-    #print 'stop'
+    npimage=numpy.fromiter(image.flat(), dtype=numpy.float32);
+    npimage=npimage.reshape(sizes[1], sizes[2], sizes[3]);
 
     return npimage;
 
