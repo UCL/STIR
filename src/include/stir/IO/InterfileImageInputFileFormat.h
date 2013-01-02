c725c7b82004a352409d72af24ced11c0a2ce006
Kris Thielemans <Kris.Thielemans@stir>
2013-01-02 17:43:42 +0000

    call error() if interfile-reading failed (e.g. because it wasn't an image). This change means that read_from_file etc always call error() when the reading failed, as opposed to silently proceeding with a null-pointer.

--- src/include/stir/IO/InterfileImageInputFileFormat.h
+++ src/include/stir/IO/InterfileImageInputFileFormat.h
@@ -4,7 +4,8 @@
 #ifndef __stir_IO_InterfileInputFileFormat_h__
 #define __stir_IO_InterfileInputFileFormat_h__
 /*
-    Copyright (C) 2006- $Date$, Hammersmith Imanet Ltd
+    Copyright (C) 2006 - 2007-10-08, Hammersmith Imanet Ltd
+    Copyright (C) 2013-01-01 - $Date$, Kris Thielemans
     This file is part of STIR.
     This file is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
@@ -33,13 +34,13 @@
 #include "stir/utilities.h"
 #include "stir/DiscretisedDensity.h"
 #include "stir/VoxelsOnCartesianGrid.h"
-
+#include "stir/error.h"
+#include "stir/is_null_ptr.h"
 
 START_NAMESPACE_STIR
 
 //! Class for reading images in Interfile file-format.
 /*! \ingroup IO
-    \preliminary
 
 */
 class InterfileImageInputFileFormat :
@@ -63,16 +64,22 @@ public InputFileFormat<DiscretisedDensity<3,float> >
   virtual std::auto_ptr<data_type>
     read_from_file(std::istream& input) const
   {
-    return
-      std::auto_ptr<data_type>
-      (read_interfile_image(input));
+    std::auto_ptr<data_type> ret(read_interfile_image(input));
+    if (is_null_ptr(ret))
+      {
+	error("failed to read an Interfile image from stream");
+      }
+    return ret;
   }
   virtual std::auto_ptr<data_type>
     read_from_file(const std::string& filename) const
   {
-    return
-      std::auto_ptr<data_type>
-      (read_interfile_image(filename));
+    std::auto_ptr<data_type> ret(read_interfile_image(filename));
+    if (is_null_ptr(ret))
+      {
+	error("failed to read an Interfile image from file \"%s\"", filename.str());
+      }
+    return ret;
   }
 };
 END_NAMESPACE_STIR
