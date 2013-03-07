c3d19084f825762434b8e6d8651d531863d242e8
Kris Thielemans <Kris.Thielemans@stir>
2013-03-07 00:44:33 +0000

    update version to 2.3beta

--- src/local/scripts/distrib.sh
+++ src/local/scripts/distrib.sh
@@ -5,16 +5,16 @@
 : ${do_license:=1}
 : ${do_ChangeLog:=1}
 : ${do_doc:=1}
-: ${do_doxygen:=0}
+: ${do_doxygen:=1}
 : ${do_zip_source:=1}
 : ${do_recon_test_pack:=1}
 : ${do_transfer:=1}
 
-: ${do_website_final_version:=0}
+: ${do_website_final_version:=1}
 : ${do_website_sync:=0}
 
 set -e
-: ${VERSION:=2.2}
+: ${VERSION:=2.3beta}
 
 # for cvs2cl.pl
 : ${BRANCH:=trunk}
