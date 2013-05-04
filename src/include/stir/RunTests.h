73d7979507f9372c48ce7eacc7b9429f36fb4023
Kris Thielemans <Kris.Thielemans@stir>
2013-05-04 14:27:47 +0000

    fixed typos in documentation

--- src/include/stir/RunTests.h
+++ src/include/stir/RunTests.h
@@ -3,7 +3,8 @@
 //
 /*
     Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000- $Date$, Hammersmith Imanet Ltd
+    Copyright (C) 2000-2005, Hammersmith Imanet Ltd
+    Copyright (C) 2013-02-05 - $Date$, Kris Thielemans
     This file is part of STIR.
 
     This file is free software; you can redistribute it and/or modify
@@ -66,13 +67,17 @@ int main(int argc, char **argv, char **env)
   At present, the various check* functions are overloaded on most basic
   types, and in some STIR specific types such as VectorWithOffset. 
   This creates problems when the arguments are not exactly of one of those
-  types. For instance, when std::size_t is not one of <code>unsigned int</code>
-  or <code>unsigned long</code>, the compilation will break.
+  types, as then the compilation might break.
 
-  A potential solution for this would be to use member templates. It would
-  also remove some code repetition. However, you then need 
-  specialisation of member template, and a relatively recent compiler.
-  We leave this for later.
+  A potential solution for this would be to use member templates and the
+  addition of a 'generic' function as a template. However, then that
+  function would take precedence for other cases which are currently resolved
+  by overloading (such as when using stir::Array which is currently handled via 
+  stir::VectorWithOffset). 
+
+  In summary, if you need to check a different type that's not provided for,
+  either use check(), add an appropriate function to your derived class, or
+  if useful for other cases to this class of course.
 */
 class RunTests
 {
@@ -101,22 +106,32 @@ public:
   double get_tolerance() const;  
 
   //! Tests if true, str can be used to tell what you are testing
-  /*! \return if this test worked */
+  /*! \return if this test worked.
+   If the test failed, writes diagnostic to \c std::cerr (including value of str) and 
+   modifies internal variable \c everything_ok.
+  */
   bool check(const bool, const char * const str = "");
 
-  //! Tests equality, see check()
+  /*! \name Test equality
+    float/double/complex numbers are compared using \c tolerance.
+    \see check()
+  */
+  //@{
   bool check_if_equal(const double a, const double b, char const * const str = "");
-    bool check_if_equal(const short a, const short b, char const * const str = "");
-    bool check_if_equal(const unsigned short a, const unsigned short b, char const * const str = "");
-    bool check_if_equal(const int a, const int b, char const * const str = "");
-    bool check_if_equal(const unsigned int a, const unsigned int b, char const * const str = "");
-    bool check_if_equal(const long a, const long b, char const * const str = "");
-    bool check_if_equal(const unsigned long a, const unsigned long b, char const * const str = "");
+  // Note: due to current C++ overloading rules, we need copies for every integer type
+  bool check_if_equal(const short a, const short b, char const * const str = "");
+  bool check_if_equal(const unsigned short a, const unsigned short b, char const * const str = "");
+  bool check_if_equal(const int a, const int b, char const * const str = "");
+  bool check_if_equal(const unsigned int a, const unsigned int b, char const * const str = "");
+  bool check_if_equal(const long a, const long b, char const * const str = "");
+  bool check_if_equal(const unsigned long a, const unsigned long b, char const * const str = "");
 #ifdef BOOST_HAS_LONG_LONG
-    bool check_if_equal(const long long a, const long long b, char const * const str = "");
-    bool check_if_equal(const unsigned long long a, const unsigned long long b, char const * const str = "");
+  // necessary to cope with std::size_t for instance (on 64 bit systems)
+  bool check_if_equal(const long long a, const long long b, char const * const str = "");
+  bool check_if_equal(const unsigned long long a, const unsigned long long b, char const * const str = "");
 #endif
   // VC 6.0 needs definition of template members in the class def unfortunately.
+  //! check equality by calling check_if_equal on real and imaginary parts
   template <class T>
     bool check_if_equal(const std::complex<T> a, const std::complex<T> b, char const * const str = "")
     {
@@ -124,6 +139,7 @@ public:
 	check_if_equal(a.real(), b.real(), str) &&
 	check_if_equal(a.imag(), b.imag(), str);
     }
+  //! check equality by comparing ranges and calling check_if_equal on all elements
   template <class T>
     bool check_if_equal(const VectorWithOffset<T>& t1, const VectorWithOffset<T>& t2, 
                         const char * const str = "")
@@ -146,6 +162,7 @@ public:
     return true;
   }
   // VC 6.0 needs definition of template members in the class def unfortunately.
+  //! check equality by comparing size and calling check_if_equal on all elements
   template <class T>
 	  bool check_if_equal(const std::vector<T>& t1, const std::vector<T>& t2, 
                         const char * const str = "")
@@ -181,49 +198,7 @@ public:
     else
       return true;
   }
-
-  //! Tests if the value is 0, see check()
-  bool check_if_zero(const double a, const char * const str = "");
-  bool check_if_zero(const short a, char const * const str = "");
-  bool check_if_zero(const unsigned short a, char const * const str = "");
-  bool check_if_zero(const int a, char const * const str = "");
-  bool check_if_zero(const unsigned int a, char const * const str = "");
-  bool check_if_zero(const long a, char const * const str = "");
-  bool check_if_zero(const unsigned long a, char const * const str = "");
-#ifdef BOOST_HAS_LONG_LONG
-  bool check_if_zero(const long long a, char const * const str = "");
-  bool check_if_zero(const unsigned long long a, char const * const str = "");
-#endif
-
-  // VC needs definition of template members in the class def unfortunately.
-  template <class T>
-    bool check_if_zero(const VectorWithOffset<T>& t, const char * const str = "")
-  {    
-    for (int i=t.get_min_index(); i<= t.get_max_index(); i++)
-    {
-      if(!check_if_zero(t[i], str))
-      {
-        cerr << "(at VectorWithOffset<" << typeid(T).name() << "> first mismatch at index " << i << ")\n";
-        return false;
-      }
-    }
-    return true;
-  }
-  
-  template <int num_dimensions, class coordT> 
-  bool 
-  check_if_zero(const BasicCoordinate<num_dimensions, coordT>& a, const char * const str = "" )
-  {
-    if (norm(a) > tolerance)
-    {
-      cerr << "Error : Coordinate value is " << a<< " expected 0s. " << str  << endl;
-      everything_ok = false;
-      return false;
-    }
-    else
-      return true;
-  }
-
+  //! check equality by comparing norm(a-b) with tolerance
   template <int num_dimensions, class coordT> 
   bool 
   check_if_equal(const BasicCoordinate<num_dimensions, coordT>& a,
@@ -240,11 +215,65 @@ public:
     else
       return true;
   }
+  //@}
 
-private:
+  /*! \name Test if a value is zero
+    float/double/complex numbers are compared using \c tolerance.
+    \see check()
+  */
+  //@{
+  bool check_if_zero(const double a, const char * const str = "");
+  bool check_if_zero(const short a, char const * const str = "");
+  bool check_if_zero(const unsigned short a, char const * const str = "");
+  bool check_if_zero(const int a, char const * const str = "");
+  bool check_if_zero(const unsigned int a, char const * const str = "");
+  bool check_if_zero(const long a, char const * const str = "");
+  bool check_if_zero(const unsigned long a, char const * const str = "");
+#ifdef BOOST_HAS_LONG_LONG
+  bool check_if_zero(const long long a, char const * const str = "");
+  bool check_if_zero(const unsigned long long a, char const * const str = "");
+#endif
+
+  // VC needs definition of template members in the class def unfortunately.
+  //! use check_if_zero on all elements
+  template <class T>
+    bool check_if_zero(const VectorWithOffset<T>& t, const char * const str = "")
+  {    
+    for (int i=t.get_min_index(); i<= t.get_max_index(); i++)
+    {
+      if(!check_if_zero(t[i], str))
+      {
+        cerr << "(at VectorWithOffset<" << typeid(T).name() << "> first mismatch at index " << i << ")\n";
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  //! compare norm with tolerance
+  template <int num_dimensions, class coordT> 
+  bool 
+  check_if_zero(const BasicCoordinate<num_dimensions, coordT>& a, const char * const str = "" )
+  {
+    if (norm(a) > tolerance)
+    {
+      cerr << "Error : Coordinate value is " << a<< " expected 0s. " << str  << endl;
+      everything_ok = false;
+      return false;
+    }
+    else
+      return true;
+  }
+  //@}
+
+
+protected:
+  //! tolerance for comparisons with real values
   double tolerance;
+  //! variable storing current status
   bool everything_ok;
 
+  //! function that is called by some check_if_equal implementations. It just uses operator!=
   template<class T>
   bool
     check_if_equal_generic(const T& a, const T& b, char const * const str)
@@ -260,6 +289,7 @@ private:
         return true;
     }
 
+  //! function that is called by some check_if_zero implementations. It just uses operator!=
   template <class T> 
     bool 
     check_if_zero_generic(T a, const char * const str)
@@ -382,6 +412,7 @@ bool RunTests::check_if_zero(const unsigned long long a, char const * const str)
 {  return this->check_if_zero_generic(a,str);}
 #endif
 
+/*! check if \code fabs(a)<=tolerance \endcode */
 bool 
 RunTests::check_if_zero(const double a, const char * const str )
 {
