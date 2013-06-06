8b2ee0bc56142222d677cbe7f8e5f9756677f7f9
Kris Thielemans <k.thielemans@ucl.ac.uk>
2013-06-06 16:35:56 +0100

    fixed ECAT8 listmode input fileformat 'can_read' function

--- src/include/UCL/listmode/ECAT8_32bitListmodeInputFileFormat.h
+++ src/include/UCL/listmode/ECAT8_32bitListmodeInputFileFormat.h
@@ -4,7 +4,6 @@
 #ifndef __UCL_IO_ECAT8_32bitListmodeInputFileFormat_h__
 #define __UCL_IO_ECAT8_32bitListmodeInputFileFormat_h__
 /*
-    Copyright (C) 2006-2011, Hammersmith Imanet Ltd
     Copyright (C) 2013 University College London
 */
 /*!
@@ -41,36 +40,14 @@ public InputFileFormat<CListModeData >
     get_name() const
   {  return "ECAT8_32bit"; }
 
-  virtual bool
-    can_read(const FileSignature& signature,
-	     std::istream& input) const
-  {
-    return this->actual_can_read(signature, input);
-  }
-  virtual bool 
-    can_read(const FileSignature& signature,
-	     const std::string&  listmode_filename_prefix) const
-  {
-    // TODO need to do check that it's a siemens list file etc
-    return is_interfile_signature(signature.get_signature());
-  }
-
  protected:
   virtual 
     bool 
     actual_can_read(const FileSignature& signature,
 		    std::istream& input) const
   {
-    warning("can_read for ECAT8_32bit listmode data with istream not implemented %s:%s. Sorry",
-	  __FILE__, __LINE__);
-    return false;
-
-    if (strncmp(signature.get_signature(), "MATRIX", 6) != 0)
-      return false;
-
-    // TODO
-    // return (is_ECAT7_image_file(filename))
-    return true;
+    // TODO need to do check that it's a siemens list file etc
+    return is_interfile_signature(signature.get_signature());
   }
  public:
   virtual std::auto_ptr<data_type>
