d58ef0cf7ce90b63d60ba63fef56200aed143918
Kris Thielemans <Kris.Thielemans@stir>
2014-01-04 23:50:12 +0000

    added parameters in doxygen of ITKOutputFileFormat

--- src/include/stir/IO/ITKOutputFileFormat.h
+++ src/include/stir/IO/ITKOutputFileFormat.h
@@ -42,11 +42,19 @@ template <int num_dimensions, typename elemT> class DiscretisedDensity;
   writing code. This translation is currently incomplete however.
 
   ITK chooses the file format based on the extension of the filename. This is currently 
-  different from STIR. Therefore, this calss can be used to write Nifti (.nii), Teem (.nhdr),
+  different from STIR. Therefore, this class can be used to write Nifti (.nii), Teem (.nhdr),
   MetaIO (.mhdr), etc.
 
   In this class, we provide a default extension that will be 
   appended if a filename without extension is used.
+
+  \par Parameters
+  \verbatim
+  ITK Output File Format Parameters:=
+    default extension:= .nhdr ; current default value
+  End ITK Output File Format Parameters:=
+  \endverbatim
+
  */
 class ITKOutputFileFormat : 
   public RegisteredParsingObject<
