1fce3ca8847c02242e0bb9d9a9478b44a7e4932c
Kris Thielemans <Kris.Thielemans@stir>
2014-01-06 09:09:27 +0000

    Modify CMake files such that STIR MPI tests work properly - add new macro create_stir_mpi_test - use it for test_PoissonLogLikelihoodWithLinearModelForMeanAndProjData

--- src/stir_test_exe_targets.cmake
+++ src/stir_test_exe_targets.cmake
@@ -1,5 +1,5 @@
 # Copyright (C) 2011, Kris Thielemans
-# Copyright (C) 2013, University College London
+# Copyright (C) 2013-2014, University College London
 
 # This file is part of STIR.
 #
@@ -19,17 +19,29 @@
 # set dir_SIMPLE_TEST_EXE_SOURCES, 
 #     dir_INVOLVED_TEST_EXE_SOURCES_NO_REGISTRIES
 # and dir_INVOLVED_TEST_EXE_SOURCES
-# to a list of executables that need to be compiled.
+# to a list of executables that need to be compiled (see below for tests that use MPI).
 # Moreover, we will use ADD_TEST to create a test for
 # the files in dir_SIMPLE_TEST_EXE_SOURCES and dir_INVOLVED_TEST_EXE_SOURCES_NO_REGISTRIES
 # (assuming these don't need any parameters).
-# The user has to add ADD_TEST for the executables in dir_INVOLVED_TEST_EXE_SOURCES
+# You then have to use ADD_TEST for the executables in dir_INVOLVED_TEST_EXE_SOURCES
+# 
 #
-# Alternatively, the user can directly use the macros create_stir_involved_test and create_stir_test as follows:
+# Alternatively, you can directly use one of 3 macros as follows:
 #
-#   create_stir_test (test_something.cxx "buildblock;recon_buildblock" "")
+#     create_stir_test(sometest.cxx "${STIR_LIBRARIES}" "${STIR_REGISTRIES}")
+# without registries
+#     create_stir_test(sometest.cxx "${STIR_LIBRARIES}" "")
+# A test that uses any of the MPI routines (e.g. in the reconstruction library)
+#     create_stir_mpi_test(sometest.cxx "${STIR_LIBRARIES}" "${STIR_REGISTRIES}")
+# The above will execute the test with ${MPIEXEC_MAX_NUMPROCS} processors 
+# (probably defaulting to 2 depending on your CMake version)
+# A test for which you will use ADD_TEST yourself
+#     create_stir_involved_test(sometest.cxx "${STIR_LIBRARIES}" "${STIR_REGISTRIES}")
+
+# Even more advanced usage:
+#     create_stir_test (test_something.cxx "buildblock;IO;data_buildblock;recon_buildblock;buildblock" "")
 # or when it needs to be linked with a registry (but this usage is not recommended as the registry mechanism is likely to change)
-#   create_stir_test (test_something.cxx "buildblock;recon_buildblock" "${CMAKE_HOME_DIRECTORY}/buildblock/buildblock_registries.cxx")
+#     create_stir_test (test_something.cxx "buildblock" "${CMAKE_HOME_DIRECTORY}/buildblock/buildblock_registries.cxx")
 
 
 
@@ -37,6 +49,8 @@
 #message(status dir_SIMPLE_TEST_EXE_SOURCES: ${${dir_SIMPLE_TEST_EXE_SOURCES}})
 #message(status dir_SIMPLE_TEST_EXE_SOURCES_NO_REGISTRIES: ${dir_SIMPLE_TEST_EXE_SOURCES_NO_REGISTRIES})
 #message(status dir_SIMPLE_TEST_EXE_SOURCES_NO_REGISTRIES: ${${dir_SIMPLE_TEST_EXE_SOURCES_NO_REGISTRIES}})
+
+#### define macros
 
 macro (create_stir_involved_test source  libraries dependencies)
    get_filename_component(executable ${source} NAME_WE)
@@ -50,6 +64,16 @@ macro (create_stir_test source  libraries dependencies)
    ADD_TEST(${executable} ${CMAKE_CURRENT_BINARY_DIR}/${executable})
 endmacro( create_stir_test)
 
+# for executables that use MPI.
+# Note that if MPI is not enabled, these MPI macros will be empty, so this
+# macro will then effectively me the same as create_stir_test
+macro (create_stir_mpi_test source  libraries dependencies)
+   create_stir_involved_test(${source}  "${libraries}" "${dependencies}")
+   ADD_TEST(${executable}  ${MPIEXEC} ${MPIEXEC_NUMPROC_FLAG} ${MPIEXEC_MAX_NUMPROCS}  ${MPIEXEC_PREFLAGS} ${CMAKE_CURRENT_BINARY_DIR}/${executable} ${MPIEXEC_POSTFLAGS})
+endmacro( create_stir_mpi_test)
+
+
+#### use the above macros for each target in dir_SIMPLE_TEST_EXE_SOURCES etc
 
 foreach(executable ${${dir_SIMPLE_TEST_EXE_SOURCES}})
    create_stir_test (${executable}.cxx "${STIR_LIBRARIES}" "${STIR_REGISTRIES}")
