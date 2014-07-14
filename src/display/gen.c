7b832c467eb4e892a46d54b93426762e1138df5b
Kris Thielemans <k.thielemans@ucl.ac.uk>
2014-07-14 01:50:10 +0100

    convert C++ comment to C in gen.c (to be able to compile in strict ansi mode)

--- src/display/gen.c
+++ src/display/gen.c
@@ -53,7 +53,7 @@ int asknr (char str[],int minv,int maxv,int def)
   while(1)
   { printf ("\n%s [%d:%d D:%d]: ",str,minv,maxv,def);
     if (fgets(ptr,10,stdin) == NULL)
-      return def; // nothing read, so return default
+      return def; /* nothing read, so return default */
     ret=sscanf(ptr,"%d",&nnn);
     if (ret==0 || ret==EOF)
       return def;
