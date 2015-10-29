e56d98b037367a45ed99afb83480aea58ac22a19
evgueni-ovtchinnikov <evgueni.ovtchinnikov@stfc.ac.uk>
2015-10-29 17:29:32 +0000

    New get method added to ProjectorByBinPairUsingProjMatrixByBin

--- src/include/stir/recon_buildblock/ProjectorByBinPairUsingProjMatrixByBin.h
+++ src/include/stir/recon_buildblock/ProjectorByBinPairUsingProjMatrixByBin.h
@@ -70,6 +70,8 @@ public:
   ProjMatrixByBin const * 
     get_proj_matrix_ptr() const;
 
+  shared_ptr<ProjMatrixByBin> get_proj_matrix_sptr() const;
+
   void set_proj_matrix_sptr(const shared_ptr<ProjMatrixByBin>& sptr);
 
 private:
