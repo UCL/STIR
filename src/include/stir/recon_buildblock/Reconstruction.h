ae54cb6b2c6733013e33e857c0e7535252e743ca
Nikos E <nikos.efthimiou@gmail.com>
2016-09-05 14:01:26 +0100

    Removed set_additive&normalisation_sptr functions.

--- src/include/stir/recon_buildblock/Reconstruction.h
+++ src/include/stir/recon_buildblock/Reconstruction.h
@@ -177,21 +177,6 @@ public:
   //! post-filter
   shared_ptr<DataProcessor<TargetT> >  post_filter_sptr;
 
-  //!
-  //! \brief set_additive_proj_data_sptr
-  //! \author Nikos Efthimiou
-  //! \details In the case the reconstruction process is called from another
-  //! piece of code, the user should be able to set any additive sinogram
-  //!
-   virtual void set_additive_proj_data_sptr(const shared_ptr<ExamData>&) = 0;
-
-  //!
-  //! \brief set_normalisation_sptr
-  //! \author Nikos Efthimiou
-  //! \details In the case the reconstruction process is called from another
-  //! piece of code, the user should be able to set any additive sinogram
-  virtual void set_normalisation_sptr(const shared_ptr<BinNormalisation>&) = 0;
-
 protected:
 
   /*! 
