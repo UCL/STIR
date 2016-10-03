2692ca3125077d20ded1ed5b1446350bd0f6e1ac
Nikos E <nikos.efthimiou@gmail.com>
2016-10-03 17:15:46 +0100

    lm_to_projdata executable lets you know which input files supports
    I great option in order to know which recon_tests you can run.

--- src/listmode_utilities/lm_to_projdata.cxx
+++ src/listmode_utilities/lm_to_projdata.cxx
@@ -30,9 +30,11 @@
 */
 
 #include "stir/listmode/LmToProjData.h"
+#include "stir/IO/InputFileFormatRegistry.h"
 
 #ifndef STIR_NO_NAMESPACES
 using std::cerr;
+using std::endl;
 #endif
 
 USING_NAMESPACE_STIR
@@ -45,6 +47,15 @@ int main(int argc, char * argv[])
   if (argc!=1 && argc!=2) {
     cerr << "Usage: " << argv[0] << " [par_file]\n";
     exit(EXIT_FAILURE);
+  }
+  // Display the supported inputs, we need this in order to know
+  // which listmode files are supported
+  if (strcmp(argv[1], "--input-formats")==0)
+  {
+      cerr<<endl<<"Supported input file formats:\n";
+      InputFileFormatRegistry<CListModeData>::default_sptr()->
+              list_registered_names(cerr);
+      exit(EXIT_FAILURE);
   }
   LmToProjData application(argc==2 ? argv[1] : 0);
   application.process_data();
