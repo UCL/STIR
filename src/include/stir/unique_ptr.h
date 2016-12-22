5ca1d2b387939f7b26e4773a097af0ed16775253
Kris Thielemans <k.thielemans@ucl.ac.uk>
2016-12-22 07:27:27 +0000

    removed the BOOST unique_ptr work-around. it failed...

--- src/include/stir/unique_ptr.h
+++ src/include/stir/unique_ptr.h
@@ -5,9 +5,9 @@
   \brief Import of std::unique_ptr into the stir namespace, together with 
   work-arounds for other compilers.
 
-  If std::unique doesn't exist, attempt to use boost::movelib::unique_ptr. However, that needs
-  boost 1.59, so if you don't have that, we will define unique_ptr to auto_ptr. This is
-  dangerous of course. If you have problems with that, upgrade your boost version.
+  If std::unique doesn't exist, we will define unique_ptr to auto_ptr. This is
+  dangerous of course. If you have problems with that, tell your compiler to use at least C++11
+  (normally by adding the -std=c++11 flag)
 
   \author Kris Thielemans
 */         
@@ -42,23 +42,43 @@ namespace stir {
 #else
 // we need to replace it with something else
 
+// KT attempted to use boost::movelib::unique_ptr, which is claimed to be compatible with
+// std::unique_ptr but even for Cxx03 compilers. However, end 2016 this still generated errors on OSX Sierra
+// with CLang (it could not return a unique_ptr).
+// So, this is attempt is now disabled.
+#if 0
+#include <boost/version.hpp>
 #if (BOOST_VERSION >= 105700)
 // Boost is recent enough to have a drop-in replacement
-#include "boost/move/unique_ptr.hpp"
+#include <boost/move/unique_ptr.hpp>
 namespace stir {
   using boost::movelib::unique_ptr;
 }
-#else
+#endif
+#endif
+
 // desperate measures. We will use a #define to auto_ptr.
 // Caveat:
-// This trick is likely to break on (older?) OSX as it will generate conflicts between
-// the define and Apple's non-C++-11 compliant unique_ptr.
+// This trick is likely to break on OSX as it will generate conflicts between
+// this define and Apple's non-C++-11 compliant unique_ptr.
 // (Reason: BOOST_NO_CXX11_SMART_PTR is set, so we define unique_ptr here, but some include files will still
 // have unique_ptr with 2 template arguments, while auto_ptr needs only one).
-// Solution: tell your compiler to use C++-11 or later, upgrade Boost to 1.57 or newer, or upgrade your compiler...
+// Best solution: tell your compiler to use C++-11 or later (normally this means adding -std=c++11)
+
+// We first include a bunch of system files which use std::unique_ptr such that we don't have a conflict.
+// You might have to add a few more...
+#include <map>
+#include <vector>
+#include <string>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <deque>
+
 #define unique_ptr auto_ptr
 using std::auto_ptr;
-#endif
 #endif
 
 #endif
