9d7c030ff13f4bd0cc0988b3286f9fc0a64be1b7
Kris Thielemans <k.thielemans@ucl.ac.uk>
2018-02-01 22:41:55 +0000

    change name of variable to avoid compiler warnings

--- src/IO/InterfileHeaderSiemens.cxx
+++ src/IO/InterfileHeaderSiemens.cxx
@@ -64,9 +64,9 @@ InterfileHeaderSiemens::InterfileHeaderSiemens()
   PET_data_type_values.push_back("Normalisation");
   PET_data_type_values.push_back("Image");
   
-  for (int patient_position_index = 0; patient_position_index <= PatientPosition::PositionValue::unknown_position; ++patient_position_index)
+  for (int patient_position_idx = 0; patient_position_idx <= PatientPosition::PositionValue::unknown_position; ++patient_position_idx)
     {
-      PatientPosition pos((PatientPosition::PositionValue)patient_position_index);
+      PatientPosition pos((PatientPosition::PositionValue)patient_position_idx);
       patient_position_values.push_back(pos.get_position_as_string());
     }
   patient_position_index = static_cast<int>(patient_position_values.size()); //unknown
