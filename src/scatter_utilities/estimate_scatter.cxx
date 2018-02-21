aa7448ca035f21b5385d8e6353eb85e4c04e83c8
Nikos Efthimiou <nikos.efthimiou@gmail.com>
2018-02-21 18:23:24 +0000

    minor update

--- src/scatter_utilities/estimate_scatter.cxx
+++ src/scatter_utilities/estimate_scatter.cxx
@@ -37,19 +37,46 @@
 #include "stir/Succeeded.h"
 /***********************************************************/     
 
+static void print_usage_and_exit()
+{
+    std::cerr<<"This executable runs a Scatter simulation method based on the options "
+               "in a parameter file";
+    std::cerr<<"\nUsage:\n simulate_scatter scatter_simulation.par\n";
+    std::cerr<<"Example parameter file:\n\n"
+               "Scatter Simulation :=\n"
+               "Simulation method := Single Scatter Simulation\n"
+               "Scatter Simulation Parameters :=\n"
+               "template projdata filename :=\n"
+               "attenuation image filename := \n"
+               "attenuation image for scatter points filename := \n"
+               "activity image filename :=\n"
+               "output filename prefix := ${OUTPUT_PREFIX}\n"
+               "zoom XY for attenuation image for scatter points: := 1\n"
+               "zoom Z for attenuation image for scatter points: := 1\n"
+               "reduce number of detectors per ring by:= 1\n"
+               "reduce number of rings by:= 1\n"
+               "attenuation threshold := 0.01\n"
+               "random := 1\n"
+               "use cache := 1\n"
+               "End Scatter Simulation Parameters :=\n"
+               "End Scatter Simulation:="<< std::endl;
+               exit(EXIT_FAILURE);
+}
+/***********************************************************/
+
 int main(int argc, const char *argv[])                                  
 {         
-  stir::ScatterEstimation scatter_estimation;
+    stir::ScatterEstimation scatter_estimation;
 
-  if (argc==2)
+    if (argc==2)
     {
-      if (scatter_estimation.parse(argv[1]) == false)
-        return EXIT_FAILURE;
+        if (scatter_estimation.parse(argv[1]) == false)
+            return EXIT_FAILURE;
     }
-  else
-    scatter_estimation.ask_parameters();
+    else
+        print_usage_and_exit();
 
-  return scatter_estimation.process_data() == stir::Succeeded::yes ?
-    EXIT_SUCCESS : EXIT_FAILURE;
+    return scatter_estimation.process_data() == stir::Succeeded::yes ?
+                EXIT_SUCCESS : EXIT_FAILURE;
 }
 
