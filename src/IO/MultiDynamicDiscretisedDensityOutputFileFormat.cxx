4891b8125a4021dd50fbee29fdd64dca19f56c48
Richard Brown <richard.brown@ucl.ac.uk>
2018-08-02 16:56:45 +0100

    use boost as to_string is c++11

--- src/IO/MultiDynamicDiscretisedDensityOutputFileFormat.cxx
+++ src/IO/MultiDynamicDiscretisedDensityOutputFileFormat.cxx
@@ -98,7 +98,7 @@ actual_write_to_file(std::string& filename,
     // Create all the filenames
     VectorWithOffset<std::string> individual_filenames(1,int(density.get_num_time_frames()));
     for (int i=1; i<=int(density.get_num_time_frames()); i++)
-        individual_filenames[i] = filename + "_" + std::to_string(i);
+        individual_filenames[i] = filename + "_" + boost::lexical_cast<std::string>(i);
     
     // Write each individual image
     for (int i=1; i<=int(density.get_num_time_frames()); i++) {
