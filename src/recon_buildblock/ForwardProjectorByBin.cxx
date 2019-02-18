7f0a28249efb4ac56961ca03305dc55759c125a0
Kris Thielemans <k.thielemans@ucl.ac.uk>
2019-02-18 08:42:25 +0000

    corrected typo in warning message

--- src/recon_buildblock/ForwardProjectorByBin.cxx
+++ src/recon_buildblock/ForwardProjectorByBin.cxx
@@ -86,7 +86,7 @@ ForwardProjectorByBin::forward_project(ProjData& proj_data,
 {
   if (image.get_exam_info().imaging_modality.is_unknown()
       || proj_data.get_exam_info().imaging_modality.is_unknown())
-    warning("forward_project. Imaging modality unknown for either the image or the projection dat or both.\n"
+    warning("forward_project. Imaging modality unknown for either the image or the projection data or both.\n"
             "Going ahead anyway.");
   else if (image.get_exam_info().imaging_modality !=
       proj_data.get_exam_info().imaging_modality)
