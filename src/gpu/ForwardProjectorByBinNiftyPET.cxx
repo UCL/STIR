affc599acdf3b3c720e16bed389a79ee9ae2dc98
Richard Brown <richard.brown@ucl.ac.uk>
2019-07-25 15:32:48 +0100

    throw error if set_input hasn't been used

--- src/gpu/ForwardProjectorByBinNiftyPET.cxx
+++ src/gpu/ForwardProjectorByBinNiftyPET.cxx
@@ -295,6 +295,16 @@ forward_project(RelatedViewgrams<float>& viewgrams,
 
 void
 ForwardProjectorByBinNiftyPET::
+actual_forward_project(RelatedViewgrams<float>&, 
+      const DiscretisedDensity<3,float>&,
+        const int min_axial_pos_num, const int max_axial_pos_num,
+        const int min_tangential_pos_num, const int max_tangential_pos_num)
+{
+    throw std::runtime_error("Need to use set_input() if wanting to use ForwardProjectorByBinNiftyPET.");
+}
+
+void
+ForwardProjectorByBinNiftyPET::
 actual_forward_project(RelatedViewgrams<float>& viewgrams,
         const int min_axial_pos_num, const int max_axial_pos_num,
         const int min_tangential_pos_num, const int max_tangential_pos_num)
@@ -303,7 +313,6 @@ actual_forward_project(RelatedViewgrams<float>& viewgrams,
                            min_axial_pos_num, max_axial_pos_num,
                            min_tangential_pos_num, max_tangential_pos_num);
 }
-
 
 void
 ForwardProjectorByBinNiftyPET::
