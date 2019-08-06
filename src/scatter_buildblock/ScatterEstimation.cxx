4e02326cd54a05d188b77580b268e5ac67894a69
Nikos Efthimiou <NikEfth@users.noreply.github.com>
2019-08-06 15:23:02 +0100

    Update src/scatter_buildblock/ScatterEstimation.cxx
    Co-Authored-By: Kris Thielemans <KrisThielemans@users.noreply.github.com>

--- src/scatter_buildblock/ScatterEstimation.cxx
+++ src/scatter_buildblock/ScatterEstimation.cxx
@@ -1071,7 +1071,7 @@ process_data()
             //we do this by min-thresholding and then subtracting the threshold.
             //as long as the threshold is tiny, this will be ok
 
-            // On the same time we are going to save to a temp projdata file
+            // At the same time we are going to save to a temp projdata file
 
             shared_ptr<ProjData> temp_projdata ( new ProjDataInMemory (scaled_est_projdata_sptr->get_exam_info_sptr(),
                                                                        scaled_est_projdata_sptr->get_proj_data_info_sptr()));
