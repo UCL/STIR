33d7732fa13571694141cf8aebe10ea2b4030aa2
Richard Brown <richard.brown@ucl.ac.uk>
2019-08-16 14:24:02 +0100

    better test

--- src/test/gpu/test_ProjectorNiftyPET.cxx
+++ src/test/gpu/test_ProjectorNiftyPET.cxx
@@ -64,7 +64,7 @@ TestGPUProjectors(std::string image_filename, std::string sinogram_filename) :
 {
 }
 
-void project(double &time, shared_ptr<ProjDataInMemory> sino_sptr, shared_ptr<DiscretisedDensity<3,float> > image_sptr, const shared_ptr<const ProjDataInMemory> input_sino_sptr, const shared_ptr<const DiscretisedDensity<3,float> > input_image_sptr, ForwardProjectorByBin &fwrd_projector, BackProjectorByBin &back_projector, const std::string &name)
+void project(double &time, shared_ptr<ProjDataInMemory> &sino_sptr, shared_ptr<DiscretisedDensity<3,float> > &image_sptr, const shared_ptr<const ProjDataInMemory> input_sino_sptr, const shared_ptr<const DiscretisedDensity<3,float> > input_image_sptr, ForwardProjectorByBin &fwrd_projector, BackProjectorByBin &back_projector, const std::string &name)
 {
     image_sptr.reset(input_image_sptr->clone());
     sino_sptr = MAKE_SHARED<ProjDataInMemory>(*input_sino_sptr);
@@ -94,7 +94,7 @@ void project(double &time, shared_ptr<ProjDataInMemory> sino_sptr, shared_ptr<Di
     // Back project
     std::cerr << "\nDoing back projection using " << back_projector.get_registered_name() << "...\n";
     back_projector.set_up(input_sino_sptr->get_proj_data_info_sptr(),image_sptr);
-    back_projector.reset_output();
+    back_projector.start_accumulating_in_new_target();
     back_projector.back_project(*input_sino_sptr);
     back_projector.get_output(*image_sptr);
     timer.stop();
@@ -109,6 +109,31 @@ void project(double &time, shared_ptr<ProjDataInMemory> sino_sptr, shared_ptr<Di
     shared_ptr<OutputFileFormat<DiscretisedDensity<3,float> > > output_file_format_sptr =
             OutputFileFormat<DiscretisedDensity<3,float> >::default_sptr();
     output_file_format_sptr->write_to_file(name + "_back_projected",*image_sptr);
+}
+
+void get_min_max_in_proj_data(float &min, float &max, const shared_ptr<const ProjDataInMemory> sino_sptr)
+{
+    const int min_segment_num = sino_sptr->get_min_segment_num();
+    const int max_segment_num = sino_sptr->get_max_segment_num();
+    bool accumulators_initialized = false;
+    float accum_min=std::numeric_limits<float>::max(); // initialize to very large in case projdata is empty (although that's unlikely)
+    float accum_max=std::numeric_limits<float>::min();
+    double sum=0.;
+    for (int segment_num = min_segment_num; segment_num<= max_segment_num; ++segment_num) {
+        const SegmentByView<float> seg(sino_sptr->get_segment_by_view(segment_num));
+        const float this_max=seg.find_max();
+        const float this_min=seg.find_min();
+        sum+=static_cast<double>(seg.sum());
+        if(!accumulators_initialized) {
+            accum_max=this_max;
+            accum_min=this_min;
+            accumulators_initialized=true;
+        }
+        else {
+            if (accum_max<this_max) max=this_max;
+            if (accum_min>this_min) min=this_min;
+        }
+    }
 }
 
 void
@@ -140,17 +165,31 @@ run_projections()
     std::cout << "\tGPU: " << time_gpu << "\n";
     std::cout << "\tCPU: " << time_cpu << "\n";
 
+    // Min and max in images
     const float min_image_gpu = gpu_image_sptr->find_min();
     const float min_image_cpu = cpu_image_sptr->find_min();
     const float max_image_gpu = gpu_image_sptr->find_max();
     const float max_image_cpu = cpu_image_sptr->find_max();
     const float percent_diff_max_image = 100.f*(max_image_gpu-max_image_cpu)/max_image_cpu;
     std::cout << "\nMin/max in back projected images\n";
-    std::cout << "\tGPU: " << min_image_gpu << "/" << max_image_gpu << "\n";
-    std::cout << "\tCPU: " << min_image_cpu << "/" << max_image_cpu << "\n";
+    std::cout << "\tGPU: " << min_image_gpu << " / " << max_image_gpu << "\n";
+    std::cout << "\tCPU: " << min_image_cpu << " / " << max_image_cpu << "\n";
     std::cout << "\tDiff in max (%): " << percent_diff_max_image << "\n";
+
+    // Min and max in sinograms
+    float min_sino_gpu, min_sino_cpu, max_sino_gpu, max_sino_cpu;
+    get_min_max_in_proj_data(min_sino_gpu,max_sino_gpu,gpu_sino_sptr);
+    get_min_max_in_proj_data(min_sino_cpu,max_sino_cpu,cpu_sino_sptr);
+    const float percent_diff_max_sino = 100.f*(max_sino_gpu-max_sino_cpu)/max_sino_cpu;
+    std::cout << "\nMin/max in forward projected sinograms\n";
+    std::cout << "\tGPU: " << min_sino_gpu << " / " << max_sino_gpu << "\n";
+    std::cout << "\tCPU: " << min_sino_cpu << " / " << max_sino_cpu << "\n";
+    std::cout << "\tDiff in max (%): " << percent_diff_max_sino << "\n";
+
     if (std::abs(percent_diff_max_image) > 1)
-        throw std::runtime_error("not ok");
+        throw std::runtime_error("Images don't agree!");
+    if (std::abs(percent_diff_max_sino) > 1)
+        throw std::runtime_error("Sinograms don't agree!");
 }
 
 void
