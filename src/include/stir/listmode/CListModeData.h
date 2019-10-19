1baaeba0089d0545c26739b578183bce7b74576f
pwadhwa351 <medpwad@leeds.ac.uk>
2019-10-19 13:07:44 +0100

    Changes made in CMakeLists to resolve Travis error log

--- src/include/stir/listmode/CListModeData.h
+++ src/include/stir/listmode/CListModeData.h
@@ -243,6 +243,7 @@ protected:
   void set_proj_data_info_sptr(shared_ptr<ProjDataInfo>);
   //! Has to be set by the derived class
   //  shared_ptr<ExamInfo> exam_info_sptr;
+  shared_ptr<Scanner> scanner_sptr;
 private:
   //! Has to be initialised by the derived class
   shared_ptr<ProjDataInfo> proj_data_info_sptr;
