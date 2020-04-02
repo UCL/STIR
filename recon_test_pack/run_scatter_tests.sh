5886572e53446be407b80b2a2f3945d753ab8885
Kris Thielemans <k.thielemans@ucl.ac.uk>
2020-04-02 10:43:39 +0000

    relax numerical test in run_scatter_test.sh
    with default tolerance (0.0001) test was failing when compiling the same code in different ways

--- recon_test_pack/run_scatter_tests.sh
+++ recon_test_pack/run_scatter_tests.sh
@@ -57,7 +57,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo "===  compare result"
-compare_projdata my_scatter_cylinder.hs scatter_cylinder.hs > my_scatter_compare_projdata.log 2>&1
+compare_projdata -t .001 my_scatter_cylinder.hs scatter_cylinder.hs > my_scatter_compare_projdata.log 2>&1
 if [ $? -ne 0 ]; then
   echo "Error comparing scatter output."
   error_log_files="${error_log_files} my_scatter_compare_projdata.log"
