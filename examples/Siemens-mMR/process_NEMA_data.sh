574544706d09d806d5d0fbe6ab0073eab72be991
Kris Thielemans <k.thielemans@ucl.ac.uk>
2020-04-02 10:48:40 +0000

    uncomment unlisting in example script

--- examples/Siemens-mMR/process_NEMA_data.sh
+++ examples/Siemens-mMR/process_NEMA_data.sh
@@ -9,6 +9,6 @@ export pardir
 ### create projection data (sinograms) from listmode file
 # use 500s of data
 echo "1 500" > frames.fdef
-#INPUT=20170809_NEMA_60min_UCL.l.hdr FRAMES=frames.fdef $pardir/unlist_and_randoms.sh
+INPUT=20170809_NEMA_60min_UCL.l.hdr FRAMES=frames.fdef $pardir/unlist_and_randoms.sh
 
 atnimg=20170809_NEMA_MUMAP_UCL.v.hdr ECATNORM=20170809_NEMA_UCL.n.hdr $pardir/scatter_and_recon.sh
