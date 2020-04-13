e04c3af66405670c84ba0104a2171ae5d451f56f
Kris Thielemans <k.thielemans@ucl.ac.uk>
2020-04-13 00:30:28 +0000

    recon_test_pack/ run_scatter_test.sh  fixes
    We were using the environment variable RANDOM before, but this
    is a system variable that returns a random number. This caused
    failurs on OSX.

--- recon_test_pack/simulate_scatter.sh
+++ recon_test_pack/simulate_scatter.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 # A script to do a simplistic analytic simulation of scatter as used by the recon_test_pack
 #
-#  Copyright (C) 2013 University College London
+#  Copyright (C) 2013, 2020 University College London
 #  This file is part of STIR.
 #
 #  This file is free software; you can redistribute it and/or modify
@@ -17,7 +17,6 @@
 #  See STIR/LICENSE.txt for details
 #      
 # Authors Kris Thielemans
-#         Nikos Efthimiou
 # 
 
 if [ $# -ne 4 ]; then
@@ -31,7 +30,7 @@ org_atten_image=$3
 TEMPLATE=$4
 ATTEN_IMAGE=${org_atten_image}
 ZOOM_ATTEN_IMAGE=my_zoomed_${org_atten_image}
-RANDOM=0
+DORANDOM=0
 
 zoom_z=.16666667
 zoom_xy=0.25
@@ -43,7 +42,7 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-export OUTPUT_PREFIX ACTIVITY_IMAGE ATTEN_IMAGE ZOOM_ATTEN_IMAGE TEMPLATE RANDOM
+export OUTPUT_PREFIX ACTIVITY_IMAGE ATTEN_IMAGE ZOOM_ATTEN_IMAGE TEMPLATE DORANDOM
 
 simulate_scatter scatter_simulation.par 2> ${OUTPUT_PREFIX}_stderr.log
 if [ $? -ne 0 ]; then
