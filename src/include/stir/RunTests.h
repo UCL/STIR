3f290eb2064fe983054b349b72db23afa8d4849c
Ashley Gillman <ashley.gillman@csiro.au>
2020-08-10 15:15:39 +1000

    FIX: Forgot to push RunTests's VectorWithOffset iterator check

--- src/include/stir/RunTests.h
+++ src/include/stir/RunTests.h
@@ -134,6 +134,32 @@ public:
 	check_if_equal(a.imag(), b.imag(), str);
     }
   //! check equality by comparing ranges and calling check_if_equal on all elements
+  template <class T=float>
+    bool check_if_equal(
+      const typename VectorWithOffset<T>::iterator& t1_begin,
+      const typename VectorWithOffset<T>::iterator& t1_end,
+      const typename VectorWithOffset<T>::iterator& t2_begin,
+      const typename VectorWithOffset<T>::iterator& t2_end,
+      const std::string& str = "")
+  {
+    assert(t1_end >= t1_begin);
+    size_t size = t1_end - t1_begin;
+    if (t2_begin + size != t2_end) {
+      std::cerr << "Error: unequal ranges. " << str << std::endl;
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+      if(!check_if_equal(*(t1_begin + i), *(t2_begin + i), str))
+      {
+        std::cerr << "(at VectorWithOffset<" << typeid(T).name()
+                  << "> first mismatch at index " << i << ")\n";
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //! check equality by comparing ranges and calling check_if_equal on all elements
   template <class T>
     bool check_if_equal(const VectorWithOffset<T>& t1, const VectorWithOffset<T>& t2, 
                         const std::string& str = "")
@@ -148,15 +174,7 @@ public:
       return everything_ok = false;
     }
 
-    for (int i=t1.get_min_index(); i<= t1.get_max_index(); i++)
-    {
-      if(!check_if_equal(t1[i], t2[i], str))
-      {
-        std::cerr << "(at VectorWithOffset<" << typeid(T).name() << "> first mismatch at index " << i << ")\n";
-        return false;
-      }
-    }
-    return true;
+    check_if_equal(t1.begin(), t1.end(), t2.begin(), t2.end(), str);
   }
   // VC 6.0 needs definition of template members in the class def unfortunately.
   //! check equality by comparing size and calling check_if_equal on all elements
