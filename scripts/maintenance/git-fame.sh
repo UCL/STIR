6b59a0e42cd3b40c76885088e26a703a3292681c
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2020-11-17 20:28:43 +0000

    Update scripts/maintenance/git-fame.sh [ci skip]
    exclude crystal map
    
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>

--- scripts/maintenance/git-fame.sh
+++ scripts/maintenance/git-fame.sh
@@ -10,6 +10,6 @@
 #  -C  Detect inter-file line moves and copies [default: False].
 #
 # In particular, `-C` takes ~30x longer to run, but a lot of this has happened in STIR
-git fame -wMC --format json --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers' \
+git fame -wMC --format json --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers|crystal_map_front.txt' \
   | jq -c '{creators: [.data[] | {name: .[0]}]}' \
   | sed -r -e 's/(\{"name")/\n    \1/g' -e 's/:/: /g'
