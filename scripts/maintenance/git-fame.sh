6673eb34e2c234eac2df80a492b58a314d8e085d
Kris Thielemans <k.thielemans@ucl.ac.uk>
2020-11-17 22:22:19 +0000

    ignore Doxyfile [ci skip]

--- scripts/maintenance/git-fame.sh
+++ scripts/maintenance/git-fame.sh
@@ -10,6 +10,7 @@
 #  -C  Detect inter-file line moves and copies [default: False].
 #
 # In particular, `-C` takes ~30x longer to run, but a lot of this has happened in STIR
-git fame -wMC --format json --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers|crystal_map_front.txt' \
+git fame -wMC --format json --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers|crystal_map_front.txt|Doxyfile' \
+  | tee git-fame-output.txt \
   | jq -c '{creators: [.data[] | {name: .[0]}]}' \
   | sed -r -e 's/(\{"name")/\n    \1/g' -e 's/:/: /g'
