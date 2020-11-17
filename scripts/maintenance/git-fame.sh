48ea0c2f0010c2c6e87cb86d3f0670b92449592e
Kris Thielemans <KrisThielemans@users.noreply.github.com>
2020-11-17 20:04:33 +0000

    white space/comments in scripts/maintenance/git-fame.sh [ci skip]
    Co-authored-by: Casper da Costa-Luis <casper.dcl@physics.org>

--- scripts/maintenance/git-fame.sh
+++ scripts/maintenance/git-fame.sh
@@ -3,13 +3,13 @@
 # per contributor (using .mailmap)
 #
 # Suggested usage of git fame:
-#-w, --ignore-whitespace  Ignore whitespace when comparing the parent's
+#  -w, --ignore-whitespace  Ignore whitespace when comparing the parent's
 #                           version and the child's to find where the lines
 #                           came from [default: False].
 #  -M  Detect intra-file line moves and copies [default: False].
 #  -C  Detect inter-file line moves and copies [default: False].
 #
-# These slow it down dramatically, but a lot of this has happened in STIR
+# In particular, `-C` takes ~30x longer to run, but a lot of this has happened in STIR
 git fame -wMC --format json --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers' \
   | jq -c '{creators: [.data[] | {name: .[0]}]}' \
   | sed -r -e 's/(\{"name")/\n    \1/g' -e 's/:/: /g'
