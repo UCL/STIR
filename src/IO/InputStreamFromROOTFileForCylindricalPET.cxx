64b24cca630c8758bfbb9391c1170cc77449bc57
Robert Twyman <robbietuk@gmail.com>
2021-02-10 14:41:53 +0000

    Check on branches as get_records progresses

--- src/IO/InputStreamFromROOTFileForCylindricalPET.cxx
+++ src/IO/InputStreamFromROOTFileForCylindricalPET.cxx
@@ -68,38 +68,50 @@ Succeeded
 InputStreamFromROOTFileForCylindricalPET::
 get_next_record(CListRecordROOT& record)
 {
-
+    auto brentry = stream_ptr->LoadTree(static_cast<Long64_t>(current_position));
     while(true)
     {
-        if (current_position == nentries)
-            return Succeeded::no;
+      if (current_position == nentries)
+          return Succeeded::no;
 
-      auto brentry = stream_ptr->LoadTree(static_cast<Long64_t>(current_position));
+      brentry = stream_ptr->LoadTree(static_cast<Long64_t>(current_position));
+      current_position ++ ;
+
       if (brentry < 0)
         return Succeeded::no;
 
-      // InputStreamFromROOTFile
-      if (br_time1->GetEntry(brentry) == 0)
-        return Succeeded::no;
-      if (br_time2->GetEntry(brentry) == 0)
-        return Succeeded::no;
+      if (this->exclude_scattered) {
+        // Ensure events are no scattered
+        if (br_comptonPhantom1->GetEntry(brentry) == 0)
+          return Succeeded::no;
+        if (br_comptonPhantom2->GetEntry(brentry) == 0)
+          return Succeeded::no;
+        if (this->comptonphantom1 > 0 || this->comptonphantom2 > 0)
+          continue;
+      }
 
-      if (br_eventID1->GetEntry(brentry) == 0)
-        return Succeeded::no;
-      if (br_eventID2->GetEntry(brentry) == 0)
-        return Succeeded::no;
+      if (this->exclude_randoms) {
+        // Ensure events are from same eventID
+        if (br_eventID1->GetEntry(brentry) == 0)
+          return Succeeded::no;
+        if (br_eventID2->GetEntry(brentry) == 0)
+          return Succeeded::no;
+        if ((this->eventID1 != this->eventID2))
+          continue;
+      }
 
+      // Check energy information
       if (br_energy1->GetEntry(brentry) == 0)
         return Succeeded::no;
       if (br_energy2->GetEntry(brentry) == 0)
         return Succeeded::no;
+      if (this->get_energy1_in_keV() < this->low_energy_window ||
+              this->get_energy1_in_keV() > this->up_energy_window ||
+              this->get_energy2_in_keV() < this->low_energy_window ||
+              this->get_energy2_in_keV() > this->up_energy_window)
+          continue;
 
-      if (br_comptonPhantom1->GetEntry(brentry) == 0)
-        return Succeeded::no;
-      if (br_comptonPhantom2->GetEntry(brentry) == 0)
-        return Succeeded::no;
-
-      // Cylindrical PET
+      // Get positional ID information
       if (br_crystalID1->GetEntry(brentry) == 0)
         return Succeeded::no;
       if (br_crystalID2->GetEntry(brentry) == 0)
@@ -120,24 +132,7 @@ get_next_record(CListRecordROOT& record)
       if (br_rsectorID2->GetEntry(brentry) == 0)
         return Succeeded::no;
 
-
-//        if (stream_ptr->GetEntry(static_cast<Long64_t>(current_position)) == 0 )
-//            return Succeeded::no;
-
-        current_position ++ ;
-
-        if ( (this->comptonphantom1 > 0 || this->comptonphantom2 > 0) && this->exclude_scattered )
-            continue;
-        if ( (this->eventID1 != this->eventID2) && this->exclude_randoms)
-            continue;
-      //multiply here by 1000 to convert the list mode energy from MeV to keV
-        if (this->get_energy1_in_keV() < this->low_energy_window ||
-                this->get_energy1_in_keV() > this->up_energy_window ||
-                this->get_energy2_in_keV() < this->low_energy_window ||
-                this->get_energy2_in_keV() > this->up_energy_window)
-            continue;
-
-        break;
+      break;
     }
 
     int ring1 = static_cast<int>(crystalID1/crystal_repeater_y)
@@ -166,6 +161,11 @@ get_next_record(CListRecordROOT& record)
     // Add offset
     crystal1 += offset_dets;
     crystal2 += offset_dets;
+
+    if (br_time1->GetEntry(brentry) == 0)
+      return Succeeded::no;
+    if (br_time2->GetEntry(brentry) == 0)
+      return Succeeded::no;
 
     return
             record.init_from_data(ring1, ring2,
