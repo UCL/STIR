d63b51fdd1433594cf39b31e8f0e6a719f56cd2a
Robert Twyman <robbietuk@gmail.com>
2021-04-08 16:47:21 -0700

    Use STIR utilities to find the filename

--- src/utilities/find_ML_singles_from_delayed.cxx
+++ src/utilities/find_ML_singles_from_delayed.cxx
@@ -103,16 +103,7 @@ int main(int argc, char **argv)
       // write fan sums to file
       {
         std::string fan_sum_name = "fansums_for_";
-        {
-          // extract filename
-          char sep = '/';
-#ifdef _WIN32
-          sep = '\\';
-#endif
-          std::string fname = argv[2];
-          size_t i = fname.rfind(sep, fname.length());
-          fan_sum_name += fname.substr(i+1, fname.length() - i);
-        }
+        fan_sum_name += get_filename(argv[2]);
 	fan_sum_name.erase(fan_sum_name.begin() + fan_sum_name.rfind('.'), 
 			   fan_sum_name.end());
 	fan_sum_name += ".dat"; 
