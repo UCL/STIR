5499253897dc8e6c8a88523b9b85867dcc48202f
Kris Thielemans <k.thielemans@ucl.ac.uk>
2021-04-22 17:55:58 +0000

    add PARAPET license to the sphx identifier

--- scripts/maintenance/change-license.sh
+++ scripts/maintenance/change-license.sh
@@ -17,3 +17,6 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 find $* -type f -exec grep -q 'GNU.*General Public License' {} \; -exec sed -E -i -f $script_path/change-license.sed {} \;
+
+find $* -path .git -prune \
+     -o -name "*[xhlkc]" -type f  -exec grep -l PARAPET {} \; -exec sed -E -i -f $script_path/add-PARAPET-license.sed {} \;
