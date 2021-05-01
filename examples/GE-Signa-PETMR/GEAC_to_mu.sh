2d091894e534ab4cb7017f4e74cf338031aa3ff1
Kris Thielemans <k.thielemans@ucl.ac.uk>
2021-05-01 07:18:09 +0000

    check-in of basic files for GE PET/CT files

---
+++ examples/GE-Signa-PETMR/GEAC_to_mu.sh
@@ -0,0 +1,23 @@
+#! /bin/bash -e
+
+# Example script to create a mu-file in STIR units from a CTAC
+# could be adapted for MRAC if slopes are adjusted
+# Author: Kris Thielemans
+
+if [ $# -ne 3 ]; then
+  echo "Usage: `basename $0` output_filename CT_filename"
+  echo "This creates a projdata with the normalisation factors"
+  exit 1
+fi
+
+# directory with some standard .par files
+: ${pardir:=$(dirname $0)}
+
+# This default will not work for most. Adjust!
+: {CT_SLOPES_FILENAME:=$STIR_install/../../stir/ct_slopes.json}
+# note: variable name is used in .par
+export CT_SLOPES_FILENAME
+# TODO get kV from the CT dicom header
+kV=120
+export kV
+postfilter ctac.hv  "${CT}"  "${pardir}/GE_HUToMu.par"
--- examples/GE-Signa-PETMR/README.md
+++ examples/GE-Signa-PETMR/README.md
@@ -5,7 +5,8 @@ Check the [howto.md](howto.md) for a description. In addition, there are some
 (rather horrible) scripts that might help.
 
 Currently incomplete is the attenuation processing, e.g. conversion from GE MRAC (PIFA) to mu-values,
-or CTAC to mu. A script is provided to cope with flipping and rotation once that's done though.
+or CTAC to mu. A basic script is provided for CTAC to mu, but this needs adaptation.
+A script is provided to cope with flipping and rotation by registration once that's done.
 
 As an example, we also provide complete processing of the data from the GE PET/MR
 spatial calibration phantom (called VQC), available from [Zenodo](https://zenodo.org/record/3887517).
