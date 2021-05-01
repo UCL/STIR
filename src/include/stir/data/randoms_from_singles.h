eea7dc2f32189db30a73c411a196fa56249d2ac3
Kris Thielemans <k.thielemans@ucl.ac.uk>
2021-05-01 05:27:45 +0000

    correct decay handling in randoms_from_singles
    We now compute integrals (hopefully!) correctly for "double decay"
    of the randoms

--- src/include/stir/data/randoms_from_singles.h
+++ src/include/stir/data/randoms_from_singles.h
@@ -31,7 +31,23 @@ class SinglesRates;
 
   This uses the formula \f$ R_{ij}= \tau S_i S_j \f$ (with \f$\tau\f$ the \c coincidence_time_window)
   for finding the randoms-rate in terms of the
-  singles-rates. The function then takes duration properly into account.
+  singles-rates. The function then takes duration properly into account using the following
+  procedure.
+
+  We actually have total counts in the singles for sinograms and need total counts in the randoms.
+  Assuming the radioactivity distribution is stationary and only decays, and dropping indices \f$i,j\f$,
+  we get
+  \f[
+     R_{ij}(t) = \tau * S_i(0) S_j(0) exp (-2 \lambda t)
+  \f]
+  Integrating over time, and writing $r$ for the integral of $R$ etc
+  \f[
+     r_{ij} = \tau S_i(0) S_j(0) \int_t1^t2 dt\,exp (-2 \lambda t)
+  \f]
+  converting that to total singles $s$ in the time frame, we get
+  \f[
+     r_{ij} = \tau s_i s_j(0) \frac{ \int_t1^t2 dt\,exp (-2 \lambda t)}{\left(\int_t1^t2 dt\,exp (-\lambda t)\right)^2}
+  \f]
 
   \todo Dead-time is currently completely ignored.
   \todo The function currently assumes F-18 half-life.
