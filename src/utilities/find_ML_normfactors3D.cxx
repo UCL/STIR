8bb6bb78c8ba95462c80e2d5db925f7ae7f7f314
emikhay <ekaterina.mikhaylova@positrigo.com>
2021-11-17 12:56:56 +0100

    solved compilation error

--- src/utilities/find_ML_normfactors3D.cxx
+++ src/utilities/find_ML_normfactors3D.cxx
@@ -115,21 +115,34 @@ int main(int argc, char **argv)
     const int num_physical_detectors_per_ring =
             measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
                     get_num_detectors_per_ring() -num_transaxial_blocks*virtual_transaxial_crystals;
+    const int num_transaxial_buckets =
+            measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
+                    get_num_transaxial_buckets();
+    const int num_axial_buckets =
+            measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
+                    get_num_axial_buckets();
+    const int num_transaxial_blocks_per_bucket =
+            measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
+                    get_num_transaxial_blocks_per_bucket();
+    const int num_axial_blocks_per_bucket =
+            measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
+                    get_num_axial_blocks_per_bucket();
 
     int num_physical_transaxial_crystals_per_basic_unit =
             measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
                     get_num_transaxial_crystals_per_block()-virtual_transaxial_crystals;
-   // If there are multiple buckets, we increase the symmetry size to a bucket. Otherwise, we use a block.
-    if(get_num_transaxial_buckets() >1) {
-	    num_physical_transaxial_crystals_per_basic_unit *= scanner_stpr->get_num_transaxial_blocks_per_bucket() ;    
-    }	
+   // If there are multiple buckets, we increase the symmetry size to a bucket. Otherwise, we use a block.                                                                                                                                             
+    if(num_transaxial_buckets >1) {
+            num_physical_transaxial_crystals_per_basic_unit *= num_transaxial_blocks_per_bucket;
+    }
     int num_physical_axial_crystals_per_basic_unit =
             measured_data->get_proj_data_info_sptr()->get_scanner_sptr()->
                     get_num_axial_crystals_per_block()-virtual_axial_crystals;
-   // If there are multiple buckets, we increase the symmetry size to a bucket. Otherwise, we use a block.
-    if(get_num_axial_buckets() >1) {
-	    num_physical_axial_crystals_per_basic_unit *= scanner_stpr->get_num_axial_blocks_per_bucket() ;    
+   // If there are multiple buckets, we increase the symmetry size to a bucket. Otherwise, we use a block.                                                                                                                                             
+    if(num_axial_buckets >1) {
+            num_physical_axial_crystals_per_basic_unit *= num_axial_blocks_per_bucket;
     }
+
 
 
 
