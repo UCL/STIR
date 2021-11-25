9fea97f843dd79a4bd173ca7804f49373fd4b6a4
Ashley Gillman <ashley.gillman@csiro.au>
2021-11-25 23:14:48 +1000

    cleanup of tests and some fixes on ProjData, not yet running

--- src/buildblock/ProjData.cxx
+++ src/buildblock/ProjData.cxx
@@ -234,7 +234,7 @@ ProjDataInMemory
 ProjData::get_subset(const std::vector<int>& views) const
 {
   auto subset_proj_data_info_sptr =
-    std::make_shared<ProjDataInfoSubsetByView>(proj_data_info_sptr, views));
+    std::make_shared<ProjDataInfoSubsetByView>(proj_data_info_sptr, views);
   ProjDataInMemory subset_proj_data(exam_info_sptr, subset_proj_data_info_sptr);
 
   for (int segment_num=get_min_segment_num(); segment_num<=get_max_segment_num(); ++segment_num)
@@ -242,8 +242,7 @@ ProjData::get_subset(const std::vector<int>& views) const
       for (int subset_view_num=0; subset_view_num<views.size(); ++subset_view_num)
         {
           const Viewgram<float> viewgram = this->get_viewgram(views[subset_view_num], segment_num);
-          Viewgram<float> subset_viewgram = subset_proj_data_info_sptr-> get_empty_viewgram(subset_view_num, segment_num);
-          subset_viewgram.fill(viewgram);
+          Viewgram<float> subset_viewgram = Viewgram<float>(viewgram, subset_proj_data_info_sptr, subset_view_num, segment_num);
           if (subset_proj_data.set_viewgram(subset_viewgram) != Succeeded::yes)
             error("ProjData::get_subset failed to set a viewgram");
         }
