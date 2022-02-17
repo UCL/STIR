e208879e21f887d050e5657b0165573179493bd5
Robert Twyman <robbietuk@gmail.com>
2022-02-17 08:48:02 -0800

    [ci skip] Add generation of .hroot files from template and remove pre-created

--- recon_test_pack/ROOT_STIR_consistency/run_pretest_script.sh
+++ recon_test_pack/ROOT_STIR_consistency/run_pretest_script.sh
@@ -2,11 +2,16 @@
 
 for I in {1..12}
 do
-generate_image generate_image${I}.par
-cd Gate_macros
-sed -e s/SOURCENAME/test${I}/ main_D690_template.mac > main_D690_test${I}.mac
-Gate main_D690_test${I}.mac
-cd ..
+	generate_image generate_image${I}.par
+	cd Gate_macros
+	# Create main GATE macro files from template
+	sed -e s/SOURCENAME/test${I}/ main_D690_template.mac > main_D690_test${I}.mac
+
+	# Run Gate
+	Gate main_D690_test${I}.mac
+	cd ..
+	# Create hroot files from template
+	sed -e s/ROOTFILENAME/RootLM_D690_test${I}.root/ root_header_test_template.hroot > root_header_test${I}.hroot
 done;
 
 mv Gate_macros/*.root .
