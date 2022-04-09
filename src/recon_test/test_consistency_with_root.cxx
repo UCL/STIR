ecb4fd41612f509fdd7173fa6f11a457060eb665
Robert Twyman <robbietuk@gmail.com>
2022-04-09 14:07:19 -0700

    [ci skip] Significant fixes for test_consistency_with_root tests
    - Updates some documentation, 
    - removes `check_if_almost_equal` method because unused,  
    - renames `masx_coords_for_each_event` to `TOF_LOR_peak_value_coords`,
    -fix issue with `num_test` and `num_tests` being different variables,
    - fix issue with `empty()` being used to clear a std::vector (correct method is `clear()`,
    - increase `TOF_distance_threshold` to 2.5*norm(grid_spacing),
    - fix indexing issue in `test_results_nonTOF` and `test_results_TOF`, and
    - remove unused code block

--- src/recon_test/test_consistency_with_root.cxx
+++ src/recon_test/test_consistency_with_root.cxx
@@ -43,8 +43,6 @@
 
 #include "stir/warning.h"
 
-#include <stack>
-
 using std::cerr;
 using std::ifstream;
 
@@ -62,11 +60,10 @@ public:
       : coord(voxel_centre_v), value(value_v)
   {}
 
-  //! Center of mass position in mm
+  //! Coordinate position
   CartesianCoordinate3D<float> coord;
-  //! Mass of all weights
+  //! Weight value
   float value;
-  //! COM standard deviation.
 };
 
 
@@ -100,9 +97,6 @@ private:
   /*! Given a ProjMatrixElemsForOneBin (probabilities), test if the closest voxel in the LOR to the original_coords
    * is within nonTOF_distance_threshold distance. If it is, pass with true, otherwise fales.
    * @param probabilities ProjMatrixElemsForOneBin object of a list mode event
-   * @param test_discretised_density_sptr Density containing a point source.
-   * @param original_coords Precomputed coordinates of the point source
-   * @param tolerance Precomputed voxel sizes - used in setting nonTOF_distance_threshold
    * @return True if test passes, false if failed.
    */
   bool test_nonTOF_LOR_closest_approach(const ProjMatrixElemsForOneBin& probabilities);
@@ -115,18 +109,18 @@ private:
   //! Checks if original and calculated coordinates are close enough and saves data to file.
   void post_processing_TOF();
 
-  //! Modified version of check_if_equal for this test
-  bool check_if_almost_equal(const float a, const float b, const std::string str, const float tolerance);
-
   // Auxilary methods
   std::string get_root_header_filename() { return "pretest_output/root_header_test" + std::to_string(test_index) + ".hroot"; }
   std::string get_generate_image_par_filename() { return "SourceFiles/generate_image" + std::to_string(test_index) + ".par"; }
 
 
-  // Class VARIABLES
+  ///// Class VARIABLES /////
 
+  // Data set variables
   int test_index;
   int num_tests;
+
+  // Test results storage. Records if each test failed or not. E.g.`test_index` result is stored in `test_results_nonTOF[test_index -1]`
   std::vector<bool> test_results_nonTOF;
   std::vector<bool> test_results_TOF;
 
@@ -135,19 +129,19 @@ private:
   CartesianCoordinate3D<float> grid_spacing;    // Stored in class because of dynamic_cast
 
   int num_events_tested = 0;
-  const float failure_tolerance_nonTOF = 0.05;
-  const float failure_tolerance_TOF = 0.05;
 
   /// NON TOF VARIABLES
   //! A vector to store the coordinates of the closest approach voxels.
+  const float failure_tolerance_nonTOF = 0.05;
   std::vector<CartesianCoordinate3D<float>> nonTOF_closest_voxels_list;
   int num_failed_nonTOF_lor_events = 0;
   double nonTOF_distance_threshold;
 
   /// TOF VARIABLES
   //! A copy of the scanner timing resolution in mm
-  float gauss_sigma_in_mm;
-  std::vector<WeightedCoordinate> max_coords_for_each_event;
+  const float failure_tolerance_TOF = 0.05;
+  float gauss_sigma_in_mm; // Currently unused.
+  std::vector<WeightedCoordinate> TOF_LOR_peak_value_coords;
   double TOF_distance_threshold;
   int num_failed_TOF_lor_events = 0;
 };
@@ -156,12 +150,12 @@ void
 ROOTConsistencyTests::run_tests()
 {
   // Loop over each of the ROOT files in the test_data directory
-  int num_test = 8;
+  num_tests = 8;
   test_results_nonTOF = std::vector<bool> (num_tests, true);
   test_results_TOF = std::vector<bool> (num_tests, true);
 
   cerr << "Testing the view offset consistency between GATE/ROOT and STIR. \n";
-  for (int i = 1; i <= num_test; ++i)
+  for (int i = 1; i <= num_tests; ++i)
   {
     test_index = i;
     cerr << "\nTesting dataset " << std::to_string(test_index) << "...\n";
@@ -179,7 +173,7 @@ ROOTConsistencyTests::run_tests()
           "------------------------------------------\n"
           "\tTest Index\t|\tnonTOF\t|\tTOF\n"
           "------------------------------------------\n";
-  for (int j = 0; j < num_test; ++j)
+  for (int j = 0; j < num_tests; ++j)
     cerr << "\t\t" << std::to_string(j + 1) << "\t\t|\t"
          << ((test_results_nonTOF[j]) ? "Pass" : "Fail") << "\t|\t"
          << ((test_results_TOF[j]) ? "Pass" : "Fail") << std::endl;
@@ -189,13 +183,11 @@ void
 ROOTConsistencyTests::
 setup()
 {
-  {
-    // Create the point source (discretised_density_sptr) with GenerateImage from parameter file
-    // GenerateImage requires `const char* const par_filename`.
-    GenerateImage image_gen_application(get_generate_image_par_filename().c_str());
-    image_gen_application.compute();
-    discretised_density_sptr = image_gen_application.get_output_sptr();
-  }
+  // Create the point source (discretised_density_sptr) with GenerateImage from parameter file
+  // GenerateImage requires `const char* const par_filename`.
+  GenerateImage image_gen_application(get_generate_image_par_filename().c_str());
+  image_gen_application.compute();
+  discretised_density_sptr = image_gen_application.get_output_sptr();
 
   // Needs to be cast to VoxelsOnCartesianGrid to be able to calculate the centre of gravity
   const VoxelsOnCartesianGrid<float> &discretised_cartesian_grid =
@@ -206,17 +198,17 @@ setup()
   grid_spacing = discretised_cartesian_grid.get_grid_spacing();
 
   // For post_processing and debugging, create a list of the closest LOR coords
-  nonTOF_closest_voxels_list.empty();
-  max_coords_for_each_event.empty();
+  nonTOF_closest_voxels_list.clear();
+  TOF_LOR_peak_value_coords.clear();
 
-  // The number of LORs with closes approach greater than the threshold.
+  // Reset the number of processed and failed events
   num_failed_nonTOF_lor_events = 0;
   num_failed_TOF_lor_events = 0;
   num_events_tested = 0;
 
   // Failure conditioner and recording
   nonTOF_distance_threshold = 1.5 * norm(grid_spacing); // Using norm(grid_spacing) as a nonTOF_distance_threshold
-  TOF_distance_threshold = 1.5 * norm(grid_spacing); // Using norm(grid_spacing) as a tof_distance_threshold
+  TOF_distance_threshold = 2.5 * norm(grid_spacing); // Using norm(grid_spacing) as a tof_distance_threshold
 }
 
 void
@@ -308,7 +300,7 @@ post_processing_nonTOF()
   cerr << "\nNon-TOF number of failed events: " << num_failed_nonTOF_lor_events
        << "\tNumber of tested events: = " << num_events_tested << std::endl;
 
-  test_results_nonTOF[test_index] = check_if_less(num_failed_nonTOF_lor_events, failure_tolerance_nonTOF * num_events_tested,
+  test_results_nonTOF[test_index-1] = check_if_less(num_failed_nonTOF_lor_events, failure_tolerance_nonTOF * num_events_tested,
                                                   "The number of failed TOF events is more than the tolerance(" +
                                                       std::to_string(100*failure_tolerance_nonTOF)+ "%)");
 
@@ -366,7 +358,7 @@ test_TOF_max_lor_voxel(const ProjMatrixElemsForOneBin& probabilities)
   sum_weighted_position.coord /= num_max_value_elements;
 
   // Add the COM position to the list of max_value_voxels
-  max_coords_for_each_event.push_back(sum_weighted_position);
+  TOF_LOR_peak_value_coords.push_back(sum_weighted_position);
 
   // Check if the COM position is within the TOF_distance_threshold tolerance
   if (norm(original_coords - sum_weighted_position.coord) > TOF_distance_threshold)
@@ -382,7 +374,7 @@ post_processing_TOF()
   cerr << "\nTOF number of failed events: " << num_failed_TOF_lor_events
        << "\tNumber of tested events: = " << num_events_tested << std::endl;
 
-  test_results_TOF[test_index] = check_if_less(num_failed_TOF_lor_events, failure_tolerance_TOF * num_events_tested,
+  test_results_TOF[test_index-1] = check_if_less(num_failed_TOF_lor_events, failure_tolerance_TOF * num_events_tested,
                                                "The number of failed TOF events is more than the tolerance(" +
                                                    std::to_string(100*failure_tolerance_nonTOF)+ "%)");
 
@@ -394,143 +386,11 @@ post_processing_TOF()
     std::ofstream myfile;
     myfile.open(lor_pos_filename.c_str());
     myfile << original_coords.x() << " " << original_coords.y() << " " << original_coords.z() << std::endl;
-    for (std::vector<WeightedCoordinate>::iterator coord_entry = max_coords_for_each_event.begin();
-         coord_entry != max_coords_for_each_event.end(); ++coord_entry)
+    for (std::vector<WeightedCoordinate>::iterator coord_entry = TOF_LOR_peak_value_coords.begin();
+         coord_entry != TOF_LOR_peak_value_coords.end(); ++coord_entry)
       myfile << coord_entry->coord.x() << " " << coord_entry->coord.y() << " " << coord_entry->coord.z() << std::endl;
     myfile.close();
   }
-
-#if 0 // old code, will not run. Used for records but can be deleted...
-  // This old code was used to check if the COM of all the max voxels was a distance_threshold of the original coordinate.
-  // There were also checks on the standard deviation of the max voxels.
-  // This is not the case anymore because this kind of analysis is done via python scripts.
-
-  // creation of a file with all LOR maxima, to be able to plot them
-  std::ofstream myfile;
-  std::string max_lor_position_file_name =
-      current_root_header_filename.substr(0,current_root_header_filename.size()-3) + "_lor_tof_pos.txt";
-  myfile.open (max_lor_position_file_name.c_str());
-
-  // Reset all COM values to 0.f
-  CartesianCoordinate3D<float> max_lor_COM(0, 0, 0);
-  CartesianCoordinate3D<float> max_lor_COM(0, 0, 0);
-  max_lor_COM.x() = 0.f;
-  max_lor_COM.x() = 0.f;
-  max_lor_COM.z() = 0.f;
-  max_lor_COM.weighted_standard_deviation.x() = 0.f;
-  max_lor_COM.weighted_standard_deviation.y() = 0.f;
-  max_lor_COM.weighted_standard_deviation.z() = 0.f;
-
-
-  // computes centre of mass (weighted mean value) = sum(x_i * v_i)
-  for (std::vector<WeightedCoordinate>::iterator lor_element_ptr= max_coords_for_each_event.begin();
-       lor_element_ptr != max_coords_for_each_event.end();++lor_element_ptr)
-  {
-    max_lor_COM.coord.x() += lor_element_ptr->coord.x()*lor_element_ptr->value;
-    max_lor_COM.coord.y() += lor_element_ptr->coord.y()*lor_element_ptr->value;
-    max_lor_COM.coord.z() += lor_element_ptr->coord.z()*lor_element_ptr->value;
-    max_lor_COM.value += lor_element_ptr->value;
-
-    myfile << lor_element_ptr->coord.x() << " "
-           << lor_element_ptr->coord.y() << " "
-           << lor_element_ptr->coord.z() << " "
-           << lor_element_ptr->value << std::endl;
-
-  }
-  myfile.close();
-
-  // needs to divide by the weights
-  if (max_lor_COM.value != 0)
-  {
-    max_lor_COM.coord.x()= max_lor_COM.coord.x()/ max_lor_COM.value;
-    max_lor_COM.coord.y()= max_lor_COM.coord.y()/ max_lor_COM.value;
-    max_lor_COM.coord.z()= max_lor_COM.coord.z()/ max_lor_COM.value;
-  }
-  else
-  {
-    warning("Total weight of the centre of mass equal to 0. Please check your data.");
-    max_lor_COM.coord.x()=0;
-    max_lor_COM.coord.y()=0;
-    max_lor_COM.coord.z()=0;
-  }
-
-  // Compute the weighted standard deviation https://stats.stackexchange.com/a/6536
-  for (std::vector<WeightedCoordinate>::iterator lor_element_ptr= max_coords_for_each_event.begin();
-       lor_element_ptr != max_coords_for_each_event.end();++lor_element_ptr)
-  {
-    CartesianCoordinate3D<float> diff = lor_element_ptr->coord - max_lor_COM.coord;
-    max_lor_COM.weighted_standard_deviation.x() += lor_element_ptr->value * pow(diff.x(),2);
-    max_lor_COM.weighted_standard_deviation.y() += lor_element_ptr->value * pow(diff.y(), 2);
-    max_lor_COM.weighted_standard_deviation.z() += lor_element_ptr->value * pow(diff.z(), 2);
-  }
-
-  float M = static_cast<float>(max_coords_for_each_event.size());
-  const float weighted_stddev_scalar = (M-1)/M * max_lor_COM.value;
-  max_lor_COM.weighted_standard_deviation.x() = sqrt(max_lor_COM.weighted_standard_deviation.x()/weighted_stddev_scalar);
-  max_lor_COM.weighted_standard_deviation.y() = sqrt(max_lor_COM.weighted_standard_deviation.y()/weighted_stddev_scalar);
-  max_lor_COM.weighted_standard_deviation.z() = sqrt(max_lor_COM.weighted_standard_deviation.z()/weighted_stddev_scalar);
-
-  cerr << "Original coordinates: \t\t\t\t\t[x] " << original_coords.x() <<"\t[y] "
-       << original_coords.y() << "\t[z] " << original_coords.z() << std::endl;
-
-  cerr << "Centre of gravity coordinates: \t\t\t[x] " << max_lor_COM.coord.x() <<"\t[y] "
-       << max_lor_COM.coord.y() << "\t[z] " << max_lor_COM.coord.z() << std::endl;
-
-  cerr << "Centre of gravity coordinates (stddev): [x] " << max_lor_COM.weighted_standard_deviation.x() <<"\t[y] "
-       << max_lor_COM.weighted_standard_deviation.y() << "\t[z] " << max_lor_COM.weighted_standard_deviation.z() << "\n\n"<< std::endl;
-
-
-  // Check if max_lor_COM is within grid spacing nonTOF_distance_threshold with the original coordinates
-  check_if_almost_equal(original_coords.x(), max_lor_COM.coord.x(),
-                        "COM not within grid spacing (x) of Original",grid_spacing[3]);
-  check_if_almost_equal(original_coords.y(), max_lor_COM.coord.y(),
-                        "COM not within grid spacing (y) of Original",grid_spacing[2]);
-  check_if_almost_equal(original_coords.z(), max_lor_COM.coord.z(),
-                        "COM not within grid spacing (z) of Original",grid_spacing[1]);
-
-  // Check if weighted standard deviation is less than 2x the tof timing resolution
-  check_if_less(max_lor_COM.weighted_standard_deviation.x(), 2*gauss_sigma_in_mm,
-                "COM weighted stddev[x] is greater than 2x TOF timing resolution (mm)");
-  check_if_less(max_lor_COM.weighted_standard_deviation.y(), 2*gauss_sigma_in_mm,
-                "COM weighted stddev[y] is greater than 2x TOF timing resolution (mm)");
-  check_if_less(max_lor_COM.weighted_standard_deviation.z(), 2*gauss_sigma_in_mm,
-                "COM weighted stddev[z] is greater than 2x TOF timing resolution (mm)");
-
-
-  // Save the origin and Center of mass data to a separate file.
-  std::ofstream myfile;
-  std::string origin_and_COM_file_name = current_root_header_filename.substr(0,current_root_header_filename.size()-6) + "_tof_ORIG_and_COM.txt";
-  myfile.open (origin_and_COM_file_name.c_str());
-  // Original coordinates
-  myfile << original_coords.x() << " "
-         << original_coords.y() << " "
-         << original_coords.z() << std::endl;
-  // max LOR mean coordinates
-  myfile << max_lor_COM.coord.x() << " "
-         << max_lor_COM.coord.y() << " "
-         << max_lor_COM.coord.z() << std::endl;
-  // max LOR weighted stddev
-  myfile << max_lor_COM.weighted_standard_deviation.x() << " "
-         << max_lor_COM.weighted_standard_deviation.y() << " "
-         << max_lor_COM.weighted_standard_deviation.z() << std::endl;
-
-  myfile.close();
-#endif
-}
-
-bool
-ROOTConsistencyTests::
-check_if_almost_equal(const float a, const float b, const std::string str, const float tolerance)
-{
-  if ((fabs(a-b) > tolerance))
-  {
-    std::cerr << "Error : unequal values are " << a << " and " << b
-              << ". " << str << std::endl;
-    everything_ok = false;
-    return false;
-  }
-  else
-    return true;
 }
 
 END_NAMESPACE_STIR
