UCL/STIR

commit 8a83e48502cfafb1fcc710cca2882c10426fd25b

listmode reconstruction speed-up: Distribute sensitivity calculation and caching (#1030)
- Distribute sensitivity calculation
- Add (somewhat experimental?) capability for caching the listmode file during reconstruction, resulting in great speed-up, certainly with OPENMP on.

Co-authored-by: Kris Thielemans <KrisThielemans@users.noreply.github.com>

src/IO/InputStreamFromROOTFileForCylindricalPET.cxx
@@ -60,10 +60,18 @@ Succeeded
 InputStreamFromROOTFileForCylindricalPET::
 get_next_record(CListRecordROOT& record)
 {
+
+    int ring1, ring2, crystal1, crystal2;
+    bool return_no = false;
+
+#ifdef STIR_OPENMP
+#pragma omp critical(LISTMODEIO)
+#endif
+    {
     while(true)
     {
       if (current_position == nentries)
-          return Succeeded::no;
+          return_no = true;
 
       Long64_t brentry = stream_ptr->LoadTree(static_cast<Long64_t>(current_position));
       current_position ++ ;
@@ -92,20 +100,20 @@ get_next_record(CListRecordROOT& record)
       break;
     }
 
-    int ring1 = static_cast<int>(crystalID1/crystal_repeater_y)
+    ring1 = static_cast<int>(crystalID1/crystal_repeater_y)
             + static_cast<int>(submoduleID1/submodule_repeater_y)*get_num_axial_crystals_per_block_v()
             + static_cast<int>(moduleID1/module_repeater_y)*submodule_repeater_z*get_num_axial_crystals_per_block_v();
 
-    int ring2 = static_cast<int>(crystalID2/crystal_repeater_y)
+    ring2 = static_cast<int>(crystalID2/crystal_repeater_y)
             + static_cast<int>(submoduleID2/submodule_repeater_y)*get_num_axial_crystals_per_block_v()
             + static_cast<int>(moduleID2/module_repeater_y)*submodule_repeater_z*get_num_axial_crystals_per_block_v();
 
-    int crystal1 = rsectorID1  * module_repeater_y * submodule_repeater_y * get_num_transaxial_crystals_per_block_v()
+    crystal1 = rsectorID1  * module_repeater_y * submodule_repeater_y * get_num_transaxial_crystals_per_block_v()
             + (moduleID1%module_repeater_y) * submodule_repeater_y * get_num_transaxial_crystals_per_block_v()
             + (submoduleID1%submodule_repeater_y) * get_num_transaxial_crystals_per_block_v()
             + (crystalID1%crystal_repeater_y);
 
-    int crystal2 = rsectorID2 * module_repeater_y * submodule_repeater_y * get_num_transaxial_crystals_per_block_v()
+    crystal2 = rsectorID2 * module_repeater_y * submodule_repeater_y * get_num_transaxial_crystals_per_block_v()
             + (moduleID2%module_repeater_y) * submodule_repeater_y * get_num_transaxial_crystals_per_block_v()
             + (submoduleID2% submodule_repeater_y) * get_num_transaxial_crystals_per_block_v()
             + (crystalID2%crystal_repeater_y);
@@ -120,6 +128,11 @@ get_next_record(CListRecordROOT& record)
     crystal1 += offset_dets;
     crystal2 += offset_dets;
 #endif
+    }
+
+    if(return_no)
+        return Succeeded::no;
+
     return
             record.init_from_data(ring1, ring2,
                                   crystal1, crystal2,

recon_test_pack/OSMAPOSL_test_lm.par
@@ -24,6 +24,8 @@ PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin Par
 	;num_events_to_use := 100
 	recompute sensitivity :=1
 	use subset sensitivities:= 0
+	max cache size := ${cache} 
+	recompute cache := ${recompute_cache}
 	sensitivity filename:=  my_sens_t_lm_pr_seg2.hv
 	zoom :=  1
   xy output image size (in pixels) := 100
@@ -33,5 +35,5 @@ enforce initial positivity condition:=  1
 number of subsets:= 1
 number of subiterations:= 1
 save estimates at subiteration intervals:= 1
-output filename prefix := my_output_t_lm_pr_seg2
+output filename prefix := ${filename}
 END := 

recon_test_pack/run_test_listmode_recon.sh
@@ -78,8 +78,41 @@ if [ $? -ne 0 ]; then
 echo "ERROR running calculate_attenuation_coefficients. Check my_create_acfs.log"; exit 1;
 fi
 
-echo "=== Reconstruct listmode data"
-${MPIRUN} OSMAPOSL OSMAPOSL_test_lm.par > OSMAPOSL_test_lm.log 2>&1
+echo "=== Reconstruct listmode data without cache"
+export filename=my_output_t_lm_pr_seg2
+export cache=0
+export recompute_cache=0
+${MPIRUN} OSMAPOSL OSMAPOSL_test_lm.par > OSMAPOSL_test_lm_1.log 2>&1
+echo "=== Reconstruct listmode data with cache and store it in the disk"
+export filename=my_output_t_lm_pr_seg2_with_new_cache
+export cache=40000
+export recompute_cache=1
+${MPIRUN} OSMAPOSL OSMAPOSL_test_lm.par > OSMAPOSL_test_lm_2.log 2>&1
+
+echo "=== Compare reconstructed images with and without caching LM file"
+if compare_image my_output_t_lm_pr_seg2_1.hv my_output_t_lm_pr_seg2_with_new_cache_1.hv 2>my_output_comparison_stderr.log;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
+echo "=== Reconstruct listmode data with cache loaded from the disk"
+export filename=my_output_t_lm_pr_seg2_with_old_cache
+export cache=40000
+export recompute_cache=0
+${MPIRUN} OSMAPOSL OSMAPOSL_test_lm.par > OSMAPOSL_test_lm_3.log 2>&1
+
+echo "=== Compare reconstructed images without caching LM file and with loading cache from disk"
+if compare_image my_output_t_lm_pr_seg2_1.hv my_output_t_lm_pr_seg2_with_old_cache_1.hv 2>my_output_comparison_stderr.log;
+then
+echo ---- This test seems to be ok !;
+else
+echo There were problems here!;
+ThereWereErrors=1;
+fi
+
 echo "=== "
 # create sinograms
 echo "=== Unlist listmode data (for comparison)"

src/IO/InputStreamFromROOTFileForECATPET.cxx
@@ -58,10 +58,18 @@ Succeeded
 InputStreamFromROOTFileForECATPET::
 get_next_record(CListRecordROOT& record)
 {
+
+    int ring1, ring2, crystal1, crystal2;
+    bool return_no = false;
+
+#ifdef STIR_OPENMP
+#pragma omp critical(LISTMODEIO)
+#endif
+    {
   while(true)
   {
     if (current_position == nentries)
-      return Succeeded::no;
+      return_no = true;
 
     Long64_t brentry = stream_ptr->LoadTree(static_cast<Long64_t>(current_position));
     current_position ++ ;
@@ -82,16 +90,16 @@ get_next_record(CListRecordROOT& record)
     break;
   }
 
-    int ring1 = static_cast<Int_t>(crystalID1/crystal_repeater_y)
+    ring1 = static_cast<Int_t>(crystalID1/crystal_repeater_y)
             + static_cast<Int_t>(blockID1/ block_repeater_y)*crystal_repeater_z;
 
-    int ring2 = static_cast<Int_t>(crystalID2/crystal_repeater_y)
+    ring2 = static_cast<Int_t>(crystalID2/crystal_repeater_y)
             + static_cast<Int_t>(blockID2/block_repeater_y)*crystal_repeater_z;
 
-    int crystal1 = (blockID1%block_repeater_y) * get_num_transaxial_crystals_per_block_v()
+    crystal1 = (blockID1%block_repeater_y) * get_num_transaxial_crystals_per_block_v()
             + (crystalID1%crystal_repeater_y);
 
-    int crystal2 = (blockID2%block_repeater_y) * get_num_transaxial_crystals_per_block_v()
+    crystal2 = (blockID2%block_repeater_y) * get_num_transaxial_crystals_per_block_v()
             + (crystalID2%crystal_repeater_y);
 
     // GATE counts crystal ID =0 the most negative. Therefore
@@ -104,6 +112,12 @@ get_next_record(CListRecordROOT& record)
     crystal1 += offset_dets;
     crystal2 += offset_dets;
 #endif
+
+    }
+
+    if(return_no)
+        return Succeeded::no;
+
     return
             record.init_from_data(ring1, ring2,
                                   crystal1, crystal2,

src/include/stir/Bin.h
@@ -101,6 +101,14 @@ private :
   
 };
 
+//! A small bundle of a Bin and its additive correction value
+//!
+struct BinAndCorr
+{
+    Bin my_bin;
+    float my_corr;
+};
+
 
 
 END_NAMESPACE_STIR

src/include/stir/IO/InputStreamWithRecords.inl
@@ -73,27 +73,40 @@ get_next_record(RecordT& record) const
   if (is_null_ptr(stream_ptr))
     return Succeeded::no;
 
+  Succeeded ret = Succeeded::yes;
+
+#ifdef STIR_OPENMP
+#pragma omp critical(LISTMODEIO)
+#endif
+  {
   // rely on file caching by the C++ library or the OS
   assert(this->size_of_record_signature <= this->max_size_of_record);
   boost::shared_array<char> data_sptr(new char[this->max_size_of_record]);
-  char * data_ptr = data_sptr.get();
-  stream_ptr->read(data_ptr, this->size_of_record_signature);
+
+  stream_ptr->read(data_sptr.get(), this->size_of_record_signature);
   if (stream_ptr->gcount()<static_cast<std::streamsize>(this->size_of_record_signature))
-    return Succeeded::no; 
-  const std::size_t size_of_record = record.size_of_record_at_ptr(data_ptr, this->size_of_record_signature,options);
+  {
+    ret = Succeeded::no;
+  }
+  const std::size_t size_of_record = record.size_of_record_at_ptr(data_sptr.get(), this->size_of_record_signature,options);
   assert(size_of_record <= this->max_size_of_record);
   if (size_of_record > this->size_of_record_signature)
-    stream_ptr->read(data_ptr + this->size_of_record_signature,
+    stream_ptr->read(data_sptr.get() + this->size_of_record_signature,
                      size_of_record - this->size_of_record_signature);
   if (stream_ptr->eof())
-    return Succeeded::no; 
+  {
+    ret = Succeeded::no;
+  }
   else if (stream_ptr->bad())
     { 
       warning("Error after reading from list mode stream in get_next_record");
-      return Succeeded::no; 
+      ret = Succeeded::no;
     }
-  return 
-    record.init_from_data_ptr(data_ptr, size_of_record,options);
+  if(ret==Succeeded::yes)
+      ret = record.init_from_data_ptr(data_sptr.get(), size_of_record,options);
+  }
+
+      return ret;
 }
 
 

src/include/stir/recon_buildblock/PoissonLogLikelihoodWithLinearModelForMeanAndListModeDataWithProjMatrixByBin.h
@@ -26,9 +26,11 @@
 #include "stir/RegisteredParsingObject.h"
 #include "stir/recon_buildblock/PoissonLogLikelihoodWithLinearModelForMeanAndListModeData.h"
 #include "stir/recon_buildblock/ProjMatrixByBin.h" 
+#include "stir/ProjDataFromStream.h"
 #include "stir/ProjDataInMemory.h"
 #include "stir/recon_buildblock/ProjectorByBinPairUsingProjMatrixByBin.h"
 #include "stir/ExamInfo.h"
+#include "stir/recon_buildblock/distributable.h"
 START_NAMESPACE_STIR
 
 
@@ -101,6 +103,9 @@ protected:
  
   virtual void
     add_subset_sensitivity(TargetT& sensitivity, const int subset_num) const;
+
+  //! This function caches the listmode file. It is run during post-processing.
+  bool cache_listmode_file();
   
   //! Maximum ring difference to take into account
   /*! \todo Might be removed */
@@ -110,10 +115,10 @@ protected:
   shared_ptr<ProjMatrixByBin> PM_sptr;
 
   //! Stores the projectors that are used for the computations
-  shared_ptr<ProjectorByBinPairUsingProjMatrixByBin> projector_pair_sptr;
+  shared_ptr<ProjectorByBinPair> projector_pair_sptr;
 
   //! points to the additive projection data
-  shared_ptr<ProjDataInMemory> additive_proj_data_sptr;
+  shared_ptr<ProjData> additive_proj_data_sptr;
  
   std::string additive_projection_data_filename ; 
   //! ProjDataInfo
@@ -131,6 +136,27 @@ protected:
 
   void
     add_view_seg_to_sensitivity(const ViewSegmentNumbers& view_seg_nums) const;
+
+  //! Cache of the listmode file
+  std::vector<BinAndCorr>  record_cache;
+  //! This is the number of records to be cached. If this parameter is more than zero, then the
+  //! flag cache_lm_file will be set to true. The listmode file up to this size will be loaded in
+  //! the RAM, alongside with any additive sinograms.
+  unsigned long int cache_size;
+  //! This flag is true when cache_size is more than zero.
+  bool cache_lm_file;
+  //! On the first cached run, the cache will be written in the working path of the reconstruction.
+  //! If recompute_cache is set to zero then every consecutive reconstruction will use that cache file.
+  //! If you want to create a new, either delete the previous or set this 1. \todo multiple cache files
+  //! need to be supported!
+  bool recompute_cache;
+  //! The additive sinogram will not be read in memory
+  bool reduce_memory_usage;
+  //! If you know, or have previously checked that the number of subsets is balanced for your
+  //! Scanner geometry, you can skip future checks.
+  bool skip_balanced_subsets;
+  //! Path to read/write the cached listmode file. \todo add the ability to set a filename.
+  std::string cache_path;
 };
 
 END_NAMESPACE_STIR

src/include/stir/recon_buildblock/distributable.h
@@ -2,6 +2,7 @@
     Copyright (C) 2000 PARAPET partners
     Copyright (C) 2000 - 2011, Hammersmith Imanet Ltd
     Copyright (C) 2013, University College London
+    Copyright (C) 2022, University of Pennsylvania
     This file is part of STIR.
 
     SPDX-License-Identifier: Apache-2.0 AND License-ref-PARAPET-license
@@ -18,6 +19,7 @@
 
   \brief Declaration of the main functions that perform parallel processing
 
+  \author Nikos Efthimiou
   \author Alexey Zverovich
   \author Kris Thielemans
   \author Matthew Jacobson
@@ -25,6 +27,8 @@
   \author PARAPET project
 */
 #include "stir/shared_ptr.h"
+#include "stir/Bin.h"
+#include <vector>
 
 START_NAMESPACE_STIR
 
@@ -39,7 +43,7 @@ class ForwardProjectorByBin;
 class BackProjectorByBin;
 class ProjectorByBinPair;
 class DistributedCachingInformation;
-
+class ProjMatrixByBin;
 
 //! \name Task-ids currently understood by stir::DistributedWorker
 /*! \ingroup distributable */
@@ -173,6 +177,18 @@ void distributable_computation(
                                RPC_process_related_viewgrams_type * RPC_process_related_viewgrams,
                                DistributedCachingInformation* caching_info_ptr);
 
+/*!
+  \brief This function essentially implements a loop over a cached listmode file
+  \ingroup distributable
+!*/
+void LM_distributable_computation(
+        const shared_ptr<ProjMatrixByBin> PM_sptr,
+        const shared_ptr<ProjDataInfo>& proj_data_info_sptr,
+        DiscretisedDensity<3,float>* output_image_ptr,
+        const DiscretisedDensity<3,float>* input_image_ptr,
+        const std::vector<BinAndCorr>& record_ptr,
+        const int subset_num, const int num_subsets,
+        const bool has_add);
 
   /*! \name Tag-names currently used by stir::distributable_computation and related functions0
      \ingroup distributable

commit a4c82832a8bb4941041026b0ad3e5191c952098d

fix bug in ROOT loop introduced in #1030

src/IO/InputStreamFromROOTFileForCylindricalPET.cxx
@@ -60,9 +60,8 @@ Succeeded
 InputStreamFromROOTFileForCylindricalPET::
 get_next_record(CListRecordROOT& record)
 {
-
     int ring1, ring2, crystal1, crystal2;
-    bool return_no = false;
+    bool eof = false;
 
 #ifdef STIR_OPENMP
 #pragma omp critical(LISTMODEIO)
@@ -71,7 +70,10 @@ get_next_record(CListRecordROOT& record)
     while(true)
     {
       if (current_position == nentries)
-          return_no = true;
+        {
+          eof = true;
+          break;
+        }
 
       Long64_t brentry = stream_ptr->LoadTree(static_cast<Long64_t>(current_position));
       current_position ++ ;
@@ -130,7 +132,7 @@ get_next_record(CListRecordROOT& record)
 #endif
     }
 
-    if(return_no)
+    if(eof)
         return Succeeded::no;
 
     return