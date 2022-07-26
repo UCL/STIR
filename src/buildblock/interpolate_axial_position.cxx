90d99747bf15bc170d03ce54be25b418d0caacc5
nmdicom-recon <nmdicom-recon@feijoa.npl.co.uk>
2022-07-26 16:13:40 +0100

    interpolate_axial: fixing roundin issue causing out of range

--- src/buildblock/interpolate_axial_position.cxx
+++ src/buildblock/interpolate_axial_position.cxx
@@ -74,7 +74,7 @@ interpolate_axial_position(ProjData& proj_data_out,
           if (proj_data_in_info==proj_data_out_info)
               relative_pos=axial_pos;
           else
-              relative_pos=abs(round((m -m_offset_in)/ in_m_sampling));
+              relative_pos=abs(((m -m_offset_in)/ in_m_sampling));
           
            Sinogram<float> sino= proj_data_out.get_empty_sinogram(axial_pos,segment);
            const auto sino_in=proj_data_in.get_sinogram(relative_pos,0);
