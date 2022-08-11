0a201e3d6d62c2394c818bbf80d25cda479c52af
Daniel Deidda <daniel.deidda@npl.co.uk>
2022-08-11 09:55:23 +0100

    interpolate_axial: fixing rounding issue causing out of range (#1061)
    * interpolate_axial_position: fix nearest neighbour interpolation for block scanners using get_m (i.e. no longer rely on equidistant spacing)
    * adding scatter developments in the release notes
    
    Co-authored-by: Kris Thielemans <KrisThielemans@users.noreply.github.com>

--- src/buildblock/interpolate_axial_position.cxx
+++ src/buildblock/interpolate_axial_position.cxx
@@ -59,25 +59,52 @@ interpolate_axial_position(ProjData& proj_data_out,
     {
       error("interpolate_axial_position needs both projection to be of a scanner with the same ring radius");
     }
-  
-  float m_offset_in=proj_data_in_info.get_m(Bin(0,0,0,0));
-  
+//  create maps for the m coordinate depending on segment and axial_position
+  VectorWithOffset<VectorWithOffset<float> > m_in(proj_data_in.get_min_segment_num(),proj_data_in.get_max_segment_num());
+  VectorWithOffset<VectorWithOffset<float> > m_out(proj_data_out.get_min_segment_num(),proj_data_out.get_max_segment_num());
+
+  for (int segment=proj_data_in.get_min_segment_num();segment<=proj_data_in.get_max_segment_num();segment++)
+  {
+      m_in.at(segment).resize(proj_data_in.get_min_axial_pos_num(segment),proj_data_in.get_max_axial_pos_num(segment));
+
+      for (int axial_pos=proj_data_in.get_min_axial_pos_num(segment); axial_pos<=proj_data_in.get_max_axial_pos_num(segment);axial_pos++)
+      {
+          Bin bin(segment,0,axial_pos,0);
+          m_in.at(segment).at(axial_pos)=proj_data_in_info.get_m(bin);
+      }
+  }
 
   for (int segment=proj_data_out.get_min_segment_num();segment<=proj_data_out.get_max_segment_num();segment++)
+  {
+      m_out.at(segment).resize(proj_data_out.get_min_axial_pos_num(segment),proj_data_out.get_max_axial_pos_num(segment));
+
       for (int axial_pos=proj_data_out.get_min_axial_pos_num(segment); axial_pos<=proj_data_out.get_max_axial_pos_num(segment);axial_pos++)
       {
           Bin bin(segment,0,axial_pos,0);
-          int relative_pos=0;
-          float m = proj_data_out_info.get_m(bin);
-          float in_m_sampling= proj_data_in_info.get_sampling_in_m(bin);
-          
+          m_out.at(segment).at(axial_pos)=proj_data_out_info.get_m(bin);
+      }
+  }
+
+//now calculate the difference between the upsampled m (m_out) and the direct m (m_in) to be used for the nearest-neigbour interpolation
+  for (int segment=proj_data_out.get_min_segment_num();segment<=proj_data_out.get_max_segment_num();segment++)
+      for (int axial_pos=proj_data_out.get_min_axial_pos_num(segment); axial_pos<=proj_data_out.get_max_axial_pos_num(segment);axial_pos++)
+      {
+          VectorWithOffset<float> diff(proj_data_in.get_min_axial_pos_num(0),proj_data_in.get_max_axial_pos_num(0));
+          int axial_pos_in=0;
+
           if (proj_data_in_info==proj_data_out_info)
-              relative_pos=axial_pos;
-          else
-              relative_pos=abs(round((m -m_offset_in)/ in_m_sampling));
+              axial_pos_in=axial_pos;
+          else{
+              for (auto it=m_in.at(0).begin();it!=m_in.at(0).end();it++)
+              {
+                 diff.at(it-m_in.at(0).begin())=abs(m_out.at(segment).at(axial_pos) - *it);
+              }
+              auto result=std::min_element(diff.begin(),diff.end());
+              axial_pos_in=std::distance(diff.begin(), result);
+          }
           
            Sinogram<float> sino= proj_data_out.get_empty_sinogram(axial_pos,segment);
-           const auto sino_in=proj_data_in.get_sinogram(relative_pos,0);
+           const auto sino_in=proj_data_in.get_sinogram(axial_pos_in,0);
 #ifdef STIR_OPENMP
 #  if _OPENMP <201107
                       #pragma omp parallel for
