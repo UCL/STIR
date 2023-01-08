734b575e41fcff585395b7514af0c3a482349a85
Kris Thielemans <k.thielemans@ucl.ac.uk>
2023-01-08 21:50:07 +0000

    update git-fame.sh [ci skip]
    - exclude more files
    - don't bother with JSON output anymore as we no longer use zenodo.json

--- scripts/maintenance/git-fame.sh
+++ scripts/maintenance/git-fame.sh
@@ -2,6 +2,9 @@
 # git-fame allows printing "loc" (lines of code), "coms" (number of commits) and "fils" (number of files)
 # per contributor (using .mailmap)
 #
+# First use `git log --format='%aN <%aE>' | sort -u` to get list of authors and sort out
+# duplicates via .mailmap.
+#
 # Suggested usage of git fame:
 #  -w, --ignore-whitespace  Ignore whitespace when comparing the parent's
 #                           version and the child's to find where the lines
@@ -13,11 +16,8 @@
 #
 # Running from 2003 (rel_1_10) as various files were apparently not in git before that (they were in CVS)
 # The file therefore needs manual ordering (see below)
-git fame -wMC --since=2003-06-27 --format json --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers|crystal_map_front.txt|Doxyfile' \
-  | tee git-fame-output.txt \
-  | jq -c '{creators: [.data[] | {name: .[0]}]}' \
-  | sed -r -e 's/(\{"name")/\n    \1/g' -e 's/:/: /g'
-
+git fame -wMC --since=2003-06-27 --excl '\.(eps|root|ahv|hv|v|hs|s|scan|l|hdr|rtf|gz|if|pdf|safir|options|png|cls|sty)$|external_helpers|crystal_map|collimator.*txt|Doxyfile.in|LICENSE.txt|LICENSES' \
+  | tee git-fame-output.txt
 exit
 
 # git history doesn't tell us how many slocs the PARAPET people had
