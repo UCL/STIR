b2058b3d6900afc5d8d4d3b4172b1b898e177c61
Markus Jehl <markus.jehl@positrigo.com>
2023-03-31 13:56:41 +0000

    Removed old code that crept in during the whitespace reversion.

--- src/buildblock/interpolate_projdata.cxx
+++ src/buildblock/interpolate_projdata.cxx
@@ -201,11 +201,6 @@ interpolate_projdata(ProjData& proj_data_out,
             "(e.g. both arc-corrected or both not arc-corrected)");
     }
   
-  if (proj_data_in_info.get_scanner_sptr()->get_scanner_geometry()=="BlocksOnCylindrical")
-  {
-      interpolate_axial_position(proj_data_out,proj_data_in);
-      return Succeeded::yes;
-  }
   // check for the same ring radius
   // This is strictly speaking only necessary for non-arccorrected data, but
   // we leave it in for all cases.
