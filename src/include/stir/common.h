6efc4dda267c818eee8d04620910f107b5529b49
Markus Jehl <markus.jehl@positrigo.com>
2023-05-17 15:55:19 +0000

    Updated release notes and removed commented code from common.h

--- src/include/stir/common.h
+++ src/include/stir/common.h
@@ -291,8 +291,4 @@ inline NUMBER square(const NUMBER &x) { return x*x; }
 
 END_NAMESPACE_STIR
 
-// // include these such that we don't have to include them all over the place
-// #include "stir/error.h"
-// #include "stir/warning.h"
-
 #endif 
