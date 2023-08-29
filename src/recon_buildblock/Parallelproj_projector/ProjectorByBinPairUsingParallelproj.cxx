4408419d9ef49c8295814087564272eed5dab706
Kris Thielemans <k.thielemans@ucl.ac.uk>
2023-08-29 08:47:21 +0100

    fix problem in parallelproj projector pair set_up causing slow-down
    set_up() had the wrong signature, such that the helper was created twice
    and hence the start/end arrays computed twice

--- src/recon_buildblock/Parallelproj_projector/ProjectorByBinPairUsingParallelproj.cxx
+++ src/recon_buildblock/Parallelproj_projector/ProjectorByBinPairUsingParallelproj.cxx
@@ -69,8 +69,8 @@ ProjectorByBinPairUsingParallelproj()
 
 Succeeded
 ProjectorByBinPairUsingParallelproj::
-set_up(const shared_ptr<ProjDataInfo>& proj_data_info_sptr,
-       const shared_ptr<DiscretisedDensity<3,float> >& image_info_sptr)
+set_up(const shared_ptr<const ProjDataInfo>& proj_data_info_sptr,
+       const shared_ptr<const DiscretisedDensity<3,float> >& image_info_sptr)
 {
   _helper = std::make_shared<detail::ParallelprojHelper>(*proj_data_info_sptr, *image_info_sptr);
   dynamic_pointer_cast<ForwardProjectorByBinParallelproj>(this->forward_projector_sptr)
