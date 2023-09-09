1cd960984a9489d737da95c519a0e268497ced54
Kris Thielemans <k.thielemans@ucl.ac.uk>
2023-09-09 11:49:43 +0100

    added timings for ProjData and Poisson log-likelihoood

--- src/utilities/stir_timings.cxx
+++ src/utilities/stir_timings.cxx
@@ -27,6 +27,9 @@
 #  include "stir/recon_buildblock/Parallelproj_projector/ProjectorByBinPairUsingParallelproj.h"
 #endif
 #include "stir/recon_buildblock/ProjMatrixByBinUsingRayTracing.h"
+#include "stir/recon_buildblock/PoissonLogLikelihoodWithLinearModelForMeanAndProjData.h"
+//#include "stir/OSMAPOSL/OSMAPOSLReconstruction.h"
+#include "stir/recon_buildblock/distributable_main.h"
 #include "stir/warning.h"
 #include "stir/error.h"
 #include "stir/Verbosity.h"
@@ -42,19 +45,9 @@ print_usage_and_exit()
   std::cerr << "\nUsage:\nstir_timings [--runs num_runs]\\\n"
             << "\t[--skip-PP 1] [--skip-PMRT 1]\\\n"
             << "\t[--image image_filename]\\\n"
-            << "\t--template-projdata template_proj_data_filename\n";
-#if 0
-    std::cerr<<"The default projector uses the ray-tracing matrix.\n\n";
-  std::cerr<<"Example parameter file:\n\n"
-	   <<"Forward Projector parameters:=\n"
-	   <<"   type := Matrix\n"
-	   <<"   Forward projector Using Matrix Parameters :=\n"
-	   <<"      Matrix type := Ray Tracing\n"
-	   <<"         Ray tracing matrix parameters :=\n"
-	   <<"         End Ray tracing matrix parameters :=\n"
-	   <<"      End Forward Projector Using Matrix Parameters :=\n"
-	   <<"End:=\n";
-#endif
+            << "\t--template-projdata template_proj_data_filename\n\n"
+            << "Timings are reported as:\n"
+            << "name\tCPU_time_in_ms\twall-clock_time_in_ms\n";
   exit(EXIT_FAILURE);
 }
 
@@ -85,6 +78,42 @@ public:
     delete im;
   }
 
+  //! copy from output_proj_data_sptr to new Interfile file
+  void copy_proj_data_file_to_file()
+  {
+    ProjDataInterfile tmp(this->template_proj_data_sptr->get_exam_info_sptr(),
+                                     this->template_proj_data_sptr->get_proj_data_info_sptr(),
+                                     "my_timings_copy.hs");
+    tmp.fill(*this->output_proj_data_sptr);
+  }
+
+  //! copy from output_proj_data_sptr to memory object
+  void copy_proj_data_file_to_mem()
+  {
+    ProjDataInMemory tmp(this->template_proj_data_sptr->get_exam_info_sptr(),
+                         this->template_proj_data_sptr->get_proj_data_info_sptr(),
+                         /* initialise*/ false);
+    tmp.fill(*this->output_proj_data_sptr);
+  }
+
+  //! copy from mem_proj_data_sptr to new Interfile file
+  void copy_proj_data_mem_to_file()
+  {
+    ProjDataInterfile tmp(this->template_proj_data_sptr->get_exam_info_sptr(),
+                                     this->template_proj_data_sptr->get_proj_data_info_sptr(),
+                                     "my_timings_copy.hs");
+    tmp.fill(*this->mem_proj_data_sptr);
+  }
+
+  //! copy from output_proj_data_sptr to memory object
+  void copy_proj_data_mem_to_mem()
+  {
+    ProjDataInMemory tmp(this->template_proj_data_sptr->get_exam_info_sptr(),
+                         this->template_proj_data_sptr->get_proj_data_info_sptr(),
+                         /* initialise*/ false);
+    tmp.fill(*this->mem_proj_data_sptr);
+  }
+  
   void projector_setup()
   {
     this->projectors_sptr->set_up(this->template_proj_data_sptr->get_proj_data_info_sptr(), this->image_sptr);
@@ -92,19 +121,31 @@ public:
 
   void forward_file()
   {
-    this->projectors_sptr->get_forward_projector_sptr()->forward_project(*this->output_projdata_sptr, *this->image_sptr);
+    this->projectors_sptr->get_forward_projector_sptr()->forward_project(*this->output_proj_data_sptr, *this->image_sptr);
   }
   void forward_memory()
   {
-    this->projectors_sptr->get_forward_projector_sptr()->forward_project(*this->mem_projdata_sptr, *this->image_sptr);
+    this->projectors_sptr->get_forward_projector_sptr()->forward_project(*this->mem_proj_data_sptr, *this->image_sptr);
   }
   void back_file()
   {
-    this->projectors_sptr->get_back_projector_sptr()->back_project(*this->image_sptr, *this->output_projdata_sptr);
+    this->projectors_sptr->get_back_projector_sptr()->back_project(*this->image_sptr, *this->output_proj_data_sptr);
   }
   void back_memory()
   {
-    this->projectors_sptr->get_back_projector_sptr()->back_project(*this->image_sptr, *this->mem_projdata_sptr);
+    this->projectors_sptr->get_back_projector_sptr()->back_project(*this->image_sptr, *this->mem_proj_data_sptr);
+  }
+
+  void obj_func_set_up()
+  {
+    this->objective_function_sptr->set_up(this->image_sptr);
+  }
+
+  void obj_func_grad_no_sens()
+  {
+    auto im = this->image_sptr->clone();
+    this->objective_function_sptr->compute_sub_gradient_without_penalty_plus_sensitivity(*im, *this->image_sptr, 0);
+    delete im;
   }
 
   void run_it(TimedFunction f, const std::string& item, const unsigned runs = 1)
@@ -123,9 +164,17 @@ public:
     init();
     // this->run_it(&Timings::sleep, "sleep", runs*1);
     this->run_it(&Timings::copy_image, "copy_image", runs * 20);
-    this->projectors_sptr = this->pmrt_projectors_sptr;
+    this->output_proj_data_sptr->fill(1.F);
+    this->run_it(&Timings::copy_proj_data_mem_to_mem, "copy_proj_data_mem_to_mem", runs * 2);
+    this->run_it(&Timings::copy_proj_data_mem_to_file, "copy_proj_data_mem_to_file", runs * 2);
+    this->run_it(&Timings::copy_proj_data_file_to_mem, "copy_proj_data_file_to_mem", runs * 2);
+    this->run_it(&Timings::copy_proj_data_file_to_file, "copy_proj_data_file_to_file", runs * 2);
+    this->objective_function_sptr.reset(new PoissonLogLikelihoodWithLinearModelForMeanAndProjData<DiscretisedDensity<3,float>>);
+    this->objective_function_sptr->set_proj_data_sptr(this->mem_proj_data_sptr);
+    // this->objective_function.set_num_subsets(proj_data_sptr->get_num_views()/2);
     if (!this->skip_PMRT)
       {
+        this->projectors_sptr = this->pmrt_projectors_sptr;
         this->run_it(&Timings::projector_setup, "PMRT_projector_setup", runs * 10);
         this->run_it(&Timings::forward_file, "PMRT_forward_file_first", 1);
         this->run_it(&Timings::forward_file, "PMRT_forward_file", 1);
@@ -133,6 +182,10 @@ public:
         this->run_it(&Timings::back_file, "PMRT_back_file_first", 1);
         this->run_it(&Timings::back_file, "PMRT_back_file", 1);
         this->run_it(&Timings::back_memory, "PMRT_back_memory", 1);
+        this->objective_function_sptr->set_projector_pair_sptr(this->projectors_sptr);
+        this->run_it(&Timings::obj_func_set_up, "PMRT LogLik set_up", 1);
+        this->run_it(&Timings::obj_func_grad_no_sens, "PMRT LogLik grad_no_sens", 1);
+        
       }
 #ifdef STIR_WITH_Parallelproj_PROJECTOR
     if (!skip_PP)
@@ -145,9 +198,12 @@ public:
         this->run_it(&Timings::back_file, "PP_back_file_first", 1);
         this->run_it(&Timings::back_file, "PP_back_file", runs);
         this->run_it(&Timings::back_memory, "PP_back_memory", runs);
+        this->objective_function_sptr->set_projector_pair_sptr(this->projectors_sptr);
+        this->run_it(&Timings::obj_func_set_up, "PP LogLik set_up", 1);
+        this->run_it(&Timings::obj_func_grad_no_sens, "PP LogLik grad_no_sens", 1);
       }
 #endif
-    write_to_file("my_timings_backproj.hv", *this->image_sptr);
+    // write_to_file("my_timings_backproj.hv", *this->image_sptr);
   }
 
   void init()
@@ -185,22 +241,33 @@ public:
             this->exam_info_sptr->set_energy_information_from(this->template_proj_data_sptr->get_exam_info());
           }
       }
-    std::string output_filename = "my_timings.hs";
-    this->output_projdata_sptr
+
+    // projection data set-up
+    {
+      std::string output_filename = "my_timings.hs";
+      this->output_proj_data_sptr
         = std::make_shared<ProjDataInterfile>(this->exam_info_sptr, this->template_proj_data_sptr->get_proj_data_info_sptr(),
                                               output_filename, std::ios::in | std::ios::out | std::ios::trunc);
-    this->mem_projdata_sptr
+      this->mem_proj_data_sptr
         = std::make_shared<ProjDataInMemory>(this->exam_info_sptr, this->template_proj_data_sptr->get_proj_data_info_sptr());
+    }
+
+    // projector set-up
+    {
+      auto PM_sptr = std::make_shared<ProjMatrixByBinUsingRayTracing>();
+      PM_sptr->set_num_tangential_LORs(5);
+      this->pmrt_projectors_sptr = std::make_shared<ProjectorByBinPairUsingProjMatrixByBin>(PM_sptr);
 
 #ifdef STIR_WITH_Parallelproj_PROJECTOR
-    this->parallelproj_projectors_sptr = std::make_shared<ProjectorByBinPairUsingParallelproj>();
+      this->parallelproj_projectors_sptr = std::make_shared<ProjectorByBinPairUsingParallelproj>();
 #endif
+    }
   }
 
   // protected:
   shared_ptr<DiscretisedDensity<3, float>> image_sptr;
-  shared_ptr<ProjData> output_projdata_sptr;
-  shared_ptr<ProjDataInMemory> mem_projdata_sptr;
+  shared_ptr<ProjData> output_proj_data_sptr;
+  shared_ptr<ProjDataInMemory> mem_proj_data_sptr;
   shared_ptr<ProjectorByBinPair> projectors_sptr;
   shared_ptr<ProjectorByBinPairUsingProjMatrixByBin> pmrt_projectors_sptr;
 #ifdef STIR_WITH_Parallelproj_PROJECTOR
@@ -208,12 +275,16 @@ public:
 #endif
   shared_ptr<ProjData> template_proj_data_sptr;
   shared_ptr<ExamInfo> exam_info_sptr;
+  shared_ptr<PoissonLogLikelihoodWithLinearModelForMeanAndProjData<DiscretisedDensity<3,float>>> objective_function_sptr;
 };
 
 END_NAMESPACE_STIR
 
-int
-main(int argc, char* argv[])
+#ifdef STIR_MPI
+int stir::distributable_main(int argc, char **argv)
+#else
+int main(int argc, char **argv)
+#endif
 {
   using namespace stir;
   Verbosity::set(0);
@@ -251,27 +322,6 @@ main(int argc, char* argv[])
   Timings timings(image_filename, template_proj_data_filename);
   timings.skip_PMRT = skip_PMRT;
   timings.skip_PP = skip_PP;
-
-  if (0)
-    {
-#if 0
-      KeyParser parser;
-      parser.add_start_key("Forward Projector parameters");
-      parser.add_parsing_key("type", &forw_projector_sptr);
-      parser.add_stop_key("END"); 
-      parser.parse(argv[3]);
-      if (!timings.projectors_sptr)
-        {
-          std::cerr << "Failure parsing\n";
-          return EXIT_FAILURE;
-        }
-#endif
-    }
-  else
-    {
-      shared_ptr<ProjMatrixByBin> PM(new ProjMatrixByBinUsingRayTracing());
-      timings.pmrt_projectors_sptr = std::make_shared<ProjectorByBinPairUsingProjMatrixByBin>(PM);
-    }
 
   timings.run_all(num_runs);
   return EXIT_SUCCESS;
