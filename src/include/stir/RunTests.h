b19b7cfac2cff665e1b1bb9199e6ac3c0b362ec5
Kris Thielemans <k.thielemans@ucl.ac.uk>
2023-09-19 15:28:08 +0100

    added functionality to test DetectionPosition
    - addeed it to RunTests
    - added it to stream.h

--- src/include/stir/RunTests.h
+++ src/include/stir/RunTests.h
@@ -2,7 +2,7 @@
     Copyright (C) 2000 PARAPET partners
     Copyright (C) 2000-2005, Hammersmith Imanet Ltd
     Copyright (C) 2013, Kris Thielemans
-    Copyright (C) 2013, 2020, 2022 University College London
+    Copyright (C) 2013, 2020, 2022, 2023 University College London
     Copyright (C) 2018, University of Hull
     This file is part of STIR.
 
@@ -26,6 +26,7 @@
 #include "stir/IndexRange.h"
 #include "stir/stream.h"
 #include "stir/Bin.h"
+#include "stir/DetectionPosition.h"
 #include <iostream>
 #include <typeinfo>
 #include <vector>
@@ -119,6 +120,9 @@ public:
   bool check_if_equal(const unsigned long long a, const unsigned long long b, const std::string& str = "");
 #endif
   bool check_if_equal(const Bin& a, const Bin& b, const std::string& str = "")
+  {  return this->check_if_equal_generic(a,b,str);}
+  template <class T>
+  bool check_if_equal(const DetectionPosition<T>& a, const DetectionPosition<T>& b, const std::string& str = "")
   {  return this->check_if_equal_generic(a,b,str);}
 
   // VC 6.0 needs definition of template members in the class def unfortunately.
