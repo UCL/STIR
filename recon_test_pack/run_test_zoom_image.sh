a93870d6a06204e81b58b38125d3024fc4b2ba8f
Kris Thielemans <k.thielemans@ucl.ac.uk>
2024-01-05 07:38:12 +0000

    change defaults in ProjDataInfo::ask_parameters and remove span=0
    also add changes to this function to release notes

--- recon_test_pack/run_test_zoom_image.sh
+++ recon_test_pack/run_test_zoom_image.sh
@@ -143,17 +143,16 @@ fi
 echo "===  make attenuation image"
 generate_image  generate_atten_cylinder.par
 
-echo "===  create template sinogram (DSTE in 3D with max ring diff 2 to save time)"
-template_sino=my_DSTE_3D_rd2_template.hs
+echo "===  create template sinogram (DSTE in 3D with max ring diff 3 to save time)"
+template_sino=my_DSTE_3D_rd3_template.hs
 cat > my_input.txt <<EOF
 Discovery STE
 
 1
-410
 n
 
-0
-2
+
+3
 EOF
 create_projdata_template  ${template_sino} < my_input.txt > my_create_${template_sino}.log 2>&1
 if [ $? -ne 0 ]; then 
