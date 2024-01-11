7a7d712d5c0889d07bc7a400b4ead5be4eb0e8ef
rts <robert@prescientimaging.com>
2024-01-11 12:40:53 -0800

    [ci skip] Fix bug with not setting segment_data, introduced 40cf96452705d5657b0953526ee79bd095d6b17d

--- examples/python/projdata_visualisation/BackendTools/UIGroupboxProjdataDimensions.py
+++ examples/python/projdata_visualisation/BackendTools/UIGroupboxProjdataDimensions.py
@@ -89,7 +89,7 @@ class UIGroupboxProjDataDimensions:
         Because of the way the STIR segment data is handled, the segment_data needs to change first."""
         new_segment_num = self.UI_slider_spinboxes[ProjDataDims.SEGMENT_NUM].value()
         new_timing_pos = self.UI_slider_spinboxes[ProjDataDims.TIMING_POS].value()
-        self.stir_interface.get_segment_data(new_segment_num, new_timing_pos)
+        self.stir_interface.segment_data = self.stir_interface.get_segment_data(new_segment_num, new_timing_pos)
         self.UI_controller_UI_change_trigger()
 
     def axial_pos_refresh(self):
