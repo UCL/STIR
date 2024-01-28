168af0b92d70d4821968b8ac26ea2e4e7bd85ac7
Kris Thielemans <k.thielemans@ucl.ac.uk>
2024-01-28 08:35:24 +0000

    removed code deprecated functions for exptending/interpolating projdata
    They were already disabled due to check on STIR_VERSION.
    
    extend_sinogram_in_views, extend_segment_in_views,interpolate_axial_position

--- src/include/stir/extend_projdata.h
+++ src/include/stir/extend_projdata.h
@@ -30,15 +30,6 @@ tangential position, the values are extrapolated by nearest neighbour known valu
 
 This is probably only useful before calling interpolation routines, or for FORE.
 */
-#if STIR_VERSION < 060000
-  STIR_DEPRECATED Array<3,float>
-  extend_segment_in_views(const SegmentBySinogram<float>& sino, 
-		const int min_view_extension, const int max_view_extension);
-    
-  STIR_DEPRECATED Array<2,float>
-  extend_sinogram_in_views(const Sinogram<float>& sino,
-		const int min_view_extension, const int max_view_extension);
-#endif
 
 /*!
  \brief Generic function to extend a segment in any or all directions. Axially and
