2b760a8f86ffd3e39f0da4786b0c3d674cb2a969
Dimitra-Kyriakopoulou <dimitra.kyriakopoulou.13@ucl.ac.uk>
2024-05-10 21:51:08 +0300

    Update SRT2D.cxx

--- src/analytic/SRT2D/SRT2D.cxx
+++ src/analytic/SRT2D/SRT2D.cxx
@@ -1,21 +1,38 @@
 //
-// $Id: SRT2D.cxx $
 //
+/*! 
+  \file 
+  \ingroup FBP2D
+  \ingroup main_programs
+  \brief Main program for FBP2D reconstruction 
+  \author Kris Thielemans
+*/
+/*
+    Copyright (C) 2003- 2004, Hammersmith Imanet
 
+    This file is part of STIR.
+
+    SPDX-License-Identifier: Apache-2.0
+
+    See STIR/LICENSE.txt for details
+*/
 
 #include "stir/analytic/SRT2D/SRT2DReconstruction.h"
 #include "stir/Succeeded.h"
+#ifndef PARALLEL
+#define Main  main
+#else
+#define Main  master_main
+#endif
 
 USING_NAMESPACE_STIR
     
-int main(int argc, char **argv)
+int Main(int argc, char **argv)
 {
-    SRT2DReconstruction reconstruction_object(argc>1?argv[1]:"");
+    SRT2DReconstruction 
+      reconstruction_object(argc>1?argv[1]:"");
 
 
   return reconstruction_object.reconstruct() == Succeeded::yes ?
            EXIT_SUCCESS : EXIT_FAILURE;
-} 
-  
-
-
+}
