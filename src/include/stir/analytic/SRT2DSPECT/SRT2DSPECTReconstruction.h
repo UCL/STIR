4afbc3ee2e6aecd1e0a58addd3640092084a27cd
Dimitra-Kyriakopoulou <dimitra.kyriakopoulou.13@ucl.ac.uk>
2024-05-15 20:52:24 +0300

    Update SRT2DSPECTReconstruction.h
    Code clean-up

--- src/include/stir/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.h
+++ src/include/stir/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.h
@@ -10,7 +10,6 @@
 #include "stir/VoxelsOnCartesianGrid.h"
 #include "stir/ProjDataInfoCylindricalArcCorr.h" 
 #include "stir/ArcCorrection.h"
-#include "stir/analytic/FBP2D/RampFilter.h"
 #include "stir/SSRB.h"
 #include "stir/ProjDataInMemory.h"
 #include "stir/Array.h"
@@ -28,11 +27,8 @@
 
 
 #include "stir/recon_buildblock/AnalyticReconstruction.h"
-//#include "stir/recon_buildblock/BackProjectorByBin.h"
 #include "stir/RegisteredParsingObject.h"
-#include "stir/analytic/FBP2D/RampFilter.h"
 #include <string>
-#include <vector>
 #include "stir/shared_ptr.h"
 #ifndef STIR_NO_NAMESPACES
 using std::string;
@@ -63,7 +59,7 @@ class SRT2DSPECTReconstruction :
   // work-around swig problem. It gets confused when using a private (or protected)
   // typedef in a definition of a public typedef/member
  public:
-#else
+#else  
  private: 
 #endif  
     typedef DiscretisedDensity < 3,float> TargetT;
@@ -77,13 +73,10 @@ public:
     \brief Constructor, initialises everything from parameter file, or (when
     parameter_filename == "") by calling ask_parameters().
   */
-  explicit 
+  explicit  
     SRT2DSPECTReconstruction(const std::string& parameter_filename);
 
- SRT2DSPECTReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v, const float thres_restr_bound_v=-pow(10,6)); //, const int num_segments_to_combine=-1
-  
-SRT2DSPECTReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v, const double alpha_ramp_v=1.0,
-		    const double fc_ramp_v=0.5,const int pad_in_s_v=1.0, const float thres_restr_bound_v=-pow(10,6), const int filter_wiener=1, const int filter_median=0, const int filter_gamma=1);
+    SRT2DSPECTReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v, const int num_segments_to_combine=-1, const int filter_wiener=0, const int filter_median=0, const int filter_gamma=0);
 
 
   virtual std::string method_info() const;
@@ -95,12 +88,6 @@ SRT2DSPECTReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v, const doub
  protected: // make parameters protected such that doc shows always up in doxygen
   // parameters used for parsing
 
-  //! Ramp filter: Alpha value
-  double alpha_ramp;
-  //! Ramp filter: Cut off frequency
-  double fc_ramp;  
-  //! amount of padding for the filter (has to be 0,1 or 2)
-  int pad_in_s;
   //! number of segments to combine (with SSRB) before starting 2D reconstruction
   /*! if -1, a value is chosen depending on the axial compression.
       If there is no axial compression, num_segments_to_combine is
@@ -112,7 +99,7 @@ SRT2DSPECTReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v, const doub
   /*! allowed values: \c display_level=0 (no display), 1 (only final image), 
       2 (filtered-viewgrams). Defaults to 0.
    */
-string attenuation_filename; 
+  string attenuation_filename; 
   int display_level;
   int filter_wiener; 
   int filter_median; 
@@ -127,16 +114,6 @@ private:
   virtual void initialise_keymap();
   virtual bool post_processing(); 
 
- /*inline void spline(float x[],float y[],int n, float y2[]);
-
-inline float hilbert_der(float x, float f[], float ddf[], float p[], int sp, float lg[], float termC);
-inline float integ(float dist, int max, float ff[]);
-float splint(float xa[], float ya[], float y2a[], int n, float x);
-float hilbert_node(float x, float f[], float ddf[], float p[], int sp, float fn); 
-//float hilbert_derivative(float x, float f[], float ddf[], float p[], int sp);
-void hilbert_der_double(float x, float f[], float ddf[], float f1[], float ddf1[], float p[], int sp, float *dhp, float *dh1p, float lg[]);
-float hilbert(float x, float f[], float ddf[], float p[], int sp, float lg[]);
-*/
 float hilbert_node(float x, const std::vector<float>& f, const std::vector<float>& ddf, const std::vector<float>& p, int sp, float fn);
 float hilbert(float x, const std::vector<float>& f, const std::vector<float>& ddf, const std::vector<float>& p, int sp, std::vector<float>& lg);
 void hilbert_der_double(float x, const std::vector<float>& f, const std::vector<float>& ddf, const std::vector<float>& f1, const std::vector<float>& ddf1, const std::vector<float>& p, int sp, float* dhp, float* dh1p, const std::vector<float>& lg);
