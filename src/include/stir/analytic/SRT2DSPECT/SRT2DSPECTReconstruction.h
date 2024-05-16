d9da410807e6a0f4a0776bcef6ed0788edf34356
Dimitra-Kyriakopoulou <dimitra.kyriakopoulou.13@ucl.ac.uk>
2024-05-16 15:51:43 +0300

    Update SRT2DSPECTReconstruction.h [ci skip]
    Header changed

--- src/include/stir/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.h
+++ src/include/stir/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.h
@@ -6,7 +6,7 @@
     Copyright (C) 2024, University College London
     This file is part of STIR.
 
-    SPDX-License-Identifier: Apache-2.0 AND License-ref-PARAPET-license
+    SPDX-License-Identifier: Apache-2.0 
 
     See STIR/LICENSE.txt for details
 */
