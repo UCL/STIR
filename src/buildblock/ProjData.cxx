UCL/STIR

commit a5582cd80aad71c910660f705509d11f870b6fcc

removed VOLPET and obsolete GE_IO code

src/buildblock/ProjData.cxx
@@ -32,7 +32,6 @@
 #include "stir/Viewgram.h"
 #include "stir/DataSymmetriesForViewSegmentNumbers.h"
 
-#include "stir/utilities.h" // TODO remove (temporary for test GEAdvance)
 // for read_from_file
 #include "stir/IO/FileSignature.h"
 #include "stir/IO/interfile.h"
@@ -43,18 +42,6 @@
 #include "stir/Viewgram.h"
 
 
-#ifndef STIR_USE_GE_IO
-#include "stir/ProjDataGEAdvance.h"
-#else
-#include "stir_experimental/IO/GE/ProjDataVOLPET.h"
-#ifdef HAVE_RDF
-#include "stir_experimental/IO/GE/stir_RDF.h"
-#include "stir_experimental/IO/GE/ProjDataRDF.h"
-#endif
-#endif // STIR_USE_GE_IO
-#ifdef HAVE_IE
-#include "stir_experimental/IO/GE/ProjDataIE.h"
-#endif
 #ifdef HAVE_HDF5
 #include "stir/ProjDataGEHDF5.h"
 #include "stir/IO/GEHDF5Wrapper.h"
@@ -90,9 +77,9 @@ START_NAMESPACE_STIR
 
    Currently supported:
    <ul>
-   <li> GE VOLPET data (via class ProjDataVOLPET)
    <li> Interfile (using  read_interfile_PDFS())
-   <li> ECAT 7 3D sinograms and attenuation files 
+   <li> ECAT 7 3D sinograms and attenuation files
+   >li> GE RDF9 (in HDF5)
    </ul>
 
    Developer's note: ideally the return value would be an stir::unique_ptr.
@@ -118,44 +105,7 @@ read_from_file(const string& filename,
     error("ProjData::read_from_file: error opening file %s", actual_filename.c_str());
 
   const FileSignature file_signature(actual_filename);
-  const char * signature = file_signature.get_signature();
-
-  // GE Advance
-  if (strncmp(signature, "2D3D", 4) == 0)
-  {
-//    if (ask("Read with old code (Y) or new (N)?",false))
-#ifndef STIR_USE_GE_IO 
-      {
-#ifndef NDEBUG
-	info("ProjData::read_from_file trying to read " + filename + " as GE Advance file", 3);
-#endif
-	return shared_ptr<ProjData>( new ProjDataGEAdvance(input) );
-      }
-      //else
-#else // use VOLPET
-      {
-#ifndef NDEBUG
-	info("ProjData::read_from_file trying to read " + filename + " as GE VOLPET file", 3);
-#endif
-	delete input;// TODO no longer use pointer after getting rid of ProjDataGEAdvance
-	return shared_ptr<ProjData>( new GE_IO::ProjDataVOLPET(filename, openmode) );
-      }
-#endif // STIR_USE_GE_IO to differentiate between Advance and VOLPET code
-  }
-
-  delete input;// TODO no longer use pointer after getting rid of ProjDataGEAdvance
-
-#ifdef HAVE_IE
-  // GE IE file format 
-  if (GE_IO::is_IE_signature(signature))
-    {
-#ifndef NDEBUG
-      info("ProjData::read_from_file trying to read " + filename + " as GE IE file", 3);
-#endif
-      return shared_ptr<ProjData>( new GE_IO::ProjDataIE(filename) );
-    }
-#endif // HAVE_IE
-      
+  const char * signature = file_signature.get_signature();      
 
 #ifdef HAVE_LLN_MATRIX
   // ECAT 7
@@ -191,19 +141,6 @@ read_from_file(const string& filename,
     if (!is_null_ptr(ptr))
       return ptr;
   }
-
-
-#if defined(STIR_USE_GE_IO) && defined(HAVE_RDF)
-  if (GE_IO::is_RDF_file(actual_filename))
-    {
-#ifndef NDEBUG
-      info("ProjData::read_from_file trying to read " + filename + " as RDF", 3);
-#endif
-      shared_ptr<ProjData> ptr(new GE_IO::ProjDataRDF(filename));
-      if (!is_null_ptr(ptr))
-	return ptr;
-  }
-#endif // RDF
       
 #ifdef HAVE_HDF5
   if (GE::RDF_HDF5::GEHDF5Wrapper::check_GE_signature(actual_filename))

CMakeLists.txt
@@ -92,7 +92,6 @@ find_package( Boost 1.36.0 REQUIRED )
 option(DISABLE_LLN_MATRIX "disable use of LLN library" OFF)
 option(DISABLE_ITK "disable use of ITK library" OFF)
 option(DISABLE_AVW "disable use of AVW library" OFF)
-option(DISABLE_RDF "disable use of GE RDF library" OFF)
 option(DISABLE_HDF5 "disable use of HDF5 libraries" OFF)
 option(DISABLE_STIR_LOCAL "disable use of LOCAL extensions to STIR" OFF)
 option(DISABLE_CERN_ROOT "disable use of Cern ROOT libraries" OFF)
@@ -129,10 +128,6 @@ endif()
 
 if(NOT DISABLE_AVW)
   find_package(AVW)
-endif()
-
-if(NOT DISABLE_RDF)
-  find_package(RDF)
 endif()
 
 if(NOT DISABLE_HDF5)

documentation/STIR-UsersGuide.tex
@@ -718,13 +718,6 @@ Siemens PET/CT and PET/MR scanners use a file format that is based on the mentio
 proposed Interfile standard but with some variations. We read this since STIR 4.0,
 but do not write it.
 
-\subsubsection{VOLPET sinograms}
-
-The GE VOLPET sinogram format for the GE Advance was supported for reading, but 
-only for data files with a single data set, maximum ring 
-difference 11 and contain 281 bins * 256 segments * 336 views.
-This has not been tested in a long time so will be removed in a future version.
-
 \subsubsection{RDF9 data}
 See section \ref{sec:RDFsupport} for enabling support for data from GE scanners
 that use the GE RDF format. We currently support uncompressed listmode,

documentation/release_6.0.htm
@@ -51,6 +51,10 @@
        number of views or rings. This was using very old scanners though, and could lead to
        confusion. These guesses have now been removed.
      </li>
+     <li>
+       support for the GE VOLPET format (an old format used by the GE Advance and Discover LS
+       sinograms when using "break-pointing") has been removed.
+     </li>
     </ul>
 
     <h3>Bug fixes</h3>

src/CMakeLists.txt
@@ -57,15 +57,6 @@ else()
   message(STATUS "AVW IO library not used.")
 endif()
 
-if (RDF_FOUND)
-  set(HAVE_RDF ON)
-  message(STATUS "GE RDF support enabled.")
-  include_directories(${RDF_INCLUDE_DIRS})
-
-else()
-  message(STATUS "RDF support disabled.")
-endif()
-
 if ((NOT DISABLE_HDF5) AND HDF5_FOUND)
   set(HAVE_HDF5 ON)
   message(STATUS "HDF5 support enabled.")

src/buildblock/CMakeLists.txt
@@ -19,7 +19,6 @@ set(${dir_LIB_SOURCES}
   ProjDataInfoSubsetByView.cxx
   ArcCorrection.cxx
   ProjDataFromStream.cxx
-  ProjDataGEAdvance.cxx
   ProjDataInMemory.cxx
   ProjDataInterfile.cxx
   Scanner.cxx

src/buildblock/DiscretisedDensity.cxx
@@ -35,9 +35,6 @@
 #endif
 #include "stir/VoxelsOnCartesianGrid.h"
 #include "stir/is_null_ptr.h"
-#ifdef STIR_USE_GE_IO
-#include "stir_experimental/IO/GE/niff.h"
-#endif
 #endif
 
 #include <typeinfo>
@@ -139,48 +136,6 @@ DiscretisedDensity<num_dimensions,elemT>::
   }
 #endif // HAVE_LLN_MATRIX
 
-#ifdef STIR_USE_GE_IO
-  // NIFF file format (a simple almost raw format from GE)
-  {
-    using namespace GE_IO;
-    if (Niff::isReadableNiffFile(filename))
-      {
-	Niff::Niff input_niff(filename, std::ios::in | std::ios::out);
-	if (input_niff.get_num_dimensions() != 3)
-	  {
-	    warning("DiscretisedDensity::read_from_file:\n"
-		    "File %s is a NIFF file but has %d dimensions (should be 3)",
-		    filename.c_str(), input_niff.get_num_dimensions());
-	    return 0;
-	  }
-	CartesianCoordinate3D<float> voxel_size;
-	{
-	  std::vector<float> pixel_spacing = input_niff.get_pixel_spacing();
-	  std::copy(pixel_spacing.begin(), pixel_spacing.end(), voxel_size.begin()); 
-	}
-	Array<3, float> data;	
-	{
-	  input_niff.fill_array(data);
-	  // change sizes to standard STIR convention
-	  for (int z=data.get_min_index(); z<= data.get_max_index(); ++z)
-	    {
-	      data[z].set_min_index(-(data[z].get_length()/2));
-	      for (int y=data[z].get_min_index(); y<= data[z].get_max_index(); ++y)
-		{
-		  data[z][y].set_min_index(-(data[z][y].get_length()/2));
-		}
-	    }
-	}
-	CartesianCoordinate3D<float> origin;
-	origin.fill(0);
-	
-	return 
-	  new VoxelsOnCartesianGrid<float>(data,
-					  origin,
-					  voxel_size);
-      }
-  }
-#endif	
 
 #ifdef HAVE_LLN_MATRIX
   {

src/buildblock/ProjDataGEAdvance.cxx
@@ -1,323 +1,0 @@
-/*!
-
-  \file
-  \ingroup projdata
-
-  \brief Implementations for class stir::ProjDataGEAdvance
-
-  \author Damiano Belluzzo
-  \author Kris Thielemans
-  \author PARAPET project
-*/
-/*
-    Copyright (C) 2000 PARAPET partners
-    Copyright (C) 2000 - 2009-06-22, Hammersmith Imanet Ltd
-    Copyright (C) 2011, Kris Thielemans
-    This file is part of STIR.
-
-    SPDX-License-Identifier: Apache-2.0 AND License-ref-PARAPET-license
-
-    See STIR/LICENSE.txt for details
-*/
-
-
-
-#include "stir/ProjDataGEAdvance.h"
-#include "stir/Succeeded.h"
-#include "stir/Viewgram.h"
-#include "stir/Sinogram.h"
-#include "stir/Scanner.h"
-#include "stir/Succeeded.h"
-#include "stir/IO/read_data.h"
-#include "stir/warning.h"
-#include "stir/error.h"
-
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <iostream>
-
-#ifndef STIR_NO_NAMESPACES
-using std::vector;
-using std::ios;
-using std::accumulate;
-using std::find;
-using std::iostream;
-using std::streamoff;
-#endif
-
-START_NAMESPACE_STIR
-
-ProjDataGEAdvance::ProjDataGEAdvance(iostream* s)
-  :
-  sino_stream(s),
-  offset(0),
-  on_disk_data_type(NumericType::SHORT),
-  on_disk_byte_order(ByteOrder::big_endian)
-{
-  // TODO find from file
-  const int max_delta = 11;
-  shared_ptr<Scanner> scanner_sptr(new Scanner(Scanner::Advance));
-  proj_data_info_sptr.reset(
-    ProjDataInfo::ProjDataInfoGE(scanner_sptr, max_delta, 
-                                 scanner_sptr->get_max_num_views(), 
-				 scanner_sptr->get_default_num_arccorrected_bins()));
-  
-  
-  const int min_view = proj_data_info_sptr->get_min_view_num();
-  const int max_view = proj_data_info_sptr->get_max_view_num();
-  view_scaling_factor.grow(min_view, max_view);
-  
-  const int offset_begin = 256;
-  sino_stream->seekg(offset, ios::beg); // overall offset
-  sino_stream->seekg(offset_begin, ios::cur); // "breakfile" offset
-  
-  // reads the views_scaling_factors
-  {
-    float scale = float(1);
-    // KT 18/10/99 added on_disk_byte_order
-    if (read_data(*sino_stream, view_scaling_factor,
-                  NumericType::FLOAT, 
-                  scale,
-                  on_disk_byte_order) == Succeeded::no
-        || scale != 1)
-      error("ProjDataGEAdvance: error reading scale factors from header\n");
-    
-    // 	for ( int k=min_view ; k <= max_view ; k++)
-    // 	  {
-    // 	    cout << "view_scaling_factor[" << k << "] = " 
-    // 		 << view_scaling_factor[k] << endl;
-    // 	  }
-  }
-  
-  //=============================================================
-  // parameters as in the unsorted case (help to access the file)
-  //=============================================================
-  //-------------------------------------------------------------
-  // WARNING: this construction works only with odd max_delta
-  //-------------------------------------------------------------
-  
-  int num_segments_orig = proj_data_info_sptr->get_max_segment_num()+1;
-  //(int) get_max_average_ring_difference(); 
-  
-  num_rings_orig.resize(num_segments_orig);
-  segment_sequence_orig.resize(num_segments_orig);
-  
-  
-  const int num_rings = proj_data_info_sptr->get_scanner_ptr()->get_num_rings();
-  num_rings_orig[0]=(2*num_rings-1);
-  
-  {
-    for (unsigned int i=1; i<= num_rings_orig.size()/2; i++)
-    {
-      num_rings_orig[2*i-1]=(2*num_rings-1) - 4*i;
-      num_rings_orig[2*i]=(2*num_rings-1) - 4*i;
-      //  cout << "num_rings_orig[" << 2*i-1 << "] = " << num_rings_orig[2*i-1] << endl;
-      // cout << "num_rings_orig[" << 2*i << "] = " << num_rings_orig[2*i] << endl;
-    }
-  }
-  
-  
-  
-  // segment sequence in the unsorted data format
-  
-  segment_sequence_orig[0]=0;
-  
-  {
-    for (unsigned int i=1; i<= segment_sequence_orig.size()/2; i++)
-    { 
-      segment_sequence_orig[2*i-1] = static_cast<int>(i);
-      segment_sequence_orig[2*i] = -static_cast<int>(i);
-      // cout << "segment_sequence_orig[" << 2*i-1 << "] = " << segment_sequence_orig[2*i-1] << endl;
-      // cout << "segment_sequence_orig[" << 2*i << "] = " << segment_sequence_orig[2*i] << endl;
-    }
-  }
-  
-}
-  
-Viewgram<float>
-ProjDataGEAdvance::
-get_viewgram(const int view_num, const int segment_num,
-             const bool make_num_tangential_poss_odd, const int timing_pos) const
-  {
-    // --------------------------------------------------------
-    // --------------------------------------------------------
-    // 	Advance GE format reader with sort of the segments
-    // --------------------------------------------------------
-    // --------------------------------------------------------
-    //
-    // segment_num     0       +1      -1      +2      -2      +3      -3 .......
-    // ave_ring_diff   0       +2      -2      +3      -3      +4      -4 .......
-    // max_ring_diff   +1      +2      -2      +3      -3      +4      -4 .......
-    // min_ring_diff   -1      +2      -2      +3      -3      +4      -4 .......
-    
-    
-    streamoff odd_ring_segment_offset;
-    streamoff num_rings_offset;
-    streamoff segment_offset;
-    streamoff jump_size;
-    streamoff jump_ring;
-    
-    
-    //----------------------------------------------------------------
-    // data with delta ring = +1 and -1 are contained in segment 0:
-    // for this reason there is not a segment +1 and a segment -1 
-    //----------------------------------------------------------------
-    
-    // segment number in the unsorted data format 
-    // (no problem for segments +1 and -1)
-    
-    const int sign_segment_num = (segment_num == 0) ? 0 : ( (segment_num > 0) ? 1 : -1 );
-    const int segment_num_orig = (int) ((segment_num+sign_segment_num) / 2);
-    
-    //      cout << "segment_num_orig " << segment_num_orig << endl;
-    
-    // segment index in the unsorted data format
-    
-    const int  index_orig = 
-      static_cast<int>(find(segment_sequence_orig.begin(), 
-	                        segment_sequence_orig.end(), segment_num_orig) - 
-                       segment_sequence_orig.begin());
-    
-    //      cout << "index_orig " << index_orig << endl;
-    
-    
-    //---------------------------------------------------------------
-    // OFFSETS
-    //---------------------------------------------------------------
-    
-    const streamoff offset_begin = 256;
-    
-    // offset in rings for the odd sorted segments (value 0 or 1)
-    
-    odd_ring_segment_offset = abs((segment_num+sign_segment_num) % 2); 
-    
-    //      cout << "odd_ring_segment_offset " << odd_ring_segment_offset << endl;
-    
-    // offset in rings in the unsorted data format
-    
-    num_rings_offset = 
-      accumulate(num_rings_orig.begin(), 
-      num_rings_orig.begin() + index_orig, 0) +
-      odd_ring_segment_offset;
-    
-    //      cout << "num_rings_offset " << num_rings_offset << endl;
-    
-    // offset to the first data in the first view_num of the segment chosen
-    
-    segment_offset =  
-      num_rings_offset * get_num_tangential_poss() * on_disk_data_type.size_in_bytes();
-    
-    //      cout << "segment_offset " << segment_offset << endl;
-    
-    // size of the jump to the first record of the following view_num
-    if (segment_num == 0)
-    {
-      jump_size = 
-        ( 
-        accumulate(num_rings_orig.begin(), num_rings_orig.end(), 0) 
-        - num_rings_orig[index_orig] +
-        + 2 * odd_ring_segment_offset
-        ) * get_num_tangential_poss() * on_disk_data_type.size_in_bytes();
-    }
-    else
-    {
-      jump_size = 
-        ( 
-        accumulate(num_rings_orig.begin(), num_rings_orig.end(), 0)
-        - num_rings_orig[index_orig]
-        - 1  // offset to balance the jump_ring in the cycle
-        + 2 * odd_ring_segment_offset
-        ) * get_num_tangential_poss() * on_disk_data_type.size_in_bytes();
-    }
-    //      cout << "jump_size " << jump_size << endl;
-    
-    // size of the jump between two rings of the same view_num and of the same 
-    // segment
-    if (segment_num == 0)
-      jump_ring = 0;
-    else
-      jump_ring = get_num_tangential_poss() * on_disk_data_type.size_in_bytes();
-    
-    streamoff ring_offset = get_num_tangential_poss() * on_disk_data_type.size_in_bytes();
-    
-    // jumps the initial offset
-    
-    sino_stream->seekg(offset, ios::beg); // overall offset
-    sino_stream->seekg(offset_begin, ios::cur); // "breakfile" offset
-    sino_stream->seekg(get_num_views() * sizeof(float), ios::cur); // skip view scaling factors
-    
-    // jumps at the beginning of the chosen segment
-    
-    sino_stream->seekg(segment_offset, ios::cur);
-    
-    // jump to the right Viewgram
-    
-    streamoff jump_ini = (view_num - get_min_view_num())*
-      (ring_offset*get_num_axial_poss(segment_num)
-      + jump_ring*get_num_axial_poss(segment_num)
-      + jump_size);
-    
-    // 	  cout << "ring_offset  " << ring_offset << endl;
-    // 	  cout << "jump_ring  " << jump_ring << endl;
-    // 	  cout << "jump_size  " << jump_size << endl;
-    // 	  cout << " jump_ini " << jump_ini << endl;
-    
-    sino_stream->seekg(jump_ini, ios::cur);
-    
-    Viewgram<float> data = get_empty_viewgram(view_num, segment_num,false);
-    
-    for (int ring =get_min_axial_pos_num(segment_num) ; ring <= get_max_axial_pos_num(segment_num); ring++)
-    {
-      {
-        float scale = float(1);
-        if (read_data(*sino_stream, data[ring],
-                      on_disk_data_type, 
-                      scale,
-                      on_disk_byte_order) == Succeeded::no
-            || scale != 1)
-          error("ProjDataGEAdvance: error reading data\n");
-      }
-      sino_stream->seekg(jump_ring, ios::cur);
-    }
-    
-    // scales the Viewgram<float>
-    data *= view_scaling_factor[view_num];
-    
-    if (make_num_tangential_poss_odd && get_num_tangential_poss()%2==0)
-    {
-      const int new_max_tangential_pos = get_max_tangential_pos_num() + 1;
-      data.grow(
-        IndexRange2D(get_min_axial_pos_num(segment_num),
-                     get_max_axial_pos_num(segment_num),        
-                     get_min_tangential_pos_num(),
-                     new_max_tangential_pos));   
-    }  
-    
-    return data;
-}
-
-
-Succeeded ProjDataGEAdvance::set_viewgram(const Viewgram<float>& v)
-{
-  // TODO
-  // but this is difficult: how to adjust the scale factors when writing only 1 viewgram ?
-  warning("ProjDataGEAdvance::set_viewgram not implemented yet\n");
-  return Succeeded::no;
-}
-
-Sinogram<float> ProjDataGEAdvance::get_sinogram(const int ax_pos_num, const int segment_num,
-                                                const bool make_num_tangential_poss_odd, const int timing_pos) const
-{ 
-  // TODO
-  error("ProjDataGEAdvance::get_sinogram not implemented yet\n"); 
-  return get_empty_sinogram(ax_pos_num, segment_num);}
-
-Succeeded ProjDataGEAdvance::set_sinogram(const Sinogram<float>& s)
-{
-  // TODO
-  warning("ProjDataGEAdvance::set_sinogram not implemented yet\n");
-  return Succeeded::no;
-}
-
-END_NAMESPACE_STIR

commit 46b1c0189297ae0b90b4100279c611c4e0544b64

Fix for memory leak introduced in commit a5582cd.

src/buildblock/ProjData.cxx
@@ -98,10 +98,6 @@ ProjData::read_from_file(const string& filename, const std::ios::openmode openmo
       }
   }
 
-  fstream* input = new fstream(actual_filename.c_str(), openmode | ios::binary);
-  if (!*input)
-    error("ProjData::read_from_file: error opening file %s", actual_filename.c_str());
-
   const FileSignature file_signature(actual_filename);
   const char* signature = file_signature.get_signature();
 