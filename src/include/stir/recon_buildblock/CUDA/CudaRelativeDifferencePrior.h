5ddce33891bc1e3b2b684fb638e70151ff7cc355
Imraj-Singh <72553490+Imraj-Singh@users.noreply.github.com>
2024-07-04 14:29:37 +0000

    Added small test, constructors not inheriting correctly causing an error

--- src/include/stir/recon_buildblock/CUDA/CudaRelativeDifferencePrior.h
+++ src/include/stir/recon_buildblock/CUDA/CudaRelativeDifferencePrior.h
@@ -3,15 +3,15 @@
 
 #include "stir/recon_buildblock/GeneralisedPrior.h"
 #include "stir/recon_buildblock/RelativeDifferencePrior.h"
+#include <memory>
 
 START_NAMESPACE_STIR
 
-// Define a native cpp struct to hold the dimensions of a CUDA grid or block
-//struct cppdim3 {
-//    int x;
-//    int y;
-//    int z;
-//}; forward declaration instead
+struct cppdim3 {
+    int x;
+    int y;
+    int z;
+};
 
 template <typename elemT>
 class CudaRelativeDifferencePrior : public RegisteredParsingObject<CudaRelativeDifferencePrior<elemT>,
@@ -24,34 +24,26 @@ class CudaRelativeDifferencePrior : public RegisteredParsingObject<CudaRelativeD
                                     RelativeDifferencePrior<elemT>>
         base_type;
     public:
-        using RelativeDifferencePrior<elemT>::RelativeDifferencePrior;
+        using base_type::base_type;
+
         // Name which will be used when parsing a GeneralisedPrior object
         inline static const char* const registered_name = "Cuda Relative Difference Prior";
-        //static const char* const registered_name;
-        //CudaRelativeDifferencePrior(const GeneralisedPrior<DiscretisedDensity<3, elemT>>& gp)
-        //: RelativeDifferencePrior<elemT>(gp) {}
         // Constructors
-        
-        //! Default constructor
-        CudaRelativeDifferencePrior();
+        //CudaRelativeDifferencePrior();
+        //CudaRelativeDifferencePrior(const bool only_2D, float penalization_factor, float gamma, float epsilon);
 
-        //! Constructs it explicitly
-        CudaRelativeDifferencePrior(const bool only_2D, float penalization_factor, float gamma, float epsilon);
-
-        //! Has to be called before using this object
-        virtual Succeeded set_up(shared_ptr<DiscretisedDensity<3, elemT>> const& target_sptr);
         // Overridden methods
         //double compute_value(const DiscretisedDensity<3, elemT>& current_image_estimate) override;
         //void compute_gradient(DiscretisedDensity<3, elemT>& prior_gradient, const DiscretisedDensity<3, elemT>& current_image_estimate) override;
 
-    //protected:
-        //int z_dim, y_dim, x_dim;
-        //cppdim3 cpp_grid_dim, cpp_block_dim;
-        //bool cuda_set_up = false;
+        // New methods
+        Succeeded set_up_cuda(shared_ptr<DiscretisedDensity<3, elemT>> const& target_sptr);
+    protected:
+        int z_dim, y_dim, x_dim;
+        cppdim3 block_dim;
+        cppdim3 grid_dim;
+        bool cuda_set_up = false;
 };
-
-// Ensure the class is registered
-//template class CudaRelativeDifferencePrior<float>;
 
 END_NAMESPACE_STIR
 
--- src/recon_buildblock/CUDA/CudaRelativeDifferencePrior.cu
+++ src/recon_buildblock/CUDA/CudaRelativeDifferencePrior.cu
@@ -1,11 +1,16 @@
+#include "stir/recon_buildblock/CUDA/CudaRelativeDifferencePrior.h"
 #include "stir/DiscretisedDensity.h"
 #include "stir/recon_buildblock/GeneralisedPrior.h"
 #include "stir/DiscretisedDensityOnCartesianGrid.h"
 #include "stir/IndexRange3D.h"
 #include "stir/is_null_ptr.h"
 #include "stir/Succeeded.h"
+#include <cuda_runtime.h>
+// include std and error
+#include <iostream>
+#include <numeric>
 
-#include "stir/recon_buildblock/CUDA/CudaRelativeDifferencePrior.h"
+
 
 extern "C" __global__
 void computeCudaRelativeDifferencePriorGradientKernel(float* tmp_grad,
@@ -136,13 +141,259 @@ void computeCudaRelativeDifferencePriorValueKernel(double* tmp_value,
 
 START_NAMESPACE_STIR
 
-//template <>
-//const char* const CudaRelativeDifferencePrior<float>::registered_name = "Cuda Relative Difference Prior";
+static void
+compute_weights(Array<3, float>& weights, const CartesianCoordinate3D<float>& grid_spacing, const bool only_2D)
+{
+  int min_dz, max_dz;
+  if (only_2D)
+    {
+      min_dz = max_dz = 0;
+    }
+  else
+    {
+      min_dz = -1;
+      max_dz = 1;
+    }
+  weights = Array<3, float>(IndexRange3D(min_dz, max_dz, -1, 1, -1, 1));
+  for (int z = min_dz; z <= max_dz; ++z)
+    for (int y = -1; y <= 1; ++y)
+      for (int x = -1; x <= 1; ++x)
+        {
+          if (z == 0 && y == 0 && x == 0)
+            weights[0][0][0] = 0;
+          else
+            {
+              weights[z][y][x]
+                  = grid_spacing.x()
+                    / sqrt(square(x * grid_spacing.x()) + square(y * grid_spacing.y()) + square(z * grid_spacing.z()));
+            }
+        }
+}
+/* 
+template <typename elemT>
+void CudaRelativeDifferencePrior<elemT>::compute_gradient(DiscretisedDensity<3, elemT>& prior_gradient,
+                                                 const DiscretisedDensity<3, elemT>& current_image_estimate)
+{
+    assert(prior_gradient.has_same_characteristics(current_image_estimate));
+    if (this->penalisation_factor == 0)
+    {
+        prior_gradient.fill(0);
+        return;
+    }
+
+    dim3 cuda_block_dim(block_dim.x, block_dim.y, block_dim.z);
+    dim3 cuda_grid_dim(grid_dim.x, grid_dim.y, grid_dim.z);
+
+    this->check(current_image_estimate);
+
+    const DiscretisedDensityOnCartesianGrid<3, elemT>& current_image_cast
+        = dynamic_cast<const DiscretisedDensityOnCartesianGrid<3, elemT>&>(current_image_estimate);
+
+    if (this->cuda_set_up == false) {
+        error("CudaRelativeDifferencePrior: set_up_cuda has not been called\n");
+    }
+
+    if (this->weights.get_length() == 0)
+    {
+        compute_weights(this->weights, current_image_cast.get_grid_spacing(), this->only_2D);
+    }
+    auto kappa_ptr = this->get_kappa_sptr();
+    const bool do_kappa = !is_null_ptr(kappa_ptr);
+    if (do_kappa && !kappa_ptr->has_same_characteristics(current_image_estimate))
+        error("RelativeDifferencePrior: kappa image has not the same index range as the reconstructed image\n");
+
+    const int z_dim = this->z_dim;
+    const int y_dim = this->y_dim;
+    const int x_dim = this->x_dim;
+
+    std::vector<float> image_data(current_image_estimate.size_all());
+    std::vector<float> weights_data(this->weights.size_all());
+    std::copy(current_image_estimate.begin_all(), current_image_estimate.end_all(), image_data.begin());
+    std::copy(this->weights.begin_all(), this->weights.end_all(), weights_data.begin());
+
+    float *d_image_data, *d_weights_data, *d_gradient_data;
+
+    // Allocate memory on the GPU
+    cudaMalloc(&d_image_data, current_image_estimate.size_all() * sizeof(float));
+    cudaMalloc(&d_weights_data, this->weights.size_all() * sizeof(float));
+    cudaMalloc(&d_gradient_data, prior_gradient.size_all() * sizeof(float));
+    
+    // Copy data from host to device
+    cudaMemcpy(d_image_data, image_data.data(), current_image_estimate.size_all() * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_weights_data, weights_data.data(), this->weights.size_all() * sizeof(float), cudaMemcpyHostToDevice);
+
+    if (do_kappa)
+    {
+        std::vector<float> kappa_data(current_image_estimate.size_all());
+        std::copy(kappa_ptr->begin_all(), kappa_ptr->end_all(), kappa_data.begin());
+        float *d_kappa;
+        cudaMalloc(&d_kappa, current_image_estimate.size_all() * sizeof(float));
+        cudaMemcpy(d_kappa, kappa_data.data(), current_image_estimate.size_all() * sizeof(float), cudaMemcpyHostToDevice);
+        computeCudaRelativeDifferencePriorGradientKernel<<<cuda_grid_dim, cuda_block_dim>>>(
+            d_gradient_data, d_image_data, d_weights_data, d_kappa, do_kappa, this->gamma, this->epsilon, this->penalisation_factor, z_dim, y_dim, x_dim
+        );
+        cudaFree(d_kappa);
+    }
+    else
+    {
+        computeCudaRelativeDifferencePriorGradientKernel<<<cuda_grid_dim, cuda_block_dim>>>(
+            d_gradient_data, d_image_data, d_weights_data, nullptr, do_kappa, this->gamma, this->epsilon, this->penalisation_factor, z_dim, y_dim, x_dim
+        );
+    }
+
+    // Check for any errors during kernel execution
+    cudaError_t cuda_error = cudaGetLastError();
+    if (cuda_error != cudaSuccess) {
+        cudaFree(d_image_data); cudaFree(d_weights_data); cudaFree(d_gradient_data);
+        //error("CUDA error in compute_value kernel execution: " << cudaGetErrorString(cuda_error));
+    }
+
+    // Allocate host memory for the result and copy from device to host
+    std::vector<float> gradient_data(current_image_estimate.size_all());
+    cudaMemcpy(gradient_data.data(), d_gradient_data, current_image_estimate.size_all() * sizeof(float), cudaMemcpyDeviceToHost);
+    
+    // Copy the gradient data to the prior_gradient
+    std::copy(gradient_data.begin(), gradient_data.end(), prior_gradient.begin_all());
+
+    // Cleanup
+    cudaFree(d_image_data);
+    cudaFree(d_weights_data);
+    cudaFree(d_gradient_data);
+}
+
+template <typename elemT>
+double CudaRelativeDifferencePrior<elemT>::compute_value(const DiscretisedDensity<3, elemT>& current_image_estimate) {
+    
+    if (this->penalisation_factor == 0)
+    {
+        return 0.;
+    }
+
+    dim3 cuda_block_dim(block_dim.x, block_dim.y, block_dim.z);
+    dim3 cuda_grid_dim(grid_dim.x, grid_dim.y, grid_dim.z);
+
+    this->check(current_image_estimate);
+
+    const DiscretisedDensityOnCartesianGrid<3, elemT>& current_image_cast
+        = dynamic_cast<const DiscretisedDensityOnCartesianGrid<3, elemT>&>(current_image_estimate);
+
+    if (this->cuda_set_up == false) {
+        error("CudaRelativeDifferencePrior: set_up_cuda has not been called\n");
+    }
+
+    if (this->weights.get_length() == 0)
+    {
+        compute_weights(this->weights, current_image_cast.get_grid_spacing(), this->only_2D);
+    }
+    // Need to get the kappa image
+    auto kappa_ptr = this->get_kappa_sptr();
+    const bool do_kappa = !is_null_ptr(kappa_ptr);
+
+    if (do_kappa && !kappa_ptr->has_same_characteristics(current_image_estimate))
+        error("RelativeDifferencePrior: kappa image has not the same index range as the reconstructed image\n");
+
+    // Assuming z_dim, y_dim, and x_dim are correctly set
+    const int z_dim = this->z_dim;
+    const int y_dim = this->y_dim;
+    const int x_dim = this->x_dim;
+
+    std::vector<float> image_data(current_image_estimate.size_all());
+    std::vector<float> weights_data(this->weights.size_all());
+    std::copy(current_image_estimate.begin_all(), current_image_estimate.end_all(), image_data.begin());
+    std::copy(this->weights.begin_all(), this->weights.end_all(), weights_data.begin());
+
+    // GPU memory pointers
+    float *d_image_data, *d_weights_data;
+    double *d_tmp_value;
+
+    // Allocate memory on the GPU
+    cudaMalloc(&d_image_data, current_image_estimate.size_all() * sizeof(float));
+    cudaMalloc(&d_weights_data, this->weights.size_all() * sizeof(float)); // Assuming weights is also a flat vector
+    cudaMalloc(&d_tmp_value, current_image_estimate.size_all() * sizeof(double));
+    
+    // Copy data from host to device
+    cudaMemcpy(d_image_data, image_data.data(), current_image_estimate.size_all() * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_weights_data, weights_data.data(), this->weights.size_all() * sizeof(float), cudaMemcpyHostToDevice);
+
+    // Launch the kernel
+    if (do_kappa)
+    {
+        std::vector<float> kappa_data(current_image_estimate.size_all());
+        std::copy(kappa_ptr->begin_all(), kappa_ptr->end_all(), kappa_data.begin());
+        float *d_kappa;
+        cudaMalloc(&d_kappa, current_image_estimate.size_all() * sizeof(float));
+        cudaMemcpy(d_kappa, kappa_data.data(), current_image_estimate.size_all() * sizeof(float), cudaMemcpyHostToDevice);
+        computeCudaRelativeDifferencePriorValueKernel<<<cuda_grid_dim, cuda_block_dim>>>(
+            d_tmp_value, d_image_data, d_weights_data, d_kappa, do_kappa, this->gamma, this->epsilon, this->penalisation_factor, z_dim, y_dim, x_dim
+        );
+        cudaFree(d_kappa);
+    }
+    else
+    {
+        computeCudaRelativeDifferencePriorValueKernel<<<cuda_grid_dim, cuda_block_dim>>>(
+            d_tmp_value, d_image_data, d_weights_data, nullptr, do_kappa, this->gamma, this->epsilon, this->penalisation_factor, z_dim, y_dim, x_dim
+        );
+    }
+
+    // Check for any errors during kernel execution
+    cudaError_t cuda_error = cudaGetLastError();
+    if (cuda_error != cudaSuccess) {
+        cudaFree(d_image_data); cudaFree(d_weights_data); cudaFree(d_tmp_value);
+        //error("CUDA error in compute_value kernel execution: " << cudaGetErrorString(cuda_error));
+        return 0.0; // Handle error appropriately
+    }
+
+    // Allocate host memory for the result and copy from device to host
+    std::vector<double> tmp_value(current_image_estimate.size_all());
+    cudaMemcpy(tmp_value.data(), d_tmp_value, current_image_estimate.size_all() * sizeof(double), cudaMemcpyDeviceToHost);
+
+    // Compute the total value from tmp_value if necessary
+    double totalValue = std::accumulate(tmp_value.begin(), tmp_value.end(), 0.0);
+
+    // Cleanup
+    cudaFree(d_image_data);
+    cudaFree(d_weights_data);
+    cudaFree(d_tmp_value);
+    return totalValue;
+}
+ */
+template <typename elemT>
+Succeeded CudaRelativeDifferencePrior<elemT>::set_up_cuda(shared_ptr<DiscretisedDensity<3, elemT>> const& target_sptr){
+    // Optionally call RelativeDifferencePrior's set_up if it adds value
+    if (RelativeDifferencePrior<elemT>::set_up(target_sptr) == Succeeded::no) {
+        return Succeeded::no;
+    }
+    // Get the number of voxels in each dimension
+    const DiscretisedDensityOnCartesianGrid<3, float>& target_cast 
+        = dynamic_cast<const DiscretisedDensityOnCartesianGrid<3, float>&>(*target_sptr);
+
+    this->z_dim = target_cast.get_max_index() - target_cast.get_min_index() + 1;
+    this->y_dim = target_cast[0].get_max_index() - target_cast[0].get_min_index() + 1;
+    this->x_dim = target_cast[0][0].get_max_index() - target_cast[0][0].get_min_index() + 1;
+
+    // Set the thread block and grid dimensions using std::tuple
+    this->block_dim.x = 8;
+    this->block_dim.y = 8;
+    this->block_dim.z = 8;
+
+    this->grid_dim.x = (this->x_dim + this->block_dim.x - 1) / this->block_dim.x;
+    this->grid_dim.y = (this->y_dim + this->block_dim.y - 1) / this->block_dim.y;
+    this->grid_dim.z = (this->z_dim + this->block_dim.z - 1) / this->block_dim.z;
+    //this->block_dim = dim3(8, 8, 8);
+    //this->grid_dim = dim3((this->x_dim + this->block_dim.x - 1) / this->block_dim.x, (this->y_dim + this->block_dim.y - 1) / this->block_dim.y, (this->z_dim + this->block_dim.z - 1) / this->block_dim.z);
+    // Check if z_dim is 1 or only 2D is true and return an error if it is
+    if (this->z_dim == 1 || this->only_2D) {
+        std::cerr << "CudaRelativeDifferencePriorClass: This prior requires a 3D image and only works for a 3x3x3 neighbourhood" << std::endl;
+        return Succeeded::no;
+    }
+    compute_weights(this->weights, target_cast.get_grid_spacing(), this->only_2D);
+    this->cuda_set_up = true;
+    return Succeeded::yes;
+}
+
+//template class CudaRelativeDifferencePrior<float>::CudaRelativeDifferencePrior() : base_type<float>() {}
+//template class CudaRelativeDifferencePrior<float>::CudaRelativeDifferencePrior(const bool only_2D, float penalization_factor, float gamma, float epsilon) : base_type<elemT>(only_2D, penalization_factor, gamma, epsilon) {}
 template class CudaRelativeDifferencePrior<float>;
-//template <typename elemT>
-//CudaRelativeDifferencePrior<elemT>::CudaRelativeDifferencePrior() : RelativeDifferencePrior<elemT>() {}
-//template <typename elemT>
-//CudaRelativeDifferencePrior<elemT>::CudaRelativeDifferencePrior(const bool only_2D, float penalization_factor, float gamma, float epsilon) : RelativeDifferencePrior<elemT>(only_2D, penalization_factor, gamma, epsilon) {}
 
 
 END_NAMESPACE_STIR
