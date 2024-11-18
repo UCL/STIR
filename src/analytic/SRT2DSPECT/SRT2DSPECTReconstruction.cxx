8a4d077c00d3d957fe2a74c565fb6732422069f8
Dimitra-Kyriakopoulou <dimitra.kyriakopoulou.13@ucl.ac.uk>
2024-11-18 15:42:16 +0200

    Update SRT2DSPECTReconstruction.cxx
    header update

--- src/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.cxx
+++ src/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.cxx
@@ -14,7 +14,7 @@
   \brief Implementation of class stir::SRT2DSPECTReconstruction
   
   The reference for the implemented SPECT algorithm is: Fokas, A. S., A. Iserles, and V. Marinakis. "Reconstruction algorithm for single photon emission computed tomography and its numerical implementation." *Journal of the Royal Society Interface* 3.6 (2006): 45-54.
-  STIR implementations: initial version 2015, 1st updated version 2023-2024
+  STIR implementations: initial version 2015-2026, 1st updated version 2023-2024
   
   \author Dimitra Kyriakopoulou
   \author Kris Thielemans
