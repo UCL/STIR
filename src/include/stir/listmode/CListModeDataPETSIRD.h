0e0b71b322096f30fb0ae26a7e275ac362ee5b81
nmdicom-recon <nmdicom-recon@feijoa.npl.co.uk>
2025-07-15 17:07:28 +0100

    creating classes for LM PETSIRD, mainly a copy at this stage

---
+++ src/include/stir/listmode/CListModeDataPETSIRD.h
@@ -0,0 +1,79 @@
+/* CListModeDataPETSIRD.h
+
+Coincidence LM Data Class for PETSIRD: Header File
+Jannis Fischer
+
+     Copyright 2015 ETH Zurich, Institute of Particle Physics
+     Copyright 2020 Positrigo AG, Zurich
+     Copyright 2025 National Physical Laboratory
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+             http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+
+ */
+
+/*!
+
+\file
+\ingroup listmode
+\brief Declaration of class stir::CListModeDataPETSIRD
+
+\author Daniel Deidda
+*/
+
+#ifndef __stir_listmode_CListModeDataPETSIRD_H__
+#define __stir_listmode_CListModeDataPETSIRD_H__
+
+
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "stir/listmode/CListModeDataBasedOnCoordinateMap.h"
+#include "stir/ProjData.h"
+#include "stir/ProjDataInfo.h"
+#include "stir/listmode/CListRecord.h"
+#include "stir/IO/InputStreamWithRecords.h"
+#include "stir/shared_ptr.h"
+#include "petsird_helpers.h"
+#include "petsird_helpers/create.h"
+#include "petsird_helpers/geometry.h"
+
+#include "stir/listmode/CListRecordPETSIRD.h"
+
+START_NAMESPACE_STIR
+
+/*!
+  \brief Class for reading PETSIRD listmode data with variable geometry
+  \ingroup listmode
+  \par
+  By providing crystal map and template projection data files, the coordinates are read from files and used defining the LOR
+  coordinates.
+*/
+template <class CListRecordT>
+class CListModeDataPETSIRD : public CListModeDataBasedOnCoordinateMap<CListRecordT>
+{
+public:
+
+  CListModeDataPETSIRD(const std::string& listmode_filename,
+                     const std::string& crystal_map_filename,
+                     const std::string& template_proj_data_filename,
+                     const double lor_randomization_sigma = 0.0);
+
+protected:
+  virtual Succeeded open_lm_file() const;
+
+};
+
+END_NAMESPACE_STIR
+#endif // CLISTMODEDATAPETSIRD_H
---
+++ src/listmode_buildblock/CListModeDataPETSIRD.cxx
@@ -0,0 +1,109 @@
+/* CListModeDataPETSIRD.cxx
+
+Coincidence LM Data Class for PETSIRD: Implementation
+
+     Copyright 2015 ETH Zurich, Institute of Particle Physics
+     Copyright 2020 Positrigo AG, Zurich
+     Copyright 2021 University College London
+     Copyright 2025 National Physical Laboratory
+
+
+     Licensed under the Apache License, Version 2.0 (the "License");
+     you may not use this file except in compliance with the License.
+     You may obtain a copy of the License at
+
+             http://www.apache.org/licenses/LICENSE-2.0
+
+     Unless required by applicable law or agreed to in writing, software
+     distributed under the License is distributed on an "AS IS" BASIS,
+     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     See the License for the specific language governing permissions and
+     limitations under the License.
+*/
+/*!
+
+\file
+\ingroup listmode
+\brief implementation of class stir::CListModeDataPETSIRD
+
+\author Jannis Fischer
+\author Kris Thielemans
+\author Markus Jehl
+\author Daniel Deidda
+*/
+#include <iostream>
+#include <fstream>
+#include <typeinfo>
+
+#include "stir/ExamInfo.h"
+#include "stir/Succeeded.h"
+#include "stir/info.h"
+#include "stir/error.h"
+
+//#include "boost/static_assert.hpp"
+
+#include "stir/listmode/CListModeDataPETSIRD.h"
+#include "stir/listmode/CListRecordPETSIRD.h"
+
+using std::ios;
+using std::fstream;
+using std::ifstream;
+using std::istream;
+
+START_NAMESPACE_STIR;
+
+template <class CListRecordT>
+CListModeDataPETSIRD<CListRecordT>::CListModeDataPETSIRD(const std::string& listmode_filename,
+                                                                                   const std::string& crystal_map_filename,
+                                                                                   const std::string& template_proj_data_filename,
+                                                                                   const double lor_randomization_sigma)
+    : CListModeDataBasedOnCoordinateMap<CListRecordT>(listmode_filename,
+                                                      crystal_map_filename,
+                                                      template_proj_data_filename,
+                                                      lor_randomization_sigma)
+{
+  if (!crystal_map_filename.empty())
+    {
+      this->map = MAKE_SHARED<DetectorCoordinateMap>(crystal_map_filename, lor_randomization_sigma);
+    }
+  else
+    {
+      if (lor_randomization_sigma != 0)
+        error("PETSIRD currently does not support LOR-randomisation unless a map is specified");
+    }
+  shared_ptr<ExamInfo> _exam_info_sptr(new ExamInfo);
+  _exam_info_sptr->imaging_modality = ImagingModality::PT;
+  this->exam_info_sptr = _exam_info_sptr;
+
+         // Here we are reading the scanner data from the template projdata
+  shared_ptr<ProjData> template_proj_data_sptr = ProjData::read_from_file(template_proj_data_filename);
+  this->set_proj_data_info_sptr(template_proj_data_sptr->get_proj_data_info_sptr()->create_shared_clone());
+
+  if (this->open_lm_file() == Succeeded::no)
+    {
+      error("CListModeDataPETSIRD: Could not open listmode file " + listmode_filename + "\n");
+    }
+}
+
+template <class CListRecordT>
+Succeeded
+CListModeDataPETSIRD<CListRecordT>::open_lm_file() const
+{
+  shared_ptr<istream> stream_ptr(new fstream(this->listmode_filename.c_str(), ios::in | ios::binary));
+  if (!(*stream_ptr))
+    {
+      return Succeeded::no;
+    }
+  info("CListModeDataPETSIRD: opening file \"" + this->listmode_filename + "\"", 2);
+  stream_ptr->seekg((std::streamoff)32);
+  this->current_lm_data_ptr.reset(
+      new InputStreamWithRecords<CListRecordT, bool>(stream_ptr,
+                                                     sizeof(CListTimeDataPETSIRD),
+                                                     sizeof(CListTimeDataPETSIRD),
+                                                     ByteOrder::little_endian != ByteOrder::get_native_order()));
+  return Succeeded::yes;
+}
+
+template class CListModeDataPETSIRD<CListRecordPETSIRD<CListEventDataPETSIRD>>;
+
+END_NAMESPACE_STIR
