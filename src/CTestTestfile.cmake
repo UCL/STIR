af85e886a6d4245a788239ef6ae6a534fc393651
root <root@DESKTOP-U7AVDMQ.>
2025-08-19 12:05:01 +0200

    Clean up: remove build artifacts and revert QuadraticPrior.cxx to original
    - Remove generated CMake and Makefile build artifacts
    - Revert QuadraticPrior.cxx to upstream version (removed experimental changes)

--- src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/devel/buildConda/sources/STIR
-# Build directory: /root/devel/buildConda/sources/STIR/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("scripts")
--- src/scripts/IO/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/devel/buildConda/sources/STIR/scripts/IO
-# Build directory: /root/devel/buildConda/sources/STIR/src/scripts/IO
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
