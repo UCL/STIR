ed415826f9ab05864404c549eb408ba9318cf0cf
Dimitra-Kyriakopoulou <dimitra.kyriakopoulou.13@ucl.ac.uk>
2025-10-22 22:52:24 +0300

    Fix reference's chapter number in SRT2DSPECTReconstruction [ci skip]

--- src/include/stir/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.h
+++ src/include/stir/analytic/SRT2DSPECT/SRT2DSPECTReconstruction.h
@@ -59,11 +59,11 @@ class ProjData;
 /*! \ingroup SRT2DSPECT
  \brief Reconstruction class for 2D Spline Reconstruction Technique
 
-The reference for the implemented SPECT algorithm is: Fokas, A. S., A. Iserles, and V. Marinakis. "Reconstruction algorithm for
+  The reference for the implemented SPECT algorithm is: Fokas, A. S., A. Iserles, and V. Marinakis. "Reconstruction algorithm for
 single photon emission computed tomography and its numerical implementation." *Journal of the Royal Society Interface* 3.6 (2006):
 45-54.
 
-The STIR implementation of this algorithm is described in Chapter 5 of Dimitra Kyriakopoulou's doctoral thesis, “Analytical and Numerical Aspects of Tomography”, University College London (UCL), 2024, supervised by Professor Athanassios S. Fokas (Cambridge) and Professor Kris Thielemans (UCL). Available at: https://discovery.ucl.ac.uk/id/eprint/10202525/
+  The STIR implementation of this algorithm is described in Chapter 6 of Dimitra Kyriakopoulou's doctoral thesis, “Analytical and Numerical Aspects of Tomography”, University College London (UCL), 2024, supervised by Professor Athanassios S. Fokas (Cambridge) and Professor Kris Thielemans (UCL). Available at: https://discovery.ucl.ac.uk/id/eprint/10202525/
 
   STIR implementations: initial version 2014-2016, 1st updated version 2023-2024
 
