24678124fe8ae044bf49e8201c16ebba45c8a59a
Dimitra-Kyriakopoulou <dimitra.kyriakopoulou.13@ucl.ac.uk>
2025-10-24 00:12:32 +0300

    Remove zoom functionality from GRD2DReconstruction [ci skip]

--- src/analytic/GRD2D/GRD2DReconstruction.cxx
+++ src/analytic/GRD2D/GRD2DReconstruction.cxx
@@ -75,7 +75,7 @@ set_defaults()
 
   display_level=0; // no display
   num_segments_to_combine = -1;
-	zoom = 1; 
+	//zoom = 1; 
   noise_filter = -1; 
   alpha_gridding = 1; 
   kappa_gridding = 4; 
@@ -90,7 +90,7 @@ GRD2DReconstruction::initialise_keymap()
   parser.add_stop_key("End");
   parser.add_key("num_segments_to_combine with SSRB", &num_segments_to_combine);
   parser.add_key("Display level",&display_level);
-	parser.add_key("zoom", &zoom);
+	//parser.add_key("zoom", &zoom);
   parser.add_key("noise filter",&noise_filter);
   parser.add_key("alpha for gridding",&alpha_gridding);
   parser.add_key("kappa for gridding",&kappa_gridding);
@@ -198,7 +198,7 @@ GRD2DReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v,
 			const double noise_filter_v, 
 			const double alpha_gridding_v, 
 			const double kappa_gridding_v, 
-			const double zoom_v,
+			//const double zoom_v,
 		    const int num_segments_to_combine_v
 )
 {
@@ -207,7 +207,7 @@ GRD2DReconstruction(const shared_ptr<ProjData>& proj_data_ptr_v,
   noise_filter = noise_filter_v; 
   alpha_gridding = alpha_gridding_v; 
   kappa_gridding = kappa_gridding_v; 
-  zoom = zoom_v;
+  //zoom = zoom_v;
   num_segments_to_combine = num_segments_to_combine_v;
   proj_data_ptr = proj_data_ptr_v;
   // have to check here because we're not parsing
@@ -444,7 +444,7 @@ actual_reconstruct(shared_ptr<DiscretisedDensity<3,float> > const & density_ptr)
     const float Rmax  = 0.5f * std::min((sx_im - 1) * vx, (sy_im - 1) * vy);
 
 		
-	if(image.get_x_size() != sp || zoom != 1) { 
+	if(image.get_x_size() != sp) { 
 		// perform bilinear interpolation 
 		if(iz==0) 
 		    info(stir::format("Image dimension mismatch: tangential positions {}, xy output {} — interpolating...",
@@ -481,7 +481,7 @@ actual_reconstruct(shared_ptr<DiscretisedDensity<3,float> > const & density_ptr)
 					float yb = img[y0+1][x0]*tx + img[y0+1][x0+1]*(1.-tx); 
 					val = ya*ty + yb*(1.-ty); 
 				}
-				image[iz][sx-1+1-ix+min_xy][sy-iy+min_xy] = val*pow(1.*sp/sp1,2.)/zoom*2.832;  
+				image[iz][sx-1+1-ix+min_xy][sy-iy+min_xy] = val*pow(1.*sp/sp1,2.)*8.890;// TODO write 8.890 as 4.445*zoom 
 			}
 		}
  
@@ -506,7 +506,7 @@ actual_reconstruct(shared_ptr<DiscretisedDensity<3,float> > const & density_ptr)
                 if (x_mm*x_mm + y_mm*y_mm > Rmax*Rmax)
                     image[iz][ix+min_xy][iy+min_xy] = 0;
                 else
-                    image[iz][sp-1+1-ix+min_xy][sp-iy+min_xy] = img[iy][ix]*sp/sp1*2.832;
+                    image[iz][sp-1+1-ix+min_xy][sp-iy+min_xy] = img[iy][ix]*sp/sp1*8.890;//TODO write 8.890 as 4.445*zoom
             }
         }
     }
