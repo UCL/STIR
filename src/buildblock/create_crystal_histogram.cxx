b58babca9cd43a63039cd80eaa8de0d80453070b
Markus Jehl <markus.jehl@positrigo.com>
2025-11-17 16:42:30 +0000

    Renaming the function to something more descriptive.

--- src/buildblock/create_crystal_histogram.cxx
+++ src/buildblock/create_crystal_histogram.cxx
@@ -3,7 +3,7 @@
   \file
   \ingroup projdata
 
-  \brief Implementation of stir::create_prompt_histogram
+  \brief Implementation of stir::create_crystal_histogram
 
   \author Kris Thielemans
 
@@ -17,7 +17,7 @@
   See STIR/LICENSE.txt for details
 */
 
-#include "stir/create_prompt_histogram.h"
+#include "stir/create_crystal_histogram.h"
 #include "stir/ProjData.h"
 #include "stir/ProjDataInfoCylindricalNoArcCorr.h"
 #include "stir/ProjDataInfoBlocksOnCylindricalNoArcCorr.h"
@@ -31,19 +31,19 @@ START_NAMESPACE_STIR
 // declaration of local function that does the work
 template <class TProjDataInfo>
 void
-create_prompt_histogram_help(Array<2, float>& prompt_histogram, const ProjData& prompt_proj_data, const TProjDataInfo& proj_data_info)
+create_crystal_histogram_help(Array<2, float>& histogram, const ProjData& proj_data, const TProjDataInfo& proj_data_info)
 {
   const auto non_tof_proj_data_info_sptr = std::dynamic_pointer_cast<TProjDataInfo>(proj_data_info.create_non_tof_clone());
   Bin bin;
 
-  for (bin.segment_num() = prompt_proj_data.get_min_segment_num(); bin.segment_num() <= prompt_proj_data.get_max_segment_num();
+  for (bin.segment_num() = proj_data.get_min_segment_num(); bin.segment_num() <= proj_data.get_max_segment_num();
        ++bin.segment_num())
     {
-      for (bin.axial_pos_num() = prompt_proj_data.get_min_axial_pos_num(bin.segment_num());
-           bin.axial_pos_num() <= prompt_proj_data.get_max_axial_pos_num(bin.segment_num());
+      for (bin.axial_pos_num() = proj_data.get_min_axial_pos_num(bin.segment_num());
+           bin.axial_pos_num() <= proj_data.get_max_axial_pos_num(bin.segment_num());
            ++bin.axial_pos_num())
         {
-          auto sinogram = prompt_proj_data.get_sinogram(bin.axial_pos_num(), bin.segment_num());
+          auto sinogram = proj_data.get_sinogram(bin.axial_pos_num(), bin.segment_num());
 #ifdef STIR_OPENMP
 #  if _OPENMP >= 200711
 #    pragma omp parallel for collapse(2) // OpenMP 3.1
@@ -51,10 +51,10 @@ create_prompt_histogram_help(Array<2, float>& prompt_histogram, const ProjData& 
 #    pragma omp parallel for // older versions
 #  endif
 #endif
-          for (int view_num = prompt_proj_data.get_min_view_num(); view_num <= prompt_proj_data.get_max_view_num(); ++view_num)
+          for (int view_num = proj_data.get_min_view_num(); view_num <= proj_data.get_max_view_num(); ++view_num)
             {
-              for (int tangential_pos_num = prompt_proj_data.get_min_tangential_pos_num();
-                   tangential_pos_num <= prompt_proj_data.get_max_tangential_pos_num();
+              for (int tangential_pos_num = proj_data.get_min_tangential_pos_num();
+                   tangential_pos_num <= proj_data.get_max_tangential_pos_num();
                    ++tangential_pos_num)
                 {
                   // Construct bin with appropriate values
@@ -79,54 +79,51 @@ create_prompt_histogram_help(Array<2, float>& prompt_histogram, const ProjData& 
                       {
 #  endif
 #endif
-                      prompt_histogram[p1.axial_coord()][p1.tangential_coord()] += count;
+                      histogram[p1.axial_coord()][p1.tangential_coord()] += count;
 #if defined(STIR_OPENMP)
 #  if _OPENMP >= 201012
 #    pragma omp atomic update
 #  endif
 #endif
-                      prompt_histogram[p2.axial_coord()][p2.tangential_coord()] += count;
+                      histogram[p2.axial_coord()][p2.tangential_coord()] += count;
 #if defined(STIR_OPENMP) && _OPENMP < 201012
-                      }
-#endif
                     }
+#endif
                 }
-              
             }
         }
     }
 }
+}
 
 void
-create_prompt_histogram(Array<2, float>& prompt_histogram, const ProjData& prompt_proj_data)
+create_crystal_histogram(Array<2, float>& histogram, const ProjData& proj_data)
 {
-  if (prompt_proj_data.get_proj_data_info_sptr()->get_scanner_ptr()->get_scanner_geometry() == "Cylindrical")
+  if (proj_data.get_proj_data_info_sptr()->get_scanner_ptr()->get_scanner_geometry() == "Cylindrical")
     {
       auto proj_data_info_ptr
-          = dynamic_cast<const ProjDataInfoCylindricalNoArcCorr* const>(prompt_proj_data.get_proj_data_info_sptr().get());
+          = dynamic_cast<const ProjDataInfoCylindricalNoArcCorr* const>(proj_data.get_proj_data_info_sptr().get());
 
       if (proj_data_info_ptr == 0)
         {
           error("Can only process not arc-corrected data\n");
         }
-      create_prompt_histogram_help(prompt_histogram, prompt_proj_data, *proj_data_info_ptr);
+      create_crystal_histogram_help(histogram, proj_data, *proj_data_info_ptr);
     }
   else
     {
       auto proj_data_info_ptr
-          = dynamic_cast<const ProjDataInfoBlocksOnCylindricalNoArcCorr* const>(prompt_proj_data.get_proj_data_info_sptr().get());
+          = dynamic_cast<const ProjDataInfoBlocksOnCylindricalNoArcCorr* const>(proj_data.get_proj_data_info_sptr().get());
 
       if (proj_data_info_ptr == 0)
         {
           error("Can only process not arc-corrected data\n");
         }
-      create_prompt_histogram_help(prompt_histogram, prompt_proj_data, *proj_data_info_ptr);
+      create_crystal_histogram_help(histogram, proj_data, *proj_data_info_ptr);
     }
 }
 
-template void
-create_prompt_histogram_help(Array<2, float>&, const ProjData&, const ProjDataInfoCylindricalNoArcCorr&);
-template void
-create_prompt_histogram_help(Array<2, float>&, const ProjData&, const ProjDataInfoBlocksOnCylindricalNoArcCorr&);
+template void create_crystal_histogram_help(Array<2, float>&, const ProjData&, const ProjDataInfoCylindricalNoArcCorr&);
+template void create_crystal_histogram_help(Array<2, float>&, const ProjData&, const ProjDataInfoBlocksOnCylindricalNoArcCorr&);
 
 END_NAMESPACE_STIR
