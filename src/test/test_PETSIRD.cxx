ed233bb4ab638050202f8318a90b3430f99f3dc1
NikEfth <nikos.efthimiou@gmail.com>
2025-12-25 16:21:20 +0100

    pre-commit

--- src/test/test_PETSIRD.cxx
+++ src/test/test_PETSIRD.cxx
@@ -1,3 +1,21 @@
+
+/*
+    Copyright (C) 2025, UMCG
+    This file is part of STIR.
+
+    SPDX-License-Identifier: Apache-2.0
+    See STIR/LICENSE.txt for details
+*/
+/*!
+
+  \file
+  \ingroup test
+
+  \brief Test program for stir::PETSIRD hierarchy
+
+  \author Nikos Efthimiou
+
+*/
 #include "stir/PETSIRDInfo.h"
 #include "stir/RunTests.h"
 #include "stir/Succeeded.h"
@@ -10,14 +28,12 @@ public:
   void run_tests() override;
 
 private:
-    shared_ptr<PETSIRDInfo> petsird_info_sptr;
+  shared_ptr<PETSIRDInfo> petsird_info_sptr;
 };
 
 void
 PETSIRDTests::run_tests()
-{
-  
-}
+{}
 
 END_NAMESPACE_STIR
 
@@ -26,7 +42,6 @@ USING_NAMESPACE_STIR
 int
 main()
 {
-
   PETSIRDTests test;
   test.run_tests();
   return test.main_return_value();
