51eeb6c8d7f3128409781ffeccf8e16370581133
NikEfth <nikos.efthimiou@gmail.com>
2025-12-25 16:18:22 +0100

    Work on tests.

---
+++ src/test/test_PETSIRD.cxx
@@ -0,0 +1,33 @@
+#include "stir/PETSIRDInfo.h"
+#include "stir/RunTests.h"
+#include "stir/Succeeded.h"
+
+START_NAMESPACE_STIR
+
+class PETSIRDTests : public RunTests
+{
+public:
+  void run_tests() override;
+
+private:
+    shared_ptr<PETSIRDInfo> petsird_info_sptr;
+};
+
+void
+PETSIRDTests::run_tests()
+{
+  
+}
+
+END_NAMESPACE_STIR
+
+USING_NAMESPACE_STIR
+
+int
+main()
+{
+
+  PETSIRDTests test;
+  test.run_tests();
+  return test.main_return_value();
+}
