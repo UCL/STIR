d681366db348daf6e7124c743d6fbbf58622c3d9
NikEfth <nikos.efthimiou@gmail.com>
2026-06-27 00:27:39 +0100

    PETSIRD: first working version
    Using PETSIRD 0.9.1

--- src/include/stir/IO/PETSIRDCListmodeInputFileFormat.h
+++ src/include/stir/IO/PETSIRDCListmodeInputFileFormat.h
@@ -1,32 +1,18 @@
-/* PETSIRDCListmodeInputFileFormat.h
-
- Class defining input file format for coincidence listmode data for PETSIRD.
-
-        Copyright 2015 ETH Zurich, Institute of Particle Physics
-        Copyright 2020 Positrigo AG, Zurich
+/*
+        Copyright 2025, 2026 University Medical Center Groningen
         Copyright 2025 National Physical Laboratory
 
-        Licensed under the Apache License, Version 2.0 (the "License");
-        you may not use this file except in compliance with the License.
-        You may obtain a copy of the License at
-
-                http://www.apache.org/licenses/LICENSE-2.0
-
-        Unless required by applicable law or agreed to in writing, software
-        distributed under the License is distributed on an "AS IS" BASIS,
-        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-        See the License for the specific language governing permissions and
-        limitations under the License.
+    SPDX-License-Identifier: Apache-2.0
+    See STIR/LICENSE.txt for details
  */
 
 /*!
 
   \file
   \ingroup listmode
-  \brief Declaration of class stir::PETSIRDCListmodeInputFileFormat
+  \brief Class defining input file format for coincidence listmode data for PETSIRD.
 
-  \author Jannis Fischer
-  \author Markus Jehl, Positrigo
+  \author Nikos Efthimiou
   \author Daniel Deidda
 
 */
@@ -44,11 +30,7 @@
 START_NAMESPACE_STIR
 
 /*! \brief Class for reading PETSIRD coincidence listmode data.
-
-  The first 32 bytes of the binary file are interpreted as file signature and matched against the strings "MUPET CListModeData\0",
-"PETSIRD". If either is successfull, the class claims it can read the file format. The
-rest of the file is read as records, e.g. CListRecordPETSIRD.
-*/
+ */
 
 class PETSIRDCListmodeInputFileFormat : public InputFileFormat<ListModeData>
 {
@@ -56,23 +38,23 @@ public:
   const std::string get_name() const override { return "PETSIRD"; }
 
   //! Checks in binary data file for correct signature.
-  bool can_read(const FileSignature& signature, const std::string& filename) override;
+  bool can_read(const FileSignature& signature, const std::string& filename) const override;
 
 protected:
   bool actual_can_read(const FileSignature& signature, std::istream& input) const override { return false; }
 
-  bool use_hdf5 = false;
+  mutable bool use_hdf5 = false;
 
 public:
   unique_ptr<data_type> read_from_file(std::istream& input) const override
   {
-    error("read_from_file for ROOT listmode data with istream not implemented %s:%s. Sorry", __FILE__, __LINE__);
+    error("read_from_file for PETSIRD listmode data with istream not implemented. Sorry");
     return unique_ptr<data_type>();
   }
 
   unique_ptr<data_type> read_from_file(const std::string& filename) const override
   {
-    info("PETSIRDCListmodeInputFileFormat: read_from_file(" + std::string(filename) + ")");
+    info("PETSIRDCListmodeInputFileFormat: read_from_file(" + filename + ")");
     return unique_ptr<data_type>(new CListModeDataPETSIRD(filename, use_hdf5));
   }
 };
--- src/listmode_buildblock/CListModeDataSAFIR.cxx
+++ src/listmode_buildblock/CListModeDataSAFIR.cxx
@@ -1,32 +1,20 @@
-/* CListModeDataSAFIR.cxx
-
-Coincidence LM Data Class for SAFIR: Implementation
-
+/*
      Copyright 2015 ETH Zurich, Institute of Particle Physics
      Copyright 2020 Positrigo AG, Zurich
- Copyright 2021 University College London
+     Copyright 2021 University College London
 
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-
-             http://www.apache.org/licenses/LICENSE-2.0
-
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
+     SPDX-License-Identifier: Apache-2.0
+     See STIR/LICENSE.txt for detail
 */
 /*!
 
-\file
-\ingroup listmode
-\brief implementation of class stir::CListModeDataSAFIR
+  \file
+  \ingroup listmode
+  \brief implementation of class stir::CListModeDataSAFIR
 
-\author Jannis Fischer
-\author Kris Thielemans
-\author Markus Jehl
+  \author Jannis Fischer
+  \author Kris Thielemans
+  \author Markus Jehl
 */
 #include <iostream>
 #include <fstream>
@@ -36,7 +24,7 @@ Coincidence LM Data Class for SAFIR: Implementation
 #include "stir/info.h"
 #include "stir/error.h"
 
-// #include "boost/static_assert.hpp"
+//#include "boost/static_assert.hpp"
 
 #include "stir/listmode/CListModeDataSAFIR.h"
 #include "stir/listmode/CListRecordSAFIR.h"
@@ -95,6 +83,31 @@ CListModeDataSAFIR<CListRecordT>::CListModeDataSAFIR(const std::string& listmode
 }
 
 template <class CListRecordT>
+shared_ptr<CListRecord>
+CListModeDataSAFIR<CListRecordT>::get_empty_record_sptr() const
+{
+  shared_ptr<CListRecordT> sptr(new CListRecordT);
+  sptr->event_SAFIR().set_scanner_sptr(this->get_proj_data_info_sptr()->get_scanner_sptr());
+  sptr->event_SAFIR().set_map_sptr(map);
+  return static_pointer_cast<CListRecord>(sptr);
+}
+
+template <class CListRecordT>
+Succeeded
+CListModeDataSAFIR<CListRecordT>::get_next_record(CListRecord& record_of_general_type) const
+{
+  auto& record = static_cast<CListRecordT&>(record_of_general_type);
+  return current_lm_data_ptr->get_next_record(record);
+}
+
+template <class CListRecordT>
+Succeeded
+CListModeDataSAFIR<CListRecordT>::reset()
+{
+  return current_lm_data_ptr->reset();
+}
+
+template <class CListRecordT>
 Succeeded
 CListModeDataSAFIR<CListRecordT>::open_lm_file() const
 {
@@ -113,34 +126,7 @@ CListModeDataSAFIR<CListRecordT>::open_lm_file() const
   return Succeeded::yes;
 }
 
-template <class CListRecordT>
-shared_ptr<CListRecordT>
-CListModeDataSAFIR<CListRecordT>::get_empty_record_sptr() const
-{
-  shared_ptr<CListRecordT> sptr(new CListRecordT);
-  sptr->event_SAFIR().set_scanner_sptr(this->get_proj_data_info_sptr()->get_scanner_sptr());
-  sptr->event_SAFIR().set_map_sptr(map);
-  return static_pointer_cast<CListRecord>(sptr);
-}
-
-template <class CListRecordT>
-Succeeded
-CListModeDataSAFIR<CListRecordT>::get_next_record(CListRecordT& record_of_general_type) const
-{
-  CListRecordT& record = static_cast<CListRecordT&>(record_of_general_type);
-  Succeeded status = current_lm_data_ptr->get_next_record(record);
-  // if( status == Succeeded::yes ) record.event_SAFIR().set_map_sptr(map);
-  return status;
-}
-
-template <class CListRecordT>
-Succeeded
-CListModeDataSAFIR<CListRecordT>::reset()
-{
-  return current_lm_data_ptr->reset();
-}
-
-// template class CListModeDataSAFIR<CListRecordSAFIR<CListEventDataSAFIR>>;
-// template class CListModeDataSAFIR<CListRecordSAFIR<CListEventDataNeuroLF>>;
+template class CListModeDataSAFIR<CListRecordSAFIR<CListEventDataSAFIR>>;
+template class CListModeDataSAFIR<CListRecordSAFIR<CListEventDataNeuroLF>>;
 
 END_NAMESPACE_STIR
