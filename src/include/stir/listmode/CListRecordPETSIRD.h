e1dc4b365cf5b713f2d9f4b3a166f09bf5d25edc
Kris Thielemans@ucl.ac.uk <k.thielemans@ucl.ac.uk>
2026-07-16 22:51:32 +0100

    PETSIRD: move a function to .cxx

--- src/include/stir/listmode/CListRecordPETSIRD.h
+++ src/include/stir/listmode/CListRecordPETSIRD.h
@@ -24,7 +24,6 @@
 #include "stir/DetectionPositionPair.h"
 #include "stir/PETSIRDInfo.h"
 #include "stir/Succeeded.h"
-#include "stir/format.h"
 
 START_NAMESPACE_STIR
 
@@ -126,38 +125,7 @@ public:
 
   bool operator==(const CListRecordPETSIRD& e2) const { return event_data == e2.event_data && time_data == e2.time_data; }
 
-  Succeeded init_from_data(const petsird::CoincidenceEvent& event, const bool is_prompt)
-  {
-    const auto scanner_info_sptr = petsird_info_sptr->get_petsird_scanner_info_sptr();
-
-    const auto exp_det_0
-        = petsird_helpers::expand_detection_bin(*scanner_info_sptr,
-                                                0, // TODO type_of_module, currently we only support single module types.
-                                                event.detection_bins[0]);
-    const auto exp_det_1
-        = petsird_helpers::expand_detection_bin(*scanner_info_sptr,
-                                                0, // TODO type_of_module, currently we only support single module types.
-                                                event.detection_bins[1]);
-    auto it0 = petsird_info_sptr->get_petsird_to_stir_map()->find(exp_det_1);
-    auto it1 = petsird_info_sptr->get_petsird_to_stir_map()->find(exp_det_0);
-    if (it0 == petsird_info_sptr->get_petsird_to_stir_map()->end() || it1 == petsird_info_sptr->get_petsird_to_stir_map()->end())
-      {
-        error(format("get_stir_det_pos_from_PETSIRD_id: one or both PETSIRD ids (mod {}, elem {}, energy {}) not found",
-                     exp_det_0.module_index,
-                     exp_det_0.element_index,
-                     exp_det_0.energy_index));
-      }
-
-    // Warning: this assumes that the PETSIRD TOF bins and the STIR ProjDataInfo
-    // timing positions have the same binning/mashing and number of TOF bins.
-    // If the STIR proj_data_info uses a different TOF mashing factor or TOF range,
-    // this simple offset conversion is not valid.
-    const DetectionPositionPair<> det_pos_pair(
-        it0->second, it1->second, static_cast<int>(event.tof_idx) + this->proj_data_info_sptr->get_min_tof_pos_num());
-    this->event_data->set_detection_position(det_pos_pair);
-    this->event_data->set_prompt(is_prompt);
-    return Succeeded::yes;
-  }
+  Succeeded init_from_data(const petsird::CoincidenceEvent& event, const bool is_prompt);
 
 private:
   static std::unique_ptr<CListEventScannerWithDiscreteDetectorsBase>
